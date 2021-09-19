5105ec0dd166db22b03d5d374bdf5eccd4303473
KIM TAE HEE <taehee.kim.dev@gmail.com>
2021-09-20 03:03:05 +0900

    refactor: 공백 제거, UserDao에서 JdbcTemplate만 주입받도록 수정

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,7 +2,6 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import java.util.List;
-import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
 import nextstep.jdbc.rowmapper.RowMapper;
 
@@ -18,42 +17,32 @@ public class UserDao {
 
     private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    public UserDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public List<User> findAll() {
-
         final String sql = "select id, account, password, email from users";
-
         return jdbcTemplate.query(sql, rowMapper);
     }
 
     public User findByAccount(String account) {
-
         final String sql = "select id, account, password, email from users where account = ?";
-
         return jdbcTemplate.query(sql, rowMapper, account);
     }
 
     public User findById(Long id) {
-
         final String sql = "select id, account, password, email from users where id = ?";
-
         return jdbcTemplate.query(sql, rowMapper, id);
     }
 
     public void insert(User user) {
-
         final String sql = "insert into users (account, password, email) values (?, ?, ?)";
-
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(User user) {
-
         final String sql = "update users set account = ?, password = ?, email = ? where id = ?";
-
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 }
