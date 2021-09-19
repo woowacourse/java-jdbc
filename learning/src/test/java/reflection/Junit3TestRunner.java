47890b90b048741a26ccd0d365facf7067d88ea3
ROOT <junroot0909@gmail.com>
2021-09-19 21:04:40 +0900

    test: 학습 테스트 완료

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -1,13 +1,39 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Junit3TestRunner {
 
+    private ByteArrayOutputStream byteArrayOutputStream;
+
+    @BeforeEach
+    void setUp() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+    }
+
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
+        Junit3Test junit3Test = clazz.getConstructor().newInstance();
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+            .filter(method -> method.getName().startsWith("test"))
+            .forEach(method -> {
+                try {
+                    method.invoke(junit3Test);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            });
+
+        assertThat("Running Test1" + System.lineSeparator() + "Running Test2" + System.lineSeparator()).isEqualTo(byteArrayOutputStream.toString());
     }
 }
--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,39 @@
 package reflection;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
+    private ByteArrayOutputStream byteArrayOutputStream;
+
+    @BeforeEach
+    void setUp() {
+        byteArrayOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(byteArrayOutputStream));
+    }
+
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Junit4Test junit4Test = clazz.getConstructor().newInstance();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        Arrays.stream(clazz.getDeclaredMethods())
+            .filter(method -> method.isAnnotationPresent(MyTest.class))
+            .forEach(method -> {
+                try {
+                    method.invoke(junit4Test);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            });
+
+        assertThat("Running Test1" + System.lineSeparator() + "Running Test2" + System.lineSeparator()).isEqualTo(byteArrayOutputStream.toString());
     }
 }
