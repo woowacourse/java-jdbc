e9698fcffb77e7a7a24e286be9a7e6b08486525e
Seed <hsik0225@gmail.com>
2021-09-21 01:20:07 +0900

    refactor: 기존 DataSource에서 JdbcTemplate을 사용하도록 수정

--- app/src/main/java/com/techcourse/support/context/ContextLoaderListener.java
+++ app/src/main/java/com/techcourse/support/context/ContextLoaderListener.java
@@ -2,15 +2,17 @@ package com.techcourse.support.context;
 
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import nextstep.jdbc.JdbcTemplate;
 
 @WebListener
 public class ContextLoaderListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
+        DatabasePopulatorUtils.execute(new JdbcTemplate(DataSourceConfig.getInstance()));
     }
 }
--- app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
+++ app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
@@ -1,45 +1,26 @@
 package com.techcourse.support.jdbc.init;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import nextstep.jdbc.JdbcTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatabasePopulatorUtils {
 
     private static final Logger log = LoggerFactory.getLogger(DatabasePopulatorUtils.class);
 
-    public static void execute(DataSource dataSource) {
-        Connection connection = null;
-        Statement statement = null;
+    public static void execute(JdbcTemplate jdbcTemplate) {
         try {
             final URL url = DatabasePopulatorUtils.class.getClassLoader().getResource("schema.sql");
             final File file = new File(url.getFile());
             final String sql = Files.readString(file.toPath());
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-            statement.execute(sql);
-        } catch (NullPointerException | IOException | SQLException e) {
+            jdbcTemplate.update(sql);
+        } catch (NullPointerException | IOException e) {
             log.error(e.getMessage(), e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ignored) {}
         }
     }
 
