9e717c6a8eed438330f839e6eb5c82c0ddbc6280
gomding <backchida@gmail.com>
2021-09-20 15:47:52 +0900

    test: 학습테스트 진행

--- learning/src/test/java/callback/CallbackTest.java
+++ learning/src/test/java/callback/CallbackTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 class CallbackTest {
 
     private static final Logger log = LoggerFactory.getLogger(CallbackTest.class);
@@ -11,6 +13,7 @@ class CallbackTest {
     @Test
     void callback() {
         final SimpleTask task = new SimpleTask();
-        task.executeWith(() -> log.info("I'm done now."));
+        Callback callback = () -> log.info("I'm done now.");
+        assertDoesNotThrow(() -> task.executeWith(callback));
     }
 }
--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -1,18 +1,39 @@
 package reflection;
 
+import annotation.Controller;
+import annotation.Repository;
+import annotation.Service;
 import org.junit.jupiter.api.Test;
 import org.reflections.Reflections;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ReflectionsTest {
 
     private static final Logger log = LoggerFactory.getLogger(ReflectionsTest.class);
 
     @Test
-    void showAnnotationClass() throws Exception {
+    void showAnnotationClass() {
         Reflections reflections = new Reflections("examples");
 
         // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
+        Set<Class<?>> controllers = reflections.getTypesAnnotatedWith(Controller.class);
+        log.info("Controller : {}", controllers);
+        int expectedControllersSize = 1;
+        assertThat(controllers).hasSize(expectedControllersSize);
+
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        log.info("Service : {}", services);
+        int expectedServicesSize = 1;
+        assertThat(services).hasSize(expectedServicesSize);
+
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+        log.info("Repository : {}", repositories);
+        int expectedRepositoriesSize = 2;
+        assertThat(repositories).hasSize(expectedRepositoriesSize);
     }
 }
