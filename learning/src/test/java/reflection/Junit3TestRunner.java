41e9ce03923689f43b3fa3c38dbf606f988c9312
김주원 <kjw11077@naver.com>
2021-09-21 16:10:46 +0900

    test: 학습 테스트

--- learning/src/test/java/reflection/Junit3TestRunner.java
+++ learning/src/test/java/reflection/Junit3TestRunner.java
@@ -2,12 +2,25 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+
 class Junit3TestRunner {
 
     @Test
     void run() throws Exception {
         Class<Junit3Test> clazz = Junit3Test.class;
 
-        // TODO Junit3Test에서 test로 시작하는 메소드 실행
+        Junit3Test junit3Test = clazz.getConstructor().newInstance();
+
+        Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.getName().startsWith("test"))
+                .forEach(method -> {
+                    try {
+                        method.invoke(junit3Test);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        e.printStackTrace();
+                    }
+                });
     }
 }
