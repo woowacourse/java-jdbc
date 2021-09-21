7f8f115d561831630b7b6bf27fa5b45059ce533f
NewWisdom <wlgp2500@gmail.com>
2021-09-21 22:49:22 +0900

    refactor(): 커스텀 예외 적용

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -4,6 +4,8 @@ import nextstep.jdbc.core.ArgumentPreparedStatementSetter;
 import nextstep.jdbc.core.PreparedStatementSetter;
 import nextstep.jdbc.core.RowMapper;
 import nextstep.jdbc.core.RowMapperResultSetExtractor;
+import nextstep.jdbc.exception.DataAccessException;
+import nextstep.jdbc.exception.NotSingleResultDataException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +15,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Objects;
 
 public class JdbcTemplate<T> {
 
@@ -21,14 +24,14 @@ public class JdbcTemplate<T> {
     private final DataSource dataSource;
 
     public JdbcTemplate(DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.dataSource = Objects.requireNonNull(dataSource, "DataSource is required");
     }
 
     public void update(String sql, Object... args) {
         update(sql, new ArgumentPreparedStatementSetter(args));
     }
 
-    public void update(String sql, PreparedStatementSetter pss) {
+    private void update(String sql, PreparedStatementSetter pss) {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pss.setValues(pstmt);
@@ -36,12 +39,13 @@ public class JdbcTemplate<T> {
             log.debug("query : {}", sql);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e.getMessage());
         }
     }
 
     public T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
         List<T> results = query(sql, rowMapper, args);
+        validateSingle(results);
         return results.iterator().next();
     }
 
@@ -49,7 +53,7 @@ public class JdbcTemplate<T> {
         return query(sql, new RowMapperResultSetExtractor<>(rowMapper), new ArgumentPreparedStatementSetter(args));
     }
 
-    public List<T> query(String sql, RowMapperResultSetExtractor<T> extractor, PreparedStatementSetter pss) {
+    private List<T> query(String sql, RowMapperResultSetExtractor<T> extractor, PreparedStatementSetter pss) {
         ResultSet rs = null;
         try (Connection conn = dataSource.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -59,14 +63,27 @@ public class JdbcTemplate<T> {
             return extractor.extractData(rs);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e.getMessage());
         } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
+            closeResultSet(rs);
+        }
+    }
+
+    private void validateSingle(List<T> results) {
+        if (Objects.isNull(results) || results.isEmpty()) {
+            throw new NotSingleResultDataException("Result is Empty");
+        }
+        if (results.size() > 1) {
+            throw new NotSingleResultDataException("Result size is " + results.size());
+        }
+    }
+
+    private void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
             }
+        } catch (SQLException ignored) {
         }
     }
 }
