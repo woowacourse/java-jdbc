ac390bb57ae2ab38527bca698487e1fa7ca742f7
멍토 <daum7766@naver.com>
2021-09-21 16:13:17 +0900

    style: 완료한 todo 삭제

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -9,7 +9,6 @@ class Junit4TestRunner {
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
         Method[] declaredMethods = clazz.getDeclaredMethods();
         Junit4Test instance = clazz.getDeclaredConstructor().newInstance();
         for (Method method : declaredMethods) {
--- learning/src/test/java/reflection/ReflectionsTest.java
+++ learning/src/test/java/reflection/ReflectionsTest.java
@@ -17,7 +17,6 @@ class ReflectionsTest {
     void showAnnotationClass() throws Exception {
         Reflections reflections = new Reflections("examples");
 
-        // TODO 클래스 레벨에 @Controller, @Service, @Repository 애노테이션이 설정되어 모든 클래스 찾아 로그로 출력한다.
         Set<Class<?>> classes = reflections.getTypesAnnotatedWith(Service.class);
         classes.addAll(reflections.getTypesAnnotatedWith(Controller.class));
         classes.addAll(reflections.getTypesAnnotatedWith(Repository.class));
