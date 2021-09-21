ed0aa4253bd24193a77f98e33132e6621bcaccf0
cjy17 <cjy1705@naver.com>
2021-09-21 10:55:28 +0900

    feat: jdbcTemplate 필드로 이동

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,26 +1,24 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import nextstep.jdbc.AbstractJdbcTemplate;
 import nextstep.jdbc.JdbcTemplate;
 import nextstep.jdbc.RowMapper;
+import nextstep.web.annotation.Repository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     private final RowMapper<User> userRowMapper = rs -> new User(
@@ -30,35 +28,28 @@ public class UserDao {
         rs.getString("email")
     );
 
-
-
     public void insert(User user) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         String sql = "insert into users (account, password, email) values (?, ?, ?)";
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
 
     public void update(User user) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         String sql = "update users set password = ?, email = ? where account = ? ";
         jdbcTemplate.update(sql, user.getPassword(), user.getEmail(), user.getAccount());
     }
 
     public List<User> findAll() {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         String sql = "select id, account, password, email from users";
         return (List<User>) jdbcTemplate.query(sql, userRowMapper);
     }
 
     public User findById(Long id) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         String sql = "select id, account, password, email from users where id = ?";
         return (User) jdbcTemplate.query(sql, userRowMapper, id);
     }
 
     public User findByAccount(String account) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
         String sql = "select id, account, password, email from users where account = ?";
         return (User) jdbcTemplate.query(sql, userRowMapper, account);
     }
