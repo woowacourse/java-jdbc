e71463229fd0720ab2fe803623a716cd1db9ea0d
cjy17 <cjy1705@naver.com>
2021-09-21 15:33:36 +0900

    feat: abstractJdbcTemplate 제거

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,5 +1,10 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
 
@@ -13,51 +18,53 @@ public class JdbcTemplate {
 
     public void update(String sql, Object ... parameters) {
         PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetterImpl(parameters);
-        AbstractJdbcTemplate abstractJdbcTemplate = new AbstractJdbcTemplate() {
-            @Override
-            protected String createQuery() {
-                return sql;
-            }
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            @Override
-            protected DataSource getDataSource() {
-                return dataSource;
-            }
-
-        };
-        abstractJdbcTemplate.update(preparedStatementSetter);
+            preparedStatementSetter.setValues(pstmt);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object ... parameters) {
         PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetterImpl(parameters);
-        AbstractJdbcTemplate abstractJdbcTemplate = new AbstractJdbcTemplate() {
-            @Override
-            protected String createQuery() {
-                return sql;
-            }
+        ResultSet rs = null;
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+        ) {
+            preparedStatementSetter.setValues(pstmt);
+            rs = pstmt.executeQuery();
 
-            @Override
-            protected DataSource getDataSource() {
-                return dataSource;
+            if (rs.next()) {
+                return rowMapper.mapRow(rs);
             }
-
-        };
-        return abstractJdbcTemplate.queryForObject(preparedStatementSetter, rowMapper);
+            throw new IllegalArgumentException();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {}
+        }
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
-        AbstractJdbcTemplate abstractJdbcTemplate = new AbstractJdbcTemplate() {
-            @Override
-            protected String createQuery() {
-                return sql;
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            ResultSet rs = pstmt.executeQuery()) {
+
+            List<T> list = new ArrayList<>();
+            while (rs.next()) {
+                list.add(rowMapper.mapRow(rs));
             }
 
-            @Override
-            protected DataSource getDataSource() {
-                return dataSource;
-            }
-
-        };
-        return abstractJdbcTemplate.query(rowMapper);
+            return list;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
