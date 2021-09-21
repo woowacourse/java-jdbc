64eb1c3a63923e0ffd457e364b6393b85ce91a8c
NewWisdom <wlgp2500@gmail.com>
2021-09-22 00:03:18 +0900

    refactor(): user-dao 사용으로 변경

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -1,7 +1,8 @@
 package com.techcourse.controller;
 
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.dao.UserDao;
 import com.techcourse.domain.User;
-import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
@@ -18,12 +19,14 @@ public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
+    private final UserDao userDao = new UserDao(DataSourceConfig.getInstance());
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public ModelAndView view(HttpServletRequest request, HttpServletResponse response) {
         return UserSession.getUserFrom(request.getSession())
                 .map(user -> {
                     log.info("logged in {}", user.getAccount());
-                    return redirect("/index.jsp");
+                    return redirect("/");
                 })
                 .orElse(new ModelAndView(new JspView("/login.jsp")));
     }
@@ -31,10 +34,9 @@ public class LoginController {
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public ModelAndView login(HttpServletRequest request, HttpServletResponse response) {
         if (UserSession.isLoggedIn(request.getSession())) {
-            return redirect("/index.jsp");
+            return redirect("/");
         }
-
-        return InMemoryUserRepository.findByAccount(request.getParameter("account"))
+        return userDao.findByAccount(request.getParameter("account"))
                 .map(user -> {
                     log.info("User : {}", user);
                     return login(request, user);
@@ -46,7 +48,7 @@ public class LoginController {
         if (user.checkPassword(request.getParameter("password"))) {
             final HttpSession session = request.getSession();
             session.setAttribute(UserSession.SESSION_KEY, user);
-            return redirect("/index.jsp");
+            return redirect("/");
         } else {
             return redirect("/401.jsp");
         }
--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,7 +1,8 @@
 package com.techcourse.controller;
 
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.dao.UserDao;
 import com.techcourse.domain.User;
-import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import nextstep.mvc.view.JspView;
@@ -13,15 +14,20 @@ import nextstep.web.support.RequestMethod;
 @Controller
 public class RegisterController {
 
+    private final UserDao userDao = new UserDao(DataSourceConfig.getInstance());
+
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ModelAndView register(HttpServletRequest request, HttpServletResponse response) {
+        final String account = request.getParameter("account");
         final User user = new User(2,
-                request.getParameter("account"),
+                account,
                 request.getParameter("password"),
                 request.getParameter("email"));
-        InMemoryUserRepository.save(user);
-
-        return new ModelAndView(new JspView("redirect:/index.jsp"));
+        if (userDao.findByAccount(account).isPresent()) {
+            return new ModelAndView(new JspView("redirect:/500.jsp"));
+        }
+        userDao.insert(user);
+        return new ModelAndView(new JspView("redirect:/"));
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
