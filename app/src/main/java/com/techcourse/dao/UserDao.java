eef0730fc98697244da9bac6d76306123770b9eb
ROOT <junroot0909@gmail.com>
2021-09-22 20:15:14 +0900

    refactor: 불필요한 임포트 제거

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,21 +1,13 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
 import nextstep.jdbc.RowMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
     private static final RowMapper<User> USER_ROW_MAPPER = resultSet -> new User(
         resultSet.getLong(1),
         resultSet.getString(2),
--- jdbc/src/test/java/nextstep/jdbc/TestUserRowMapper.java
+++ jdbc/src/test/java/nextstep/jdbc/TestUserRowMapper.java
@@ -3,7 +3,7 @@ package nextstep.jdbc;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class TestUserRowMapper implements RowMapper<TestUser>{
+public class TestUserRowMapper implements RowMapper<TestUser> {
 
     @Override
     public TestUser map(final ResultSet resultset) throws SQLException {
