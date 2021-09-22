ccb6659e55c16e5f0166968a3d72d5ee568ff66d
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-09-22 22:33:02 +0900

    refactor: userRowMapper 메서드 분리

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,11 +2,11 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
@@ -18,46 +18,37 @@ public class UserDao {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void insert(User user) throws SQLException {
+    public void insert(User user) {
         final String sql = "insert into users (account, password, email) values (?, ?, ?)";
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
-    public void update(User user) throws SQLException {
+    public void update(User user) {
         final String sql = "update users set account = ?, password = ?, email = ? where id = ?";
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
-    public List<User> findAll() throws SQLException {
+    public List<User> findAll() {
         final String sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(sql, ((rs, rowNum) ->
-                new User(
-                        rs.getLong("id"),
-                        rs.getString("account"),
-                        rs.getString("password"),
-                        rs.getString("email")
-                )));
+        return jdbcTemplate.query(sql, userRowMapper());
     }
 
-    public User findById(Long id) throws SQLException {
+    public User findById(Long id) {
         final String sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) ->
-                new User(
-                        rs.getLong("id"),
-                        rs.getString("account"),
-                        rs.getString("password"),
-                        rs.getString("email")
-                ), id);
+        return jdbcTemplate.queryForObject(sql, userRowMapper(), id);
     }
 
-    public User findByAccount(String account) throws SQLException {
+    public User findByAccount(String account) {
         final String sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) ->
-                new User(
-                        rs.getLong("id"),
-                        rs.getString("account"),
-                        rs.getString("password"),
-                        rs.getString("email")
-                ), account);
+        return jdbcTemplate.queryForObject(sql, userRowMapper(), account);
+    }
+
+    public final RowMapper<User> userRowMapper() {
+        return (rs -> new User(
+                rs.getLong("id"),
+                rs.getString("account"),
+                rs.getString("password"),
+                rs.getString("email")
+        ));
     }
 }
--- jdbc/src/main/java/nextstep/jdbc/RowMapper.java
+++ jdbc/src/main/java/nextstep/jdbc/RowMapper.java
@@ -7,5 +7,5 @@ import java.sql.SQLException;
 @FunctionalInterface
 public interface RowMapper<T> {
     @Nullable
-    T mapRow(ResultSet rs, int rowNum) throws SQLException;
+    T mapRow(ResultSet rs) throws SQLException;
 }
