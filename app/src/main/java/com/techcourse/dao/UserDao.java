187eda78559a6391bae296e3de3d9445ddb9a952
younghoonkwon <younghoon.kwon91@gmail.com>
2021-09-24 16:07:26 +0900

    refactor: UserDao logger추가

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -19,26 +19,31 @@ public class UserDao {
     }
 
     public void insert(User user) {
+        log.info("UserDao.insert, user:{}", user.toString());
         final String sql = "insert into users (account, password, email) values (?, ?, ?)";
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(User user) {
+        log.info("UserDao.update, user:{}", user.toString());
         final String sql = "update users set password=? where id=?";
         jdbcTemplate.update(sql, user.getPassword(), user.getId());
     }
 
     public List<User> findAll() {
+        log.info("UserDao.findAll");
         final String sql = "SELECT * FROM users";
         return jdbcTemplate.query(sql, userMapper());
     }
 
     public User findById(Long id) {
+        log.info("UserDao.findById, userId:{}", id);
         final String sql = "select id, account, password, email from users where id = ?";
         return jdbcTemplate.queryForObject(sql, userMapper(), id);
     }
 
     public User findByAccount(String account) {
+        log.info("UserDao.findByAccount, userAccount:{}", account);
         final String sql = "select id, account, password, email from users where account = ?";
         return jdbcTemplate.queryForObject(sql, userMapper(), account);
     }
