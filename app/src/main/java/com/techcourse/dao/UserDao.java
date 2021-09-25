f4807e50b0320a024674947756b0d337240e7965
Aaron <whilter08@gmail.com>
2021-09-25 14:26:05 +0900

    feat: 조회 기능 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -35,58 +36,17 @@ public class UserDao {
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
+        final String sql = "select id, account, password, email from users";
+        return jdbcTemplate.queryForList(sql, User.class);
     }
 
     public User findById(Long id) {
-        final String sql = "select id, account, password, email from users where id = ?";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
-        }
+        final String sql = "select id, account, password, email from users where id=?";
+        return jdbcTemplate.queryForObject(sql, User.class, id);
     }
 
     public User findByAccount(String account) {
-        // todo
-        return null;
+        final String sql = "select id, account, password, email from users where account=?";
+        return jdbcTemplate.queryForObject(sql, User.class, account);
     }
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,8 +1,13 @@
 package nextstep.jdbc;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,8 +22,8 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(String sql, Object... params) {
-        update(sql, createPreparedStatementSetter(params));
+    public void update(String sql, Object... args) {
+        update(sql, createPreparedStatementSetter(args));
     }
 
     public void update(String sql, PreparedStatementSetter pss) {
@@ -34,15 +39,61 @@ public class JdbcTemplate {
         }
     }
 
-    private PreparedStatementSetter createPreparedStatementSetter(Object... params) {
+    public <T> List<T> queryForList(String sql, Class<T> type, Object... args) {
+        return query(sql, (rs, rowNum) -> getSingleObject(rs, type), createPreparedStatementSetter(args));
+    }
+
+    public <T> T queryForObject(String sql, Class<T> type, Object... args) {
+        return queryForObject(sql, (rs, rowNum) -> getSingleObject(rs, type), args);
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        List<T> result = query(sql, new RowMapperExtractor<>(rowMapper), createPreparedStatementSetter(args));
+
+        if (result.size() > 1) {
+            throw new IllegalStateException("조회된 데이터가 2개 이상입니다.");
+        }
+        return result.get(0);
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pss) {
+        return query(sql, new RowMapperExtractor<>(rowMapper), pss);
+    }
+
+    public <T> List<T> query(String sql, RowMapperExtractor<T> extractor, PreparedStatementSetter pss) {
+        ResultSet rs = null;
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pss.setValue(pstmt);
+            rs = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+            return extractor.extractData(rs);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> T getSingleObject(ResultSet rs, Class<T> type) throws SQLException {
+        try {
+            T object = type.getConstructor().newInstance();
+            for (Field field : object.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                field.set(object, rs.getObject(field.getName()));
+            }
+            return object;
+        } catch (Exception e) {
+            throw new SQLException();
+        }
+    }
+
+    private PreparedStatementSetter createPreparedStatementSetter(Object... args) {
         return pstmt -> {
-            for (int i = 0; i < params.length; i++) {
-                pstmt.setObject(i + 1, params[i]);
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i + 1, args[i]);
             }
         };
     }
-
-//    public <T> T queryForObject(String sql, String... args) {
-//
-//    }
 }
+
