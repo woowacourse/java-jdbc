04d2290db7355b1b0dc81824f97ab9d4d4f5e412
unluckyjung <ybook2012@gmail.com>
2021-09-26 03:29:44 +0900

    feat(UserDao): findByAccount 메소드 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -5,12 +5,8 @@ import java.util.List;
 import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
 import nextstep.jdbc.RowMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UserDao {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserDao.class);
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -44,8 +40,8 @@ public class UserDao {
         return jdbcTemplate.queryForObject(sql, userRowMapper, id);
     }
 
-    public User findByAccount(String account) {
-        // todo
-        return null;
+    public User findByAccount(final String account) {
+        String sql = "select id, account, password, email from users where account = ?";
+        return jdbcTemplate.queryForObject(sql, userRowMapper, account);
     }
 }
