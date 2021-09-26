6cc0011162cfe1bf41606c1ce21aeb1881dc93d4
TaewanKimmmm <ktww88@gmail.com>
2021-09-26 19:50:51 +0900

    refactor: SelectJdbcTemplate 추출

--- jdbc/src/main/java/com/techcourse/dao/UserDao.java
+++ jdbc/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,17 +1,15 @@
 package com.techcourse.dao;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import javax.sql.DataSource;
 
 import com.techcourse.domain.User;
 
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.SelectJdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -69,79 +67,95 @@ public class UserDao {
     }
 
     public List<User> findAll() {
-        List<User> users = new ArrayList<>();
 
-        final String sql = "select id, account, password, email from users";
+        SelectJdbcTemplate selectJdbcTemplate = new SelectJdbcTemplate() {
+            @Override
+            public DataSource getDataSource() {
+                return dataSource;
+            }
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql);
-             ResultSet resultSet = preparedStatement.executeQuery()) {
+            @Override
+            public String createQuery() {
+                return "select id, account, password, email from users";
+            }
 
-            while (resultSet.next()) {
-                users.add(
-                        new User(
-                                resultSet.getLong(1),
-                                resultSet.getString(2),
-                                resultSet.getString(3),
-                                resultSet.getString(4)
-                        )
+            @Override
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
+            }
+
+            @Override
+            public Object mapRow(ResultSet resultSet) throws SQLException {
+                return new User(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4)
                 );
             }
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-        return users;
+        };
+        return (List<User>) selectJdbcTemplate.query();
     }
 
     public User findById(Long id) {
-        final String sql = "select id, account, password, email from users where id = ?";
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
-            preparedStatement.setLong(1, id);
-
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    return new User(
-                            resultSet.getLong(1),
-                            resultSet.getString(2),
-                            resultSet.getString(3),
-                            resultSet.getString(4)
-                    );
-                }
-                throw new NoSuchElementException("존재하지 않는 데이터입니다.");
+        SelectJdbcTemplate selectJdbcTemplate = new SelectJdbcTemplate() {
+            @Override
+            public DataSource getDataSource() {
+                return dataSource;
             }
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+
+            @Override
+            public String createQuery() {
+                return "select id, account, password, email from users where id = ?";
+            }
+
+            @Override
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setLong(1, id);
+            }
+
+            @Override
+            public Object mapRow(ResultSet resultSet) throws SQLException {
+                return new User(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4)
+                );
+            }
+        };
+
+        return (User) selectJdbcTemplate.queryForObject();
     }
 
     public User findByAccount(String account) {
-        final String sql = "select id, account, password, email from users where account = ?";
 
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
-            preparedStatement.setString(1, account);
-
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-
-                if (resultSet.next()) {
-                    return new User(
-                            resultSet.getLong(1),
-                            resultSet.getString(2),
-                            resultSet.getString(3),
-                            resultSet.getString(4)
-                    );
-                }
-                throw new NoSuchElementException("존재하지 않는 데이터입니다.");
+        SelectJdbcTemplate selectJdbcTemplate = new SelectJdbcTemplate() {
+            @Override
+            public DataSource getDataSource() {
+                return dataSource;
             }
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+
+            @Override
+            public String createQuery() {
+                return "select id, account, password, email from users where account = ?";
+            }
+
+            @Override
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, account);
+            }
+
+            @Override
+            public Object mapRow(ResultSet resultSet) throws SQLException {
+                return new User(
+                        resultSet.getLong(1),
+                        resultSet.getString(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4)
+                );
+            }
+        };
+        return (User) selectJdbcTemplate.queryForObject();
     }
 }
