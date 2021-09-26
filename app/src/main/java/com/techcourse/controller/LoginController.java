6495e90764cae95e547e3fa5c58ec16be7c9e1a5
Eugine Park <pkeugine@gmail.com>
2021-09-27 04:54:30 +0900

    refactor: app 모듈 Controller In-Memory 저장 방식에서 UserDao로 변경

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -1,12 +1,13 @@
 package com.techcourse.controller;
 
+import com.techcourse.dao.UserDao;
 import com.techcourse.domain.User;
-import com.techcourse.repository.InMemoryUserRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import nextstep.mvc.view.JspView;
 import nextstep.mvc.view.ModelAndView;
+import nextstep.web.annotation.Autowired;
 import nextstep.web.annotation.Controller;
 import nextstep.web.annotation.RequestMapping;
 import nextstep.web.support.RequestMethod;
@@ -15,6 +16,13 @@ import org.slf4j.LoggerFactory;
 
 @Controller
 public class LoginController {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public LoginController(UserDao userDao) {
+        this.userDao = userDao;
+    }
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
@@ -34,7 +42,7 @@ public class LoginController {
             return redirect("/index.jsp");
         }
 
-        return InMemoryUserRepository.findByAccount(request.getParameter("account"))
+        return userDao.findByAccount(request.getParameter("account"))
                 .map(user -> {
                     log.info("User : {}", user);
                     return login(request, user);
