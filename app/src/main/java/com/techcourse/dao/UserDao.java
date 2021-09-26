e5f9b0ee0cb8963c560f72d291b17054750ce921
pkeugine <pkeugine@gmail.com>
2021-09-26 12:51:25 +0900

    refactor: UserDao insert 메서드 클래스로 추출

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -22,42 +22,8 @@ public class UserDao {
     }
 
     public void insert(User user) {
-        String sql = "insert into users (account, password, email) values (?, ?, ?)";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            log.debug("query : {}", sql);
-
-            setValueForInsert(user, pstmt);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
-    }
-
-    private void setValueForInsert(User user, PreparedStatement pstmt) throws SQLException {
-        pstmt.setString(1, user.getAccount());
-        pstmt.setString(2, user.getPassword());
-        pstmt.setString(3, user.getEmail());
+        InsertJdbcTemplate insertJdbcTemplate = new InsertJdbcTemplate();
+        insertJdbcTemplate.execute(user, this);
     }
 
     public void update(User user) {
@@ -148,5 +114,9 @@ public class UserDao {
     public User findByAccount(String account) {
         // todo
         return null;
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
     }
 }
