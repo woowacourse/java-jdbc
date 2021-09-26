bb4cb452e13b1c295263df54c64d10509f328b92
Bepoz <45073750+Be-poz@users.noreply.github.com>
2021-09-27 00:43:13 +0900

    refactor: DatabasePopulatorUtils try-resource로 변경

--- app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
+++ app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
@@ -17,31 +17,14 @@ public class DatabasePopulatorUtils {
     private static final Logger log = LoggerFactory.getLogger(DatabasePopulatorUtils.class);
 
     public static void execute(DataSource dataSource) {
-        Connection connection = null;
-        Statement statement = null;
-        try {
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
             final URL url = DatabasePopulatorUtils.class.getClassLoader().getResource("schema.sql");
             final File file = new File(url.getFile());
             final String sql = Files.readString(file.toPath());
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
             statement.execute(sql);
         } catch (NullPointerException | IOException | SQLException e) {
             log.error(e.getMessage(), e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
