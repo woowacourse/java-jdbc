b8a8e56f123059c091f578c7bc084c9683db4bd4
naeun <66905013+knae11@users.noreply.github.com>
2021-09-27 19:58:12 +0900

    [JDBC 라이브러리 구현하기 - 1, 2단계] 배럴(김나은) 미션 제출합니다. (#5)
    * docs: write todos
    
    * feat: findByAccount 구현
    
    * feat: updateUser 구현
    
    * feat: findAll 구현
    
    * feat: removeAll 구현 및 테스트 수정
    
    - 하드코딩 된 id 값 수정하여 테스트가 순서에 의존하지 않게 만듦
    
    * refactor: try-with-resources 구문으로 수정
    
    * test: try-with-resources 테스트 구현
    
    * docs: check done todo
    
    * refactor: JdbcTemplate.update 구현
    
    * refactor: UserDao 공통로직 JdbcTemplate 으로 이전
    
    * test: JdbcTemplateTest 작성
    
    * refactor(JdbcTemplate): 중복 메서드 처리
    
    * test(JdbcTemplateTest): 예외발생 테스트 추가
    
    * docs: check done todo, write new todos
    
    * refactor: 템플릿메소드 패턴 적용
    
    * test: 템플릿메소드 패턴 테스트 작성
    
    * style: 2줄 개행 제거
    
    * refactor: userRowMapper 메소드로 추출
    
    * refactor: delete 메소드 추가
    
    * chore: jacoco 설정파일 추가
    
    * refactor: insert 메소드 추가
    
    * refactor: StateCallBack 을 사용하는 execute 메소드 추가
    
    * style: 메소드 라인 정리
    
    * refactor: setObject로 변경
    
    * refactor: setObject로 변경
    
    * refactor: 람다식 전달로 변경
    
    * refactor: sonarqube codesmell 피드백 반영
    
    * refactor: sonarqube codesmell 피드백 반영

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,14 +1,16 @@
 package com.techcourse.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class UserDaoTest {
 
@@ -23,6 +25,11 @@ class UserDaoTest {
         userDao.insert(user);
     }
 
+    @AfterEach
+    void tearDown() {
+        userDao.removeAll();
+    }
+
     @Test
     void findAll() {
         final List<User> users = userDao.findAll();
@@ -30,11 +37,29 @@ class UserDaoTest {
         assertThat(users).isNotEmpty();
     }
 
+    @DisplayName("findAll 에서 내부의 값을 확인한다.")
+    @Test
+    void findAllItems() {
+        User better = new User("better", "password", "better@email.com");
+        userDao.insert(better);
+
+        final List<User> users = userDao.findAll();
+        List<String> userNames = users.stream()
+                .map(User::getAccount)
+                .collect(Collectors.toList());
+
+        assertThat(users)
+                .isNotEmpty()
+                .hasSize(2);
+        assertThat(userNames).contains("gugu", "better");
+    }
+
     @Test
     void findById() {
-        final User user = userDao.findById(1L);
+        User gugu = userDao.findByAccount("gugu");
+        final User actual = userDao.findById(gugu.getId());
 
-        assertThat(user.getAccount()).isEqualTo("gugu");
+        assertThat(actual.getAccount()).isEqualTo("gugu");
     }
 
     @Test
@@ -45,13 +70,21 @@ class UserDaoTest {
         assertThat(user.getAccount()).isEqualTo(account);
     }
 
+    @DisplayName("없는 account 로 찾으면 null 을 리턴한다.")
+    @Test
+    void findByAccountFail() {
+        final String account = "gugu11";
+
+        assertThat(userDao.findByAccount(account)).isNull();
+    }
+
     @Test
     void insert() {
         final String account = "insert-gugu";
         final User user = new User(account, "password", "hkkang@woowahan.com");
         userDao.insert(user);
 
-        final User actual = userDao.findById(2L);
+        final User actual = userDao.findByAccount(account);
 
         assertThat(actual.getAccount()).isEqualTo(account);
     }
@@ -59,13 +92,25 @@ class UserDaoTest {
     @Test
     void update() {
         final String newPassword = "password99";
-        final User user = userDao.findById(1L);
+        final User user = userDao.findByAccount("gugu");
         user.changePassword(newPassword);
 
         userDao.update(user);
 
-        final User actual = userDao.findById(1L);
+        final User actual = userDao.findById(user.getId());
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
+    }
+
+    @DisplayName("전체 삭제를 한다.")
+    @Test
+    void removeAll() {
+        List<User> users = userDao.findAll();
+        assertThat(users).isNotEmpty();
+
+        userDao.removeAll();
+        List<User> actual = userDao.findAll();
+
+        assertThat(actual).isEmpty();
     }
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,92 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+
+    private final DataSource dataSource;
+
+    public JdbcTemplate(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = createPreparedStatement(conn, sql, args)) {
+            log.debug("query : {}", sql);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void insert(String sql, Object... args) {
+        update(sql, args);
+    }
+
+    public void delete(String sql, Object... args) {
+        update(sql, args);
+    }
+
+    public <T> T query(String sql, RowMapper<T> rowMapper, Object... args) {
+        return execute(((stmt, rs) -> {
+            log.debug("query : {}", sql);
+
+            if (rs.next()) {
+                return rowMapper.mapRow(rs);
+            }
+            return null;
+        }), sql, args);
+    }
+
+    public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper) {
+        return execute((stmt, rs) -> {
+            log.debug("query : {}", sql);
+
+            List<T> users = new ArrayList<>();
+            while (rs.next()) {
+                T t = rowMapper.mapRow(rs);
+                users.add(t);
+            }
+            return users;
+        }, sql);
+    }
+
+    private void setPreparedStatement(PreparedStatement pstmt, Object[] args) throws SQLException {
+        int index = 1;
+        for (Object arg : args) {
+            pstmt.setObject(index, arg);
+            index += 1;
+        }
+    }
+
+    private PreparedStatement createPreparedStatement(Connection conn, String sql, Object... args) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        setPreparedStatement(pstmt, args);
+        return pstmt;
+    }
+
+    private <T> T execute(StateCallBack<T> action, String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = createPreparedStatement(conn, sql, args);
+                ResultSet rs = pstmt.executeQuery()
+        ) {
+            return action.doInStatement(pstmt, rs);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
 }
