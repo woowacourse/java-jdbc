d1e3c91058f0f5945cc3b57dbe84551b36bb7888
rok93 <goodboy302@naver.com>
2021-09-27 21:33:27 +0900

    feat: UserDao 기능 모두 동작하도록 기능 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -9,9 +9,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
-public class UserDao {
+public class UserDao { // todo: 일단은 이 코드를 다 동작하게
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
@@ -23,94 +25,109 @@ public class UserDao {
 
     public void insert(User user) {
         final String sql = "insert into users (account, password, email) values (?, ?, ?)";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
             log.debug("query : {}", sql);
-
             pstmt.setString(1, user.getAccount());
             pstmt.setString(2, user.getPassword());
             pstmt.setString(3, user.getEmail());
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
+            //todo: 추후 Custom Exception으로 변경하기!
+            throw new IllegalArgumentException();
         }
     }
 
     public void update(User user) {
-        // todo
+        final String sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            pstmt.setString(1, user.getAccount());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setLong(4, user.getId());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            //todo: 추후 Custom Exception으로 변경하기!
+            throw new IllegalArgumentException();
+        }
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
+        final String sql = "select id, account, password, email from users";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            List<User> users = new ArrayList<>();
+            while (rs.next()) {
+                User user = parseUser(rs);
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) {
+            //todo: Custom Exception
+            throw new IllegalArgumentException();
+        }
     }
 
     public User findById(Long id) {
         final String sql = "select id, account, password, email from users where id = ?";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = createPreParedStatementByIndex(conn, sql, id);
+             ResultSet rs = pstmt.executeQuery()) {
             log.debug("query : {}", sql);
 
             if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
+                return parseUser(rs);
             }
-            return null;
+
+            // todo: 데이터가 존재하지 않는 경우 예외 추가
+            throw new IllegalArgumentException("해당 데이터가 존재하지 않습니다.");
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
         }
     }
 
+    private PreparedStatement createPreParedStatementByIndex(Connection conn, String sql, Long id) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setLong(1, id);
+        return pstmt;
+    }
+
     public User findByAccount(String account) {
-        // todo
-        return null;
+        final String sql = "select id, account, password, email from users where account = ?";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = ((Supplier<PreparedStatement>) () -> {
+                 try {
+                     PreparedStatement s = conn.prepareStatement(sql);
+                     s.setString(1, account);
+                     return s;
+                 } catch (SQLException e) {
+                     throw new RuntimeException(e);
+                 }
+             }).get();
+             ResultSet rs = pstmt.executeQuery()) {
+            log.debug("query : {}", sql);
+
+            if (rs.next()) {
+                return parseUser(rs);
+            }
+
+            // todo: 데이터가 존재하지 않는 경우 예외 추가
+            throw new IllegalArgumentException("해당 데이터가 존재하지 않습니다.");
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private User parseUser(ResultSet rs) throws SQLException {
+        return new User(rs.getLong(1), rs.getString(2), rs.getString(3),
+                rs.getString(4));
     }
 }
