74f8be00ffad758048ecef2c89068e13cc406e32
youngbin <thisisyoungbin@gmail.com>
2021-09-28 12:18:53 +0900

    refactor: 사용하지 않는 인터페이스 제거

--- jdbc/src/main/java/nextstep/jdbc/PreparedStatementSetter.java
+++
@@ -1,9 +0,0 @@
-package nextstep.jdbc;
-
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public interface PreparedStatementSetter {
-
-    void setParams(PreparedStatement pstmt) throws SQLException;
-}
