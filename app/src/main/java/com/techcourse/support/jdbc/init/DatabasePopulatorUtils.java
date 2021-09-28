7ee46adc9ebfc7d7e3a450ac9c45dc5c5297dd4e
RinSabbit <celena1004@naver.com>
2021-09-28 12:13:56 +0900

    refactor: HikariDataSource로 변경

--- app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
+++ app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
@@ -1,9 +1,5 @@
 package com.techcourse.support.jdbc.init;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -11,37 +7,27 @@ import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatabasePopulatorUtils {
 
     private static final Logger log = LoggerFactory.getLogger(DatabasePopulatorUtils.class);
 
+    private DatabasePopulatorUtils() {
+    }
+
     public static void execute(DataSource dataSource) {
-        Connection connection = null;
-        Statement statement = null;
-        try {
+        try (Connection connection = dataSource.getConnection();
+            Statement statement = connection.createStatement()) {
             final URL url = DatabasePopulatorUtils.class.getClassLoader().getResource("schema.sql");
             final File file = new File(url.getFile());
             final String sql = Files.readString(file.toPath());
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
             statement.execute(sql);
+            log.info("create schema!");
         } catch (NullPointerException | IOException | SQLException e) {
             log.error(e.getMessage(), e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ignored) {}
         }
     }
-
-    private DatabasePopulatorUtils() {}
 }
