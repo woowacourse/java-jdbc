0366a426f18bfb49dbeebed5293f520390d7ed94
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-09-28 09:53:04 +0900

    test: Junit Reflection 관련 테스트 작성

--- learning/src/test/java/reflection/Junit4TestRunner.java
+++ learning/src/test/java/reflection/Junit4TestRunner.java
@@ -2,12 +2,31 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
-class Junit4TestRunner {
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Junit4TestRunner extends JunitOutput {
 
     @Test
     void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        final Junit4Test junit4Test = clazz.getConstructor().newInstance();
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final Method[] methods = clazz.getMethods();
+        for (Method method : methods) {
+            final MyTest myTestAnnotation = method.getAnnotation(MyTest.class);
+            if (!Objects.isNull(myTestAnnotation)) {
+                method.invoke(junit4Test);
+            }
+        }
+
+        String output = captor.toString().trim();
+
+        assertThat(output)
+                .contains("Running Test1")
+                .contains("Running Test2")
+                .doesNotContain("Running Test3");
     }
 }
