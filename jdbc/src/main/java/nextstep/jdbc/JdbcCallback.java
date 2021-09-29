c72f839c87669b444c877d720cc25a8155e59424
ROOT <junroot0909@gmail.com>
2021-09-29 11:30:18 +0900

    refactor: 파라미터 바인딩 클래스 추가

--- jdbc/src/main/java/nextstep/jdbc/JdbcCallback.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcCallback.java
@@ -1,9 +1,8 @@
 package nextstep.jdbc;
 
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public interface JdbcCallback<T> {
 
-    T call(final PreparedStatement preparedStatement) throws SQLException;
+    T call(final ParameterizedStatement statement) throws SQLException;
 }
