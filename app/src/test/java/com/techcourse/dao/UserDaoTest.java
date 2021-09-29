c21ff3883cf2291de93e1bac7544bb708c11c1e6
김주원 <kjw11077@naver.com>
2021-09-29 17:26:33 +0900

    test: UserDaoTest tearDown() 작성

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -3,10 +3,14 @@ package com.techcourse.dao;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -14,16 +18,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserDaoTest {
 
     private UserDao userDao;
+    private DataSource dataSource;
 
     @BeforeEach
     void setup() {
-        final DataSource dataSource = DataSourceConfig.getInstance();
+        dataSource = DataSourceConfig.getInstance();
 
         DatabasePopulatorUtils.execute(dataSource);
 
         userDao = new UserDao(dataSource);
         final User user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(user);
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        final String sql = "delete from users";
+        final String sql2 = "alter table users alter column id restart with 1";
+
+        try (Connection conn = dataSource.getConnection()) {
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                pstmt.execute();
+            }
+            try (PreparedStatement pstmt = conn.prepareStatement(sql2)) {
+                pstmt.execute();
+            }
+        }
     }
 
     @Test
