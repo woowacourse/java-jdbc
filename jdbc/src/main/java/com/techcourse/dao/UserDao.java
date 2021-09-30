bc8af1d3619749f9d5fd2b1e07bb567cafe936e0
TaewanKimmmm <ktww88@gmail.com>
2021-09-30 11:01:41 +0900

    refactor: UserDao에서 사용하지 않는 Logger 제거 및 JdbcTemplate 내부에서 로그를 남기도록 수정

--- jdbc/src/main/java/com/techcourse/dao/UserDao.java
+++ jdbc/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,65 +1,59 @@
 package com.techcourse.dao;
 
+import com.techcourse.domain.User;
 import java.sql.ResultSet;
 import java.util.List;
 import javax.sql.DataSource;
-
-import com.techcourse.domain.User;
-
 import nextstep.jdbc.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UserDao {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(UserDao.class);
+	private final JdbcTemplate jdbcTemplate;
 
-    private final JdbcTemplate jdbcTemplate;
+	public UserDao(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
 
-    public UserDao(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
+	public void insert(User user) {
+		String sql = "insert into users (account, password, email) values (?, ?, ?)";
+		jdbcTemplate.executeQuery(sql, user.getAccount(), user.getPassword(), user.getEmail());
+	}
 
-    public void insert(User user) {
-        String sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.executeQuery(sql, user.getAccount(), user.getPassword(), user.getEmail());
-    }
+	public void update(User user) {
+		String sql = "update users set password = ? where id = ?";
+		jdbcTemplate.executeQuery(sql, user.getPassword(), user.getId());
+	}
 
-    public void update(User user) {
-        String sql = "update users set password = ? where id = ?";
-        jdbcTemplate.executeQuery(sql, user.getPassword(), user.getId());
-    }
+	public List<User> findAll() {
+		String sql = "select id, account, password, email from users";
+		return jdbcTemplate.query(sql, (ResultSet resultSet) ->
+			new User(
+				resultSet.getLong(1),
+				resultSet.getString(2),
+				resultSet.getString(3),
+				resultSet.getString(4)
+			));
+	}
 
-    public List<User> findAll() {
-        String sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(sql, (ResultSet resultSet) ->
-                new User(
-                        resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4)
-                ));
-    }
+	public User findById(Long id) {
+		String sql = "select id, account, password, email from users where id = ?";
+		return jdbcTemplate.queryForObject(sql, (ResultSet resultSet) ->
+			new User(
+				resultSet.getLong(1),
+				resultSet.getString(2),
+				resultSet.getString(3),
+				resultSet.getString(4)
+			), id);
+	}
 
-    public User findById(Long id) {
-        String sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, (ResultSet resultSet) ->
-                new User(
-                        resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4)
-                ), id);
-    }
-
-    public User findByAccount(String account) {
-        String sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(sql, (ResultSet resultSet) ->
-                new User(
-                        resultSet.getLong(1),
-                        resultSet.getString(2),
-                        resultSet.getString(3),
-                        resultSet.getString(4)
-                ), account);
-    }
+	public User findByAccount(String account) {
+		String sql = "select id, account, password, email from users where account = ?";
+		return jdbcTemplate.queryForObject(sql, (ResultSet resultSet) ->
+			new User(
+				resultSet.getLong(1),
+				resultSet.getString(2),
+				resultSet.getString(3),
+				resultSet.getString(4)
+			), account);
+	}
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -8,70 +8,75 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import javax.sql.DataSource;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class JdbcTemplate {
-    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcTemplate.class);
 
-    private final DataSource dataSource;
+	private static final Logger LOGGER = LoggerFactory.getLogger(JdbcTemplate.class);
 
-    public JdbcTemplate(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+	private final DataSource dataSource;
 
-    public void executeQuery(String sql, Object... values) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-            setValues(preparedStatement, values);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new InvalidDataException(e.getMessage());
-        }
-    }
+	public JdbcTemplate(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
-    private void setValues(PreparedStatement preparedStatement, Object[] values) throws SQLException {
-        for (int i = 0; i < values.length; i++) {
-            preparedStatement.setObject(i + 1, values[i]);
-        }
-    }
+	public void executeQuery(String sql, Object... values) {
+		try (Connection connection = dataSource.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+			setValues(preparedStatement, values);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new InvalidDataException(e.getMessage());
+		}
+	}
 
-    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... values) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+	private void setValues(PreparedStatement preparedStatement, Object[] values)
+		throws SQLException {
+		for (int i = 0; i < values.length; i++) {
+			preparedStatement.setObject(i + 1, values[i]);
+		}
+	}
 
-            if (Objects.nonNull(values)) {
-                setValues(preparedStatement, values);
-            }
+	public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... values) {
+		try (Connection connection = dataSource.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
-            List<T> result = new ArrayList<>();
+			if (Objects.nonNull(values)) {
+				setValues(preparedStatement, values);
+			}
 
-            ResultSet resultSet = preparedStatement.executeQuery();
+			List<T> result = new ArrayList<>();
 
-            while (resultSet.next()) {
-                result.add(rowMapper.mapRow(resultSet));
-            }
+			ResultSet resultSet = preparedStatement.executeQuery();
 
-            return result;
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage(), e);
-            throw new InvalidDataException(e.getMessage());
-        }
-    }
+			while (resultSet.next()) {
+				result.add(rowMapper.mapRow(resultSet));
+			}
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... values) {
-        List<T> result = query(sql, rowMapper, values);
+			return result;
+		} catch (SQLException e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new InvalidDataException(e.getMessage());
+		}
+	}
 
-        if (result.isEmpty()) {
-            throw new InvalidDataException("존재하지 않는 데이터입니다.");
-        }
+	public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... values) {
+		List<T> result = query(sql, rowMapper, values);
 
-        if (result.size() > 1) {
-            throw new InvalidDataException("두 개 이상의 데이터가 존재합니다.");
-        }
+		if (result.isEmpty()) {
+			String message = "존재하지 않는 데이터입니다.";
+			LOGGER.error(message);
+			throw new InvalidDataException(message);
+		}
 
-        return result.get(0);
-    }
+		if (result.size() > 1) {
+			String message = "두 개 이상의 데이터가 존재합니다.";
+			LOGGER.error(message);
+			throw new InvalidDataException(message);
+		}
+
+		return result.get(0);
+	}
 }
