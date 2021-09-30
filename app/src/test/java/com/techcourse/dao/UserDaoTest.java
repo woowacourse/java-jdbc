df1edcf738f54c021ae5abbb52cbd26a05674937
TaewanKimmmm <ktww88@gmail.com>
2021-09-30 16:54:54 +0900

    refactor: h2 데이터베이스 경로 및 UserDaoTest 수정

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -6,6 +6,7 @@ import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 import java.util.List;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +23,11 @@ class UserDaoTest {
 		userDao.insert(user);
 	}
 
+	@AfterEach
+	void tearDown() {
+		userDao.deleteAll();
+	}
+
 	@Test
 	void findAll() {
 		final List<User> users = userDao.findAll();
@@ -31,7 +37,8 @@ class UserDaoTest {
 
 	@Test
 	void findById() {
-		final User user = userDao.findById(1L);
+		User gugu = userDao.findByAccount("gugu");
+		final User user = userDao.findById(gugu.getId());
 
 		assertThat(user.getAccount()).isEqualTo("gugu");
 	}
@@ -50,7 +57,7 @@ class UserDaoTest {
 		final User user = new User(account, "password", "hkkang@woowahan.com");
 		userDao.insert(user);
 
-		final User actual = userDao.findById(2L);
+		final User actual = userDao.findByAccount(account);
 
 		assertThat(actual.getAccount()).isEqualTo(account);
 	}
@@ -58,12 +65,12 @@ class UserDaoTest {
 	@Test
 	void update() {
 		final String newPassword = "password99";
-		final User user = userDao.findById(1L);
+		final User user = userDao.findByAccount("gugu");
 		user.changePassword(newPassword);
 
 		userDao.update(user);
 
-		final User actual = userDao.findById(1L);
+		final User actual = userDao.findByAccount("gugu");
 
 		assertThat(actual.getPassword()).isEqualTo(newPassword);
 	}
