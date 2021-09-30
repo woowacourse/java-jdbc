08842369d97bf9cc6231241293112e8fd71841c2
TaewanKimmmm <ktww88@gmail.com>
2021-09-30 11:50:41 +0900

    refactor: ReulstSet을 상수 추출하여 중복 제거

--- jdbc/src/main/java/com/techcourse/dao/UserDao.java
+++ jdbc/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,12 +1,20 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.sql.ResultSet;
 import java.util.List;
 import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
 
 public class UserDao {
+
+	private static final RowMapper<User> ROW_MAPPER = (resultSet) -> {
+		long id = resultSet.getLong("id");
+		String account = resultSet.getString("account");
+		String password = resultSet.getString("password");
+		String email = resultSet.getString("email");
+		return new User(id, account, password, email);
+	};
 
 	private final JdbcTemplate jdbcTemplate;
 
@@ -26,34 +34,16 @@ public class UserDao {
 
 	public List<User> findAll() {
 		String sql = "select id, account, password, email from users";
-		return jdbcTemplate.query(sql, (ResultSet resultSet) ->
-			new User(
-				resultSet.getLong(1),
-				resultSet.getString(2),
-				resultSet.getString(3),
-				resultSet.getString(4)
-			));
+		return jdbcTemplate.query(sql, ROW_MAPPER);
 	}
 
 	public User findById(Long id) {
 		String sql = "select id, account, password, email from users where id = ?";
-		return jdbcTemplate.queryForObject(sql, (ResultSet resultSet) ->
-			new User(
-				resultSet.getLong(1),
-				resultSet.getString(2),
-				resultSet.getString(3),
-				resultSet.getString(4)
-			), id);
+		return jdbcTemplate.queryForObject(sql, ROW_MAPPER, id);
 	}
 
 	public User findByAccount(String account) {
 		String sql = "select id, account, password, email from users where account = ?";
-		return jdbcTemplate.queryForObject(sql, (ResultSet resultSet) ->
-			new User(
-				resultSet.getLong(1),
-				resultSet.getString(2),
-				resultSet.getString(3),
-				resultSet.getString(4)
-			), account);
+		return jdbcTemplate.queryForObject(sql, ROW_MAPPER, account);
 	}
 }
