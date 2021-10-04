a7f65562db1b0c6907bff85c2faddc633a25eb52
DESKTOP-VDP6SAT\xntm3 <ohjoohyung@github.com>
2021-10-05 00:31:51 +0900

    test: 성공 케이스 추가

--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -9,9 +9,14 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -65,6 +70,72 @@ class JdbcTemplateTest {
         verify(preparedStatement).close();
         verify(resultSet).close();
     }
+
+    @Test
+    @DisplayName("update 결과값으로 1이 나온다.")
+    void update() throws SQLException {
+        // given
+        String sql = "insert into user (name) values (oz)";
+        Connection connection = mock(Connection.class);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+
+        // when
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeUpdate()).thenReturn(1);
+        final int actual = jdbcTemplate.update(sql);
+
+        // then
+        assertThat(actual).isEqualTo(1);
+        verify(preparedStatement).executeUpdate();
+    }
+
+    @Test
+    @DisplayName("query 결과값 가져오는 것을 성공한다.")
+    void query() throws SQLException {
+        // given
+        String sql = "select * from user";
+        Connection connection = mock(Connection.class);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        RowMapper<TestUser> rowMapper = mock(RowMapper.class);
+
+        // when
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, true, false);
+        when(rowMapper.mapRow(any(), anyInt())).thenReturn(new TestUser());
+        final List<TestUser> testUsers = jdbcTemplate.query(sql, rowMapper);
+
+        // then
+        assertThat(testUsers).hasSize(2);
+        verify(rowMapper, times(2)).mapRow(any(), anyInt());
+    }
+
+    @Test
+    @DisplayName("queryForObject 결과값이 1개 일 때 성공한다.")
+    void queryForObjectWhenOneResult() throws SQLException {
+        // given
+        String sql = "select * from user";
+        Connection connection = mock(Connection.class);
+        PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        ResultSet resultSet = mock(ResultSet.class);
+        RowMapper<TestUser> rowMapper = mock(RowMapper.class);
+
+        // when
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, false);
+        when(rowMapper.mapRow(any(), anyInt())).thenReturn(new TestUser());
+        final TestUser actual = jdbcTemplate.queryForObject(sql, rowMapper);
+
+        // then
+        assertThat(actual).isInstanceOf(TestUser.class);
+        verify(rowMapper).mapRow(any(), anyInt());
+    }
+
 
     @Test
     @DisplayName("queryForObject 결과값이 2개 이상일 때 실패한다.")
