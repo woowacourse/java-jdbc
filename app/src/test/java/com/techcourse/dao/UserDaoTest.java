608ec14c86582f70fc4520e182326d487c4b5412
Yeongsang Jo <61370901+PapimonLikelion@users.noreply.github.com>
2021-10-05 13:31:03 +0900

    test(UserDaoTest): setUp을 전체 테스트 중 한번만 수행하도록 변경
    - `@BeforeEach` 대신 `@BeforeAll`로 변경

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -3,44 +3,49 @@ package com.techcourse.dao;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class UserDaoTest {
 
-    private UserDao userDao;
+    private static UserDao userDao;
 
-    @BeforeEach
-    void setup() throws SQLException {
+    @BeforeAll
+    static void setup() {
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
 
         userDao = new UserDao(DataSourceConfig.getInstance());
-        final User user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.insert(user);
+        final User user1 = new User("gugu", "password", "hkkang@woowahan.com");
+        userDao.insert(user1);
+        final User user2 = new User("joel", "joel", "hkkang@woowahan.com");
+        userDao.insert(user2);
+        final User user3 = new User("middlebear", "middlebear", "hkkang@woowahan.com");
+        userDao.insert(user3);
     }
 
     @Test
-    void findAll() throws SQLException {
+    void findAll() {
         final List<User> users = userDao.findAll();
 
-        assertThat(users).isNotEmpty();
+        assertThat(users).hasSizeGreaterThan(3);
         assertThat(users.get(0).getAccount()).isEqualTo("gugu");
+        assertThat(users.get(1).getAccount()).isEqualTo("joel");
+        assertThat(users.get(2).getAccount()).isEqualTo("middlebear");
     }
 
     @Test
-    void findById() throws SQLException {
+    void findById() {
         final User user = userDao.findById(1L);
 
         assertThat(user.getAccount()).isEqualTo("gugu");
     }
 
     @Test
-    void findByAccount() throws SQLException {
+    void findByAccount() {
         final String account = "gugu";
         final User user = userDao.findByAccount(account);
 
@@ -48,18 +53,18 @@ class UserDaoTest {
     }
 
     @Test
-    void insert() throws SQLException {
+    void insert() {
         final String account = "insert-gugu";
         final User user = new User(account, "password", "hkkang@woowahan.com");
         userDao.insert(user);
 
-        final User actual = userDao.findById(2L);
+        final User actual = userDao.findById(4L);
 
         assertThat(actual.getAccount()).isEqualTo(account);
     }
 
     @Test
-    void update() throws SQLException {
+    void update() {
         final String newPassword = "password99";
         final User user = userDao.findById(1L);
         user.changePassword(newPassword);
