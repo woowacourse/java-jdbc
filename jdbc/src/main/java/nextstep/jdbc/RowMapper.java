226ff1bb71613d920abd47e43b63937011c5f4e8
Byung Wook Park <bbwwpark@naver.com>
2021-10-13 17:02:43 +0900

    refactor: 애너테이션으로 함수형 인터페이스를 표시한다.

--- jdbc/src/main/java/nextstep/jdbc/RowMapper.java
+++ jdbc/src/main/java/nextstep/jdbc/RowMapper.java
@@ -3,6 +3,7 @@ package nextstep.jdbc;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface RowMapper {
 
      Object mapRow(ResultSet rs) throws SQLException;
