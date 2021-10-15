d86a7bc1a340110b227a7a608860bfa75e7e9606
Byung Wook Park <bbwwpark@naver.com>
2021-10-15 10:35:25 +0900

    fix: findById 테스트를 수행하도록 수정한다.

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
-import java.sql.SQLException;
 import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -16,7 +15,7 @@ class UserDaoTest {
     private UserDao userDao;
 
     @BeforeEach
-    void setup() throws SQLException {
+    void setup() {
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
         userDao = new UserDao(DataSourceConfig.getInstance());
 
@@ -25,12 +24,12 @@ class UserDaoTest {
     }
 
     @AfterEach
-    void cleanUp() throws SQLException {
+    void cleanUp() {
         userDao.deleteAll();
     }
 
     @Test
-    void findAll() throws SQLException {
+    void findAll() {
         final List<User> users = userDao.findAll();
 
         assertThat(users).isNotEmpty();
@@ -38,14 +37,15 @@ class UserDaoTest {
     }
 
     @Test
-    void findById() throws SQLException {
-        final User user = userDao.findAll().get(0);
+    void findById() {
+        User user = userDao.findAll().get(0);
+        final User expected = userDao.findById(user.getId());
 
-        assertThat(user.getAccount()).isEqualTo("gugu");
+        assertThat(expected.getAccount()).isEqualTo("gugu");
     }
 
     @Test
-    void findByAccount() throws SQLException {
+    void findByAccount() {
         final String account = "gugu";
         final User user = userDao.findByAccount(account);
 
@@ -53,7 +53,7 @@ class UserDaoTest {
     }
 
     @Test
-    void insert() throws SQLException {
+    void insert() {
         final String account = "insert-gugu";
         final User user = new User(account, "password", "hkkang@woowahan.com");
         userDao.insert(user);
@@ -64,7 +64,7 @@ class UserDaoTest {
     }
 
     @Test
-    void update() throws SQLException {
+    void update() {
         final String newPassword = "password99";
         final User user = userDao.findAll().get(0);
         user.changePassword(newPassword);
