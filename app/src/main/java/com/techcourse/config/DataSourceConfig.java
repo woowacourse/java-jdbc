c9104130e02af116fcfffa8fa12e3c30dd46ce3d
Solji Choi <bssj9307@gmail.com>
2021-10-29 01:41:27 +0900

    feat: HikariCP 설정

--- app/src/main/java/com/techcourse/config/DataSourceConfig.java
+++ app/src/main/java/com/techcourse/config/DataSourceConfig.java
@@ -1,5 +1,8 @@
 package com.techcourse.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
 import org.h2.jdbcx.JdbcDataSource;
 
 import java.util.Objects;
@@ -10,17 +13,21 @@ public class DataSourceConfig {
 
     public static javax.sql.DataSource getInstance() {
         if (Objects.isNull(INSTANCE)) {
-            INSTANCE = createJdbcDataSource();
+            INSTANCE = createDateSource();
         }
         return INSTANCE;
     }
 
-    private static JdbcDataSource createJdbcDataSource() {
-        final JdbcDataSource jdbcDataSource = new JdbcDataSource();
-        jdbcDataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;");
-        jdbcDataSource.setUser("");
-        jdbcDataSource.setPassword("");
-        return jdbcDataSource;
+    private static DataSource createDateSource() {
+        final HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;");
+        hikariConfig.setUsername("");
+        hikariConfig.setPassword("");
+        hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
+        hikariConfig.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        hikariConfig.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+
+        return new HikariDataSource(hikariConfig);
     }
 
     private DataSourceConfig() {}
