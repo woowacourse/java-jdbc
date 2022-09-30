fb7611fa0f57f2eaa716a4f569bbb37d455b37b5
awesomeo184 <awesomeo184@gmail.com>
2022-09-30 20:37:46 +0900

    fix: FunctionalInterface 애너테이션 추가

--- jdbc/src/main/java/nextstep/jdbc/PreparedStatementCallback.java
+++ jdbc/src/main/java/nextstep/jdbc/PreparedStatementCallback.java
@@ -3,6 +3,7 @@ package nextstep.jdbc;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementCallback<T> {
 
     T doInPreparedStatement(PreparedStatement ps) throws SQLException;
