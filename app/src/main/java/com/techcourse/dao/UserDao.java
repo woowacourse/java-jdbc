7c738dc2f0c002ed931f7357387ef673b2c4c1ca
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-30 23:08:39 +0900

    refactor(UserDao): cleanse duplicate ResultSet logic with query and RowMapper

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,16 +1,16 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import nextstep.jdbc.DataAccessException;
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -67,17 +67,7 @@ public class UserDao {
         try (final var conn = getConnection();
              final var pstmt = conn.prepareStatement(sql)) {
             log.debug("query : {}", sql);
-            try (final var rs = pstmt.executeQuery()) {
-                final var users = new ArrayList<User>();
-                if (rs.next()) {
-                    users.add(new User(
-                            rs.getLong(1),
-                            rs.getString(2),
-                            rs.getString(3),
-                            rs.getString(4)));
-                }
-                return users;
-            }
+            return query(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -91,16 +81,7 @@ public class UserDao {
              final var pstmt = conn.prepareStatement(sql)) {
             pstmt.setLong(1, id);
             log.debug("query : {}", sql);
-            try (final var rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return new User(
-                            rs.getLong(1),
-                            rs.getString(2),
-                            rs.getString(3),
-                            rs.getString(4));
-                }
-                return null;
-            }
+            return query(pstmt).get(0); //  TODO: IndexOutOfBoundsException 대응되도록 수정 필요
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -114,16 +95,7 @@ public class UserDao {
              final var pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, account);
             log.debug("query : {}", sql);
-            try (final var rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return new User(
-                            rs.getLong(1),
-                            rs.getString(2),
-                            rs.getString(3),
-                            rs.getString(4));
-                }
-                return null;
-            }
+            return query(pstmt).get(0);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -139,4 +111,20 @@ public class UserDao {
             throw new DataAccessException(e);
         }
     }
+
+    private List<User> query(PreparedStatement pstmt) throws SQLException {
+        try (final var rs = pstmt.executeQuery()) {
+            List<User> users = new ArrayList<>();
+            if (rs.next()) {
+                users.add(rowMapper.mapRow(rs));
+            }
+            return users;
+        }
+    }
+
+    private final RowMapper<User> rowMapper = (resultSet) ->
+            new User(resultSet.getLong("id"),
+                    resultSet.getString("account"),
+                    resultSet.getString("password"),
+                    resultSet.getString("email"));
 }
