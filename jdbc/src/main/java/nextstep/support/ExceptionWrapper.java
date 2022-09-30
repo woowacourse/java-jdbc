f3328110ecf25ceca5f76a6793c8528c03c2fbc8
Richard JEON <ztzy1907@gmail.com>
2022-09-30 16:46:09 +0900

    feat: ExceptionWrapper 전달된 Function의 클래스의 로거로 로깅하도록 개선

--- jdbc/src/main/java/nextstep/support/ExceptionWrapper.java
+++ jdbc/src/main/java/nextstep/support/ExceptionWrapper.java
@@ -6,12 +6,11 @@ import org.slf4j.LoggerFactory;
 
 public class ExceptionWrapper {
 
-    private static final Logger log = LoggerFactory.getLogger(ExceptionWrapper.class);
-
     public static <T> T get(final Callable<T> throwableCall) {
         try {
             return throwableCall.call();
         } catch (Exception e) {
+            final Logger log = LoggerFactory.getLogger(throwableCall.getClass());
             log.error(e.getMessage(), e);
             throw new RuntimeException();
         }
