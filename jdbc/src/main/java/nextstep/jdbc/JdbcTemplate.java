3d804f1a6d6dbeff717f6532ad4482473e3e3958
jayjaehunchoi <wogns0108@naver.com>
2022-10-01 17:34:01 +0900

    feat: jdbcTemplate 조회 생성

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.RowMapperResultSetExtractor;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,42 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    private <T> T execute(final StatementCallback statementCallback, final Object[] objects,
+                          final ResultSetExtractor<T> resultSetExtractor) {
+        statementCallback.setPreparedSql(objects);
+        return statementCallback.doInStatement(resultSetExtractor);
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... objects)
+            throws DataAccessException {
+        try (Connection connection = dataSource.getConnection();
+             StatementCallback statementCallback = new StatementCallback(connection.prepareStatement(sql))) {
+            return execute(statementCallback, objects, new RowMapperResultSetExtractor<>(rowMapper));
+        } catch (SQLException e) {
+            log.error("query exception", e);
+            throw new DataAccessException();
+        }
+    }
+
+    public <T> List<T> query(final String sql, final Class<T> cls, Object... objects) throws DataAccessException {
+        return query(sql, new SingleColumnRowMapper<>(cls), objects);
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, Object... objects)
+            throws DataAccessException {
+        try (Connection connection = dataSource.getConnection();
+             StatementCallback statementCallback = new StatementCallback(connection.prepareStatement(sql))) {
+            return DataAccessUtils.nullableSingleResult(
+                    execute(statementCallback, objects, new RowMapperResultSetExtractor<>(rowMapper)));
+        } catch (SQLException e) {
+            log.error("queryForObject exception", e);
+            throw new DataAccessException();
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final Class<T> cls, Object... objects) throws DataAccessException {
+        return queryForObject(sql, new SingleColumnRowMapper<>(cls), objects);
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,55 @@
 package nextstep.jdbc;
 
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private JdbcTemplate jdbcTemplate;
+    private Connection connection;
+    private DataSource dataSource;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        this.connection = mock(Connection.class);
+        this.dataSource = mock(DataSource.class);
+        this.preparedStatement = mock(PreparedStatement.class);
+        this.resultSet = mock(ResultSet.class);
+
+        given(dataSource.getConnection()).willReturn(connection);
+        given(connection.prepareStatement(anyString())).willReturn(preparedStatement);
+        given(preparedStatement.executeQuery()).willReturn(resultSet);
+        given(preparedStatement.executeQuery(anyString())).willReturn(resultSet);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Test
+    void closeWhenQuery() throws SQLException {
+        jdbcTemplate.query("SELECT name FROM MEMBER", String.class);
+
+        verify(connection).close();
+        verify(preparedStatement).close();
+        verify(resultSet).close();
+    }
+
+    @Test
+    void closeWhenQueryForObject() throws SQLException {
+        jdbcTemplate.query("SELECT name FROM MEMBER WHERE id = ?", String.class, 1);
+
+        verify(connection).close();
+        verify(preparedStatement).close();
+        verify(resultSet).close();
+    }
 }
