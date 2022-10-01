85ebf45ee05e4a163e4ba9ef10381952af086d27
Wishoon <heonga2@gmail.com>
2022-10-01 19:56:50 +0900

    feat: JdbcTemplate Connect 구현

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,17 +1,40 @@
 package nextstep.jdbc;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int PREPARED_STATEMENT_START_INDEX = 1;
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    private <T> T connect(final String sql,
+                          final ExecuteStrategy<T> strategy,
+                          final Object... args) {
+        try (
+            final var connection = dataSource.getConnection();
+            final var preparedStatement = connection.prepareStatement(sql)) {
+
+            for (int i = PREPARED_STATEMENT_START_INDEX; i < args.length; i++) {
+                preparedStatement.setObject(i, args[i - 1]);
+            }
+            return strategy.execute(preparedStatement);
+        } catch (SQLException e) {
+            log.error("Not Data Access : {}", e.getMessage());
+            throw new DataAccessException(e);
+        }
+    }
+
+    interface ExecuteStrategy<T> {
+        T execute(final PreparedStatement pstmt) throws SQLException;
     }
 }
