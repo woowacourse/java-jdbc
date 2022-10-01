b71e79e879e722e27e7c70c406ce2ea7ea87d686
Ohzzi <clover7even@naver.com>
2022-10-01 19:48:37 +0900

    feat: 한 개만 조회하는 jdbcTemplate의 queryForObject 메서드 구현

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,20 +1,23 @@
 package nextstep.jdbc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.core.RowMapper;
+import static java.sql.ResultSet.CONCUR_READ_ONLY;
+import static java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.RowMapper;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final String INCORRECT_RESULT_SIZE_MESSAGE = "Incorrect result size: expected 1, actual %d";
 
     private final DataSource dataSource;
 
@@ -44,19 +47,52 @@ public class JdbcTemplate {
         }
     }
 
-    private void setArguments(final PreparedStatement pstmt, final Object[] args) throws SQLException {
+    public <T> T queryForObject(final String sql, Object[] args, RowMapper<T> rowMapper) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql, TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY)) {
+            setArguments(pstmt, args);
+            ResultSet rs = pstmt.executeQuery();
+            validateResultSetSize(rs);
+            return mapResultSetToObject(rowMapper, rs);
+        } catch (SQLException e) {
+            log.debug("ERROR CODE: {} SQL STATE: {}", e.getErrorCode(), e.getSQLState());
+            throw new DataAccessException(e);
+        }
+    }
+
+    private static void setArguments(final PreparedStatement pstmt, final Object[] args) throws SQLException {
         int parameterIndex = 1;
         for (Object arg : args) {
             pstmt.setObject(parameterIndex++, arg);
         }
     }
 
-    private <T> List<T> mapResultSetToList(RowMapper<T> rowMapper, ResultSet rs) throws SQLException {
+    private static <T> List<T> mapResultSetToList(final RowMapper<T> rowMapper, final ResultSet rs)
+            throws SQLException {
         List<T> result = new ArrayList<>();
         while (rs.next()) {
             result.add(rowMapper.mapRow(rs, rs.getRow()));
         }
         return result;
+    }
+
+    private static int getResultSetSize(final ResultSet rs) throws SQLException {
+        rs.last();
+        int size = rs.getRow();
+        rs.beforeFirst();
+        return size;
+    }
+
+    private static void validateResultSetSize(final ResultSet rs) throws SQLException {
+        int size = getResultSetSize(rs);
+        if (size != 1) {
+            throw new DataAccessException(String.format(INCORRECT_RESULT_SIZE_MESSAGE, size));
+        }
+    }
+
+    private static <T> T mapResultSetToObject(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
+        rs.next();
+        return rowMapper.mapRow(rs, 1);
     }
 
     public DataSource getDataSource() {
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,20 +1,27 @@
 package nextstep.jdbc;
 
-import org.junit.jupiter.api.Test;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-
+import static java.sql.ResultSet.CONCUR_READ_ONLY;
+import static java.sql.ResultSet.TYPE_SCROLL_INSENSITIVE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class JdbcTemplateTest {
 
@@ -30,12 +37,17 @@ class JdbcTemplateTest {
 
         when(dataSource.getConnection()).thenReturn(conn);
         when(conn.prepareStatement(sql)).thenReturn(pstmt);
+        when(conn.prepareStatement(sql, TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY)).thenReturn(pstmt);
         when(pstmt.executeUpdate()).thenThrow(SQLException.class);
         when(pstmt.executeQuery()).thenThrow(SQLException.class);
 
         assertAll(
-                () -> assertThatThrownBy(() -> jdbcTemplate.update(sql)).isExactlyInstanceOf(DataAccessException.class),
+                () -> assertThatThrownBy(() -> jdbcTemplate.update(sql)).isInstanceOf(DataAccessException.class),
                 () -> assertThatThrownBy(() -> jdbcTemplate.query(sql, new Object[0], (rs1, rowNum) -> "dummy"))
+                        .isInstanceOf(DataAccessException.class),
+                () -> assertThatThrownBy(
+                        () -> jdbcTemplate.queryForObject(sql, new Object[0], (rs1, rowNum) -> "dummy"))
+                        .isInstanceOf(DataAccessException.class)
         );
     }
 
@@ -64,7 +76,7 @@ class JdbcTemplateTest {
         when(pstmt.executeQuery()).thenReturn(rs);
         when(rs.next()).thenReturn(false);
 
-        List<String> result = jdbcTemplate.query(sql, new Object[0], ((rs1, rowNum) -> "dummy"));
+        List<Object> result = jdbcTemplate.query(sql, new Object[0], ((rs1, rowNum) -> "dummy"));
 
         assertAll(
                 () -> assertThat(result).isEmpty(),
@@ -72,6 +84,56 @@ class JdbcTemplateTest {
                 () -> verify(conn).prepareStatement(sql),
                 () -> verify(pstmt, times(0)).setObject(anyInt(), any()),
                 () -> verify(rs).next()
+        );
+    }
+
+    @Test
+    void queryForObject_메서드_내부_동작을_테스트한다() throws SQLException {
+        String sql = "";
+
+        when(dataSource.getConnection()).thenReturn(conn);
+        when(conn.prepareStatement(sql, TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY)).thenReturn(pstmt);
+        when(pstmt.executeQuery()).thenReturn(rs);
+        when(rs.last()).thenReturn(true);
+        when(rs.getRow()).thenReturn(1);
+        doNothing().when(rs)
+                .beforeFirst();
+        when(rs.next()).thenReturn(true);
+
+        Object result = jdbcTemplate.queryForObject(sql, new Object[0], ((rs1, rowNum) -> "dummy"));
+
+        assertAll(
+                () -> assertThat(result).isNotNull(),
+                () -> verify(dataSource).getConnection(),
+                () -> verify(conn).prepareStatement(sql, TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY),
+                () -> verify(pstmt, times(0)).setObject(anyInt(), any()),
+                () -> verify(rs).next(),
+                () -> verify(rs).getRow()
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 2})
+    void queryForObject의_조회_결과가_1개가_아니면_예외를_반환한다(final int resultSetSize) throws SQLException {
+        String sql = "";
+
+        when(dataSource.getConnection()).thenReturn(conn);
+        when(conn.prepareStatement(sql, TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY)).thenReturn(pstmt);
+        when(pstmt.executeQuery()).thenReturn(rs);
+        when(rs.last()).thenReturn(true);
+        when(rs.getRow()).thenReturn(resultSetSize);
+        doNothing().when(rs)
+                .beforeFirst();
+        when(rs.next()).thenReturn(resultSetSize > 0);
+
+        assertAll(
+                () -> assertThatThrownBy(
+                        () -> jdbcTemplate.queryForObject(sql, new Object[0], (rs1, rowNum) -> "dummy"))
+                        .isInstanceOf(DataAccessException.class),
+                () -> verify(dataSource).getConnection(),
+                () -> verify(conn).prepareStatement(sql, TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY),
+                () -> verify(pstmt, times(0)).setObject(anyInt(), any()),
+                () -> verify(rs).getRow()
         );
     }
 }
