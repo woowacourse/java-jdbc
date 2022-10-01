fc7402cdfe0a10c6036a2dc77870b8bb9b4da642
jurlring <78091011+jurlring@users.noreply.github.com>
2022-10-02 03:24:05 +0900

    feat: JdbcTemplate 구현

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,72 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Parameters parameters) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParameter(statement, parameters);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final Parameters parameters, final RowMapper<T> rowMapper) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParameter(statement, parameters);
+            return queryForObject(statement, rowMapper);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> T queryForObject(PreparedStatement statement, RowMapper<T> rowMapper) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
+                return rowMapper.mapRow(resultSet);
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+        throw new IllegalStateException();
+    }
+
+    public <T> List<T> query(final String sql, final Parameters parameters, final RowMapper<T> rowMapper) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParameter(statement, parameters);
+            return query(statement, rowMapper);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> List<T> query(PreparedStatement statement, RowMapper<T> rowMapper) {
+        try (ResultSet resultSet = statement.executeQuery()) {
+            List<T> result = new ArrayList<>();
+            if (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet));
+            }
+            return result;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void setParameter(PreparedStatement statement, Parameters parameters) throws SQLException {
+        int index = 1;
+        for (Object parameter : parameters.getParameters()) {
+            statement.setObject(index++, parameter);
+        }
     }
 }
