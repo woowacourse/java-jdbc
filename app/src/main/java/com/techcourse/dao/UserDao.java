e252230837a14c05fd0e7a88838b5f15024dc6e1
DOES <ldk980130@gmail.com>
2022-10-01 15:52:39 +0900

    refactor: insert, update 쿼리 jdbcTemplate에서 처리

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -21,84 +21,28 @@ public class UserDao {
 	private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
 	private final DataSource dataSource;
+	private final JdbcTemplate jdbcTemplate;
 
 	public UserDao(final DataSource dataSource) {
 		this.dataSource = dataSource;
-	}
-
-	public UserDao(final JdbcTemplate jdbcTemplate) {
-		this.dataSource = null;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	public void insert(final User user) {
-		final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = dataSource.getConnection();
-			pstmt = conn.prepareStatement(sql);
-
-			log.debug("query : {}", sql);
-
-			pstmt.setString(1, user.getAccount());
-			pstmt.setString(2, user.getPassword());
-			pstmt.setString(3, user.getEmail());
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			log.error(e.getMessage(), e);
-			throw new RuntimeException(e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-			} catch (SQLException ignored) {
-			}
-
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException ignored) {
-			}
-		}
+		jdbcTemplate.createQuery("insert into users (account, password, email) values (?, ?, ?)")
+			.setString(1, user.getAccount())
+			.setString(2, user.getPassword())
+			.setString(3, user.getEmail())
+			.executeUpdate();
 	}
 
 	public void update(final User user) {
-		final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
-
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		try {
-			conn = dataSource.getConnection();
-			pstmt = conn.prepareStatement(sql);
-
-			log.debug("query : {}", sql);
-
-			pstmt.setString(1, user.getAccount());
-			pstmt.setString(2, user.getPassword());
-			pstmt.setString(3, user.getEmail());
-			pstmt.setLong(4, user.getId());
-			pstmt.executeUpdate();
-		} catch (SQLException e) {
-			log.error(e.getMessage(), e);
-			throw new RuntimeException(e);
-		} finally {
-			try {
-				if (pstmt != null) {
-					pstmt.close();
-				}
-			} catch (SQLException ignored) {
-			}
-
-			try {
-				if (conn != null) {
-					conn.close();
-				}
-			} catch (SQLException ignored) {
-			}
-		}
+		jdbcTemplate.createQuery("update users set account = ?, password = ?, email = ? where id = ?")
+			.setString(1, user.getAccount())
+			.setString(2, user.getPassword())
+			.setString(3, user.getEmail())
+			.setLong(4, user.getId())
+			.executeUpdate();
 	}
 
 	public List<User> findAll() {
@@ -201,51 +145,51 @@ public class UserDao {
 	}
 
 	public User findByAccount(final String account) {
-        final var sql = "select id, account, password, email from users where account = ?";
+		final var sql = "select id, account, password, email from users where account = ?";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, account);
-            rs = pstmt.executeQuery();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, account);
+			rs = pstmt.executeQuery();
 
-            log.debug("query : {}", sql);
+			log.debug("query : {}", sql);
 
-            if (rs.next()) {
-                return new User(
-                    rs.getLong(1),
-                    rs.getString(2),
-                    rs.getString(3),
-                    rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
+			if (rs.next()) {
+				return new User(
+					rs.getLong(1),
+					rs.getString(2),
+					rs.getString(3),
+					rs.getString(4));
+			}
+			return null;
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+			throw new RuntimeException(e);
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+			} catch (SQLException ignored) {
+			}
 
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+			} catch (SQLException ignored) {
+			}
 
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException ignored) {
+			}
+		}
 	}
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,17 +1,94 @@
 package nextstep.jdbc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+	private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
-    private final DataSource dataSource;
+	private final DataSource dataSource;
 
-    public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+	public JdbcTemplate(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
+
+	public SqlBuilder createQuery(String sql) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		try {
+			conn = dataSource.getConnection();
+			pstmt = conn.prepareStatement(sql);
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+			close(conn, pstmt);
+			throw new RuntimeException(e);
+		}
+		return new SqlBuilder(conn, pstmt);
+	}
+
+	public static class SqlBuilder {
+
+		private final Connection conn;
+		private final PreparedStatement pstmt;
+
+		public SqlBuilder(Connection conn, PreparedStatement pstmt) {
+			this.conn = conn;
+			this.pstmt = pstmt;
+		}
+
+		public SqlBuilder setString(int parameterIndex, String parameter) {
+			try {
+				pstmt.setString(parameterIndex, parameter);
+			} catch (SQLException e) {
+				log.error(e.getMessage(), e);
+				close(conn, pstmt);
+				throw new RuntimeException(e);
+			}
+			return this;
+		}
+
+		public SqlBuilder setLong(int parameterIndex, Long parameter){
+			try {
+				pstmt.setLong(parameterIndex, parameter);
+			} catch (SQLException e) {
+				log.error(e.getMessage(), e);
+				close(conn, pstmt);
+				throw new RuntimeException(e);
+			}
+			return this;
+		}
+
+		public void executeUpdate() {
+			try {
+				pstmt.executeUpdate();
+			} catch (SQLException e) {
+				log.error(e.getMessage(), e);
+				throw new RuntimeException(e);
+			} finally {
+				close(conn, pstmt);
+			}
+		}
+
+	}
+	private static void close(Connection conn, PreparedStatement pstmt) {
+		try {
+			if (pstmt != null) {
+				pstmt.close();
+			}
+		} catch (SQLException ignored) {
+		}
+		try {
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException ignored) {
+		}
+	}
 }
