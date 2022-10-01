9cd3508884e090e742f37df18fb537a21e82d519
cjlee38 <cjlee38@github.com>
2022-10-02 00:14:38 +0900

    feat: change data source username

--- app/src/main/java/com/techcourse/config/DataSourceConfig.java
+++ app/src/main/java/com/techcourse/config/DataSourceConfig.java
@@ -18,7 +18,7 @@ public class DataSourceConfig {
     private static JdbcDataSource createJdbcDataSource() {
         final var jdbcDataSource = new JdbcDataSource();
         jdbcDataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;");
-        jdbcDataSource.setUser("");
+        jdbcDataSource.setUser("sa");
         jdbcDataSource.setPassword("");
         return jdbcDataSource;
     }
