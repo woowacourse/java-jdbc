91226aa8c3a8dfe77f77c074af9abae209361f50
hyunrrr <whgusrms96@gmail.com>
2022-10-01 22:50:52 +0900

    feat: jdbcTemplate클래스에 insert 메서드 구현

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,5 +1,10 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,4 +19,37 @@ public class JdbcTemplate {
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public Long insert(final String sql, Object ... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt= conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            int index = 1;
+            for (Object arg : args) {
+                if (arg instanceof String) {
+                    pstmt.setString(index++, (String)arg);
+                } else if (arg instanceof Long) {
+                    pstmt.setLong(index++, (Long)arg);
+                }
+            }
+            pstmt.executeUpdate();
+
+            return getGeneratedKey(pstmt);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Long getGeneratedKey(PreparedStatement pstmt) {
+        try (ResultSet rs = pstmt.getGeneratedKeys()) {
+            if (rs.next()) {
+                return rs.getLong(1);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,34 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import javax.sql.DataSource;
+import org.h2.jdbcx.JdbcDataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        final var jdbcDataSource = new JdbcDataSource();
+        jdbcDataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;");
+        jdbcDataSource.setUser("");
+        jdbcDataSource.setPassword("");
+        DatabasePopulatorUtils.execute(jdbcDataSource);
+        jdbcTemplate = new JdbcTemplate(jdbcDataSource);
+    }
+
+    @DisplayName("insert 쿼리를 완성시켜 실행시킨다.")
+    @Test
+    void insert() {
+        TestUser user = new TestUser("account", "password", "email");
+        final String sql = "insert into users (account, password, email) values (?, ?, ?)";
+        Long id = jdbcTemplate.insert(sql, user.getAccount(), user.getPassword(), user.getEmail());
+
+        assertThat(id).isEqualTo(1L);
+    }
 }
