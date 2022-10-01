be0b1c143e3475e481b9165684043d3ee6f6342e
Wishoon <heonga2@gmail.com>
2022-10-01 21:48:06 +0900

    refactor: UserDao findById 기능 JdbcTemplate으로 변경

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -46,6 +46,12 @@ public class UserDao {
         return jdbcTemplate.queryForList(sql, getUserRowMapper());
     }
 
+    public User findById(final Long id) {
+        final var sql = "select id, account, password, email from users where id = ?";
+
+        return jdbcTemplate.queryForObject(sql, getUserRowMapper(), id);
+    }
+
     private RowMapper<User> getUserRowMapper() {
         return (rs, rm) ->
             new User(
@@ -54,55 +60,6 @@ public class UserDao {
                 rs.getString(3),
                 rs.getString(4)
             );
-    }
-
-    public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                    rs.getLong(1),
-                    rs.getString(2),
-                    rs.getString(3),
-                    rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
     }
 
     public User findByAccount(final String account) {
