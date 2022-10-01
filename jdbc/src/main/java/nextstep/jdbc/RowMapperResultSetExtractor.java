403e495c6ef8111b59bd1b88118e0d6a8235c313
jaejae-yoo <wotj102@gmail.com>
2022-10-02 01:00:36 +0900

    refactor: resultSet 결과 추출 클래스 분리

---
+++ jdbc/src/main/java/nextstep/jdbc/RowMapperResultSetExtractor.java
@@ -0,0 +1,25 @@
+package nextstep.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.jdbc.core.RowMapper;
+
+public class RowMapperResultSetExtractor<T> {
+
+    private final RowMapper<T> rowMapper;
+
+    public RowMapperResultSetExtractor(final RowMapper<T> rowMapper) {
+        this.rowMapper = rowMapper;
+    }
+
+    public List<T> extractData(ResultSet resultSet) throws SQLException {
+        List<T> results = new ArrayList<>();
+        int rowNum = 0;
+        while (resultSet.next()) {
+            results.add(rowMapper.mapRow(resultSet, rowNum++));
+        }
+        return results;
+    }
+}
