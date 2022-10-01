9f086040cee7292739faa671d787a33c28c78886
jaejae-yoo <wotj102@gmail.com>
2022-10-02 00:58:45 +0900

    test: JdbcTemplate 테스트 작성

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,37 +21,33 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(final String sql, Object... args) {
+    public int update(final String sql, Object... args) {
         try (final Connection connection = dataSource.getConnection();
              final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
             for (int idx = 0; idx < args.length; idx++) {
                 preparedStatement.setObject(idx + 1, args[idx]);
             }
-            preparedStatement.executeUpdate();
+            return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             log.error("Execute Query Failed: " + e);
-            throw new RuntimeException("Query를 성공적으로 실행하지 못했습니다.");
+            throw new DataAccessException("Query를 성공적으로 실행하지 못했습니다.");
         }
     }
 
     public <T> List<T> query(final String sql, RowMapper<T> rowMapper, Object... args) {
-        List<T> results = new ArrayList<>();
         try (final Connection connection = dataSource.getConnection();
              final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
             for (int idx = 0; idx < args.length; idx++) {
                 preparedStatement.setObject(idx + 1, args[idx]);
             }
+
             final ResultSet resultSet = preparedStatement.executeQuery();
-            int rowNum = 0;
-            while (resultSet.next()) {
-                results.add(rowMapper.mapRow(resultSet, rowNum++));
-            }
-            return results;
+            final RowMapperResultSetExtractor<T> resultSetExtractor = new RowMapperResultSetExtractor<>(rowMapper);
+            return resultSetExtractor.extractData(resultSet);
         } catch (SQLException e) {
             log.error("Execute Query Failed: " + e);
-            throw new RuntimeException("Query를 성공적으로 실행하지 못했습니다.");
+            throw new DataAccessException("Query를 성공적으로 실행하지 못했습니다.");
         }
     }
 }
