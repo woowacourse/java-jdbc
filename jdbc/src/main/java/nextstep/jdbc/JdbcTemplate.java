2066431515aef2574c4ad6f3a5023439e4b2e845
yxxnghwan <younghwan960@gmail.com>
2022-10-02 02:38:58 +0900

    feat: JdbcTemplate 구현하고 테스트 작성

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,80 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             final ResultSet resultSet = getResultSet(preparedStatement, params)) {
+            return makeQueryResult(rowMapper, resultSet);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> List<T> makeQueryResult(final RowMapper<T> rowMapper, final ResultSet resultSet) throws SQLException {
+        final List<T> result = new ArrayList<>();
+        while (resultSet.next()) {
+            result.add(rowMapper.mapRow(resultSet, resultSet.getRow()));
+        }
+        return result;
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             final ResultSet resultSet = getResultSet(preparedStatement, params)) {
+            checkEmptyResult(resultSet);
+            final T result = rowMapper.mapRow(resultSet, resultSet.getRow());
+            checkMultiResult(resultSet);
+            return result;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void checkEmptyResult(final ResultSet resultSet) throws SQLException {
+        if (!resultSet.next()) {
+            throw new DataAccessException("결과가 없습니다!");
+        }
+    }
+
+    private void checkMultiResult(final ResultSet resultSet) throws SQLException {
+        if (!resultSet.isAfterLast()) {
+            throw new DataAccessException("쿼리 결과가 한 개가 아닙니다.");
+        }
+    }
+
+    private ResultSet getResultSet(final PreparedStatement prepareStatement, final Object[] params)
+            throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            prepareStatement.setObject(i - 1, params[i]);
+        }
+
+        return prepareStatement.executeQuery();
+    }
+
+    public Long insert(final PreparedStatementCreator preparedStatementCreator) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = preparedStatementCreator.createPreparedStatement(connection)) {
+            preparedStatement.executeUpdate();
+            final ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+            if (generatedKeys.next()) {
+                return generatedKeys.getLong(1);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public int update(final PreparedStatementCreator preparedStatementCreator) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = preparedStatementCreator.createPreparedStatement(connection)) {
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,128 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private final DataSource dataSource = mock(DataSource.class);
+    private final Connection connection = mock(Connection.class);
+    private final PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    private final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+    private final ResultSet resultSet = mock(ResultSet.class);
+
+    @Test
+    void query() throws SQLException {
+        final String sql = "";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+
+        final List<Object> result = jdbcTemplate.query(sql, ((rs, rowNum) -> null), 1004, "대충 더미파라미터");
+
+        assertAll(
+                () -> assertThat(result).isEmpty(),
+                () -> verify(preparedStatement, times(2)).setObject(anyInt(), any())
+        );
+    }
+
+    @Test
+    void queryForObject() throws SQLException {
+        final String sql = "";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.getRow()).thenReturn(1);
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.isAfterLast()).thenReturn(true);
+
+        final Object result = jdbcTemplate.queryForObject(sql, ((rs1, rowNum) -> null), 1004, "대충 더미파라미터");
+
+        assertAll(
+                () -> assertThat(result).isNull(),
+                () -> verify(preparedStatement, times(2)).setObject(anyInt(), any())
+        );
+    }
+
+    @Test
+    @DisplayName("queryForObject의 조회 결과가 없으면 예외를 반환한다.")
+    void queryForObjectNoResult() throws SQLException {
+        final String sql = "";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+
+        assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, (rs, rowNum) -> null))
+                .isInstanceOf(DataAccessException.class);
+    }
+
+    @Test
+    @DisplayName("queryForObject의 조회 결과가 1보다 많으면 예외가 발생한다.")
+    void queryForObjectMultiResult() throws SQLException {
+        final String sql = "";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.isAfterLast()).thenReturn(false);
+
+        assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, (rs, rowNum) -> null))
+                .isInstanceOf(DataAccessException.class);
+    }
+
+    @Test
+    void update() throws SQLException {
+        final String sql = "";
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+
+        jdbcTemplate.update(connection -> connection.prepareStatement(sql));
+
+        assertAll(
+                () -> verify(dataSource).getConnection(),
+                () -> verify(connection).prepareStatement(sql),
+                () -> verify(preparedStatement).executeUpdate()
+        );
+    }
+
+    @Test
+    @DisplayName("SQLExcetion을 unchecked Exception으로 변경한다.")
+    void SQLExceptionToDataAccessException() throws SQLException {
+        final String sql = "";
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeUpdate()).thenThrow(SQLException.class);
+        when(preparedStatement.executeQuery()).thenThrow(SQLException.class);
+
+        assertAll(
+                () -> assertThatThrownBy(() -> jdbcTemplate.update((conn) -> conn.prepareStatement(sql)))
+                        .isInstanceOf(DataAccessException.class),
+                () -> assertThatThrownBy(() -> jdbcTemplate.query(sql, (rs, rowNum) -> null))
+                        .isInstanceOf(DataAccessException.class),
+                () -> assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, (rs, rowNum) -> null))
+                        .isInstanceOf(DataAccessException.class)
+        );
+    }
 }
