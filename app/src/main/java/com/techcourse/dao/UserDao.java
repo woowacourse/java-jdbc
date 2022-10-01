13a6f3573ce2dd842f714addf89ae3ffd372bc2d
Wishoon <heonga2@gmail.com>
2022-10-01 21:48:57 +0900

    refactor: UserDao findByAccount 기능 JdbcTemplate으로 변경

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,10 +1,6 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
@@ -52,6 +48,12 @@ public class UserDao {
         return jdbcTemplate.queryForObject(sql, getUserRowMapper(), id);
     }
 
+    public User findByAccount(final String account) {
+        final var sql = "select * from users where account = ?";
+
+        return jdbcTemplate.queryForObject(sql, getUserRowMapper(), account);
+    }
+
     private RowMapper<User> getUserRowMapper() {
         return (rs, rm) ->
             new User(
@@ -60,54 +62,5 @@ public class UserDao {
                 rs.getString(3),
                 rs.getString(4)
             );
-    }
-
-    public User findByAccount(final String account) {
-        final var sql = "select * from users where account = ?";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, account);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                    rs.getLong(1),
-                    rs.getString(2),
-                    rs.getString(3),
-                    rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException e) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException e) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException e) {
-            }
-        }
     }
 }
