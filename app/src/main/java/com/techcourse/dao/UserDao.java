200e0cc41c2a195bafa63778dd08226f13508845
Jeong Jinwoo <jwjeong96@gmail.com>
2022-10-02 19:10:50 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 정(정진우) 미션 제출합니다. (#53)
    * refactor(DatabasePopulatorUtils): cleanse with try-with-resources syntax
    
    * refactor(UserDao): auto close with try-with-resources syntax
    
    * feat(UserDao): implement update method
    
    * feat(UserDao): implement all find methods
    
    * feat(RowMapper): implement functional interface for generating object with ResultSet
    
    * refactor(UserDao): cleanse duplicate ResultSet logic with query and RowMapper
    
    * feat(ParameterSource): implement class for setting values to PreparedStatement
    
    * refactor(UserDao): implement executeUpdate and executeQuery as private methods
    
    * feat(JdbcTemplate): implement executeQuery and executeUpdate with generics
    
    * refactor(UserDao): move all JDBC logic to JdbcTemplate
    
    * feat(UserDao): return Optional instead of null
    
    * fix(JdbcTemplate): read all rows instead of zeor or single row
    
    * refactor: overall cleansing
    
    * feat(JdbcTemplate): implement custom queryForObject for returning Optional

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,121 +1,68 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
+import java.util.Optional;
 import nextstep.jdbc.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import nextstep.jdbc.ParameterSource;
+import nextstep.jdbc.RowMapper;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
-
-    private final DataSource dataSource;
-
-    public UserDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = null;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        final var parameterSource = new ParameterSource();
+        parameterSource.addParam(user.getAccount());
+        parameterSource.addParam(user.getPassword());
+        parameterSource.addParam(user.getEmail());
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            log.debug("query : {}", sql);
-
-            pstmt.setString(1, user.getAccount());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getEmail());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
-        }
+        jdbcTemplate.executeUpdate(sql, parameterSource);
     }
 
     public void update(final User user) {
-        // todo
+        final var sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE id = ?";
+        final var parameterSource = new ParameterSource();
+        parameterSource.addParam(user.getAccount());
+        parameterSource.addParam(user.getPassword());
+        parameterSource.addParam(user.getEmail());
+        parameterSource.addParam(user.getId());
+
+        jdbcTemplate.executeUpdate(sql, parameterSource);
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
+        final var sql = "SELECT id, account, password, email FROM users";
+        final var parameterSource = new ParameterSource();
+
+        return jdbcTemplate.executeQuery(sql, parameterSource, rowMapper);
     }
 
-    public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
+    public Optional<User> findById(final Long id) {
+        final var sql = "SELECT id, account, password, email FROM users WHERE id = ?";
+        final var parameterSource = new ParameterSource();
+        parameterSource.addParam(id);
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
-        }
+        return jdbcTemplate.executeQueryForObject(sql, parameterSource, rowMapper);
     }
 
-    public User findByAccount(final String account) {
-        // todo
-        return null;
+    public Optional<User> findByAccount(final String account) {
+        final var sql = "SELECT id, account, password, email FROM users WHERE account = ?";
+        final var parameterSource = new ParameterSource();
+        parameterSource.addParam(account);
+
+        return jdbcTemplate.executeQueryForObject(sql, parameterSource, rowMapper);
     }
+
+    private final RowMapper<User> rowMapper = (resultSet) ->
+            new User(resultSet.getLong("id"),
+                    resultSet.getString("account"),
+                    resultSet.getString("password"),
+                    resultSet.getString("email"));
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,5 +1,11 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +19,60 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> executeQuery(String sql, ParameterSource parameterSource, RowMapper<T> rowMapper) {
+        try (final var connection = getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            setParams(preparedStatement, parameterSource);
+            return query(preparedStatement, rowMapper);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> Optional<T> executeQueryForObject(String sql, ParameterSource parameterSource, RowMapper<T> rowMapper) {
+        List<T> entities = executeQuery(sql, parameterSource, rowMapper);
+        if (entities.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(entities.get(0));
+    }
+
+    public void executeUpdate(String sql, ParameterSource parameterSource) {
+        try (final var connection = getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            setParams(preparedStatement, parameterSource);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void setParams(PreparedStatement preparedStatement, ParameterSource parameterSource) throws SQLException {
+        for (var index = 0; index < parameterSource.getParamCount(); index++) {
+            preparedStatement.setObject(index + 1, parameterSource.getParam(index));
+        }
+    }
+
+    private Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> List<T> query(PreparedStatement preparedStatement, RowMapper<T> rowMapper) throws SQLException {
+        try (final var resultSet = preparedStatement.executeQuery()) {
+            List<T> entities = new ArrayList<>();
+            while (resultSet.next()) {
+                entities.add(rowMapper.mapRow(resultSet));
+            }
+            return entities;
+        }
     }
 }
