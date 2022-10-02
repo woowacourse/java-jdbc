01a7a0d37a6d045326eb8d94789053e675dbaded
sinb57 <sinb57@naver.com>
2022-10-03 01:28:41 +0900

    refactor: QueryExecutor 인터페이스 분리 및 JdbcTemplate 리팩토링

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -23,59 +23,49 @@ public class JdbcTemplate {
     }
 
     public void update(final String sql, final Object... objects) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
+        executePreparedStatement(sql, pstmt -> {
             setParams(pstmt, objects);
             pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+            return null;
+        });
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-            return execute(pstmt, rowMapper);
-
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        return executePreparedStatement(sql, pstmt -> executeQuery(pstmt, rowMapper));
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
+        return executePreparedStatement(sql, pstmt -> {
+            setParams(pstmt, objects);
+            final var results = executeQuery(pstmt, rowMapper);
+            return nullableSingleResult(results);
+        });
+    }
+
+    private <T> T executePreparedStatement(final String sql, QueryExecutor<T> executor) {
         try (final Connection conn = dataSource.getConnection();
              final PreparedStatement pstmt = conn.prepareStatement(sql)
         ) {
             log.debug("query : {}", sql);
-            setParams(pstmt, objects);
+            return executor.apply(pstmt);
 
-            final List<T> results = execute(pstmt, rowMapper);
-            return nullableSingleResult(results);
-
-        } catch (SQLException e) {
+        } catch (final SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
 
-    private <T> List<T> execute(final PreparedStatement pstmt, final RowMapper<T> rowMapper) {
+    private <T> List<T> executeQuery(final PreparedStatement pstmt, final RowMapper<T> rowMapper) {
         try (final ResultSet rs = pstmt.executeQuery()) {
-            return extract(rs, rowMapper);
+            return convertRows(rs, rowMapper);
 
-        } catch (SQLException e) {
+        } catch (final SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
 
-    private <T> List<T> extract(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
+    private <T> List<T> convertRows(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
         final List<T> results = new ArrayList<>();
         while (rs.next()) {
             final T result = rowMapper.mapTow(rs);
