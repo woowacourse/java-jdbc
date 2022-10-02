6f99f59b4f243236c4bd805f80d3ca28f92c183b
hyunrrr <whgusrms96@gmail.com>
2022-10-02 18:53:44 +0900

    feat: jdbcTemplate의 find, finds 기능 구현

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,14 +1,17 @@
 package nextstep.jdbc;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -52,4 +55,86 @@ public class JdbcTemplate {
         }
     }
 
+    public List<Object> finds(Class<?> classType, String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            setSqlParameters(pstmt, args);
+            Constructor<?> properConstructor = getProperConstructor(classType);
+
+            List<Object> bindingObjects = new ArrayList<>();
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                while (resultSet.next()) {
+                    bindingObjects.add(makeInstanceFromSqlResult(resultSet, properConstructor));
+                }
+            }
+            return bindingObjects;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    public Object find(Class<?> classType, String sql, Object...args) {
+        List<Object> result = finds(classType, sql, args);
+        if (result.size() != 1) {
+            throw new IllegalStateException();
+        }
+        return result.get(0);
+    }
+
+    private Object makeInstanceFromSqlResult(ResultSet resultSet, Constructor<?> constructor) {
+        int index = 1;
+        Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+        List<Object> constructorParameters = new ArrayList<>();
+        for (Class<?> parameterType : parameterTypes) {
+            constructorParameters.add(extractData(parameterType, resultSet, index++));
+        }
+
+        try {
+            return constructor.newInstance(constructorParameters.toArray());
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    private Object extractData(Class<?> parameterType, ResultSet resultSet, int index) {
+        try {
+            if (String.class.equals(parameterType)) {
+                return resultSet.getString(index);
+            }
+            if (Long.TYPE.equals(parameterType)) {
+                return resultSet.getLong(index);
+            }
+            throw new IllegalStateException();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            throw new RuntimeException();
+        }
+    }
+
+    private void setSqlParameters(PreparedStatement pstmt, Object... args) throws SQLException {
+        int index = 1;
+        for (Object arg : args) {
+            Class<?> aClass = arg.getClass();
+            if (String.class.equals(aClass)) {
+                pstmt.setString(index++, (String) arg);
+            } else if (Long.class.equals(aClass) || Long.TYPE.equals(aClass)) {
+                pstmt.setLong(index++, (long) arg);
+            }
+        }
+    }
+
+    private Constructor<?> getProperConstructor(Class<?> classType) {
+        Constructor<?>[] constructors = classType.getConstructors();
+        int fieldNumber = classType.getDeclaredFields().length;
+        for (Constructor<?> constructor : constructors) {
+            if (fieldNumber == constructor.getParameterCount()) {
+                return constructor;
+            }
+        }
+        throw new IllegalStateException();
+    }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,8 +1,9 @@
 package nextstep.jdbc;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-import javax.sql.DataSource;
+import java.util.List;
 import org.h2.jdbcx.JdbcDataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -30,5 +31,34 @@ class JdbcTemplateTest {
         Long id = jdbcTemplate.insert(sql, user.getAccount(), user.getPassword(), user.getEmail());
 
         assertThat(id).isEqualTo(1L);
+    }
+
+    @DisplayName("데이터 하나만 반환하는 find 쿼리를 완성시켜 실행시킨다.")
+    @Test
+    void find() {
+        TestUser user = new TestUser("account", "password", "email");
+        String sql = "insert into users (account, password, email) values (?, ?, ?)";
+        Long id = jdbcTemplate.insert(sql, user.getAccount(), user.getPassword(), user.getEmail());
+
+        sql = "select id, account, password, email from users where id = ?";
+        Object result = jdbcTemplate.find(TestUser.class, sql, id);
+
+        assertThat((TestUser) result).isEqualTo(user);
+    }
+
+    @DisplayName("데이터를 조회하는 finds 쿼리를 완성시켜 실행시킨다.")
+    @Test
+    void finds() {
+        TestUser user = new TestUser("account", "password", "email");
+        String sql = "insert into users (account, password, email) values (?, ?, ?)";
+        jdbcTemplate.insert(sql, user.getAccount(), user.getPassword(), user.getEmail());
+
+        sql = "select id, account, password, email from users where account = ?";
+        List<Object> results = jdbcTemplate.finds(TestUser.class, sql, user.getAccount());
+
+        assertAll(
+                () -> assertThat(results.size()).isEqualTo(1),
+                () -> assertThat(results.get(0)).isEqualTo(user)
+        );
     }
 }
