e5e8362c1b4efd070a4e7af04d85eff3a52a20f1
hyeonic <dev.hyeonic@gmail.com>
2022-10-02 14:23:52 +0900

    refactor: 불필요한 Spring 의존성 제거

--- jdbc/src/main/java/nextstep/util/Assert.java
+++ jdbc/src/main/java/nextstep/util/Assert.java
@@ -1,10 +1,8 @@
 package nextstep.util;
 
-import org.springframework.lang.Nullable;
-
 public class Assert {
 
-    public static void notNull(@Nullable Object object, String message) {
+    public static void notNull(final Object object, final String message) {
         if (object == null) {
             throw new IllegalArgumentException(message);
         }
