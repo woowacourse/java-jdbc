b34836c228a0614c8f306d2decef50d9ecf2fcd0
bomi <bonita9931@gmail.com>
2022-10-02 15:29:13 +0900

    feat: jdbcTemplate을 이용하여 findById 할 수 있도록 수정

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,10 +1,7 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
@@ -54,50 +51,13 @@ public class UserDao {
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
+        return jdbcTemplate.queryForObject(sql, (ResultSet rs, int rowNum) ->
+                        new User(
+                                rs.getLong("id"),
+                                rs.getString("account"),
+                                rs.getString("password"),
+                                rs.getString("email"))
+                , id);
     }
 
     public User findByAccount(final String account) {
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -2,10 +2,12 @@ package nextstep.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.RowMapper;
 
 public class JdbcTemplate {
 
@@ -17,16 +19,12 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(String sql, Object... params) {
+    public void update(String sql, Object... args) {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             log.debug("query : {}", sql);
 
-            int index = 1;
-            for (Object param : params) {
-                pstmt.setObject(index, param);
-                index++;
-            }
+            setArgument(pstmt, args);
 
             pstmt.executeUpdate();
         } catch (SQLException e) {
@@ -34,6 +32,40 @@ public class JdbcTemplate {
             throw new RuntimeException(e);
         }
     }
-    
 
+    private void setArgument(final PreparedStatement pstmt, final Object[] args) throws SQLException {
+        int index = 1;
+        for (Object arg : args) {
+            pstmt.setObject(index, arg);
+            index++;
+        }
+    }
+
+    public <T> T queryForObject(final String sql, RowMapper<T> rowMapper, Object... args) {
+
+        ResultSet rs = null;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setArgument(pstmt, args);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return rowMapper.mapRow(rs, rs.getRow());
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
+    }
+
+    public void queryForObject(final String sql, final Object o, final Long id) {
+    }
 }
