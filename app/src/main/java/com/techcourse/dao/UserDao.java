5d2ee15a15c071d83526c4592c85bf017ec43f6f
Gyuchool <balance0591@naver.com>
2022-10-02 22:02:00 +0900

    refactor: apply constructor chaining

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import java.util.List;
+import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
 import nextstep.jdbc.core.RowMapper;
 import org.slf4j.Logger;
@@ -10,12 +11,16 @@ import org.slf4j.LoggerFactory;
 public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
-    static RowMapper<User> userMapper = (rs, rowNum) -> new User( rs.getLong("id"),
+    static RowMapper<User> userMapper = (rs, rowNum) -> new User(rs.getLong("id"),
             rs.getString("account"),
             rs.getString("password"),
             rs.getString("email"));
 
     private final JdbcTemplate jdbcTemplate;
+
+    public UserDao(final DataSource dataSource) {
+        this(new JdbcTemplate(dataSource));
+    }
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -44,7 +49,7 @@ public class UserDao {
     }
 
     public User findByAccount(final String account) {
-        final var sql  = "select id, account, password, email from users where account = ?";
+        final var sql = "select id, account, password, email from users where account = ?";
 
         return jdbcTemplate.queryForObject(sql, userMapper, account);
     }
