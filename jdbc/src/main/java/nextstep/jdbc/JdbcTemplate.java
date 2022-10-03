d5725919b837dd461ce08464d17edb43c57cdad6
devHudi <devhudi@gmail.com>
2022-10-04 00:35:12 +0900

    feat: update 및 query 메소드 구현

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,42 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+
+            return preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new DataAccessException(e.getMessage(), e);
+            // TODO: 더 추상화된 예외 메시지를 사용해야함
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+
+            List<T> results = new ArrayList<>();
+            ResultSet rs = preparedStatement.executeQuery();
+            if (rs.next()) {
+                T result = rowMapper.run(rs);
+                results.add(result);
+            }
+
+            return results;
+        } catch (final SQLException e) {
+            throw new DataAccessException(e.getMessage(), e);
+            // TODO: 더 추상화된 예외 메시지를 사용해야함
+        }
     }
 }
