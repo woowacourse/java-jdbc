b2502bca70e055de589cea27bc047d54a20c945b
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-10-03 18:30:43 +0900

    feat: JdbcTemplate query 추가 및 적용

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -101,51 +102,7 @@ public class UserDao {
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
+        return jdbcTemplate.query(sql, userRowMapper, id);
     }
 
     public User findByAccount(final String account) {
@@ -183,4 +140,10 @@ public class UserDao {
             }
         }
     }
+
+    private RowMapper<User> userRowMapper = rs -> new User(
+            rs.getLong(1),
+            rs.getString(2),
+            rs.getString(3),
+            rs.getString(4));
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -2,10 +2,12 @@ package nextstep.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
 
 public class JdbcTemplate {
 
@@ -15,6 +17,30 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> T query(final String sql, @NonNull final RowMapper<T> rowMapper, final Object... params) {
+        ResultSet resultSet = null;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            prepareStatementSetParamters(pstmt, params);
+
+
+            resultSet = pstmt.executeQuery();
+            if (resultSet.next()) {
+                return rowMapper.mapRow(resultSet);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
     }
 
     public int executeUpdate(final String sql, final Object... params) {
@@ -27,7 +53,8 @@ public class JdbcTemplate {
         }
     }
 
-    private void prepareStatementSetParamters(final PreparedStatement pstmt, final Object[] params) throws SQLException {
+    private void prepareStatementSetParamters(final PreparedStatement pstmt, final Object[] params)
+            throws SQLException {
         for (int i = 1; i <= params.length; i++) {
             pstmt.setObject(i, params[i - 1]);
         }
