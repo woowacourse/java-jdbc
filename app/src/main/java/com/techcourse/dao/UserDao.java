f06cee3740e84316dbca6aa923960990d3fd859a
nailseong <nnis9857@gmail.com>
2022-10-03 21:53:26 +0900

    feat: findAll 메서드

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -30,8 +30,8 @@ public class UserDao {
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
+        final String sql = "SELECT id, account, password, email FROM users";
+        return jdbcTemplate.query(sql, userRowMapper);
     }
 
     public User findById(final Long id) {
