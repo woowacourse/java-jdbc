0720cce195804c5eab5ae513f9209987e29dfd8b
leokim <kunga077@gmail.com>
2022-10-03 20:33:29 +0900

    refactor: 적절한 예외가 발생하도록 수정

--- jdbc/src/main/java/nextstep/jdbc/DataAccessUtils.java
+++ jdbc/src/main/java/nextstep/jdbc/DataAccessUtils.java
@@ -1,9 +1,17 @@
 package nextstep.jdbc;
 
 import java.util.Collection;
+import nextstep.jdbc.exception.EmptyResultException;
+import nextstep.jdbc.exception.InvalidResultSizeException;
 
 public class DataAccessUtils {
     public static <T> T getSingleResult(final Collection<T> results) {
+        if (results.isEmpty()) {
+            throw new EmptyResultException();
+        }
+        if (results.size() > 1) {
+            throw new InvalidResultSizeException();
+        }
         return results.iterator().next();
     }
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcConnector.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcConnector.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import nextstep.jdbc.exception.ImpossibleSQLExecutionException;
 
 public class JdbcConnector implements Connector {
 
@@ -22,7 +23,7 @@ public class JdbcConnector implements Connector {
             }
             return queryExecutor.executePreparedStatement(preparedStatement);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new ImpossibleSQLExecutionException();
         }
     }
 }
