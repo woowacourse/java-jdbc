3339c04d6bb9a94224ce39cebc992ca8178ca41e
2yujeong <yjfflq4872@gmail.com>
2022-10-03 16:49:34 +0900

    refactor: JdbcTemplate 메소드 분리, springframework 의존성 제거

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -9,10 +9,6 @@ import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.dao.IncorrectResultSizeDataAccessException;
-import org.springframework.util.CollectionUtils;
 
 public class JdbcTemplate {
 
@@ -30,48 +26,58 @@ public class JdbcTemplate {
 
             log.debug("query : {}", sql);
 
-            for (int i = 0; i < args.length; i++) {
-                pstmt.setObject(i + 1, args[i]);
-            }
-
+            setParameters(pstmt, args);
             return pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void setParameters(final PreparedStatement pstmt, final Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
         }
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) throws DataAccessException {
-        try(Connection conn = dataSource.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            for (int i = 0; i < args.length; i++) {
-                pstmt.setObject(i + 1, args[i]);
-            }
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setParameters(pstmt, args);
             ResultSet rs = pstmt.executeQuery();
 
             log.debug("query : {}", sql);
 
-            List<T> result = new ArrayList<>();
-            while (rs.next()) {
-                result.add(rowMapper.mapRow(rs));
-            }
-            rs.close();
-
-            return result;
+            return createMethodByRowMapper(rowMapper, rs);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
+    }
+
+    private <T> List<T> createMethodByRowMapper(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
+        List<T> result = new ArrayList<>();
+        while (rs.next()) {
+            result.add(rowMapper.mapRow(rs));
+        }
+        rs.close();
+
+        return result;
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) throws DataAccessException {
         List<T> results = query(sql, rowMapper, args);
-        if (CollectionUtils.isEmpty(results)) {
-            throw new EmptyResultDataAccessException(1);
+        validateResultSizeSingle(results);
+
+        return results.iterator().next();
+    }
+
+    private <T> void validateResultSizeSingle(final List<T> results) {
+        if (results.isEmpty()) {
+            throw new DataAccessException("Empty result");
         }
         if (results.size() > 1) {
-            throw new IncorrectResultSizeDataAccessException(1, results.size());
+            throw new DataAccessException("Incorrect result size, expected : 1, actual : " + results.size());
         }
-        return results.iterator().next();
     }
 }
