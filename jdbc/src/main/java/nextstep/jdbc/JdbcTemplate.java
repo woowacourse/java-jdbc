82a83d0a5043f1b549247ff1f37eec87f718b488
dwl21 <nggus5@naver.com>
2022-10-03 20:03:06 +0900

    feat: INSERT 쿼리에 사용하는 update 메서드 구현

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,17 +1,46 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    public static final int PARAMETER_START_INDEX = 1;
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql) {
+        try (Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public int update(final String sql, final List<Object> values) {
+        try (Connection connection = dataSource.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            int index = PARAMETER_START_INDEX;
+            for (Object object : values) {
+                preparedStatement.setObject(index, object);
+                index++;
+            }
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,80 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 class JdbcTemplateTest {
 
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        DataSource dataSource = Mockito.mock(DataSource.class);
+        connection = Mockito.mock(Connection.class);
+        preparedStatement = Mockito.mock(PreparedStatement.class);
+        Mockito.when(dataSource.getConnection()).thenReturn(connection);
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Test
+    @DisplayName("update 메서드는 INSERT 쿼리를 처리할 수 있다.")
+    void update_success() throws SQLException {
+        User user = new User("leo", "password");
+        String sql = String.format(
+            "insert into users (account, password) values (%s, %s)",
+            user.getAccount(), user.getPassword());
+
+        Mockito.when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        jdbcTemplate.update(sql);
+
+        Mockito.verify(preparedStatement).executeUpdate();
+    }
+
+    @Test
+    @DisplayName("update 메서드는 파라미터가 있는 INSERT 쿼리를 처리할 수 있다.")
+    void update_insert() throws SQLException {
+        User user = new User("leo", "password");
+        List<Object> values = new ArrayList<>();
+        values.add(user.getAccount());
+        values.add(user.getPassword());
+
+        String sql = "insert into users (account, password) values (?, ?)";
+
+        Mockito.when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        jdbcTemplate.update(sql, values);
+
+        Mockito.verify(preparedStatement).setObject(1, "leo");
+        Mockito.verify(preparedStatement).setObject(2, "password");
+        Mockito.verify(preparedStatement).executeUpdate();
+    }
+
+    public static class User {
+
+        private final String account;
+        private final String password;
+
+        public User(String account, String password) {
+            this.account = account;
+            this.password = password;
+        }
+
+        public String getAccount() {
+            return account;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+    }
 }
