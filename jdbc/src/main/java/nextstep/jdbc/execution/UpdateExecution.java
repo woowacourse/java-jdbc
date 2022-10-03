0b7bb1d5adf1d9692dbdf0054cfce72e19149d3f
cjlee38 <cjlee38@github.com>
2022-10-03 19:03:57 +0900

    refactor: change to return int instead of void

--- jdbc/src/main/java/nextstep/jdbc/execution/UpdateExecution.java
+++ jdbc/src/main/java/nextstep/jdbc/execution/UpdateExecution.java
@@ -4,16 +4,15 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import nextstep.jdbc.execution.support.ArgumentsSetter;
 
-public class UpdateExecution extends AbstractExecution<Void> {
+public class UpdateExecution extends AbstractExecution<Integer> {
 
     public UpdateExecution(String sql, Object[] arguments) {
         super(sql, arguments);
     }
 
     @Override
-    public Void execute(PreparedStatement statement) throws SQLException {
+    public Integer execute(PreparedStatement statement) throws SQLException {
         ArgumentsSetter.setArguments(statement, arguments);
-        statement.executeUpdate();
-        return null;
+        return statement.executeUpdate();
     }
 }
