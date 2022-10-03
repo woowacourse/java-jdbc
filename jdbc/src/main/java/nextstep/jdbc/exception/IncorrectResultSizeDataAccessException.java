7731d61e9ec55c3be07eee689351556434ff0f3d
yeongunheo <gjduddns@gmail.com>
2022-10-03 19:26:13 +0900

    refactor: IncorrectResultSizeDataAccessException이 DataAccessException을 상속받도록 변경

--- jdbc/src/main/java/nextstep/jdbc/exception/IncorrectResultSizeDataAccessException.java
+++ jdbc/src/main/java/nextstep/jdbc/exception/IncorrectResultSizeDataAccessException.java
@@ -1,6 +1,8 @@
 package nextstep.jdbc.exception;
 
-public class IncorrectResultSizeDataAccessException extends RuntimeException {
+import nextstep.jdbc.DataAccessException;
+
+public class IncorrectResultSizeDataAccessException extends DataAccessException {
 
     private static final String ERROR_MESSAGE = "결과값은 오직 1개이어야만 합니다.";
 
