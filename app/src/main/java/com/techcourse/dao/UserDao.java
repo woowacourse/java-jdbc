6d228f14aad5962f2e61d3305041bb826573e86f
hyeonbinsa <shb1833@gmail.com>
2022-10-03 15:36:58 +0900

    feat: JdbcTemplate query() 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import nextstep.jdbc.JdbcTemplate;
 import org.slf4j.Logger;
@@ -34,52 +33,10 @@ public class UserDao {
     public List<User> findAll() {
         final var sql = "select * from users";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet resultSet = null;
-        List<User> users = new ArrayList<>();
-
-        try {
-            conn = jdbcTemplate.getDataSource().getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            log.debug("query : {}", sql);
-
-            resultSet = pstmt.executeQuery();
-
-            while (resultSet.next()) {
-                long id = resultSet.getLong("id");
-                String account = resultSet.getString("account");
-                String password = resultSet.getString("password");
-                String email = resultSet.getString("email");
-                users.add(new User(id, account, password, email));
-            }
-            return users;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
+        return jdbcTemplate.query(sql, (rs, rowNum) -> new User(rs.getLong("id"),
+                rs.getString("account"),
+                rs.getString("password"),
+                rs.getString("email")));
     }
 
     public User findById(final Long id) {
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -2,10 +2,14 @@ package nextstep.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.RowMapper;
 
 public class JdbcTemplate {
 
@@ -25,6 +29,27 @@ public class JdbcTemplate {
                 pstmt.setObject(index++, obj);
             }
             return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            int index = 1;
+            for (Object obj : args) {
+                pstmt.setObject(index++, obj);
+            }
+            ResultSet resultSet = pstmt.executeQuery();
+
+            List<T> result = new ArrayList<>();
+            int rowNum = 0;
+            while (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet, rowNum++));
+            }
+            return result;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
