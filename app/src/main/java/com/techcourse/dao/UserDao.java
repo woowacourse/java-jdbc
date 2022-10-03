835290b5da27091530c281b9c31d13aee46b1f0d
asebn1 <asebn121@gmail.com>
2022-10-03 16:08:13 +0900

    refactor: 사용하지 않는 변수 제거

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -3,14 +3,11 @@ package com.techcourse.dao;
 import com.techcourse.domain.User;
 import nextstep.jdbc.JdbcTemplate;
 import nextstep.jdbc.RowMapper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
     private static final RowMapper<User> rowMapper = (resultSet) -> new User(
             resultSet.getLong("id"),
             resultSet.getString("account"),
