aff21c5d955f7f5e6be5ccf30bcaf9853303b563
hyeonbinsa <shb1833@gmail.com>
2022-10-03 17:37:28 +0900

    refactor: PreparedStatement Data 삽입 메서드 분리

--- README.md
+++ README.md
@@ -3,14 +3,16 @@
 ## 1단계 JDBC 라이브러 구현하기
 
 ### 요구 사항
-- [ ] SQL 쿼리 작성, 쿼리 인자 등에 집중할 수 있도록 라이브러리 구현
+
+- [x] SQL 쿼리 작성, 쿼리 인자 등에 집중할 수 있도록 라이브러리 구현
 
 ### 체크리스트
+
 - [x] UserDaoTest 통과
-  - 테스트 목록
-    - [x] findAll()
-    - [x] findById()
-    - [X] findByAccount()
-    - [x] insert()
-    - [x] update()
-- [ ] UserDAo가 아닌 JdbcTemplate 클래스에서 JDBC 관련 처리 
+    - 테스트 목록
+        - [x] findAll()
+        - [x] findById()
+        - [X] findByAccount()
+        - [x] insert()
+        - [x] update()
+- [x] UserDAo가 아닌 JdbcTemplate 클래스에서 JDBC 관련 처리 
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
@@ -26,10 +25,7 @@ public class JdbcTemplate {
     public int update(String sql, Object... args) {
         try (final Connection conn = dataSource.getConnection();
              final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            int index = 1;
-            for (Object obj : args) {
-                pstmt.setObject(index++, obj);
-            }
+            setPreparedStatementData(pstmt, args);
             return pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -40,17 +36,10 @@ public class JdbcTemplate {
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
         try (final Connection conn = dataSource.getConnection();
              final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            int index = 1;
-            for (Object obj : args) {
-                pstmt.setObject(index++, obj);
-            }
+            setPreparedStatementData(pstmt, args);
             ResultSet resultSet = pstmt.executeQuery();
-
-            List<T> result = new ArrayList<>();
-            int rowNum = 0;
-            while (resultSet.next()) {
-                result.add(rowMapper.mapRow(resultSet, rowNum++));
-            }
+            RowMapperResultSetExtractor rowMapperResultSetExtractor = new RowMapperResultSetExtractor(rowMapper);
+            List<T> result = rowMapperResultSetExtractor.extractData(resultSet);
             return result;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -61,22 +50,21 @@ public class JdbcTemplate {
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
         try (final Connection conn = dataSource.getConnection();
              final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            int index = 1;
-            for (Object obj : args) {
-                pstmt.setObject(index++, obj);
-            }
+            setPreparedStatementData(pstmt, args);
             ResultSet resultSet = pstmt.executeQuery();
-
-            List<T> result = new ArrayList<>();
-            resultSet.getFetchSize();
-            int rowNum = 0;
-            while (resultSet.next()) {
-                result.add(rowMapper.mapRow(resultSet, rowNum++));
-            }
+            RowMapperResultSetExtractor rowMapperResultSetExtractor = new RowMapperResultSetExtractor(rowMapper);
+            List<T> result = rowMapperResultSetExtractor.extractData(resultSet);
             return getSingleResult(result);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
+        }
+    }
+
+    private void setPreparedStatementData(PreparedStatement pstmt, Object[] args) throws SQLException {
+        int index = 1;
+        for (Object obj : args) {
+            pstmt.setObject(index++, obj);
         }
     }
 
@@ -88,9 +76,5 @@ public class JdbcTemplate {
             throw new IncorrectResultSizeDataAccessException("쿼리 실행 결과의 개수가 초과되었습니다.", 1);
         }
         return result.get(0);
-    }
-
-    public DataSource getDataSource() {
-        return dataSource;
     }
 }
