2b83766ea2d11ffbd7a6c2aa0c3a7a00cd6c467b
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-10-03 21:58:35 +0900

    feat: JdbcTemplate update 메서드 구현

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,11 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -14,4 +16,28 @@ public class JdbcTemplate {
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public void update(final String sql, final Object... args) {
+        execute(sql, pstmt -> setParameters(pstmt, args).executeUpdate());
+    }
+
+    private <T> T execute(String sql, PreparedStatementCallback<T> preparedStatementCallback) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            return preparedStatementCallback.doPreparedStatement(preparedStatement);
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private PreparedStatement setParameters(final PreparedStatement preparedStatement, final Object... args)
+            throws SQLException {
+
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject(i + 1, args[i]);
+        }
+        return preparedStatement;
+    }
+
 }
