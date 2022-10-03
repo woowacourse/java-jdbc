edc5a44ce9c1798c7479b3305855f975b7f7f97b
정진혁 <wilgur513@naver.com>
2022-10-03 23:23:31 +0900

    refactor: PreparedStatement 생성 방식 수정

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.function.BiFunction;
 import java.util.function.Function;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,29 +24,10 @@ public class JdbcTemplate {
     }
 
     public void execute(final String sql, final Object... params) {
-        execute(sql, preparedStatementByParams(params));
-    }
-
-    private Consumer<PreparedStatement> preparedStatementByParams(final Object[] params) {
-        return pstmt -> {
-            int index = 1;
-            for (Object param : params) {
-                try {
-                    pstmt.setObject(index++, param);
-                } catch (SQLException e) {
-                    log.error(e.getMessage(), e);
-                    throw new RuntimeException();
-                }
-            }
-        };
-    }
-
-    public void execute(final String sql, final Consumer<PreparedStatement> consumer) {
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)
+             PreparedStatement pstmt = preparedStatementCreator(params).apply(conn, sql)
         ) {
             log.debug("query : {}", sql);
-            consumer.accept(pstmt);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -55,19 +36,14 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> queryForList(final String sql, final Function<ResultSet, T> rowMapper, final Object... params) {
-        return queryForList(sql, preparedStatementByParams(params), rowMapper);
-    }
-
-    public <T> List<T> queryForList(final String sql, final Consumer<PreparedStatement> consumer, final Function<ResultSet, T> rowMapper) {
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)
+             PreparedStatement pstmt = preparedStatementCreator(params).apply(conn, sql)
         ) {
             log.debug("query : {}", sql);
-            consumer.accept(pstmt);
 
             try (ResultSet rs = pstmt.executeQuery()) {
                 List<T> result = new ArrayList<>();
-                while(rs.next()) {
+                while (rs.next()) {
                     result.add(rowMapper.apply(rs));
                 }
                 return result;
@@ -79,15 +55,10 @@ public class JdbcTemplate {
     }
 
     public <T> T queryForObject(final String sql, final Function<ResultSet, T> rowMapper, final Object... params) {
-        return queryForObject(sql, preparedStatementByParams(params), rowMapper);
-    }
-
-    public <T> T queryForObject(final String sql, final Consumer<PreparedStatement> consumer, final Function<ResultSet, T> rowMapper) {
         try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)
+             PreparedStatement pstmt = preparedStatementCreator(params).apply(conn, sql)
         ) {
             log.debug("query : {}", sql);
-            consumer.accept(pstmt);
 
             try (ResultSet rs = pstmt.executeQuery()) {
                 if (rs.next()) {
@@ -99,5 +70,21 @@ public class JdbcTemplate {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
+    }
+
+    private BiFunction<Connection, String, PreparedStatement> preparedStatementCreator(final Object... params) {
+        return (conn, sql) -> {
+            try {
+                final PreparedStatement pstmt = conn.prepareStatement(sql);
+                int index = 1;
+                for (Object param : params) {
+                    pstmt.setObject(index++, param);
+                }
+                return pstmt;
+            } catch (SQLException e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException();
+            }
+        };
     }
 }
