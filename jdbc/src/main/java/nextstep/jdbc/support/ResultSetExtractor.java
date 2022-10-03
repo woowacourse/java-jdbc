7b94f0e3fef592baf24ea3bab7c5275428945cb9
corinne <yoo77hyeon@gmail.com>
2022-10-03 16:43:10 +0900

    refactor: FuntionalInterface 적용

--- jdbc/src/main/java/nextstep/jdbc/support/ResultSetExtractor.java
+++ jdbc/src/main/java/nextstep/jdbc/support/ResultSetExtractor.java
@@ -3,6 +3,7 @@ package nextstep.jdbc.support;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface ResultSetExtractor<T> {
 
     T extract(ResultSet resultSet) throws SQLException;
