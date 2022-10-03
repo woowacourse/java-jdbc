ee533d4b3ac53ee2ea7dc1b8ed25edfaa7fc6f29
RIANAEH <ririhan217@gmail.com>
2022-10-03 14:34:17 +0900

    refactor(jdbc): try-with-resources로 ResultSet 관리하도록 변경

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -6,6 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
+import nextstep.jdbc.exception.DataAccessException;
+import nextstep.jdbc.resultset.ResultSetExecutor;
+import nextstep.jdbc.resultset.RowMapper;
+import nextstep.jdbc.resultset.RowMapperResultSetExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,15 +24,15 @@ public class JdbcTemplate {
     }
 
     public void update(final String sql, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
 
             log.debug("query : {}", sql);
 
             for (int i = 0; i < args.length; i++) {
-                pstmt.setObject(i + 1, args[i]);
+                statement.setObject(i + 1, args[i]);
             }
-            pstmt.executeUpdate();
+            statement.executeUpdate();
         } catch (final SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException("Failed to execute a sql.", e);
@@ -44,28 +48,28 @@ public class JdbcTemplate {
     }
 
     private <T> T query(final String sql, final ResultSetExecutor<T> executor, final Object... args) {
-        ResultSet rs = null;
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
 
             log.debug("query : {}", sql);
 
             for (int i = 0; i < args.length; i++) {
-                pstmt.setObject(i + 1, args[i]);
+                statement.setObject(i + 1, args[i]);
             }
 
-            rs = pstmt.executeQuery();
-            return executor.extractData(rs);
+            return extractData(statement, executor);
         } catch (final SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException("Failed to execute a sql.", e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (final SQLException ignored) {
-            }
+        }
+    }
+
+    private <T> T extractData(final PreparedStatement statement, final ResultSetExecutor<T> executor) {
+        try (final ResultSet resultSet = statement.executeQuery()) {
+            return executor.extractData(resultSet);
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException("Failed to execute a sql.", e);
         }
     }
 }
