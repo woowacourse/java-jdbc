cbe0734cc42417072a9b868770ae1c75f8f31232
syoun602 <syoun602@gmail.com>
2022-10-03 23:31:55 +0900

    refactor: try with resources로 수정

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -21,51 +21,30 @@ public class JdbcTemplate {
     }
 
     public int update(final String sql, Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             log.debug("query : {}", sql);
 
-            setParameters(pstmt, args);
-            return pstmt.executeUpdate();
+            setParameters(preparedStatement, args);
+            return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            setParameters(pstmt, args);
-            rs = pstmt.executeQuery();
+        ResultSet resultSet = null;
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            setParameters(preparedStatement, args);
+            resultSet = preparedStatement.executeQuery();
 
             log.debug("query : {}", sql);
 
             List<T> values = new ArrayList<>();
-            while (rs.next()) {
-                values.add(rowMapper.mapRow(rs, rs.getRow()));
+            while (resultSet.next()) {
+                values.add(rowMapper.mapRow(resultSet, resultSet.getRow()));
             }
             return values;
         } catch (SQLException e) {
@@ -73,22 +52,8 @@ public class JdbcTemplate {
             throw new RuntimeException(e);
         } finally {
             try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
+                if (resultSet != null) {
+                    resultSet.close();
                 }
             } catch (SQLException ignored) {
             }
@@ -98,7 +63,7 @@ public class JdbcTemplate {
     private void setParameters(final PreparedStatement pstmt, final Object... args) {
         for (int i = 0; i < args.length; i++) {
             try {
-                pstmt.setObject(i + 1,  args[i]);
+                pstmt.setObject(i + 1, args[i]);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
