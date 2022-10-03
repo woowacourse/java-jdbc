1320fd4ee664835a6a3854461fc68afcfafc2821
nbalance97 <nbalance__@kyonggi.ac.kr>
2022-10-03 23:44:55 +0900

    refactor: 불필요한 공백 제거

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -33,11 +32,15 @@ public class JdbcTemplate {
 
     public <T> T queryForObject(final String sql, RowMapper<T> rowMapper, Object... params) {
         return doExecute(sql, preparedStatement -> {
-            setParamsToStatement(preparedStatement, params);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
+            try {
+                setParamsToStatement(preparedStatement, params);
+                ResultSet resultSet = preparedStatement.executeQuery();
+                resultSet.next();
 
-            return rowMapper.mapRow(resultSet, ONE_OBJECT_ROW_NUM);
+                return rowMapper.mapRow(resultSet, ONE_OBJECT_ROW_NUM);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         });
     }
 
@@ -47,15 +50,15 @@ public class JdbcTemplate {
             setParamsToStatement(preparedStatement, params);
             final ResultSet resultSet = preparedStatement.executeQuery();
             final List<T> objects = new ArrayList<>();
-            int rowNum = 1;
-            mappingRowToObjects(rowMapper, resultSet, objects, rowNum);
+            mappingRowToObjects(rowMapper, resultSet, objects);
 
             return objects;
         });
     }
 
-    private <T> void mappingRowToObjects(RowMapper<T> rowMapper, ResultSet resultSet, List<T> objects, int rowNum)
+    private <T> void mappingRowToObjects(RowMapper<T> rowMapper, ResultSet resultSet, List<T> objects)
             throws SQLException {
+        int rowNum = 1;
         while (resultSet.next()) {
             objects.add(rowMapper.mapRow(resultSet, rowNum++));
         }
--- jdbc/src/main/java/nextstep/jdbc/PreparedStatementCallback.java
+++ jdbc/src/main/java/nextstep/jdbc/PreparedStatementCallback.java
@@ -5,5 +5,5 @@ import java.sql.SQLException;
 
 @FunctionalInterface
 public interface PreparedStatementCallback<T> {
-    T doInPreparedStatement( PreparedStatement ps) throws SQLException, DataAccessException;
+    T doInPreparedStatement(PreparedStatement ps) throws SQLException, DataAccessException;
 }
