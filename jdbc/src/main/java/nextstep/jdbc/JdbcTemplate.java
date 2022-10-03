e52077ce82936ca43f7d5f56a9612e50a61e0f26
devHudi <devhudi@gmail.com>
2022-10-04 01:39:45 +0900

    refactor: 람다를 사용한 템플릿 메소드 패턴을 사용해 중복 코드 제거

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,43 +22,53 @@ public class JdbcTemplate {
     }
 
     public int update(final String sql, final Object... args) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
-            for (int i = 0; i < args.length; i++) {
-                preparedStatement.setObject(i + 1, args[i]);
+        return usePreparedStatement(sql, pstmt -> {
+            try {
+                return pstmt.executeUpdate(sql);
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage(), e);
+                // TODO: 더 추상화된 예외 메시지를 사용해야함
             }
-
-            return preparedStatement.executeUpdate();
-        } catch (final SQLException e) {
-            throw new DataAccessException(e.getMessage(), e);
-            // TODO: 더 추상화된 예외 메시지를 사용해야함
-        }
+        }, args);
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+        return usePreparedStatement(sql, pstmt -> {
+            try {
+                List<T> results = new ArrayList<>();
+                ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    T result = rowMapper.run(rs);
+                    results.add(result);
+                }
 
-            for (int i = 0; i < args.length; i++) {
-                preparedStatement.setObject(i + 1, args[i]);
+                return results;
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage(), e);
+                // TODO: 더 추상화된 예외 메시지를 사용해야함
             }
-
-            List<T> results = new ArrayList<>();
-            ResultSet rs = preparedStatement.executeQuery();
-            if (rs.next()) {
-                T result = rowMapper.run(rs);
-                results.add(result);
-            }
-
-            return results;
-        } catch (final SQLException e) {
-            throw new DataAccessException(e.getMessage(), e);
-            // TODO: 더 추상화된 예외 메시지를 사용해야함
-        }
+        }, args);
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        return usePreparedStatement(sql, pstmt -> {
+            try {
+                T result = null;
+                ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    result = rowMapper.run(rs);
+                }
+
+                return result;
+            } catch (SQLException e) {
+                throw new DataAccessException(e.getMessage(), e);
+                // TODO: 더 추상화된 예외 메시지를 사용해야함
+            }
+        }, args);
+    }
+
+    public <T> T usePreparedStatement(final String sql, final Function<PreparedStatement, T> function,
+                                      final Object... args) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
 
@@ -65,13 +76,7 @@ public class JdbcTemplate {
                 preparedStatement.setObject(i + 1, args[i]);
             }
 
-            T result = null;
-            ResultSet rs = preparedStatement.executeQuery();
-            if (rs.next()) {
-                result = rowMapper.run(rs);
-            }
-
-            return result;
+            return function.apply(preparedStatement);
         } catch (final SQLException e) {
             throw new DataAccessException(e.getMessage(), e);
             // TODO: 더 추상화된 예외 메시지를 사용해야함
