d952a6a9547de165e66acc3bd6dbbc683110c756
정진혁 <wilgur513@naver.com>
2022-10-04 10:27:43 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 베루스(정진혁) 미션 제출합니다. (#107)
    * feat: UserDao 구현
    
    * refactor: 쿼리 실행 관련 메서드 JdbcTemplate으로 이동
    
    * refactor: JdbcTemplate 파라미터를 받는 방식으로 인터페이스 변경
    
    * refactor: PreparedStatement 생성 방식 수정
    
    * refactor: UserDao에서 사용하지 않는 DataSource 필드 제거
    
    * refactor: RowMapper 인터페이스 추출
    
    * refactor: PreparedStatementCreator 인터페이스 추출
    
    * refactor: JdbcTemplate 메서드 들여쓰기 수준 정리
    
    * refactor: DataAccessException 사용

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,72 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void execute(final String sql, final Object... params) {
+        final PreparedStatementCreator creator = preparedStatementCreator(params);
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = creator.create(conn, sql)
+        ) {
+            log.debug("query : {}", sql);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> queryForList(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        final PreparedStatementCreator creator = preparedStatementCreator(params);
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = creator.create(conn, sql);
+             ResultSet rs = pstmt.executeQuery()
+        ) {
+            log.debug("query : {}", sql);
+            return mapToList(rowMapper, rs);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> List<T> mapToList(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
+        List<T> result = new ArrayList<>();
+        while (rs.next()) {
+            result.add(rowMapper.map(rs));
+        }
+        return result;
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        final PreparedStatementCreator creator = preparedStatementCreator(params);
+
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = creator.create(conn, sql);
+             ResultSet rs = pstmt.executeQuery()
+        ) {
+            log.debug("query : {}", sql);
+            return mapToObject(rowMapper, rs);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> T mapToObject(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
+        return rs.next() ? rowMapper.map(rs) : null;
+    }
+
+    private PreparedStatementCreator preparedStatementCreator(final Object... params) {
+        return (conn, sql) -> {
+            final PreparedStatement pstmt = conn.prepareStatement(sql);
+            int index = 1;
+            for (Object param : params) {
+                pstmt.setObject(index++, param);
+            }
+            return pstmt;
+        };
     }
 }
