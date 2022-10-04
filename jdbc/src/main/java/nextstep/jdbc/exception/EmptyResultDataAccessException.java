092a5bec31e7b47ca8b394f880ede7a4665e2676
Hyunta <mohai2618@gmail.com>
2022-10-04 19:31:07 +0900

    refactor: 불필요한 필드값 삭제

--- jdbc/src/main/java/nextstep/jdbc/exception/EmptyResultDataAccessException.java
+++ jdbc/src/main/java/nextstep/jdbc/exception/EmptyResultDataAccessException.java
@@ -1,7 +1,6 @@
 package nextstep.jdbc.exception;
 
 public class EmptyResultDataAccessException extends IncorrectResultSizeDataAccessException {
-    
 
     public EmptyResultDataAccessException(int expectedSize) {
         super(expectedSize, 0);
--- jdbc/src/main/java/nextstep/jdbc/exception/IncorrectResultSizeDataAccessException.java
+++ jdbc/src/main/java/nextstep/jdbc/exception/IncorrectResultSizeDataAccessException.java
@@ -2,13 +2,7 @@ package nextstep.jdbc.exception;
 
 public class IncorrectResultSizeDataAccessException extends DataAccessException {
 
-    private final int expectedSize;
-
-    private final int actualSize;
-
     public IncorrectResultSizeDataAccessException(int expectedSize, int actualSize) {
         super("Incorrect result size: expected " + expectedSize + ", actual " + actualSize);
-        this.expectedSize = expectedSize;
-        this.actualSize = actualSize;
     }
 }
