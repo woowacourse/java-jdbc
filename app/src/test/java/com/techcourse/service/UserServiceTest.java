4ba81ac7780fd3bc92a8f3cd72d39840373b2b3f
cjlee38 <cjlee38@github.com>
2022-10-04 19:55:35 +0900

    feat: implement manual proxy-style transactional service

--- app/src/test/java/com/techcourse/service/UserServiceTest.java
+++ app/src/test/java/com/techcourse/service/UserServiceTest.java
@@ -14,6 +14,7 @@ import nextstep.jdbc.transaction.RollbackException;
 import nextstep.jdbc.transaction.TransactionalProcessor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 class UserServiceTest {
 
@@ -62,6 +63,27 @@ class UserServiceTest {
     }
 
     @Test
+    void testTransactionRollbackWithWrapping() {
+        // 트랜잭션 롤백 테스트를 위해 mock으로 교체
+        final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
+        // 애플리케이션 서비스
+        final var appUserService = new AppUserService(userDao, userHistoryDao);
+        // 트랜잭션 서비스 추상화
+        final var transactionManager = new DataSourceTransactionManager(DataSourceConfig.getInstance());
+        final var userService = new TxWrappingUserService(transactionManager, appUserService);
+
+        final var newPassword = "newPassword";
+        final var createBy = "gugu";
+        // 트랜잭션이 정상 동작하는지 확인하기 위해 의도적으로 MockUserHistoryDao에서 예외를 발생시킨다.
+        assertThrows(DataAccessException.class,
+                () -> userService.changePassword(1L, newPassword, createBy));
+
+        final var actual = userService.findById(1L);
+
+        assertThat(actual.getPassword()).isNotEqualTo(newPassword);
+    }
+
+    @Test
     void testTransactional() {
         TransactionalProcessor processor = new TransactionalProcessor(DataSourceConfig.getInstance());
         final UserHistoryDao userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
@@ -77,4 +99,5 @@ class UserServiceTest {
 
         assertThat(actual.getPassword()).isNotEqualTo(newPassword);
     }
+
 }
