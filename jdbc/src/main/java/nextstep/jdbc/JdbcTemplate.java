4b29ccde733105acb73c4ab36bc3761a5585a534
Richard JEON <ztzy1907@gmail.com>
2022-10-04 15:14:40 +0900

    docs: JdbcTemplate 주요 메서드에 주석 추가

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -26,14 +26,42 @@ public class JdbcTemplate {
         return dataSource.getConnection();
     }
 
+    /**
+     * Create, Update, Delete 시 사용할 수 있는 메서드입니다.
+     *
+     * @param sql    SQL문을 String 형태로 받습니다. 매개변수 위치에는 ?를 표기해주세요.
+     * @param params 가변인자로 SQL에 적용될 매개변수를 받습니다. SQL문 내에 ? 표기된 곳에 순서대로 주입합니다.
+     * @return 영향 받은 행의 수를 반환합니다.
+     */
     public int command(final String sql, final Object... params) {
         return execute(sql, null, ((rowMapper, pstmt) -> pstmt.executeUpdate()), params);
     }
 
+    /**
+     * 여러 건을 Select 시 사용할 수 있는 메서드입니다.
+     *
+     * @param sql       SQL문을 String 형태로 받습니다. 매개변수 위치에는 ?를 표기해주세요.
+     * @param rowMapper 하나의 행을 매핑하여 하나의 결과를 반환하는 rowMapper를 구현해서 전달해주세요.
+     * @param params    가변인자로 SQL에 적용될 매개변수를 받습니다. SQL문 내에 ? 표기된 곳에 순서대로 주입합니다.
+     * @param <T>       RowMapper가 하나의 행을 매핑하여 반환할 타입 파라미터 입니다.
+     * @return 조회 결과를 List<T> 로 반환합니다. 결과가 없을 경우 빈 List를 반환합니다.
+     * @see nextstep.jdbc.RowMapper
+     */
     public <T> List<T> queryForList(final String sql, final RowMapper<T> rowMapper, final Object... params) {
         return execute(sql, rowMapper, (this::mapRows), params);
     }
 
+    /**
+     * 단 건을 Select 시 사용할 수 있는 메서드입니다.
+     *
+     * @param sql       SQL문을 String 형태로 받습니다. 매개변수 위치에는 ?를 표기해주세요.
+     * @param rowMapper 하나의 행을 매핑하여 하나의 결과를 반환하는 rowMapper를 구현해서 전달해주세요.
+     * @param params    가변인자로 SQL에 적용될 매개변수를 받습니다. SQL문 내에 ? 표기된 곳에 순서대로 주입합니다.
+     * @param <T>       RowMapper가 하나의 행을 매핑하여 반환할 타입 파라미터 입니다.
+     * @return 조회 결과를 T로 반환합니다.
+     * @throws DataAccessException 조회 결과가 없거나 2건 이상인 경우 예외를 던집니다.
+     * @see nextstep.jdbc.RowMapper
+     */
     public <T> T queryForOne(final String sql, final RowMapper<T> rowMapper, final Object... params) {
         final var results = queryForList(sql, rowMapper, params);
 
