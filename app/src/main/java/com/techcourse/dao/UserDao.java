f25f01a767b079964dcd41ec0302ec9ff618498a
S. J. Kang <83529599+sojukang@users.noreply.github.com>
2022-10-04 09:51:51 +0900

    feat: UserDao 기능 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -30,29 +30,34 @@ public class UserDao {
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.insert(sql, user.getAccount(), user.getPassword(), user.getEmail());
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
-        // todo
+        final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
+        final var sql = "select id, account, password, email from users";
+        return jdbcTemplate.select(sql, USER_MAPPER);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        User user = jdbcTemplate.select(sql, USER_MAPPER, id);
-        if (user == null) {
+        final List<User> users = jdbcTemplate.select(sql, USER_MAPPER, id);
+        if (users.isEmpty()) {
             throw new IllegalArgumentException("user not found");
         }
-        return user;
+        return users.get(0);
     }
 
     public User findByAccount(final String account) {
-        // todo
-        return null;
+        final var sql = "select id, account, password, email from users where account = ?";
+        final List<User> users = jdbcTemplate.select(sql, USER_MAPPER, account);
+        if (users.isEmpty()) {
+            throw new IllegalArgumentException("user not found");
+        }
+        return users.get(0);
     }
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
@@ -21,16 +23,13 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void insert(final String sql, final Object... parameters) {
+    public void update(final String sql, final Object... parameters) {
         try (
             final Connection conn = dataSource.getConnection();
             final PreparedStatement pstmt = conn.prepareStatement(sql)
         ) {
             log.debug("query : {}", sql);
-
-            for (int i = 0; i < parameters.length; i++) {
-                pstmt.setObject(i + 1, parameters[i]);
-            }
+            setPreparedStatement(parameters, pstmt);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -38,24 +37,30 @@ public class JdbcTemplate {
         }
     }
 
-    public <T> T select(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
+    public <T> List<T> select(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
         try (
             final Connection conn = dataSource.getConnection();
             final PreparedStatement pstmt = conn.prepareStatement(sql)
         ) {
             log.debug("query : {}", sql);
 
-            for (int i = 0; i < parameters.length; i++) {
-                pstmt.setObject(i + 1, parameters[i]);
-            }
+            setPreparedStatement(parameters, pstmt);
             ResultSet resultSet = pstmt.executeQuery();
-            if (resultSet.next()) {
-                return rowMapper.mapRow(resultSet, resultSet.getRow());
+
+            List<T> mappedTypes = new ArrayList<>();
+            while (resultSet.next()) {
+                mappedTypes.add(rowMapper.mapRow(resultSet, resultSet.getRow()));
             }
-            throw new DataAccessException("empty result exception");
+            return mappedTypes;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
+        }
+    }
+
+    private void setPreparedStatement(final Object[] parameters, final PreparedStatement pstmt) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            pstmt.setObject(i + 1, parameters[i]);
         }
     }
 }
