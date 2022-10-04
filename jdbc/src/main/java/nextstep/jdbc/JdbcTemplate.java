897c5c34c4caebffe66b82d2949d07f38164ea2b
써머(최혜원) <80666066+hyewoncc@users.noreply.github.com>
2022-10-04 20:48:28 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 써머(최혜원) 미션 제출합니다 (#128)
    * feat: UserDaoTest가 통과하도록 UserDao 구현
    
    * refactor: try with resources로 개선
    
    * refactor: 메서드 간 중복되는 부분 추출
    
    * refactor: insert, update 문을 JdbcTemplate으로 이동
    
    * refactor: findById, findByAccount문을 JdbcTemplate으로 이동
    
    * refactor: findAll을 JdbcTemplate으로 이동 후, dataSource 제거
    
    * refactor: 불필요한 instanceof 제거
    
    * refactor: RuntimeException을 DataAccessException으로 변경
    
    * refactor: sql 매개변수 개수 확인과 에외 변환 테스트

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,70 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(final String sql, final ObjectMapper<T> objectMapper) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql);
+             ResultSet resultSet = statement.executeQuery()) {
+
+            log.debug("query : {}", sql);
+
+            List<T> results = new ArrayList<>();
+            if (resultSet.next()) {
+                T t = objectMapper.map(resultSet);
+                results.add(t);
+            }
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    public <T> T query(final String sql, final Object parameter, final ObjectMapper<T> objectMapper) {
+        validateSql(sql, parameter);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setObject(1, parameter);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                log.debug("query : {}", sql);
+
+                if (resultSet.next()) {
+                    return objectMapper.map(resultSet);
+                }
+                throw new DataAccessException("조회 결과가 존재하지 않습니다.");
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    public void update(final String sql, final Object... parameters) {
+        validateSql(sql, parameters);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            for (int i = 0; i < parameters.length; i++) {
+                statement.setObject(i + 1, parameters[i]);
+            }
+
+            statement.executeUpdate();
+            log.debug("query : {}", sql);
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    private void validateSql(final String sql, final Object... parameters) {
+        int required = sql.length() - sql.replaceAll("\\?", "").length();
+        if (required != parameters.length) {
+            throw new DataAccessException("sql문 내의 매개변수와 주어진 매개변수의 수가 다릅니다.");
+        }
     }
 }
