6baa91dd9c0b9fd7c0ee9bd30a61fd707a6e07f1
RIANAEH <ririhan217@gmail.com>
2022-10-05 02:07:15 +0900

    test(jdbc): Mockito의 when 대신 BDDMockito의 given을 사용하도록 변경 및 중복 제거

--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -4,9 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.verify;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -15,6 +15,7 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 import nextstep.jdbc.exception.DataAccessException;
 import nextstep.jdbc.resultset.RowMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -35,15 +36,18 @@ class JdbcTemplateTest {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
+    @BeforeEach
+    void setUp() throws SQLException {
+        given(dataSource.getConnection()).willReturn(connection);
+        given(connection.prepareStatement(anyString())).willReturn(statement);
+    }
+
     @DisplayName("update query 수행을 확인한다.")
     @Test
     void update() throws SQLException {
         // given
         final var sql = "";
         final var args = new Object[]{"arg1", 2};
-
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
 
         // when
         jdbcTemplate.update(sql, args);
@@ -65,10 +69,8 @@ class JdbcTemplateTest {
         final var sql = "";
         final RowMapper<String> rowMapper = (rs) -> "result object";
 
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
-        when(statement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true).thenReturn(true).thenReturn(false);
+        given(statement.executeQuery()).willReturn(resultSet);
+        given(resultSet.next()).willReturn(true).willReturn(true).willReturn(false);
 
         // when
         final var actual = jdbcTemplate.query(sql, rowMapper);
@@ -90,10 +92,8 @@ class JdbcTemplateTest {
         final RowMapper<String> rowMapper = (rs) -> "result object";
         final var args = new Object[]{"arg1", 2};
 
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
-        when(statement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true).thenReturn(false);
+        given(statement.executeQuery()).willReturn(resultSet);
+        given(resultSet.next()).willReturn(true).willReturn(false);
 
         // when
         final var actual = jdbcTemplate.queryForObject(sql, rowMapper, args);
@@ -115,10 +115,8 @@ class JdbcTemplateTest {
         final RowMapper<String> rowMapper = (rs) -> "result object";
         final var args = new Object[]{"arg1", 2};
 
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
-        when(statement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(false);
+        given(statement.executeQuery()).willReturn(resultSet);
+        given(resultSet.next()).willReturn(false);
 
         // when & then
         assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, rowMapper, args))
@@ -134,10 +132,8 @@ class JdbcTemplateTest {
         final RowMapper<String> rowMapper = (rs) -> "result object";
         final var args = new Object[]{"arg1", 2};
 
-        when(dataSource.getConnection()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenReturn(statement);
-        when(statement.executeQuery()).thenReturn(resultSet);
-        when(resultSet.next()).thenReturn(true).thenReturn(true).thenReturn(false);
+        given(statement.executeQuery()).willReturn(resultSet);
+        given(resultSet.next()).willReturn(true).willReturn(true).willReturn(false);
 
         // when & then
         assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, rowMapper, args))
