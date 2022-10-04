09cd33debe94feba0efefa516db5b919cf54b8e3
Hyunta <mohai2618@gmail.com>
2022-10-04 19:34:39 +0900

    feat: 유틸 클래스의 생성자 제한

--- jdbc/src/main/java/nextstep/jdbc/support/Assert.java
+++ jdbc/src/main/java/nextstep/jdbc/support/Assert.java
@@ -4,6 +4,9 @@ import javax.annotation.Nullable;
 
 public class Assert {
 
+    private Assert() {
+    }
+
     public static void notNull(@Nullable Object object, String message) {
         if (object == null) {
             throw new IllegalArgumentException(message);
