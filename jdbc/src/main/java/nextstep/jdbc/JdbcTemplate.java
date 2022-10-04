879e83efa6d229bb9752c263ab1f5034a22bf617
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2022-10-05 00:16:49 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 라쿤(이병훈) 미션 제출합니다. (#65)
    * feat: JdbcTemplate Update 메서드 구현
    
    * feat: JdbcTemplate queryForObject 메서드, RowMapper 함수형 인터페이스 구현
    
    * feat: JdbcTemplate query 메서드 구현
    
    * refactor: test jdbctemplate 주입 방법 변경
    
    * refactor: UserDao JdbcTemplate 적용하여 구현
    
    * refactor: RowMapper 메서드명 변경
    
    * test: JdbcTemplateTest 추가
    
    * refactor: callback 구조로 변경
    
    * refactor: 불필요한 공백 제거
    
    * refactor: 불필요 try문 제거

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,17 +1,77 @@
 package nextstep.jdbc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int ONE_OBJECT_ROW_NUM = 1;
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, Object... params) {
+        return doExecute(sql, preparedStatement -> {
+            setParamsToStatement(preparedStatement, params);
+            return preparedStatement.executeUpdate();
+        });
+    }
+
+    public <T> T queryForObject(final String sql, RowMapper<T> rowMapper, Object... params) {
+        return doExecute(sql, preparedStatement -> {
+            setParamsToStatement(preparedStatement, params);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+
+            return rowMapper.mapRow(resultSet, ONE_OBJECT_ROW_NUM);
+        });
+    }
+
+    public <T> List<T> query(final String sql, RowMapper<T> rowMapper, Object... params) {
+
+        return doExecute(sql, preparedStatement -> {
+            setParamsToStatement(preparedStatement, params);
+            final ResultSet resultSet = preparedStatement.executeQuery();
+            final List<T> objects = new ArrayList<>();
+            mappingRowToObjects(rowMapper, resultSet, objects);
+
+            return objects;
+        });
+    }
+
+    private <T> void mappingRowToObjects(RowMapper<T> rowMapper, ResultSet resultSet, List<T> objects)
+            throws SQLException {
+        int rowNum = 1;
+        while (resultSet.next()) {
+            objects.add(rowMapper.mapRow(resultSet, rowNum++));
+        }
+    }
+
+    private void setParamsToStatement(PreparedStatement preparedStatement, Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            preparedStatement.setObject(i + 1, params[i]);
+        }
+    }
+
+    private <T> T doExecute(String sql, PreparedStatementCallback<T> callback) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            return callback.doInPreparedStatement(preparedStatement);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
