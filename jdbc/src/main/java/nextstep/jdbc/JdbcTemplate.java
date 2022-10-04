1ea218c43008da6a15a28e2efb249d063e424360
정수현 <63030569+awesomeo184@users.noreply.github.com>
2022-10-04 14:40:59 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 어썸오(정수현) 미션 제출합니다. (#52)
    * refactor: try-with-resources 적용
    
    * feat: PreparedStatementSetter 구현
    
    * feat: queryForObject 메서드 구현
    
    * refactor: queryForObject 적용
    
    * feat: queryForList 구현
    
    * refactor: UserDao에 jdbcTemplate 적용
    
    * refactor: 템플릿 콜백 패턴 적용하여 예외처리 중복 제거
    
    * refactor: 메서드 추출 및 중복제거
    
    * fix: 잘못된 검증 수정
    
    * fix: FunctionalInterface 애너테이션 추가

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,5 +1,9 @@
 package nextstep.jdbc;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +17,49 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final PreparedStatementSetter pss) {
+        return execute(sql, ps -> {
+            pss.setValues(ps);
+            return ps.executeUpdate();
+        });
+    }
+
+    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final PreparedStatementSetter pss) {
+        return execute(sql, ps -> {
+            pss.setValues(ps);
+            return getSingleOrEmpty(extractResults(sql, rowMapper, ps));
+        });
+    }
+
+    public <T> List<T> queryForList(final String sql, final RowMapper<T> rowMapper) {
+        return execute(sql, ps -> extractResults(sql, rowMapper, ps));
+    }
+
+    private <T> T execute(String sql, PreparedStatementCallback<T> action) {
+        try (var conn = dataSource.getConnection();
+             var ps = conn.prepareStatement(sql)) {
+            return action.doInPreparedStatement(ps);
+        } catch (SQLException e) {
+            log.error("Error in executing SQL statement: {}", sql, e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> List<T> extractResults(final String sql, final RowMapper<T> rowMapper, final PreparedStatement ps) {
+        try (var rs = ps.executeQuery()) {
+            return new ResultSetExtractor<T>(rowMapper).extractData(rs);
+        } catch (SQLException e) {
+            log.error("Error in executing SQL statement: {}", sql, e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> Optional<T> getSingleOrEmpty(final List<T> results) {
+        if (results.size() == 0) {
+            return Optional.empty();
+        }
+        return Optional.of(results.get(0));
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,136 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
+import nextstep.jdbc.fixture.Tester;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private Connection connection;
+    private DataSource dataSource;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+    private JdbcTemplate template;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        this.connection = mock(Connection.class);
+        this.dataSource = mock(DataSource.class);
+        this.resultSet = mock(ResultSet.class);
+        this.preparedStatement = mock(PreparedStatement.class);
+        this.template = new JdbcTemplate(this.dataSource);
+        given(this.dataSource.getConnection()).willReturn(this.connection);
+        given(this.connection.prepareStatement(anyString())).willReturn(this.preparedStatement);
+        given(this.preparedStatement.executeQuery()).willReturn(this.resultSet);
+    }
+
+    @Test
+    @DisplayName("PreparedStatementSetter에 지정된 콜백을 실행하고 자원을 닫는다.")
+    void testPreparedStatementSetterSucceeds() throws Exception {
+        // given
+        final var sql = "UPDATE member SET name=? WHERE id = 1";
+        final var name = "awesomeo";
+        var expectedRowsUpdated = 1;
+        given(this.preparedStatement.executeUpdate()).willReturn(expectedRowsUpdated);
+
+        // when
+        PreparedStatementSetter pss = ps -> ps.setString(1, name);
+
+        // then
+        int actualRowsUpdated = new JdbcTemplate(this.dataSource).update(sql, pss);
+        assertThat(expectedRowsUpdated).isEqualTo(actualRowsUpdated);
+        verify(this.preparedStatement).setString(1, name);
+        verify(this.preparedStatement).close();
+        verify(this.connection).close();
+    }
+
+    @Test
+    @DisplayName("예외 발생시 자원을 닫는다.")
+    void testPreparedStatementSetterFails() throws Exception {
+        // given
+        final var sql = "UPDATE member SET name=? WHERE id = 1";
+        final var name = "Gary";
+        var sqlException = new SQLException();
+        given(this.preparedStatement.executeUpdate()).willThrow(sqlException);
+
+        // when
+        PreparedStatementSetter pss = ps -> ps.setString(1, name);
+
+        // then
+        assertThatExceptionOfType(DataAccessException.class).isThrownBy(() ->
+                        new JdbcTemplate(this.dataSource).update(sql, pss))
+                .withCause(sqlException);
+        verify(this.preparedStatement).setString(1, name);
+        verify(this.preparedStatement).close();
+        verify(this.connection).close();
+    }
+
+    @Test
+    @DisplayName("PreparedStatementSetter와 RowMapper를 입력받아 단일 쿼리 결과 객체를 리턴한다")
+    void testQueryForObjectSucceeds() throws SQLException {
+        // given
+        final var sql = "SELECT id, name FROM member WHERE id=?";
+        var id = 1L;
+
+        given(this.resultSet.next()).willReturn(true, false);
+        given(this.resultSet.getLong(1)).willReturn(1L);
+        given(this.resultSet.getString(2)).willReturn("awesomeo");
+
+        PreparedStatementSetter pss = ps -> ps.setLong(1, id);
+        RowMapper<Tester> rowMapper = (rs, rowNum) -> new Tester(rs.getLong(1), rs.getString(2));
+
+        // when
+        final var actual = template.queryForObject(sql, rowMapper, pss).get();
+
+        // then
+        final var expected = new Tester(1L, "awesomeo");
+        assertThat(actual).isEqualTo(expected);
+
+        verify(this.preparedStatement).setLong(1, id);
+        verify(this.resultSet).close();
+        verify(this.preparedStatement).close();
+        verify(this.connection).close();
+    }
+
+    @Test
+    @DisplayName("PreparedStatementSetter와 RowMapper를 입력받아 결과 객체 리스트를 리턴한다")
+    void testQueryForListSucceeds() throws SQLException {
+        // given
+        final var sql = "SELECT id, name FROM member";
+        var id = 1L;
+
+        given(this.resultSet.next()).willReturn(true, false);
+        given(this.resultSet.getLong(1)).willReturn(1L);
+        given(this.resultSet.getString(2)).willReturn("awesomeo");
+
+        RowMapper<Tester> rowMapper = (rs, rowNum) -> new Tester(rs.getLong(1), rs.getString(2));
+
+        // when
+        final var actual = template.queryForList(sql, rowMapper);
+
+        // then
+        var expected = List.of(new Tester(1L, "awesomeo"));
+        assertThat(actual).isEqualTo(expected);
+
+        verify(this.resultSet).close();
+        verify(this.preparedStatement).close();
+        verify(this.connection).close();
+    }
 }
