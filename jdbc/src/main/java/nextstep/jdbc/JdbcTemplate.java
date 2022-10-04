53f387154890571073aff11239233596dd0c3915
Jeong Jinwoo <jwjeong96@gmail.com>
2022-10-04 14:44:24 +0900

    refactor(JdbcTemplate): cleanse multiple try-with-resources with PreparedStatementCallback

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -21,15 +21,21 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+    private <T> T execute(String sql, PreparedStatementCallback<T> statementCallback) {
         try (final var connection = getConnection();
              final var preparedStatement = connection.prepareStatement(sql)) {
-            setParams(preparedStatement, args);
-            return executeQuery(preparedStatement, rowMapper);
+            return statementCallback.execute(preparedStatement);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
         }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        return execute(sql, statement -> {
+            setParams(statement, args);
+            return executeQuery(statement, rowMapper);
+        });
     }
 
     public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
@@ -40,15 +46,11 @@ public class JdbcTemplate {
         return Optional.of(entities.get(0));
     }
 
-    public void update(String sql, Object... args) {
-        try (final var connection = getConnection();
-             final var preparedStatement = connection.prepareStatement(sql)) {
-            setParams(preparedStatement, args);
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
-        }
+    public int update(String sql, Object... args) {
+        return execute(sql, statement -> {
+            setParams(statement, args);
+            return statement.executeUpdate();
+        });
     }
 
     private void setParams(PreparedStatement preparedStatement, Object... args) throws SQLException {
