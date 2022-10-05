4c22dfa492b01c5624687add36e3b0990acaf762
kth990303 <kth990303@naver.com>
2022-10-05 22:42:13 +0900

    refactor: 메서드명 수정

--- jdbc/src/main/java/nextstep/jdbc/RowMapper.java
+++ jdbc/src/main/java/nextstep/jdbc/RowMapper.java
@@ -5,5 +5,6 @@ import java.sql.SQLException;
 
 @FunctionalInterface
 public interface RowMapper<T> {
-    T rowMap(final ResultSet resultSet) throws SQLException;
+
+    T mapRow(final ResultSet resultSet) throws SQLException;
 }
