1ed5439d81e204c7d44b43018cfb2158c784a516
dongho <dongho108@naver.com>
2022-10-05 16:27:02 +0900

    refactor: userMapper Dao 내부에서 static으로 생성해 사용하도록 수정

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -4,6 +4,7 @@ import com.techcourse.domain.User;
 import java.util.List;
 import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
+import nextstep.jdbc.RowMapper;
 
 public class UserDao {
 
@@ -34,16 +35,24 @@ public class UserDao {
 
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(sql, UserMapper.getInstance());
+        return jdbcTemplate.query(sql, userRowMapper);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, UserMapper.getInstance(), id);
+        return jdbcTemplate.queryForObject(sql, userRowMapper, id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(sql, UserMapper.getInstance(), account);
+        return jdbcTemplate.queryForObject(sql, userRowMapper, account);
     }
+
+    private static final RowMapper<User> userRowMapper = (resultSet, ignored) ->
+            new User(
+                    resultSet.getLong("id"),
+                    resultSet.getString("account"),
+                    resultSet.getString("password"),
+                    resultSet.getString("email")
+            );
 }
