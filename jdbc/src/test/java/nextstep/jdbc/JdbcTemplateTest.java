c9059afcbe2f4d10df0b26c3600a6f0d492bb749
kth990303 <kth990303@naver.com>
2022-10-05 22:42:56 +0900

    refactor: 사용하지 않는 메서드 제거

--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -84,13 +84,5 @@ class JdbcTemplateTest {
             this.id = id;
             this.account = account;
         }
-
-        public Long getId() {
-            return id;
-        }
-
-        public String getAccount() {
-            return account;
-        }
     }
 }
