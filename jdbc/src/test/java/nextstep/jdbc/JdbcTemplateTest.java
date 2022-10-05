53154abbcc94aec19de108e8483956dbd18f201b
tiki <78459713+yh20studio@users.noreply.github.com>
2022-10-05 17:12:47 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 티키(방영호) 미션 제출합니다. (#104)
    * feat: UserDao update, findAll, findByAccount 기능 구현
    
    * refactor: UserDao가 아닌 JdbcTemplate에서 JDBC 처리하도록 구현
    
    * test: JdbcTemplate test 추가
    
    * refactor: forEach문에서 for문을 사용하도록 변경

--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,119 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private RowMapper<TestUser> rowMapper() {
+        return (rs, rowNum) -> new TestUser(1L);
+    }
+
+    @Test
+    @DisplayName("여러개의 결과가 나오는 query")
+    void query() throws SQLException {
+        // given
+        final DataSource dataSource = mock(DataSource.class);
+        final Connection connection = mock(Connection.class);
+        final PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        final ResultSet resultSet = mock(ResultSet.class);
+
+        final String sql = "select id from user";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, true, false);
+
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        // when
+        final List<TestUser> users = jdbcTemplate.query(sql, rowMapper());
+
+        // then
+        assertThat(users).hasSize(2);
+    }
+
+    @Test
+    @DisplayName("1개의 결과가 나오는 queryForObject")
+    void queryForObject() throws SQLException {
+        // given
+        final DataSource dataSource = mock(DataSource.class);
+        final Connection connection = mock(Connection.class);
+        final PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        final ResultSet resultSet = mock(ResultSet.class);
+
+        final String sql = "select id from user where id = ?";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, false);
+
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        // when
+        final TestUser testUser = jdbcTemplate.queryForObject(sql, rowMapper(), 1L);
+
+        // then
+        assertThat(testUser).isNotNull();
+    }
+
+    @Test
+    @DisplayName("2개의 결과가 나오는 queryForObject는 예외가 발생한다")
+    void queryForObjectException() throws SQLException {
+        // given
+        final DataSource dataSource = mock(DataSource.class);
+        final Connection connection = mock(Connection.class);
+        final PreparedStatement preparedStatement = mock(PreparedStatement.class);
+        final ResultSet resultSet = mock(ResultSet.class);
+
+        final String sql = "select id from user where account = ?";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.getRow()).thenReturn(2);
+
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        // when, then
+        assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, rowMapper(), "tiki"))
+                .isExactlyInstanceOf(DataAccessException.class);
+    }
+
+
+    @Test
+    @DisplayName("update 로직 테스트")
+    void update() throws SQLException {
+        // given
+        final DataSource dataSource = mock(DataSource.class);
+        final Connection connection = mock(Connection.class);
+        final PreparedStatement preparedStatement = mock(PreparedStatement.class);
+
+        final String sql = "insert into users (id) values (?)";
+
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeUpdate()).thenReturn(1);
+
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+
+        // when
+        final int update = jdbcTemplate.update(sql, rowMapper(), 1L);
+
+        // then
+        assertThat(update).isEqualTo(1);
+    }
 }
---
+++ jdbc/src/test/java/nextstep/jdbc/TestUser.java
@@ -0,0 +1,10 @@
+package nextstep.jdbc;
+
+public class TestUser {
+
+    private final Long id;
+
+    public TestUser(final Long id) {
+        this.id = id;
+    }
+}
