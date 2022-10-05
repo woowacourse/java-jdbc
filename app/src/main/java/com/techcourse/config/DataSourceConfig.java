11a3eb654d861590437dc2e0fb424201eb49f4dc
hyeonic <dev.hyeonic@gmail.com>
2022-10-06 08:48:10 +0900

    refactor: 풀 패키지명 제거

--- app/src/main/java/com/techcourse/config/DataSourceConfig.java
+++ app/src/main/java/com/techcourse/config/DataSourceConfig.java
@@ -1,14 +1,18 @@
 package com.techcourse.config;
 
+import javax.sql.DataSource;
 import org.h2.jdbcx.JdbcDataSource;
 
 import java.util.Objects;
 
 public class DataSourceConfig {
 
-    private static javax.sql.DataSource INSTANCE;
+    private static DataSource INSTANCE;
 
-    public static javax.sql.DataSource getInstance() {
+    private DataSourceConfig() {
+    }
+
+    public static DataSource getInstance() {
         if (Objects.isNull(INSTANCE)) {
             INSTANCE = createJdbcDataSource();
         }
@@ -22,6 +26,4 @@ public class DataSourceConfig {
         jdbcDataSource.setPassword("");
         return jdbcDataSource;
     }
-
-    private DataSourceConfig() {}
 }
