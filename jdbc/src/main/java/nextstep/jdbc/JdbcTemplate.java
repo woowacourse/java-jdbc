3ea42e7020aba87c697d0b34b51259bbda67b7aa
devHudi <devhudi@gmail.com>
2022-10-05 13:22:58 +0900

    refactor: ThrowingFunction 함수형 인터페이스를 정의하여 불필요한 try - catch 제거

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -6,7 +6,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Function;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,31 +22,19 @@ public class JdbcTemplate {
     }
 
     public int update(final String sql, final Object... args) {
-        return usePreparedStatement(sql, pstmt -> {
-            try {
-                return pstmt.executeUpdate();
-            } catch (SQLException e) {
-                throw new DataAccessException(e.getMessage(), e);
-                // TODO: 더 추상화된 예외 메시지를 사용해야함
-            }
-        }, args);
+        return usePreparedStatement(sql, PreparedStatement::executeUpdate, args);
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
         return usePreparedStatement(sql, pstmt -> {
-            try {
-                List<T> results = new ArrayList<>();
-                ResultSet rs = pstmt.executeQuery();
-                if (rs.next()) {
-                    T result = rowMapper.run(rs);
-                    results.add(result);
-                }
-
-                return results;
-            } catch (SQLException e) {
-                throw new DataAccessException(e.getMessage(), e);
-                // TODO: 더 추상화된 예외 메시지를 사용해야함
+            List<T> results = new ArrayList<>();
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                T result = rowMapper.run(rs);
+                results.add(result);
             }
+
+            return results;
         }, args);
     }
 
@@ -56,7 +43,8 @@ public class JdbcTemplate {
         return results.get(FIRST_RESULT_INDEX);
     }
 
-    public <T> T usePreparedStatement(final String sql, final Function<PreparedStatement, T> function,
+    public <T> T usePreparedStatement(final String sql,
+                                      final ThrowingFunction<PreparedStatement, T, SQLException> function,
                                       final Object... args) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
