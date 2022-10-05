ca9092483e5b7626c39f10f779ec9df4282a99a5
Yeoleum <harukawa99@naver.com>
2022-10-05 13:26:58 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 열음(이주형) 미션 제출합니다. (#84)
    * docs: 1단계 요구사항 정리
    
    * docs : 1단계 요구사항 작성
    
    * test : JdbcTemplate 테스트 작성
    
    * feat : JdbcTemplate 구현
    
    * test : UserDaoTest dataSource 세팅 방식 수정
    
    * feat : UserDao 구현
    
    * docs: 1단계 요구사항 완료
    
    * style: 코드 포멧팅
    
    * test: User 테이블 초기화 로직 추가
    
    * refactor: Rowmapper 의존성 제거
    
    Signed-off-by: Juhyung990122 <harukawa99@naver.com>
    
    * refactor: 변수 사용
    
    Signed-off-by: Juhyung990122 <harukawa99@naver.com>
    
    * refactor: 검증 메서드명 변경
    
    Signed-off-by: Juhyung990122 <harukawa99@naver.com>
    
    * refactor: 오타수정
    
    * chore: 공백 제거
    
    * refactor: 템플릿 콜백 페턴 적용
    
    Signed-off-by: Juhyung990122 <harukawa99@naver.com>

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,12 +1,14 @@
 package com.techcourse.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import javax.sql.DataSource;
+import nextstep.jdbc.JdbcTemplate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class UserDaoTest {
 
@@ -14,11 +16,18 @@ class UserDaoTest {
 
     @BeforeEach
     void setup() {
-        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
-
-        userDao = new UserDao(DataSourceConfig.getInstance());
+        final DataSource dataSource = DataSourceConfig.getInstance();
+        DatabasePopulatorUtils.execute(dataSource);
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        initUserTable(jdbcTemplate);
+        userDao = new UserDao(jdbcTemplate);
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(user);
+    }
+
+    private void initUserTable(JdbcTemplate jdbcTemplate) {
+        jdbcTemplate.executeUpdate("truncate table users");
+        jdbcTemplate.executeUpdate("alter table users alter column id restart with 1");
     }
 
     @Test
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,88 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    private <T> T execute(final String sql, final JdbcCallback<T> jdbcCallback) {
+        try (final Connection conn = getConnection();
+             final PreparedStatement preparedStatement = conn.prepareStatement(sql)
+        ) {
+            return jdbcCallback.call(preparedStatement);
+        } catch (SQLException e) {
+            throw new DataAccessException("커넥션을 가져올 수 없습니다.");
+        }
+    }
+
+    public <T> List<T> query(final String sql,
+                             final RowMapper<T> rowMapper,
+                             final Object... args) {
+        return execute(sql, preparedStatement -> {
+            setParams(preparedStatement, args);
+            return executeQuery(preparedStatement, rowMapper);
+        });
+    }
+
+    public <T> T queryForObject(final String sql,
+                                final RowMapper<T> rowMapper,
+                                final Object... args) {
+        return execute(sql, preparedStatement -> {
+            setParams(preparedStatement, args);
+            final List<T> result = executeQuery(preparedStatement, rowMapper);
+            validateSingleResultSize(result);
+            return result.get(0);
+        });
+    }
+
+    private <T> void validateSingleResultSize(List<T> result) {
+        if (result.size() != 1) {
+            throw new DataAccessException("조회 결과가 1개가 아닙니다.");
+        }
+    }
+
+    public void executeUpdate(final String sql,
+                              final Object... args) {
+        execute(sql, preparedStatement -> {
+            setParams(preparedStatement, args);
+            return update(preparedStatement);
+        });
+    }
+
+    private <T> List<T> executeQuery(final PreparedStatement preparedStatement,
+                                     final RowMapper<T> rowMapper) throws SQLException {
+        try (final ResultSet resultSet = preparedStatement.executeQuery()) {
+            final List<T> mappedRowResult = new ArrayList<>();
+            int rowNum = 0;
+            while (resultSet.next()) {
+                mappedRowResult.add(rowMapper.mapRow(resultSet, rowNum++));
+            }
+            return mappedRowResult;
+        }
+    }
+
+    private int update(PreparedStatement preparedStatement) {
+        try {
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("쿼리를 실행하지 못했습니다.");
+        }
+    }
+
+    private void setParams(PreparedStatement preparedStatement, Object[] args) {
+        int parameterIndex = 1;
+        for (Object arg : args) {
+            try {
+                preparedStatement.setObject(parameterIndex++, arg);
+            } catch (SQLException e) {
+                throw new DataAccessException("잘못된 파라미터입니다.");
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new DataAccessException("DB 커넥트 도중 에러가 발생했습니다.");
+        }
     }
 }
