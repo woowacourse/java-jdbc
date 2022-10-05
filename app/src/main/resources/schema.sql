92a355a1eb4847344af567d0ad1f3a6f6fedc7a4
seong-wooo <seongwoo0513@uos.ac.kr>
2022-10-05 11:21:27 +0900

    refactor: truncate를 이용하여 테스트 격리하도록 변경

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,13 +1,10 @@
-drop table if exists users;
-drop table if exists user_history;
-
-create table users (
-    id bigint auto_increment,
-    account varchar(100) not null,
+create table if not exists users (
+     id bigint auto_increment,
+     account varchar(100) not null,
     password varchar(100) not null,
     email varchar(100) not null,
     primary key(id)
-);
+    );
 
 create table if not exists user_history (
     id bigint auto_increment,
@@ -18,4 +15,7 @@ create table if not exists user_history (
     created_at datetime not null,
     created_by varchar(100) not null,
     primary key(id)
-);
+    );
+
+truncate table users;
+alter table users alter column id restart with 1;
