bf3ba1dc2d3d46001e456b4e09694fc2125a2f4c
2yujeong <76840965+2yujeong@users.noreply.github.com>
2022-10-05 11:56:51 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 이브(이유정) 미션 제출합니다. (#106)
    * chore: jdbc 모듈 JavaVersion 설정 추가
    
    * refactor: Dao에서 중복되는 JDBC 로직을 JdbcTemplate으로 분리
    
    * refactor: JdbcTemplate 메소드 분리, springframework 의존성 제거
    
    * refactor: 테스트 격리를 위한 schema.sql 파일 수정
    
    * refactor: 템플릿 콜백 패턴 적용
    
    * refactor: JdbcTemplate을 이용하여 DatabasePopulatorUtils 리팩토링
    
    * refactor: JdbcTemplate 테스트 작성

--- app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
+++ app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
@@ -1,46 +1,27 @@
 package com.techcourse.support.jdbc.init;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
+import javax.sql.DataSource;
+import nextstep.jdbc.JdbcTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatabasePopulatorUtils {
 
     private static final Logger log = LoggerFactory.getLogger(DatabasePopulatorUtils.class);
 
     public static void execute(final DataSource dataSource) {
-        Connection connection = null;
-        Statement statement = null;
-        try {
-            final var url = DatabasePopulatorUtils.class.getClassLoader().getResource("schema.sql");
-            final var file = new File(url.getFile());
-            final var sql = Files.readString(file.toPath());
-            connection = dataSource.getConnection();
-            statement = connection.createStatement();
-            statement.execute(sql);
-        } catch (NullPointerException | IOException | SQLException e) {
-            log.error(e.getMessage(), e);
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-            } catch (SQLException ignored) {}
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ignored) {}
+        final var url = DatabasePopulatorUtils.class.getClassLoader().getResource("schema.sql");
+        final var file = new File(url.getFile());
+        try {
+            String sql = Files.readString(file.toPath());
+            jdbcTemplate.update(sql);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
         }
     }
-
-    private DatabasePopulatorUtils() {}
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,17 +1,55 @@
 package nextstep.jdbc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
-    private final DataSource dataSource;
+    private final JdbcExecutor jdbcExecutor;
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.jdbcExecutor = new JdbcExecutor(dataSource);
+    }
+
+    public int update(final String sql, final Object... args) {
+        return jdbcExecutor.execute(sql, args, PreparedStatement::executeUpdate);
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) throws DataAccessException {
+        return jdbcExecutor.execute(sql, args, (pstmt) -> {
+            ResultSet rs = pstmt.executeQuery();
+            return createResultByRowMapper(rowMapper, rs);
+        });
+    }
+
+    private <T> List<T> createResultByRowMapper(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
+        List<T> result = new ArrayList<>();
+        while (rs.next()) {
+            result.add(rowMapper.mapRow(rs));
+        }
+        rs.close();
+
+        return result;
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) throws DataAccessException {
+        List<T> results = query(sql, rowMapper, args);
+        validateResultSizeSingle(results);
+
+        return results.iterator().next();
+    }
+
+    private <T> void validateResultSizeSingle(final List<T> results) {
+        if (results.isEmpty()) {
+            throw new DataAccessException("Empty result");
+        }
+        if (results.size() > 1) {
+            throw new DataAccessException("Incorrect result size, expected : 1, actual : " + results.size());
+        }
     }
 }
