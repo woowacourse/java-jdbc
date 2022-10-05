274cb6cd2817595960c03ca56a6a6aff5e66b316
jaejae-yoo <wotj102@gmail.com>
2022-10-05 21:19:31 +0900

    refactor: resultSet 수정

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,5 +1,4 @@
 package nextstep.jdbc;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,35 +6,28 @@ import java.sql.SQLException;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.sql.DataSource;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.util.CollectionUtils;
-
 public class JdbcTemplate {
-
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
     private final DataSource dataSource;
-
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public int update(final String sql, final Object... args) {
+    public int update(final String sql, Object... args) {
         try (final Connection connection = dataSource.getConnection();
              final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             setValues(preparedStatement, args);
-
             return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             log.error("Execute Query Failed: " + e);
             throw new DataAccessException("Query를 성공적으로 실행하지 못했습니다.");
         }
     }
-
     private void setValues(final PreparedStatement preparedStatement, final Object[] args) throws SQLException {
         for (int idx = 0; idx < args.length; idx++) {
             preparedStatement.setObject(idx + 1, args[idx]);
@@ -44,10 +36,10 @@ public class JdbcTemplate {
 
     public <T> List<T> query(final String sql, RowMapper<T> rowMapper, Object... args) {
         try (final Connection connection = dataSource.getConnection();
-             final PreparedStatement preparedStatement = connection.prepareStatement(sql);
-             final ResultSet resultSet = preparedStatement.executeQuery()
-        ) {
+             final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             setValues(preparedStatement, args);
+
+            final ResultSet resultSet = preparedStatement.executeQuery();
             final RowMapperResultSetExtractor<T> resultSetExtractor = new RowMapperResultSetExtractor<>(rowMapper);
             return resultSetExtractor.extractData(resultSet);
         } catch (SQLException e) {
@@ -55,7 +47,6 @@ public class JdbcTemplate {
             throw new DataAccessException("Query를 성공적으로 실행하지 못했습니다.");
         }
     }
-
     public <T> T queryForObject(final String sql, RowMapper<T> rowMapper, Object... args) {
         final List<T> results = query(sql, rowMapper, args);
         if (CollectionUtils.isEmpty(results)) {
