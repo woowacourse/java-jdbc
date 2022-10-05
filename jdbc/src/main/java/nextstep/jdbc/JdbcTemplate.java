d8f42e74f69ac564cbba588cc2d5b0817d35ecd3
파랑 <77425729+summerlunaa@users.noreply.github.com>
2022-10-05 14:46:02 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 파랑(이하은) 미션 제출합니다.  (#85)
    * docs: 1단계 기능 요구 사항 추가
    
    * feat: jdbctemplate update 메서드 구현
    
    * feat: jdbctemplate queryForObject 메서드 구현
    
    * feat: user update, findByAccount 기능 구현
    
    * feat: jdbctemplate query 메서드 구현
    
    * fix: result set close 하도록 수정
    
    * test: JdbcTemplate 테스트 작성
    
    * test: query 테스트 수정
    
    * test: 클래스명 수정
    
    * refactor: static 키워드 제거
    
    * fix: queryForObject에서 결과가 존재하지 않는 경우 예외를 던지도록 수정

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,85 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            setArgsToPreparedStatement(preparedStatement, args);
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            setArgsToPreparedStatement(preparedStatement, args);
+            return executeGetObjectQuery(rowMapper, preparedStatement);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> T executeGetObjectQuery(final RowMapper<T> rowMapper,
+                                               final PreparedStatement preparedStatement) {
+        try (
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            if (resultSet.next()) {
+                return rowMapper.mapRow(resultSet);
+            }
+            throw new RuntimeException("결과가 존재하지 않습니다.");
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            setArgsToPreparedStatement(preparedStatement, args);
+            return executeGetListQuery(rowMapper, preparedStatement);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> ArrayList<T> executeGetListQuery(final RowMapper<T> rowMapper,
+                                                        final PreparedStatement preparedStatement) {
+        try (
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            ArrayList<T> result = new ArrayList<>();
+            while (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet));
+            }
+            return result;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setArgsToPreparedStatement(final PreparedStatement preparedStatement, final Object[] args)
+            throws SQLException {
+        for (int idx = 0; idx < args.length; idx++) {
+            preparedStatement.setObject(idx + 1, args[idx]);
+        }
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,84 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+import nextstep.support.DataSourceConfigForTest;
+import nextstep.support.TestUser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private static final RowMapper<TestUser> TEST_USER_ROW_MAPPER = resultSet -> new TestUser(
+            resultSet.getLong("id"),
+            resultSet.getString("account"),
+            resultSet.getString("password"),
+            resultSet.getString("email")
+    );
+
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void beforeEach() {
+        jdbcTemplate = new JdbcTemplate(DataSourceConfigForTest.getInstance());
+
+        String dropSql = "drop table if exists test_users;";
+        jdbcTemplate.update(dropSql);
+
+        String createTableSql = "create table test_users (\n"
+                + "    id bigint auto_increment,\n"
+                + "    account varchar(100) not null,\n"
+                + "    password varchar(100) not null,\n"
+                + "    email varchar(100) not null,\n"
+                + "    primary key(id)\n"
+                + ");";
+        jdbcTemplate.update(createTableSql);
+    }
+
+    @Test
+    void update() {
+        TestUser user = new TestUser("parang", "pass", "parang@email.com");
+        String sql = "insert into test_users (account, password, email) values (?, ?, ?)";
+
+        int result = jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void queryForObject() {
+        TestUser user = new TestUser("parang", "pass", "parang@email.com");
+        String insertSql = "insert into test_users (account, password, email) values (?, ?, ?)";
+        jdbcTemplate.update(insertSql, user.getAccount(), user.getPassword(), user.getEmail());
+
+        String findSql = "select id, account, password, email from test_users where account = ?";
+        TestUser findUser = jdbcTemplate.queryForObject(findSql, TEST_USER_ROW_MAPPER, "parang");
+
+        assertAll(
+                () -> {
+                    assertThat(findUser.getAccount()).isEqualTo("parang");
+                    assertThat(findUser.getPassword()).isEqualTo("pass");
+                    assertThat(findUser.getEmail()).isEqualTo("parang@email.com");
+                }
+        );
+    }
+
+    @Test
+    void query() {
+        String insertSql = "insert into test_users (account, password, email) values (?, ?, ?)";
+        TestUser user1 = new TestUser("parang1", "pass", "parang@email.com");
+        TestUser user2 = new TestUser("parang2", "pass", "parang@email.com");
+
+        jdbcTemplate.update(insertSql, user1.getAccount(), user1.getPassword(), user1.getEmail());
+        jdbcTemplate.update(insertSql, user2.getAccount(), user2.getPassword(), user2.getEmail());
+
+        String findSql = "select id, account, password, email from test_users";
+        List<TestUser> findUsers = jdbcTemplate.query(findSql, TEST_USER_ROW_MAPPER);
+
+        assertThat(findUsers).hasSize(2);
+        assertThat(findUsers).extracting("account")
+                .contains("parang1", "parang2");
+    }
 }
