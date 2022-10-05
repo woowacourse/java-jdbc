be2535b7c87ca3e782c0df4383cfbc0e49552fc8
Youngwoo Yoo <63579113+Yboyu0u@users.noreply.github.com>
2022-10-05 11:17:45 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 잉(유영우) 미션 제출합니다. (#103)
    * docs: 1단계 요구사향 작성
    
    * feat: UserDao CRUD 메서드 정의
    
    * feat: JDBC관련 로직 JDBCTemplate로 위임

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,58 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(String sql, Object... values) {
+        return execute(sql, preparedStatement -> {
+            setParameters(preparedStatement, values);
+            return preparedStatement.executeUpdate();
+        });
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        return execute(sql, (preparedStatement -> {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return getResult(rowMapper, resultSet);
+            }
+        }));
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... values) {
+        return execute(sql, (preparedStatement -> {
+            setParameters(preparedStatement, values);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                List<T> result = getResult(rowMapper, resultSet);
+                return result.get(0);
+            }
+        }));
+    }
+
+    private void setParameters(PreparedStatement preparedStatement, Object[] values) throws SQLException {
+        for (int i = 0; i < values.length; i++) {
+            preparedStatement.setObject(i + 1, values[i]);
+        }
+    }
+
+    private <T> List<T> getResult(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
+        int rowNum = 0;
+        List<T> result = new ArrayList<>();
+        while (resultSet.next()) {
+            result.add(rowMapper.mapRow(resultSet, rowNum++));
+        }
+        return result;
+    }
+
+    private <T> T execute(String sql, Executable<T> executable) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+
+            return executable.execute(pstmt);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
