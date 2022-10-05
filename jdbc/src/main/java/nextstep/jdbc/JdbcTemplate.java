66410ec0aec01f590cd12fb1b88360d03820abf5
최진영 <jinyoungchoi95@gmail.com>
2022-10-05 11:10:53 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 오리(최진영) 미션 제출합니다. (#99)
    * feat: findAll 메소드 구현
    
    * feat: findByAccount 메소드 구현
    
    * feat: update 구현
    
    * feat: JdbcTemplate executeUpdate 추가 및 적용
    
    * feat: JdbcTemplate query 추가 및 적용
    
    * feat: JdbcTemplate query findByAccount 적용
    
    * feat: JdbcTemplate executeUpdate insert 적용
    
    * refactor: query메소드 List 반환 수정
    
    * feat: queryForObject 추가
    
    * feat: 미사용 생성자 수정
    
    * feat: 미사용 로거 제거
    
    * feat: row mapper final
    
    * refactor: 메소드 분리

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
+import org.springframework.lang.NonNull;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,72 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(final String sql, @NonNull final RowMapper<T> rowMapper, final Object... params) {
+        ResultSet resultSet = null;
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            prepareStatementSetParamters(pstmt, params);
+
+            resultSet = pstmt.executeQuery();
+            List<T> result = new ArrayList<>();
+            return parseResultSet(rowMapper, resultSet, result);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closeResultSet(resultSet);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        List<T> result = query(sql, rowMapper, params);
+        return singleResult(result);
+    }
+
+    public int executeUpdate(final String sql, final Object... params) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            prepareStatementSetParamters(pstmt, params);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement prepareStatementSetParamters(final PreparedStatement pstmt, final Object[] params)
+            throws SQLException {
+        for (int i = 1; i <= params.length; i++) {
+            pstmt.setObject(i, params[i - 1]);
+        }
+        return pstmt;
+    }
+
+    private <T> List<T> parseResultSet(final RowMapper<T> rowMapper, final ResultSet resultSet, final List<T> result)
+            throws SQLException {
+        if (resultSet.next()) {
+            result.add(rowMapper.mapRow(resultSet));
+        }
+        return result;
+    }
+
+    private void closeResultSet(final ResultSet resultSet) {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        } catch (SQLException ignored) {
+            throw new RuntimeException("result set close 에러");
+        }
+    }
+
+    private <T> T singleResult(final List<T> values) {
+        if (values.isEmpty()) {
+            throw new RuntimeException("쿼리 결과가 비어있습니다.");
+        }
+        if (values.size() > 1) {
+            throw new RuntimeException("쿼리 결과가 2개 이상입니다.");
+        }
+        return values.get(0);
     }
 }
