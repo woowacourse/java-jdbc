f602f8bba95f9a73fc10348a5e1895687e245987
YJGwon <trulyforky@gmail.com>
2022-10-05 13:50:37 +0900

    refactor(UserDao): 가변인자 사용으로 코드 간결하게 수정

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -8,7 +8,16 @@ import nextstep.jdbc.RowMapper;
 
 public class UserDao {
 
+    private static final RowMapper<User> USER_MAPPER = resultSet -> {
+        final long id = resultSet.getLong("id");
+        final String account = resultSet.getString("account");
+        final String password = resultSet.getString("password");
+        final String email = resultSet.getString("email");
+        return new User(id, account, password, email);
+    };
+
     private final JdbcTemplate jdbcTemplate;
+
 
     public UserDao(final DataSource dataSource) {
         this(new JdbcTemplate(dataSource));
@@ -20,45 +29,25 @@ public class UserDao {
 
     public void insert(final User user) {
         final String sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.update(sql, statement -> {
-            statement.setString(1, user.getAccount());
-            statement.setString(2, user.getPassword());
-            statement.setString(3, user.getEmail());
-        });
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
         final String sql = "update users set account = ?, password = ?, email = ? where id = ?";
-        jdbcTemplate.update(sql, statement -> {
-            statement.setString(1, user.getAccount());
-            statement.setString(2, user.getPassword());
-            statement.setString(3, user.getEmail());
-            statement.setLong(4, user.getId());
-        });
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
         final String sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(sql, mapUser());
+        return jdbcTemplate.query(sql, USER_MAPPER);
     }
 
     public User findById(final Long id) {
         final String sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, mapUser(), id);
+        return jdbcTemplate.queryForObject(sql, USER_MAPPER, id);
     }
-
     public User findByAccount(final String account) {
         final String sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(sql, mapUser(), account);
-    }
-
-    private RowMapper<User> mapUser() {
-        return resultSet -> {
-            final long id = resultSet.getLong("id");
-            final String account = resultSet.getString("account");
-            final String password = resultSet.getString("password");
-            final String email = resultSet.getString("email");
-            return new User(id, account, password, email);
-        };
+        return jdbcTemplate.queryForObject(sql, USER_MAPPER, account);
     }
 }
