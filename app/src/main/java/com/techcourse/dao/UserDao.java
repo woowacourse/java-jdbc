687a1e92bcc9d79c4a974bebc02f42baf8624ac0
Soomin Sohn <soomin.sohn.dev@gmail.com>
2022-10-06 09:38:07 +0900

    refactor: UserDao RowMapper 칼럼이름으로 변경

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -12,10 +12,10 @@ public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
-    private static final RowMapper<User> ROW_MAPPER = rs -> new User(rs.getLong(1),
-            rs.getString(2),
-            rs.getString(3),
-            rs.getString(4));
+    private static final RowMapper<User> ROW_MAPPER = rs -> new User(rs.getLong("id"),
+            rs.getString("account"),
+            rs.getString("password"),
+            rs.getString("email"));
 
     private final JdbcTemplate jdbcTemplate;
 
