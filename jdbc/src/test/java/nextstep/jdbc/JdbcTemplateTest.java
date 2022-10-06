5e0c52701788017b7ea314423c0c8546d58cf4ab
dongho <dongho108@naver.com>
2022-10-06 10:26:57 +0900

    refactor: JdbctemplateTest 에서 다른 의존성 제거

--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,76 +1,94 @@
 package nextstep.jdbc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
-import java.util.stream.Collectors;
-import nextstep.config.DataSourceConfig;
-import nextstep.config.DatabasePopulatorUtils;
-import org.junit.jupiter.api.AfterEach;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class JdbcTemplateTest {
 
     private JdbcTemplate jdbcTemplate;
+    private PreparedStatement preparedStatement;
+    private Connection connection;
+    private ResultSet resultSet;
 
     @BeforeEach
-    void setup() {
-        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
-        jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
-    }
+    void setup() throws SQLException {
+        preparedStatement = mock(PreparedStatement.class);
+        connection = mock(Connection.class);
+        resultSet = mock(ResultSet.class);
+        final DataSource dataSource = mock(DataSource.class);
+        jdbcTemplate = new JdbcTemplate(dataSource);
 
-    @AfterEach
-    void reset() {
-        final String findQuery = "select id, account, password, email from users";
-        final List<User> users = jdbcTemplate.query(findQuery, UserMapper.getInstance());
-
-        final String deleteQuery = "delete from users where id = ?";
-        users.forEach(user -> jdbcTemplate.update(deleteQuery, user.getId()));
-    }
-
-    @Test
-    void update() {
-        insertUser(new User("gugu", "password", "hkkang@woowahan.com"));
-
-        final String findQuery = "select id, account, password, email from users where id = ?";
-        final User findUser = jdbcTemplate.queryForObject(findQuery, UserMapper.getInstance(), 1L);
-        assertThat(findUser).isNotNull();
+        when(connection.prepareStatement(any())).thenReturn(preparedStatement);
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
     }
 
     @Test
-    void query() {
-        insertUser(new User("gugu", "password", "hkkang@woowahan.com"));
-        insertUser(new User("ash", "ash123", "ash@gmail.com"));
-
-        final String findQuery = "select id, account, password, email from users";
-        final List<User> users = jdbcTemplate.query(findQuery, UserMapper.getInstance());
-        assertThat(users.stream()
-                    .map(User::getAccount)
-                    .collect(Collectors.toList()))
-                .hasSize(2)
-                .contains("gugu", "ash");
-    }
-
-    @Test
-    void queryForObject() {
-        final User user = new User("gugu", "password", "hkkang@woowahan.com");
-        insertUser(user);
-
-        final String findQuery = "select id, account, password, email from users where account = ?";
-        final User findUser = jdbcTemplate.queryForObject(findQuery, UserMapper.getInstance(), "gugu");
-        assertAll(
-                () -> assertThat(findUser.getAccount()).isEqualTo(user.getAccount()),
-                () -> assertThat(findUser.getPassword()).isEqualTo(user.getPassword()),
-                () -> assertThat(findUser.getEmail()).isEqualTo(user.getEmail())
-        );
-    }
-
-    private void insertUser(final User user) {
+    void update() throws SQLException {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        final User user = new User("gugu", "password", "hkkang@woowahan.com");
         jdbcTemplate.update(sql, user.getAccount(),
                 user.getPassword(),
                 user.getEmail());
+
+        verify(preparedStatement).setObject(1, "gugu");
+        verify(preparedStatement).setObject(2, "password");
+        verify(preparedStatement).setObject(3, "hkkang@woowahan.com");
+        verify(preparedStatement).executeUpdate();
+        verify(connection).close();
+        verify(preparedStatement).close();
+    }
+
+    @Test
+    void query() throws SQLException {
+        // given
+        final User gugu = new User("gugu", "password", "hkkang@woowahan.com");
+        final User ash = new User("ash", "ash123", "ash@gmail.com");
+        final RowMapper<User> rowMapper = mock(RowMapper.class);
+        final String sql = "select id, account, password, email from users";
+
+        // when
+        when(resultSet.next()).thenReturn(true, true, false);
+        when(rowMapper.mapRow(resultSet, 0)).thenReturn(gugu);
+        when(rowMapper.mapRow(resultSet, 1)).thenReturn(ash);
+        final List<User> users = jdbcTemplate.query(sql, rowMapper);
+
+        // then
+        assertThat(users).extracting("account")
+                .contains("gugu", "ash");
+        verify(resultSet, times(3)).next();
+        verify(preparedStatement).executeQuery();
+        verify(connection).close();
+        verify(preparedStatement).close();
+    }
+
+    @Test
+    void queryForObject() throws SQLException {
+        final User gugu = new User("gugu", "password", "hkkang@woowahan.com");
+        final RowMapper<User> rowMapper = mock(RowMapper.class);
+        final String sql = "select id, account, password, email from users where account = ?";
+
+        when(resultSet.next()).thenReturn(true, false);
+        when(rowMapper.mapRow(resultSet, 0)).thenReturn(gugu);
+        final User user = jdbcTemplate.queryForObject(sql, rowMapper);
+
+        assertThat(user.getAccount()).isEqualTo("gugu");
+        verify(resultSet).next();
+        verify(preparedStatement).executeQuery();
+        verify(connection).close();
+        verify(preparedStatement).close();
     }
 }
