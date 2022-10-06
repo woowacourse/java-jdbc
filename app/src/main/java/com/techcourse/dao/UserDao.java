c5aa14a3489d2cbf4e2ac98c380e049872321dbd
Seongwon97 <osw970325@naver.com>
2022-10-07 01:37:55 +0900

    refactor: 사용하지 않는 로그 필드 제거

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,9 +2,6 @@ package com.techcourse.dao;
 
 import java.util.List;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.techcourse.domain.User;
 
 import nextstep.jdbc.JdbcTemplate;
@@ -12,7 +9,6 @@ import nextstep.jdbc.RowMapper;
 
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
     private static final RowMapper<User> USER_ROW_MAPPER = (resultSet, count) -> new User(
             resultSet.getLong("id"),
             resultSet.getString("account"),
