dd2f3a24a2e50696612343b87b7b5c8b9adcd3de
JoJo Green <82805588+jojogreen91@users.noreply.github.com>
2022-10-06 23:24:48 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 조조그린(조한석) 미션 제출합니다. (#125)
    * feat: JdbcTemplate 구현
    
    * feat: JdbcTemplateTest 구현

---
+++ jdbc/src/main/java/nextstep/jdbc/RowMapper.java
@@ -0,0 +1,10 @@
+package nextstep.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+
+    T mapping(final ResultSet resultSet) throws SQLException;
+}
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,72 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 class JdbcTemplateTest {
 
+    private static RowMapper<String> testRowMapper = resultSet -> "entity";
+
+    private DataSource dataSource = Mockito.mock(DataSource.class);
+    private Connection connection = Mockito.mock(Connection.class);
+    private PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+    private ResultSet resultSet = Mockito.mock(ResultSet.class);
+
+    private JdbcTemplate jdbcTemplate = new JdbcTemplate(this.dataSource);
+    @BeforeEach
+    private void setUp() throws SQLException {
+        Mockito.when(dataSource.getConnection())
+                .thenReturn(connection);
+        Mockito.when(connection.prepareStatement(Mockito.any()))
+                .thenReturn(preparedStatement);
+        Mockito.when(preparedStatement.executeQuery())
+                .thenReturn(resultSet);
+        Mockito.when(resultSet.next())
+                .thenReturn(false);
+    }
+
+    @Test
+    void update() throws SQLException {
+        // when
+        jdbcTemplate.update("sql", "name");
+
+        // then
+        Mockito.verify(dataSource).getConnection();
+        Mockito.verify(connection).prepareStatement("sql");
+        Mockito.verify(preparedStatement).setObject(1, "name");
+    }
+
+    @Test
+    void queryForObject() throws SQLException {
+        // when
+        Object target = jdbcTemplate.queryForObject("sql", testRowMapper, "name");
+
+        // then
+        Mockito.verify(dataSource).getConnection();
+        Mockito.verify(connection).prepareStatement("sql");
+        Mockito.verify(preparedStatement).setObject(1, "name");
+        Mockito.verify(resultSet).next();
+        assertThat(target).isEqualTo("entity");
+    }
+
+    @Test
+    void query() throws SQLException {
+        // when
+        List<String> result = jdbcTemplate.query("sql", testRowMapper);
+
+        // then
+        Mockito.verify(dataSource).getConnection();
+        Mockito.verify(connection).prepareStatement("sql");
+        Mockito.verify(resultSet).next();
+        assertThat(result).hasSize(0);
+    }
 }
