064132e20b5461c6dd06f223b99bf7991a2c7542
yeonLog <53105735+yeon-06@users.noreply.github.com>
2022-10-06 11:13:22 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 연로그(권시연) 미션 제출합니다. (#88)
    * docs: 1단계 요구 사항 정리
    
    * refactor: insert, findById 메서드에 try-with-resources 적용
    
    * feat: update 구현
    
    * feat: findByAccount 구현
    
    * feat: findAll 구현
    
    * refactor: create, update의 공통 로직 분리
    
    * refactor: create, update의 인자 세팅하는 로직 JdbcTemplate으로 이동
    
    * refactor: findByXXX 공통 로직 JdbcTemplate으로 이동
    
    * refactor: findAll 로직 JdbcTemplate으로 이동
    
    * refactor: query와 queryForObject의 공통 로직 제거
    
    * test: 테이블 초기화 로직 추가
    
    * style: 하드코딩 제거
    
    * refactor: 불필요한 로직 제거
    
    * docs: 1단계 요구 사항 체크
    
    * style: 메서드명 변경
    
    * test: given-when-then 형식으로 변경
    
    * test: 테이블 재생성 schema.sql로 이동
    
    * style: queryForObject의 익셉션 발생 시 에러 메시지 변경
    
    * test: 모든 자원을 close 했는지 확인

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,17 +1,84 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
-
 public class JdbcTemplate {
 
+    private static final int SINGLE_COUNT = 1;
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... params) {
+        log.debug("query : {}", sql);
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            setParams(preparedStatement, params);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        log.debug("query : {}", sql);
+        return execute(sql, rowMapper, params);
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        log.debug("query : {}", sql);
+
+        List<T> result = execute(sql, rowMapper, params);
+        validateSingleSize(result);
+        return result.get(0);
+    }
+
+    private <T> List<T> execute(final String sql, final RowMapper<T> rowMapper, final Object[] params) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            setParams(preparedStatement, params);
+            return toList(preparedStatement.executeQuery(), rowMapper);
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void setParams(final PreparedStatement preparedStatement, final Object[] params) throws SQLException {
+        int size = params.length;
+        for (int i = 0; i < size; i++) {
+            preparedStatement.setObject(i + 1, params[i]);
+        }
+    }
+
+    private <T> List<T> toList(final ResultSet resultSet, final RowMapper<T> rowMapper) {
+        try (resultSet) {
+            List<T> result = new ArrayList<>();
+            while (resultSet.next()) {
+                result.add(rowMapper.map(resultSet));
+            }
+            return result;
+        } catch (SQLException e) {
+            return new ArrayList<>();
+        }
+    }
+
+    private <T> void validateSingleSize(final List<T> result) {
+        if (result.size() != SINGLE_COUNT) {
+            throw new DataAccessException("조회 결과 값이 0개 또는 여러개가 존재합니다.");
+        }
     }
 }
