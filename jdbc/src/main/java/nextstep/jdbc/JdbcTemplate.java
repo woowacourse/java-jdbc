447a17d58d6742239970a8b67da41204482f56af
Byunghoon <76891875+nbalance97@users.noreply.github.com>
2022-10-08 00:25:11 +0900

    [JDBC 라이브러리 구현하기 - 2단계] 라쿤(이병훈) 미션 제출합니다. (#147)
    * refactor: DataAccessException으로 변경
    
    * test: Connection 검증 추가
    
    * refactor: RowMapper을 닫지 않았던 문제점 해결
    
    * refactor: queryForObject 행이 한개일 시 예외 로직 추가
    
    * test: userDao 테스트 격리, AutoIncrement 초기화
    
    * refactor: 공백 제거

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -33,31 +33,39 @@ public class JdbcTemplate {
     public <T> T queryForObject(final String sql, RowMapper<T> rowMapper, Object... params) {
         return doExecute(sql, preparedStatement -> {
             setParamsToStatement(preparedStatement, params);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            resultSet.next();
-
-            return rowMapper.mapRow(resultSet, ONE_OBJECT_ROW_NUM);
+            return processMapping(preparedStatement, rowMapper, this::mappingRowToObject);
         });
+    }
+
+    private <T> T mappingRowToObject(ResultSet resultSet, RowMapper<T> rowMapper) throws SQLException {
+        if (!resultSet.next()) {
+            throw new DataAccessException("매칭되는 결과값이 없습니다.");
+        }
+
+        final T mappingObject = rowMapper.mapRow(resultSet, ONE_OBJECT_ROW_NUM);
+        if (resultSet.next()) {
+            throw new DataAccessException("행이 여러개가 있습니다.");
+        }
+
+        return mappingObject;
     }
 
     public <T> List<T> query(final String sql, RowMapper<T> rowMapper, Object... params) {
-
         return doExecute(sql, preparedStatement -> {
             setParamsToStatement(preparedStatement, params);
-            final ResultSet resultSet = preparedStatement.executeQuery();
-            final List<T> objects = new ArrayList<>();
-            mappingRowToObjects(rowMapper, resultSet, objects);
-
-            return objects;
+            return processMapping(preparedStatement, rowMapper, this::mappingRowToObjects);
         });
     }
 
-    private <T> void mappingRowToObjects(RowMapper<T> rowMapper, ResultSet resultSet, List<T> objects)
+    private <T> List<T> mappingRowToObjects(ResultSet resultSet, RowMapper<T> rowMapper)
             throws SQLException {
+        final List<T> objects = new ArrayList<>();
         int rowNum = 1;
         while (resultSet.next()) {
             objects.add(rowMapper.mapRow(resultSet, rowNum++));
         }
+
+        return objects;
     }
 
     private void setParamsToStatement(PreparedStatement preparedStatement, Object... params) throws SQLException {
@@ -71,7 +79,15 @@ public class JdbcTemplate {
              PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             return callback.doInPreparedStatement(preparedStatement);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T, R> R processMapping(PreparedStatement preparedStatement, RowMapper<T> rowMapper, RowMapperCallback<T, R> callback) {
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            return callback.doInRowMapper(resultSet, rowMapper);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
         }
     }
 }
