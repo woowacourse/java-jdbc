7e6a6a2bfc486f8d1c9aa86cec096cbdf2ab5701
leokim <kunga077@gmail.com>
2022-10-07 20:37:44 +0900

    refactor: 불필요한 공백 제거

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -1,7 +1,6 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
 import nextstep.jdbc.JdbcTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +18,13 @@ public class UserHistoryDao {
     public void log(final UserHistory userHistory) {
         final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
 
-        jdbcTemplate.update(sql, userHistory.getUserId(), userHistory.getAccount(), userHistory.getPassword(),
-                userHistory.getEmail(), userHistory.getCreatedAt(), userHistory.getCreateBy());
+        jdbcTemplate.update(sql,
+                userHistory.getUserId(),
+                userHistory.getAccount(),
+                userHistory.getPassword(),
+                userHistory.getEmail(),
+                userHistory.getCreatedAt(),
+                userHistory.getCreateBy()
+        );
     }
 }
