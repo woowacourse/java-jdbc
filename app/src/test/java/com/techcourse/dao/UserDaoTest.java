5736ba541dfe5139333d2b6fd4179c4137d9cca4
her0807 <her0807@naver.com>
2022-10-08 19:38:15 +0900

    feat: update 구현

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,69 +1,70 @@
 package com.techcourse.dao;
 
-import com.techcourse.config.DataSourceConfig;
-import com.techcourse.domain.User;
-import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 
 class UserDaoTest {
 
-    private UserDao userDao;
+	private UserDao userDao;
 
-    @BeforeEach
-    void setup() {
-        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
+	@BeforeEach
+	void setup() {
+		DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
 
-        userDao = new UserDao(DataSourceConfig.getInstance());
-        final var user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.insert(user);
-    }
+		userDao = new UserDao(DataSourceConfig.getInstance());
+		final var user = new User("gugu", "password", "hkkang@woowahan.com");
+		userDao.insert(user);
+	}
 
-    @Test
-    void findAll() {
-        final var users = userDao.findAll();
+	@Test
+	void findAll() {
+		final var users = userDao.findAll();
 
-        assertThat(users).isNotEmpty();
-    }
+		assertThat(users).isNotEmpty();
+	}
 
-    @Test
-    void findById() {
-        final var user = userDao.findById(1L);
+	@Test
+	void findById() {
+		final var user = userDao.findById(1L);
 
-        assertThat(user.getAccount()).isEqualTo("gugu");
-    }
+		assertThat(user.getAccount()).isEqualTo("gugu");
+	}
 
-    @Test
-    void findByAccount() {
-        final var account = "gugu";
-        final var user = userDao.findByAccount(account);
+	@Test
+	void findByAccount() {
+		final var account = "gugu";
+		final var user = userDao.findByAccount(account);
 
-        assertThat(user.getAccount()).isEqualTo(account);
-    }
+		assertThat(user.getAccount()).isEqualTo(account);
+	}
 
-    @Test
-    void insert() {
-        final var account = "insert-gugu";
-        final var user = new User(account, "password", "hkkang@woowahan.com");
-        userDao.insert(user);
+	@Test
+	void insert() {
+		final var account = "insert-gugu";
+		final var user = new User(account, "password", "hkkang@woowahan.com");
+		userDao.insert(user);
 
-        final var actual = userDao.findById(2L);
+		final var actual = userDao.findById(2L);
 
-        assertThat(actual.getAccount()).isEqualTo(account);
-    }
+		assertThat(actual.getAccount()).isEqualTo(account);
+	}
 
-    @Test
-    void update() {
-        final var newPassword = "password99";
-        final var user = userDao.findById(1L);
-        user.changePassword(newPassword);
+	@Test
+	void update() {
+		final var newPassword = "password99";
+		final var user = userDao.findById(1L);
+		user.changePassword(newPassword);
 
-        userDao.update(user);
+		userDao.update(user);
 
-        final var actual = userDao.findById(1L);
+		final var actual = userDao.findById(1L);
 
-        assertThat(actual.getPassword()).isEqualTo(newPassword);
-    }
+		assertThat(actual.getPassword()).isEqualTo(newPassword);
+	}
 }
