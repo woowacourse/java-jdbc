04193f9fe2dbb6555884609a4f43e8310640db38
Hongdonggeon <67885363+Hongdonggeon@users.noreply.github.com>
2022-10-08 21:45:01 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 쿤(홍동건) 미션 제출합니다. (#117)
    * docs: 1단계 기능 요구사항 추가
    
    * feat: findAll 메서드 구현
    
    * feat: findByAccount 메서드 구현
    
    * feat: update 메서드 구현
    
    * feat: JdbcTemplate update 메서드 추가
    
    insert, update, delete할 때 사용하는 메서드
    
    * feat: JdbcTemplate queryForObject 메서드 추가
    
    * feat: JdbcTemplate query 메서드 추가
    
    * refactor: RowMapper 상수화
    
    * refactor: rowmapper 바인드 칼럼 필드명으로 변경
    
    * refactor: 매직넘버 상수화

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -1,17 +1,69 @@
 package nextstep.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int FIRST_INDEX = 0;
+    private static final int PARAMETER_INDEX_INCREMENT = 1;
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (final Connection connection = getConnection();
+             final PreparedStatement preparedStatement = createPreparedStatement(connection, sql, args)) {
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error("error : {}", e);
+            throw new RuntimeException("해당 sql문을 실행할 수 없습니다.", e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = getConnection();
+             final PreparedStatement preparedStatement = createPreparedStatement(connection, sql, args);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            final List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet));
+            }
+            return results;
+        } catch (SQLException e) {
+            log.error("error : {}", e);
+            throw new RuntimeException("해당 sql문을 실행할 수 없습니다.", e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        final List<T> query = query(sql, rowMapper, args);
+        return query.get(FIRST_INDEX);
+    }
+
+    private Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    private PreparedStatement createPreparedStatement(final Connection connection, final String sql,
+                                                      final Object... args) throws SQLException {
+        final PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = FIRST_INDEX; i < args.length; i++) {
+            preparedStatement.setObject(i + PARAMETER_INDEX_INCREMENT, args[i]);
+        }
+        return preparedStatement;
     }
 }
