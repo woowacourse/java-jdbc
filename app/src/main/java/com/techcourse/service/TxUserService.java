60d0320559455cacd3a4cf982f131edb86cb84bf
chawani <chawani@naver.com>
2022-10-08 23:46:06 +0900

    feat: 트랜잭션 서비스 추상화 구현

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,40 @@
+package com.techcourse.service;
+
+import com.techcourse.domain.User;
+import nextstep.jdbc.exception.DataAccessException;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+public class TxUserService implements UserService {
+
+    private final PlatformTransactionManager transactionManager;
+    private final UserService userService;
+
+    public TxUserService(PlatformTransactionManager transactionManager, UserService userService) {
+        this.transactionManager = transactionManager;
+        this.userService = userService;
+    }
+
+    @Override
+    public User findById(long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void insert(User user) {
+        userService.insert(user);
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createBy) {
+        TransactionStatus transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            userService.changePassword(id, newPassword, createBy);
+            transactionManager.commit(transactionStatus);
+        } catch (DataAccessException e) {
+            transactionManager.rollback(transactionStatus);
+            throw e;
+        }
+    }
+}
