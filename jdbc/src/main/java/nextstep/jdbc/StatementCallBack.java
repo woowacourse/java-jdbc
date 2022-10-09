68556d69dbc98585a6e3eb045c349c9d1ad0c6b5
Wishoon <heonga2@gmail.com>
2022-10-09 13:15:14 +0900

    refactor: 클래스 이름 변경

--- jdbc/src/main/java/nextstep/jdbc/StatementCallBack.java
+++ jdbc/src/main/java/nextstep/jdbc/StatementCallBack.java
@@ -4,7 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 @FunctionalInterface
-public interface ExecuteStrategy<T> {
+public interface StatementCallBack<T> {
 
     T execute(final PreparedStatement pstmt) throws SQLException;
 }
