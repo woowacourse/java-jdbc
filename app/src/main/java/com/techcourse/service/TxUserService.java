e9fa2a1bff3065c93f2be6551a6e5d6a6d858d87
BeomsooKim <kbsat@naver.com>
2022-10-09 11:14:20 +0900

    refactor: TxUserService 내에서 RuntimeException을 처리하도록 수정

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -2,11 +2,15 @@ package com.techcourse.service;
 
 import com.techcourse.domain.User;
 import nextstep.jdbc.DataAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TxUserService implements UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(TxUserService.class);
 
     private final PlatformTransactionManager transactionManager;
     private final UserService userService;
@@ -34,9 +38,10 @@ public class TxUserService implements UserService {
         try {
             userService.changePassword(id, newPassword, createBy);
             transactionManager.commit(transactionStatus);
-        } catch (final DataAccessException e) {
+            log.info("tx commit!");
+        } catch (final RuntimeException e) {
             transactionManager.rollback(transactionStatus);
-            throw new DataAccessException();
+            throw new DataAccessException("tx rollback!",e);
         }
     }
 }
