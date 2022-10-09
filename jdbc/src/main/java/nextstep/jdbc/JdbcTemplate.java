3300db5d2991de634cd408c92c584cbf8c643108
brorae <jhr3239@gmail.com>
2022-10-09 17:16:49 +0900

    refactor: 사용하지 않는 메서드 제거

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -20,24 +20,28 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public int update(String sql, Object... args) {
-        return execute(sql, pstmt -> pstmt.executeUpdate(), args);
+    public int update(final String sql, final Object... args) {
+        return execute(sql, pstmt -> {
+            setSqlParameters(pstmt, args);
+            return pstmt.executeUpdate();
+        });
     }
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args)
-            throws DataAccessException {
-        return execute(sql, pstmt -> getResultSet(rowMapper, pstmt), args);
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        return execute(sql, pstmt -> {
+            setSqlParameters(pstmt, args);
+            return getResultSet(rowMapper, pstmt);
+        });
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        return execute(sql, pstmt -> getResult(rowMapper, pstmt.executeQuery()), args);
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
+        return execute(sql, pstmt -> getResult(rowMapper, pstmt.executeQuery()));
     }
 
-    private <T> T execute(final String sql, Executor<T> executor, Object[] args) {
+    private <T> T execute(final String sql, final Executor<T> executor) {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
             log.debug("query : {}", sql);
-            setSqlParameters(pstmt, args);
             return executor.execute(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -45,7 +49,7 @@ public class JdbcTemplate {
         }
     }
 
-    private void setSqlParameters(PreparedStatement pstmt, Object[] args) throws SQLException {
+    private void setSqlParameters(final PreparedStatement pstmt, final Object[] args) throws SQLException {
         for (int i = 0; i < args.length; i++) {
             if (args[i].getClass().equals(String.class)) {
                 pstmt.setString(i + 1, (String) args[i]);
@@ -55,7 +59,7 @@ public class JdbcTemplate {
         }
     }
 
-    private <T> T getResultSet(RowMapper<T> rowMapper, PreparedStatement pstmt) throws SQLException {
+    private <T> T getResultSet(final RowMapper<T> rowMapper, final PreparedStatement pstmt) throws SQLException {
         ResultSet rs = pstmt.executeQuery();
         if (rs.next()) {
             return rowMapper.mapRow(rs, 0);
@@ -63,7 +67,7 @@ public class JdbcTemplate {
         return null;
     }
 
-    private <T> List<T> getResult(RowMapper<T> rowMapper, ResultSet rs) throws SQLException {
+    private <T> List<T> getResult(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
         int rowNum = 0;
         List<T> result = new ArrayList<>();
         while (rs.next()) {
