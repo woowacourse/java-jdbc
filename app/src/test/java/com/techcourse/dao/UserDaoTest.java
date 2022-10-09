538b9ead2580abfcd1e5a31262fff4d527cd2122
Maru <60432062+chawani@users.noreply.github.com>
2022-10-09 18:35:34 +0900

    [JDBC 라이브러리 구현하기 - 2단계] 마루(송채원) 미션 제출합니다. (#139)
    * refactor: PreparedStatementSetter 함수형 인터페이스 정의 및 리팩터링
    
    * feat: 커스텀 예외 생성
    
    * refactor: RowMapperResultSetExtractor 객체 반환 개수에 따라 메서드 분리 및 커스텀 예외 적용
    
    * test: 테스트 코드 추가

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,24 +1,40 @@
 package com.techcourse.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import nextstep.jdbc.exception.EmptyResultDataAccessException;
+import nextstep.jdbc.exception.IncorrectResultSizeDataAccessException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class UserDaoTest {
 
+    private static final DataSource DATASOURCE = DataSourceConfig.getInstance();
     private UserDao userDao;
 
     @BeforeEach
     void setup() {
-        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
+        DatabasePopulatorUtils.execute(DATASOURCE);
 
-        userDao = new UserDao(DataSourceConfig.getInstance());
+        userDao = new UserDao(DATASOURCE);
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(user);
+    }
+
+    @AfterEach
+    void clear() throws SQLException {
+        Connection connection = DATASOURCE.getConnection();
+        Statement statement = connection.createStatement();
+        statement.execute("drop table if exists users");
     }
 
     @Test
@@ -65,5 +81,20 @@ class UserDaoTest {
         final var actual = userDao.findById(1L);
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
+    }
+
+    @Test
+    void incorrectResultSizeDataAccessException() {
+        final var user = new User("gugu", "password", "hkkang@woowahan.com");
+        userDao.insert(user);
+
+        assertThatThrownBy(() -> userDao.findByAccount("gugu"))
+                .isInstanceOf(IncorrectResultSizeDataAccessException.class);
+    }
+
+    @Test
+    void emptyResultDataAccessException() {
+        assertThatThrownBy(() -> userDao.findByAccount("gugu1"))
+                .isInstanceOf(EmptyResultDataAccessException.class);
     }
 }
--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -6,6 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
+import nextstep.jdbc.exception.DataAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,28 +22,23 @@ public class JdbcTemplate {
 
     public int update(String sql, Object... parameters) {
         return execute(sql, ps -> {
-            setPreparedStatement(ps, parameters);
+            getPreparedStatementSetter(parameters).setValues(ps);
             return ps.executeUpdate();
         });
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... parameters) {
         return execute(sql, ps -> {
-            setPreparedStatement(ps, parameters);
+            getPreparedStatementSetter(parameters).setValues(ps);
             ResultSet resultSet = ps.executeQuery();
-
-            List<T> extracted = RowMapperResultSetExtractor.extractData(resultSet, rowMapper);
-            return extracted.stream()
-                    .findFirst()
-                    .orElseThrow(DataAccessException::new);
+            return RowMapperResultSetExtractor.extractData(resultSet, rowMapper);
         });
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
         return execute(sql, ps -> {
             ResultSet resultSet = ps.executeQuery();
-
-            return RowMapperResultSetExtractor.extractData(resultSet, rowMapper);
+            return RowMapperResultSetExtractor.extractDataList(resultSet, rowMapper);
         });
     }
 
@@ -52,13 +48,15 @@ public class JdbcTemplate {
             return action.doInPreparedStatement(ps);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException("쿼리를 실행하는 데에 문제가 발생했습니다.");
         }
     }
 
-    private void setPreparedStatement(PreparedStatement ps, Object... parameters) throws SQLException {
-        for (int i = 1; i <= parameters.length; i++) {
-            ps.setObject(i, parameters[i - 1]);
-        }
+    private PreparedStatementSetter getPreparedStatementSetter(Object... parameters) {
+        return preparedStatement -> {
+            for (int i = 1; i <= parameters.length; i++) {
+                preparedStatement.setObject(i, parameters[i - 1]);
+            }
+        };
     }
 }
