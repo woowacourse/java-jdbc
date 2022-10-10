aa2e94c59ef90a502932bc94306eea0b8193c9be
her0807 <her0807@naver.com>
2022-10-11 03:01:20 +0900

    feat: userDao와 userHistoryDao를 한 트랜잭션으로 묶기

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -7,26 +7,26 @@ import com.techcourse.domain.UserHistory;
 
 public class UserService {
 
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
+	private final UserDao userDao;
+	private final UserHistoryDao userHistoryDao;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-    }
+	public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+		this.userDao = userDao;
+		this.userHistoryDao = userHistoryDao;
+	}
 
-    public User findById(final long id) {
-        return userDao.findById(id);
-    }
+	public User findById(final long id) {
+		return userDao.findById(id);
+	}
 
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
+	public void insert(final User user) {
+		userDao.insert(user);
+	}
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
-    }
+	public void changePassword(final long id, final String newPassword, final String createBy) {
+		final var user = findById(id);
+		user.changePassword(newPassword);
+		userDao.update(user);
+		userHistoryDao.log(new UserHistory(user, createBy));
+	}
 }
