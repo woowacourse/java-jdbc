c49fba462c7f0902be2eaa24602242c31c13c572
skullKim <kyk990328@gmail.com>
2022-10-10 15:28:41 +0900

    fix: 시간 관련 타입을 변환하지 못하는 문제 해결

--- jdbc/src/main/java/nextstep/jdbc/util/SqlArgumentConverter.java
+++ jdbc/src/main/java/nextstep/jdbc/util/SqlArgumentConverter.java
@@ -1,10 +1,12 @@
 package nextstep.jdbc.util;
 
+import java.time.temporal.Temporal;
+
 public class SqlArgumentConverter {
 
     public static String convertObjectToString(final Object object) {
-        if (object instanceof String) {
-            return '\'' + (String) object + '\'';
+        if ((object instanceof String) || (object instanceof Temporal)) {
+            return '\'' + object.toString() + '\'';
         }
         return String.valueOf(object);
     }
