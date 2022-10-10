d91560a62a0b6d26d974ee3734739fac0d07b247
her0807 <her0807@naver.com>
2022-10-11 04:16:07 +0900

    test: JdbcTemplateTest 추가

--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,93 @@
 package nextstep.jdbc;
 
+import static nextstep.jdbc.Extractor.extractData;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
 class JdbcTemplateTest {
+
+	private static final String FIND_BY_ACCOUNT = "SELECT * FROM users WHERE account = ?";
+	private static MockedStatic<Extractor> Extractor;
+
+	private JdbcTemplate jdbcTemplate;
+	private DataSource dataSource;
+	private Connection connection;
+	private PreparedStatement preparedStatement;
+	private ResultSet resultSet;
+
+	@BeforeEach
+	void setUp() throws SQLException {
+		this.dataSource = mock(DataSource.class);
+		this.connection = mock(Connection.class);
+		this.preparedStatement = mock(PreparedStatement.class);
+		this.resultSet = mock(ResultSet.class);
+
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+
+		given(dataSource.getConnection()).willReturn(connection);
+		given(connection.prepareStatement(anyString())).willReturn(preparedStatement);
+		given(preparedStatement.executeQuery()).willReturn(resultSet);
+	}
+
+	@BeforeAll
+	public static void beforeALl() {
+		Extractor = mockStatic(Extractor.class);
+	}
+
+	@Test
+	void 쿼리를_실행시켜_한_개의_엔티티를_반환한다() throws SQLException {
+		String findAccount = "깜찍_수달";
+		// given
+		Extractor.when(() -> extractData(any(), any()))
+			.thenReturn(Arrays.asList(new User(1L, findAccount, "pw", "sudal@naver.com")));
+
+		final User user = jdbcTemplate.queryForObject(FIND_BY_ACCOUNT, User.class, findAccount);
+
+		// then
+		assertThat(user).extracting("account").isEqualTo(findAccount);
+	}
+
+	@Test
+	void 쿼리를_실행시켜_한_개의_데이터를_찾아오는데_결과가_없는_경우_예외가_발생한다() throws SQLException {
+		// given
+		Extractor.when(() -> extractData(any(), any()))
+			.thenReturn(Arrays.asList());
+
+		// when, then
+		assertThatThrownBy(() -> jdbcTemplate.queryForObject(FIND_BY_ACCOUNT, User.class,"안깜찍_수달"))
+			.isInstanceOf(DataAccessException.class)
+			.hasMessage("일치하는 데이터가 없습니다.");
+	}
+
+	@Test
+	void 쿼리를_실행시켜_한_개의_데이터를_찾아오는데_결과가_두_개_이상일_경우_예외가_발생한다() throws SQLException {
+		// given
+		Extractor.when(() -> extractData(any(), any()))
+			.thenReturn(Arrays.asList(new User(1L, "데이터1", "pw", "sudal@naver.com"),
+				new User(2L, "데이터2", "pw", "sudal@naver.com")));
+
+		// when, then
+		assertThatThrownBy(() -> jdbcTemplate.queryForObject(FIND_BY_ACCOUNT, User.class))
+			.isInstanceOf(DataAccessException.class)
+			.hasMessage("조회 데이터 갯수가 2 입니다.");
+	}
 
 }
---
+++ jdbc/src/test/java/nextstep/jdbc/User.java
@@ -0,0 +1,60 @@
+package nextstep.jdbc;
+
+public class User {
+
+	private Long id;
+	private String account;
+	private String password;
+	private String email;
+
+	public User(Long id, String account, String password, String email) {
+		this.id = id;
+		this.account = account;
+		this.password = password;
+		this.email = email;
+	}
+
+
+	public User(String account, String password, String email) {
+		this.account = account;
+		this.password = password;
+		this.email = email;
+	}
+
+	public User() {
+	}
+
+	public boolean checkPassword(String password) {
+		return this.password.equals(password);
+	}
+
+	public void changePassword(String password) {
+		this.password = password;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String toString() {
+		return "User{" +
+			"id=" + id +
+			", account='" + account + '\'' +
+			", email='" + email + '\'' +
+			", password='" + password + '\'' +
+			'}';
+	}
+}
