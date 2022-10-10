3b12d6a13fc35d9925a8f5a84f9db301bea5aaf5
corinne <yoo77hyeon@gmail.com>
2022-10-10 14:48:31 +0900

    refactor: changePassword에 트랜잭션 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,16 +1,23 @@
 package com.techcourse.service;
 
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import javax.sql.DataSource;
+import nextstep.jdbc.exception.DataAccessException;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource;
 
     public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+        this.dataSource = DataSourceConfig.getInstance();
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
     }
@@ -24,9 +31,17 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        final var transactionManager = new DataSourceTransactionManager(dataSource);
+        final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
+            transactionManager.commit(transactionStatus);
+        } catch (DataAccessException e) {
+            transactionManager.rollback(transactionStatus);
+            throw new DataAccessException();
+        }
     }
 }
