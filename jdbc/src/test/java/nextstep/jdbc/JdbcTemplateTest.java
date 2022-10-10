da19e2402ce05ce9196a2d24962ae684fc2ecb45
Kim Seunglae <obey1342@gmail.com>
2022-10-10 18:51:47 +0900

    refactor: 사용하지 않는 Import 제거

--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,7 +1,6 @@
 package nextstep.jdbc;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import nextstep.support.Crew;
