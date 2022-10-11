2fb5191768374b36da6f462886172b3dd3e0905f
SuyeonChoi <tndus970120@gmail.com>
2022-10-11 13:54:01 +0900

    test(AppUserService): 테스트 추가

---
+++ app/src/test/java/com/techcourse/service/AppUserServiceTest.java
@@ -0,0 +1,42 @@
+package com.techcourse.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.dao.UserDao;
+import com.techcourse.dao.UserHistoryDao;
+import com.techcourse.domain.User;
+import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import nextstep.jdbc.JdbcTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AppUserServiceTest {
+
+    private JdbcTemplate jdbcTemplate;
+    private UserDao userDao;
+
+    @BeforeEach
+    void setUp() {
+        this.jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
+        this.userDao = new UserDao(jdbcTemplate);
+
+        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
+        final var user = new User("gugu", "password", "hkkang@woowahan.com");
+        userDao.insert(user);
+    }
+
+    @Test
+    void testChangePassword() {
+        final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
+        final var userService = new AppUserService(userDao, userHistoryDao);
+
+        final var newPassword = "qqqqq";
+        final var createBy = "gugu";
+        userService.changePassword(1L, newPassword, createBy);
+
+        final var actual = userService.findById(1L);
+
+        assertThat(actual.getPassword()).isEqualTo(newPassword);
+    }
+}
