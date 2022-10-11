df2daebf87b1a0dd701f7d7d02ca73484b169b34
yxxnghwan <younghwan960@gmail.com>
2022-10-11 11:36:38 +0900

    refactor: UserHistoryDao 변경

--- app/src/test/java/com/techcourse/service/UserServiceTest.java
+++ app/src/test/java/com/techcourse/service/UserServiceTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 class UserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
