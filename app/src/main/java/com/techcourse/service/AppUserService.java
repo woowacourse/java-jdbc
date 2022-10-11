93123a9c2ddd2f4cdb1cb824894bd403757e96cb
Kim Seunglae <obey1342@gmail.com>
2022-10-11 12:05:35 +0900

    refactor: UserServcie 인터페이스 분리

--- app/src/main/java/com/techcourse/service/AppUserService.java
+++ app/src/main/java/com/techcourse/service/AppUserService.java
@@ -5,24 +5,27 @@ import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 
-public class UserService {
+public class AppUserService implements UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public AppUserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
     }
 
+    @Override
     public User findById(final long id) {
         return userDao.findById(id);
     }
 
+    @Override
     public void insert(final User user) {
         userDao.insert(user);
     }
 
+    @Override
     public void changePassword(final long id, final String newPassword, final String createBy) {
         final var user = findById(id);
         user.changePassword(newPassword);
