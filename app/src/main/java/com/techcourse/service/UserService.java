91c4e13bb89d6670f59b42caed9745322b2e5928
injoon2019 <ison@sfu.ca>
2022-10-11 13:14:27 +0900

    feat: 트랜잭션 기능 구현

---
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -0,0 +1,10 @@
+package com.techcourse.service;
+
+import com.techcourse.domain.User;
+
+public interface UserService {
+
+    User findById(final long id);
+    void insert(final User user);
+    void changePassword(final long id, final String newPassword, final String createBy);
+}
--- app/src/test/java/com/techcourse/service/AppUserServiceTest.java
+++ app/src/test/java/com/techcourse/service/AppUserServiceTest.java
@@ -8,14 +8,13 @@ import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 import nextstep.jdbc.DataAccessException;
 import nextstep.jdbc.JdbcTemplate;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled
-class UserServiceTest {
+class AppUserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
     private UserDao userDao;
@@ -32,8 +31,12 @@ class UserServiceTest {
 
     @Test
     void testChangePassword() {
+        // 트랜잭션 롤백 테스트를 위해 mock으로 교체
         final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        // 애플리케이션 서비스
+        UserService appUserService = new AppUserService(userDao, userHistoryDao);
+        // 트랜잭션 서비스 추상화
+        final var userService = new TxUserService(new DataSourceTransactionManager(DataSourceConfig.getInstance()), appUserService);
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -48,7 +51,7 @@ class UserServiceTest {
     void testTransactionRollback() {
         // 트랜잭션 롤백 테스트를 위해 mock으로 교체
         final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var userService = new AppUserService(userDao, userHistoryDao);
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
