e83f3e2f90ca382c7a876a1d5e9beb5879f70fa9
Donggyu <a29661498@gmail.com>
2022-10-12 08:48:14 +0900

    refactor: 비즈니스 로직에 대한 테스트와 트랜잭션에 대한 테스트 분리

--- app/src/test/java/com/techcourse/service/AppUserServiceTest.java
+++ app/src/test/java/com/techcourse/service/AppUserServiceTest.java
@@ -1,18 +1,18 @@
 package com.techcourse.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
+import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 import nextstep.jdbc.JdbcTemplate;
-import nextstep.jdbc.exception.DataAccessException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class TxUserServiceTest {
+class AppUserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
     private UserDao userDao;
@@ -28,20 +28,17 @@ class TxUserServiceTest {
     }
 
     @Test
-    void testTransactionRollback() {
-        // 트랜잭션 롤백 테스트를 위해 mock으로 교체
-        final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
+    void testChangePassword() {
+        final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
         final AppUserService appUserService = new AppUserService(userDao, userHistoryDao);
         final var userService = new TxUserService(appUserService);
 
-        final var newPassword = "newPassword";
+        final var newPassword = "qqqqq";
         final var createBy = "gugu";
-        // 트랜잭션이 정상 동작하는지 확인하기 위해 의도적으로 MockUserHistoryDao에서 예외를 발생시킨다.
-        assertThrows(DataAccessException.class,
-                () -> userService.changePassword(1L, newPassword, createBy));
+        userService.changePassword(1L, newPassword, createBy);
 
         final var actual = userService.findById(1L);
 
-        assertThat(actual.getPassword()).isNotEqualTo(newPassword);
+        assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
 }
