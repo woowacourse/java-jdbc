71e6dd6337089708c78e26a1cb9b52caba1af117
Juhyung990122 <harukawa99@naver.com>
2022-10-11 12:49:30 +0900

    style: 미사용 import 문 제거

--- app/src/test/java/com/techcourse/service/AppUserServiceTest.java
+++ app/src/test/java/com/techcourse/service/AppUserServiceTest.java
@@ -13,7 +13,6 @@ import nextstep.jdbc.JdbcTemplate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 
 class AppUserServiceTest {
 
