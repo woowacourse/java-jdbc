30c716ea7e73879d5c58654590cdaa1538883a4d
Juhyung990122 <harukawa99@naver.com>
2022-10-11 12:14:15 +0900

    feat: 트랜잭션 서비스 구현
    Signed-off-by: Juhyung990122 <harukawa99@naver.com>

--- app/src/main/java/com/techcourse/service/AppUserService.java
+++ app/src/main/java/com/techcourse/service/AppUserService.java
@@ -15,15 +15,18 @@ public class AppUserService implements UserService {
         this.userHistoryDao = userHistoryDao;
     }
 
+    @Override
     public User findById(final long id) {
         return userDao.findById(id);
     }
 
+    @Override
     public void insert(final User user) {
         userDao.insert(user);
     }
 
 
+    @Override
     public void changePassword(final long id, final String newPassword, final String createBy) {
         final var user = findById(id);
         user.changePassword(newPassword);
--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -2,22 +2,51 @@ package com.techcourse.service;
 
 import com.techcourse.domain.User;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
-public class TxUserService implements UserService{
+public class TxUserService implements UserService {
+
+    private final PlatformTransactionManager transactionManager;
+    private final AppUserService appUserService;
+
+    public TxUserService(final PlatformTransactionManager transactionManager,
+                         final AppUserService appUserService) {
+        this.transactionManager = transactionManager;
+        this.appUserService = appUserService;
+    }
 
     @Override
-    public User findById(long id) {
-        return null;
+    public User findById(final long id) {
+        return appUserService.findById(id);
     }
 
     @Override
     public void insert(User user) {
-
+        execute(() -> appUserService.insert(user));
     }
 
     @Override
-    public void changePassword(long id, String newPassword, String createBy) {
-
+    public void changePassword(final long id, final String newPassword, final String createBy) {
+        execute(() -> appUserService.changePassword(
+                id,
+                newPassword,
+                createBy
+        ));
     }
 
+    private void execute(final Runnable runnable) {
+        final TransactionStatus transactionStatus = transactionManager.getTransaction(
+                new DefaultTransactionDefinition());
+
+        try {
+            runnable.run();
+            transactionManager.commit(transactionStatus);
+        } catch (final RuntimeException e) {
+            transactionManager.rollback(transactionStatus);
+            throw e;
+        } catch (final Exception e) {
+            transactionManager.commit(transactionStatus);
+        }
+    }
 }
