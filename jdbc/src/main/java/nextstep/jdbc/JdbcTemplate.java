dc3d57d718e7a2a9aa6dbceac426871f1bc2d278
hyewoncc <hyewoncc@gmail.com>
2022-10-12 11:52:03 +0900

    refactor: 메서드 추출 및 선언 순서 변경

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -22,16 +22,30 @@ public class JdbcTemplate {
 
     public <T> List<T> query(final String sql, final ObjectMapper<T> objectMapper) {
         return executeQuery(sql, statement -> {
-            ResultSet resultSet = statement.executeQuery();
             log.debug("query : {}", sql);
+            ResultSet resultSet = statement.executeQuery();
 
-            List<T> results = new ArrayList<>();
-            if (resultSet.next()) {
-                T t = objectMapper.map(resultSet);
-                results.add(t);
-            }
-            return results;
+            return convertRows(resultSet, objectMapper);
         });
+    }
+
+    private <T> T executeQuery(final String sql, final Executor<T> executor) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            return executor.execute(statement);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    private <T> List<T> convertRows(final ResultSet resultSet, final ObjectMapper<T> objectMapper) throws SQLException {
+        List<T> results = new ArrayList<>();
+        if (resultSet.next()) {
+            T t = objectMapper.map(resultSet);
+            results.add(t);
+        }
+        return results;
     }
 
     public <T> T query(final String sql, final Object parameter, final ObjectMapper<T> objectMapper) {
@@ -39,43 +53,40 @@ public class JdbcTemplate {
         return executeQuery(sql, statement -> {
             statement.setObject(1, parameter);
 
-            ResultSet resultSet = statement.executeQuery();
             log.debug("query : {}", sql);
+            ResultSet resultSet = statement.executeQuery();
 
-            if (resultSet.next()) {
-                return objectMapper.map(resultSet);
-            }
-            throw new DataAccessException("조회 결과가 존재하지 않습니다.");
+            return convertRow(resultSet, objectMapper);
         });
+    }
+
+    private <T> T convertRow(final ResultSet resultSet, final ObjectMapper<T> objectMapper) throws SQLException {
+        if (resultSet.next()) {
+            return objectMapper.map(resultSet);
+        }
+        throw new DataAccessException("조회 결과가 존재하지 않습니다.");
     }
 
     public void update(final String sql, final Object... parameters) {
         validateSql(sql, parameters);
         executeQuery(sql, statement -> {
-            for (int i = 0; i < parameters.length; i++) {
-                statement.setObject(i + 1, parameters[i]);
-            }
+            setParams(statement, parameters);
 
             log.debug("query : {}", sql);
             return statement.executeUpdate();
         });
     }
 
+    private void setParams(final PreparedStatement statement, final Object[] parameters) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            statement.setObject(i + 1, parameters[i]);
+        }
+    }
+
     private void validateSql(final String sql, final Object... parameters) {
         int required = sql.length() - sql.replaceAll("\\?", "").length();
         if (required != parameters.length) {
             throw new DataAccessException("sql문 내의 매개변수와 주어진 매개변수의 수가 다릅니다.");
-        }
-    }
-
-    private <T> T executeQuery(final String sql, final Executor<T> executor) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)) {
-
-            return executor.execute(statement);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e.getMessage(), e);
         }
     }
 }
