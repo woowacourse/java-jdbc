42de9b66ac9c0fe2bab46c4e400c4df6b3dd2e7d
skullKim <kyk990328@gmail.com>
2022-10-13 17:16:05 +0900

    feat: SqlArgumentConverter에 검증 로직 추가

--- jdbc/src/main/java/nextstep/jdbc/util/SqlArgumentConverter.java
+++ jdbc/src/main/java/nextstep/jdbc/util/SqlArgumentConverter.java
@@ -4,10 +4,22 @@ import java.time.temporal.Temporal;
 
 public class SqlArgumentConverter {
 
+    public static final String INVALID_TYPE = "Cannot convert Object to String.";
+
     public static String convertObjectToString(final Object object) {
+        validateTargetObjectType(object);
         if ((object instanceof String) || (object instanceof Temporal)) {
             return '\'' + object.toString() + '\'';
         }
         return String.valueOf(object);
+    }
+
+    private static void validateTargetObjectType(final Object target) {
+        if (!((target instanceof Short) || (target instanceof Integer) || (target instanceof Long)
+                ||(target instanceof Double) || (target instanceof Float) || (target instanceof Boolean)
+                || (target instanceof String) || (target instanceof Temporal) || (target instanceof Character)
+                || (target instanceof Byte))) {
+            throw new IllegalArgumentException(INVALID_TYPE);
+        }
     }
 }
