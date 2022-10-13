c08ffea8f19dc8439cbfc1c82fc654f2de904114
yeongunheo <gjduddns@gmail.com>
2022-10-13 21:08:11 +0900

    refactor: sql문에 fianl 키워드 추가

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -2,12 +2,8 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.UserHistory;
 import nextstep.jdbc.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UserHistoryDao {
-
-    private static final Logger log = LoggerFactory.getLogger(UserHistoryDao.class);
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -16,7 +12,7 @@ public class UserHistoryDao {
     }
 
     public void log(UserHistory userHistory) {
-        String sql = "INSERT INTO user_history (user_id, account, password, email, created_at, created_by) VALUES (?, ?, ?, ?, ?, ?)";
+        final String sql = "INSERT INTO user_history (user_id, account, password, email, created_at, created_by) VALUES (?, ?, ?, ?, ?, ?)";
         jdbcTemplate.executeUpdate(sql,
                 userHistory.getUserId(),
                 userHistory.getAccount(),
