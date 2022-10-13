2a26151fea9661907ae46ee37ae0e3716982a608
Gyuchool <balance0591@naver.com>
2022-10-13 12:19:00 +0900

    refactor: delete unused method

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,7 +1,6 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.sql.Connection;
 import java.util.List;
 import javax.sql.DataSource;
 import nextstep.jdbc.JdbcTemplate;
@@ -12,7 +11,7 @@ import org.slf4j.LoggerFactory;
 public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
-    static RowMapper<User> userMapper = (rs) -> new User(rs.getLong("id"),
+    private final static RowMapper<User> USER_ROW_MAPPER = (rs) -> new User(rs.getLong("id"),
             rs.getString("account"),
             rs.getString("password"),
             rs.getString("email"));
@@ -38,25 +37,20 @@ public class UserDao {
         jdbcTemplate.update(sql, user.getAccount(), user.getEmail(), user.getPassword(), user.getId());
     }
 
-    public void update(Connection connection, final User user) {
-        final var sql = "update users set account = ?, email = ?, password = ? where id = ?";
-        jdbcTemplate.update(connection, sql, user.getAccount(), user.getEmail(), user.getPassword(), user.getId());
-    }
-
     public List<User> findAll() {
         final var sql = "select id, account, email, password from users";
-        return jdbcTemplate.query(sql, userMapper);
+        return jdbcTemplate.query(sql, USER_ROW_MAPPER);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        return jdbcTemplate.queryForObject(sql, userMapper, id);
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
 
-        return jdbcTemplate.queryForObject(sql, userMapper, account);
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, account);
     }
 }
