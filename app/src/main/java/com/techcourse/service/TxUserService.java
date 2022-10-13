786cc66d091b783d686fda4c9deabc449a2fbf69
dongho <dongho108@naver.com>
2022-10-13 13:41:16 +0900

    refactor: Transaction 관련로직 분리

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -1,55 +1,38 @@
 package com.techcourse.service;
 
 import com.techcourse.domain.User;
+import nextstep.jdbc.support.TransactionTemplate;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TxUserService implements UserService {
 
-    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate transactionTemplate;
     private final UserService userService;
 
     public TxUserService(final PlatformTransactionManager transactionManager,
                          final UserService userService) {
-        this.transactionManager = transactionManager;
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
         this.userService = userService;
     }
 
     @Override
     public User findById(final long id) {
-        final TransactionStatus transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
-            final User user = userService.findById(id);
-            transactionManager.commit(transactionStatus);
-            return user;
-        } catch (Exception exception) {
-            transactionManager.rollback(transactionStatus);
-            throw exception;
-        }
+        return transactionTemplate.doTransaction(() -> userService.findById(id));
     }
 
     @Override
     public void insert(final User user) {
-        final TransactionStatus transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
+        transactionTemplate.doTransaction(() -> {
             userService.insert(user);
-            transactionManager.commit(transactionStatus);
-        } catch (Exception exception) {
-            transactionManager.rollback(transactionStatus);
-            throw exception;
-        }
+            return null;
+        });
     }
 
     @Override
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final TransactionStatus transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
-        try {
+        transactionTemplate.doTransaction(() -> {
             userService.changePassword(id, newPassword, createBy);
-            transactionManager.commit(transactionStatus);
-        } catch (Exception exception) {
-            transactionManager.rollback(transactionStatus);
-            throw exception;
-        }
+            return null;
+        });
     }
 }
