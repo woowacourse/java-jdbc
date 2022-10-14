f66860065f0f28c043ad878f44d94a8d4ea6562f
Dongseok Shin <70707629+shindong96@users.noreply.github.com>
2022-10-14 14:40:51 +0900

    [JDBC 라이브러리 구현하기 - 3단계] 아스피(신동석) 미션 제출합니다. (#224)
    * feat: 트랜잭션 적용
    
    * docs: 3단계 요구사항 README 추가
    
    * refactor: 롤백 후 예외는 그대로 던지게 변경
    
    * refactor: 변수 타입을 명시

--- jdbc/src/main/java/nextstep/jdbc/DataAccessException.java
+++ jdbc/src/main/java/nextstep/jdbc/DataAccessException.java
@@ -8,18 +8,6 @@ public class DataAccessException extends RuntimeException {
         super();
     }
 
-    public DataAccessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
-
-    public DataAccessException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public DataAccessException(String message) {
-        super(message);
-    }
-
     public DataAccessException(Throwable cause) {
         super(cause);
     }
