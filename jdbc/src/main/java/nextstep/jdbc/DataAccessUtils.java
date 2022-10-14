2e50588d28774825b636d11f07e3c5e89830dada
Wishoon <heonga2@gmail.com>
2022-10-14 13:46:51 +0900

    refactor: Custom 예외를 통한 예외처리로 변경

--- jdbc/src/main/java/nextstep/jdbc/DataAccessUtils.java
+++ jdbc/src/main/java/nextstep/jdbc/DataAccessUtils.java
@@ -1,6 +1,8 @@
 package nextstep.jdbc;
 
 import java.util.List;
+import nextstep.jdbc.exception.InvalidSizeResultException;
+import nextstep.jdbc.exception.NotFoundResultException;
 
 public class DataAccessUtils {
 
@@ -9,10 +11,10 @@ public class DataAccessUtils {
 
     public static <T> T nullableSingleResult(final List<T> results) {
         if (results.isEmpty()) {
-            throw new DataAccessException("Result is empty");
+            throw new NotFoundResultException("Result is empty");
         }
         if (results.size() > 1) {
-            throw new DataAccessException("Result size is not only one");
+            throw new InvalidSizeResultException("Result size is not only one");
         }
         return results.get(0);
     }
---
+++ jdbc/src/main/java/nextstep/jdbc/exception/InvalidSizeResultException.java
@@ -0,0 +1,8 @@
+package nextstep.jdbc.exception;
+
+public class InvalidSizeResultException extends RuntimeException {
+
+    public InvalidSizeResultException(final String message) {
+        super(message);
+    }
+}
