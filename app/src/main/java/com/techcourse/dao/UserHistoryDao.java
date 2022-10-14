92934da4d116ad888f594fb5f8820ace2ba5ab9d
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2022-10-14 15:49:50 +0900

    refactor: 미사용 logger 제거

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -2,12 +2,8 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.UserHistory;
 import nextstep.jdbc.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UserHistoryDao {
-
-    private static final Logger log = LoggerFactory.getLogger(UserHistoryDao.class);
 
     private final JdbcTemplate jdbcTemplate;
 
