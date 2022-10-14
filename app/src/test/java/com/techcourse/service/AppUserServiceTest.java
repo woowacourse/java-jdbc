1c80347f14a2713c4fdba8462c5bdbedda28aea6
Youngyoon-1 <76875654+Youngyoon-1@users.noreply.github.com>
2022-10-14 16:59:10 +0900

    [JDBC 라이브러리 구현하기 - 3단계] 칙촉(안영윤) 미션 제출합니다. (#208)
    * feat: JdbcTemplate 에 템플릿 콜백 패턴을 사용해서 중복 로직 제거
    
    * refactor: checkedException 인 SqlException 을 uncheckedException 인 DataAccessException 으로 변경
    
    * test: 테스트 케이스 추가 및 리팩토링
    
    * docs: README 파일 수정
    
    * docs: 테스트 격리를 위한 DDL 추가
    
    * test: 테이스 케이스 추가
    
    * refactor: 변수명 수정
    
    * refactor: 메서드명 수정
    
    * refactor: ResultSet 에 try-with-resources 적용
    
    * refactor: DataSource 대신 JdbcTemplate 사용하도록 리팩토링
    
    * feat: User 비밀번호 변경 기능에 트랜잭션 적용
    
    * feat: TxUserService 에 트랜잭션 처리 로직 분리
    
    * docs: 리드미 수정
    
    * fix: 롤백을 실행하는 예외 조건 변경

---
+++ app/src/test/java/com/techcourse/service/AppUserServiceTest.java
@@ -0,0 +1,97 @@
+package com.techcourse.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.dao.UserDao;
+import com.techcourse.dao.UserHistoryDao;
+import com.techcourse.domain.User;
+import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import nextstep.jdbc.DataAccessException;
+import nextstep.jdbc.JdbcTemplate;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+
+class AppUserServiceTest {
+
+    private JdbcTemplate jdbcTemplate;
+    private UserDao userDao;
+
+    @BeforeEach
+    void setUp() {
+        this.jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
+        this.userDao = new UserDao(jdbcTemplate);
+
+        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
+        final var user = new User("gugu", "password", "hkkang@woowahan.com");
+        userDao.insert(user);
+    }
+
+    @Test
+    void testChangePassword() {
+        final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
+        final var userService = new AppUserService(userDao, userHistoryDao);
+
+        final var newPassword = "qqqqq";
+        final var createBy = "gugu";
+        userService.changePassword(1L, newPassword, createBy);
+
+        final var actual = userService.findById(1L);
+
+        assertThat(actual.getPassword()).isEqualTo(newPassword);
+    }
+
+    @Test
+    void testTransactionRollback() {
+        // 트랜잭션 롤백 테스트를 위해 mock으로 교체
+        final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
+        // 애플리케이션 서비스
+        final var appUserService = new AppUserService(userDao, userHistoryDao);
+        // 트랜잭션 서비스 추상화
+        final var transactionManager = new DataSourceTransactionManager(jdbcTemplate.getDataSource());
+        final var userService = new TxUserService(transactionManager, appUserService);
+
+        final var newPassword = "newPassword";
+        final var createBy = "gugu";
+        // 트랜잭션이 정상 동작하는지 확인하기 위해 의도적으로 MockUserHistoryDao에서 예외를 발생시킨다.
+        assertThrows(DataAccessException.class,
+                () -> userService.changePassword(1L, newPassword, createBy));
+
+        final var actual = userService.findById(1L);
+
+        assertThat(actual.getPassword()).isNotEqualTo(newPassword);
+    }
+
+    @Test
+    void AppUserService_에서_로직_수행중_예외가_발생하면_롤백이_호출된다() {
+        // given
+        final var transactionManager = mock(PlatformTransactionManager.class);
+        final var userService = mock(UserService.class);
+        final var txUserService = new TxUserService(transactionManager, userService);
+        final var transactionStatus = mock(TransactionStatus.class);
+        given(transactionManager.getTransaction(any()))
+                .willReturn(transactionStatus);
+        willThrow(new RuntimeException()).given(userService)
+                .changePassword(anyLong(), anyString(), anyString());
+
+        // when
+        try {
+            txUserService.changePassword(1L, "1", "1");
+        } catch (RuntimeException ignored) {
+        }
+
+        // then
+        verify(transactionManager).rollback(transactionStatus);
+    }
+}
