7aaa5fc80dd9777f180a120d04f7aa686b41a4cb
bcc0830 <bcc0830@bithub.com>
2022-10-15 13:56:44 +0900

    refactor : UserJdbcTemplate 삭제

--- jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
+++ jdbc/src/main/java/nextstep/jdbc/JdbcTemplate.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class JdbcTemplate {
+public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
