496e06ca9b7e9e337c6d02aeb639828871d42472
bcc0830 <bcc0830@bithub.com>
2022-10-15 15:02:56 +0900

    refactor : 중복 코드 수정

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -1,26 +1,26 @@
 package com.techcourse.service;
 
-import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import nextstep.jdbc.DataAccessException;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TxUserService implements UserService {
 
     private final PlatformTransactionManager transactionManager;
     private final UserService userService;
+    private final TransactionStatus transactionStatus;
+
 
     public TxUserService(PlatformTransactionManager transactionManager, UserService userService) {
         this.transactionManager = transactionManager;
         this.userService = userService;
+        this.transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
     }
 
     @Override
     public User findById(long id) {
-        final var transactionManager = new DataSourceTransactionManager(DataSourceConfig.getInstance());
-        final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
             User user = userService.findById(id);
             transactionManager.commit(transactionStatus);
@@ -35,8 +35,6 @@ public class TxUserService implements UserService {
 
     @Override
     public void insert(User user) {
-        final var transactionManager = new DataSourceTransactionManager(DataSourceConfig.getInstance());
-        final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
             userService.insert(user);
             transactionManager.commit(transactionStatus);
@@ -49,8 +47,6 @@ public class TxUserService implements UserService {
 
     @Override
     public void changePassword(long id, String newPassword, String createBy) {
-        final var transactionManager = new DataSourceTransactionManager(DataSourceConfig.getInstance());
-        final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
             userService.changePassword(id, newPassword, createBy);
             transactionManager.commit(transactionStatus);
