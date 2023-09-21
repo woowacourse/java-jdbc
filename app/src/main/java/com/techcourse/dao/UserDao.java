4736be960c789f78b1d990e8a7024ef800a55de5
kang-hyungu <hkkang@woowahan.com>
2023-09-21 14:11:58 +0900

    패키지 위치 변경 및 코드 정리

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -3,12 +3,17 @@ package com.techcourse.dao;
 import com.techcourse.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
 import java.util.List;
 
 public class UserDao {
+
+    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
     private final JdbcTemplate jdbcTemplate;
 
