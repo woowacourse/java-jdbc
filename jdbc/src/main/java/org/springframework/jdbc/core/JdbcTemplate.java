e4f4b393af30040dac10f8af4bb059703e74a306
greeng00se <thegreengoose7@gmail.com>
2023-09-26 17:55:38 +0900

    feat: try-with-resources 적용

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -21,11 +21,8 @@ public class JdbcTemplate {
     }
 
     public void update(final String sql, final Object... parameters) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(sql, connection, parameters)) {
             log.debug("query : {}", sql);
             for (int i = 0; i < parameters.length; i++) {
                 preparedStatement.setObject(i + 1, parameters[i]);
@@ -34,36 +31,14 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally{
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException ignored) {
-            }
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-
-            for (int i = 0; i < parameters.length; i++) {
-                preparedStatement.setObject(i + 1, parameters[i]);
-            }
-            resultSet = preparedStatement.executeQuery();
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(sql, connection, parameters);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
             log.debug("query : {}", sql);
-
             if (resultSet.next()) {
                 return rowMapper.mapRow(resultSet);
             }
@@ -71,44 +46,26 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
+    private PreparedStatement getPreparedStatement(
+            final String sql,
+            final Connection connection,
+            final Object[] parameters
+    ) throws SQLException {
+        final PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < parameters.length; i++) {
+            preparedStatement.setObject(i + 1, parameters[i]);
+        }
+        return preparedStatement;
+    }
+
     public <T> List<T> queryForList(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
-
-            for (int i = 0; i < parameters.length; i++) {
-                preparedStatement.setObject(i + 1, parameters[i]);
-            }
-            resultSet = preparedStatement.executeQuery();
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(sql, connection, parameters);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
             log.debug("query : {}", sql);
-
             final List<T> result = new ArrayList<>();
             while (resultSet.next()) {
                 result.add(rowMapper.mapRow(resultSet));
@@ -117,27 +74,6 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally{
-            try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 }
