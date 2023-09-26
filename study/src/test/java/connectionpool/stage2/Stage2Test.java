e69a2eab921b3794c6939fa6c3e724d68bf96a89
70825 <hello70825@gmail.com>
2023-09-26 11:04:38 +0900

    refactor: stage3 코드 정리

--- study/src/test/java/connectionpool/stage2/Stage2Test.java
+++ study/src/test/java/connectionpool/stage2/Stage2Test.java
@@ -1,20 +1,19 @@
 package connectionpool.stage2;
 
+import static com.zaxxer.hikari.util.UtilityElf.quietlySleep;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.zaxxer.hikari.HikariDataSource;
 import com.zaxxer.hikari.pool.HikariPool;
 import connectionpool.DataSourceConfig;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.sql.DataSource;
-import java.lang.reflect.Field;
-import java.sql.Connection;
-
-import static com.zaxxer.hikari.util.UtilityElf.quietlySleep;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Stage2Test {
@@ -22,10 +21,9 @@ class Stage2Test {
     private static final Logger log = LoggerFactory.getLogger(Stage2Test.class);
 
     /**
-     * spring boot에서 설정 파일인 application.yml를 사용하여 DataSource를 설정할 수 있다.
-     * 하지만 DataSource를 여러 개 사용하거나 세부 설정을 하려면 빈을 직접 생성하는 방법을 사용한다.
-     * DataSourceConfig 클래스를 찾아서 어떻게 빈으로 직접 생성하는지 확인해보자.
-     * 그리고 아래 DataSource가 직접 생성한 빈으로 주입 받았는지 getPoolName() 메서드로 확인해보자.
+     * spring boot에서 설정 파일인 application.yml를 사용하여 DataSource를 설정할 수 있다. <br/> 하지만 DataSource를 여러 개 사용하거나 세부 설정을 하려면 빈을
+     * 직접 생성하는 방법을 사용한다. <br/> DataSourceConfig 클래스를 찾아서 어떻게 빈으로 직접 생성하는지 확인해보자. <br/> 그리고 아래 DataSource가 직접 생성한 빈으로 주입
+     * 받았는지 getPoolName() 메서드로 확인해보자. <br/>
      */
     @Autowired
     private DataSource dataSource;
@@ -36,7 +34,7 @@ class Stage2Test {
         final var hikariPool = getPool((HikariDataSource) dataSource);
 
         // 설정한 커넥션 풀 최대값보다 더 많은 스레드를 생성해서 동시에 디비에 접근을 시도하면 어떻게 될까?
-        final var threads = new Thread[20];
+        final var threads = new Thread[40];
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread(getConnection());
         }
@@ -71,8 +69,7 @@ class Stage2Test {
     }
 
     // 학습 테스트를 위해 HikariPool을 추출
-    public static HikariPool getPool(final HikariDataSource hikariDataSource)
-    {
+    public static HikariPool getPool(final HikariDataSource hikariDataSource) {
         try {
             Field field = hikariDataSource.getClass().getDeclaredField("pool");
             field.setAccessible(true);
