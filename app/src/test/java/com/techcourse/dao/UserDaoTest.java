ce094121c4978bd9d62494c7cd0cc780c3371a1b
yujamint <woawoa0513@gmail.com>
2023-09-26 21:26:40 +0900

    test: JdbcTemplate.queryForObject()의 결과로 두 건 이상 나오는 상황 테스트

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -3,10 +3,12 @@ package com.techcourse.dao;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class UserDaoTest {
 
@@ -17,12 +19,12 @@ class UserDaoTest {
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
 
         userDao = new UserDao(DataSourceConfig.getInstance());
-        final var user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.insert(user);
     }
 
     @Test
     void findAll() {
+        userDao.insert(new User("gugu", "password", "hkkang@woowahan.com"));
+
         final var users = userDao.findAll();
 
         assertThat(users).isNotEmpty();
@@ -30,17 +32,29 @@ class UserDaoTest {
 
     @Test
     void findById() {
+        userDao.insert(new User("gugu", "password", "hkkang@woowahan.com"));
+
         final var user = userDao.findById(1L);
 
-        assertThat(user.getAccount()).isEqualTo("gugu");
+        assertThat(user.getId()).isEqualTo(1L);
     }
 
     @Test
     void findByAccount() {
         final var account = "gugu";
+        userDao.insert(new User(account, "password", "hkkang@woowahan.com"));
         final var user = userDao.findByAccount(account);
 
         assertThat(user.getAccount()).isEqualTo(account);
+    }
+
+    @Test
+    void findByAccount_resultSizeTwo_fail() {
+        final var account = "gugu";
+        userDao.insert(new User(account, "password", "hkkang@woowahan.com"));
+        userDao.insert(new User(account, "password", "hkkang@woowahan.com"));
+        assertThatThrownBy(() -> userDao.findByAccount(account))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -49,13 +63,14 @@ class UserDaoTest {
         final var user = new User(account, "password", "hkkang@woowahan.com");
         userDao.insert(user);
 
-        final var actual = userDao.findById(2L);
+        final var actual = userDao.findById(1L);
 
         assertThat(actual.getAccount()).isEqualTo(account);
     }
 
     @Test
     void update() {
+        userDao.insert(new User("gugu", "password", "hkkang@woowahan.com"));
         final var newPassword = "password99";
         final var user = userDao.findById(1L);
         user.changePassword(newPassword);
