b0ebdea538e2b6a3b20f61019cf53e27d6a6781b
junpakPark <112045553+junpakPark@users.noreply.github.com>
2023-09-26 16:36:30 +0900

    feat: query 메서드 분리

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -15,11 +15,12 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
+
 public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
-    private static RowMapper<User> USER_ROW_MAPPER = (resultSet, rowNum) ->
+    private static final RowMapper<User> USER_ROW_MAPPER = (resultSet, rowNum) ->
             new User(
                     resultSet.getLong(1),
                     resultSet.getString(2),
@@ -70,53 +71,30 @@ public class UserDao {
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users ";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            rs = pstmt.executeQuery();
+        return query(sql, USER_ROW_MAPPER);
+    }
+
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
 
             log.debug("query : {}", sql);
 
-            List<User> users = new ArrayList<>();
-            while (rs.next()) {
-                users.add(
-                        new User(
-                                rs.getLong(1),
-                                rs.getString(2),
-                                rs.getString(3),
-                                rs.getString(4)
-                        )
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(
+                        rowMapper.mapRow(resultSet, resultSet.getRow())
                 );
             }
 
-            return users;
+            return results;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
@@ -158,4 +136,5 @@ public class UserDao {
             throw new RuntimeException(e);
         }
     }
+
 }
