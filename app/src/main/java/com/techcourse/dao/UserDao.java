4e395299e0c4ad7217255ad9af047049ca7a5aa3
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-09-27 08:52:44 +0900

    feat: findByAccount 추가

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -44,7 +44,7 @@ public class UserDao {
     }
 
     public User findByAccount(final String account) {
-        // todo
-        return null;
+        final var sql = "select id, account, password, email from users where account = ?";
+        return jdbcTemplate.queryForObject(sql, userRowMapper, account);
     }
 }
