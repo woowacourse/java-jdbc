ed30b8cbe2418a8aff8a98d58bc7b903d780cb0f
junpakPark <112045553+junpakPark@users.noreply.github.com>
2023-09-26 16:44:02 +0900

    feat: query 메서드 분리

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,24 +1,14 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 
 public class UserDao {
-
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
     private static final RowMapper<User> USER_ROW_MAPPER = (resultSet, rowNum) ->
             new User(
@@ -28,113 +18,46 @@ public class UserDao {
                     resultSet.getString(4)
             );
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = null;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
 
-        update(sql, user.getAccount(), user.getPassword(), user.getEmail());
-
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
         final var sql = "update users set account = ?, password = ?, email = ? ";
 
-        update(sql, user.getAccount(), user.getPassword(), user.getEmail());
-    }
-
-    private void update(final String sql, final Object... parameters) {
-        try (
-                Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-
-            for (int i = 1; i <= parameters.length; i++) {
-                preparedStatement.setObject(i, parameters[i - 1]);
-            }
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users ";
 
-        return query(sql, USER_ROW_MAPPER);
-    }
-
-
-    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
-        try (
-                Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-                ResultSet resultSet = preparedStatement.executeQuery()
-        ) {
-
-            log.debug("query : {}", sql);
-
-            List<T> results = new ArrayList<>();
-            while (resultSet.next()) {
-                results.add(
-                        rowMapper.mapRow(resultSet, resultSet.getRow())
-                );
-            }
-
-            return results;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        return jdbcTemplate.query(sql, USER_ROW_MAPPER);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        return queryForObject(sql, USER_ROW_MAPPER, id)
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, id)
                 .orElseThrow(DataAccessException::new);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
 
-        return queryForObject(sql, USER_ROW_MAPPER, account)
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, account)
                 .orElseThrow(DataAccessException::new);
-    }
-
-
-    private <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object id) {
-        try (
-                Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        ) {
-            preparedStatement.setObject(1, id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (resultSet.next()) {
-                T result = rowMapper.mapRow(resultSet, resultSet.getRow());
-
-                resultSet.close();
-                return Optional.ofNullable(result);
-            }
-            resultSet.close();
-            return Optional.empty();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
     }
 
 }
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,91 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    public static final String QUERY_FORMAT = "query : {}";
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public void update(final String sql, final Object... parameters) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug(QUERY_FORMAT, sql);
+
+            for (int i = 1; i <= parameters.length; i++) {
+                preparedStatement.setObject(i, parameters[i - 1]);
+            }
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+
+    public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object id) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            preparedStatement.setObject(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            log.debug(QUERY_FORMAT, sql);
+
+            if (resultSet.next()) {
+                T result = rowMapper.mapRow(resultSet, resultSet.getRow());
+                resultSet.close();
+
+                return Optional.ofNullable(result);
+            }
+
+            resultSet.close();
+
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+
+            log.debug(QUERY_FORMAT, sql);
+
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(
+                        rowMapper.mapRow(resultSet, resultSet.getRow())
+                );
+            }
+
+            return results;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
 }
+
