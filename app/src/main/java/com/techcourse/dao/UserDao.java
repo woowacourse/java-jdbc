05115cca1e27e2063af4291101f7fa2b68e0bded
70825 <hello70825@gmail.com>
2023-09-26 17:11:49 +0900

    refactor: 여러개의 반환값을 얻는 쿼리문을 JdbcTemplate의 queryForList로 변경

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -42,48 +42,7 @@ public class UserDao {
 
     public List<User> findAll() {
         final var sql = "SELECT id, account, password, email FROM users";
-        final List<User> users = new ArrayList<>();
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            while (rs.next()) {
-                users.add(new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4)));
-            }
-            return users;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
-        }
+        return jdbcTemplate.queryForList(sql, User.class);
     }
 
     public User findById(final Long id) {
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -113,4 +113,60 @@ public class JdbcTemplate {
             } catch (SQLException ignored) {}
         }
     }
+
+    public <T> List<T> queryForList(final String query, final Class<T> convertClass, final Object... args) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(query);
+
+            log.debug("query : {}", query);
+
+            for(int i = 0; i < args.length; i++) {
+                pstmt.setObject(i + 1, args[i]);
+            }
+            rs = pstmt.executeQuery();
+
+            final List<T> objects = new ArrayList<>();
+            while (rs.next()) {
+                final ResultSetMetaData metaData = rs.getMetaData();
+                final int columnCount = metaData.getColumnCount();
+
+                final Object[] params = new Object[columnCount];
+                final Class<?>[] paramTypes = new Class<?>[columnCount];
+
+                for (int i = 0; i < columnCount; i++) {
+                    params[i] = rs.getObject(i + 1);
+                    paramTypes[i] = Class.forName(metaData.getColumnClassName(i + 1));
+                }
+
+                final Constructor<T> constructor = convertClass.getConstructor(paramTypes);
+                objects.add(constructor.newInstance(params));
+            }
+            return objects;
+        } catch (final Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {}
+        }
+    }
 }
