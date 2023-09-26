2d8e12ee9b271a200be47b15a415ecd237b23d76
junpakPark <112045553+junpakPark@users.noreply.github.com>
2023-09-26 16:23:00 +0900

    feat: queryForObject 메서드 분리

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -7,14 +7,25 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
+
+    private static RowMapper<User> USER_ROW_MAPPER = (resultSet, rowNum) ->
+            new User(
+                    resultSet.getLong(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3),
+                    resultSet.getString(4)
+            );
 
     private final DataSource dataSource;
 
@@ -33,6 +44,12 @@ public class UserDao {
 
     }
 
+    public void update(final User user) {
+        final var sql = "update users set account = ?, password = ?, email = ? ";
+
+        update(sql, user.getAccount(), user.getPassword(), user.getEmail());
+    }
+
     private void update(final String sql, final Object... parameters) {
         try (
                 Connection connection = dataSource.getConnection();
@@ -48,12 +65,6 @@ public class UserDao {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
-    }
-
-    public void update(final User user) {
-        final var sql = "update users set account = ?, password = ?, email = ? ";
-
-        update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public List<User> findAll() {
@@ -112,98 +123,39 @@ public class UserDao {
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
+        return queryForObject(sql, USER_ROW_MAPPER, id)
+                .orElseThrow(DataAccessException::new);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, account);
-            rs = pstmt.executeQuery();
+        return queryForObject(sql, USER_ROW_MAPPER, account)
+                .orElseThrow(DataAccessException::new);
+    }
+
+
+    private <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object id) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        ) {
+            preparedStatement.setObject(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
 
             log.debug("query : {}", sql);
 
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
+            if (resultSet.next()) {
+                T result = rowMapper.mapRow(resultSet, resultSet.getRow());
+
+                resultSet.close();
+                return Optional.ofNullable(result);
             }
-            return null;
+            resultSet.close();
+            return Optional.empty();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 }
