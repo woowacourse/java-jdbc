e59ac216ab8401698711c4a77d3a50ff7686aab0
cpot5620 <cpot5620@gmail.com>
2023-09-26 20:56:04 +0900

    feat: UserDao CRU 기능 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,16 +1,17 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserDao {
 
@@ -18,22 +19,19 @@ public class UserDao {
 
     private final DataSource dataSource;
 
-    public UserDao(final DataSource dataSource) {
+    public UserDao(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    public UserDao(final JdbcTemplate jdbcTemplate) {
+    public UserDao(JdbcTemplate jdbcTemplate) {
         this.dataSource = null;
     }
 
-    public void insert(final User user) {
-        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+    public void insert(User user) {
+        String sql = "insert into users (account, password, email) values (?, ?, ?)";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             log.debug("query : {}", sql);
 
@@ -44,43 +42,60 @@ public class UserDao {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
         }
     }
 
-    public void update(final User user) {
-        // todo
+    public void update(User user) {
+        String sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE id = ?";
+
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+
+            pstmt.setString(1, user.getAccount());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setLong(4, user.getId());
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
-    }
+        String sql = "SELECT * from users";
 
-    public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                ResultSet rs = pstmt.executeQuery()) {
 
             log.debug("query : {}", sql);
+
+            List<User> users = new ArrayList<>();
+
+            while (rs.next()) {
+                User user = new User(rs.getLong("id"),
+                        rs.getString("account"),
+                        rs.getString("password"),
+                        rs.getString("email")
+                );
+
+                users.add(user);
+            }
+
+            return users;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public User findById(Long id) {
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = getFindByIdPstmt(conn, id);
+                ResultSet rs = pstmt.executeQuery()) {
 
             if (rs.next()) {
                 return new User(
@@ -93,29 +108,52 @@ public class UserDao {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
         }
     }
 
-    public User findByAccount(final String account) {
-        // todo
-        return null;
+    private PreparedStatement getFindByIdPstmt(Connection conn, Long id) throws SQLException {
+        String sql = "select * from users where id = ?";
+
+        log.debug("query : {}", sql);
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setLong(1, id);
+
+        return pstmt;
     }
+
+    public User findByAccount(String account) {
+
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = getFindByAccountPstmt(conn, account);
+                ResultSet rs = pstmt.executeQuery()) {
+
+            if (rs.next()) {
+                return new User(
+                        rs.getLong(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4));
+            }
+
+            log.debug("Can't find user ! account : {}", account);
+            throw new NoSuchElementException();
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement getFindByAccountPstmt(Connection conn, String account) throws SQLException {
+        String sql = "select * from users where account = ?";
+
+        log.debug("query : {}", sql);
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, account);
+
+        return pstmt;
+    }
+
 }
