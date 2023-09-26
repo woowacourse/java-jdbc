1c468a3b9642a6268a4694987d1d4f109cdd0b09
cpot5620 <cpot5620@gmail.com>
2023-09-26 22:51:05 +0900

    feat: JdbcTemplate 기능 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,13 +1,7 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,143 +11,72 @@ public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public UserDao(JdbcTemplate jdbcTemplate) {
-        this.dataSource = null;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public void insert(User user) {
         String sql = "insert into users (account, password, email) values (?, ?, ?)";
 
-        try (Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        log.debug("query : {}", sql);
 
-            log.debug("query : {}", sql);
-
-            pstmt.setString(1, user.getAccount());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getEmail());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(User user) {
         String sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE id = ?";
 
-        try (Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        log.debug("query : {}", sql);
 
-            log.debug("query : {}", sql);
-
-            pstmt.setString(1, user.getAccount());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getEmail());
-            pstmt.setLong(4, user.getId());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
         String sql = "SELECT * from users";
 
-        try (Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = conn.prepareStatement(sql);
-                ResultSet rs = pstmt.executeQuery()) {
+        log.debug("query : {}", sql);
 
-            log.debug("query : {}", sql);
-
-            List<User> users = new ArrayList<>();
-
-            while (rs.next()) {
-                User user = new User(rs.getLong("id"),
-                        rs.getString("account"),
-                        rs.getString("password"),
-                        rs.getString("email")
-                );
-
-                users.add(user);
-            }
-
-            return users;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        return jdbcTemplate.query(sql, (rs) -> {
+            return new User(
+                    rs.getLong("id"),
+                    rs.getString("account"),
+                    rs.getString("password"),
+                    rs.getString("email"));
+        });
     }
 
     public User findById(Long id) {
-        try (Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = getFindByIdPstmt(conn, id);
-                ResultSet rs = pstmt.executeQuery()) {
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private PreparedStatement getFindByIdPstmt(Connection conn, Long id) throws SQLException {
         String sql = "select * from users where id = ?";
 
         log.debug("query : {}", sql);
 
-        PreparedStatement pstmt = conn.prepareStatement(sql);
-        pstmt.setLong(1, id);
-
-        return pstmt;
+        return jdbcTemplate.queryForObject(sql, (rs) -> {
+            return new User(
+                    rs.getLong("id"),
+                    rs.getString("account"),
+                    rs.getString("password"),
+                    rs.getString("email"));
+        }, id);
     }
 
     public User findByAccount(String account) {
-
-        try (Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = getFindByAccountPstmt(conn, account);
-                ResultSet rs = pstmt.executeQuery()) {
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-
-            log.debug("Can't find user ! account : {}", account);
-            throw new NoSuchElementException();
-
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private PreparedStatement getFindByAccountPstmt(Connection conn, String account) throws SQLException {
         String sql = "select * from users where account = ?";
 
         log.debug("query : {}", sql);
 
-        PreparedStatement pstmt = conn.prepareStatement(sql);
-        pstmt.setString(1, account);
-
-        return pstmt;
+        return jdbcTemplate.queryForObject(sql, (rs) -> {
+            return new User(
+                    rs.getLong("id"),
+                    rs.getString("account"),
+                    rs.getString("password"),
+                    rs.getString("email"));
+        }, account);
     }
 
 }
