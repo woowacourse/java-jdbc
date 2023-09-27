7bcd58fa50b6ce7b7694e8118dc1635709752351
java-saeng <gusdn85554@naver.com>
2023-09-28 06:35:16 +0900

    refactor : UserDao#findById 에서 JdbcTemplate 사용

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -7,23 +7,28 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
 
   private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
   private final DataSource dataSource;
+  private final JdbcTemplate jdbcTemplate;
 
   public UserDao(final DataSource dataSource) {
     this.dataSource = dataSource;
+    this.jdbcTemplate = new JdbcTemplate(dataSource);
   }
 
   public UserDao(final JdbcTemplate jdbcTemplate) {
     this.dataSource = null;
+    this.jdbcTemplate = jdbcTemplate;
   }
 
   public void insert(final User user) {
@@ -150,50 +155,14 @@ public class UserDao {
   public User findById(final Long id) {
     final var sql = "select id, account, password, email from users where id = ?";
 
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    ResultSet rs = null;
-    try {
-      conn = dataSource.getConnection();
-      pstmt = conn.prepareStatement(sql);
-      pstmt.setLong(1, id);
-      rs = pstmt.executeQuery();
+    final Map<Integer, Object> result = jdbcTemplate.query(sql, id);
 
-      log.debug("query : {}", sql);
-
-      if (rs.next()) {
-        return new User(
-            rs.getLong(1),
-            rs.getString(2),
-            rs.getString(3),
-            rs.getString(4));
-      }
-      return null;
-    } catch (SQLException e) {
-      log.error(e.getMessage(), e);
-      throw new RuntimeException(e);
-    } finally {
-      try {
-        if (rs != null) {
-          rs.close();
-        }
-      } catch (SQLException ignored) {
-      }
-
-      try {
-        if (pstmt != null) {
-          pstmt.close();
-        }
-      } catch (SQLException ignored) {
-      }
-
-      try {
-        if (conn != null) {
-          conn.close();
-        }
-      } catch (SQLException ignored) {
-      }
-    }
+    return new User(
+        (Long) result.get(1),
+        (String) result.get(2),
+        (String) result.get(3),
+        (String) result.get(4)
+    );
   }
 
   public User findByAccount(final String account) {
