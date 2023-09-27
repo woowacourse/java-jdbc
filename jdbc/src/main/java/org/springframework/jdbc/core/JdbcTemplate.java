53334ad02ac65a282e260479e5f49c5f4c14555c
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-09-27 10:51:05 +0900

    feat: JdbcTemplate execute 메서드 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,8 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +16,24 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void execute(String sql, Object...parameters) throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            setParameterIfExist(preparedStatement, parameters);
+            log.info("JDBC EXECUTE SQL = {}", sql);
+            preparedStatement.execute();
+        }
+    }
+
+    private void setParameterIfExist(PreparedStatement preparedStatement, Object[] parameters) throws SQLException {
+        if (parameters.length == 0) {
+            return;
+        }
+
+        for (int i = 0; i < parameters.length; i++) {
+            preparedStatement.setObject(i+1, parameters[i]);
+        }
     }
 }
