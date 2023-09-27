a11fc4936c17c8f616de5cbaafba6ae740719f1c
Youngho <cyh6099@gmail.com>
2023-09-27 17:39:39 +0900

    feat: findAll 구현

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -8,6 +8,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class JdbcTemplate {
@@ -93,6 +95,52 @@ public class JdbcTemplate {
             }
         }
     }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            for (int index = 1; index < parameters.length + 1; index++) {
+                pstmt.setObject(index, parameters[index - 1]);
+            }
+            rs = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+            List<T> results = new ArrayList<>();
+            if (rs.next()) {
+                results.add(rowMapper.map(rs));
+            }
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {
+            }
+
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {
+            }
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
+    }
+
 
     public DataSource getDataSource() {
         return dataSource;
