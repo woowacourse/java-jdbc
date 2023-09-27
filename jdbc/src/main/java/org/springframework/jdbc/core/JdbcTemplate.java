2e6820e1741e62d54dd76b4e58aafb387be1946b
jjongwa <troas96@naver.com>
2023-09-27 21:58:38 +0900

    feat: UserDao insert, JdbcTemplate execute 구현

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,11 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +15,24 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void execute(final String sql, final Object... objects) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            setValues(pstmt, objects);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setValues(final PreparedStatement pstmt, Object... objects) throws SQLException {
+        for (int i = 0; i < objects.length; i++) {
+            pstmt.setObject(i + 1, objects[i]);
+        }
     }
 }
