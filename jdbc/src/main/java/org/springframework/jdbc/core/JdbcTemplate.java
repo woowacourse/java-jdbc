ff7b0276c1ade3d3273d3d037e0dfcd28c087955
권예진 <96688810+kwonyj1022@users.noreply.github.com>
2023-09-28 00:41:59 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 엔초(권예진) 미션 제출합니다.  (#276)
    * 패키지 위치 변경 및 코드 정리
    
    * feat: JdbcTemplate 구현 및 UserDao에 JdbcTemplate 사용해서 기능 구현
    
    * refactor: JdbcTemplate에 try-with-resources 사용
    
    * refactor: JdbcTemplate에 PreparedStatement의 파라미터를 설정하는 중복 코드 제거
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,8 +2,15 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.exception.IncorrectResultSizeDataAccessException;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,82 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            processPreparedStatementParameter(pstmt, args);
+
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void processPreparedStatementParameter(final PreparedStatement pstmt, final Object[] args) throws SQLException {
+        final int argsLength = args.length;
+        for (int i = 0; i < argsLength; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMaper, final Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            processPreparedStatementParameter(pstmt, args);
+
+            List<T> results = new ArrayList<>();
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    results.add(rowMaper.mapRow(rs, rs.getRow()));
+                }
+            }
+
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            processPreparedStatementParameter(pstmt, args);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+
+                T findObject = null;
+                if (rs.next()) {
+                    findObject = rowMapper.mapRow(rs, rs.getRow());
+                }
+
+                validateSingleResult(findObject, rs);
+
+                return findObject;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> void validateSingleResult(final T findObject, final ResultSet rs) throws SQLException {
+        if (findObject == null) {
+            throw new IncorrectResultSizeDataAccessException(0);
+        }
+        if (rs.next()) {
+            rs.last();
+            throw new IncorrectResultSizeDataAccessException(rs.getRow());
+        }
     }
 }
---
+++ jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
@@ -0,0 +1,12 @@
+package org.springframework.jdbc.core;
+
+import javax.annotation.Nullable;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+
+    @Nullable
+    T mapRow(ResultSet rs, int rowNum) throws SQLException;
+}
