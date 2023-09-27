7e847712693f2066e97e6a0870589484b504b258
Miseong Kim <miseongk@naver.com>
2023-09-27 15:47:16 +0900

    feat: JdbcTemplate 리팩토링

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,8 +1,6 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
@@ -30,80 +28,26 @@ public class UserDao {
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-
-        jdbcTemplate.update(connection -> {
-            try {
-                final PreparedStatement pstmt = connection.prepareStatement(sql);
-                pstmt.setString(1, user.getAccount());
-                pstmt.setString(2, user.getPassword());
-                pstmt.setString(3, user.getEmail());
-                return pstmt;
-            } catch (final SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
-            }
-        });
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
         final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
-
-        jdbcTemplate.update(connection -> {
-            try {
-                final PreparedStatement pstmt = connection.prepareStatement(sql);
-                pstmt.setString(1, user.getAccount());
-                pstmt.setString(2, user.getPassword());
-                pstmt.setString(3, user.getEmail());
-                pstmt.setLong(4, user.getId());
-                return pstmt;
-            } catch (final SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
-            }
-        });
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
-
-        return jdbcTemplate.query(connection -> {
-            try {
-                final PreparedStatement pstmt = connection.prepareStatement(sql);
-                return pstmt;
-            } catch (final SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
-            }
-        }, ROW_MAPPER);
+        return jdbcTemplate.query(sql, ROW_MAPPER);
     }
 
     public Optional<User> findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-
-        return jdbcTemplate.queryForObject(connection -> {
-            try {
-                final PreparedStatement pstmt = connection.prepareStatement(sql);
-                pstmt.setLong(1, id);
-                return pstmt;
-            } catch (final SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
-            }
-        }, ROW_MAPPER);
+        return jdbcTemplate.queryForObject(sql, ROW_MAPPER, id);
     }
 
     public Optional<User> findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-
-        return jdbcTemplate.queryForObject(connection -> {
-            try {
-                final PreparedStatement pstmt = connection.prepareStatement(sql);
-                pstmt.setString(1, account);
-                return pstmt;
-            } catch (final SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
-            }
-        }, ROW_MAPPER);
+        return jdbcTemplate.queryForObject(sql, ROW_MAPPER, account);
     }
 }
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,9 +21,10 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(final Function<Connection, PreparedStatement> function) {
+    public void update(final String query, final Object... columns) {
         try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = function.apply(conn)) {
+             final PreparedStatement pstmt = conn.prepareStatement(query)) {
+            setParameters(pstmt, columns);
             pstmt.executeUpdate();
         } catch (final SQLException e) {
             log.error(e.getMessage(), e);
@@ -32,10 +32,25 @@ public class JdbcTemplate {
         }
     }
 
-    public <T> Optional<T> queryForObject(final Function<Connection, PreparedStatement> function,
-                                          final RowMapper<T> rowMapper) {
+    public <T> List<T> query(final String query, final RowMapper<T> rowMapper) {
         try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = function.apply(conn)) {
+             final PreparedStatement pstmt = conn.prepareStatement(query)) {
+            final ResultSet rs = pstmt.executeQuery();
+            final List<T> result = new ArrayList<>();
+            while (rs.next()) {
+                result.add(rowMapper.map(rs));
+            }
+            return result;
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> Optional<T> queryForObject(final String query, final RowMapper<T> rowMapper, final Object... columns) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(query)) {
+            setParameters(pstmt, columns);
             final ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
                 return Optional.of(rowMapper.map(rs));
@@ -47,18 +62,9 @@ public class JdbcTemplate {
         }
     }
 
-    public <T> List<T> query(final Function<Connection, PreparedStatement> function, final RowMapper<T> rowMapper) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = function.apply(conn)) {
-            final ResultSet rs = pstmt.executeQuery();
-            final List<T> result = new ArrayList<>();
-            while (rs.next()) {
-                result.add(rowMapper.map(rs));
-            }
-            return result;
-        } catch (final SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+    private void setParameters(final PreparedStatement pstmt, final Object[] columns) throws SQLException {
+        for (int i = 0; i < columns.length; i++) {
+            pstmt.setObject(i + 1, columns[i]);
         }
     }
 }
