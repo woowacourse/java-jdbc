a244fa5f3f08409c4ec54e54af54d2aec62ed051
hanueleee <ezzeezz@naver.com>
2023-09-27 14:26:54 +0900

    feat: JdbcTemplate에 query메소드 추가 (select)

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,13 +2,14 @@ package org.springframework.jdbc.core;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 
-public class JdbcTemplate {
+public class JdbcTemplate<T> {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
@@ -33,6 +34,47 @@ public class JdbcTemplate {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {}
+        }
+    }
+
+    public T query(String sql, RowMapper<T> mapper, Object... args) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            log.debug("query : {}", sql);
+
+            setParameter(pstmt, args);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return mapper.map(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {}
+
             try {
                 if (pstmt != null) {
                     pstmt.close();
