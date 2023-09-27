5b6dcf6547ca83df397d29f2985e4dd9bde52f66
hanueleee <ezzeezz@naver.com>
2023-09-27 14:37:36 +0900

    refactor: UserDao의 findAll에 JdbcTemplate의 queryForList 메소드 사용

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,17 +1,11 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import java.util.ArrayList;
-import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserDao {
 
@@ -42,58 +36,16 @@ public class UserDao {
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        return (User) jdbcTemplate.query(sql, mapper, id);
+        return (User) jdbcTemplate.queryForObject(sql, mapper, id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-        return (User) jdbcTemplate.query(sql, mapper, account);
+        return (User) jdbcTemplate.queryForObject(sql, mapper, account);
     }
 
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            List<User> users = new ArrayList<>();
-            while (rs.next()) {
-                User user = new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-                users.add(user);
-            }
-            return users;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
-        }
+        return jdbcTemplate.queryForList(sql, mapper);
     }
 }
