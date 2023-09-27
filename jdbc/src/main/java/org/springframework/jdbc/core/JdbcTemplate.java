f4350146df5e0f33f818e7467b4f97636c8b2b6b
Jaeyoon Lee <63213487+sosow0212@users.noreply.github.com>
2023-09-28 01:16:48 +0900

    [1단계 - JDBC 라이브러리 구현하기] 제이(이재윤) 미션 제출합니다. (#265)
    * feat: 레거시 DAO 구현
    
    * feat: JdbcTemplate 기능 추가
    
    * refactor: 레거시 코드 변경
    
    * refactor: jdbcTemplate 리팩토링 및 dao 변경

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,8 +2,15 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,58 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(connection, sql, args);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(rowMapper.mapToRow(resultSet));
+            }
+
+            return results;
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(connection, sql, args);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            if (!resultSet.next()) {
+                return null;
+            }
+
+            return rowMapper.mapToRow(resultSet);
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    public void update(final String sql, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(connection, sql, args)) {
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    private PreparedStatement getPreparedStatement(final Connection connection, final String sql, final Object[] args) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject(i + 1, args[i]);
+        }
+
+        return preparedStatement;
     }
 }
