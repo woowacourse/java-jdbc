1623fdc6f97c3e93c91a472e6aa7146ee2962f88
apptie <57691173+apptie@users.noreply.github.com>
2023-09-27 19:14:36 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 지토(김지민) 미션 제출합니다.  (#275)
    * feat: 쿼리 결과를 원하는 결과로 매핑하기 위한 RowMapper 추가
    
    * feat: JdbcTemplate 쿼리 실행 로직 추가
    
    * refactor: UserDao가 DataSource가 아니라 JdbcTemplate을 사용하도록 변경

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,86 @@
 package org.springframework.jdbc.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.sql.DataSource;
+import org.springframework.jdbc.core.exception.JdbcTemplateException;
 
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int START_STATEMENT_INDEX = 1;
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int query(final String sql, final Object... statements) {
+        try (
+                final Connection connection = dataSource.getConnection();
+                final PreparedStatement preparedStatement = processPreparedStatement(connection, sql, statements)
+        ) {
+            return preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new JdbcTemplateException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... statements) {
+        try (
+                final Connection connection = dataSource.getConnection();
+                final PreparedStatement preparedStatement = processPreparedStatement(connection, sql, statements);
+                final ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            if (resultSet.next()) {
+                return rowMapper.mapRow(resultSet);
+            }
+
+            return null;
+        } catch (final SQLException e) {
+            throw new JdbcTemplateException(e);
+        }
+    }
+
+    public <T> List<T> queryForList(final String sql, final RowMapper<T> rowMapper, final Object... statements) {
+        try (
+                final Connection connection = dataSource.getConnection();
+                final PreparedStatement preparedStatement = processPreparedStatement(connection, sql, statements);
+                final ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            final List<T> result = new ArrayList<>();
+
+            while (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet));
+            }
+
+            return result;
+        } catch (final SQLException e) {
+            throw new JdbcTemplateException(e);
+        }
+    }
+
+    private PreparedStatement processPreparedStatement(
+            final Connection connection,
+            final String sql,
+            final Object[] statements
+    ) throws SQLException {
+        final PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+        bindStatements(preparedStatement, statements);
+        return preparedStatement;
+    }
+
+    private void bindStatements(
+            final PreparedStatement preparedStatement,
+            final Object[] statements
+    ) throws SQLException {
+        for (int i = START_STATEMENT_INDEX; i < statements.length + 1; i++) {
+            preparedStatement.setObject(i, statements[i - START_STATEMENT_INDEX]);
+        }
     }
 }
---
+++ jdbc/src/main/java/org/springframework/jdbc/core/exception/JdbcTemplateException.java
@@ -0,0 +1,8 @@
+package org.springframework.jdbc.core.exception;
+
+public class JdbcTemplateException extends RuntimeException {
+
+    public JdbcTemplateException(final Throwable cause) {
+        super(cause);
+    }
+}
