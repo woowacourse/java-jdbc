6c92d766b9679b7244ebaec371a69cc3ec2a1dfd
java-saeng <gusdn85554@naver.com>
2023-09-28 06:44:42 +0900

    refactor : UserDao#findAll 에서 JdbcTemplate 사용

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -12,7 +12,6 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
 
@@ -103,53 +102,22 @@ public class UserDao {
   public List<User> findAll() {
     final var sql = "select id, account, password, email from users";
 
-    Connection conn = null;
-    PreparedStatement pstmt = null;
-    ResultSet rs = null;
-    try {
-      conn = dataSource.getConnection();
-      pstmt = conn.prepareStatement(sql);
-      rs = pstmt.executeQuery();
+    final List<Map<Integer, Object>> results = jdbcTemplate.queryPlural(sql);
 
-      log.debug("query : {}", sql);
+    List<User> users = new ArrayList<>();
 
-      List<User> users = new ArrayList<>();
+    for (int i = 0; i < results.size(); i++) {
+      final Map<Integer, Object> result = results.get(i);
 
-      while (rs.next()) {
-        users.add(new User(
-            rs.getLong(1),
-            rs.getString(2),
-            rs.getString(3),
-            rs.getString(4))
-        );
-      }
-
-      return users;
-    } catch (SQLException e) {
-      log.error(e.getMessage(), e);
-      throw new RuntimeException(e);
-    } finally {
-      try {
-        if (rs != null) {
-          rs.close();
-        }
-      } catch (SQLException ignored) {
-      }
-
-      try {
-        if (pstmt != null) {
-          pstmt.close();
-        }
-      } catch (SQLException ignored) {
-      }
-
-      try {
-        if (conn != null) {
-          conn.close();
-        }
-      } catch (SQLException ignored) {
-      }
+      users.add(new User(
+          (Long) result.get(1),
+          (String) result.get(2),
+          (String) result.get(3),
+          (String) result.get(4)
+      ));
     }
+
+    return users;
   }
 
   public User findById(final Long id) {
