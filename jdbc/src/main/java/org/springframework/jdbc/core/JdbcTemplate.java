5cf6847a59c92bab7d5df82eb8f2c0f1fa5c76d8
Youngho <cyh6099@gmail.com>
2023-09-27 18:23:04 +0900

    refactor: jdbc template try with resources 적용

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -23,11 +23,10 @@ public class JdbcTemplate {
     }
 
     public void update(String sql, Object... parameters) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
             for (int index = 1; index < parameters.length + 1; index++) {
                 pstmt.setObject(index, parameters[index - 1]);
             }
@@ -35,94 +34,48 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... parameters) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
             for (int index = 1; index < parameters.length + 1; index++) {
                 pstmt.setObject(index, parameters[index - 1]);
             }
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-            if (rs.next()) {
-                return rowMapper.map(rs);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                log.debug("query : {}", sql);
+                if (rs.next()) {
+                    return rowMapper.map(rs);
+                }
+                throw new NoSuchElementException("조건에 맞는 데이터가 없습니다.");
             }
-            throw new NoSuchElementException("조건에 맞는 데이터가 없습니다.");
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            closeResources(conn, pstmt, rs);
         }
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
             for (int index = 1; index < parameters.length + 1; index++) {
                 pstmt.setObject(index, parameters[index - 1]);
             }
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-            List<T> results = new ArrayList<>();
-            if (rs.next()) {
-                results.add(rowMapper.map(rs));
+            try (ResultSet rs = pstmt.executeQuery()) {
+                log.debug("query : {}", sql);
+                List<T> results = new ArrayList<>();
+                while (rs.next()) {
+                    results.add(rowMapper.map(rs));
+                }
+                return results;
             }
-            return results;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            closeResources(conn, pstmt, rs);
-        }
-    }
-
-    private void closeResources(Connection conn, PreparedStatement pstmt, ResultSet rs) {
-        try {
-            if (rs != null) {
-                rs.close();
-            }
-        } catch (SQLException ignored) {
-        }
-
-        try {
-            if (pstmt != null) {
-                pstmt.close();
-            }
-        } catch (SQLException ignored) {
-        }
-
-        try {
-            if (conn != null) {
-                conn.close();
-            }
-        } catch (SQLException ignored) {
         }
     }
 }
