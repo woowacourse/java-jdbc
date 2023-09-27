af46958756d676e651914b3728e6cbe5cf339559
준팍(junpak) <112045553+junpakPark@users.noreply.github.com>
2023-09-27 21:59:12 +0900

    [JDBC 라이브러리 구현하기 - 1,2단계] 준팍(박준현) 미션 제출합니다. (#263)
    * feat: UserDao 모든 메서드 구현
    
    * feat: update 메서드 분리
    
    * feat: queryForObject 메서드 분리
    
    * feat: query 메서드 분리
    
    * feat: query 메서드 분리
    
    * test: JdbcTemplate 테스트 케이스 추가
    
    * feat: queryForObject의 where 절 다중 조건 대응 구현

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,92 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    public static final String QUERY_FORMAT = "query : {}";
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public void update(final String sql, final Object... args) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug(QUERY_FORMAT, sql);
+
+            bindStatementWithArgs(args, preparedStatement);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            bindStatementWithArgs(args, preparedStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            log.debug(QUERY_FORMAT, sql);
+
+            if (resultSet.next()) {
+                T result = rowMapper.mapRow(resultSet, resultSet.getRow());
+                resultSet.close();
+
+                return Optional.ofNullable(result);
+            }
+
+            resultSet.close();
+
+            return Optional.empty();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void bindStatementWithArgs(Object[] args, PreparedStatement preparedStatement) throws SQLException {
+        for (int i = 1; i <= args.length; i++) {
+            preparedStatement.setObject(i, args[i - 1]);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(sql);
+                ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            log.debug(QUERY_FORMAT, sql);
+
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(
+                        rowMapper.mapRow(resultSet, resultSet.getRow())
+                );
+            }
+
+            return results;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,104 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
 class JdbcTemplateTest {
+
+    private JdbcTemplate jdbcTemplate;
+    @Mock
+    private DataSource dataSource;
+    @Mock
+    private Connection connection;
+    @Mock
+    private PreparedStatement preparedStatement;
+    @Mock
+    private ResultSet resultSet;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Test
+    void update() throws SQLException {
+        // given
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+
+        String testSql = "UPDATE some_table SET column1 = ?, column2 = ? WHERE id = ?";
+        Object[] params = {"value1", "value2", 1};
+
+        // when
+        jdbcTemplate.update(testSql, params);
+
+        // then
+        verify(preparedStatement, times(1)).setObject(1, "value1");
+        verify(preparedStatement, times(1)).setObject(2, "value2");
+        verify(preparedStatement, times(1)).setObject(3, 1);
+        verify(preparedStatement, times(1)).executeUpdate();
+    }
+
+    @Test
+    void queryForObject() throws SQLException {
+        // given
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true);
+
+        RowMapper<String> rowMapper = (rs, rowNum) -> rs.getString("column_name");
+        when(resultSet.getString("column_name")).thenReturn("mockValue");
+
+        // when
+        String sql = "SELECT * FROM some_table where id = ? ";
+        Optional<String> result = jdbcTemplate.queryForObject(sql, rowMapper, 1);
+
+        // then
+        assertThat(result).isPresent();
+        assertThat(result.get()).contains("mockValue");
+    }
+
+    @Test
+    void query() throws SQLException {
+        // given
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        when(resultSet.next()).thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(false);
+
+        when(resultSet.getString("column_name")).thenReturn("value1", "value2");
+
+        RowMapper<String> rowMapper = (rs, rowNum) -> rs.getString("column_name");
+
+        // when
+        String sql = "SELECT * FROM some_table ";
+        List<String> results = jdbcTemplate.query(sql, rowMapper);
+
+        // then
+        assertThat(results).hasSize(2)
+                .contains("value1", "value2");
+    }
 
 }
