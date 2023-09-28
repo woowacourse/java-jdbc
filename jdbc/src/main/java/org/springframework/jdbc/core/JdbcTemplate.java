9957d004022a58fe306371e17d6176385c7cb3a0
동해 <donghae1999@gmail.com>
2023-09-28 18:30:25 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 오션(김동해) 미션 제출합니다.  (#273)
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * feat: update & queryForObject 추가
    
    * feat: query 추가
    
    * chore: try-catch-resource 수정
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -0,0 +1,74 @@
+package org.springframework.jdbc.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcTemplate {
+
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+
+    private final DataSource dataSource;
+
+    public JdbcTemplate(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... objects) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            for (int i = 0; i < objects.length; i++) {
+                pstmt.setObject(i + 1, objects[i]);
+            }
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            for (int i = 0; i < objects.length; i++) {
+                pstmt.setObject(i + 1, objects[i]);
+            }
+            final ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rowMapper.execute(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql);
+             final ResultSet rs = pstmt.executeQuery()) {
+
+            log.debug("query : {}", sql);
+            final List<T> results = new ArrayList<>();
+            while (rs.next()) {
+                results.add(rowMapper.execute(rs));
+            }
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
@@ -0,0 +1,9 @@
+package org.springframework.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+    T execute(final ResultSet resultSet) throws SQLException;
+}
