b77af743094162afab2fccafa2b937e1a8b9513e
최준호 <junho5336@gmail.com>
2023-09-28 12:31:02 +0900

    style: 라인 포맷팅

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,56 +1,54 @@
 package com.techcourse.dao;
 
-import java.util.List;
-
-import javax.sql.DataSource;
-
+import com.techcourse.domain.User;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
-import com.techcourse.domain.User;
+import javax.sql.DataSource;
+import java.util.List;
 
 public class UserDao {
 
-	private static final RowMapper<User> USER_ROW_MAPPER = resultSet ->
-		new User(
-			resultSet.getLong("id"),
-			resultSet.getString("account"),
-			resultSet.getString("password"),
-			resultSet.getString("email")
-		);
+    private static final RowMapper<User> USER_ROW_MAPPER = resultSet ->
+            new User(
+                    resultSet.getLong("id"),
+                    resultSet.getString("account"),
+                    resultSet.getString("password"),
+                    resultSet.getString("email")
+            );
 
-	private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-	public UserDao(final DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
+    public UserDao(final DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 
-	public UserDao(final JdbcTemplate jdbcTemplate) {
-		this.jdbcTemplate = jdbcTemplate;
-	}
+    public UserDao(final JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
 
-	public void insert(final User user) {
-		final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-		jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
-	}
+    public void insert(final User user) {
+        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
+    }
 
-	public void update(final User user) {
-		final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
-		jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
-	}
+    public void update(final User user) {
+        final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
+    }
 
-	public List<User> findAll() {
-		final var sql = "select id, account, password, email from users";
-		return jdbcTemplate.query(sql, USER_ROW_MAPPER);
-	}
+    public List<User> findAll() {
+        final var sql = "select id, account, password, email from users";
+        return jdbcTemplate.query(sql, USER_ROW_MAPPER);
+    }
 
-	public User findById(final Long id) {
-		final var sql = "select id, account, password, email from users where id = ?";
-		return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, id);
-	}
+    public User findById(final Long id) {
+        final var sql = "select id, account, password, email from users where id = ?";
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, id);
+    }
 
-	public User findByAccount(final String account) {
-		final var sql = "select id, account, password, email from users where account = ?";
-		return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, account);
-	}
+    public User findByAccount(final String account) {
+        final var sql = "select id, account, password, email from users where account = ?";
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, account);
+    }
 }
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,9 @@
 package org.springframework.jdbc.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,78 +11,73 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class JdbcTemplate {
 
-	private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
-	private final DataSource dataSource;
+    private final DataSource dataSource;
 
-	public JdbcTemplate(final DataSource dataSource) {
-		this.dataSource = dataSource;
-	}
+    public JdbcTemplate(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
-	public void update(String sql, Object... args) {
-		try (Connection connection = dataSource.getConnection();
-			 PreparedStatement preparedStatement = getPreparedStatement(sql, connection, args)) {
-			preparedStatement.executeUpdate();
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			throw new IllegalArgumentException(e);
-		}
-	}
+    public void update(String sql, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = getPreparedStatement(sql, connection, args)) {
+            preparedStatement.executeUpdate();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
-		try (Connection connection = dataSource.getConnection();
-			 PreparedStatement preparedStatement = getPreparedStatement(sql, connection, args)) {
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = getPreparedStatement(sql, connection, args)) {
 
-			ResultSet resultSet = preparedStatement.executeQuery();
-			List<T> result = new ArrayList<>();
+            ResultSet resultSet = preparedStatement.executeQuery();
+            List<T> result = new ArrayList<>();
 
-			while (resultSet.next()) {
-				result.add(rowMapper.mapRow(resultSet));
-			}
+            while (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet));
+            }
 
-			return result;
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			throw new IllegalArgumentException(e);
-		}
-	}
+            return result;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
-		try (Connection connection = dataSource.getConnection();
-			 PreparedStatement preparedStatement = getPreparedStatement(sql, connection, args)) {
-			ResultSet resultSet = preparedStatement.executeQuery();
-			T result = null;
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = getPreparedStatement(sql, connection, args)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            T result = null;
 
-			while (resultSet.next()) {
-				if (result != null) {
-					throw new IllegalArgumentException("1개 이상의 결과가 존재합니다.");
-				}
-				result = rowMapper.mapRow(resultSet);
-			}
+            while (resultSet.next()) {
+                if (result != null) {
+                    throw new IllegalArgumentException("1개 이상의 결과가 존재합니다.");
+                }
+                result = rowMapper.mapRow(resultSet);
+            }
 
-			if (result == null) {
-				throw new IllegalArgumentException("결과가 존재하지 않습니다.");
-			}
-			return result;
-		} catch (Exception e) {
-			log.error(e.getMessage(), e);
-			throw new IllegalArgumentException(e);
-		}
-	}
+            if (result == null) {
+                throw new IllegalArgumentException("결과가 존재하지 않습니다.");
+            }
+            return result;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new IllegalArgumentException(e);
+        }
+    }
 
-	private PreparedStatement getPreparedStatement(String sql, Connection connection, Object... args) throws
-		SQLException {
-		PreparedStatement preparedStatement = connection.prepareStatement(sql);
-		for (int i = 0; i < args.length; i++) {
-			preparedStatement.setObject(i + 1, args[i]);
-		}
-		return preparedStatement;
-	}
+    private PreparedStatement getPreparedStatement(String sql, Connection connection, Object... args) throws
+            SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject(i + 1, args[i]);
+        }
+        return preparedStatement;
+    }
 }
