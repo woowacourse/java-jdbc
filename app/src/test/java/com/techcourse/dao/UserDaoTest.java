358bdeb8205162404f4c2a19eede57310065e1c8
zun <50602742+cpot5620@users.noreply.github.com>
2023-09-28 19:41:19 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 쥬니(전정준) 미션 제출합니다.  (#271)
    * feat: UserDao CRU 기능 구현
    
    * feat: JdbcTemplate 기능 구현
    
    * refactor: RowMapper 상수로 추출
    
    * refactor: final & var 키워드 제거
    
    * test: 테스트 격리를 위한 sql문 추가
    
    * style: 불필요한 개행 제거

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,68 +1,74 @@
 package com.techcourse.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 class UserDaoTest {
 
+    private final JdbcTemplate jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
     private UserDao userDao;
 
     @BeforeEach
     void setup() {
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
 
+        jdbcTemplate.update("TRUNCATE TABLE users RESTART IDENTITY");
         userDao = new UserDao(DataSourceConfig.getInstance());
-        final var user = new User("gugu", "password", "hkkang@woowahan.com");
+
+        User user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(user);
     }
 
     @Test
     void findAll() {
-        final var users = userDao.findAll();
+        List<User> users = userDao.findAll();
 
         assertThat(users).isNotEmpty();
     }
 
     @Test
     void findById() {
-        final var user = userDao.findById(1L);
+        userDao.insert(new User("gugu", "password", "hkkang@woowahan.com"));
+        User user = userDao.findById(1L);
 
         assertThat(user.getAccount()).isEqualTo("gugu");
     }
 
     @Test
     void findByAccount() {
-        final var account = "gugu";
-        final var user = userDao.findByAccount(account);
+        String account = "gugu";
+        User user = userDao.findByAccount(account);
 
         assertThat(user.getAccount()).isEqualTo(account);
     }
 
     @Test
     void insert() {
-        final var account = "insert-gugu";
-        final var user = new User(account, "password", "hkkang@woowahan.com");
+        String account = "insert-gugu";
+        User user = new User(account, "password", "hkkang@woowahan.com");
         userDao.insert(user);
 
-        final var actual = userDao.findById(2L);
+        User actual = userDao.findById(2L);
 
         assertThat(actual.getAccount()).isEqualTo(account);
     }
 
     @Test
     void update() {
-        final var newPassword = "password99";
-        final var user = userDao.findById(1L);
+        String newPassword = "password99";
+        User user = userDao.findById(1L);
         user.changePassword(newPassword);
 
         userDao.update(user);
 
-        final var actual = userDao.findById(1L);
+        User actual = userDao.findById(1L);
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -11,7 +17,77 @@ public class JdbcTemplate {
 
     private final DataSource dataSource;
 
-    public JdbcTemplate(final DataSource dataSource) {
+    public JdbcTemplate(DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public void update(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            initializePstmtArgs(pstmt, args);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void initializePstmtArgs(PreparedStatement pstmt, Object... args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+                ResultSet rs = pstmt.executeQuery()) {
+
+            List<T> results = new ArrayList<>();
+
+            while (rs.next()) {
+                T result = rowMapper.mapRow(rs);
+
+                results.add(result);
+            }
+
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = getQueryPstmtForObject(sql, conn, args);
+                ResultSet rs = pstmt.executeQuery()) {
+
+            if (rs.next()) {
+                T result = rowMapper.mapRow(rs);
+
+                if (rs.next()) {
+                    throw new IllegalArgumentException("Incorrect Result Size ! Result  must be one");
+                }
+
+                return result;
+            }
+
+            throw new NoSuchElementException("Incorrect Result Size ! Result is null");
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement getQueryPstmtForObject(String sql, Connection conn, Object... args) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+
+        initializePstmtArgs(pstmt, args);
+
+        return pstmt;
+    }
+
 }
