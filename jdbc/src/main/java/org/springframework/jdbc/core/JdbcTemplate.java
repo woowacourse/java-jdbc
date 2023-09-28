6f99e0e4700e4b3cdde8553a8ea0f912da71aef4
MoonJeWoong <munjin0201@naver.com>
2023-09-29 02:52:50 +0900

    feat: JdbcTemplate update 메서드 구현

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,11 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +15,24 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(String sql, Object... args) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            int setValueIdx = 1;
+            for (Object arg : args) {
+                pstmt.setObject(setValueIdx++, arg);
+            }
+
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,53 @@
 package nextstep.jdbc;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 class JdbcTemplateTest {
 
+    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSourceMock;
+    private Connection connectionMock;
+    private PreparedStatement preparedStatementMock;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        dataSourceMock = mock(DataSource.class);
+        connectionMock = mock(Connection.class);
+        preparedStatementMock = mock(PreparedStatement.class);
+
+        given(dataSourceMock.getConnection()).willReturn(connectionMock);
+        given(connectionMock.prepareStatement(anyString())).willReturn(preparedStatementMock);
+
+        jdbcTemplate = new JdbcTemplate(dataSourceMock);
+    }
+
+    @Test
+    void updateTest() throws SQLException {
+        //given
+        String sql = "insert into users (account, password, email) values (?, ?, ?)";
+        String account = "account";
+        String password = "pwd";
+        String email = "email";
+
+        //when
+        int result = jdbcTemplate.update(sql, account, password, email);
+
+        //then
+        then(preparedStatementMock).should(times(3)).setObject(anyInt(), any());
+        then(preparedStatementMock).should(times(1)).executeUpdate();
+    }
 }
