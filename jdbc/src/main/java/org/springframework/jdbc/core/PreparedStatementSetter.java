c4956e42d7bd22cb124c6c5cf92aba9b1e9486cf
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-09-28 23:01:50 +0900

    feat: insert 쿼리를 쉽게 작성할 수 있도록 jdbctemplate execute 메소드 구현

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementSetter.java
@@ -0,0 +1,24 @@
+package org.springframework.jdbc.core;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PreparedStatementSetter {
+
+    private final List<PreparedStatementTypeValue> values;
+
+    public PreparedStatementSetter(final List<Object> values) {
+        this.values = values.stream()
+                .map(PreparedStatementTypeValue::new)
+                .collect(Collectors.toList());
+    }
+
+    public void setPreparedStatement(final PreparedStatement ps) throws SQLException {
+        for (int i = 0; i < values.size(); i++) {
+            PreparedStatementTypeValue pstv = values.get(i);
+            pstv.setPreparedStatement(ps, i+1);
+        }
+    }
+}
---
+++ jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementTypeValue.java
@@ -0,0 +1,30 @@
+package org.springframework.jdbc.core;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class PreparedStatementTypeValue {
+
+    private final SqlType sqlType;
+    private final Object object;
+
+    public PreparedStatementTypeValue(final Object object) {
+        this.sqlType = SqlType.get(object);
+        this.object = object;
+    }
+
+    public void setPreparedStatement(final PreparedStatement ps, final int idx) throws SQLException {
+        if (sqlType == SqlType.STRING) {
+            ps.setString(idx, (String) object);
+        }
+        if (sqlType == SqlType.BOOLEAN) {
+            ps.setBoolean(idx, (boolean) object);
+        }
+        if (sqlType == SqlType.INT) {
+            ps.setInt(idx, (int) object);
+        }
+        if (sqlType == SqlType.LONG) {
+            ps.setLong(idx, (long) object);
+        }
+    }
+}
