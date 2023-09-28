ecb34d9d573e4f776d50ff7ac033af920f129523
Kong-hana01 <gksqlsl11@khu.ac.kr>
2023-09-29 07:42:15 +0900

    feat: dao에서 update 기능 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -15,30 +15,32 @@ public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
-    private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
 
     public UserDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = null;
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
 
-        PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter(
+        final PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter(
                 List.of(user.getAccount(), user.getPassword(), user.getEmail()));
         jdbcTemplate.execute(sql, preparedStatementSetter);
     }
 
     public void update(final User user) {
-        // todo
+        final var sql = "update users set (account, password, email) = (?, ?, ?) where id = ?";
+
+        final PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter(
+                List.of(user.getAccount(), user.getPassword(), user.getEmail(), user.getId()));
+        jdbcTemplate.execute(sql, preparedStatementSetter);
     }
+
 
     public List<User> findAll() {
         // todo
@@ -48,15 +50,15 @@ public class UserDao {
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        Map<String, Class<?>> values = new HashMap<>();
+        final Map<String, Class<?>> values = new HashMap<>();
         values.put("id", Long.class);
         values.put("account", String.class);
         values.put("password", String.class);
         values.put("email", String.class);
 
-        ResultSetGetter<User> userResultSetGetter = new ResultSetGetter<>(values, User.class);
-        PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter(List.of(id));
-        return (User) jdbcTemplate.find(sql, preparedStatementSetter, userResultSetGetter);
+        final PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter(List.of(id));
+        final ResultSetGetter<User> userResultSetGetter = new ResultSetGetter<>(values, User.class);
+        return jdbcTemplate.find(sql, preparedStatementSetter, userResultSetGetter);
     }
 
     public User findByAccount(final String account) {
