23226fa096e13fe7229bbf9ff84bb1ef63791717
ReO <yt930310@gmail.com>
2023-09-29 18:48:12 +0900

    refactor: final 키워드 제거

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -33,24 +33,24 @@ public class UserDao {
     }
 
     public void update(User user) {
-        final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        String sql = "update users set account = ?, password = ?, email = ? where id = ?";
 
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
-        final String sql = "select id, account, password, email from users";
+        String sql = "select id, account, password, email from users";
         return jdbcTemplate.query(sql, rowMapper);
     }
 
-    public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
+    public User findById(Long id) {
+        String sql = "select id, account, password, email from users where id = ?";
 
         return jdbcTemplate.queryForObject(sql, rowMapper, id);
     }
 
-    public User findByAccount(final String account) {
-        final var sql = "select id, account, password, email from users where account = ?";
+    public User findByAccount(String account) {
+        String sql = "select id, account, password, email from users where account = ?";
 
         return jdbcTemplate.queryForObject(sql, rowMapper, account);
     }
