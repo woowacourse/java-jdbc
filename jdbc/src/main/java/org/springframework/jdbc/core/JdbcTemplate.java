c5e4217d1fbd562fdb3d6bb6a112b3301cb53524
Go-Jaecheol <gojaech@naver.com>
2023-09-29 23:48:35 +0900

    feat: JdbcTemplate 구현

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,8 +2,14 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,56 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... objects) {
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = prepareStatement(connection, sql, objects)) {
+            log.debug("query : {}", sql);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = prepareStatement(connection, sql, objects);
+             final var resultSet = preparedStatement.executeQuery()) {
+            log.debug("query : {}", sql);
+            if (resultSet.next()) {
+                return rowMapper.mapRow(resultSet);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql);
+             final var resultSet = preparedStatement.executeQuery()) {
+            log.debug("query : {}", sql);
+            final var results = new ArrayList<T>();
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet));
+            }
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private PreparedStatement prepareStatement(final Connection connection, final String sql, final Object[] objects)
+            throws SQLException {
+        final var preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < objects.length; i++) {
+            preparedStatement.setObject(i + 1, objects[i]);
+        }
+        return preparedStatement;
     }
 }
