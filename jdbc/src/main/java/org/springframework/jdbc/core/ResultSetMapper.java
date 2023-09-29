484d89a7c43916e5513ae5381fa2d48b5cd52683
chaewon121 <cw121@naver.com>
2023-09-29 19:24:41 +0900

    feat: jdbc템플릿 구현

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/ResultSetMapper.java
@@ -0,0 +1,10 @@
+package org.springframework.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface ResultSetMapper<T> {
+
+    T apply(ResultSet resultSet) throws SQLException;
+}
