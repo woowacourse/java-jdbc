60a9092a604b3a5f10616f886848ff9bd3bdcc2b
pilyang <pilyang.dev@gmail.com>
2023-09-29 17:55:41 +0900

    feat: JdbcTeamplate update 구현
    - JdbcTemplate update 구현
    - UserDao insert를 jdbcTemplate update를 사용하도록 리팩터링

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class JdbcTemplate {
 
@@ -13,5 +16,20 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            for (int parameterIndex = 0; parameterIndex < args.length; parameterIndex++) {
+                pstmt.setString(parameterIndex + 1, (String) args[parameterIndex]);
+            }
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
