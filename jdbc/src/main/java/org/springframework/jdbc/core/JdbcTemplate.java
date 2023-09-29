b36bd78bd7dc9c705173d259548484b5f790f20d
Bojun Kim <qhwns1649@naver.com>
2023-09-29 18:38:25 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 포이(김보준) 미션 제출합니다.  (#269)
    * 패키지 위치 변경 및 코드 정리
    
    * feat: JDBC Template 추가
    
    * fix : 몇몇 메서드들이 예외를 명시적으로 던지도록 변경
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+import javax.annotation.Nullable;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,93 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void execute(final String sql, final Object... params) throws DataAccessException {
+        log.debug("query : {}", sql);
+        execute(sql, new PreparedStatementCreator(), ps -> {
+            doSetValue(params, ps);
+            ps.execute();
+            return null;
+        });
+    }
+
+    public int update(final String sql, final Object... params) throws DataAccessException {
+        log.debug("query : {}", sql);
+        return updateCount(execute(
+                sql,
+                new PreparedStatementCreator(),
+                ps -> {
+                    doSetValue(params, ps);
+                    return ps.executeUpdate();
+                }
+        ));
+    }
+
+    private int updateCount(@Nullable final Integer result) throws DataAccessException {
+        if (result == null) {
+            throw new DataAccessException("no update count");
+        }
+        return result;
+    }
+
+    @Nullable
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) throws DataAccessException {
+        final var results = query(sql, rowMapper, params);
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results.get(0);
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... params) throws DataAccessException {
+        log.debug("query : {}", sql);
+        return results(execute(
+                sql,
+                new PreparedStatementCreator(),
+                ps -> {
+                    doSetValue(params, ps);
+                    final var rs = ps.executeQuery();
+                    final var extractor = new ResultSetExtractor<>(rs, rowMapper);
+                    return extractor.extractData();
+                }
+        ));
+    }
+
+    private <T> List<T> results(@Nullable final List<T> results) throws DataAccessException {
+        if (results == null) {
+            throw new DataAccessException("no result");
+        }
+        return results;
+    }
+
+    private void doSetValue(final Object[] params, final PreparedStatement ps) throws SQLException {
+        for (int i = 1; i <= params.length; i++) {
+            ps.setObject(i, params[i - 1]);
+        }
+    }
+
+    @Nullable
+    private <T> T execute(
+            final String sql,
+            final PreparedStatementCreator psc,
+            final PreparedStatementCallback<T> action
+    ) throws DataAccessException {
+        try (
+                final var conn = getConnection();
+                final var ps = psc.createPreparedStatement(conn, sql)
+        ) {
+            return action.doInPreparedStatement(ps);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new CannotGetJdbcConnectionException(e.getMessage());
+        }
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,95 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
 class JdbcTemplateTest {
 
+    @Test
+    void execute() throws SQLException {
+
+        //given
+        final var mockDataSource = Mockito.mock(DataSource.class);
+        final var mockConnection = Mockito.mock(Connection.class);
+        final var mockPreparedStatement = Mockito.mock(PreparedStatement.class);
+
+        Mockito.when(DataSourceUtils.getConnection(mockDataSource)).thenReturn(mockConnection);
+        Mockito.when(mockDataSource.getConnection()).thenReturn(mockConnection);
+        Mockito.when(mockConnection.prepareStatement(Mockito.anyString())).thenReturn(mockPreparedStatement);
+        Mockito.when(mockPreparedStatement.executeUpdate()).thenReturn(1);
+
+        final var jdbcTemplate = new JdbcTemplate(mockDataSource);
+
+        //when
+        jdbcTemplate.execute("insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)", 1L, "account", "password", "email",
+                "2021-08-10 00:00:00", "admin");
+
+        //then
+        Mockito.verify(mockDataSource, Mockito.only()).getConnection();
+        Mockito.verify(mockConnection, Mockito.times(1)).prepareStatement(Mockito.anyString());
+        Mockito.verify(mockPreparedStatement, Mockito.times(1)).setObject(1, 1L);
+        Mockito.verify(mockPreparedStatement, Mockito.times(1)).setObject(2, "account");
+        Mockito.verify(mockPreparedStatement, Mockito.times(1)).setObject(3, "password");
+        Mockito.verify(mockPreparedStatement, Mockito.times(1)).setObject(4, "email");
+        Mockito.verify(mockPreparedStatement, Mockito.times(1)).setObject(5, "2021-08-10 00:00:00");
+        Mockito.verify(mockPreparedStatement, Mockito.times(1)).setObject(6, "admin");
+        Mockito.verify(mockPreparedStatement, Mockito.times(1)).executeUpdate();
+    }
+
+    @Test
+    void query() throws SQLException {
+        //given
+        final var mockDataSource = Mockito.mock(DataSource.class);
+        final var mockConnection = Mockito.mock(Connection.class);
+        final var mockPreparedStatement = Mockito.mock(PreparedStatement.class);
+        final var mockResultSet = Mockito.mock(ResultSet.class);
+
+        Mockito.when(mockDataSource.getConnection()).thenReturn(mockConnection);
+        Mockito.when(mockConnection.prepareStatement(Mockito.anyString())).thenReturn(mockPreparedStatement);
+        Mockito.when(mockPreparedStatement.executeQuery()).thenReturn(mockResultSet);
+        Mockito.when(mockResultSet.next()).thenReturn(true).thenReturn(false);
+        Mockito.when(mockResultSet.getObject(Mockito.anyString())).thenReturn(1L).thenReturn("account").thenReturn("password").thenReturn("email").thenReturn("2021-08-10 00:00:00")
+               .thenReturn("admin");
+
+        final var jdbcTemplate = new JdbcTemplate(mockDataSource);
+        final var params = new ArrayList<>();
+
+        //when
+        jdbcTemplate.query("select * from user_history where user_id = ?", rs -> {
+            params.add(rs.getObject("user_id"));
+            params.add(rs.getObject("account"));
+            params.add(rs.getObject("password"));
+            params.add(rs.getObject("email"));
+            params.add(rs.getObject("created_at"));
+            params.add(rs.getObject("created_by"));
+            return null;
+        }, 1L);
+
+        //then
+        assertAll(
+                () -> Mockito.verify(mockDataSource, Mockito.only()).getConnection(),
+                () -> Mockito.verify(mockConnection, Mockito.times(1)).prepareStatement(Mockito.anyString()),
+                () -> Mockito.verify(mockPreparedStatement, Mockito.times(1)).setObject(1, 1L),
+                () -> Mockito.verify(mockPreparedStatement, Mockito.times(1)).executeQuery(),
+                () -> Mockito.verify(mockResultSet, Mockito.times(2)).next(),
+                () -> Mockito.verify(mockResultSet, Mockito.times(1)).getObject("user_id"),
+                () -> Mockito.verify(mockResultSet, Mockito.times(1)).getObject("account"),
+                () -> Mockito.verify(mockResultSet, Mockito.times(1)).getObject("password"),
+                () -> Mockito.verify(mockResultSet, Mockito.times(1)).getObject("email"),
+                () -> Mockito.verify(mockResultSet, Mockito.times(1)).getObject("created_at"),
+                () -> Mockito.verify(mockResultSet, Mockito.times(1)).getObject("created_by"),
+                () -> assertThat(params).containsExactly(1L, "account", "password", "email", "2021-08-10 00:00:00", "admin")
+        );
+    }
 }
