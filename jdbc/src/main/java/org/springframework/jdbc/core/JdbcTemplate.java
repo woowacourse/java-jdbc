de6075b7b29bc358a34c06cbbaaf45a274853b2b
kokodak <kokodakadokok@gmail.co>
2023-09-30 02:31:02 +0900

    feat: 코드 중복을 제거하는 JdbcTemplate 기능 구현

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,80 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... objects) {
+        validateBindableSql(sql, objects);
+
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = prepareStatementWithBindingQuery(connection.prepareStatement(sql), objects)) {
+
+            log.debug("query : {}", sql);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validateBindableSql(final String sql, final Object... objects) {
+        int count = 0;
+        for (int i = 0; i < sql.length(); i++) {
+            if (sql.charAt(i) == '?') {
+                count++;
+            }
+        }
+        if (count != objects.length) {
+            throw new IllegalArgumentException("Can Not Binding");
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = prepareStatementWithBindingQuery(connection.prepareStatement(sql), objects);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            final List<T> results = new ArrayList<>();
+
+            log.debug("query : {}", sql);
+
+            while (resultSet.next()) {
+                results.add(rowMapper.mapping(resultSet));
+            }
+            return results;
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
+        validateBindableSql(sql, objects);
+
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = prepareStatementWithBindingQuery(connection.prepareStatement(sql), objects);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            log.debug("query : {}", sql);
+
+            if (resultSet.next()) {
+                return Optional.of(rowMapper.mapping(resultSet));
+            }
+            return Optional.empty();
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement prepareStatementWithBindingQuery(final PreparedStatement preparedStatement, final Object... objects) throws SQLException {
+        for (int i = 0; i < objects.length; i++) {
+            preparedStatement.setObject(i + 1, objects[i]);
+        }
+        return preparedStatement;
     }
 }
