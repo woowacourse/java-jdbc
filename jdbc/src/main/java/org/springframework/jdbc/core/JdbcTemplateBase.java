d8f353009973d34038b60ab19a345c3b29f27f19
splitCoding <voicesplit@daum.net>
2023-09-30 02:45:46 +0900

    refactor: try-finally 를 try-with-resources 로 변경

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplateBase.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplateBase.java
@@ -3,7 +3,6 @@ package org.springframework.jdbc.core;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Objects;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,56 +17,27 @@ public class JdbcTemplateBase {
     }
 
     protected void executionBaseWithNonReturn(final String sql, final JdbcTemplateVoidExecution execution) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
+        try (
+            final Connection connection = dataSource.getConnection();
+            final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
 
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
             execution.execute(preparedStatement);
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (Objects.nonNull(preparedStatement)) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException e) {
-            }
-            try {
-                if (Objects.nonNull(connection)) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-            }
         }
     }
 
     protected <T> T executionBaseWithReturn(final String sql, final JdbcTemplateExecution<T> execution) {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = connection.prepareStatement(sql);
+
+        try (final Connection connection = dataSource.getConnection();
+            final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
             log.debug("query : {}", sql);
+            
             return execution.execute(preparedStatement);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (preparedStatement != null) {
-                    preparedStatement.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 }
