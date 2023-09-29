81363a45bacb1c938841948586a611a0e5c24c03
Hanuel Lee <91522259+hanueleee@users.noreply.github.com>
2023-09-29 14:13:06 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 오잉(이하늘) 미션 제출합니다. (#283)
    * 패키지 위치 변경 및 코드 정리
    
    * feat: UserDao의 update, findAll, findByAccount 기능 구현
    
    * feat: UserDao의 Insert 기능 JdbcTemplate로 이전
    
    * feat: UserDao의 update 기능 JdbcTemplate로 이전
    
    * refactor: JdbcTemplate의 중복 로직 제거 (update & insert -> update로 통합)
    
    * feat: ResultSet 결과를 매핑하기 위한 RowMapper 인터페이스 구현
    
    * feat: ResultSet을 User로 매핑하기 위한 UserMapper 구현
    
    * feat: JdbcTemplate에 query메소드 추가 (select)
    
    * refactor: UserDao의 findById와 findByAccount에 JdbcTemplate의 query 메소드 사용
    
    * refactor: JdbcTemplate의 중복 로직(사용한 리소스 close) 추출
    
    * feat: JdbcTemplate에 queryForList 메소드 추가 (결과가 여러개인 select)
    
    * refactor: UserDao의 findAll에 JdbcTemplate의 queryForList 메소드 사용
    
    * refactor: UserDao에서 DataSource 제거
    
    * refactor: JdbcTemplate에 try-with-resources 적용
    
    * refactor: UserMapper 제거하고 UserDao에 람다식으로 표현
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,11 +1,16 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
-
-public class JdbcTemplate {
+public class JdbcTemplate<T> {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
@@ -13,5 +18,62 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatement(conn, sql, args)) {
+
+            log.debug("query : {}", sql);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public T queryForObject(String sql, RowMapper<T> mapper, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatement(conn, sql, args);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            log.debug("query : {}", sql);
+
+            if (rs.next()) {
+                return mapper.map(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<T> queryForList(String sql, RowMapper<T> mapper, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatement(conn, sql, args);
+             ResultSet rs = pstmt.executeQuery()) {
+
+            log.debug("query : {}", sql);
+
+            List<T> results = new ArrayList<>();
+            while (rs.next()) {
+                T result = mapper.map(rs);
+                results.add(result);
+            }
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement getPreparedStatement(final Connection conn, final String sql, Object... args) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+        return pstmt;
     }
 }
