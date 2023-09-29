7c22c7b1f514d6c002bef7092dfc4ea2a41348ff
JunH <tjdtls690@gmail.com>
2023-09-29 22:42:30 +0900

    :art: refactor : 쿼리 로깅 추가

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -3,14 +3,10 @@ package com.techcourse.dao;
 import com.techcourse.domain.User;
 import java.util.List;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
-
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
     private final JdbcTemplate jdbcTemplate;
 
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,7 +2,6 @@ package org.springframework.jdbc.core;
 
 import static java.util.Objects.requireNonNull;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -28,9 +27,9 @@ public class JdbcTemplate {
             setParameters(params, preparedStatement);
 
             try (final var resultSet = preparedStatement.executeQuery()) {
+                log.info("JDBC QUERY SQL = {}", sql);
                 return getResults(rowMapper, resultSet);
             }
-
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -53,6 +52,7 @@ public class JdbcTemplate {
 
             try (final var resultSet = preparedStatement.executeQuery()) {
                 if (resultSet.next()) {
+                    log.info("JDBC QUERY_FOR_OBJECT SQL = {}", sql);
                     return rowMapper.mapRow(resultSet);
                 }
             }
@@ -68,6 +68,7 @@ public class JdbcTemplate {
              final var preparedStatement = connection.prepareStatement(sql)) {
             setParameters(params, preparedStatement);
             preparedStatement.executeUpdate();
+            log.info("JDBC UPDATE SQL = {}", sql);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
