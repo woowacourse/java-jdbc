7a04db5f0a66ab25116f107a7399d1538d3bf76a
java-saeng <62413589+java-saeng@users.noreply.github.com>
2023-09-29 20:06:08 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 우르(김현우) 미션 제출합니다.  (#314)
    * feat : UserDao 모든 기능 구현
    
    * refactor : UserDao#findById 에서 JdbcTemplate 사용
    
    * refactor : UserDao#findAll 에서 JdbcTemplate 사용
    
    * refactor : UserDao#update 에서 JdbcTemplate 사용
    
    * refactor : UserDao#insert 에서 JdbcTemplate 사용
    
    * refactor : UserDao#findByAccount 에서 JdbcTemplate 사용
    
    * refactor : UserDao에서 DataSource 삭제
    
    * refactor : 쿼리 결과로 User를 생성하는 중복 코드 메서드 extract

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,121 +1,73 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
+  private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
-    private final DataSource dataSource;
+  private final JdbcTemplate jdbcTemplate;
 
-    public UserDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
+  public UserDao(final JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  public void insert(final User user) {
+    final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+
+    jdbcTemplate.execute(sql, user.getAccount(), user.getPassword(), user.getEmail());
+  }
+
+  public void update(final User user) {
+    final String sql = "update users set password = ? where id = ?";
+
+    jdbcTemplate.execute(sql, user.getPassword(), user.getId());
+  }
+
+  public List<User> findAll() {
+    final var sql = "select id, account, password, email from users";
+
+    final List<Map<Integer, Object>> results = jdbcTemplate.queryPlural(sql);
+
+    List<User> users = new ArrayList<>();
+
+    for (int i = 0; i < results.size(); i++) {
+      final Map<Integer, Object> result = results.get(i);
+
+      users.add(createUserFrom(result));
     }
 
-    public UserDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = null;
-    }
+    return users;
+  }
 
-    public void insert(final User user) {
-        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+  public User findById(final Long id) {
+    final var sql = "select id, account, password, email from users where id = ?";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
+    final Map<Integer, Object> result = jdbcTemplate.query(sql, id);
 
-            log.debug("query : {}", sql);
+    return createUserFrom(result);
+  }
 
-            pstmt.setString(1, user.getAccount());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getEmail());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
+  public User findByAccount(final String account) {
+    final var sql = "select id, account, password, email from users where account = ?";
 
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
-        }
-    }
+    final Map<Integer, Object> result = jdbcTemplate.query(sql, account);
 
-    public void update(final User user) {
-        // todo
-    }
+    return createUserFrom(result);
+  }
 
-    public List<User> findAll() {
-        // todo
-        return null;
-    }
-
-    public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
-        }
-    }
-
-    public User findByAccount(final String account) {
-        // todo
-        return null;
-    }
+  private User createUserFrom(final Map<Integer, Object> result) {
+    return new User(
+        (Long) result.get(1),
+        (String) result.get(2),
+        (String) result.get(3),
+        (String) result.get(4)
+    );
+  }
 }
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,97 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
-
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+  private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
-    private final DataSource dataSource;
+  private final DataSource dataSource;
 
-    public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+  public JdbcTemplate(final DataSource dataSource) {
+    this.dataSource = dataSource;
+  }
+
+  public Map<Integer, Object> query(final String sql, final Object... values) {
+
+    try (final Connection conn = dataSource.getConnection();
+        final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+      for (int index = 1; index <= values.length; index++) {
+        pstmt.setObject(index, values[index - 1]);
+      }
+
+      final ResultSet resultSet = pstmt.executeQuery();
+
+      final Map<Integer, Object> result = new HashMap<>();
+
+      if (resultSet.next()) {
+        result.put(1, resultSet.getLong(1));
+        result.put(2, resultSet.getString(2));
+        result.put(3, resultSet.getString(3));
+        result.put(4, resultSet.getString(4));
+      }
+
+      return result;
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException(e);
     }
+  }
+
+  public List<Map<Integer, Object>> queryPlural(final String sql, final Object... values) {
+    try (final Connection conn = dataSource.getConnection();
+        final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+      for (int index = 1; index <= values.length; index++) {
+        pstmt.setObject(index, values[index - 1]);
+      }
+
+      final ResultSet resultSet = pstmt.executeQuery();
+
+      final List<Map<Integer, Object>> result = new ArrayList<>();
+
+      int index = 0;
+      while (resultSet.next()) {
+        result.add(new HashMap<>());
+
+        result.get(index).put(1, resultSet.getLong(1));
+        result.get(index).put(2, resultSet.getString(2));
+        result.get(index).put(3, resultSet.getString(3));
+        result.get(index).put(4, resultSet.getString(4));
+
+        index++;
+      }
+
+      return result;
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void execute(final String sql, final Object... values) {
+    try (final Connection conn = dataSource.getConnection();
+        final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+      for (int index = 1; index <= values.length; index++) {
+        pstmt.setObject(index, values[index - 1]);
+      }
+
+      pstmt.executeUpdate();
+    } catch (SQLException e) {
+      log.error(e.getMessage(), e);
+      throw new RuntimeException(e);
+    }
+  }
 }
