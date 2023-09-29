131d64a314ce904c6daedb56b96b03289efe96db
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-09-29 14:12:05 +0900

    refactor: 인터페이스 접근 제어자 제거

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 public interface UserDao {
 
-    public void insert(final User user);
+    void insert(final User user);
 
-    public void update(final User user);
+    void update(final User user);
 
-    public List<User> findAll();
+    List<User> findAll();
 
-    public User findById(final Long id);
+    User findById(final Long id);
 
-    public User findByAccount(final String account);
+    User findByAccount(final String account);
 
 }
