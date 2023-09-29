5d3d60e70c217b4a7caa6a2a292bb58fa3d0d577
gitchan <eunkeeeee@gmail.com>
2023-09-29 18:38:57 +0900

    refactor: 테스트 코드 auto_increment 아이디에 의존하지 않도록 수정

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -3,6 +3,7 @@ package com.techcourse.dao;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +22,11 @@ class UserDaoTest {
         userDao.insert(user);
     }
 
+    @AfterEach
+    void tearDown() {
+        userDao.deleteAll();
+    }
+
     @Test
     void findAll() {
         final var users = userDao.findAll();
@@ -30,9 +36,10 @@ class UserDaoTest {
 
     @Test
     void findById() {
-        final var user = userDao.findById(1L);
+        User user = userDao.findByAccount("gugu");
+        User findUser = userDao.findById(user.getId());
 
-        assertThat(user.getAccount()).isEqualTo("gugu");
+        assertThat(findUser.getAccount()).isEqualTo("gugu");
     }
 
     @Test
@@ -57,12 +64,12 @@ class UserDaoTest {
     @Test
     void update() {
         final var newPassword = "password99";
-        final var user = userDao.findById(1L);
+        final var user = userDao.findByAccount("gugu");
         user.changePassword(newPassword);
 
         userDao.update(user);
 
-        final var actual = userDao.findById(1L);
+        final var actual = userDao.findByAccount("gugu");
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
