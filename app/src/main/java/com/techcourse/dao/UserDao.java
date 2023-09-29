5b2b024f4c7efcea9ca8dcf57f95874124c73afb
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-09-30 04:04:29 +0900

    feat: UserDao의 findByAccount 기능 추가

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -54,7 +54,8 @@ public class UserDao {
     }
 
     public User findByAccount(final String account) {
-        // todo
-        return null;
+        final var sql = "select id, account, password, email from users where account = ?";
+
+        return jdbcTemplate.queryForObject(sql, rowMapper, account);
     }
 }
