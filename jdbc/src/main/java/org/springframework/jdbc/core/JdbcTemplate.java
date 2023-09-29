3ef112d379679893b1aa74b7ec71ea229e1ed185
xxeol2 <jhj01177@naver.com>
2023-09-29 15:18:45 +0900

    feat: update, queryForObject 구현

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,12 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +16,41 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    private static void setUpPreparedStatement(PreparedStatement prepareStatement, Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            prepareStatement.setObject(i + 1, args[i]);
+        }
+    }
+
+    public void update(String sql, Object... args) {
+        try (
+            Connection conn = dataSource.getConnection();
+            PreparedStatement prepareStatement = conn.prepareStatement(sql)
+        ) {
+            setUpPreparedStatement(prepareStatement, args);
+            prepareStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (
+            Connection conn = dataSource.getConnection();
+            PreparedStatement prepareStatement = conn.prepareStatement(sql)
+        ) {
+            setUpPreparedStatement(prepareStatement, args);
+            ResultSet resultSet = prepareStatement.executeQuery();
+            if (resultSet.next()) {
+                return rowMapper.mapRow(resultSet);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
