ba2f72da7db04b2f91f2ab020f7bf5ad8780b204
MoonJeWoong <munjin0201@naver.com>
2023-09-29 17:55:20 +0900

    feat: UserDao findByAccount, update 메서드 기능 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,15 +2,11 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import java.util.List;
-import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
     private static final RowMapper<User> USER_ROW_MAPPER = (rs, rowNum) -> new User(
             rs.getLong(1),
             rs.getString(2),
@@ -19,10 +15,6 @@ public class UserDao {
     );
 
     private final JdbcTemplate jdbcTemplate;
-
-    public UserDao(final DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
@@ -34,7 +26,14 @@ public class UserDao {
     }
 
     public void update(final User user) {
-        // todo
+        String sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        jdbcTemplate.update(
+                sql,
+                user.getAccount(),
+                user.getPassword(),
+                user.getPassword(),
+                user.getId()
+        );
     }
 
     public List<User> findAll() {
@@ -48,7 +47,7 @@ public class UserDao {
     }
 
     public User findByAccount(final String account) {
-        // todo
-        return null;
+        String sql = "select id, account, password, email from users where account = ?";
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, account);
     }
 }
