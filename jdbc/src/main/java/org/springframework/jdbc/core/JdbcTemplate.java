a3e9fd767dcd1c20810bfc070b721e474f8d8dfb
Sung Chaeyeon <ch.yeondev@gmail.com>
2023-09-29 20:12:01 +0900

    feat: jdbc 라이브러리 구현 (#299)
    저의 전 페어 이리네..오랜만이네요 ㅎㅎ 잘지네시죠!?
    이렇게 만나뵙게되어서 방갑네요~!
    궁금한 부분 질문 드렸고 꼭 수정해야할 부분은 보이지 않아 (너무 잘 구현해주셔서..!) 머지하겠습니다! 
    고생 많으셨습니다 ㅎㅎ!

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,68 @@
 package org.springframework.jdbc.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Nullable;
 import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
 
 public class JdbcTemplate {
-
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            setParameters(pstmt, args);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public void setParameters(final PreparedStatement pstmt, final Object... args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+    }
+
+    @Nullable
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            setParameters(pstmt, args);
+            try (final ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return rowMapper.mapRow(rs);
+                }
+                return null;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            setParameters(pstmt, args);
+            try (final ResultSet rs = pstmt.executeQuery()) {
+                List<T> results = new ArrayList<>();
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs));
+                }
+                return results;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
