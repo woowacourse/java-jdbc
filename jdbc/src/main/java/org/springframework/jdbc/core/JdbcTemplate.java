bf8a6f1449d949d0967c8a3068f3bd0615a7dd5c
헙크 <chocomilk8604@gmail.com>
2023-09-30 18:40:48 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 헙크(정현승) 미션 제출합니다. (#294)
    * feat: JdbcTemplate 뼈대 구현 및 findById 로직 구현
    
    * feat: findAll() 로직 구현
    
    * feat: update, insert 구현
    
    * feat: findByAccount 구현
    
    * refactor: queryForObject의 반환 타입을 Optional로 개선
    
    * refactor: 예외 변환 타입 수정
    
    * refactor: 테스트 케이스 롤백 및 UserDao로 로직 이동
    
    * refactor: UserDao의 RowMapper 반환값 상수화

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,65 @@
 package org.springframework.jdbc.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class JdbcTemplate {
-
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, Object... args) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i + 1, args[i]);
+            }
+            try (final ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return Optional.ofNullable(rowMapper.mapRow(rs));
+                }
+                return Optional.empty();
+            }
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) throws DataAccessException {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            final List<T> result = new ArrayList<>();
+            try (final ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    result.add(rowMapper.mapRow(rs));
+                }
+                return result;
+            }
+
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i + 1, args[i]);
+            }
+            return pstmt.executeUpdate();
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
