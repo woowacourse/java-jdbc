03217573237cfad2f9b7e1b204042a73ae0c5242
chaewon121 <80631952+chaewon121@users.noreply.github.com>
2023-09-30 12:19:09 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 채채(신채원) 미션 제출합니다. (#325)
    * feat: jdbc템플릿 구현
    
    * refactor: userDao 수정

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
@@ -14,4 +20,65 @@ public class JdbcTemplate {
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public void update(final String sql,
+                       final Object... parameters) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(sql, connection, parameters)) {
+            log.debug("query : {}", sql);
+            for (int i = 0; i < parameters.length; i++) {
+                preparedStatement.setObject(i + 1, parameters[i]);
+            }
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql,
+                                final ResultSetMapper<T> rowMapper,
+                                final Object... parameters) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(sql, connection, parameters);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+            log.debug("query : {}", sql);
+            if (resultSet.next()) {
+                return rowMapper.apply(resultSet);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement getPreparedStatement(final String sql,
+                                                   final Connection connection,
+                                                   final Object[] parameters) throws SQLException {
+        final PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < parameters.length; i++) {
+            preparedStatement.setObject(i + 1, parameters[i]);
+        }
+        return preparedStatement;
+    }
+
+    public <T> List<T> queryForList(final String sql,
+                                    final ResultSetMapper<T> rowMapper,
+                                    final Object... parameters) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = getPreparedStatement(sql, connection, parameters);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+            log.debug("query : {}", sql);
+            final List<T> result = new ArrayList<>();
+            while (resultSet.next()) {
+                result.add(rowMapper.apply(resultSet));
+            }
+            return result;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
 }
+
