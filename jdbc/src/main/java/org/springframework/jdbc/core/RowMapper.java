7e5d3974b8319833b9f6b98a1edaf96bdee515c8
ReO <yt930310@gmail.com>
2023-10-01 01:45:36 +0900

    refactor: RowMapper 외부로 분리

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
@@ -0,0 +1,9 @@
+package org.springframework.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+    T run(ResultSet resultSet) throws SQLException;
+}
