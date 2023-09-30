5d40922fe4aa8a38a72e551a3bb159ba8459ed76
Youngho <cyh6099@gmail.com>
2023-10-01 01:58:52 +0900

    feat: 단건 조회 시 예외처리

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,13 +2,13 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 public class JdbcTemplate {
 
@@ -28,10 +28,13 @@ public class JdbcTemplate {
         return queryTemplate.query(sql, (pstmt -> {
             try (ResultSet rs = pstmt.executeQuery()) {
                 log.debug("query : {}", sql);
-                if (rs.next()) {
-                    return rowMapper.map(rs);
+                if (!rs.first()) {
+                    throw new DataAccessException("조건에 맞는 데이터가 없습니다.");
                 }
-                throw new NoSuchElementException("조건에 맞는 데이터가 없습니다.");
+//                if (rs.last() && rs.getRow() > 1) {
+//                    throw new DataAccessException("단건 조회에 조회된 데이터가 2개 이상입니다.");
+//                }
+                return rowMapper.map(rs);
             }
         }), parameters);
     }
