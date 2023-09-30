da59093593a564d4e0c4f21c3ee5ceafa9ceb424
yujamint <woawoa0513@gmail.com>
2023-09-30 17:25:31 +0900

    chore: TransactionManager 주석 처리

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,9 +2,9 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.transaction.support.TransactionManager;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,27 +16,26 @@ public class JdbcTemplate {
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
     private final PreparedStatementCreator preparedStatementCreator = new PreparedStatementCreator();
-    private final TransactionManager transactionManager;
+    private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.transactionManager = new TransactionManager(dataSource);
+        this.dataSource = dataSource;
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        return transactionManager.executeWithReadOnlyTransaction(conn -> {
-            try (final PreparedStatement pstmt = preparedStatementCreator.createPreparedStatement(conn, sql, args);
-                 final ResultSet rs = pstmt.executeQuery()
-            ) {
-                if (rs.last()) {
-                    validateSingleRow(rs);
-                    return rowMapper.mapRow(rs);
-                }
-            } catch (SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = preparedStatementCreator.createPreparedStatement(conn, sql, args);
+             final ResultSet rs = pstmt.executeQuery()
+        ) {
+            if (rs.last()) {
+                validateSingleRow(rs);
+                return rowMapper.mapRow(rs);
             }
-            return null;
-        });
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+        return null;
     }
 
     private void validateSingleRow(final ResultSet rs) throws SQLException {
@@ -46,31 +45,29 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, Object... args) {
-        return transactionManager.executeWithReadOnlyTransaction(conn -> {
-            try (final PreparedStatement pstmt = preparedStatementCreator.createPreparedStatement(conn, sql, args);
-                 final ResultSet rs = pstmt.executeQuery()
-            ) {
-                final List<T> results = new ArrayList<>();
-                while (rs.next()) {
-                    results.add(rowMapper.mapRow(rs));
-                }
-                return results;
-            } catch (SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = preparedStatementCreator.createPreparedStatement(conn, sql, args);
+             final ResultSet rs = pstmt.executeQuery()
+        ) {
+            final List<T> results = new ArrayList<>();
+            while (rs.next()) {
+                results.add(rowMapper.mapRow(rs));
             }
-        });
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 
     public void execute(final String sql, final Object... args) {
-        transactionManager.executeWithTransaction(conn -> {
-            try (final PreparedStatement pstmt = preparedStatementCreator.createPreparedStatement(conn, sql, args)) {
-                pstmt.executeUpdate();
-            } catch (SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
-            }
-        });
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = preparedStatementCreator.createPreparedStatement(conn, sql, args)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 
 }
