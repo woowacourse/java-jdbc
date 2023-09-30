8e4ed2e63302c9429ce5a971b27550bec4e16bf8
bjk1649 <qhwns1649@naver.com>
2023-09-30 22:22:27 +0900

    refactor: insert에서 execute대신 update를 이용하도록 변경

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -27,7 +27,7 @@ public class UserDao {
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.execute(sql, user.getAccount(), user.getPassword(), user.getEmail());
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public int update(final User user) {
