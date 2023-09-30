459ab91405cf3e555dfdf8f557c97e908d02a1e7
Jae_Philip_Yang <pilyang.dev@gmail.com>
2023-10-01 02:57:20 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 필립(양재필) 미션 제출합니다. (#326)
    * feat: JdbcTeamplate update 구현
    
    - JdbcTemplate update 구현
    - UserDao insert를 jdbcTemplate update를 사용하도록 리팩터링
    
    * feat: UserDao update 구현
    
    * feat: JdbcTemplate query 메서드 구현
    
    - JdbcTemplate query 메서드 구현
    - UserDao findAll 구현
    
    * feat: JdbcTemplate queryForObject 구현
    
    * refactor: Dao 에서 Datasource, Logger 제거
    
    * refactor: JdbcTemplate에 로깅 적용
    
    * test: UserHsitoryDao테스트 생성
    
    * refactor: UserHistoryDao를 jdbcTemplate 기반으로 변경

---
+++ app/src/test/java/com/techcourse/dao/UserHistoryDaoTest.java
@@ -0,0 +1,58 @@
+package com.techcourse.dao;
+
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+import com.techcourse.domain.UserHistory;
+import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserHistoryDaoTest {
+
+    private UserHistoryDao userHistoryDao;
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setup() {
+        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
+
+        userHistoryDao = new UserHistoryDao(DataSourceConfig.getInstance());
+        jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
+    }
+
+    @Test
+    void log() {
+        final User philip = new User(1L, "philip", "jaepil", "pilyang.dev@gmail.com");
+        final UserHistory userHistory = new UserHistory(philip, "sun-shot");
+
+        userHistoryDao.log(userHistory);
+
+        var selectSql = "select id, user_id, account, password, email, created_at, created_by from user_history where created_by = ?";
+        Optional<UserHistory> result = jdbcTemplate.queryForObject(selectSql,
+                resultSet -> new UserHistory(
+                        resultSet.getLong("id"),
+                        resultSet.getLong("user_id"),
+                        resultSet.getString("account"),
+                        resultSet.getString("password"),
+                        resultSet.getString("email"),
+                        resultSet.getString("created_by")
+                ),
+                "sun-shot");
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(result).isPresent();
+            softly.assertThat(result.get().getAccount()).isEqualTo("philip");
+            softly.assertThat(result.get().getCreateBy()).isEqualTo("sun-shot");
+        });
+    }
+
+}
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,67 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            for (int parameterIndex = 0; parameterIndex < args.length; parameterIndex++) {
+                pstmt.setString(parameterIndex + 1, String.valueOf(args[parameterIndex]));
+            }
+
+            log.debug("run sql {}", sql);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            for (int parameterIndex = 0; parameterIndex < args.length; parameterIndex++) {
+                pstmt.setString(parameterIndex + 1, String.valueOf(args[parameterIndex]));
+            }
+
+            final ResultSet resultSet = pstmt.executeQuery();
+            final List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(rowMapper.map(resultSet));
+            }
+
+            log.debug("run sql {}", sql);
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            for (int parameterIndex = 0; parameterIndex < args.length; parameterIndex++) {
+                pstmt.setString(parameterIndex + 1, String.valueOf(args[parameterIndex]));
+            }
+
+            final ResultSet resultSet = pstmt.executeQuery();
+
+            log.debug("run sql {}", sql);
+            if (resultSet.next()) {
+                return Optional.of(rowMapper.map(resultSet));
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
