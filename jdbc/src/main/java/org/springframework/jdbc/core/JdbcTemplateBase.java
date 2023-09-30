403692f515e370b9e4a92194256097c67196c62f
splitCoding <voicesplit@daum.net>
2023-09-30 23:10:06 +0900

    refactor: setAutoCommit 설정 변경을 통해 트랜잭션 적용 로직 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplateBase.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplateBase.java
@@ -10,31 +10,44 @@ import org.slf4j.LoggerFactory;
 public class JdbcTemplateBase {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplateBase.class);
+    protected static final boolean TRANSACTION_ENABLE = false;
+    protected static final boolean TRANSACTION_DISABLE = true;
     private final DataSource dataSource;
 
     public JdbcTemplateBase(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
-    protected void executionBaseWithNonReturn(final String sql, final JdbcTemplateVoidExecution execution) {
+    protected void executionBaseWithNonReturn(final String sql,
+                                              final JdbcTemplateVoidExecution execution,
+                                              final boolean enableTransaction) {
         try (
             final Connection connection = dataSource.getConnection();
             final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            connection.setAutoCommit(!enableTransaction);
             log.debug("query : {}", sql);
-
             execution.execute(preparedStatement);
+            if (connection.getAutoCommit() == TRANSACTION_ENABLE) {
+                connection.commit();
+            }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    protected <T> T executionBaseWithReturn(final String sql, final JdbcTemplateExecution<T> execution) {
+    protected <T> T executionBaseWithReturn(final String sql,
+                                            final JdbcTemplateExecutor<T> execution,
+                                            final boolean enableTransaction) {
 
         try (final Connection connection = dataSource.getConnection();
             final PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            connection.setAutoCommit(!enableTransaction);
             log.debug("query : {}", sql);
-            
-            return execution.execute(preparedStatement);
+            final T result = execution.execute(preparedStatement);
+            if (connection.getAutoCommit() == TRANSACTION_ENABLE) {
+                connection.commit();
+            }
+            return result;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
