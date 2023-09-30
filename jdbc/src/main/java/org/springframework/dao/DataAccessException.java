91c7b4634f7049de6ad3e3c6217a7c3869787545
ezzanzzan <cjsdmswjd821@naver.com>
2023-10-01 02:37:51 +0900

    feat: JdbcTemplate update, query, queryForObject 메소드 구현 및 UserDao 중복 코드 제거

--- jdbc/src/main/java/org/springframework/dao/DataAccessException.java
+++ jdbc/src/main/java/org/springframework/dao/DataAccessException.java
@@ -1,7 +1,6 @@
 package org.springframework.dao;
 
 public class DataAccessException extends RuntimeException {
-
     private static final long serialVersionUID = 1L;
 
     public DataAccessException() {
---
+++ jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
@@ -0,0 +1,9 @@
+package org.springframework.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+    T mapRow(ResultSet rs) throws SQLException;
+}
