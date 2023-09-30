14d35c9096390e3ba6b43525ed47a96f3c577d95
JFe <33208246+Go-Jaecheol@users.noreply.github.com>
2023-09-30 14:25:52 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 망고(고재철) 미션 제출합니다. (#331)

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
@@ -0,0 +1,10 @@
+package org.springframework.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+
+    T mapRow(final ResultSet resultSet) throws SQLException;
+}
