a3149d6cc44f774f94356e0c9cd070a7745c1311
mcodnjs <codnjs8989@gmail.com>
2023-09-30 18:23:13 +0900

    refactor: PreparedStatementCallback 적용

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -23,46 +23,62 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        ResultSet rs = null;
+    public <T> T execute(final String sql, final PreparedStatementCallback<T> action) {
         try (
                 final Connection conn = dataSource.getConnection();
                 final PreparedStatement pstmt = conn.prepareStatement(sql)
         ) {
             log.debug("query : {}", sql);
-            setParameters(pstmt, args);
-            rs = pstmt.executeQuery();
-            final List<T> objects = new ArrayList<>();
-            while (rs.next()) {
-                objects.add(rowMapper.mapRow(rs));
-            }
-            return objects;
+            return action.doInPreparedStatement(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            closeResultSetResource(rs);
         }
     }
 
-    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        ResultSet rs = null;
-        try (
-                final Connection conn = dataSource.getConnection();
-                final PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-            setParameters(pstmt, args);
-            rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return Optional.of(rowMapper.mapRow(rs));
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        return execute(sql, pstmt -> {
+            ResultSet rs = null;
+            try {
+                setParameters(pstmt, args);
+                final List<T> objects = new ArrayList<>();
+                rs = pstmt.executeQuery();
+                while (rs.next()) {
+                    objects.add(rowMapper.mapRow(rs));
+                }
+                return objects;
+            } finally {
+                closeResultSetResource(rs);
             }
-            return Optional.empty();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            closeResultSetResource(rs);
+        });
+    }
+
+    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        return execute(sql, pstmt -> {
+            ResultSet rs = null;
+            try {
+                setParameters(pstmt, args);
+                rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    return Optional.of(rowMapper.mapRow(rs));
+                }
+                return Optional.empty();
+            } finally {
+                closeResultSetResource(rs);
+            }
+        });
+    }
+
+    public int update(final String sql, final Object... args) {
+        return execute(sql, pstmt -> {
+            setParameters(pstmt, args);
+            return pstmt.executeUpdate();
+        });
+    }
+
+    private void setParameters(final PreparedStatement pstmt, final Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
         }
     }
 
@@ -70,26 +86,6 @@ public class JdbcTemplate {
         try {
             if (rs != null) rs.close();
         } catch (SQLException ignored) {
-        }
-    }
-
-    public int update(final String sql, final Object... args) {
-        try (
-                final Connection conn = dataSource.getConnection();
-                final PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-            setParameters(pstmt, args);
-            return pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void setParameters(final PreparedStatement pstmt, final Object[] args) throws SQLException {
-        for (int i = 0; i < args.length; i++) {
-            pstmt.setObject(i + 1, args[i]);
         }
     }
 }
