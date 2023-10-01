f928b224d5cf83fe955a544af793be2379534338
jyeost <pig9619@naver.com>
2023-10-01 19:42:53 +0900

    feat: DB Connection과 같은 공통 작업들을 JdbcTemplate으로 분리하여, UserDao에서는 쿼리와 인자 조회결과 추출에 집중하도록 함

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.function.Function;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,77 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(String sql, Object... parameters) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+
+            for (int i = 0; i < parameters.length; i++) {
+                pstmt.setString(i + 1, String.valueOf(parameters[i]));
+            }
+            return pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, Function<ResultSet, T> function, Object... parameters) {
+
+        ResultSet rs = null;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
+            for (int i = 0; i < parameters.length; i++) {
+                pstmt.setString(i + 1, String.valueOf(parameters[i]));
+            }
+            rs = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            if (rs.next()) {
+                return function.apply(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
+
+    }
+
+    public <T> T queryForList(final String sql, final Function<ResultSet, T> function) {
+        ResultSet rs = null;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+            rs = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            return function.apply(rs);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
+
     }
 }
