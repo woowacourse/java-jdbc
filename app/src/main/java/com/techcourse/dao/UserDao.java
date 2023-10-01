b5a4df0152f2893bf77647423465cbdcfed75121
sh111-coder <ohk9134@naver.com>
2023-10-01 10:09:18 +0900

    refactor: 사용하지 않는 로깅 삭제

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,14 +2,10 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
-
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
     private final JdbcTemplate jdbcTemplate;
     private final RowMapper<User> rowMapper = (rs, rowNum) ->
