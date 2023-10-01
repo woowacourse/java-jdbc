d4e38050c7f967f9211de7d43d3f583b2bc3276c
Ethan <kim76744dev@gmail.com>
2023-10-02 08:37:47 +0900

    refactor: resultset 을 autoclose 하도록 변경

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -19,9 +19,7 @@ public class JdbcTemplate extends AutoClosableTemplate {
         pstmt.executeUpdate();
     }
 
-    @Override
-    protected <T> List<T> queryAll(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
-        final List<T> results = new ArrayList<>();
+            setCondition(params, pstmt);
 
         while (rs.next()) {
             T object = rowMapper.mapRow(rs);
@@ -31,13 +29,61 @@ public class JdbcTemplate extends AutoClosableTemplate {
         return results;
     }
 
-    @Override
-    protected <T> Optional<T> queryForOne(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
-        while (rs.next()) {
-            T object = rowMapper.mapRow(rs);
-            return Optional.ofNullable(object);
+    public <T> List<T> query(final String sql, RowMapper<T> rowMapper) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql);
+                final ResultSet rs = pstmt.executeQuery();
+        ) {
+            log.debug("query : {}", sql);
+
+            final List<T> results = new ArrayList<>();
+
+            while (rs.next()) {
+                T object = rowMapper.mapRow(rs);
+                results.add(object);
+            }
+
+            return results;
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
         }
 
         return Optional.empty();
     }
+
+    public <T> Optional<T> queryForObject(final String sql, RowMapper<T> rowMapper, final Object... params) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = getPreparedStatement(sql, conn, params);
+                final ResultSet rs = pstmt.executeQuery();
+        ) {
+            log.debug("query : {}", sql);
+
+
+            while (rs.next()) {
+                T object = rowMapper.mapRow(rs);
+                return Optional.ofNullable(object);
+            }
+
+            return Optional.empty();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private PreparedStatement getPreparedStatement(final String sql, final Connection conn, final Object[] params) throws SQLException {
+        final PreparedStatement pstmt = conn.prepareStatement(sql);
+        setCondition(params, pstmt);
+        return pstmt;
+    }
+
+    private void setCondition(final Object[] params, final PreparedStatement pstmt) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            pstmt.setObject(i + 1, params[i]);
+        }
+    }
+
 }
