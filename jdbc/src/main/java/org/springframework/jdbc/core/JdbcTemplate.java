dc51e1f03ab42954163063604163c42b2ef51ee3
김동호 <66300965+kdkdhoho@users.noreply.github.com>
2023-10-01 19:45:32 +0900

    [JDBC 구현하기 - 1단계] 도기(김동호) 미션 제출합니다. (#337)
    * feat: Database clear 메서드 구현
    
    * feat: JdbcTemplate 구현
    
    * refactor: JdbcTemplate 적용

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package org.springframework.jdbc.core;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,55 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void executeQuery(final String sql, final Object... params) {
+        try (final var conn = dataSource.getConnection();
+             final var pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        List<T> result = query(sql, rowMapper, params);
+        if (result.size() != 1) {
+            throw new DataAccessException("Result Count is Not Only 1. ResultCount=" + result.size());
+        }
+        return result.get(0);
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... params) {
+        try (final var conn = dataSource.getConnection();
+             final var pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+
+            ResultSet resultSet = pstmt.executeQuery();
+            if (resultSet.next()) {
+                return resultToList(rowMapper, resultSet);
+            }
+            return Collections.emptyList();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private <T> List<T> resultToList(final RowMapper<T> rowMapper, final ResultSet resultSet) throws SQLException {
+        List<T> result = new ArrayList<>();
+        do {
+            result.add(rowMapper.mapRow(resultSet));
+        } while (resultSet.next());
+        return result;
     }
 }
