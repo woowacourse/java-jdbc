93649f7d8a968a67f7b8d2e9f90db7d72e5589f0
apptie <appti@naver.com>
2023-10-01 20:16:19 +0900

    refactor: ResultSet을 통해 쿼리의 결과를 매핑하는 템플릿 클래스 추가

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/ResultSetTemplate.java
@@ -0,0 +1,20 @@
+package org.springframework.jdbc.core;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.exception.ResultSetTemplateException;
+
+public class ResultSetTemplate {
+
+    public <T> T resultSetExecute(
+            final PreparedStatement preparedStatement,
+            final ResultSetMapper<T> resultSetMapper
+    ) {
+        try (final ResultSet resultSet = preparedStatement.executeQuery()) {
+            return resultSetMapper.execute(resultSet);
+        } catch (final SQLException e) {
+            throw new ResultSetTemplateException(e);
+        }
+    }
+}
