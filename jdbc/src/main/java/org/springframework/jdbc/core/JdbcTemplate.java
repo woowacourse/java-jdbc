03722bbd7d873c5e5d658d26ce09f43450eb4439
Donghun Shin <huipulci1@naver.com>
2023-10-01 14:55:26 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 말랑(신동훈) 미션 제출합니다.  (#344)
    * 패키지 위치 변경 및 코드 정리
    
    * 학습테스트 통과
    
    * userDao insert를 JdbcTemplate을 통해 간소화
    
    * userDao update 구현
    
    * userDao findById 구현
    
    * userDao findByAccount 구현
    
    * userDao findAll 구현
    
    * polishing
    
    * JdbcTemplate PreparedStatement 생성 방법 리팩토링
    
    * jdbcTemplate queryForList 오류 해결
    
    * jdbcTemplate resultSet 자원 회수
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,13 @@
 package org.springframework.jdbc.core;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +17,48 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void execute(String sql, Object... params) {
+        try (PreparedStatement pstmt = preparedStatementWithParams(sql, params)) {
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (ResultSet resultSet = preparedStatementWithParams(sql, params).executeQuery()) {
+            if (resultSet.next()) {
+                return rowMapper.mapRow(resultSet);
+            }
+            throw new RuntimeException();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (ResultSet resultSet = preparedStatementWithParams(sql, params).executeQuery()) {
+            List<T> result = new ArrayList<>();
+            while (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet));
+            }
+            return result;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement preparedStatementWithParams(String sql, Object... params) throws SQLException {
+        PreparedStatement pstmt = dataSource.getConnection().prepareStatement(sql);
+        log.debug("query : {}", sql);
+        for (int i = 1; i <= params.length; i++) {
+            pstmt.setObject(i, params[i - 1]);
+        }
+        return pstmt;
     }
 }
---
+++ jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
@@ -0,0 +1,9 @@
+package org.springframework.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface RowMapper<T> {
+
+    T mapRow(ResultSet rs) throws SQLException;
+}
