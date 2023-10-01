61009f1e3092f3984a3a3bd454512ab2953f891d
SproutMj <mjk54565@gmail.com>
2023-10-01 16:54:02 +0900

    feat: JdbcTemplate의 3개 메서드 구현, RowMapper 생성

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,8 +2,16 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
 
 public class JdbcTemplate {
 
@@ -13,5 +21,51 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        final List<T> results = query(sql, rowMapper, args);
+        if (results.isEmpty()) {
+            throw new NoSuchElementException("존재하지 않는 데이터 row입니다.");
+        }
+
+        return results.get(0);
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement psmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            setArgs(args, psmt);
+            final ResultSet resultSet = psmt.executeQuery();
+
+            final List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet));
+            }
+
+            return results;
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void setArgs(final Object[] args, final PreparedStatement psmt) throws SQLException {
+        for (int i = 0; i< args.length; i++) {
+            psmt.setObject(i + 1, args[i]);
+        }
+    }
+
+    public void update(final String sql, final Object... args) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            setArgs(args, pstmt);
+            pstmt.executeUpdate();
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
