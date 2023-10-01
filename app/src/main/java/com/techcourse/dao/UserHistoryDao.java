6e43afeae5ce6a6678a724c31598c08d94914efb
yujamint <woawoa0513@gmail.com>
2023-10-01 20:40:22 +0900

    refactor(UserHistoryDao): 불필요한 메서드 제거

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -17,19 +17,6 @@ public class UserHistoryDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void log(final UserHistory userHistory) {
-        final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.execute(
-                sql,
-                userHistory.getUserId(),
-                userHistory.getAccount(),
-                userHistory.getPassword(),
-                userHistory.getEmail(),
-                userHistory.getCreatedAt(),
-                userHistory.getCreateBy()
-        );
-    }
-
     public void log(final Connection connection, final UserHistory userHistory) {
         final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
         jdbcTemplate.execute(
