37ae7873aef867e3397a08552718f22c18ce15c6
somin <70891072+somsom13@users.noreply.github.com>
2023-10-01 14:27:26 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 바론(이소민) 미션 제출합니다. (#305)
    * 패키지 위치 변경 및 코드 정리
    
    * feat: JdbcTemplate update, query 메서드 구현
    
    * feat: UserDao 추가 기능 구현
    
    * refactor: UserDao DataSource 제거
    
    * feat: UserDao 투두 로직 구현
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # JDBC 라이브러리 구현하기
+
+### 1단계
+- [x] JdbcTemplate insert, update, delete, select 쿼리 메서드 구현
+  - [x] UserDao rowmapper 구현
+- [x] UserDao todo 쿼리 로직 구현
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,62 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+            final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            setParamsToPreparedStatement(pstmt, args);
+
+            final ResultSet resultSet = pstmt.executeQuery();
+            final List<T> results = new ArrayList<>();
+            if (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet, resultSet.getRow()));
+            }
+
+            return results;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParamsToPreparedStatement(final PreparedStatement pstmt, final Object[] args)
+        throws SQLException
+    {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+            final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            setParamsToPreparedStatement(pstmt, args);
+
+            final ResultSet resultSet = pstmt.executeQuery();
+            if (resultSet.next()) {
+                return rowMapper.mapRow(resultSet, resultSet.getRow());
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int update(final String sql, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+            final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            setParamsToPreparedStatement(pstmt, args);
+
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
