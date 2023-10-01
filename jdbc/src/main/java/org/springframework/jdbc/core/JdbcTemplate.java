83d0db175dc4d520143b137225d20dd3a949f653
kokodakadokok <45879491+kokodak@users.noreply.github.com>
2023-10-01 11:41:07 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 코코닥(이승용) 미션 제출합니다! (#333)
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * feat: User Dao 기능 완성
    
    * feat: 코드 중복을 제거하는 JdbcTemplate 기능 구현
    
    * refactor: JdbcTemplate 을 적용하여 코드 중복 제거
    
    * refactor: resultSet.getXXX() 파라미터 수정
    
    * refactor: 메서드 순서 수정
    
    * feat: 검증 추가
    
    * refactor: 바인딩 여부에 대한 검증 로직 삭제
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>
    Co-authored-by: kokodak <kokodakadokok@gmail.co>

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -0,0 +1,82 @@
+package org.springframework.jdbc.core;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JdbcTemplate {
+
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+
+    private final DataSource dataSource;
+
+    public JdbcTemplate(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... objects) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = prepareStatementWithBindingQuery(connection.prepareStatement(sql), objects)) {
+
+            log.debug("query : {}", sql);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement prepareStatementWithBindingQuery(final PreparedStatement preparedStatement, final Object... objects) throws SQLException {
+        for (int i = 0; i < objects.length; i++) {
+            preparedStatement.setObject(i + 1, objects[i]);
+        }
+        return preparedStatement;
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = prepareStatementWithBindingQuery(connection.prepareStatement(sql), objects);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            final List<T> results = new ArrayList<>();
+
+            log.debug("query : {}", sql);
+
+            while (resultSet.next()) {
+                results.add(rowMapper.mapping(resultSet));
+            }
+            return results;
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement preparedStatement = prepareStatementWithBindingQuery(connection.prepareStatement(sql), objects);
+             final ResultSet resultSet = preparedStatement.executeQuery()) {
+
+            log.debug("query : {}", sql);
+
+            if (resultSet.next()) {
+                return Optional.of(rowMapper.mapping(resultSet));
+            }
+            return Optional.empty();
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -0,0 +1,23 @@
+package org.springframework.transaction.support;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.util.Map;
+
+public abstract class TransactionSynchronizationManager {
+
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+
+    private TransactionSynchronizationManager() {}
+
+    public static Connection getResource(DataSource key) {
+        return null;
+    }
+
+    public static void bindResource(DataSource key, Connection value) {
+    }
+
+    public static Connection unbindResource(DataSource key) {
+        return null;
+    }
+}
