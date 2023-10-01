d791e9bb43e72a4f468f2cdd7c3d5941451f77a6
Ohjintaek <116645747+Ohjintaek@users.noreply.github.com>
2023-10-01 23:06:16 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 썬샷(오진택) 미션 제출합니다. (#312)
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * feat: jdbcTemplate 구현
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -0,0 +1,159 @@
+package org.springframework.jdbc.core;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
+import org.springframework.jdbc.exception.IncorrectQueryArgumentException;
+
+public class JdbcTemplate {
+
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+
+    private final DataSource dataSource;
+
+    public JdbcTemplate(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public int execute(final String sql, final Object ... args) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            log.debug("query : {}", sql);
+
+            validateArgsCount(sql, args.length);
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i+1, args[i]);
+            }
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {}
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object ... args) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            validateArgsCount(sql, args.length);
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i+1, args[i]);
+            }
+            rs = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            final List<T> result = new ArrayList<>();
+            while (rs.next()) {
+                result.add(rowMapper.mapRow(rs));
+            }
+            return result;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {}
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object ... args) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            validateArgsCount(sql, args.length);
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i+1, args[i]);
+            }
+            rs = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            if (rs.next()) {
+                return rowMapper.mapRow(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {}
+        }
+    }
+
+    private void validateArgsCount(final String str, final int argsCount) {
+        final long questionMarksCount = str.chars()
+                .filter(c -> c == '?')
+                .count();
+
+        if (questionMarksCount > argsCount) {
+            throw new IncorrectQueryArgumentException("delivered parameter's count is deficient.");
+        }
+
+        if (questionMarksCount < argsCount) {
+            throw new IncorrectQueryArgumentException("delivered parameter's count is many.");
+        }
+    }
+}
--- mvc/src/main/java/nextstep/mvc/controller/asis/Controller.java
+++
@@ -1,8 +0,0 @@
-package nextstep.mvc.controller.asis;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-public interface Controller {
-    String execute(final HttpServletRequest req, final HttpServletResponse res) throws Exception;
-}
