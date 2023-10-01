d55af128160697dbc2eb13d444c259e85a11b6d9
yujamint <woawoa0513@gmail.com>
2023-10-01 21:05:30 +0900

    feat(UserService): findById() 트랜잭션 범위 내에 포함

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -32,9 +32,10 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
         transactionTemplate.executeWithTransaction(connection -> {
+            final var user = userDao.findById(connection, id)
+                    .orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
+            user.changePassword(newPassword);
             userDao.update(connection, user);
             userHistoryDao.log(connection, new UserHistory(user, createBy));
         });
