1f89dac8830f75e6a8ad0ef3f5397caddb7e60a3
김지현 <pig9619@naver.com>
2023-10-01 22:32:59 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 케로(김지현) 미션 제출합니다.  (#360)
    * 패키지 위치 변경 및 코드 정리
    
    * feat: DB Connection과 같은 공통 작업들을 JdbcTemplate으로 분리하여, UserDao에서는 쿼리와 인자 조회결과 추출에 집중하도록 함
    
    * refactor: JdbcTemplate 클래스 정리, SQL Exception이 발생시 DataAccessException를 던지도록 수정
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,8 +2,14 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.function.Function;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,62 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, final Object... parameters) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = makeStatement(conn, sql, parameters)
+        ) {
+            log.debug("query : {}", sql);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private PreparedStatement makeStatement(final Connection connection,final  String sql, final Object... parameters) {
+        try {
+            final PreparedStatement pstmt = connection.prepareStatement(sql);
+            setParams(pstmt, parameters);
+            return pstmt;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void setParams(final PreparedStatement pstmt, final Object[] parameters) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            pstmt.setString(i + 1, String.valueOf(parameters[i]));
+        }
+    }
+
+    public <T> T queryForObject(final String sql, Function<ResultSet, T> function, final Object... parameters) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = makeStatement(conn, sql, parameters);
+             ResultSet rs = pstmt.executeQuery()
+        ) {
+            log.debug("query : {}", sql);
+            if (rs.next()) {
+                return function.apply(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> T queryForList(final String sql, final Function<ResultSet, T> function) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()
+        ) {
+            log.debug("query : {}", sql);
+            return function.apply(rs);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
     }
 }
