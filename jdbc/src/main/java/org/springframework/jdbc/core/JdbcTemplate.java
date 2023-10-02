c051042ba643b536c7d17ceedb80c64f2b657c94
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-10-02 12:06:26 +0900

    fix: 단일 조회 시, 결과가 2개 이상인 경우 예외 처리 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -5,7 +5,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.Nullable;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,13 +26,15 @@ public class JdbcTemplate {
         );
     }
 
-    @Nullable
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
         List<T> results = preparedStatementExecutor.execute(
                 getPreparedStatementCreator(sql, args),
                 preparedStatement -> mappingQueryResult(preparedStatement, rowMapper)
         );
-        return results.isEmpty() ? null : results.get(0);
+        if (results.size() != 1) {
+            throw new IllegalStateException("1개의 결과만 반환 해야 합니다.");
+        }
+        return results.get(0);
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
