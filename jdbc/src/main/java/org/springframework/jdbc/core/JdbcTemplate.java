c231f0410f79e44629fa46402383f5980eae9267
amaran-th <songsy405@naver.com>
2023-10-03 02:52:01 +0900

    refactor: setObject()로 메서드 대체 및 메서드 다시 분리

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,10 +2,13 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
-import java.sql.*;
-import java.time.LocalDateTime;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -20,14 +23,11 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(final String sql, Object... obj) {
+    public void update(final String sql, final Object... obj) {
         try (final Connection conn = dataSource.getConnection();
              final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
             log.debug("query : {}", sql);
-            for (int i = 0; i < obj.length; i++) {
-                setSQLParameter(obj[i], i + 1, pstmt);
-            }
+            setSqlParameter(obj, pstmt);
             pstmt.execute();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -35,54 +35,46 @@ public class JdbcTemplate {
         }
     }
 
-    private void setSQLParameter(final Object parameter, final int parameterIndex,
-                                 final PreparedStatement pstmt)
-            throws SQLException {
-        if (parameter instanceof String) {
-            pstmt.setString(parameterIndex, (String) parameter);
-            return;
-        }
-        if (parameter instanceof Long) {
-            pstmt.setLong(parameterIndex, (Long) parameter);
-            return;
-        }
-        if (parameter instanceof Double) {
-            pstmt.setDouble(parameterIndex, (Double) parameter);
-            return;
-        }
-        if (parameter instanceof LocalDateTime) {
-            pstmt.setTimestamp(parameterIndex, Timestamp.valueOf((LocalDateTime) parameter));
+    private void setSqlParameter(final Object[] obj, final PreparedStatement pstmt) throws SQLException {
+        for (int i = 0; i < obj.length; i++) {
+            pstmt.setObject(i + 1, obj[i]);
         }
     }
 
     public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... obj) {
         try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
+             final PreparedStatement pstmt = getPreparedStatement(sql, obj, conn);
+             final ResultSet rs = pstmt.executeQuery()) {
             log.debug("query : {}", sql);
-            for (int i = 0; i < obj.length; i++) {
-                setSQLParameter(obj[i], i + 1, pstmt);
+            List<T> result = new ArrayList<>();
+            while (rs.next()) {
+                result.add(rowMapper.mapRow(rs, rs.getRow()));
             }
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return Optional.of(rowMapper.mapRow(rs, rs.getRow()));
+            if (result.isEmpty()) {
+                throw new DataAccessException("조회하려는 레코드가 존재하지 않습니다.");
             }
-            return Optional.empty();
+            if (result.size() >= 2) {
+                throw new DataAccessException("조회하려는 레코드는 2개 이상일 수 없습니다.");
+            }
+            return Optional.of(result.get(0));
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
 
+    private PreparedStatement getPreparedStatement(final String sql, final Object[] obj, final Connection conn) throws SQLException {
+        final PreparedStatement pstmt = conn.prepareStatement(sql);
+        setSqlParameter(obj, pstmt);
+        return pstmt;
+    }
+
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... obj) {
         try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+             final PreparedStatement pstmt = getPreparedStatement(sql, obj, conn);
+             final ResultSet rs = pstmt.executeQuery()) {
 
             log.debug("query : {}", sql);
-            for (int i = 0; i < obj.length; i++) {
-                setSQLParameter(obj[i], i + 1, pstmt);
-            }
-            ResultSet rs = pstmt.executeQuery();
             List<T> result = new ArrayList<>();
             while (rs.next()) {
                 result.add(rowMapper.mapRow(rs, rs.getRow()));
