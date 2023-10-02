62f630c085b4ff4ee77b7c0a769d7b1894a1e5f4
odo27 <odo27@naver.com>
2023-10-02 13:14:09 +0900

    refactor: 공통된 try-with-resources 제거

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -21,17 +21,15 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... values) {
+        return execute(sql, preparedStatement -> mapTable(preparedStatement, rowMapper), values);
+    }
+
+    private <T> T execute(String sql, PreparedStatementExecutor<T> preparedStatementExecutor, Object... values) {
         try (
                 Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = prepareStatement(connection, sql, values);
-                ResultSet resultSet = preparedStatement.executeQuery()
+                PreparedStatement preparedStatement = prepareStatement(connection, sql, values)
         ) {
-            List<T> results = new ArrayList<>();
-            while (resultSet.next()) {
-                T result = rowMapper.mapRow(resultSet);
-                results.add(result);
-            }
-            return results;
+            return preparedStatementExecutor.execute(preparedStatement);
         } catch (SQLException e) {
             log.error(e.getMessage());
             throw new RuntimeException(e);
@@ -50,31 +48,29 @@ public class JdbcTemplate {
         return preparedStatement;
     }
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... values) {
-        try (
-                Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = prepareStatement(connection, sql, values);
-                ResultSet resultSet = preparedStatement.executeQuery()
-        ) {
-            if (resultSet.next()) {
-                return rowMapper.mapRow(resultSet);
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
+    private <T> List<T> mapTable(PreparedStatement preparedStatement, RowMapper<T> rowMapper) throws SQLException {
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<T> results = new ArrayList<>();
+        while (resultSet.next()) {
+            T result = rowMapper.mapRow(resultSet);
+            results.add(result);
         }
+        return results;
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... values) {
+        return execute(sql, preparedStatement -> mapRow(preparedStatement, rowMapper), values);
+    }
+
+    private <T> T mapRow(PreparedStatement preparedStatement, RowMapper<T> rowMapper) throws SQLException {
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            return rowMapper.mapRow(resultSet);
+        }
+        return null;
     }
 
     public void update(String sql, Object... values) {
-        try (
-                Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = prepareStatement(connection, sql, values)
-        ) {
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
+        execute(sql, PreparedStatement::executeUpdate, values);
     }
 }
