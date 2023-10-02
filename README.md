cd74a4422e8624eb664d9a81ad99d4565de65a33
hong-sile <gurwns9325@gmail.com>
2023-10-02 17:25:59 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 홍실(홍혁준) 미션 제출합니다. (#270)
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * feat: user findAll 구현
    
    * feat: user update 구현
    
    * feat: user findByAccount 구현
    
    * refactor: try with resources로 리팩터링
    
    * chore: JdbcTemplate 테스트를 위한 세팅
    
    * feat: jdbcTemplate에서 쿼리를 실행시키는 메서드 추가
    
    * feat: jdbcTemplate에서 조회성 쿼리를 실행하는 기능 추가
    
    * refactor: 쓰기 쿼리 부분 jdbcTemplate으로 리팩터링
    
    * fix: 조회하는 부분에서, rs.next 추가
    
    * feat: userDao의 조회쿼리 부분 JdbcTemplate으로 리팩터링
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # JDBC 라이브러리 구현하기
+
+- [x] UserDaoTest 통과시키기
+    - [x] user findAll 구현하기
+    - [x] user update 구현하기
+    - [x] user findByAccount 구현하기
+- [x] UserDao 리팩터링
+    - [x] 공통부분 분리
+    - [ ] JdbcTemplate 구현하기
+        - [x] 조회기능 구현
+        - [x] 쓰기기능 구현
+- [x] UserDao를 JdbcTemplate을 사용하도록 리팩터링
+    - [x] findByAccount
+    - [x] findById
+    - [x] findAll
+    - [x] update
+    - [x] insert
---
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -0,0 +1,74 @@
+package org.springframework.jdbc.core;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JdbcTemplate {
+
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+
+    private final DataSource dataSource;
+
+    public JdbcTemplate(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public Long executeUpdate(final String sql, final Object... parameters) {
+        try (final Connection connection = dataSource.getConnection();
+            final PreparedStatement pstmt = connection.prepareStatement(sql,
+                RETURN_GENERATED_KEYS)) {
+            log.debug("query : {}", sql);
+            setPreparedStatement(pstmt, parameters);
+            pstmt.executeUpdate();
+            return extractId(pstmt);
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Long extractId(final PreparedStatement pstmt) throws SQLException {
+        try (final ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+            if (generatedKeys.next()) {
+                return generatedKeys.getLong(1);
+            }
+            throw new SQLException("id를 찾을 수 없습니다.");
+        }
+    }
+
+    private void setPreparedStatement(
+        final PreparedStatement pstmt,
+        final Object[] parameters
+    ) throws SQLException {
+        for (int index = 1; index <= parameters.length; index++) {
+            pstmt.setObject(index, parameters[index - 1]);
+        }
+    }
+
+    public <T> T executeQuery(
+        final String sql,
+        final Mapper<T> mapper,
+        final Object... objects) {
+
+        try (final Connection connection = dataSource.getConnection();
+            final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            setPreparedStatement(pstmt, objects);
+            final ResultSet rs = pstmt.executeQuery();
+            log.debug("query : {}", sql);
+            if (rs.next()) {
+                return mapper.map(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+}
