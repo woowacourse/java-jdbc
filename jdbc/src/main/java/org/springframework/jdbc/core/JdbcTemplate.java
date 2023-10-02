7a40df10b88f1035ac5776e3ad2737b2713e35f9
Combi153 <jcm04@naver.com>
2023-10-02 16:30:47 +0900

    [refactor] queryForObject 예외 처리 및 수정
    수정 내용
    - query 사용하도록 변경
    - queryForObject 관련 테스트 수정
    - size 검증 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,19 +1,21 @@
 package org.springframework.jdbc.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nullable;
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.annotation.Nullable;
-import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int SINGLE_SIZE = 1;
 
     private final DataSource dataSource;
 
@@ -43,28 +45,6 @@ public class JdbcTemplate {
         }
     }
 
-    @Nullable
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
-        try (
-                Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-
-            setParameters(pstmt, args);
-
-            try (ResultSet resultSet = pstmt.executeQuery()) {
-                if (resultSet.next()) {
-                    return rowMapper.mapRow(resultSet);
-                }
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
-
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
         try (
                 Connection conn = dataSource.getConnection();
@@ -84,6 +64,30 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
+        }
+    }
+
+    @Nullable
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        final List<T> objects = query(sql, rowMapper, args);
+        return getSingleObject(objects);
+    }
+
+    private <T> T getSingleObject(List<T> objects) {
+        validateEmpty(objects);
+        validateSingleSize(objects);
+        return objects.iterator().next();
+    }
+
+    private <T> void validateEmpty(List<T> objects) {
+        if (objects.isEmpty()) {
+            throw new IllegalArgumentException("조회 데이터가 존재하지 않습니다.");
+        }
+    }
+
+    private <T> void validateSingleSize(List<T> objects) {
+        if (objects.size() > SINGLE_SIZE) {
+            throw new IllegalArgumentException("조회 데이터가 한 개 이상 존재합니다.");
         }
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,25 +1,24 @@
 package nextstep.jdbc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.times;
+import org.junit.jupiter.api.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-import javax.sql.DataSource;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -73,11 +72,127 @@ class JdbcTemplateTest {
                 .should(times(1))
                 .setObject(1, "arg");
 
-        assertThatConnectionHasOpenedAndClosed();
-        assertThatPrepareStatementHasOpenedAndClosed();
+        assertThatConnectionOpenedAndClosed();
+        assertThatPrepareStatementOpenedAndClosed();
     }
 
-    private void assertThatConnectionHasOpenedAndClosed() throws SQLException {
+    @Nested
+    class 객체_단일_조회_쿼리를_실행할_때 {
+
+        @BeforeEach
+        void setUp() throws SQLException {
+            given(preparedStatement.executeQuery())
+                    .willReturn(resultSet);
+        }
+
+        @AfterEach
+        void tearDown() throws SQLException {
+            assertThatConnectionOpenedAndClosed();
+            assertThatPrepareStatementOpenedAndClosed();
+            assertThatResultSetOpenedAndClosed();
+        }
+
+        @Test
+        void 객체가_한_개_존재하면_객체를_반환한다() throws SQLException {
+            // given
+            given(resultSet.next())
+                    .willReturn(true, false);
+            given(rowMapper.mapRow(resultSet))
+                    .willReturn(new TestObject());
+
+            final String sql = "sql";
+            final String arg = "arg";
+
+            // when
+            final TestObject testObject = jdbcTemplate.queryForObject(sql, rowMapper, arg);
+
+            // then
+            assertThat(testObject).isNotNull();
+        }
+
+        @Test
+        void 객체가_존재하지_않으면_예외를_발생시킨다() throws SQLException {
+            // given
+            given(resultSet.next())
+                    .willReturn(false);
+
+            final String sql = "sql";
+            final String arg = "arg";
+
+            // expect
+            assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, rowMapper, arg))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("조회 데이터가 존재하지 않습니다.");
+        }
+
+        @Test
+        void 객체가_여러_개_존재하면_예외를_발생시킨다() throws SQLException {
+            // given
+            given(resultSet.next())
+                    .willReturn(true, true, false);
+
+            final String sql = "sql";
+            final String arg = "arg";
+
+            // expect
+            assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, rowMapper, arg))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("조회 데이터가 한 개 이상 존재합니다.");
+        }
+    }
+
+    @Nested
+    class 객체_다중_조회_쿼리를_실행할_때 {
+
+        @BeforeEach
+        void setUp() throws SQLException {
+            given(preparedStatement.executeQuery())
+                    .willReturn(resultSet);
+        }
+
+        @AfterEach
+        void tearDown() throws SQLException {
+            assertThatConnectionOpenedAndClosed();
+            assertThatPrepareStatementOpenedAndClosed();
+            assertThatResultSetOpenedAndClosed();
+        }
+
+        @Test
+        void 객체가_존재하면_객체_리스트를_반환한다() throws SQLException {
+            // given
+            given(resultSet.next())
+                    .willReturn(true, true, false);
+            given(rowMapper.mapRow(resultSet))
+                    .willReturn(new TestObject());
+
+            final String sql = "sql";
+            final String arg = "arg";
+
+            // when
+            List<TestObject> objects = jdbcTemplate.query(sql, rowMapper, arg);
+
+            // then
+            assertThat(objects).hasSize(2);
+        }
+
+        @Test
+        void 객체가_존재하지_않으면_빈_리스트를_반환한다() throws SQLException {
+            // given
+            given(resultSet.next())
+                    .willReturn(false);
+
+            final String sql = "sql";
+            final String arg = "arg";
+
+            // when
+            List<TestObject> objects = jdbcTemplate.query(sql, rowMapper, arg);
+
+            // then
+            assertThat(objects).isEmpty();
+        }
+    }
+
+    private void assertThatConnectionOpenedAndClosed() throws SQLException {
         then(dataSource)
                 .should(times(1))
                 .getConnection();
@@ -87,7 +202,7 @@ class JdbcTemplateTest {
                 .close();
     }
 
-    private void assertThatPrepareStatementHasOpenedAndClosed() throws SQLException {
+    private void assertThatPrepareStatementOpenedAndClosed() throws SQLException {
         then(connection)
                 .should(times(1))
                 .prepareStatement(anyString());
@@ -97,35 +212,7 @@ class JdbcTemplateTest {
                 .close();
     }
 
-    @Test
-    void 객체_단일_조회_쿼리를_실행할_때_객체가_존재하면_객체를_반환한다() throws SQLException {
-        // given
-        given(rowMapper.mapRow(resultSet))
-                .willReturn(new TestObject());
-        given(preparedStatement.executeQuery())
-                .willReturn(resultSet);
-        given(resultSet.next())
-                .willReturn(true);
-
-        final String sql = "sql";
-        final String arg = "arg";
-
-        // when
-        final TestObject testObject = jdbcTemplate.queryForObject(sql, rowMapper, arg);
-
-        // then
-        assertThat(testObject).isNotNull();
-
-        then(preparedStatement)
-                .should(times(1))
-                .setObject(1, "arg");
-
-        assertThatConnectionHasOpenedAndClosed();
-        assertThatPrepareStatementHasOpenedAndClosed();
-        assertThatResultSetHasOpenedAndClosed();
-    }
-
-    private void assertThatResultSetHasOpenedAndClosed() throws SQLException {
+    private void assertThatResultSetOpenedAndClosed() throws SQLException {
         then(preparedStatement)
                 .should(times(1))
                 .executeQuery();
@@ -133,84 +220,6 @@ class JdbcTemplateTest {
         then(resultSet)
                 .should(times(1))
                 .close();
-    }
-
-    @Test
-    void 객체_단일_조회_쿼리를_실행할_때_객체가_존재하지_않으면_null을_반환한다() throws SQLException {
-        // given
-        given(preparedStatement.executeQuery())
-                .willReturn(resultSet);
-        given(resultSet.next())
-                .willReturn(false);
-
-        final String sql = "sql";
-        final String arg = "arg";
-
-        // when
-        final TestObject testObject = jdbcTemplate.queryForObject(sql, rowMapper, arg);
-
-        // then
-        assertThat(testObject).isNull();
-
-        then(preparedStatement)
-                .should(times(1))
-                .setObject(1, "arg");
-
-        assertThatConnectionHasOpenedAndClosed();
-        assertThatPrepareStatementHasOpenedAndClosed();
-        assertThatResultSetHasOpenedAndClosed();
-    }
-
-    @Test
-    void 객체_다중_조회_쿼리를_실행할_때_객체가_존재하면_객체_리스트를_반환한다() throws SQLException {
-        // given
-        given(preparedStatement.executeQuery())
-                .willReturn(resultSet);
-        given(resultSet.next())
-                .willReturn(true, true, false);
-
-        final String sql = "sql";
-        final String arg = "arg";
-
-        // when
-        List<TestObject> objects = jdbcTemplate.query(sql, rowMapper, arg);
-
-        // then
-        assertThat(objects).hasSize(2);
-
-        then(preparedStatement)
-                .should(times(1))
-                .setObject(1, "arg");
-
-        assertThatConnectionHasOpenedAndClosed();
-        assertThatPrepareStatementHasOpenedAndClosed();
-        assertThatResultSetHasOpenedAndClosed();
-    }
-
-    @Test
-    void 객체_다중_조회_쿼리를_실행할_때_객체가_존재하지_않으면_빈_리스트를_반환한다() throws SQLException {
-        // given
-        given(preparedStatement.executeQuery())
-                .willReturn(resultSet);
-        given(resultSet.next())
-                .willReturn(false);
-
-        final String sql = "sql";
-        final String arg = "arg";
-
-        // when
-        List<TestObject> objects = jdbcTemplate.query(sql, rowMapper, arg);
-
-        // then
-        assertThat(objects).isEmpty();
-
-        then(preparedStatement)
-                .should(times(1))
-                .setObject(1, "arg");
-
-        assertThatConnectionHasOpenedAndClosed();
-        assertThatPrepareStatementHasOpenedAndClosed();
-        assertThatResultSetHasOpenedAndClosed();
     }
 
     private static class TestObject {
