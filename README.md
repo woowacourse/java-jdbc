a17f0d97a3a2784e850fff19634986ad4f3632ba
thdwoqor <celebarte01@gmail.com>
2023-10-02 14:26:04 +0900

    refactor: 익명 클래스를 활용해서 중복로직 제거

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # JDBC 라이브러리 구현하기
+
+- [x] 익명 클래스 (JdbcTemplate)
+- [x] 함수형 인터페이스 (RowMapper)
+- [x] 제네릭 (RowMapper, JdbcTemplate)
+- [x] 가변 인자 (JdbcTemplate)
+- [ ] 람다
+- [x] try-with-resources (JdbcTemplate)
+- [x] checked vs unchecked exception (SQLException)
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -20,54 +20,84 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    private PreparedStatement setPrepareStatement(final Object[] args, final PreparedStatement pstmt) throws SQLException {
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        return execute(new PreparedStatementCallback<T>() {
+            @Override
+            public T doPreparedStatement(final PreparedStatement pstmt) throws SQLException {
+                ResultSet rs;
+
+                setPrepareStatement(pstmt, args);
+                rs = pstmt.executeQuery();
+
+                log.debug("query : {}", sql);
+                if (rs.next()) {
+                    return rowMapper.mapRow(rs);
+                }
+                return null;
+            }
+
+            @Override
+            public String getSql() {
+                return sql;
+            }
+        });
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        return execute(new PreparedStatementCallback<ArrayList<T>>() {
+            @Override
+            public ArrayList<T> doPreparedStatement(final PreparedStatement pstmt) throws SQLException {
+                ResultSet rs;
+
+                rs = pstmt.executeQuery();
+
+                log.debug("query : {}", sql);
+                ArrayList<T> objects = new ArrayList<>();
+                while (rs.next()) {
+                    objects.add(rowMapper.mapRow(rs));
+                }
+                return objects;
+            }
+
+            @Override
+            public String getSql() {
+                return sql;
+            }
+        });
+    }
+
+    public int update(String sql, Object... args) {
+        return execute(new PreparedStatementCallback<Integer>() {
+            @Override
+            public Integer doPreparedStatement(final PreparedStatement pstmt) throws SQLException {
+                log.debug("query : {}", sql);
+
+                setPrepareStatement(pstmt, args);
+
+                return pstmt.executeUpdate();
+            }
+
+            @Override
+            public String getSql() {
+                return sql;
+            }
+        });
+    }
+
+    private PreparedStatement setPrepareStatement(final PreparedStatement pstmt, final Object... args)
+            throws SQLException {
         for (int i = 0; i < args.length; i++) {
             pstmt.setObject(i + 1, args[i]);
         }
         return pstmt;
     }
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
-        try (Connection conn = dataSource.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            ResultSet rs;
-
-            setPrepareStatement(args, pstmt);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-            if (rs.next()) {
-                return rowMapper.mapRow(rs);
-            }
-            return null;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
-        try (Connection conn = dataSource.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            ResultSet rs;
-
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-            ArrayList<T> objects = new ArrayList<>();
-            while (rs.next()) {
-                objects.add(rowMapper.mapRow(rs));
-            }
-            return objects;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public int update(String sql, Object... args) {
-        try (Connection conn = dataSource.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-
-            setPrepareStatement(args, pstmt);
-
-            return pstmt.executeUpdate();
+    private  <T> T execute(PreparedStatementCallback callback) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(callback.getSql())
+        ) {
+            return (T) callback.doPreparedStatement(pstmt);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
