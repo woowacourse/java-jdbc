3eb6a0b8c88362c1fef4f4974cf8762286089401
amaran-th <songsy405@naver.com>
2023-10-03 02:06:50 +0900

    refactor: SQL 구문 코드스타일 설정 변경

--- jdbc/src/test/resources/schema.sql
+++ jdbc/src/test/resources/schema.sql
@@ -1,23 +1,8 @@
 create table if not exists users
 (
-    id
-    bigint
-    auto_increment,
-    account
-    varchar
-(
-    100
-) not null,
-    password varchar
-(
-    100
-) not null,
-    email varchar
-(
-    100
-) not null,
-    primary key
-(
-    id
-)
-    );
+    id       bigint auto_increment,
+    account  varchar(100) not null,
+    password varchar(100) not null,
+    email    varchar(100) not null,
+    primary key (id)
+);
