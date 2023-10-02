7da60c084ac4b6a8a7ad476cddfd7e78d7cad1fe
jaeyeon kim <jakind@naver.com>
2023-10-02 16:20:06 +0900

    feat : JdbcTemplate update 메서드 구현

--- jdbc/src/main/java/org/springframework/dao/DataAccessException.java
+++ jdbc/src/main/java/org/springframework/dao/DataAccessException.java
@@ -8,7 +8,12 @@ public class DataAccessException extends RuntimeException {
         super();
     }
 
-    public DataAccessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public DataAccessException(
+            String message,
+            Throwable cause,
+            boolean enableSuppression,
+            boolean writableStackTrace
+    ) {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,11 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -14,4 +16,26 @@ public class JdbcTemplate {
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public void update(String sql, Object... parameters) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParametersInPreparedStatement(preparedStatement, parameters);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParametersInPreparedStatement(
+            PreparedStatement preparedStatement,
+            Object[] parameters
+    ) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            preparedStatement.setString(i + 1, String.valueOf(parameters[i]));
+        }
+    }
+
 }
