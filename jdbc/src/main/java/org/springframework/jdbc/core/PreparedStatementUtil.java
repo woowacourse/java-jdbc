34b3607a78e2e6df477d85275958efef2c88accf
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2023-10-03 02:07:02 +0900

    feat: PreparedStatementUtil 관련 리소스는 PreparedStatementUtil로 가져온다.

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementUtil.java
@@ -0,0 +1,35 @@
+package org.springframework.jdbc.core;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PreparedStatementUtil {
+
+    public static PreparedStatement getPreparedStatement(final Connection connection, final String sql, final Object... conditions) {
+        try (
+                final PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            setConditions(preparedStatement, conditions);
+            return preparedStatement;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void setConditions(PreparedStatement preparedStatement, Object[] conditions) throws SQLException {
+        for (int i = 1; i <= conditions.length; i++) {
+            preparedStatement.setObject(i, conditions[i - 1]);
+        }
+    }
+
+    public static ResultSet getResultSet(final PreparedStatement preparedStatement) {
+        try {
+            return preparedStatement.getResultSet();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
