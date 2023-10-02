e3cd1b92fd94b087b02eebd1a2832912dcd70739
Ethan <kim76744dev@gmail.com>
2023-10-02 11:44:45 +0900

    refactor: autocloseable 클래스로 분리

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,11 +1,6 @@
 package org.springframework.jdbc.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,85 +8,36 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class JdbcTemplate {
-
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
-    private final DataSource dataSource;
+public class JdbcTemplate extends AutoClosableTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        super(dataSource);
     }
 
-    public void execute(final String sql, final Object... params) {
-        try (
-                final Connection conn = dataSource.getConnection();
-                final PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
+    @Override
+    protected void commandQuery(final PreparedStatement pstmt) throws SQLException {
+        pstmt.executeUpdate();
+    }
 
-            setCondition(params, pstmt);
+    @Override
+    protected <T> List<T> queryAll(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
+        final List<T> results = new ArrayList<>();
 
-            pstmt.executeUpdate();
-        } catch (final SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
+        while (rs.next()) {
+            T object = rowMapper.mapRow(rs);
+            results.add(object);
         }
+
+        return results;
     }
 
-    public <T> List<T> query(final String sql, RowMapper<T> rowMapper) {
-        try (
-                final Connection conn = dataSource.getConnection();
-                final PreparedStatement pstmt = conn.prepareStatement(sql);
-                final ResultSet rs = pstmt.executeQuery();
-        ) {
-            log.debug("query : {}", sql);
-
-            final List<T> results = new ArrayList<>();
-
-            while (rs.next()) {
-                T object = rowMapper.mapRow(rs);
-                results.add(object);
-            }
-
-            return results;
-        } catch (final SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
+    @Override
+    protected <T> Optional<T> queryForOne(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
+        while (rs.next()) {
+            T object = rowMapper.mapRow(rs);
+            return Optional.ofNullable(object);
         }
+
+        return Optional.empty();
     }
-
-    public <T> Optional<T> queryForObject(final String sql, RowMapper<T> rowMapper, final Object... params) {
-        try (
-                final Connection conn = dataSource.getConnection();
-                final PreparedStatement pstmt = getPreparedStatement(sql, conn, params);
-                final ResultSet rs = pstmt.executeQuery();
-        ) {
-            log.debug("query : {}", sql);
-
-
-            while (rs.next()) {
-                T object = rowMapper.mapRow(rs);
-                return Optional.ofNullable(object);
-            }
-
-            return Optional.empty();
-        } catch (final SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
-        }
-    }
-
-    private PreparedStatement getPreparedStatement(final String sql, final Connection conn, final Object[] params) throws SQLException {
-        final PreparedStatement pstmt = conn.prepareStatement(sql);
-        setCondition(params, pstmt);
-        return pstmt;
-    }
-
-    private void setCondition(final Object[] params, final PreparedStatement pstmt) throws SQLException {
-        for (int i = 0; i < params.length; i++) {
-            pstmt.setObject(i + 1, params[i]);
-        }
-    }
-
 }
