2d7fe8fa2c7622e878f0fd6487d4626910819b3f
donghae-kim <donghae1999@gmail.com>
2023-10-02 19:44:14 +0900

    refactor: SqlException -> DataAccessException 예외 포장

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,6 +2,7 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -67,7 +68,7 @@ public class JdbcTemplate {
             return preparedStatementExecutor.execute(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
     }
 }
