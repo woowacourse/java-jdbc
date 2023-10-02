092e2661a25babb00ede9615b9644e721c9369ab
woo-chang <clllickme@naver.com>
2023-10-02 17:15:19 +0900

    feat: PreparedStatement 생성 기능 추상화

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementCreator.java
@@ -0,0 +1,11 @@
+package org.springframework.jdbc.core;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface PreparedStatementCreator {
+
+    PreparedStatement createPreparedStatement(Connection con) throws SQLException;
+}
