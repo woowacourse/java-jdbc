b2c8398d06e28980d30ab551ab88313559c1d51a
parkmuhyeun <pjhg410@gmail.com>
2023-10-02 15:02:27 +0900

    refactor: 애노테이션 추가

--- jdbc/src/main/java/org/springframework/dao/RowMapper.java
+++ jdbc/src/main/java/org/springframework/dao/RowMapper.java
@@ -3,6 +3,7 @@ package org.springframework.dao;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface RowMapper<T> {
 
     T mapRow(ResultSet rs) throws SQLException;
