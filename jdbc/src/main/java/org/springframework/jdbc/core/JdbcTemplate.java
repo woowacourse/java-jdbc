62213c236ed0c8245cd8c9ca7ac6e792695be5b9
Ethan <kim76744dev@gmail.com>
2023-10-02 18:27:23 +0900

    refactor: 원래대로 Rollback

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,6 +1,11 @@
 package org.springframework.jdbc.core;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+
 import javax.sql.DataSource;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,25 +13,30 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class JdbcTemplate extends AutoClosableTemplate {
+public class JdbcTemplate {
+
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+
+    private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
-        super(dataSource);
+        this.dataSource = dataSource;
     }
 
-    @Override
-    protected void commandQuery(final PreparedStatement pstmt) throws SQLException {
-        pstmt.executeUpdate();
-    }
+    public void execute(final String sql, final Object... params) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
 
             setCondition(params, pstmt);
 
-        while (rs.next()) {
-            T object = rowMapper.mapRow(rs);
-            results.add(object);
+            pstmt.executeUpdate();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
         }
-
-        return results;
     }
 
     public <T> List<T> query(final String sql, RowMapper<T> rowMapper) {
@@ -49,8 +59,6 @@ public class JdbcTemplate extends AutoClosableTemplate {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
         }
-
-        return Optional.empty();
     }
 
     public <T> Optional<T> queryForObject(final String sql, RowMapper<T> rowMapper, final Object... params) {
@@ -60,7 +68,6 @@ public class JdbcTemplate extends AutoClosableTemplate {
                 final ResultSet rs = pstmt.executeQuery();
         ) {
             log.debug("query : {}", sql);
-
 
             while (rs.next()) {
                 T object = rowMapper.mapRow(rs);
@@ -85,5 +92,4 @@ public class JdbcTemplate extends AutoClosableTemplate {
             pstmt.setObject(i + 1, params[i]);
         }
     }
-
 }
