0b7a256fbc0a0ba6722789a64cace7a78102285e
0chil <0@chll.it>
2023-10-03 15:39:28 +0900

    feat: insert, select 추상화

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JdbcTemplate {
 
@@ -13,5 +17,41 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int insert(String sql, Object... arguments) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = prepareStatement(connection, sql, arguments)) {
+
+            log.info(sql);
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... arguments) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = prepareStatement(connection, sql, arguments)) {
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (!resultSet.next()) {
+                throw new IllegalArgumentException("결과가 없습니다");
+            }
+
+            return rowMapper.map(resultSet);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement prepareStatement(Connection connection, String sql, Object... arguments) throws SQLException {
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        for (int i = 0; i < arguments.length; i++) {
+            preparedStatement.setObject(i + 1, arguments[i]);
+        }
+        return preparedStatement;
     }
 }
