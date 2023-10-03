186e333c49428ac2068ccfb8974ecc95be80fb37
ChoiYoungHoon <dudgns2865@naver.com>
2023-10-03 21:55:21 +0900

    feat: PreparedStatementExecutor 콜백 함수로 JdbcTemplate 중복 제거

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -20,51 +20,31 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... arguments) {
+    public <T> T query(final String sql, final PreparedStatementExecutor<T> preparedStatementExecutor, final Object... arguments) {
         try (final var conn = dataSource.getConnection();
-             final var pstmt = conn.prepareStatement(sql)) {
+             final var preparedStatement = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
 
-            setObjectToPreparedStatement(pstmt, arguments);
+            setObjectToPreparedStatement(preparedStatement, arguments);
 
-            return getObject(sql, rowMapper, pstmt);
+            return preparedStatementExecutor.execute(preparedStatement);
         } catch (final SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
 
-    private void setObjectToPreparedStatement(final PreparedStatement pstmt, final Object[] arguments) throws SQLException {
+    private void setObjectToPreparedStatement(final PreparedStatement preparedStatement, final Object[] arguments) throws SQLException {
         for (int parameterIndex = 1; parameterIndex < arguments.length + 1; parameterIndex++) {
-            pstmt.setObject(parameterIndex, arguments[parameterIndex - 1]);
-        }
-    }
-
-    private <T> Optional<T> getObject(final String sql, final RowMapper<T> rowMapper, final PreparedStatement pstmt) throws SQLException {
-        try (final ResultSet rs = pstmt.executeQuery()) {
-            log.debug("query : {}", sql);
-            if (!rs.next()) {
-                return Optional.empty();
-            }
-            final T object = rowMapper.map(rs);
-            if (rs.next()) {
-                throw new SQLException("Query returned more than one result");
-            }
-            return Optional.of(object);
+            preparedStatement.setObject(parameterIndex, arguments[parameterIndex - 1]);
         }
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
-        try (final var conn = dataSource.getConnection();
-             final var pstmt = conn.prepareStatement(sql);
-             final var rs = pstmt.executeQuery();) {
-
-            log.debug("query : {}", sql);
-
-            return getObjects(rowMapper, rs);
-        } catch (final SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        return query(sql, (PreparedStatementExecutor<List<T>>) preparedStatement -> {
+            final var resultSet = preparedStatement.executeQuery();
+            return getObjects(rowMapper, resultSet);
+        });
     }
 
     private <T> List<T> getObjects(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
@@ -76,18 +56,19 @@ public class JdbcTemplate {
         return result;
     }
 
+    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... arguments) {
+        final List<T> results = query(sql, preparedStatement -> {
+            final ResultSet resultSet = preparedStatement.executeQuery();
+            return getObjects(rowMapper, resultSet);
+        }, arguments);
+        // todo: transaction 구현하면 주석 해제
+//        if (results.size() > 1) {
+//            throw new DataAccessException("Query returned more than one result");
+//        }
+        return Optional.ofNullable(results.iterator().next());
+    }
+
     public void update(final String sql, final Object... arguments) {
-        try (final var conn = dataSource.getConnection();
-             final var pstmt = conn.prepareStatement(sql);) {
-
-            log.debug("query : {}", sql);
-
-            setObjectToPreparedStatement(pstmt, arguments);
-
-            pstmt.executeUpdate();
-        } catch (final SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        query(sql, PreparedStatement::executeUpdate, arguments);
     }
 }
