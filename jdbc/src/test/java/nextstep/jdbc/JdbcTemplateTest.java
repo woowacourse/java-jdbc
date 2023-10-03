b3ab7c3fb68a0406cf3b7a171803a4bae1904953
Songusika <74398096+Songusika@users.noreply.github.com>
2023-10-03 22:09:10 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 블랙캣(송우석) 미션 제출합니다.  (#363)
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * feat: 객체 매퍼를 받아서 단일 조회를 하는 기능 구현
    
    * feat: id로 User를 찾는 기능 구현
    
    * feat: 복수 조회를 하는 기능 구현
    
    * feat: 모든 User를 찾는 기능 구현
    
    * feat: account를 가진 User를 찾는 기능 구현
    
    * feat: insert, update, delete 쿼리를 수행하는 기능 구현
    
    * refactor: 중복되는 자원 열기-닫기 코드 제거
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,105 @@
 package nextstep.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class JdbcTemplateTest {
 
+    static class Member {
+        private final Long id;
+        private final String username;
+
+        public Member(final Long id, final String username) {
+            this.id = id;
+            this.username = username;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+    }
+
+    private final String sql = "SELECT * FROM MEMBER WHERE username = ?";
+    private final RowMapper<Member> memberMapper = (resultSet) -> new Member(
+            resultSet.getLong(1),
+            resultSet.getString(2)
+    );
+    private final DataSource dataSource = mock(DataSource.class);
+    private final Connection connection = mock(Connection.class);
+    private final PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    private final ResultSet resultSet = mock(ResultSet.class);
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+    }
+
+    @Test
+    void 단일_조회시_쿼리결과가_있을_경우_해당_객체를_반환한다() throws SQLException {
+        // given
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getLong(1)).thenReturn(1L);
+        when(resultSet.getString(2)).thenReturn("blackcat");
+
+        // when
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        final Optional<Member> found = jdbcTemplate.queryForObject(sql, memberMapper, "blackcat");
+
+        // then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(found).isNotEmpty();
+            softly.assertThat(found.get().getId()).isEqualTo(1L);
+            softly.assertThat(found.get().getUsername()).isEqualTo("blackcat");
+        });
+    }
+
+    @Test
+    void 단일_조회시_쿼리결과가_없을_경우_빈_옵셔널을_반환한다() throws SQLException {
+        // given
+        when(resultSet.next()).thenReturn(false);
+
+        // when
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        final Optional<Member> found = jdbcTemplate.queryForObject(sql, memberMapper, "blackcat");
+
+        // then
+        assertThat(found).isEmpty();
+    }
+
+    @Test
+    void 복수_조회시_쿼리_결과가_없을_경우_빈_리스트를_반환한다() throws SQLException {
+        // given
+        when(resultSet.next()).thenReturn(false);
+
+        // when
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        List<Member> query = jdbcTemplate.query(sql, memberMapper, "blackcat");
+
+        // then
+        assertThat(query).isEmpty();
+    }
 }
