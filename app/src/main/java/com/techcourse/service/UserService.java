b2a4d31602e6a0d4548794b46a31f3aea55d2080
JunDonghyuk <ehdgur4814@naver.com>
2023-10-03 19:21:07 +0900

    refactor: 트랜잭션 매니저를 외부에서 주입받고, 트랜잭션 로직 추가

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,15 +4,22 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import org.springframework.dao.DataAccessException;
+import org.springframework.transaction.TransactionManager;
+import org.springframework.transaction.support.Transaction;
 
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final TransactionManager transactionManager;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao,
+                       final UserHistoryDao userHistoryDao,
+                       final TransactionManager transactionManager) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.transactionManager = transactionManager;
     }
 
     public User findById(final long id) {
@@ -24,9 +31,18 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
+        Transaction transaction = transactionManager.getTransaction();
+        transaction.start();
+
         final var user = findById(id);
         user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        try {
+            userDao.update(transaction.getConnection(), user);
+            userHistoryDao.log(transaction.getConnection(), new UserHistory(user, createBy));
+            transaction.commit();
+        } catch (Exception e) {
+            transaction.rollback();
+            throw new DataAccessException("data access error.", e);
+        }
     }
 }
