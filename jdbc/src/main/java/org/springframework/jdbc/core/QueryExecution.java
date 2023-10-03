d5f5000a9cf13ca81a539f2c8cbfefd1401167d3
nuyh <hjaehyun25@gmail.com>
2023-10-04 00:06:40 +0900

    refactor: 함수형 인터페이스로 선언

--- jdbc/src/main/java/org/springframework/jdbc/core/QueryExecution.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/QueryExecution.java
@@ -3,6 +3,7 @@ package org.springframework.jdbc.core;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface QueryExecution {
 
     Object execute(PreparedStatement preparedStatement) throws SQLException;
--- jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
@@ -3,6 +3,7 @@ package org.springframework.jdbc.core;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface RowMapper<T> {
 
     T map(ResultSet resultSet) throws SQLException;
