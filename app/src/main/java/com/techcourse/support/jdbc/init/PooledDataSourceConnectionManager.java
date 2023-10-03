d45188fe7d88b7ec1d37b49201e630168fb158a1
yoondgu <doyoungwork@gmail.com>
2023-10-04 02:04:33 +0900

    chore: Hikari CP 동작 확인을 위한 logger 추가

--- app/src/main/java/com/techcourse/support/jdbc/init/PooledDataSourceConnectionManager.java
+++ app/src/main/java/com/techcourse/support/jdbc/init/PooledDataSourceConnectionManager.java
@@ -4,8 +4,6 @@ import com.techcourse.config.DataSourceConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.CannotGetJdbcConnectionException;
 import org.springframework.jdbc.core.ConnectionManager;
 
