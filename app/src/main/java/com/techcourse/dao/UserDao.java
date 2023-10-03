4569af20dc372aac6802eafb138500b940d57c40
thdwoqor <celebarte01@gmail.com>
2023-10-03 14:14:07 +0900

    refactor: RowMapper index->label로 변경

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,14 +2,10 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
-
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -43,10 +39,10 @@ public class UserDao {
 
     private static RowMapper<User> getUserRowMapper() {
         return rs -> new User(
-                rs.getLong(1),
-                rs.getString(2),
-                rs.getString(3),
-                rs.getString(4));
+                rs.getLong("id"),
+                rs.getString("account"),
+                rs.getString("password"),
+                rs.getString("email"));
     }
 
     public User findById(final Long id) {
