0d727a02179bf59bd1ecfd33255e657e0c914b27
ChoiYoungHoon <dudgns2865@naver.com>
2023-10-03 22:43:37 +0900

    refactor: PreparedStatementExecutor 콜백 함수로 JdbcTemplate 중복 제거

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -21,8 +21,8 @@ public class JdbcTemplate {
     }
 
     public <T> T query(final String sql, final PreparedStatementExecutor<T> preparedStatementExecutor, final Object... arguments) {
-        try (final var conn = dataSource.getConnection();
-             final var preparedStatement = conn.prepareStatement(sql)) {
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
             log.debug("query : {}", sql);
 
             setObjectToPreparedStatement(preparedStatement, arguments);
@@ -34,23 +34,23 @@ public class JdbcTemplate {
         }
     }
 
-    private void setObjectToPreparedStatement(final PreparedStatement preparedStatement, final Object[] arguments) throws SQLException {
+    private void setObjectToPreparedStatement(final PreparedStatement preparedStatement, final Object... arguments) throws SQLException {
         for (int parameterIndex = 1; parameterIndex < arguments.length + 1; parameterIndex++) {
             preparedStatement.setObject(parameterIndex, arguments[parameterIndex - 1]);
         }
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
-        return query(sql, (PreparedStatementExecutor<List<T>>) preparedStatement -> {
+    public <T> List<T> queryForList(final String sql, final RowMapper<T> rowMapper) {
+        return query(sql, preparedStatement -> {
             final var resultSet = preparedStatement.executeQuery();
             return getObjects(rowMapper, resultSet);
         });
     }
 
-    private <T> List<T> getObjects(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
+    private <T> List<T> getObjects(final RowMapper<T> rowMapper, final ResultSet resultSet) throws SQLException {
         final List<T> result = new ArrayList<>();
-        while (rs.next()) {
-            final T data = rowMapper.map(rs);
+        while (resultSet.next()) {
+            final T data = rowMapper.map(resultSet);
             result.add(data);
         }
         return result;
@@ -61,10 +61,6 @@ public class JdbcTemplate {
             final ResultSet resultSet = preparedStatement.executeQuery();
             return getObjects(rowMapper, resultSet);
         }, arguments);
-        // todo: transaction 구현하면 주석 해제
-//        if (results.size() > 1) {
-//            throw new DataAccessException("Query returned more than one result");
-//        }
         return Optional.ofNullable(results.iterator().next());
     }
 
