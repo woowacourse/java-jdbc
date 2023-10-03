527f3f84e80ca19a0c821b75f10c809c47acddb3
yoondgu <doyoungwork@gmail.com>
2023-10-04 00:51:43 +0900

    refactor: DataSource를 필드에서 한 번만 초기화하도록 함

--- app/src/main/java/com/techcourse/support/jdbc/init/DataSourceConnectionManager.java
+++ app/src/main/java/com/techcourse/support/jdbc/init/DataSourceConnectionManager.java
@@ -9,9 +9,15 @@ import org.springframework.jdbc.core.ConnectionManager;
 
 public class DataSourceConnectionManager implements ConnectionManager {
 
+    private final DataSource dataSource;
+
+    public DataSourceConnectionManager() {
+        this.dataSource = DataSourceConfig.getInstance();
+    }
+
+    @Override
     public Connection getConnection() throws CannotGetJdbcConnectionException {
         try {
-            DataSource dataSource = DataSourceConfig.getInstance();
             return dataSource.getConnection();
         } catch (final SQLException exception) {
             throw new CannotGetJdbcConnectionException("Datasource connection error:" + exception.getMessage());
