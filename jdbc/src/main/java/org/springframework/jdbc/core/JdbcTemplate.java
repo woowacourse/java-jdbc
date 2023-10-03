d5eca6526bd821e25fdec773e4da0969bace12d8
hyena <goatsfish@gmail.com>
2023-10-04 02:06:16 +0900

    refactor: JdbcTemplate 업데이트, 조회 기능이 템플릿을 사용하도록 리팩터링

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,79 +1,37 @@
 package org.springframework.jdbc.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
-    private final DataSource dataSource;
+    private final PreparedStatementExecuteTemplate preparedStatementExecuteTemplate;
+    private final ResultSetMappingTemplate resultSetMappingTemplate;
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.preparedStatementExecuteTemplate = new PreparedStatementExecuteTemplate(dataSource);
+        this.resultSetMappingTemplate = new ResultSetMappingTemplate();
     }
 
     public int update(final String sql, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = prepareMappedStatement(conn, sql, args);) {
-
-            return pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.warn("쿼리 실행 도중에 오류가 발생하였습니다. {} ====> SQL = {} {} ====> Args = {}",
-                    System.lineSeparator(), sql, System.lineSeparator(), args, e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private PreparedStatement prepareMappedStatement(final Connection conn, final String sql, final Object[] args) throws SQLException {
-        final PreparedStatement pstmt = conn.prepareStatement(sql);
-        for (int argumentIndex = 1; argumentIndex <= args.length; argumentIndex++) {
-            pstmt.setObject(argumentIndex, args[argumentIndex - 1]);
-        }
-
-        return pstmt;
+        return preparedStatementExecuteTemplate.execute(PreparedStatement::executeUpdate, sql, args);
     }
 
     public <T> T queryObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = prepareMappedStatement(conn, sql, args);
-             final ResultSet rs = pstmt.executeQuery();) {
+        return preparedStatementExecuteTemplate.execute(pstmt -> {
+            final ResultSet rs = pstmt.executeQuery();
 
-            while (rs.next()) {
-                return rowMapper.toObject(rs);
-            }
-
-            return null;
-        } catch (SQLException e) {
-            log.warn("쿼리 실행 도중에 오류가 발생하였습니다. {} ====> SQL = {} {} ====> Args = {}",
-                    System.lineSeparator(), sql, System.lineSeparator(), args, e);
-            throw new RuntimeException(e);
-        }
+            return resultSetMappingTemplate.mappingOne(rs, rowMapper);
+        }, sql, args);
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql);
-             final ResultSet rs = pstmt.executeQuery();) {
+        return preparedStatementExecuteTemplate.execute(pstmt -> {
+            final ResultSet rs = pstmt.executeQuery();
 
-            final List<T> queriedData = new ArrayList<>();
-            while (rs.next()) {
-                queriedData.add(rowMapper.toObject(rs));
-            }
-
-            return queriedData;
-        } catch (SQLException e) {
-            log.warn("조회하던 도중에 오류가 발생하였습니다. {} ====> SQL = {}",
-                    System.lineSeparator(), sql, e);
-            throw new RuntimeException(e);
-        }
+            return resultSetMappingTemplate.mapping(rs, rowMapper);
+        }, sql);
     }
 }
