867555e0e8dc50ee1b7a8e8ca71623523b310420
iamjooon2 <iamjooon2@gmail.com>
2023-10-03 16:18:47 +0900

    chore: final 키워드 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/RowMapper.java
@@ -6,6 +6,6 @@ import java.sql.SQLException;
 @FunctionalInterface
 public interface RowMapper<T> {
 
-    T mapRow(ResultSet resultSet) throws SQLException;
+    T mapRow(final ResultSet resultSet) throws SQLException;
 
 }
