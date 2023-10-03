a3dbf4e50b88764389b6e902d380f901afa7c350
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-10-03 15:54:04 +0900

    refactor: query 및 queryForObject 메서드 리팩터링

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -35,29 +34,38 @@ public class JdbcTemplate {
     }
 
     public <T> T queryForObject(final String sql, final Mapper<T> mapper, final Object... values) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement psmt = conn.prepareStatement(sql)) {
-            setValues(psmt, values);
-            final ResultSet rs = psmt.executeQuery();
-            log.info("query : {}", sql);
-            if (rs.next()) {
-                return mapper.map(rs);
-            }
-            return null;
-        } catch (final SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        return preparedStatementExecutor.execute(
+                conn -> {
+                    final PreparedStatement psmt = conn.prepareStatement(sql);
+                    setValues(psmt, values);
+                    return psmt;
+                },
+                psmt -> {
+                    final ResultSet rs = psmt.executeQuery();
+                    if (rs.next()) {
+                        return mapper.map(rs);
+                    }
+                    return null;
+                }
+        );
     }
 
     public <T> List<T> query(final String sql, final Mapper<T> rowMapper, final Object... values) {
-        return queryForObject(sql, rs -> {
-            final List<T> results = new ArrayList<>();
-            do {
-                results.add(rowMapper.map(rs));
-            } while (rs.next());
-            return results;
-        }, values);
+        return preparedStatementExecutor.execute(
+                conn -> {
+                    final PreparedStatement psmt = conn.prepareStatement(sql);
+                    setValues(psmt, values);
+                    return psmt;
+                },
+                psmt -> {
+                    final ResultSet rs = psmt.executeQuery();
+                    final List<T> result = new ArrayList<>();
+                    while (rs.next()) {
+                        result.add(rowMapper.map(rs));
+                    }
+                    return result;
+                }
+        );
     }
 
     private void setValues(final PreparedStatement psmt, final Object[] values) throws SQLException {
