710bbdd25e8c53daa8582b661e0c9520900403e9
Kiara Kim <101039161+kiarakim@users.noreply.github.com>
2023-10-04 11:48:15 +0900

    [JDBC 1단계] 키아라 미션 제출합니다 (#292)
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * 패키지 위치 변경 및 코드 정리
    
    * feat: 1단계 JDBC 라이브러리 구현하기
    
    * refactor: 메서드 분리
    
    * refactor: 빈 객체에 대한 예외 처리
    
    * refactor: if -> while 변경
    
    * refactor: static 키워드 제거
    
    ---------
    
    Co-authored-by: kang-hyungu <hkkang@woowahan.com>

---
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -0,0 +1,77 @@
+package org.springframework.jdbc.core;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcTemplate {
+
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int FIRST_RESULT_INDEX = 0;
+
+    private final DataSource dataSource;
+
+    public JdbcTemplate(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public int update(final String sql, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            setObjects(args, preparedStatement);
+
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.info("update error: {}", e.getMessage());
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    private void setObjects(Object[] args, PreparedStatement preparedStatement) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject(i + 1, args[i]);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+
+            setObjects(args, preparedStatement);
+
+            return getResults(rowMapper, preparedStatement);
+        } catch (SQLException e) {
+            log.info("query error: {}", e.getMessage());
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    private <T> List<T> getResults(RowMapper<T> rowMapper, PreparedStatement preparedStatement) throws SQLException {
+        List<T> results = new ArrayList<>();
+        ResultSet rs = preparedStatement.executeQuery();
+        while (rs.next()) {
+            T result = rowMapper.run(rs);
+            results.add(result);
+        }
+
+        return results;
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        List<T> results = query(sql, rowMapper, args);
+        T t = results.get(FIRST_RESULT_INDEX);
+        if (t == null) {
+            throw new DataAccessException("queryForObject error: result is null");
+        }
+        return t;
+    }
+}
