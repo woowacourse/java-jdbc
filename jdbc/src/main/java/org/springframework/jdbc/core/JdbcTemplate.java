ed8288305619ea60f6b4af674a7535207a989f12
이지우 <49433615+LJW25@users.noreply.github.com>
2023-10-05 07:07:01 +0900

    [JDBC 라이브러리 구현하기 - 2단계] 이오(이지우) 미션 제출합니다. (#428)
    * refactor: 중복코드 executor로 분리
    
    * refactor: Custom 함수형 인터페이스 구현

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,77 +1,43 @@
 package org.springframework.jdbc.core;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.CannotGetJdbcConnectionException;
 
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
-    private final DataSource dataSource;
+    private final PreparedStatementExecutor preparedStatementExecutor;
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.preparedStatementExecutor = new PreparedStatementExecutor(dataSource);
     }
 
     public void update(final String sql, final Object... params) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-
-            for (int i = 0; i < params.length; i++) {
-                pstmt.setObject(i + 1, params[i]);
-            }
-            pstmt.executeUpdate();
-        } catch (final SQLException e) {
-            throw new CannotGetJdbcConnectionException(e.getMessage());
-        }
+        preparedStatementExecutor.execute(sql, PreparedStatement::executeUpdate, params);
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... params) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql);
-             final ResultSet rs = getResultSet(pstmt, params)) {
-            log.debug("query : {}", sql);
-
+        return preparedStatementExecutor.execute(sql, pstmt -> {
+            final ResultSet rs = pstmt.executeQuery();
             final List<T> results = new ArrayList<>();
 
             while (rs.next()) {
                 results.add(rowMapper.mapRow(rs, rs.getRow()));
             }
             return results;
-        } catch (final SQLException e) {
-            throw new CannotGetJdbcConnectionException(e.getMessage());
-        }
+        }, params);
     }
 
     public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql);
-             final ResultSet rs = getResultSet(pstmt, params)) {
-            log.debug("query : {}", sql);
-
+        return preparedStatementExecutor.execute(sql, pstmt -> {
+            final ResultSet rs = pstmt.executeQuery();
             if (rs.next()) {
                 return Optional.of(rowMapper.mapRow(rs, rs.getRow()));
             }
             return Optional.empty();
-        } catch (final SQLException e) {
-            throw new CannotGetJdbcConnectionException(e.getMessage());
-        }
-    }
-
-    private ResultSet getResultSet(final PreparedStatement pstmt, final Object... params) throws SQLException {
-        for (int i = 0; i < params.length; i++) {
-            pstmt.setObject(i + 1, params[i]);
-        }
-        return pstmt.executeQuery();
+        }, params);
     }
 }
---
+++ jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementExecutor.java
@@ -0,0 +1,37 @@
+package org.springframework.jdbc.core;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+
+public class PreparedStatementExecutor {
+    private static final Logger log = LoggerFactory.getLogger(PreparedStatementExecutor.class);
+
+    private final DataSource dataSource;
+
+    public PreparedStatementExecutor(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public <T> T execute(final String sql, final PreparedStatementFunction<T> pstmtFunction, final Object... params) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            setPreparedStatement(pstmt, params);
+            return pstmtFunction.apply(pstmt);
+        } catch (final SQLException e) {
+            throw new CannotGetJdbcConnectionException(e.getMessage());
+        }
+    }
+
+    private void setPreparedStatement(final PreparedStatement pstmt, final Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            pstmt.setObject(i + 1, params[i]);
+        }
+    }
+}
