4753a27045b956d9c3b5834cd4abc21d0f6a7d17
hanueleee <ezzeezz@naver.com>
2023-10-04 15:24:18 +0900

    refactor(UserDao): RowMapper에서 index대신 컬럼명 사용

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -10,10 +10,10 @@ public class UserDao {
 
     private static final RowMapper<User> MAPPER = (ResultSet rs) ->
             new User(
-                    rs.getLong(1),
-                    rs.getString(2),
-                    rs.getString(3),
-                    rs.getString(4));
+                    rs.getLong("id"),
+                    rs.getString("account"),
+                    rs.getString("password"),
+                    rs.getString("email"));
 
     private final JdbcTemplate jdbcTemplate;
 
