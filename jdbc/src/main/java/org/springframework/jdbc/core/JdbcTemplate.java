83e1bb27e2d10e8233ef98721fa53507f77404a4
kwonyj1022 <kwonyj1022@naver.com>
2023-10-04 21:59:28 +0900

    refactor: JdbcTemplate의 try-with-resources 중복 코드 제거

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -11,6 +11,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 public class JdbcTemplate {
 
@@ -22,7 +23,7 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public int update(final String sql, final Object... args) {
+    private <T> T executeQuery(String sql, Object[] args, Function<PreparedStatement, T> action) {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)
         ) {
@@ -30,7 +31,7 @@ public class JdbcTemplate {
 
             processPreparedStatementParameter(pstmt, args);
 
-            return pstmt.executeUpdate();
+            return action.apply(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -44,47 +45,55 @@ public class JdbcTemplate {
         }
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rowMaper, final Object... args) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-
-            processPreparedStatementParameter(pstmt, args);
-
-            List<T> results = new ArrayList<>();
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                results.add(rowMaper.mapRow(rs));
+    public int update(final String sql, final Object... args) {
+        final Function<PreparedStatement, Integer> fuction = pstmt -> {
+            try {
+                return pstmt.executeUpdate();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
             }
+        };
 
-            return results;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        return executeQuery(sql, args, fuction);
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        final Function<PreparedStatement, List<T>> function = pstmt -> {
+            try {
+                List<T> results = new ArrayList<>();
+                ResultSet rs = pstmt.executeQuery();
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs));
+                }
+                return results;
+            } catch (SQLException e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e);
+            }
+        };
+
+        return executeQuery(sql, args, function);
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
+        final Function<PreparedStatement, T> function = pstmt -> {
+            try {
+                ResultSet rs = pstmt.executeQuery();
+                T findObject = null;
+                if (rs.next()) {
+                    findObject = rowMapper.mapRow(rs);
+                }
 
-            processPreparedStatementParameter(pstmt, args);
+                validateSingleResult(findObject, rs);
 
-            ResultSet rs = pstmt.executeQuery();
-
-            T findObject = null;
-            if (rs.next()) {
-                findObject = rowMapper.mapRow(rs);
+                return findObject;
+            } catch (SQLException e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e);
             }
+        };
 
-            validateSingleResult(findObject, rs);
-
-            return findObject;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        return executeQuery(sql, args, function);
     }
 
     private <T> void validateSingleResult(final T findObject, final ResultSet rs) throws SQLException {
