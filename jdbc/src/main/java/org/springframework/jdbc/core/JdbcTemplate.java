0317493160f481c8fbc32ebbda0738cc63018c18
sh111-coder <ohk9134@naver.com>
2023-10-04 20:52:17 +0900

    refactor: UserDao, UserHistoryDao에서 같은 Connection 사용하도록 리팩토링

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,6 @@
 package org.springframework.jdbc.core;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
@@ -18,6 +19,10 @@ public class JdbcTemplate {
 
     public void update(final String sql, final Object... args) {
         preparedStatementExecutor.execute(sql, PreparedStatement::executeUpdate, args);
+    }
+
+    public void update(final Connection connection, final String sql, final Object... args) {
+        preparedStatementExecutor.execute(connection, sql, PreparedStatement::executeUpdate, args);
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
