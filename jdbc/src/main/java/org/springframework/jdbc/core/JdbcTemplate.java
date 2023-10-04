79006118131e3b38ef7c366180c747cf144bd063
LJW25 <dbjwlee@gmail.com>
2023-10-04 21:38:11 +0900

    refactor: 중복코드 executor로 분리

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,77 +1,58 @@
 package org.springframework.jdbc.core;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.dao.DataAccessException;
 
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
-    private final DataSource dataSource;
+    private final PreparedStatementExecutor preparedStatementExecutor;
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.preparedStatementExecutor = new PreparedStatementExecutor(dataSource);
     }
 
     public void update(final String sql, final Object... params) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-
-            for (int i = 0; i < params.length; i++) {
-                pstmt.setObject(i + 1, params[i]);
+        preparedStatementExecutor.execute(sql, preparedStatement -> {
+            try {
+                return preparedStatement.executeUpdate();
+            } catch (final SQLException e) {
+                throw new DataAccessException(e);
             }
-            pstmt.executeUpdate();
-        } catch (final SQLException e) {
-            throw new CannotGetJdbcConnectionException(e.getMessage());
-        }
+        }, params);
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... params) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql);
-             final ResultSet rs = getResultSet(pstmt, params)) {
-            log.debug("query : {}", sql);
+        return preparedStatementExecutor.execute(sql, pstmt -> {
+            try {
+                final ResultSet rs = pstmt.executeQuery();
+                final List<T> results = new ArrayList<>();
 
-            final List<T> results = new ArrayList<>();
-
-            while (rs.next()) {
-                results.add(rowMapper.mapRow(rs, rs.getRow()));
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs, rs.getRow()));
+                }
+                return results;
+            } catch (final SQLException e) {
+                throw new DataAccessException(e);
             }
-            return results;
-        } catch (final SQLException e) {
-            throw new CannotGetJdbcConnectionException(e.getMessage());
-        }
+        }, params);
     }
 
     public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql);
-             final ResultSet rs = getResultSet(pstmt, params)) {
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return Optional.of(rowMapper.mapRow(rs, rs.getRow()));
+        return preparedStatementExecutor.execute(sql, pstmt -> {
+            try {
+                final ResultSet rs = pstmt.executeQuery();
+                if (rs.next()) {
+                    return Optional.of(rowMapper.mapRow(rs, rs.getRow()));
+                }
+                return Optional.empty();
+            } catch (final SQLException e) {
+                throw new DataAccessException(e);
             }
-            return Optional.empty();
-        } catch (final SQLException e) {
-            throw new CannotGetJdbcConnectionException(e.getMessage());
-        }
-    }
-
-    private ResultSet getResultSet(final PreparedStatement pstmt, final Object... params) throws SQLException {
-        for (int i = 0; i < params.length; i++) {
-            pstmt.setObject(i + 1, params[i]);
-        }
-        return pstmt.executeQuery();
+        }, params);
     }
 }
