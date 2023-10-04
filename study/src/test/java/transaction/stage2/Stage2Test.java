434c043f82a5b2d911a52323cdb98981986fb998
iamjooon2 <iamjooon2@gmail.com>
2023-10-04 22:14:34 +0900

    트랜잭션 학습 테스트

--- study/src/test/java/transaction/stage2/Stage2Test.java
+++ study/src/test/java/transaction/stage2/Stage2Test.java
@@ -37,7 +37,10 @@ class Stage2Test {
 
     /**
      * 생성된 트랜잭션이 몇 개인가?
+     * - 생성된 트랜잭션은 1개
      * 왜 그런 결과가 나왔을까?
+     * - 두번째 메서드의 트랜잭션 전파 레벨이 PROPAGATION_REQUIRED이기 때문
+     * - 첫번째 트랜잭션 생성 후, 두번째 트랜잭션은 시작되지 않고 첫 트랜잭션에 참여함
      */
     @Test
     void testRequired() {
@@ -45,13 +48,16 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactly("transaction.stage2.FirstUserService.saveFirstTransactionWithRequired");
     }
 
     /**
      * 생성된 트랜잭션이 몇 개인가?
+     * - 생성된 트랜잭션은 2개
      * 왜 그런 결과가 나왔을까?
+     * - 두번째 메서드의 트랜잭션 전파 레벨이 PROPAGATION_REQUIRED_NEW이기 때문
+     * - 첫번째 트랜잭션이 있어도, 새로운 트랜잭션을 생성함
      */
     @Test
     void testRequiredNew() {
@@ -59,27 +65,35 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(2)
+                .containsExactly(
+                        "transaction.stage2.SecondUserService.saveSecondTransactionWithRequiresNew",
+                        "transaction.stage2.FirstUserService.saveFirstTransactionWithRequiredNew"
+                );
     }
 
     /**
      * firstUserService.saveAndExceptionWithRequiredNew()에서 강제로 예외를 발생시킨다.
      * REQUIRES_NEW 일 때 예외로 인한 롤백이 발생하면서 어떤 상황이 발생하는 지 확인해보자.
+     * - 새 트랜잭션에서 생성된 save()는 예외가 터지지 않아 롤백되지 않음
+     * - 기존 트랜잭션에서의 save()만 롤백이 된다
      */
     @Test
     void testRequiredNewWithRollback() {
-        assertThat(firstUserService.findAll()).hasSize(-1);
+        assertThat(firstUserService.findAll()).isEmpty();
 
         assertThatThrownBy(() -> firstUserService.saveAndExceptionWithRequiredNew())
                 .isInstanceOf(RuntimeException.class);
 
-        assertThat(firstUserService.findAll()).hasSize(-1);
+        assertThat(firstUserService.findAll()).hasSize(1);
     }
 
     /**
      * FirstUserService.saveFirstTransactionWithSupports() 메서드를 보면 @Transactional이 주석으로 되어 있다.
      * 주석인 상태에서 테스트를 실행했을 때와 주석을 해제하고 테스트를 실행했을 때 어떤 차이점이 있는지 확인해보자.
+     * - 주석 있을 시 : 첫 트랜잭션만 실행됨
+     * - 주석 없을 시 : 새 트랜잭션만 실행됨
+     * Propagation.supports -> 기존 트랜잭션이 있으면 거기 붙는 놈
      */
     @Test
     void testSupports() {
@@ -87,14 +101,17 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactly("transaction.stage2.FirstUserService.saveFirstTransactionWithSupports");
     }
 
     /**
      * FirstUserService.saveFirstTransactionWithMandatory() 메서드를 보면 @Transactional이 주석으로 되어 있다.
      * 주석인 상태에서 테스트를 실행했을 때와 주석을 해제하고 테스트를 실행했을 때 어떤 차이점이 있는지 확인해보자.
      * SUPPORTS와 어떤 점이 다른지도 같이 챙겨보자.
+     * - 주석 있을 시 : 예외 발생
+     * - 주석 없을 시 : 첫 트랜잭션만 실행 됨
+     *  Propagation.Mandatory -> 말 그대로 기존 트랜잭션이 의무로 있어야됨
      */
     @Test
     void testMandatory() {
@@ -102,8 +119,8 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactly("transaction.stage2.FirstUserService.saveFirstTransactionWithMandatory");
     }
 
     /**
@@ -112,6 +129,13 @@ class Stage2Test {
      * 다시 테스트를 실행하면 몇 개의 물리적 트랜잭션이 동작할까?
      *
      * 스프링 공식 문서에서 물리적 트랜잭션과 논리적 트랜잭션의 차이점이 무엇인지 찾아보자.
+     *
+     * 주석 있을 시 : 트랜잭션 1개
+     * 주석 없을 시 : 트랜잭션 2개
+     * 실제 데이터베이스의 트랜잭션 -> 물리적 트랜잭션
+     * 스프링이 처리하는 트랜잭션 -> 논리적 트랜잭션
+     * https://docs.spring.io/spring-framework/docs/4.2.x/spring-framework-reference/html/transaction.html
+     *
      */
     @Test
     void testNotSupported() {
@@ -119,13 +143,21 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(2)
+                .containsExactly(
+                        "transaction.stage2.SecondUserService.saveSecondTransactionWithNotSupported",
+                        "transaction.stage2.FirstUserService.saveFirstTransactionWithNotSupported"
+                );
     }
 
     /**
      * 아래 테스트는 왜 실패할까?
      * FirstUserService.saveFirstTransactionWithNested() 메서드의 @Transactional을 주석 처리하면 어떻게 될까?
+     *
+     * - 주석 있을 시: 두 번째 트랜잭션만 실행됨
+     * - 주석 없을 시: 예외발생 NestedTransactionNotSupportedException -> JPA는 변경감지로 업데이트해서 못쓴다~
+     * https://velog.io/@changyeonyoo/%EB%8F%84%EB%8C%80%EC%B2%B4-%EC%99%9C-%EB%A1%A4%EB%B0%B1%EB%90%98%EB%8A%94%EA%B1%B0%EC%95%BC
+     * Propagation.Nested -> 새로 트랜잭션 만듬
      */
     @Test
     void testNested() {
@@ -133,12 +165,16 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactly("transaction.stage2.SecondUserService.saveSecondTransactionWithNested");
     }
 
     /**
      * 마찬가지로 @Transactional을 주석처리하면서 관찰해보자.
+     * - 주석 있을 시: 두 번째 트랜잭션만 실행됨 -> 스프링단에서만 논리적으로 실행되는 것뿐 물리적으로는 실행되지 않는듯..?
+     * - 주석 없을 시: 예외발생 IllegalTransactionStateException -> never는 트랜잭션 못쓴다
+     *
+     * Propagation.Never -> 트랜잭션 멈춰!
      */
     @Test
     void testNever() {
@@ -146,7 +182,7 @@ class Stage2Test {
 
         log.info("transactions : {}", actual);
         assertThat(actual)
-                .hasSize(0)
-                .containsExactly("");
+                .hasSize(1)
+                .containsExactly("transaction.stage2.SecondUserService.saveSecondTransactionWithNever");
     }
 }
