1a99a87df96dc3a80edc330c4370ace75c0ccbfd
swonny <wonny921@gmail.com>
2023-10-04 09:27:41 +0900

    fix: 단일 조회 누락된 로직 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int FIRST_PARAMETER_INDEX = 1;
 
     private final DataSource dataSource;
 
@@ -26,6 +27,8 @@ public class JdbcTemplate {
              final PreparedStatement preparedStatement = connection.prepareStatement(sql);
         ) {
             log.debug("query : {}", sql);
+
+            preparedStatement.setObject(FIRST_PARAMETER_INDEX, parameter);
 
             final ResultSet resultSet = preparedStatement.executeQuery();
             if (resultSet.next()) {
