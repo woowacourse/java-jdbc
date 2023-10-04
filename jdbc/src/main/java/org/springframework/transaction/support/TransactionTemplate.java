31030a3013aa11e57e29158f497fd1cffec2c134
seokjin8678 <seokjin8678@gmail.com>
2023-10-04 14:46:15 +0900

    feat: Propagation 적용

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionTemplate.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionTemplate.java
@@ -5,10 +5,17 @@ import org.springframework.jdbc.datasource.ConnectionManager;
 
 public class TransactionTemplate {
 
+    private static final ThreadLocal<Boolean> propagations = new ThreadLocal<>();
+
     private TransactionTemplate() {
     }
 
     public static void execute(Runnable runnable) {
+        if (isPropagation()) {
+            runnable.run();
+            return;
+        }
+        propagations.set(Boolean.TRUE);
         TransactionManager.begin();
         try {
             runnable.run();
@@ -17,10 +24,20 @@ public class TransactionTemplate {
             throw e;
         } finally {
             ConnectionManager.releaseConnection();
+            propagations.remove();
         }
     }
 
+    private static boolean isPropagation() {
+        Boolean propagation = propagations.get();
+        return propagation != null;
+    }
+
     public static <T> T execute(Supplier<T> supplier) {
+        if (isPropagation()) {
+            return supplier.get();
+        }
+        propagations.set(Boolean.TRUE);
         TransactionManager.begin();
         try {
             return supplier.get();
@@ -29,6 +46,7 @@ public class TransactionTemplate {
             throw e;
         } finally {
             ConnectionManager.releaseConnection();
+            propagations.remove();
         }
     }
 }
