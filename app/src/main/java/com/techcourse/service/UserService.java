3b8838a4dbc408470c81591152dd2e966b114f57
cpot5620 <cpot5620@gmail.com>
2023-10-05 01:06:18 +0900

    feat: UserService 트랜잭션 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,29 +4,56 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import org.springframework.dao.DataAccessException;
+import org.springframework.transaction.Transaction;
+import org.springframework.transaction.TransactionManager;
 
 public class UserService {
 
+    private final TransactionManager transactionManager;
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(TransactionManager transactionManager, UserDao userDao, UserHistoryDao userHistoryDao) {
+        this.transactionManager = transactionManager;
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
     }
 
-    public User findById(final long id) {
+    public User findById(long id) {
         return userDao.findById(id);
     }
 
-    public void insert(final User user) {
-        userDao.insert(user);
+    public void insert(User user) {
+        Transaction transaction = transactionManager.getTransaction();
+        transaction.start();
+
+        try {
+            userDao.insert(transaction.getConnection(), user);
+            transaction.commit();
+        } catch (DataAccessException e) {
+            transaction.rollback();
+            throw e;
+        }
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(long id, String newPassword, String createBy) {
+        Transaction transaction = transactionManager.getTransaction();
+        transaction.start();
+        try {
+            Connection connection = transaction.getConnection();
+            User user = findById(id);
+
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+            transaction.commit();
+        } catch (DataAccessException e) {
+            transaction.rollback();
+            throw e;
+        }
     }
+
 }
