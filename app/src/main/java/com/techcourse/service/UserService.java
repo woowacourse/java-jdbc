3e897d362a67bd10829e552da1c24d6a21133f1d
java-saeng <gusdn85554@naver.com>
2023-10-04 16:59:41 +0900

    feat : Service 계층에 TransactionManager 추가

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,29 +4,40 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import org.springframework.jdbc.core.TransactionManager;
 
 public class UserService {
 
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
+  private final UserDao userDao;
+  private final UserHistoryDao userHistoryDao;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-    }
+  public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    this.userDao = userDao;
+    this.userHistoryDao = userHistoryDao;
+  }
 
-    public User findById(final long id) {
-        return userDao.findById(id);
-    }
+  public User findById(final long id) {
+    return userDao.findById(id);
+  }
 
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
+  public void insert(final User user) {
+    userDao.insert(user);
+  }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+  public void changePassword(final long id, final String newPassword, final String createBy) {
+    final var user = findById(id);
+
+    TransactionManager.start();
+    try {
+      user.changePassword(newPassword);
+      userDao.update(user);
+      userHistoryDao.log(new UserHistory(user, createBy));
+      TransactionManager.commit();
+    } catch (Exception e) {
+      TransactionManager.rollback();
+      throw e;
+    } finally {
+      TransactionManager.release();
     }
+  }
 }
