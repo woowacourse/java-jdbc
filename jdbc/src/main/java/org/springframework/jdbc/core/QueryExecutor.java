0144455b24f634f6f9de072248a7cc1c9d4dc4d8
Miseong Kim <miseongk@naver.com>
2023-10-05 14:03:23 +0900

    refactor: 함수형 인터페이스 어노테이션 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/QueryExecutor.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/QueryExecutor.java
@@ -3,6 +3,7 @@ package org.springframework.jdbc.core;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface QueryExecutor<T> {
 
     T execute(PreparedStatement preparedStatement) throws SQLException;
