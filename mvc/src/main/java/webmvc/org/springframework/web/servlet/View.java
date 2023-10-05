ea1f106f883babf12311c63628094b2185737a45
ReO <yt930310@gmail.com>
2023-10-05 18:55:19 +0900

    chore: 개행 추가

--- mvc/src/main/java/webmvc/org/springframework/web/servlet/View.java
+++ mvc/src/main/java/webmvc/org/springframework/web/servlet/View.java
@@ -6,5 +6,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 public interface View {
+
     void render(Map<String, ?> model, HttpServletRequest request, HttpServletResponse response) throws Exception;
 }
