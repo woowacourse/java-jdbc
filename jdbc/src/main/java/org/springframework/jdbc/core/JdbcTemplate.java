037523db3bbe3a4a23f1e277eb762383b26c8d5a
신종화 <77482065+jjongwa@users.noreply.github.com>
2023-10-05 16:50:50 +0900

    [JDBC 라이브러리 구현하기 - 2단계] 디노(신종화) 미션 제출합니다. (#350)
    * refactor: `@FunctionalInterface` 어노테이션 추가
    
    * refactor: 중복 메서드 제거 및 ResultSet 리소스 반납
    
    * refactor: final 추가
    
    * refactor: exception 변경
    
    * refactor: 줄바꿈 동일하게 변경
    
    * refactor: 리뷰 반영
    
    * refactor: 템플릿 콜백 패턴 적용

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -1,6 +1,5 @@
 package org.springframework.jdbc.core;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -8,71 +7,33 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Nullable;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class JdbcTemplate {
-
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
-    private final DataSource dataSource;
+public class JdbcTemplate extends AutoClosableTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        super(dataSource);
     }
 
-    public void execute(final String sql, final Object... objects) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-            setValues(pstmt, objects);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+    @Override
+    protected void commandQuery(final PreparedStatement pstmt) throws SQLException {
+        pstmt.executeUpdate();
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-            setValues(pstmt, objects);
-            final ResultSet rs = pstmt.executeQuery();
-            final List<T> list = new ArrayList<>();
-            while (rs.next()) {
-                list.add(rowMapper.mapRow(rs));
-            }
-            return list;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+    @Override
+    protected <T> List<T> queryAll(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
+        final List<T> list = new ArrayList<>();
+        while (rs.next()) {
+            list.add(rowMapper.mapRow(rs));
         }
+        return list;
     }
 
     @Nullable
-    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... objects) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-            setValues(pstmt, objects);
-            final ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return rowMapper.mapRow(rs);
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+    @Override
+    protected <T> T queryForOne(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
+        if (rs.next()) {
+            return rowMapper.mapRow(rs);
         }
-    }
-
-    private void setValues(final PreparedStatement pstmt, Object... objects) throws SQLException {
-        for (int i = 0; i < objects.length; i++) {
-            pstmt.setObject(i + 1, objects[i]);
-        }
+        return null;
     }
 }
