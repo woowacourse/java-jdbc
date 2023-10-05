56f7781f50b61db5af5fad97a41d6471aaec85b4
zillionme <beatus96@naver.com>
2023-10-06 08:08:43 +0900

    refactor: try-with-resource 중복 제거

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,18 +1,13 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.util.List;
-import java.util.Optional;
 
 public class UserDao {
-
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
     private static final RowMapper<User> USER_MAPPER = (rs) -> new User(
             rs.getLong(1),
@@ -48,13 +43,11 @@ public class UserDao {
 
     public User findById(final Long id) {
         final var sql = "select * from users where id = ?";
-        Optional<User> user = jdbcTemplate.queryForObject(sql, USER_MAPPER, id);
-        return user.orElseThrow(() -> new RuntimeException());
+        return jdbcTemplate.queryForObject(sql, USER_MAPPER, id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select * from users where account = ?";
-        Optional<User> user = jdbcTemplate.queryForObject(sql, USER_MAPPER, account);
-        return user.orElseThrow(() -> new RuntimeException());
+        return jdbcTemplate.queryForObject(sql, USER_MAPPER, account);
     }
 }
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,6 +2,7 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -10,7 +11,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 public class JdbcTemplate {
 
@@ -23,57 +23,42 @@ public class JdbcTemplate {
     }
 
     public void update(final String sql, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-
-            bindParameterWithStatement(pstmt, args);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        execute(sql, args, PreparedStatement::executeUpdate);
     }
 
-    private static void bindParameterWithStatement(final PreparedStatement pstmt, final Object[] args) throws SQLException {
-        for (int i = 0; i < args.length; i++) {
-            pstmt.setObject(i + 1, args[i]);
-        }
-    }
-
-    public <T> Optional<T> queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-
-            bindParameterWithStatement(pstmt, args);
-            final ResultSet resultSet = pstmt.executeQuery();
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        return execute(sql, args, (PreparedStatement pstmt) -> {
+            ResultSet resultSet = pstmt.executeQuery();
             if (resultSet.next()) {
-                return Optional.of(rowMapper.mapRow(resultSet));
+                return rowMapper.mapRow(resultSet);
             }
-            return Optional.empty();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+            throw new RuntimeException();
+        });
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-
-            bindParameterWithStatement(pstmt, args);
-
-            final ResultSet resultSet = pstmt.executeQuery();
+        return execute(sql, args, (PreparedStatement pstmt) -> {
+            ResultSet resultSet = pstmt.executeQuery();
             final List<T> result = new ArrayList<>();
             while (resultSet.next()) {
                 result.add(rowMapper.mapRow(resultSet));
             }
             return result;
+        });
+    }
+
+    private <T> T execute(final String sql, final Object[] args, final PreparedStatementFunction<T> preparedStatementExecutor) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i + 1, args[i]);
+            }
+            return preparedStatementExecutor.execute(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
     }
 }
