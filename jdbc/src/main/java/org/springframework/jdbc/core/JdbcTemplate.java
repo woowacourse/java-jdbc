87cf51c23dffa704eb5b2d5ad15ae051808cf2e2
70825 <hello70825@gmail.com>
2023-10-06 00:53:53 +0900

    refactor: executeQuery로 중복 제거

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -15,6 +15,8 @@ import org.springframework.dao.DataAccessException;
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int ONE_DATA_INDEX = 0;
+    private static final int TWO = 2;
 
     private final DataSource dataSource;
 
@@ -34,15 +36,17 @@ public class JdbcTemplate {
     }
 
     public <T> Optional<T> queryForObject(final String query, final RowMapper<T> rowMapper, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = getPreparedStatement(conn, query, args);
-             final ResultSet rs = pstmt.executeQuery()) {
+        try {
+            final List<T> results = executeQuery(query, rowMapper, args);
 
-            if (!rs.next()) {
-                return Optional.empty();
+            if (results.size() >= TWO) {
+                throw new DataAccessException("2개 이상의 데이터가 존재합니다");
             }
 
-            return Optional.of(rowMapper.mapToRow(rs));
+            if (results.isEmpty()) {
+                return Optional.empty();
+            }
+            return Optional.of(results.get(ONE_DATA_INDEX));
         } catch (final Exception e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
@@ -50,25 +54,32 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> queryForList(final String query, final RowMapper<T> rowMapper, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = getPreparedStatement(conn, query, args);
-             final ResultSet rs = pstmt.executeQuery()) {
-
-            final List<T> objects = new ArrayList<>();
-            while (rs.next()) {
-                objects.add(rowMapper.mapToRow(rs));
-            }
-
-            return objects;
+        try {
+            return executeQuery(query, rowMapper, args);
         } catch (final Exception e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
         }
     }
 
+    private <T> List<T> executeQuery(final String query, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = getPreparedStatement(conn, query, args);
+             final ResultSet rs = pstmt.executeQuery()) {
+            final List<T> objects = new ArrayList<>();
+
+            while (rs.next()) {
+                objects.add(rowMapper.mapToRow(rs));
+            }
+
+            return objects;
+        } catch (final SQLException e) {
+            throw new RuntimeException("Error database connection", e);
+        }
+    }
+
     private PreparedStatement getPreparedStatement(final Connection connection, final String sql, final Object... args)
             throws SQLException {
-
         final PreparedStatement preparedStatement = connection.prepareStatement(sql);
 
         for (int i = 0; i < args.length; i++) {
