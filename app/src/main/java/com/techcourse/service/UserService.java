ef1ac62038813ce4fb3d9486608cf30d6fc226cc
최준호 <junho5336@gmail.com>
2023-10-05 12:57:25 +0900

    feat: TransactionExecutor 추가

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,10 +4,8 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
-import org.springframework.dao.DataAccessException;
+import org.springframework.transaction.TransactionExecutor;
 import org.springframework.transaction.TransactionManager;
-
-import java.sql.SQLException;
 
 public class UserService {
 
@@ -30,20 +28,11 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        try {
-            transactionManager.begin();
+        TransactionExecutor.execute(transactionManager, () -> {
             final var user = findById(id);
             user.changePassword(newPassword);
             userDao.update(transactionManager.getConnection(), user);
             userHistoryDao.log(transactionManager.getConnection(), new UserHistory(user, createBy));
-            transactionManager.commit();
-        } catch (Exception e) {
-            try {
-                transactionManager.rollback();
-            } catch (SQLException ex) {
-                throw new DataAccessException(ex);
-            }
-            throw new DataAccessException(e);
-        }
+        });
     }
 }
