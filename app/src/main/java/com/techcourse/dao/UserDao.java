4dc1b9b72710cfd6da8811a28fc5d0ab898286b4
kim dae hee <akdlzmf1123@gmail.com>
2023-10-05 13:54:16 +0900

    refactor: 중복되는 rowmapper 상수 추출

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -4,8 +4,16 @@ import com.techcourse.domain.User;
 import java.util.List;
 import java.util.NoSuchElementException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.MyRowMapper;
 
 public class UserDao {
+
+    public static final MyRowMapper<User> USER_ROW_MAPPER = rs -> new User(
+        rs.getLong(1),
+        rs.getString(2),
+        rs.getString(3),
+        rs.getString(4)
+    );
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -37,33 +45,18 @@ public class UserDao {
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
 
-        return jdbcTemplate.queryAll(sql, rs -> new User(
-            rs.getLong(1),
-            rs.getString(2),
-            rs.getString(3),
-            rs.getString(4)
-        ));
+        return jdbcTemplate.queryAll(sql, USER_ROW_MAPPER);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        return jdbcTemplate.queryForObject(sql,  rs -> new User(
-            rs.getLong(1),
-            rs.getString(2),
-            rs.getString(3),
-            rs.getString(4)
-        ), id).orElseThrow(NoSuchElementException::new);
+        return jdbcTemplate.queryForObject(sql,  USER_ROW_MAPPER, id).orElseThrow(NoSuchElementException::new);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
 
-        return jdbcTemplate.queryForObject(sql,  rs -> new User(
-            rs.getLong(1),
-            rs.getString(2),
-            rs.getString(3),
-            rs.getString(4)
-        ), account).orElseThrow(NoSuchElementException::new);
+        return jdbcTemplate.queryForObject(sql,  USER_ROW_MAPPER, account).orElseThrow(NoSuchElementException::new);
     }
 }
