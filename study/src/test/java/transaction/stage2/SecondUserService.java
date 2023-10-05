8347f1298d10bdfcd8f7694a7827f362c82b66e8
greeng00se <thegreengoose7@gmail.com>
2023-10-05 09:23:04 +0900

    test: transaction 학습 테스트

--- study/src/test/java/transaction/stage2/SecondUserService.java
+++ study/src/test/java/transaction/stage2/SecondUserService.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 @Service
