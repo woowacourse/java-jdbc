83c83655e83cd65cc67c97d8293cecb5bb9bde9a
70825 <hello70825@gmail.com>
2023-10-06 01:52:25 +0900

    refactor: try-catch문 중복 제거

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -7,11 +7,11 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.DatabaseConnectionException;
 
 public class JdbcTemplate {
 
@@ -27,10 +27,21 @@ public class JdbcTemplate {
     }
 
     public int update(final String query, final Object... args) {
+        return execute(this::executeUpdate, query, args);
+    }
+
+    private int executeUpdate(final PreparedStatement pstmt) {
+        try {
+            return pstmt.executeUpdate();
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> T execute(final Function<PreparedStatement, T> function, final String query, final Object... args) {
         try (final Connection conn = dataSource.getConnection();
              final PreparedStatement pstmt = getPreparedStatement(conn, query, args)) {
-
-            return pstmt.executeUpdate();
+            return function.apply(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
@@ -38,46 +49,35 @@ public class JdbcTemplate {
     }
 
     public <T> Optional<T> queryForObject(final String query, final RowMapper<T> rowMapper, final Object... args) {
-        try {
-            final List<T> results = executeQuery(query, rowMapper, args);
+        final List<T> results = executeQuery(query, rowMapper, args);
 
-            if (results.size() >= TWO) {
-                throw new DataAccessException("2개 이상의 데이터가 존재합니다");
-            }
-
-            if (results.isEmpty()) {
-                return Optional.empty();
-            }
-            return Optional.of(results.get(ONE_DATA_INDEX));
-        } catch (final Exception e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
+        if (results.size() >= TWO) {
+            throw new DataAccessException("2개 이상의 데이터가 존재합니다");
         }
+
+        if (results.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(results.get(ONE_DATA_INDEX));
     }
 
     public <T> List<T> queryForList(final String query, final RowMapper<T> rowMapper, final Object... args) {
-        try {
-            return executeQuery(query, rowMapper, args);
-        } catch (final Exception e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
-        }
+        return executeQuery(query, rowMapper, args);
     }
 
     private <T> List<T> executeQuery(final String query, final RowMapper<T> rowMapper, final Object... args) {
-        try (final Connection conn = dataSource.getConnection();
-             final PreparedStatement pstmt = getPreparedStatement(conn, query, args);
-             final ResultSet rs = pstmt.executeQuery()) {
-            final List<T> objects = new ArrayList<>();
-
-            while (rs.next()) {
-                objects.add(rowMapper.mapToRow(rs));
+        return execute(pstmt -> {
+            try {
+                final ResultSet rs = pstmt.executeQuery();
+                final List<T> objects = new ArrayList<>();
+                while (rs.next()) {
+                    objects.add(rowMapper.mapToRow(rs));
+                }
+                return objects;
+            } catch (final SQLException e) {
+                throw new DataAccessException("Error execute query", e);
             }
-
-            return objects;
-        } catch (final SQLException e) {
-            throw new DatabaseConnectionException("Error database connection", e);
-        }
+        }, query, args);
     }
 
     private PreparedStatement getPreparedStatement(final Connection connection, final String sql, final Object... args)
