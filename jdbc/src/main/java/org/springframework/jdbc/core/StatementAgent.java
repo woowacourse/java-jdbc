3195800baa4eef5899d123b08e2c442e26eb3aa5
woosung1223 <dntjd991223@naver.com>
2023-10-05 13:15:47 +0900

    feat: 트랜잭션 기능 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/StatementAgent.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/StatementAgent.java
@@ -19,6 +19,19 @@ public class StatementAgent {
         this.dataSource = dataSource;
     }
 
+    public <T> T service(final Connection connection, final String sql, final StatementCallback<T> statementCallback,
+                         final Object... args) {
+        try (final PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            setParameters(statement, args);
+            return statementCallback.call(statement);
+
+        } catch (SQLException e) {
+            log.error("statement agent service failed.");
+            throw new DataAccessException(e);
+        }
+    }
+
     public <T> T service(final String sql, final StatementCallback<T> statementCallback, final Object... args) {
         try (final Connection connection = dataSource.getConnection();
              final PreparedStatement statement = connection.prepareStatement(sql)) {
