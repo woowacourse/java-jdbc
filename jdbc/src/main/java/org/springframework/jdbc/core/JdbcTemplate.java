d3cd82f6f193743aeaf19b72bbc63e2994d91e18
Miseong Kim <miseongk@naver.com>
2023-10-06 01:30:22 +0900

    refactor: JdbcTemplate 메소드 분리 및 ResultSet 직접 close 하는 부분 제거

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import javax.sql.DataSource;
-import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.exception.InvalidDataSizeException;
 
 public class JdbcTemplate {
@@ -26,22 +25,11 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(final String query, final RowMapper<T> rowMapper, final Object... columns) {
-        return queryExecutorService.execute(pstmt -> {
-            final ResultSet resultSet = pstmt.executeQuery();
-            final List<T> result = getResult(rowMapper, resultSet);
-            closeResultSet(resultSet);
-            return result;
-        }, query, columns);
+        return execute(query, rowMapper, columns);
     }
 
     public <T> Optional<T> queryForObject(final String query, final RowMapper<T> rowMapper, final Object... columns) {
-        final List<T> results = queryExecutorService.execute(pstmt -> {
-            final ResultSet resultSet = pstmt.executeQuery();
-            final List<T> result = getResult(rowMapper, resultSet);
-            closeResultSet(resultSet);
-            return result;
-        }, query, columns);
-
+        final List<T> results = execute(query, rowMapper, columns);
         validateSize(results);
         if (results.isEmpty()) {
             return Optional.empty();
@@ -49,29 +37,24 @@ public class JdbcTemplate {
         return Optional.of(results.get(FIRST_INDEX_OF_RESULT));
     }
 
-    private <T> List<T> getResult(final RowMapper<T> rowMapper, final ResultSet rs) {
+    private <T> List<T> execute(final String query, final RowMapper<T> rowMapper, final Object[] columns) {
+        return queryExecutorService.execute(pstmt -> {
+            final ResultSet resultSet = pstmt.executeQuery();
+            return getResult(rowMapper, resultSet);
+        }, query, columns);
+    }
+
+    private <T> List<T> getResult(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
         final List<T> result = new ArrayList<>();
-        try {
-            while (rs.next()) {
-                result.add(rowMapper.map(rs));
-            }
-            return result;
-        } catch (final SQLException e) {
-            throw new DataAccessException(e);
+        while (rs.next()) {
+            result.add(rowMapper.map(rs));
         }
+        return result;
     }
 
     private <T> void validateSize(final List<T> result) {
         if (result.size() > MIN_RESULT_SIZE) {
             throw new InvalidDataSizeException("결과가 1건 이상 조회되었습니다.");
-        }
-    }
-
-    private void closeResultSet(final ResultSet resultSet) {
-        try {
-            resultSet.close();
-        } catch (final SQLException e) {
-            throw new DataAccessException(e);
         }
     }
 }
