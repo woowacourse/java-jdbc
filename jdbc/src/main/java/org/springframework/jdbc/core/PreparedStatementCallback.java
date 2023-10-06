078099915a6d595db20335d7d4d6007fbb73bda0
chaewon121 <cw121@naver.com>
2023-10-06 17:14:35 +0900

    refactor: 어노테이션 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementCallback.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementCallback.java
@@ -3,6 +3,7 @@ package org.springframework.jdbc.core;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementCallback<T> {
     T execute(final PreparedStatement preparedStatement) throws SQLException;
 }
