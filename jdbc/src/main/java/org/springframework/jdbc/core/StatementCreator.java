8a42b5300a45873cf44116e0dd1466cec21203c1
hum02 <ehdgma02@gmail.com>
2023-10-06 14:46:44 +0900

    refactor:sql검증 로직 삭제

--- jdbc/src/main/java/org/springframework/jdbc/core/StatementCreator.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/StatementCreator.java
@@ -6,8 +6,6 @@ import java.sql.SQLException;
 
 public class StatementCreator {
 
-    private static final String EMPTY_STRING = "";
-
     private StatementCreator() {
     }
 
@@ -19,22 +17,7 @@ public class StatementCreator {
 
     private static void setPreparedStatement(PreparedStatement pstmt, Object... arguments) throws SQLException {
         for (int argumentIdx = 1; argumentIdx < arguments.length + 1; argumentIdx++) {
-            Object argument = SQLValidator.filterArgument(arguments[argumentIdx - 1]);
-            pstmt.setObject(argumentIdx, argument);
-        }
-    }
-
-
-    private static class SQLValidator {
-
-        private static final String SQL_PATTERN = "[^a-zA-Z_=,\\-:'!><.?\"`% ()0-9*@\n\r]*";
-
-        private static Object filterArgument(Object argument) {
-            Object filtered = argument;
-            if (argument instanceof String) {
-                filtered = ((String) argument).replaceAll(SQL_PATTERN, EMPTY_STRING);
-            }
-            return filtered;
+            pstmt.setObject(argumentIdx, arguments[argumentIdx - 1]);
         }
     }
 }
