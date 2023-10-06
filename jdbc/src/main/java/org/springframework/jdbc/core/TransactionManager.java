fcc4d7aa8d11f4ebdafe92ced56cd4c601375ef8
yoondgu <doyoungwork@gmail.com>
2023-10-06 14:00:43 +0900

    refactor: setAutoCommit true finally 블록으로 통일

--- jdbc/src/main/java/org/springframework/jdbc/core/TransactionManager.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/TransactionManager.java
@@ -2,12 +2,16 @@ package org.springframework.jdbc.core;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 
 public class TransactionManager {
 
+    private final Logger log = LoggerFactory.getLogger(TransactionManager.class);
     private final ConnectionManager connectionManager;
 
     public TransactionManager(final ConnectionManager connectionManager) {
@@ -20,7 +24,6 @@ public class TransactionManager {
             connection.setAutoCommit(false);
             consumer.accept(connection, entity);
             connection.commit();
-            connection.setAutoCommit(true);
             return entity;
         } catch (Exception exception) {
             try {
@@ -28,6 +31,13 @@ public class TransactionManager {
                 throw new DataAccessException(exception);
             } catch (SQLException sqlException) {
                 throw new DataAccessException(sqlException);
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException ignored) {
+                log.warn("fail to set auto commit true due to {}", ignored.getMessage());
+                log.warn(Arrays.toString(ignored.getStackTrace()));
             }
         }
     }
@@ -38,7 +48,6 @@ public class TransactionManager {
             connection.setAutoCommit(false);
             final var result = function.apply(connection, parameters);
             connection.commit();
-            connection.setAutoCommit(true);
             return result;
         } catch (Exception exception) {
             try {
@@ -46,6 +55,13 @@ public class TransactionManager {
                 throw new DataAccessException(exception);
             } catch (SQLException sqlException) {
                 throw new DataAccessException(sqlException);
+            }
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+            } catch (SQLException ignored) {
+                log.warn("fail to set auto commit true due to {}", ignored.getMessage());
+                log.warn(Arrays.toString(ignored.getStackTrace()));
             }
         }
     }
