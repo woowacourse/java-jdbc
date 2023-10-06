1601e3a40b266bfb428b108933cc9a5ace719d60
jaeyeon kim <jakind@naver.com>
2023-10-07 00:39:57 +0900

    refactor : 함수형 인터페이스를 통해 중복되는 로직 간소화

---
+++ app/src/main/java/com/techcourse/service/TransactionExecutor.java
@@ -0,0 +1,43 @@
+package com.techcourse.service;
+
+import com.techcourse.config.DataSourceConfig;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import org.springframework.dao.DataAccessException;
+
+public class TransactionExecutor {
+
+    private TransactionExecutor() {
+    }
+
+    public static void transactionCommand(Consumer<Connection> consumer) {
+        try {
+            Connection connection = getConnection();
+            connection.setAutoCommit(false);
+
+            consumer.accept(connection);
+
+            connection.commit();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public static <T> T transactionQuery(Function<Connection, T> function) {
+        try {
+            Connection connection = getConnection();
+
+            return function.apply(connection);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private static Connection getConnection() throws SQLException {
+        return DataSourceConfig.getInstance()
+                .getConnection();
+    }
+
+}
--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,67 +1,37 @@
 package com.techcourse.service;
 
-import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
-import java.sql.SQLException;
-import javax.sql.DataSource;
-import org.springframework.dao.DataAccessException;
 
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
-    private final DataSource dataSource;
 
     public UserService(UserDao userDao, UserHistoryDao userHistoryDao) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
-        this.dataSource = DataSourceConfig.getInstance();
     }
 
     public User findById(long id) {
-        try {
-            Connection connection = getConnection();
-            return userDao.findById(connection, id);
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        return TransactionExecutor.transactionQuery(connection -> userDao.findById(connection, id));
     }
 
     public void insert(User user) {
-        try {
-            Connection connection = getConnection();
-            connection.setAutoCommit(false);
-
-            userDao.insert(connection, user);
-
-            connection.commit();
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        TransactionExecutor.transactionCommand(connection -> userDao.insert(connection, user));
     }
 
     public void changePassword(long id, String newPassword, String createBy) {
-        try {
-            Connection connection = getConnection();
-            connection.setAutoCommit(false);
-
-            User user = userDao.findById(connection, id);
-            user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(connection, new UserHistory(user, createBy));
-
-            connection.commit();
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
-    }
-
-    private Connection getConnection() throws SQLException {
-        return dataSource.getConnection();
+        TransactionExecutor.transactionCommand(
+                connection -> {
+                    User user = userDao.findById(connection, id);
+                    user.changePassword(newPassword);
+                    userDao.update(connection, user);
+                    userHistoryDao.log(connection, new UserHistory(user, createBy));
+                }
+        );
     }
 
 }
