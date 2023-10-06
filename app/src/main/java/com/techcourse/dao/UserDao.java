6d7851dbeda309046fa9abade0be3b3f14b34677
hanueleee <ezzeezz@naver.com>
2023-10-06 17:01:23 +0900

    feat(UserDao): Connection을 받는 update 메소드 추가

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.util.List;
 import javax.sql.DataSource;
@@ -34,6 +35,11 @@ public class UserDao {
     public void update(final User user) {
         final var sql = "update users set account=?, password=?, email=? where id=?";
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
+    }
+
+    public void update(final User user, final Connection connection) {
+        final var sql = "update users set account=?, password=?, email=? where id=?";
+        jdbcTemplate.update(connection, sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public User findById(final Long id) {
