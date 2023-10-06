ed75670128ac2b0438c5abd6649b6500d07999f2
kdkdhoho <hkim4410@naver.com>
2023-10-06 19:34:44 +0900

    feat: JdbcTemplate 코드 중복 제거

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -6,36 +6,22 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
-    private final DataSource dataSource;
+    private final PrepareStatementExecutor prepareStatementExecutor;
 
     public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.prepareStatementExecutor = new PrepareStatementExecutor(dataSource);
     }
 
     public void executeQuery(final String sql, final Object... params) {
-        try (final var conn = dataSource.getConnection();
-             final var pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-            setParameters(pstmt, params);
-            pstmt.executeUpdate();
-        } catch (final SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e.getMessage());
-        }
-    }
-
-    private void setParameters(final PreparedStatement pstmt, final Object[] params) throws SQLException {
-        for (int i = 0; i < params.length; i++) {
-            pstmt.setObject(i + 1, params[i]);
-        }
+        prepareStatementExecutor.execute(
+                PreparedStatement::executeUpdate,
+                sql,
+                params
+        );
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... params) {
@@ -47,20 +33,20 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... params) {
-        try (final var conn = dataSource.getConnection();
-             final var pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-            setParameters(pstmt, params);
-            final ResultSet resultSet = pstmt.executeQuery();
-            
-            final List<T> queryResult = new ArrayList<>();
-            while (resultSet.next()) {
-                queryResult.add(rowMapper.mapRow(resultSet));
-            }
-            return queryResult;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e.getMessage());
+        return prepareStatementExecutor.execute(
+                preparedStatement -> findQueryResults(rowMapper, preparedStatement),
+                sql,
+                params
+        );
+    }
+
+    private <T> List<T> findQueryResults(final RowMapper<T> rowMapper, final PreparedStatement pstmt) throws SQLException {
+        ResultSet resultSet = pstmt.executeQuery();
+
+        final List<T> results = new ArrayList<>();
+        while (resultSet.next()) {
+            results.add(rowMapper.mapRow(resultSet));
         }
+        return results;
     }
 }
