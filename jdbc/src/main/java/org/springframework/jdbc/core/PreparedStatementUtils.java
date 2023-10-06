2b4ec2fa6a2c8cf2dbbb4087d588f10102087ab0
amaran-th <songsy405@naver.com>
2023-10-06 09:43:13 +0900

    refactor: 불필요한 메서드 인라인화

--- jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementUtils.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementUtils.java
@@ -8,14 +8,10 @@ public class PreparedStatementUtils {
 
     public static PreparedStatement getPreparedStatement(final String sql, final Object[] obj, final Connection conn) throws SQLException {
         final PreparedStatement pstmt = conn.prepareStatement(sql);
-        setSqlParameter(obj, pstmt);
-        return pstmt;
-    }
-
-    private static void setSqlParameter(final Object[] obj, final PreparedStatement pstmt) throws SQLException {
         for (int i = 0; i < obj.length; i++) {
             pstmt.setObject(i + 1, obj[i]);
         }
+        return pstmt;
     }
 
     private PreparedStatementUtils() {
