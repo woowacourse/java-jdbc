70196124ebbd39e1d53d59b50a88c66947c88d49
iamjooon2 <iamjooon2@gmail.com>
2023-10-06 21:50:34 +0900

    [JDBC 구현하기 3단계] 가비(이준희) 미션 제출합니다 (#451)
    * 트랜잭션 학습 테스트
    
    * fix: UserHistoryDao가 jdbcTemplate을 사용하도록 변경
    
    * chore: UserServiceTest의 @Disabled 어노테이션 삭제
    
    * feat: jdbctemplate에 외부로부터 Connection을 주입받아 사용하는 update 메서드 생성
    
    * feat: dao 내 connection을 파라미터로 받는 메서드 생성
    
    * feat: UserService 내 트랜잭션 적용
    
    * test: JDBCTest 작성

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,11 +1,15 @@
 package com.techcourse.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Optional;
 
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import org.springframework.dao.DataAccessException;
 
 public class UserService {
 
@@ -26,10 +30,31 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final User user = findById(id).orElseThrow();
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        Connection connection = null;
+        try {
+            connection = DataSourceConfig.getInstance().getConnection();
+            connection.setAutoCommit(false);
+
+            final User user = findById(id).orElseThrow();
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+            connection.commit();
+        } catch (final SQLException e) {
+            try {
+                connection.rollback();
+            } catch (final SQLException rollbackException) {
+                throw new DataAccessException(rollbackException);
+            }
+            throw new DataAccessException(e);
+        } finally {
+            try {
+                connection.close();
+            } catch (final SQLException e) {
+                throw new DataAccessException(e);
+            }
+        }
     }
 
 }
