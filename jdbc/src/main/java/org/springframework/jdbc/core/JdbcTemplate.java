d27453099da4371660033d590a574f89af406e98
DEOKWOO KIM <the9rla@gmail.com>
2023-10-06 12:50:27 +0900

    [JDBC라이브러리 / 2단계] 로이(김덕우) 미션 제출합니다. (#439)
    * feat: 템플릿 콜백 패턴 적용
    
    * refactor: 메서드 분리
    
    * refactor: 중복 코드 제거를 위한 메서드 분리

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,11 +2,9 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeException;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -17,62 +15,50 @@ import java.util.NoSuchElementException;
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-    public static final int MAX_RESULT_SIZE = 1;
+    private static final int MAX_RESULT_SIZE = 1;
 
-    private final DataSource dataSource;
+    private QueryExecutorService queryExecutorService;
 
-    public JdbcTemplate(final DataSource dataSource) {
-        this.dataSource = dataSource;
+    public JdbcTemplate(DataSource dataSource) {
+        this.queryExecutorService = new QueryExecutorService(dataSource);
     }
 
     public int update(String sql, Object... args) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = createPreparedStatement(connection, sql, args)) {
-
-            log.debug("query : {}", sql);
-
-            return preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+        return queryExecutorService.execute(PreparedStatement::executeUpdate, sql, args);
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = createPreparedStatement(connection, sql, args);
-             ResultSet resultSet = preparedStatement.executeQuery()) {
+        return queryExecutorService.execute(preparedStatement -> {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return getResults(rowMapper, resultSet);
+        }, sql, args);
+    }
 
-            log.debug("query : {}", sql);
-
-            List<T> results = new ArrayList<>();
-            while (resultSet.next()) {
-                results.add(rowMapper.mapRow(resultSet));
-            }
-
-            return results;
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
+    private static <T> List<T> getResults(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
+        List<T> results = new ArrayList<>();
+        while (resultSet.next()) {
+            results.add(rowMapper.mapRow(resultSet));
         }
+        return results;
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
-        List<T> results = query(sql, rowMapper, args);
+        List<T> results = queryExecutorService.execute(preparedStatement -> {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return getResults(rowMapper, resultSet);
+        }, sql, args);
+
+        validate(results);
+
+        return results.iterator().next();
+    }
+
+    private static <T> void validate(List<T> results) {
         if (results.isEmpty()) {
             throw new NoSuchElementException("Data Row가 빈 값입니다.");
         }
         if (results.size() > MAX_RESULT_SIZE) {
             throw new IncorrectResultSizeException("적합한 ResultSize를 초과했습니다.");
         }
-
-        return results.iterator().next();
-    }
-
-    private PreparedStatement createPreparedStatement(Connection connection, String sql, Object[] args) throws SQLException {
-        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-        for (int i = 0; i < args.length; i++) {
-            preparedStatement.setObject(i + 1, args[i]);
-        }
-
-        return preparedStatement;
     }
 }
