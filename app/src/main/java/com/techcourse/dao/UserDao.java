c37cf5b9e537662145f322bda2dc6f567acb7a8d
eunbii0213 <eunbii0213@naver.com>
2023-10-06 11:03:59 +0900

    feat: UserDao 중복 코드 제거

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,12 +1,17 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.User;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
 
     private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<User> userMapper = (resultSet, rowNum) -> mapUser(resultSet);
 
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
@@ -25,26 +30,24 @@ public class UserDao {
 
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(sql, (resultSet, rowNum) -> new User(resultSet.getLong("id"),
-                resultSet.getString("account"),
-                resultSet.getString("password"),
-                resultSet.getString("email")));
+        return jdbcTemplate.query(sql, userMapper);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, (resultSet, rowNum) -> new User(resultSet.getLong("id"),
-                resultSet.getString("account"),
-                resultSet.getString("password"),
-                resultSet.getString("email")), id);
+        return jdbcTemplate.queryForObject(sql, userMapper, id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(sql, (resultSet, rowNum) -> new User(resultSet.getLong("id"),
+        return jdbcTemplate.queryForObject(sql, userMapper, account);
+    }
+
+    private User mapUser(ResultSet resultSet) throws SQLException {
+        return new User(resultSet.getLong("id"),
                 resultSet.getString("account"),
                 resultSet.getString("password"),
-                resultSet.getString("email")), account);
+                resultSet.getString("email"));
     }
 
 }
