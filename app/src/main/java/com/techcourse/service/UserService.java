ac126b8f2d6dd28d9aa8977fe0d1932216200614
woo-chang <clllickme@naver.com>
2023-10-07 16:04:56 +0900

    feat: TransactionTemplate 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,29 +4,35 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import org.springframework.transaction.support.TransactionTemplate;
 
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final TransactionTemplate transactionTemplate;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(UserDao userDao, UserHistoryDao userHistoryDao, TransactionTemplate transactionTemplate) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.transactionTemplate = transactionTemplate;
     }
 
-    public User findById(final long id) {
+    public User findById(long id) {
         return userDao.findById(id);
     }
 
-    public void insert(final User user) {
+    public void insert(User user) {
         userDao.insert(user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(long id, String newPassword, String createBy) {
+        transactionTemplate.execute(() -> {
+            var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
+            return null;
+        });
     }
 }
