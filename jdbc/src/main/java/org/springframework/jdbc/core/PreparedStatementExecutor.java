47c23f504c0dbfa465cb89d63e6bcd4414071a31
parkmuhyeun <pjhg410@gmail.com>
2023-10-07 18:01:50 +0900

    refactor: 예외 변경

--- jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementExecutor.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/PreparedStatementExecutor.java
@@ -2,6 +2,7 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -29,7 +30,7 @@ public class PreparedStatementExecutor {
             return preparedStatementProcessor.process(preparedStatement);
         } catch (final SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
     }
 
@@ -45,7 +46,7 @@ public class PreparedStatementExecutor {
             setParameters(preparedStatement, parameters);
             return preparedStatement;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
     }
 
