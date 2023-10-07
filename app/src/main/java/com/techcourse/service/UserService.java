063c7d78d2cd2e475669a6c6790869f57525378f
yeonkkk <tjddus0302@gmail.com>
2023-10-07 20:16:47 +0900

    refactor: password 변경 로직에 트랜잭션 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,15 @@
 package com.techcourse.service;
 
+import static java.util.Objects.requireNonNull;
+
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class UserService {
 
@@ -19,14 +25,32 @@ public class UserService {
         return userDao.findById(id);
     }
 
+    public User findById(final Connection connection, final long id) {
+        return userDao.findById(connection, id);
+    }
+
+
     public void insert(final User user) {
         userDao.insert(user);
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        final DataSource dataSource = DataSourceConfig.getInstance();
+        try (Connection connection = requireNonNull(dataSource).getConnection()) {
+            try {
+                connection.setAutoCommit(false);
+
+                final var user = findById(connection, id);
+                user.changePassword(newPassword);
+                userDao.update(connection, user);
+                userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- jdbc/src/main/java/org/springframework/jdbc/core/BaseJdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/BaseJdbcTemplate.java
@@ -20,9 +20,23 @@ public class BaseJdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public  <T> T execute(final String sql, final PreparedStatementAction<T> action) {
+    public <T> T execute(final String sql,
+                         final PreparedStatementAction<T> action) {
         try (
                 final Connection connection = requireNonNull(dataSource).getConnection();
+                final PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            return action.execute(preparedStatement);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> T execute(final Connection connection,
+                         final String sql,
+                         final PreparedStatementAction<T> action) {
+        try (
                 final PreparedStatement preparedStatement = connection.prepareStatement(sql)
         ) {
             return action.execute(preparedStatement);
