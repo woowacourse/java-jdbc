9f08af0634fc87ee9e19ec24958b4603d64b4d61
odo27 <84677292+odo27@users.noreply.github.com>
2023-10-07 16:43:06 +0900

    [JDBC 라이브러리 구현하기 - 2단계] 오도(문민혁) 미션 제출합니다. (#384)
    * refactor: 공통된 try-with-resources 제거
    
    * refactor: mapTable -> mapAllRows 변경
    
    * feat: 값이 2개 이상일 때 예외 처리

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,68 +1,71 @@
 package com.techcourse.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 class UserDaoTest {
 
     private UserDao userDao;
+    private User user;
 
     @BeforeEach
     void setup() {
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
-
-        userDao = new UserDao(DataSourceConfig.getInstance());
-        final var user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.insert(user);
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
+        jdbcTemplate.update("delete from users");
+        userDao = new UserDao(jdbcTemplate);
+        userDao.insert(new User("gugu", "password", "hkkang@woowahan.com"));
+        user = userDao.findByAccount("gugu");
     }
 
     @Test
     void findAll() {
-        final var users = userDao.findAll();
+        List<User> users = userDao.findAll();
 
         assertThat(users).isNotEmpty();
     }
 
     @Test
     void findById() {
-        final var user = userDao.findById(1L);
+        User findUser = userDao.findById(user.getId());
 
-        assertThat(user.getAccount()).isEqualTo("gugu");
+        assertThat(findUser.getAccount()).isEqualTo("gugu");
     }
 
     @Test
     void findByAccount() {
-        final var account = "gugu";
-        final var user = userDao.findByAccount(account);
+        String account = "gugu";
+        User user = userDao.findByAccount(account);
 
         assertThat(user.getAccount()).isEqualTo(account);
     }
 
     @Test
     void insert() {
-        final var account = "insert-gugu";
-        final var user = new User(account, "password", "hkkang@woowahan.com");
+        String account = "insert-gugu";
+        User user = new User(account, "password", "hkkang@woowahan.com");
         userDao.insert(user);
 
-        final var actual = userDao.findById(2L);
+        User actual = userDao.findByAccount(account);
 
         assertThat(actual.getAccount()).isEqualTo(account);
     }
 
     @Test
     void update() {
-        final var newPassword = "password99";
-        final var user = userDao.findById(1L);
-        user.changePassword(newPassword);
+        String newPassword = "password99";
+        User findUser = userDao.findById(user.getId());
+        findUser.changePassword(newPassword);
 
-        userDao.update(user);
-
-        final var actual = userDao.findById(1L);
+        userDao.update(findUser);
+        User actual = userDao.findById(user.getId());
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -21,17 +21,15 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... values) {
+        return execute(sql, preparedStatement -> mapAllRows(preparedStatement, rowMapper), values);
+    }
+
+    private <T> T execute(String sql, PreparedStatementExecutor<T> preparedStatementExecutor, Object... values) {
         try (
                 Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = prepareStatement(connection, sql, values);
-                ResultSet resultSet = preparedStatement.executeQuery()
+                PreparedStatement preparedStatement = prepareStatement(connection, sql, values)
         ) {
-            List<T> results = new ArrayList<>();
-            while (resultSet.next()) {
-                T result = rowMapper.mapRow(resultSet);
-                results.add(result);
-            }
-            return results;
+            return preparedStatementExecutor.execute(preparedStatement);
         } catch (SQLException e) {
             log.error(e.getMessage());
             throw new RuntimeException(e);
@@ -50,31 +48,32 @@ public class JdbcTemplate {
         return preparedStatement;
     }
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... values) {
-        try (
-                Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = prepareStatement(connection, sql, values);
-                ResultSet resultSet = preparedStatement.executeQuery()
-        ) {
-            if (resultSet.next()) {
-                return rowMapper.mapRow(resultSet);
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
+    private <T> List<T> mapAllRows(PreparedStatement preparedStatement, RowMapper<T> rowMapper) throws SQLException {
+        ResultSet resultSet = preparedStatement.executeQuery();
+        List<T> results = new ArrayList<>();
+        while (resultSet.next()) {
+            T result = rowMapper.mapRow(resultSet);
+            results.add(result);
         }
+        return results;
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... values) {
+        return execute(sql, preparedStatement -> mapRow(preparedStatement, rowMapper), values);
+    }
+
+    private <T> T mapRow(PreparedStatement preparedStatement, RowMapper<T> rowMapper) throws SQLException {
+        List<T> allRows = mapAllRows(preparedStatement, rowMapper);
+        if (allRows.isEmpty()) {
+            return null;
+        }
+        if (allRows.size() == 1) {
+            return allRows.get(0);
+        }
+        throw new RuntimeException("결과가 2개 이상입니다.");
     }
 
     public void update(String sql, Object... values) {
-        try (
-                Connection connection = dataSource.getConnection();
-                PreparedStatement preparedStatement = prepareStatement(connection, sql, values)
-        ) {
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage());
-            throw new RuntimeException(e);
-        }
+        execute(sql, PreparedStatement::executeUpdate, values);
     }
 }
