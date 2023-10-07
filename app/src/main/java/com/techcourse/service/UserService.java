a197be329447d8e19da248b21d27b147b83558a1
odo27 <odo27@naver.com>
2023-10-07 22:08:58 +0900

    feat: 비밀번호 변경 시 같은 connection을 사용하도록 구현

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,13 @@
 package com.techcourse.service;
 
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class UserService {
 
@@ -23,10 +27,35 @@ public class UserService {
         userDao.insert(user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(long id, String newPassword, String createBy) {
+        Connection connection = getConnection();
+        try {
+            connection.setAutoCommit(false);
+            User user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            connection.commit();
+        } catch (SQLException e) {
+            rollback(connection);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Connection getConnection() {
+        try {
+            DataSource dataSource = DataSourceConfig.getInstance();
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void rollback(Connection connection) {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
