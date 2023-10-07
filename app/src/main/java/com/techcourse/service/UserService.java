36bd6b2f3e1b586df243b3bfc490c7494b9b00ad
Jaeyoon Lee <63213487+sosow0212@users.noreply.github.com>
2023-10-07 21:31:17 +0900

    [JDBC 라이브러리 구현하기 - 3단계] 제이(이재윤) 미션 제출합니다. (#450)
    * refactor: 템플릿 콜백 패턴 적용
    
    * feat: 트랜잭션 적용
    
    * refactor: service 구조 개선

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,15 @@
 package com.techcourse.service;
 
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import org.springframework.dao.DataAccessException;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserService {
 
@@ -25,9 +31,33 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        DataSource dataSource = DataSourceConfig.getInstance();
+
+        try (final Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(false);
+
+            User user = findById(id);
+
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+            commit(connection);
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void commit(final Connection connection) throws SQLException {
+        try {
+            connection.commit();
+        } catch (final SQLException e) {
+            rollback(connection, e);
+        }
+    }
+
+    private void rollback(final Connection connection, final SQLException e) throws SQLException {
+        connection.rollback();
+        throw new DataAccessException(e);
     }
 }
