801b941f43f7269991a3e610d1fde177a8240430
yeonkkk <tjddus0302@gmail.com>
2023-10-07 17:57:25 +0900

    test: transaction 학습 테스트

--- study/src/test/java/transaction/stage1/Stage1Test.java
+++ study/src/test/java/transaction/stage1/Stage1Test.java
@@ -34,7 +34,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  *   Read phenomena | Dirty reads | Non-repeatable reads | Phantom reads
  * Isolation level  |             |                      |
  * -----------------|-------------|----------------------|--------------
- * Read Uncommitted |             |                      |
+ * Read Uncommitted |            |                      |
  * Read Committed   |             |                      |
  * Repeatable Read  |             |                      |
  * Serializable     |             |                      |
@@ -58,10 +58,10 @@ class Stage1Test {
      *   Read phenomena | Dirty reads
      * Isolation level  |
      * -----------------|-------------
-     * Read Uncommitted |
-     * Read Committed   |
-     * Repeatable Read  |
-     * Serializable     |
+     * Read Uncommitted |      +
+     * Read Committed   |      -
+     * Repeatable Read  |      -
+     * Serializable     |      -
      */
     @Test
     void dirtyReading() throws SQLException {
@@ -82,6 +82,10 @@ class Stage1Test {
 
             // 적절한 격리 레벨을 찾는다.
             final int isolationLevel = Connection.TRANSACTION_NONE;
+//            final int isolationLevel = Connection.TRANSACTION_READ_UNCOMMITTED;
+//            final int isolationLevel = Connection.TRANSACTION_READ_COMMITTED;
+//            final int isolationLevel = Connection.TRANSACTION_REPEATABLE_READ;
+//            final int isolationLevel = Connection.TRANSACTION_SERIALIZABLE;
 
             // 트랜잭션 격리 레벨을 설정한다.
             subConnection.setTransactionIsolation(isolationLevel);
@@ -111,10 +115,10 @@ class Stage1Test {
      *   Read phenomena | Non-repeatable reads
      * Isolation level  |
      * -----------------|---------------------
-     * Read Uncommitted |
-     * Read Committed   |
-     * Repeatable Read  |
-     * Serializable     |
+     * Read Uncommitted |           +
+     * Read Committed   |           +
+     * Repeatable Read  |           -
+     * Serializable     |           -
      */
     @Test
     void noneRepeatable() throws SQLException {
@@ -131,6 +135,10 @@ class Stage1Test {
 
         // 적절한 격리 레벨을 찾는다.
         final int isolationLevel = Connection.TRANSACTION_NONE;
+//            final int isolationLevel = Connection.TRANSACTION_READ_UNCOMMITTED;
+//            final int isolationLevel = Connection.TRANSACTION_READ_COMMITTED;
+//            final int isolationLevel = Connection.TRANSACTION_REPEATABLE_READ;
+//            final int isolationLevel = Connection.TRANSACTION_SERIALIZABLE;
 
         // 트랜잭션 격리 레벨을 설정한다.
         connection.setTransactionIsolation(isolationLevel);
@@ -173,17 +181,18 @@ class Stage1Test {
      *   Read phenomena | Phantom reads
      * Isolation level  |
      * -----------------|--------------
-     * Read Uncommitted |
-     * Read Committed   |
-     * Repeatable Read  |
-     * Serializable     |
+     * Read Uncommitted |      +
+     * Read Committed   |      +
+     * Repeatable Read  |      +
+     * Serializable     |      -
      */
     @Test
     void phantomReading() throws SQLException {
 
         // testcontainer로 docker를 실행해서 mysql에 연결한다.
         final var mysql = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.30"))
-                .withLogConsumer(new Slf4jLogConsumer(log));
+                .withLogConsumer(new Slf4jLogConsumer(log))
+                .withUrlParam("allowMultiQueries", "true");
         mysql.start();
         setUp(createMySQLDataSource(mysql));
 
@@ -197,7 +206,11 @@ class Stage1Test {
         connection.setAutoCommit(false);
 
         // 적절한 격리 레벨을 찾는다.
-        final int isolationLevel = Connection.TRANSACTION_NONE;
+//        final int isolationLevel = Connection.TRANSACTION_NONE;
+//                    final int isolationLevel = Connection.TRANSACTION_READ_UNCOMMITTED;
+//                    final int isolationLevel = Connection.TRANSACTION_READ_COMMITTED;
+//                    final int isolationLevel = Connection.TRANSACTION_REPEATABLE_READ;
+                    final int isolationLevel = Connection.TRANSACTION_SERIALIZABLE;
 
         // 트랜잭션 격리 레벨을 설정한다.
         connection.setTransactionIsolation(isolationLevel);
