0a323a473bade8bdc7c2a0d2cd7ce5eaa056e97e
hong-sile <gurwns9325@naver.com>
2023-10-07 19:52:15 +0900

    feat: connection을 생성, 롤백, 커밋, setAutocomiit(false)를 해주는 기능 추가

--- jdbc/src/main/java/org/springframework/transaction/support/ConnectionManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/ConnectionManager.java
@@ -1,21 +1,30 @@
 package org.springframework.transaction.support;
 
+import static java.lang.Boolean.TRUE;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.springframework.transaction.exception.ConnectionManagerException;
 
 public class ConnectionManager {
 
     private static final ConnectionManager INSTANCE = new ConnectionManager();
 
     private final ThreadLocal<Connection> connectionResource;
+    private final ThreadLocal<Boolean> transactionStarted;
 
     private ConnectionManager() {
         connectionResource = new ThreadLocal<>();
+        transactionStarted = new ThreadLocal<>();
     }
 
-    public static ConnectionManager getInstance(){
+    public static ConnectionManager getInstance() {
         return INSTANCE;
+    }
+
+    public void beginTransaction() {
+        transactionStarted.set(TRUE);
     }
 
     public Connection getConnection(final DataSource dataSource) throws SQLException {
@@ -24,10 +33,44 @@ public class ConnectionManager {
             connectionResource.set(connection);
         }
 
-        return connectionResource.get();
+        final Connection connection = connectionResource.get();
+
+        if (transactionStarted.get() != null && transactionStarted.get()) {
+            connection.setAutoCommit(false);
+        }
+
+        return connection;
     }
 
     private boolean isNotAlreadyStarted() {
         return connectionResource.get() == null;
+    }
+
+    public void close() {
+        final Connection connection = connectionResource.get();
+
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new ConnectionManagerException(e);
+        } finally {
+            connectionResource.remove();
+        }
+    }
+
+    public void rollback() {
+        try {
+            connectionResource.get().rollback();
+        } catch (final SQLException e) {
+            throw new ConnectionManagerException(e);
+        }
+    }
+
+    public void commit() {
+        try {
+            connectionResource.get().commit();
+        } catch (final SQLException e) {
+            throw new ConnectionManagerException(e);
+        }
     }
 }
--- jdbc/src/test/java/org/springframework/transaction/support/ConnectionManagerTest.java
+++ jdbc/src/test/java/org/springframework/transaction/support/ConnectionManagerTest.java
@@ -1,15 +1,33 @@
 package org.springframework.transaction.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.core.test_supporter.DataSourceConfig;
 
 class ConnectionManagerTest {
+
+    private ConnectionManager connectionManager;
+    private DataSource datasource;
+    private Connection connection;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        connectionManager = ConnectionManager.getInstance();
+        datasource = mock(DataSource.class);
+        connection = spy(Connection.class);
+        when(datasource.getConnection()).thenReturn(connection);
+    }
 
     @Nested
     @DisplayName("datasource로 connection을 맞는다.")
@@ -18,9 +36,7 @@ class ConnectionManagerTest {
         @Test
         @DisplayName("스레드에서 connection을 생성한 적이 없다면, tx를 생성한다.")
         void getNewConnection() throws SQLException {
-            final ConnectionManager txManager = ConnectionManager.getInstance();
-
-            final Connection connection = txManager.getConnection(DataSourceConfig.getInstance());
+            final Connection connection = connectionManager.getConnection(datasource);
 
             assertThat(connection)
                 .isNotNull();
@@ -29,15 +45,52 @@ class ConnectionManagerTest {
         @Test
         @DisplayName("스레드에서 connection을 생성한 적이 있다면, 기존 tx를 반환한다.")
         void getOldConnection() throws SQLException {
-            final ConnectionManager txManager = ConnectionManager.getInstance();
-            final Connection oldTransaction = txManager.getConnection(
-                DataSourceConfig.getInstance());
+            final Connection oldTransaction = connectionManager.getConnection(datasource);
 
-            final Connection newTransaction = txManager.getConnection(
-                DataSourceConfig.getInstance());
+            final Connection newTransaction = connectionManager.getConnection(datasource);
 
             assertThat(oldTransaction)
                 .isEqualTo(newTransaction);
         }
+    }
+
+    @Test
+    @DisplayName("beginTransaction을 호출하고, getTransaction을 호출하면,setAutocommit을 false로 만든다.")
+    void beginTransaction() throws SQLException {
+        connectionManager.beginTransaction();
+
+        final Connection conn = connectionManager.getConnection(datasource);
+
+        verify(conn, times(1)).setAutoCommit(false);
+    }
+
+    @Test
+    @DisplayName("connection을 close한다")
+    void closeConnection() throws SQLException {
+        final Connection conn = connectionManager.getConnection(datasource);
+
+        connectionManager.close();
+
+        verify(conn, times(1)).close();
+    }
+
+    @Test
+    @DisplayName("connection을 commit한다")
+    void commit() throws SQLException {
+        final Connection conn = connectionManager.getConnection(datasource);
+
+        connectionManager.commit();
+
+        verify(conn, times(1)).commit();
+    }
+
+    @Test
+    @DisplayName("connection을 rollback한다")
+    void rollback() throws SQLException {
+        final Connection conn = connectionManager.getConnection(datasource);
+
+        connectionManager.rollback();
+
+        verify(conn, times(1)).rollback();
     }
 }
