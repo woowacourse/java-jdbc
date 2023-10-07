8b881a71940fd9b1d1620e5f1332fa063d591d8b
ezzanzzan <cjsdmswjd821@naver.com>
2023-10-07 18:28:43 +0900

    refactor: 메서드 네이밍 변경

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,6 +2,7 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 
 import javax.annotation.Nullable;
@@ -22,20 +23,20 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public int update(final String sql, final Object... args) {
+    public int update(final String sql, final Object... args) throws DataAccessException {
         try (final Connection connection = dataSource.getConnection();
-             final PreparedStatement preparedStatement = newArgPreparedStatementSetter(connection, sql, args)) {
+             final PreparedStatement preparedStatement = createPreparedStatementSetter(connection, sql, args)) {
             log.debug("query : {}", sql);
             return preparedStatement.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
         try (final Connection connection = dataSource.getConnection();
-             final PreparedStatement preparedStatement = newArgPreparedStatementSetter(connection, sql, args);
+             final PreparedStatement preparedStatement = createPreparedStatementSetter(connection, sql, args);
              final ResultSet resultSet = preparedStatement.executeQuery()) {
             log.debug("query : {}", sql);
             final List<T> list = new ArrayList<>();
@@ -58,7 +59,7 @@ public class JdbcTemplate {
         return results.iterator().next();
     }
 
-    private PreparedStatement newArgPreparedStatementSetter(final Connection connection, final String sql, final Object[] args) throws SQLException {
+    private PreparedStatement createPreparedStatementSetter(final Connection connection, final String sql, final Object[] args) throws SQLException {
         final PreparedStatement preparedStatement = connection.prepareStatement(sql);
         for (int i = 0; i < args.length; i++) {
             preparedStatement.setObject(i + 1, args[i]);
