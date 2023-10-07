a5fd0e48d53b030dabb9128a58ed927b2560171d
Eun-chan Cho <echo.backend@gmail.com>
2023-10-07 15:43:38 +0900

    [JDBC 라이브러리 구현하기 - 2단계] 에코(조은찬) 미션 제출합니다  (#440)
    * refactor: PreparedStatementSetter를 사용해 app 영역에서 값 설정하도록 변경
    
    * test: JdbcTemplateTest 추가
    
    * feat: 가변인자를 파라미터로 가지는 query 메서드 추가
    
    * refactor: final 추가

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -2,6 +2,7 @@ package org.springframework.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -21,55 +22,70 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void executeQuery(final String sql, SQLParameters parameters) throws SQLException {
+    public void update(final String sql, final PreparedStatementSetter preparedStatementSetter) throws DataAccessException {
         try (Connection conn = dataSource.getConnection()) {
             try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                log.debug("query : {}", sql);
-                parameters.getParameters().forEach(p -> {
-                    try {
-                        pstmt.setObject(parameters.getParameters().indexOf(p) + 1, p);
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+                preparedStatementSetter.setValues(pstmt);
                 pstmt.executeUpdate();
             }
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
         }
     }
 
-    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, SQLParameters parameters) throws SQLException {
-        try (final Connection connection = dataSource.getConnection()) {
-            try (final PreparedStatement pstmt = connection.prepareStatement(sql)) {
-                parameters.getParameters().forEach(p -> {
-                    try {
-                        pstmt.setObject(parameters.getParameters().indexOf(p) + 1, p);
-                    } catch (SQLException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-                try (final ResultSet rs = pstmt.executeQuery()) {
-                    log.debug("query : {}", sql);
-                    if (rs.next()) {
-                        return rowMapper.mapRow(rs, rs.getRow());
-                    }
-                    return null;
+    public <T> T query(final String sql, final RowMapper<T> rowMapper, final PreparedStatementSetter preparedStatementSetter) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            try (final ResultSet rs = executeQuery(preparedStatementSetter, pstmt)) {
+                log.debug("query : {}", sql);
+                if (rs.next()) {
+                    return rowMapper.mapRow(rs, rs.getRow());
                 }
+                throw new DataAccessException("Empty Result");
             }
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
         }
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) throws SQLException {
-        try (final Connection connection = dataSource.getConnection()) {
-            try (final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+    private ResultSet executeQuery(final PreparedStatementSetter preparedStatementSetter, final PreparedStatement preparedStatement) throws SQLException {
+        preparedStatementSetter.setValues(preparedStatement);
+        return preparedStatement.executeQuery();
+    }
+
+    public <T> T query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            try (final ResultSet rs = executeQuery(args, pstmt)) {
+                if (rs.next()) {
+                    return rowMapper.mapRow(rs, rs.getRow());
+                }
+                throw new DataAccessException("Empty Result");
+            }
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private ResultSet executeQuery(final Object[] args, final PreparedStatement pstmt) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+        return pstmt.executeQuery();
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) throws DataAccessException {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            try (final ResultSet rs = pstmt.executeQuery()) {
                 List<T> result = new ArrayList<>();
-                try (final ResultSet rs = pstmt.executeQuery()) {
-                    log.debug("query : {}", sql);
-                    while (rs.next()) {
-                        result.add(rowMapper.mapRow(rs, rs.getRow()));
-                    }
+                while (rs.next()) {
+                    result.add(rowMapper.mapRow(rs, rs.getRow()));
                 }
                 return result;
             }
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
         }
     }
 }
--- jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
+++ jdbc/src/test/java/nextstep/jdbc/JdbcTemplateTest.java
@@ -1,5 +1,123 @@
 package nextstep.jdbc;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
 class JdbcTemplateTest {
 
+    private final JdbcTemplate jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
+
+
+    @BeforeEach
+    public void setUp() {
+        final DataSource instance = DataSourceConfig.getInstance();
+        try (final Connection connection = instance.getConnection()) {
+            final Statement statement = connection.createStatement();
+            statement.execute("drop table if exists users");
+            statement.execute("create table if not exists users (id bigint auto_increment, account varchar(255), password varchar(255), email varchar(255), primary key (id))");
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+
+    }
+
+    @Test
+    void update() {
+        //given
+        //when
+        String sql = "insert into users (account, password, email) values (?, ?, ?)";
+        jdbcTemplate.update(sql, preparedStatement -> {
+            preparedStatement.setString(1, "account");
+            preparedStatement.setString(2, "password");
+            preparedStatement.setString(3, "email");
+        });
+
+        //then
+        RowMapper<Map<String, Object>> rowMapper = (resultSet, rowNum) -> Map.of(
+                "id", resultSet.getLong("id"),
+                "account", resultSet.getString("account"),
+                "password", resultSet.getString("password"),
+                "email", resultSet.getString("email")
+        );
+        String selectSql = "select * from users";
+        final List<Map<String, Object>> result = jdbcTemplate.query(selectSql, rowMapper);
+        assertSoftly(softly -> {
+            softly.assertThat(result).hasSize(1);
+            softly.assertThat(result.get(0).get("account")).isEqualTo("account");
+            softly.assertThat(result.get(0).get("password")).isEqualTo("password");
+            softly.assertThat(result.get(0).get("email")).isEqualTo("email");
+        });
+    }
+
+    @Test
+    public void queryList() {
+        //given
+        String sql = "insert into users (account, password, email) values (?, ?, ?)";
+        jdbcTemplate.update(sql, preparedStatement -> {
+            preparedStatement.setString(1, "account");
+            preparedStatement.setString(2, "password");
+            preparedStatement.setString(3, "email");
+        });
+        jdbcTemplate.update(sql, preparedStatement -> {
+            preparedStatement.setString(1, "account2");
+            preparedStatement.setString(2, "password2");
+            preparedStatement.setString(3, "email2");
+        });
+
+        //when
+        String selectSql = "select * from users";
+        final List<Map<String, Object>> result = jdbcTemplate.query(selectSql, (resultSet, rowNum) -> Map.of(
+                "id", resultSet.getLong("id"),
+                "account", resultSet.getString("account"),
+                "password", resultSet.getString("password"),
+                "email", resultSet.getString("email")
+        ));
+
+        //then
+        assertSoftly(softly -> {
+            softly.assertThat(result).hasSize(2);
+        });
+    }
+
+    @Test
+    @DisplayName("가변인자를 이용한 쿼리 테스트")
+    void vargsQuery() {
+        //given
+        String sql = "insert into users (account, password, email) values (?, ?, ?)";
+        jdbcTemplate.update(sql, preparedStatement -> {
+            preparedStatement.setString(1, "account");
+            preparedStatement.setString(2, "password");
+            preparedStatement.setString(3, "email");
+        });
+
+        //when
+        String selectSql = "select * from users where account = ?";
+        final Map<String, Object> result = jdbcTemplate.query(selectSql, (resultSet, rowNum) -> Map.of(
+                "id", resultSet.getLong("id"),
+                "account", resultSet.getString("account"),
+                "password", resultSet.getString("password"),
+                "email", resultSet.getString("email")
+        ), "account");
+
+        //then
+        assertSoftly(softly -> {
+            softly.assertThat(result.get("account")).isEqualTo("account");
+            softly.assertThat(result.get("password")).isEqualTo("password");
+            softly.assertThat(result.get("email")).isEqualTo("email");
+        });
+
+    }
 }
