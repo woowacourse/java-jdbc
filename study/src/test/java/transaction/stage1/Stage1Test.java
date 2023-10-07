40f84dcb502f802b47f480fff29e1debe3c614fa
Ohjintaek <jt950930@gmail.com>
2023-10-07 19:18:09 +0900

    chore: windows에서 testcontainer 작동하도록 수정

--- study/src/test/java/transaction/stage1/Stage1Test.java
+++ study/src/test/java/transaction/stage1/Stage1Test.java
@@ -184,7 +184,7 @@ class Stage1Test {
         // testcontainer로 docker를 실행해서 mysql에 연결한다.
         final var mysql = new MySQLContainer<>(DockerImageName.parse("mysql:8.0.30"))
                 .withLogConsumer(new Slf4jLogConsumer(log));
-        //mysql.withUrlParam("allowMultiQueries", "true");
+        mysql.withUrlParam("allowMultiQueries", "true");
         mysql.start();
         setUp(createMySQLDataSource(mysql));
 
