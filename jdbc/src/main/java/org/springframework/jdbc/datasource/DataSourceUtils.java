953d7d8a36da216283647344a0b3b2a32306215e
kokodak <kokodakadokok@gmail.com>
2023-10-09 02:39:55 +0900

    feat: DataSourceUtils 내부 로직 구현
    - DataSourceUtils 는 트랜잭션 동기화 기능을 사용하여, 같은 Connection 객체를 재사용할 수 있도록 한다.

--- jdbc/src/main/java/org/springframework/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/org/springframework/jdbc/datasource/DataSourceUtils.java
@@ -1,35 +1,46 @@
 package org.springframework.jdbc.datasource;
 
-import org.springframework.jdbc.CannotGetJdbcConnectionException;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.transaction.support.TransactionManager;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-// 4단계 미션에서 사용할 것
 public abstract class DataSourceUtils {
 
-    private DataSourceUtils() {}
+    private DataSourceUtils() {
+    }
 
-    public static Connection getConnection(DataSource dataSource) throws CannotGetJdbcConnectionException {
+    public static Connection getConnection(final DataSource dataSource) throws CannotGetJdbcConnectionException {
         Connection connection = TransactionSynchronizationManager.getResource(dataSource);
         if (connection != null) {
             return connection;
         }
+        return createConnection(dataSource);
+    }
 
+    private static Connection createConnection(final DataSource dataSource) {
         try {
-            connection = dataSource.getConnection();
-            TransactionSynchronizationManager.bindResource(dataSource, connection);
+            final Connection connection = dataSource.getConnection();
+            if (TransactionManager.isTxBegin(dataSource)) {
+                connection.setAutoCommit(false);
+                TransactionSynchronizationManager.bindResource(dataSource, connection);
+            }
             return connection;
+
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to obtain JDBC Connection", ex);
         }
     }
 
-    public static void releaseConnection(Connection connection, DataSource dataSource) {
+    public static void releaseConnection(final Connection connection, final DataSource dataSource) {
+        if (TransactionManager.isTxBegin(dataSource)) {
+            return;
+        }
         try {
             connection.close();
+
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
         }
