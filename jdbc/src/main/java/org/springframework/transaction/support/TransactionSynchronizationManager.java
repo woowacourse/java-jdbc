0890fba02c6805373370ced0f46144f24119fbf4
kokodak <kokodakadokok@gmail.com>
2023-10-09 02:33:07 +0900

    feat: TransactionSynchronizationManager 내부 로직 구현

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,26 @@
 package org.springframework.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
-    private TransactionSynchronizationManager() {}
-
-    public static Connection getResource(DataSource key) {
-        return null;
+    private TransactionSynchronizationManager() {
     }
 
-    public static void bindResource(DataSource key, Connection value) {
+    public static Connection getResource(final DataSource key) {
+        return resources.get().get(key);
     }
 
-    public static Connection unbindResource(DataSource key) {
-        return null;
+    public static void bindResource(final DataSource key, final Connection value) {
+        resources.get().put(key, value);
+    }
+
+    public static Connection unbindResource(final DataSource key) {
+        return resources.get().remove(key);
     }
 }
