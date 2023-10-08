35985c21d02f6a01f0ee6fe39f310aa789f947d2
최준호 <junho5336@gmail.com>
2023-10-08 20:48:31 +0900

    refactor: UserService 추상화

--- jdbc/src/main/java/org/springframework/transaction/TransactionExecutor.java
+++ jdbc/src/main/java/org/springframework/transaction/TransactionExecutor.java
@@ -3,17 +3,37 @@ package org.springframework.transaction;
 import org.springframework.dao.DataAccessException;
 
 import java.sql.SQLException;
+import java.util.function.Supplier;
 
 public class TransactionExecutor {
 
-    private TransactionExecutor() {
+    private final TransactionManager transactionManager;
+
+    public TransactionExecutor(TransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
     }
 
-    public static void execute(final TransactionManager transactionManager, final Runnable runnable) {
+    public void execute(final Runnable runnable) {
         try {
             transactionManager.begin();
             runnable.run();
             transactionManager.commit();
+        } catch (Exception e) {
+            try {
+                transactionManager.rollback();
+            } catch (SQLException ex) {
+                throw new DataAccessException(ex);
+            }
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <R> R executeWithResult(final Supplier<R> supplier) {
+        try {
+            transactionManager.begin();
+            R result = supplier.get();
+            transactionManager.commit();
+            return result;
         } catch (Exception e) {
             try {
                 transactionManager.rollback();
