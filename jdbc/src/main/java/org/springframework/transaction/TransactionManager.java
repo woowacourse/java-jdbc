8637110b58ed5efaa55ef65c1902ca1f628dfae2
iamjooon2 <iamjooon2@gmail.com>
2023-10-08 18:44:15 +0900

    feat: TransactionManager 구현

---
+++ jdbc/src/main/java/org/springframework/transaction/TransactionManager.java
@@ -0,0 +1,43 @@
+package org.springframework.transaction;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionExecutor;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class TransactionManager {
+
+    private final DataSource dataSource;
+
+    public TransactionManager(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public <T> T run(final TransactionExecutor<T> transactionExecutor) {
+        Connection connection = null;
+        try {
+            connection = DataSourceUtils.getConnection(dataSource);
+            connection.setAutoCommit(false);
+
+            final T action = transactionExecutor.action();
+
+            connection.commit();
+            return action;
+        } catch (final SQLException e) {
+            try {
+                connection.rollback();
+            } catch (final SQLException rollbackException) {
+                throw new DataAccessException(rollbackException);
+            }
+            throw new DataAccessException(e);
+        } finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+            TransactionSynchronizationManager.unbindResource(dataSource);
+        }
+    }
+
+}
