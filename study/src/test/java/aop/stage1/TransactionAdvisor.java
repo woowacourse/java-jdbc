fb6fe50f94c9f6705511e8eff5c3d510540ff49d
hyena <goatsfish@gmail.com>
2023-10-08 19:50:51 +0900

    test: Stage1 Pointcut, Advice, Advisor 구현

--- study/src/test/java/aop/stage1/TransactionAdvisor.java
+++ study/src/test/java/aop/stage1/TransactionAdvisor.java
@@ -10,18 +10,30 @@ import org.springframework.aop.PointcutAdvisor;
  */
 public class TransactionAdvisor implements PointcutAdvisor {
 
+    private final Pointcut pointcut;
+    private final Advice advice;
+
+    public TransactionAdvisor(final Pointcut pointcut, final Advice advice) {
+        this.pointcut = pointcut;
+        this.advice = advice;
+    }
+
     @Override
     public Pointcut getPointcut() {
-        return null;
+        return pointcut;
     }
 
     @Override
     public Advice getAdvice() {
-        return null;
+        return advice;
     }
 
+    /**
+     * 현재 프레임워크에서 사용되지는 않는다.
+     * @return true
+     */
     @Override
     public boolean isPerInstance() {
-        return false;
+        return true;
     }
 }
