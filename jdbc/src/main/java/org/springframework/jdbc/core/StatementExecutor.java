ddf6c5a4c83f548d1edf123dd53601f419354e30
amaran-th <songsy405@naver.com>
2023-10-08 15:23:24 +0900

    refactor: @FunctionalInterface 어노테이션 붙이기

--- jdbc/src/main/java/org/springframework/jdbc/core/StatementExecutor.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/StatementExecutor.java
@@ -3,6 +3,7 @@ package org.springframework.jdbc.core;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface StatementExecutor<T> {
     T execute(final ResultSet rs) throws SQLException;
 
