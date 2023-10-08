42debe92df4f1a0936365eccf22e094335ace229
waterricecake <fruturum@nate.com>
2023-10-08 16:39:13 +0900

    refactor: JdbcTemplate transactional 적용

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -23,69 +23,66 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(final String sql, RowMapper<T> rowMapper, Object... parameters) {
-        try (
-                final Connection connection = dataSource.getConnection();
-                final PreparedStatement preparedStatement = setValues(sql, connection, parameters);
-                final ResultSet resultSet = executeQuery(PreparedStatement::executeQuery, preparedStatement)
-        ) {
-            final List<T> users = new ArrayList<>();
+        return executeQuery(sql,
+                preparedStatement -> {
+                    final ResultSet resultSet = preparedStatement.executeQuery();
+                    final List<T> users = new ArrayList<>();
 
-            while (resultSet.next()) {
-                T result = rowMapper.rowMap(resultSet);
-                users.add(result);
-            }
-            return users;
-        } catch (SQLException e) {
-            throw new JdbcException(e);
-        }
+                    while (resultSet.next()) {
+                        users.add(rowMapper.rowMap(resultSet));
+                    }
+                    return users;
+                },
+                parameters
+        );
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        try (
-                final Connection connection = dataSource.getConnection();
-                final PreparedStatement preparedStatement = setValues(sql, connection, parameters);
-                final ResultSet resultSet = executeQuery(PreparedStatement::executeQuery, preparedStatement)
-        ) {
-            if (resultSet.next()) {
-                return rowMapper.rowMap(resultSet);
-            }
-
-            throw new NoSuchElementException();
-        } catch (SQLException e) {
-            throw new JdbcException(e);
-        }
+        return executeQuery(sql,
+                preparedStatement -> {
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    if (resultSet.next()) {
+                        return rowMapper.rowMap(resultSet);
+                    }
+                    throw new NoSuchElementException();
+                },
+                parameters
+        );
     }
 
     public int update(final String sql, final Object... parameters) {
-        try (
-                final Connection connection = dataSource.getConnection();
-                final PreparedStatement preparedStatement = setValues(sql, connection, parameters)
-        ) {
-            return executeQuery(PreparedStatement::executeUpdate, preparedStatement);
-        } catch (SQLException e) {
-            throw new JdbcException(e);
-        }
+        return executeQuery(sql, PreparedStatement::executeUpdate, parameters);
     }
 
-    private PreparedStatement setValues(
+    private void setValues(
             final String sql,
-            final Connection connection,
+            final PreparedStatement preparedStatement,
             final Object... parameters
     ) throws SQLException {
         log.debug("query : {}", sql);
 
-        final PreparedStatement preparedStatement = connection.prepareStatement(sql);
         for (int i = 0; i < parameters.length; i++) {
             preparedStatement.setObject(i + 1, parameters[i]);
         }
-        return preparedStatement;
     }
 
     private <T> T executeQuery(
+            final String sql,
             final CallBack<T> callBack,
-            final PreparedStatement preparedStatement
-    ) throws SQLException {
-        return callBack.call(preparedStatement);
+            final Object... parameters
+    ) {
+        try (
+                final Connection connection = dataSource.getConnection();
+                final PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            connection.setAutoCommit(false);
+            setValues(sql, preparedStatement, parameters);
+            final T result = callBack.call(preparedStatement);
+            connection.commit();
+            return result;
+        } catch (SQLException e) {
+            throw new JdbcException(e);
+        }
     }
 
     public DataSource getDataSource() {
