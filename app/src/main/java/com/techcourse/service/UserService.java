0c5987de3416c83046ca4006dd1803c7383a29fa
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-10-08 21:47:05 +0900

    refactor: 트랜잭션 실행 로직 분리

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -26,34 +26,29 @@ public class UserService {
     }
 
     public void insert(final User user) {
-        Connection connection = getConnection();
-        try {
-            connection.setAutoCommit(false);
-
+        executeWithTransaction(connection -> {
             userDao.insert(connection, user);
-
-            connection.commit();
-        } catch (SQLException e) {
-            throw new ConnectionException(e.getMessage());
-        } catch (DataAccessException e) {
-            rollback(connection);
-            throw new DataAccessException(e.getMessage());
-        } finally {
-            close(connection);
-        }
+            return null;
+        });
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
         final var user = findById(id);
-        Connection connection = getConnection();
-        try {
-            connection.setAutoCommit(false);
-
+        executeWithTransaction(connection -> {
             user.changePassword(newPassword);
             userDao.update(connection, user);
             userHistoryDao.log(connection, new UserHistory(user, createBy));
+            return null;
+        });
+    }
 
+    private <T> T executeWithTransaction(TransactionExecutor<T> transactionExecutor) {
+        Connection connection = getConnection();
+        try {
+            connection.setAutoCommit(false);
+            T result = transactionExecutor.execute(connection);
             connection.commit();
+            return result;
         } catch (SQLException e) {
             throw new ConnectionException(e.getMessage());
         } catch (DataAccessException e) {
