f2cd5a1eea540c5c93e1eddbf6eac96bbb0ae5cd
JFe <33208246+Go-Jaecheol@users.noreply.github.com>
2023-10-08 13:21:47 +0900

    [JDBC 라이브러리 구현하기 - 3단계] 망고(고재철) 미션 제출합니다. (#509)
    * test: 트랜잭션 학습 테스트 - 1단계
    
    * test: 트랜잭션 학습 테스트 - 2단계
    
    * feat: JdbcTemplate 사용하도록 UserHistoryDao 수정
    
    * feat: 트랜잭션 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,15 +4,22 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import org.springframework.dao.DataAccessException;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final DataSource dataSource) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.dataSource = dataSource;
     }
 
     public User findById(final long id) {
@@ -24,9 +31,32 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        try (final var connection = dataSource.getConnection()) {
+            executeTransaction(id, newPassword, createBy, connection);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void executeTransaction(final long id, final String newPassword, final String createBy,
+                                    final Connection connection) throws SQLException {
+        try {
+            connection.setAutoCommit(false);
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            connection.commit();
+        } catch (SQLException e) {
+            rollbackConnection(connection);
+        }
+    }
+
+    private void rollbackConnection(final Connection connection) {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
