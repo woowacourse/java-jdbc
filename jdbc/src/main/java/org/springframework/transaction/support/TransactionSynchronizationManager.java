f82dc2188b7a525814c0f7d63b14e00ea0b6cb41
zillionme <beatus96@naver.com>
2023-10-09 02:30:26 +0900

    feat: TransactionSynchronizationManager 리소스 bind, unbind, get 기능 추가

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -2,22 +2,38 @@ package org.springframework.transaction.support;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+        return map.getOrDefault(key, null);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            map = new HashMap<>();
+            resources.set(map);
+        }
+        map.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null || !map.containsKey(key)) {
+            return null;
+        }
+        return map.remove(key);
     }
 }
