c532e3b35ab619f722262ae42a0ebc32eb262ddc
chaewon121 <cw121@naver.com>
2023-10-09 07:00:05 +0900

    fix: log함수에 sql오류 수정

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -19,6 +19,14 @@ public class UserHistoryDao {
 
     public void log(final UserHistory userHistory) {
         final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.update(sql, userHistory.getAccount(), userHistory.getPassword(), userHistory.getEmail(), userHistory.getId());
+        jdbcTemplate.update(
+                sql,
+                userHistory.getUserId(),
+                userHistory.getAccount(),
+                userHistory.getPassword(),
+                userHistory.getEmail(),
+                userHistory.getCreatedAt(),
+                userHistory.getCreateBy()
+        );
     }
 }
