cccbf630a0121f55a6d27fca37f06ba723417a6b
hyena <goatsfish@gmail.com>
2023-10-08 12:49:06 +0900

    feat: AppUserService 틀 구현

---
+++ app/src/main/java/com/techcourse/service/AppUserService.java
@@ -0,0 +1,21 @@
+package com.techcourse.service;
+
+import com.techcourse.domain.User;
+
+public class AppUserService implements UserService {
+
+    @Override
+    public User findById(final long id) {
+        return null;
+    }
+
+    @Override
+    public void insert(final User user) {
+
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createBy) {
+
+    }
+}
