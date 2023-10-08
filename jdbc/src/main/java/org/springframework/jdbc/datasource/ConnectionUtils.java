68a02b4a762ebd4206ce57b4bce03449fbef77ed
zillionme <beatus96@naver.com>
2023-10-08 10:33:48 +0900

    refactor: Connection 생성, 종료 유틸 클래스 추가

---
+++ jdbc/src/main/java/org/springframework/jdbc/datasource/ConnectionUtils.java
@@ -0,0 +1,25 @@
+package org.springframework.jdbc.datasource;
+
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class ConnectionUtils {
+
+    public static Connection getConnection(DataSource dataSource) {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new CannotGetJdbcConnectionException("Failed to obtain JDBC Connection", e);
+        }
+    }
+
+    public static void releaseConnection(Connection conn) {
+        try{
+            conn.close();
+        } catch (SQLException e) {
+            throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
+        }
+    }
+}
