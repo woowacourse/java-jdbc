43121cb5740aba7a041c8066e2213c77b5c4e8a8
chaewon121 <cw121@naver.com>
2023-10-09 06:28:56 +0900

    refactor: 서비스 인터페이스 리팩터링

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,30 @@
+package com.techcourse.service;
+
+import com.techcourse.domain.User;
+import org.springframework.transaction.support.TransactionTemplate;
+
+public class TxUserService implements UserService {
+
+    private final UserService userService;
+
+    public TxUserService(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public User findById(final long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void insert(final User user) {
+        userService.insert(user);
+    }
+
+    @Override
+    public void changePassword(final long id,
+                               final String newPassword,
+                               final String createBy) {
+        TransactionTemplate.execute(() -> userService.changePassword(id, newPassword, createBy));
+    }
+}
