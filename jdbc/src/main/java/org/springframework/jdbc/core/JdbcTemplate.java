7c720da09b96b8e3dfc7d18fac937536c0dfbf6b
MoonJeWoong <munjin0201@naver.com>
2023-10-08 13:23:32 +0900

    feat: query 메서드 관련 템플릿 콜백 패턴 적용

--- jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/JdbcTemplate.java
@@ -22,7 +22,7 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public int update(String sql, Object... args) {
+    public int update(final String sql, Object... args) {
         try (
                 Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql)
@@ -39,7 +39,7 @@ public class JdbcTemplate {
         }
     }
 
-    public int update(Connection conn, String sql, Object... args) {
+    public int update(final Connection conn, final String sql, Object... args) {
         try (
                 PreparedStatement pstmt = conn.prepareStatement(sql)
         ) {
@@ -55,48 +55,45 @@ public class JdbcTemplate {
         }
     }
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
-        try (
-                Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, Object... args) {
+        log.debug("query : {}", sql);
 
-            ArgumentPreparedStatementSetter pss = new ArgumentPreparedStatementSetter(args);
-            pss.setValues(pstmt);
+        PreparedStatementCallback<T> callback = pstmt -> {
             List<T> results = extractResultSet(pstmt.executeQuery(), rowMapper);
-
             if (results.size() == 1) {
                 return results.get(0);
             }
             throw new IncorrectResultSizeDataAccessException(results.size());
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw SQLExceptionTranslator.translate(sql, e);
-        }
+        };
+        return executeQuery(callback, sql, args);
     }
 
-    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
-        try (
-                Connection conn = dataSource.getConnection();
-                PreparedStatement pstmt = conn.prepareStatement(sql)
-        ) {
-            ArgumentPreparedStatementSetter pss = new ArgumentPreparedStatementSetter(args);
-            pss.setValues(pstmt);
-
-            return extractResultSet(pstmt.executeQuery(), rowMapper);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw SQLExceptionTranslator.translate(sql, e);
-        }
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, Object... args) {
+        PreparedStatementCallback<List<T>> callBack = pstmt ->
+                extractResultSet(pstmt.executeQuery(), rowMapper);
+        return executeQuery(callBack, sql, args);
     }
 
-    private <T> List<T> extractResultSet(ResultSet rs, RowMapper<T> rowMapper) throws SQLException {
+    private <T> List<T> extractResultSet(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
         List<T> results = new ArrayList<>();
         int rowNum = 0;
         while (rs.next()) {
             results.add(rowMapper.mapRow(rs, rowNum++));
         }
         return results;
+    }
+
+    private <T> T executeQuery(final PreparedStatementCallback<T> action, final String sql, Object... args) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            ArgumentPreparedStatementSetter pss = new ArgumentPreparedStatementSetter(args);
+            pss.setValues(pstmt);
+            return action.doInPreparedStatement(pstmt);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw SQLExceptionTranslator.translate(sql, e);
+        }
     }
 }
