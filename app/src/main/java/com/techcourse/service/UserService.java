a0636eb4904451087d1958b2ecf272b20d588f0b
이지우 <49433615+LJW25@users.noreply.github.com>
2023-10-08 14:30:07 +0900

    [JDBC 라이브러리 구현하기 - 3단계] 이오(이지우) 미션 제출합니다. (#498)
    * feat: UserHistoryDao에 PreparedStatementExecutor 적용
    
    * test: Disabled 삭제
    
    * feat: transaction 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,14 @@
 package com.techcourse.service;
 
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
 
 public class UserService {
 
@@ -26,7 +31,35 @@ public class UserService {
     public void changePassword(final long id, final String newPassword, final String createBy) {
         final var user = findById(id);
         user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+
+        final Connection conn = getConnection();
+        try {
+            conn.setAutoCommit(false);
+
+            userDao.update(conn, user);
+            userHistoryDao.log(conn, new UserHistory(user, createBy));
+
+            conn.commit();
+        } catch (final SQLException e) {
+            rollback(conn);
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    private Connection getConnection() {
+        try {
+            final DataSource dataSource = DataSourceConfig.getInstance();
+            return dataSource.getConnection();
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void rollback(final Connection conn) {
+        try {
+            conn.rollback();
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
