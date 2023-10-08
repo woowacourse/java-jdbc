046a62445102da3730811d4e5c30af426bac11ea
cpot5620 <cpot5620@gmail.com>
2023-10-08 16:15:20 +0900

    refactor: Transaction 객체 제거

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,9 +4,7 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
 import org.springframework.dao.DataAccessException;
-import org.springframework.transaction.Transaction;
 import org.springframework.transaction.TransactionManager;
 
 public class UserService {
@@ -26,32 +24,29 @@ public class UserService {
     }
 
     public void insert(User user) {
-        Transaction transaction = transactionManager.getTransaction();
-        transaction.start();
+        transactionManager.start();
 
         try {
-            userDao.insert(transaction.getConnection(), user);
-            transaction.commit();
+            userDao.insert(user);
+            transactionManager.commit();
         } catch (DataAccessException e) {
-            transaction.rollback();
+            transactionManager.rollback();
             throw e;
         }
     }
 
     public void changePassword(long id, String newPassword, String createBy) {
-        Transaction transaction = transactionManager.getTransaction();
-        transaction.start();
+        transactionManager.start();
         try {
-            Connection connection = transaction.getConnection();
             User user = findById(id);
 
             user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
 
-            transaction.commit();
+            transactionManager.commit();
         } catch (DataAccessException e) {
-            transaction.rollback();
+            transactionManager.rollback();
             throw e;
         }
     }
--- jdbc/src/main/java/org/springframework/transaction/TransactionManager.java
+++ jdbc/src/main/java/org/springframework/transaction/TransactionManager.java
@@ -1,23 +1,38 @@
 package org.springframework.transaction;
 
-import java.sql.SQLException;
+import java.sql.Connection;
 import javax.sql.DataSource;
-import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 public class TransactionManager {
 
+    private final TransactionTemplate transactionTemplate;
     private final DataSource dataSource;
 
     public TransactionManager(DataSource dataSource) {
+        this.transactionTemplate = new TransactionTemplate(dataSource);
         this.dataSource = dataSource;
     }
 
-    public Transaction getTransaction() {
-        try {
-            return new Transaction(dataSource.getConnection());
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
+    public void start() {
+        transactionTemplate.execute(connection -> connection.setAutoCommit(false));
+    }
+
+    public void commit() {
+        transactionTemplate.execute(Connection::commit);
+        close();
+    }
+
+    private void close() {
+        transactionTemplate.execute(connection -> {
+            connection.close();
+            DataSourceUtils.releaseConnection(connection, dataSource);
+        });
+    }
+
+    public void rollback() {
+        transactionTemplate.execute(Connection::rollback);
+        close();
     }
 
 }
