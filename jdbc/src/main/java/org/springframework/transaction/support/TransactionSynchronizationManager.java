06622c51beb7c270d0606232048a07c421c0edc7
hyena <goatsfish@gmail.com>
2023-10-08 12:40:04 +0900

    feat: 트랜잭션 동기화 매니저 기능 구현

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,41 @@
 package org.springframework.transaction.support;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TransactionSynchronizationManager {
 
+    private static final Logger log = LoggerFactory.getLogger(TransactionSynchronizationManager.class);
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    static {
+        initializeResource();
+    }
+
+    private static void initializeResource() {
+        if (resources.get() == null) {
+            resources.set(new HashMap<>());
+            log.info("====> TransactionSynchronizationManager.initializeResource()");
+        }
+    }
+
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        return null;
+        return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        resources.get().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        return resources.get().remove(key);
     }
 }
