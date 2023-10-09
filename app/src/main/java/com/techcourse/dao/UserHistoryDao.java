92626ec7a5ad809022afab3f21a9f1cd5cfd0438
hgo641 <hgo641@gmail.com>
2023-10-09 18:51:20 +0900

    refactor: DataSourceUtils를 사용해서 Connection을 가져오게 변경

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -32,4 +32,16 @@ public class UserHistoryDao {
                 userHistory.getCreateBy()
         );
     }
+
+    public void log(final UserHistory userHistory) {
+        final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(sql,
+                userHistory.getUserId(),
+                userHistory.getAccount(),
+                userHistory.getPassword(),
+                userHistory.getEmail(),
+                userHistory.getCreatedAt(),
+                userHistory.getCreateBy()
+        );
+    }
 }
