afa3589ebf72d3e71744970b46191b834cd17881
LJW25 <dbjwlee@gmail.com>
2023-10-09 17:10:15 +0900

    refactor: UserHistoryDao 파라미터 변경

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -1,7 +1,6 @@
 package com.techcourse.dao;
 
 import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserHistoryDao {
@@ -12,11 +11,10 @@ public class UserHistoryDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void log(final Connection conn, final UserHistory userHistory) {
+    public void log(final UserHistory userHistory) {
         final String sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
 
         jdbcTemplate.update(
-                conn,
                 sql,
                 userHistory.getUserId(),
                 userHistory.getAccount(),
