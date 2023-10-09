060b5fccd7394d25be7918a7d85988b064bfd777
amaranth <81465068+amaran-th@users.noreply.github.com>
2023-10-09 22:03:44 +0900

    [JDBC 라이브러리 구현하기 - 4단계] 아마란스(송세연) 미션 제출합니다. (#578)
    * feat: 4단계 요구사항 구현
    
    - UserService의 트랜잭션 로직을 별도의 클래스로 추상화
    
    * feat: 트랜잭션 구현 메서드 오버로딩
    
    - 반환값이 존재하는 서비스 로직에 대해 트랜잭션 처리할 수 있도록 함.
    
    * feat: 트랜잭션을 사용하지 않는 로직에서 connection을 해제하도록 구현
    
    - jdbc template 로직에 connection 해제/유지 분기문 추가
    
    ---------
    
    Co-authored-by: amaran-th <songsy405@naver.com>

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,43 +1,13 @@
 package com.techcourse.service;
 
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
-import com.techcourse.domain.UserHistory;
-import org.springframework.transaction.support.TransactionManager;
 
 import java.util.Optional;
 
+public interface UserService {
+    Optional<User> findById(final long id);
 
-public class UserService {
+    void insert(final User user);
 
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
-    private final TransactionManager transactionManager;
-
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final TransactionManager transactionManager) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-        this.transactionManager = transactionManager;
-    }
-
-    public Optional<User> findById(final long id) {
-        return userDao.findById(id);
-    }
-
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
-
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        transactionManager.executeInTransaction(() -> {
-            final var user = findById(id);
-            if (user.isPresent()) {
-                final User presentUser = user.get();
-                presentUser.changePassword(newPassword);
-                userDao.update(presentUser);
-                userHistoryDao.log(new UserHistory(presentUser, createBy));
-            }
-        });
-    }
+    void changePassword(final long id, final String newPassword, final String createBy);
 }
