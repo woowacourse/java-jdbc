82feb7614765c3dc70f5831c1110e161c78e31f4
dev_kong <ahdjd5@gmail.com>
2023-10-09 20:36:56 +0900

    feat : ThreadLocal 초기값 갖고있도록 수정

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -1,12 +1,15 @@
 package org.springframework.transaction.support;
 
+import static java.lang.ThreadLocal.withInitial;
+
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, ConnectionManager>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, ConnectionManager>> resources = withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {}
 
