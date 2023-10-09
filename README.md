adcd977411a49559ca53c1758a5b0a5ca96e8289
seokhwan-an <asok369258@naver.com>
2023-10-09 12:41:33 +0900

    feat: transactionSynchronizationManager 구현

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
   - [x] jdbcTemplate에 connection을 받는 update메소드 오버라이드하기
 ## 트랜잭션 synchronization 적용하기
 - [ ] Transaction synchronization 적용하기
-  - [ ] TransactionSynchronizationManager 구현하기
+  - [x] TransactionSynchronizationManager 구현하기
   - [ ] Dao에 connection 인자 제거하기
 - [ ] 트랜잭션 서비스 추상화하기
 
--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -2,22 +2,34 @@ package org.springframework.transaction.support;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
+    static {
+        resources.set(new HashMap<>());
+    }
+
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
+        final Map<DataSource, Connection> connections = resources.get();
+        if (connections.containsKey(key)) {
+            return connections.get(key);
+        }
         return null;
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        final Map<DataSource, Connection> connections = resources.get();
+        connections.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        final Map<DataSource, Connection> connections = resources.get();
+        return connections.remove(key);
     }
 }
