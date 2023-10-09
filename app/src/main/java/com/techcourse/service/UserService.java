a11c37d0b23999929842b4a9abc76fc7853aa64e
sosow0212 <sosow0212@naver.com>
2023-10-09 17:34:28 +0900

    refactor: DatasourceUtils 사용하도록 변경

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -6,6 +6,7 @@ import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -32,32 +33,30 @@ public class UserService {
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
         DataSource dataSource = DataSourceConfig.getInstance();
+        Connection connection = DataSourceUtils.getConnection(dataSource);
 
-        try (final Connection connection = dataSource.getConnection()) {
+        try {
             connection.setAutoCommit(false);
 
             User user = findById(id);
-
             user.changePassword(newPassword);
             userDao.update(connection, user);
             userHistoryDao.log(connection, new UserHistory(user, createBy));
 
-            commit(connection);
+            connection.commit();
+        } catch (final Exception e) {
+            rollback(connection);
+            throw new DataAccessException(e);
+        } finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+        }
+    }
+
+    public void rollback(final Connection connection) {
+        try {
+            connection.rollback();
         } catch (final SQLException e) {
             throw new DataAccessException(e);
         }
-    }
-
-    private void commit(final Connection connection) throws SQLException {
-        try {
-            connection.commit();
-        } catch (final SQLException e) {
-            rollback(connection, e);
-        }
-    }
-
-    private void rollback(final Connection connection, final SQLException e) throws SQLException {
-        connection.rollback();
-        throw new DataAccessException(e);
     }
 }
