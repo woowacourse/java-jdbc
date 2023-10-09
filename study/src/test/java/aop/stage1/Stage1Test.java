d2c79fc0f7bc9e510d636e4d4334ebc0da6b8dae
thdwoqor <celebarte01@gmail.com>
2023-10-09 13:09:11 +0900

    chore: 학습 테스트

--- study/src/test/java/aop/stage1/Stage1Test.java
+++ study/src/test/java/aop/stage1/Stage1Test.java
@@ -1,20 +1,24 @@
 package aop.stage1;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import aop.DataAccessException;
 import aop.StubUserHistoryDao;
 import aop.domain.User;
 import aop.repository.UserDao;
 import aop.repository.UserHistoryDao;
+import aop.service.AppUserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.PointcutAdvisor;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Stage1Test {
@@ -41,7 +45,7 @@ class Stage1Test {
 
     @Test
     void testChangePassword() {
-        final UserService userService = null;
+        final UserService userService = new UserService(userDao, userHistoryDao);
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -54,7 +58,13 @@ class Stage1Test {
 
     @Test
     void testTransactionRollback() {
-        final UserService userService = null;
+        ProxyFactoryBean pfBean = new ProxyFactoryBean();
+        pfBean.setTarget(new UserService(userDao, stubUserHistoryDao));
+        TransactionAdvice advice = new TransactionAdvice();
+        advice.setTransactionManager(platformTransactionManager);
+        pfBean.addAdvisor(new TransactionAdvisor(advice,new TransactionPointcut()));
+
+        final UserService userService = (UserService) pfBean.getObject();
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
