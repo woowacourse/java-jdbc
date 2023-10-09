f0480cf2238e084444f74ecf36f91f6695672b78
shin-mallang <huipulci1@naver.com>
2023-10-09 13:21:23 +0900

    DataSourceUtils 작성

--- jdbc/src/main/java/org/springframework/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/org/springframework/jdbc/datasource/DataSourceUtils.java
@@ -17,7 +17,6 @@ public abstract class DataSourceUtils {
         if (connection != null) {
             return connection;
         }
-
         try {
             connection = dataSource.getConnection();
             TransactionSynchronizationManager.bindResource(dataSource, connection);
@@ -30,6 +29,7 @@ public abstract class DataSourceUtils {
     public static void releaseConnection(Connection connection, DataSource dataSource) {
         try {
             connection.close();
+            TransactionSynchronizationManager.unbindResource(dataSource);
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
         }
