84010edeb416ab22a3675fa50eeeab6e8c52b532
HyeonjaeKwon <kwonhyeonjae01@gmail.com>
2023-10-09 19:18:23 +0900

    refactor: 컨벤션을 위한 인자에 final 키워드 추가

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -8,7 +8,7 @@ public interface UserService {
 
     Optional<User> findById(final long id);
 
-    void insert(User user);
+    void insert(final User user);
 
     void changePassword(final long id, final String newPassword, final String createBy);
 }
