ba64ed4755f874aeff3fc44c10b8fd2d436c7402
jaehee329 <jjhs9803@gmail.com>
2023-10-10 02:18:16 +0900

    feat: TxUserService 구현

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,106 @@
+package com.techcourse.service;
+
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Supplier;
+import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class TxUserService implements UserService {
+
+    private final AppUserService appUserService;
+    private final DataSource datasource = DataSourceConfig.getInstance();
+
+    public TxUserService(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @Override
+    public User findById(long id) {
+        return execute(() -> appUserService.findById(id));
+    }
+
+    @Override
+    public void insert(User user) {
+        execute(() -> appUserService.insert(user));
+    }
+
+    @Override
+    public void changePassword(long id, String newPassword, String createBy) {
+        execute(() -> appUserService.changePassword(id, newPassword, createBy));
+    }
+
+    private void execute(Runnable runnable) {
+        try (TransactionExecutor executor = TransactionExecutor.initTransaction(datasource)) {
+            try {
+                runnable.run();
+                executor.commit();
+            } catch (Exception e) {
+                executor.rollback();
+                throw new DataAccessException();
+            }
+        }
+    }
+
+    private <T> T execute(Supplier<T> supplier) {
+        try (TransactionExecutor transactionExecutor = TransactionExecutor.initTransaction(datasource)) {
+            try {
+                T result = supplier.get();
+                transactionExecutor.commit();
+                return result;
+            } catch (Exception e) {
+                transactionExecutor.rollback();
+                throw new DataAccessException();
+            }
+        }
+    }
+
+    private static class TransactionExecutor implements AutoCloseable {
+
+        private final DataSource dataSource;
+        private final Connection connection;
+
+        private TransactionExecutor(DataSource dataSource, Connection connection) {
+            this.dataSource = dataSource;
+            this.connection = connection;
+        }
+
+        public static TransactionExecutor initTransaction(DataSource dataSource) {
+            try {
+                Connection conn = dataSource.getConnection();
+                TransactionSynchronizationManager.bindResource(dataSource, conn);
+                Connection connection = DataSourceUtils.getConnection(dataSource);
+                connection.setAutoCommit(false);
+                return new TransactionExecutor(dataSource, connection);
+            } catch (SQLException e) {
+                throw new RuntimeException();
+            }
+        }
+
+        public void commit() {
+            try {
+                connection.commit();
+            } catch (SQLException e) {
+                throw new RuntimeException();
+            }
+        }
+
+        public void rollback() {
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                throw new RuntimeException();
+            }
+        }
+
+        @Override
+        public void close() {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+            TransactionSynchronizationManager.unbindResource(dataSource);
+        }
+    }
+}
--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,61 +1,10 @@
 package com.techcourse.service;
 
-import com.techcourse.config.DataSourceConfig;
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
-import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
-import java.sql.SQLException;
-import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.dao.DataAccessException;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
 
-public class UserService {
+public interface UserService {
 
-    private static final Logger log = LoggerFactory.getLogger(UserService.class);
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
-
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-    }
-
-    public User findById(final long id) {
-        return userDao.findById(id);
-    }
-
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
-
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        DataSource dataSource = DataSourceConfig.getInstance();
-        Connection conn = null;
-        try {
-            conn = dataSource.getConnection();
-            conn.setAutoCommit(false);
-            TransactionSynchronizationManager.bindResource(dataSource, conn);
-            User user = findById(id);
-            user.changePassword(newPassword);
-            userDao.update(user);
-            userHistoryDao.log(new UserHistory(user, createBy));
-            conn.commit();
-        } catch (Exception e) {
-            log.error(e.getMessage(), e);
-            if (conn != null) {
-                try {
-                    TransactionSynchronizationManager.unbindResource(dataSource);
-                    conn.rollback();
-                    conn.close();
-                } catch (SQLException ex) {
-                    log.error(ex.getMessage(), ex);
-                }
-            }
-            throw new DataAccessException();
-        }
-    }
+    User findById(final long id);
+    void insert(final User user);
+    void changePassword(final long id, final String newPassword, final String createBy);
 }
