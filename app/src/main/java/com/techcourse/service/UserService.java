17717a2749afe059f7be27150bfd2a5af1e3cbf1
dev_kong <96301958+green-kong@users.noreply.github.com>
2023-10-09 14:58:50 +0900

    jdbc 3단계 폴로 미션 제출합니다. (#464)
    * feat : 3단계 구현
    
    * fix : TransactionManager 를 거치지 않는 통신또한 통신이후 정상적으로 종료되도록 수정
    
    * fix : TransactionManager#transact DataAccessException 도 캐치하도록 수정
    
    * fix : connection 반환 저장 전에 connection close 가 됐는지 확인하도록 수정
    
    * fix : 반환과정에서 closevalidate 제거

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,5 +1,7 @@
 package com.techcourse.service;
 
+import org.springframework.transaction.TransactionManager;
+
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -9,10 +11,13 @@ public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final TransactionManager transactionManager;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao,
+                       final TransactionManager transactionManager) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.transactionManager = transactionManager;
     }
 
     public User findById(final long id) {
@@ -24,9 +29,11 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        transactionManager.transact(() -> {
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
+        });
     }
 }
---
+++ jdbc/src/main/java/org/springframework/transaction/ConnectionHolder.java
@@ -0,0 +1,40 @@
+package org.springframework.transaction;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.springframework.dao.DataAccessException;
+
+public class ConnectionHolder {
+    public static final ThreadLocal<Connection> HOLDER = new ThreadLocal<>();
+
+    private ConnectionHolder() {
+    }
+
+    public static Connection getConnection() {
+        return HOLDER.get();
+    }
+
+    private static void validateClose(final Connection connection) {
+        try {
+            if (connection.isClosed()) {
+                throw new DataAccessException();
+            }
+        } catch(SQLException exception) {
+            throw new DataAccessException();
+        }
+    }
+
+    public static void setConnection(final Connection connection) {
+        validateClose(connection);
+        HOLDER.set(connection);
+    }
+
+    public static void clean() {
+        HOLDER.remove();
+    }
+
+    public static boolean hasSame(final Connection connection) {
+        return HOLDER.get() == connection;
+    }
+}
