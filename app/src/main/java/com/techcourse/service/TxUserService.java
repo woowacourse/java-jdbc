c13f3d63cd3b168eefa817ec0e7f6b9285883869
ReO <yt930310@gmail.com>
2023-10-09 16:16:39 +0900

    feat: 서비스 분리

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,30 @@
+package com.techcourse.service;
+
+import com.techcourse.domain.User;
+import org.springframework.transaction.support.TransactionTemplate;
+
+public class TxUserService implements UserService {
+
+    private final UserService userService;
+    private final TransactionTemplate transactionTemplate;
+
+    public TxUserService(UserService userService, TransactionTemplate transactionTemplate) {
+        this.userService = userService;
+        this.transactionTemplate = transactionTemplate;
+    }
+
+    @Override
+    public User findById(long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void insert(User user) {
+        transactionTemplate.execute(() -> userService.insert(user));
+    }
+
+    @Override
+    public void changePassword(long id, String newPassword, String createBy) {
+        transactionTemplate.execute(() -> userService.changePassword(id, newPassword, createBy));
+    }
+}
---
+++ app/src/test/java/com/techcourse/service/TxUserServiceTest.java
@@ -0,0 +1,56 @@
+package com.techcourse.service;
+
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.dao.UserDao;
+import com.techcourse.domain.User;
+import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.CannotGetJdbcConnectionException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.sql.DataSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class TxUserServiceTest {
+
+    private JdbcTemplate jdbcTemplate;
+    private UserDao userDao;
+    private TransactionTemplate transactionTemplate;
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        DataSource dataSource = DataSourceConfig.getInstance();
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.userDao = new UserDao(jdbcTemplate);
+        this.transactionTemplate = new TransactionTemplate(dataSource);
+
+        DatabasePopulatorUtils.execute(dataSource);
+        userDao.deleteAll();
+
+        final var user = new User("gugu", "password", "hkkang@woowahan.com");
+        userDao.insert(user);
+        this.user = userDao.findByAccount("gugu");
+    }
+
+    @Test
+    void testTransactionRollback() {
+        final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
+        final var appUserService = new AppUserService(userDao, userHistoryDao);
+        final var userService = new TxUserService(appUserService, transactionTemplate);
+
+        final var newPassword = "newPassword";
+        final var createBy = "gugu";
+
+        assertThrows(CannotGetJdbcConnectionException.class,
+                () -> userService.changePassword(1L, newPassword, createBy));
+
+        final var actual = userService.findById(user.getId());
+
+        assertThat(actual.getPassword()).isNotEqualTo(newPassword);
+    }
+}
