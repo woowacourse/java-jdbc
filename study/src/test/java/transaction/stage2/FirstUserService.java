5ad575ea6a36d55797fa42d872a91d8359cdb963
yujamint <woawoa0513@gmail.com>
2023-10-09 16:22:51 +0900

    test: 학습 테스트 로깅 추가

--- study/src/test/java/transaction/stage2/FirstUserService.java
+++ study/src/test/java/transaction/stage2/FirstUserService.java
@@ -110,7 +110,7 @@ public class FirstUserService {
         return of(firstTransactionName, secondTransactionName);
     }
 
-//    @Transactional(propagation = Propagation.REQUIRED)
+    @Transactional(propagation = Propagation.REQUIRED)
     public Set<String> saveFirstTransactionWithNever() {
         final var firstTransactionName = TransactionSynchronizationManager.getCurrentTransactionName();
         userRepository.save(User.createTest());
@@ -130,7 +130,10 @@ public class FirstUserService {
     private void logActualTransactionActive() {
         final var currentTransactionName = TransactionSynchronizationManager.getCurrentTransactionName();
         final var actualTransactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+        final var synchronizationActive = TransactionSynchronizationManager.isSynchronizationActive();
         final var emoji = actualTransactionActive ? "✅" : "❌";
+        final var emoji2 = synchronizationActive ? "✅" : "❌";
         log.info("\n{} is Actual Transaction Active : {} {}", currentTransactionName, emoji, actualTransactionActive);
+        log.info("\n{} is Synchronization Transaction Active : {} {}", currentTransactionName, emoji2, synchronizationActive);
     }
 }
