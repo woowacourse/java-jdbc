7f136104b1d0abbcd1b398e0f4fcfe9e50dd0109
donghae-kim <donghae1999@gmail.com>
2023-10-09 19:20:57 +0900

    refactor: hashMap -> concurrentHashMap 수정

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -2,30 +2,28 @@ package org.springframework.transaction.support;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources =  ThreadLocal.withInitial(HashMap::new);
+    private static final ThreadLocal<ConcurrentHashMap<DataSource, Connection>> resources = ThreadLocal.withInitial(ConcurrentHashMap::new);
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        Map<DataSource, Connection> connections = resources.get();
+        ConcurrentHashMap<DataSource, Connection> connections = resources.get();
         return connections.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        Map<DataSource, Connection> connections = new HashMap<>();
+        ConcurrentHashMap<DataSource, Connection> connections = new ConcurrentHashMap<>();
         connections.put(key, value);
         resources.set(connections);
     }
 
-
     public static Connection unbindResource(DataSource key) {
-        final Map<DataSource, Connection> connections = resources.get();
+        final ConcurrentHashMap<DataSource, Connection> connections = resources.get();
         final Connection removedConnection = connections.remove(key);
 
         return removedConnection;
