0691422adb5b8abdcf9ab47418a5814ead9bd472
seokhwan-an <asok369258@naver.com>
2023-10-09 15:05:07 +0900

    style: 불필요한 import문 제거 및 가독성 향상

--- jdbc/src/main/java/org/springframework/jdbc/exception/DataAccessException.java
+++ jdbc/src/main/java/org/springframework/jdbc/exception/DataAccessException.java
@@ -1,7 +1,5 @@
 package org.springframework.jdbc.exception;
 
-import java.sql.SQLException;
-
 public class DataAccessException extends RuntimeException {
 
     public DataAccessException(String message) {
--- jdbc/src/main/java/org/springframework/jdbc/exception/RollbackFailException.java
+++ jdbc/src/main/java/org/springframework/jdbc/exception/RollbackFailException.java
@@ -1,6 +1,6 @@
 package org.springframework.jdbc.exception;
 
-public class RollbackFailException extends RuntimeException{
+public class RollbackFailException extends RuntimeException {
 
     public RollbackFailException(String message) {
         super(message);
