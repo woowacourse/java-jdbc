932af39d8501fe801688c0af5fbbc19e4e3aa3dd
nuyh <hjaehyun25@gmail.com>
2023-10-09 14:50:36 +0900

    feat: TransactionSynchronizationManager 구현

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,56 @@
 package org.springframework.transaction.support;
 
+import javax.annotation.Nullable;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.Objects.isNull;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
+    @Nullable
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> connections = getResources();
+        return connections.get(key);
+    }
+
+    private static Map<DataSource, Connection> getResources() {
+        if (isNull(resources.get())) {
+            resources.set(new HashMap<>());
+        }
+
+        return resources.get();
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        final Map<DataSource, Connection> resources = getResources();
+
+        if (resources.containsKey(key)) {
+            throw new IllegalStateException("이미 커넥션이 존재합니다.");
+        }
+        resources.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        final Map<DataSource, Connection> resources = getResources();
+        final Connection connection = resources.get(key);
+
+        if (isNull(connection)) {
+            throw new IllegalStateException("커넥션이 존재하지 않습니다.");
+        }
+        resources.remove(key);
+
+        if (resources.isEmpty()) {
+            TransactionSynchronizationManager.resources.remove();
+        }
+
+        return connection;
     }
 }
