835ec94b4f476ffc5743fe6d3e1dc592e0cd1224
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2023-10-09 12:00:17 +0900

    feat: TransactionSynchronizationManager 구현

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -1,8 +1,10 @@
 package org.springframework.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
@@ -10,14 +12,21 @@ public abstract class TransactionSynchronizationManager {
 
     private TransactionSynchronizationManager() {}
 
-    public static Connection getResource(DataSource key) {
-        return null;
+    public static Connection getResource(final DataSource key) {
+        final Map<DataSource, Connection> connectionByDataSource = resources.get();
+        if (Objects.isNull(connectionByDataSource)) {
+            return null;
+        }
+        return connectionByDataSource.get(key);
     }
 
-    public static void bindResource(DataSource key, Connection value) {
+    public static void bindResource(final DataSource key, final Connection value) {
+        resources.set(new ConcurrentHashMap<>(
+                Map.of(key, value)
+        ));
     }
 
-    public static Connection unbindResource(DataSource key) {
-        return null;
+    public static Connection unbindResource(final DataSource key) {
+        return resources.get().remove(key);
     }
 }
