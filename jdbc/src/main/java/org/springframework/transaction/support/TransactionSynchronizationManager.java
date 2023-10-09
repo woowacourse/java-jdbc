9adab2f6081735fd41ce5319ee7c0df6b4b1416c
DongUk <68818952+Kim0914@users.noreply.github.com>
2023-10-09 15:05:53 +0900

    feat: TransactionSynchronizationManager 구현

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -1,8 +1,10 @@
 package org.springframework.transaction.support;
 
+import java.util.HashMap;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.Map;
+import org.springframework.jdbc.exception.ConnectionBindingException;
 
 public abstract class TransactionSynchronizationManager {
 
@@ -11,13 +13,30 @@ public abstract class TransactionSynchronizationManager {
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> dataSourceConnection = resources.get();
+        if (dataSourceConnection == null) {
+            return null;
+        }
+        return dataSourceConnection.getOrDefault(key, null);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> dataSourceConnection = resources.get();
+        if (dataSourceConnection == null) {
+            dataSourceConnection = new HashMap<>();
+            resources.set(dataSourceConnection);
+        }
+        if (dataSourceConnection.containsKey(key)) {
+            throw new ConnectionBindingException("커넥션이 이미 바인딩되어 있습니다.");
+        }
+        dataSourceConnection.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> dataSourceConnection = resources.get();
+        if (dataSourceConnection == null || !dataSourceConnection.containsKey(key)) {
+            throw new ConnectionBindingException("바인딩 된 커넥션이 존재하지 않습니다.");
+        }
+        return dataSourceConnection.remove(key);
     }
 }
---
+++ jdbc/src/test/java/org/springframework/transaction/support/TransactionSynchronizationManagerTest.java
@@ -0,0 +1,74 @@
+package org.springframework.transaction.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+
+import java.sql.Connection;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.exception.ConnectionBindingException;
+
+@SuppressWarnings("NonAsciiCharacters")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class TransactionSynchronizationManagerTest {
+
+    @Test
+    void DataSource에_해당하는_커넥션을_등록한다() {
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        assertThat(TransactionSynchronizationManager.getResource(dataSource)).isNotNull();
+    }
+
+    @Test
+    void DataSource에_등록된_커넥션을_등록하면_예외가_발생한다() {
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        assertThatThrownBy(() -> TransactionSynchronizationManager.bindResource(dataSource, connection))
+                .isInstanceOf(ConnectionBindingException.class)
+                .hasMessage("커넥션이 이미 바인딩되어 있습니다.");
+    }
+
+    @Test
+    void DataSource에_해당하는_커넥션을_조회한다() {
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        assertThat(TransactionSynchronizationManager.getResource(dataSource)).isNotNull();
+    }
+
+    @Test
+    void DataSource에_해당하는_커넥션이_없으면_NULL_반환한다() {
+        DataSource dataSource = mock(DataSource.class);
+
+        assertThat(TransactionSynchronizationManager.getResource(dataSource)).isNull();
+    }
+
+    @Test
+    void DataSource에_해당하는_커넥션을_삭제한다() {
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        TransactionSynchronizationManager.unbindResource(dataSource);
+
+        assertThat(TransactionSynchronizationManager.getResource(dataSource)).isNull();
+    }
+
+    @Test
+    void 존재하지_않는_DataSource에_해당하는_커넥션을_삭제하면_예외가_발생한다() {
+        DataSource dataSource = mock(DataSource.class);
+
+        assertThatThrownBy(() -> TransactionSynchronizationManager.unbindResource(dataSource))
+                .isInstanceOf(ConnectionBindingException.class)
+                .hasMessage("바인딩 된 커넥션이 존재하지 않습니다.");
+    }
+}
