8929b4ee4d560dd2247cf088862f7337c4d73e04
SproutMj <mjk54565@gmail.com>
2023-10-09 09:40:03 +0900

    refactor: 반환값이 있는 경우에는 Callable을 사용하기 위해 메서드 오버로딩으로 execute 메서드를 리팩토링

--- jdbc/src/main/java/org/springframework/jdbc/core/TransactionManager.java
+++ jdbc/src/main/java/org/springframework/jdbc/core/TransactionManager.java
@@ -6,6 +6,8 @@ import org.springframework.jdbc.datasource.DataSourceUtils;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
 
 public final class TransactionManager {
 
@@ -15,12 +17,30 @@ public final class TransactionManager {
         this.dataSource = dataSource;
     }
 
+    public <T> T execute(final Callable<T> callable) {
+        return executeTemplate(()-> {
+            try {
+                return callable.call();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
     public void execute(final Runnable runnable) {
+        executeTemplate(()-> {
+            runnable.run();
+            return null;
+        });
+    }
+
+    private <T> T executeTemplate(final Supplier<T> supplier) {
         final Connection connection = DataSourceUtils.getConnection(dataSource);
         try {
             connection.setAutoCommit(false);
-            runnable.run();
+            final T result = supplier.get();
             connection.commit();
+            return result;
         } catch (SQLException | DataAccessException e) {
             try {
                 connection.rollback();
