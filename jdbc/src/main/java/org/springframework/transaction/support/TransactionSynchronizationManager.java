a7d0b48d524484466b2c5e3430c8987452321811
hanueleee <ezzeezz@naver.com>
2023-10-10 10:39:09 +0900

    refactor(TransactionSynchronizationManager): 예외처리

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -1,31 +1,32 @@
 package org.springframework.transaction.support;
 
-import java.util.HashMap;
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
+import org.springframework.dao.DataAccessException;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
         Map<DataSource, Connection> connections = resources.get();
-        if (connections == null) {
-            return null;
+        Connection connection = connections.get(key);
+        if (connection == null) {
+            throw new DataAccessException("there is no connection");
         }
-        return connections.get(key);
+        return connection;
     }
 
     public static void bindResource(DataSource key, Connection value) {
         Map<DataSource, Connection> connections = resources.get();
-        if (connections == null) {
-            connections = new HashMap<>();
-            resources.set(connections);
+        Connection oldConnection = connections.put(key, value);
+        if (oldConnection != null) {
+            throw new DataAccessException("connection is already bound");
         }
-        connections.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
