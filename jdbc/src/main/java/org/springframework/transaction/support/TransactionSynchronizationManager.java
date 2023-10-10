f6bcebdd866f749a70d43c83be5dcf672db750a2
parkmuhyeun <pjhg410@gmail.com>
2023-10-10 11:54:19 +0900

    refactor: null 처리 추가

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -2,6 +2,7 @@ package org.springframework.transaction.support;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TransactionSynchronizationManager {
@@ -14,16 +15,28 @@ public abstract class TransactionSynchronizationManager {
         if (resources.get() == null) {
             return null;
         }
+        if (key == null) {
+            throw new IllegalArgumentException("Key must not be null");
+        }
         return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        resources.set(Map.of(key, value));
+        if (value == null) {
+            throw new IllegalArgumentException("Value must not be null");
+        }
+        if (resources.get() == null) {
+            resources.set(new HashMap<>());
+        }
+        final Map<DataSource, Connection> map = resources.get();
+        map.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        final Connection connection = resources.get().get(key);
-        resources.remove();
-        return connection;
+        if (resources.get() == null) {
+            throw new IllegalStateException("resource is Empty");
+        }
+        final Map<DataSource, Connection> map = resources.get();
+        return map.remove(key);
     }
 }
