557825c9d13abf240907aeef825e450ecf8181d6
hanueleee <ezzeezz@naver.com>
2023-10-10 09:50:30 +0900

    chore(TxUserService): @Override 붙이기

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -13,14 +13,17 @@ public class TxUserService implements UserService {
         this.userService = userService;
     }
 
+    @Override
     public User findById(final long id) {
         return transactionTemplate.executeWithResult(() -> userService.findById(id));
     }
 
+    @Override
     public void insert(final User user) {
         transactionTemplate.execute(() -> userService.insert(user));
     }
 
+    @Override
     public void changePassword(final long id, final String newPassword, final String createBy) {
         transactionTemplate.execute(() -> userService.changePassword(id, newPassword, createBy));
     }
