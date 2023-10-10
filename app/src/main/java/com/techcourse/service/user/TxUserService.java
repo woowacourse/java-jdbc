b984bc6fca6c8b00c03ae4868ae303e1b821aca4
jaeyeon kim <jakind@naver.com>
2023-10-10 12:04:28 +0900

    refactor : TxUserService 에 Connection 을 닫아주는 역할 위임

--- app/src/main/java/com/techcourse/service/user/TxUserService.java
+++ app/src/main/java/com/techcourse/service/user/TxUserService.java
@@ -1,6 +1,9 @@
 package com.techcourse.service.user;
 
 import com.techcourse.domain.User;
+import java.sql.Connection;
+import org.springframework.jdbc.datasource.DataSourceConfig;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.transaction.support.TransactionExecutor;
 
 public class TxUserService implements UserService {
@@ -13,17 +16,27 @@ public class TxUserService implements UserService {
 
     @Override
     public User findById(long id) {
-        return TransactionExecutor.transactionQuery(() -> userService.findById(id));
+        DataSourceUtils.getConnection(DataSourceConfig.getInstance());
+        User user = TransactionExecutor.transactionQuery(() -> userService.findById(id));
+        DataSourceUtils.releaseConnection(DataSourceConfig.getInstance());
+
+        return user;
     }
 
     @Override
     public void insert(User user) {
-        TransactionExecutor.transactionCommand(() -> userService.insert(user));
+        Connection connection = DataSourceUtils.getConnection(DataSourceConfig.getInstance());
+        TransactionExecutor.transactionCommand(connection, () -> userService.insert(user));
+        DataSourceUtils.releaseConnection(DataSourceConfig.getInstance());
     }
 
     @Override
     public void changePassword(long id, String newPassword, String createBy) {
-        TransactionExecutor.transactionCommand(() -> userService.changePassword(id, newPassword, createBy));
+        Connection connection = DataSourceUtils.getConnection(DataSourceConfig.getInstance());
+        TransactionExecutor.transactionCommand(
+                connection, () -> userService.changePassword(id, newPassword, createBy)
+        );
+        DataSourceUtils.releaseConnection(DataSourceConfig.getInstance());
     }
 
 }
--- jdbc/src/main/java/org/springframework/transaction/support/TransactionExecutor.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionExecutor.java
@@ -4,37 +4,27 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.function.Supplier;
 import org.springframework.dao.DataAccessException;
-import org.springframework.jdbc.datasource.DataSourceConfig;
-import org.springframework.jdbc.datasource.DataSourceUtils;
 
 public class TransactionExecutor {
 
     private TransactionExecutor() {
     }
 
-    public static void transactionCommand(Runnable runnable) {
+    public static void transactionCommand(Connection connection, Runnable runnable) {
         try {
-            Connection connection = getConnection();
             connection.setAutoCommit(false);
 
             runnable.run();
 
             connection.commit();
         } catch (SQLException e) {
-            rollback();
+            rollback(connection);
             throw new DataAccessException(e);
-        } finally {
-            DataSourceUtils.releaseConnection(DataSourceConfig.getInstance());
         }
     }
 
-    private static Connection getConnection() {
-        return DataSourceUtils.getConnection(DataSourceConfig.getInstance());
-    }
-
-    private static void rollback() {
+    private static void rollback(Connection connection) {
         try {
-            Connection connection = getConnection();
             connection.rollback();
         } catch (SQLException e) {
             throw new DataAccessException(e);
@@ -42,14 +32,7 @@ public class TransactionExecutor {
     }
 
     public static <T> T transactionQuery(Supplier<T> supplier) {
-        try {
-            getConnection();
-            return supplier.get();
-        } catch (Exception e) {
-            throw new DataAccessException(e);
-        } finally {
-            DataSourceUtils.releaseConnection(DataSourceConfig.getInstance());
-        }
+        return supplier.get();
     }
 
 }
