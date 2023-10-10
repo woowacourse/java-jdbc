ee33f0e6e41acce8555c7585fdaffcc18790add6
nuyh <hjaehyun25@gmail.com>
2023-10-10 22:49:51 +0900

    refactor: close(), unbindResource() 같이 실행 되도록 변경

--- jdbc/src/main/java/org/springframework/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/org/springframework/jdbc/datasource/DataSourceUtils.java
@@ -31,10 +31,9 @@ public abstract class DataSourceUtils {
     public static void releaseConnection(Connection connection, DataSource dataSource) {
         try {
             connection.close();
+            TransactionSynchronizationManager.unbindResource(dataSource);
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
-        } finally {
-            TransactionSynchronizationManager.unbindResource(dataSource);
         }
     }
 }
