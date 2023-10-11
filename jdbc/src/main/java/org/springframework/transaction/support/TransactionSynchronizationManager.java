e157665c88d11faf5a0336b1037d79fb8eefbe96
Rosie <dev.kayoung@gmail.com>
2023-10-11 15:21:27 +0900

    ThreadLocal.withInitial 사용

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -7,35 +7,21 @@ import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {
     }
 
     public static Connection getResource(DataSource key) {
-        final Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
-
-        if (dataSourceConnectionMap == null) {
-            return null;
-        }
-        return dataSourceConnectionMap.get(key);
+        return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        if (resources.get() == null) {
-            resources.set(new HashMap<>());
-        }
-
         resources.get().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        final Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
-
-        if (dataSourceConnectionMap == null) {
-            return null;
-        }
-        return dataSourceConnectionMap.remove(key);
+        return resources.get().remove(key);
     }
 
 }
