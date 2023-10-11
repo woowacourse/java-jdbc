8cf200211dda107426b0dd630e9a4a83f21cca1e
zillionme <beatus96@naver.com>
2023-10-11 14:17:30 +0900

    fix: 트랜잭션 동기화 매니저 unbind에 스레드 로컬에 대한 자원 해제 기능 추가

--- jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/org/springframework/transaction/support/TransactionSynchronizationManager.java
@@ -30,7 +30,7 @@ public abstract class TransactionSynchronizationManager {
 
     public static ConnectionHolder unbindResource(DataSource key) {
         Map<DataSource, ConnectionHolder> map = resources.get();
-        if (map == null || !map.containsKey(key)) {
+        if (map.isEmpty() || !map.containsKey(key)) { // 새로 추가된 코드: map이 비어있다면, 스레드 로컬에서 Map<DataSource, ConnectionHolder> 객체를 삭제
             return null;
         }
         return map.remove(key);
