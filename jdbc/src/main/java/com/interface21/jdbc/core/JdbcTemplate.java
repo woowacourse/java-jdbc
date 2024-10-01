72df58c112b6508d6c5d88bae1ce6dc5f51aecf7
JUHA <84626225+khabh@users.noreply.github.com>
2024-10-01 10:52:26 +0900

    refactor: ParameterSetter와 MappedResultSet을 JdbcTemplate에 적용

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -2,9 +2,7 @@ package com.interface21.jdbc.core;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +12,6 @@ import javax.sql.DataSource;
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-    private static final int START_PARAMETER_INDEX = 1;
 
     private final DataSource dataSource;
 
@@ -22,49 +19,38 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public int update(String sql, Object... parameters) {
+    public int update(String sql, ParameterSetter parameterSetter) {
         return execute(sql, preparedStatement -> {
-            setParameters(preparedStatement, parameters);
+            parameterSetter.setParameters(preparedStatement);
             return preparedStatement.executeUpdate();
         });
     }
 
-    public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper, Object... parameters) {
-        return execute(sql, (preparedStatement) -> {
-            setParameters(preparedStatement, parameters);
-            ResultSet rs = preparedStatement.executeQuery();
-            List<T> results = new ArrayList<>();
+    public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper) {
+        return queryForList(sql, rowMapper, preparedStatement -> {});
+    }
 
-            while (rs.next()) {
-                results.add(rowMapper.map(rs));
-            }
-            return results;
+    public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper,  ParameterSetter parameterSetter) {
+        return execute(sql, (preparedStatement) -> {
+            parameterSetter.setParameters(preparedStatement);
+            return MappedResultSet.create(rowMapper, preparedStatement)
+                    .getResults();
         });
     }
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... parameters) {
-        return execute(sql, (preparedStatement) -> {
-            setParameters(preparedStatement, parameters);
-            ResultSet resultSet = preparedStatement.executeQuery();
-
-            if (resultSet.next()) {
-                return rowMapper.map(resultSet);
-            }
-            return null;
-        });
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper) {
+        return queryForObject(sql, rowMapper, preparedStatement -> {});
     }
 
-    private void setParameters(PreparedStatement preparedStatement, Object... parameters)
-            throws SQLException {
-        int parameterIndex = START_PARAMETER_INDEX;
-        for (Object parameter: parameters) {
-            preparedStatement.setObject(parameterIndex, parameter);
-            parameterIndex++;
-        }
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, ParameterSetter parameterSetter) {
+        return execute(sql, (preparedStatement) -> {
+            parameterSetter.setParameters(preparedStatement);
+            return MappedResultSet.create(rowMapper, preparedStatement, 1)
+                    .getFirst();
+        });
     }
 
     public <T> T execute(String sql, SqlFunction<PreparedStatement, T> action) {
-        validateSql(sql);
         try (Connection conn = dataSource.getConnection();
              PreparedStatement preparedStatement = conn.prepareStatement(sql)) {
             log.debug("Executing query: {}", sql);
@@ -73,13 +59,6 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error("Error executing query: {}", e.getMessage(), e);
             throw new RuntimeException(e);
-        }
-    }
-
-    private void validateSql(String sql) {
-        // 예: SQL 예약어가 포함되어 있는지 확인
-        if (sql.toLowerCase().contains("drop") || sql.toLowerCase().contains("delete")) {
-            throw new IllegalArgumentException("Unsafe SQL query detected.");
         }
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,98 @@
 package com.interface21.jdbc.core;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 class JdbcTemplateTest {
 
+    @Mock
+    private DataSource dataSource;
+
+    @Mock
+    private Connection connection;
+
+    @Mock
+    private PreparedStatement preparedStatement;
+
+    @Mock
+    private ResultSet resultSet;
+
+    @InjectMocks
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        MockitoAnnotations.openMocks(this);
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+    }
+
+    @DisplayName("업데이트 쿼리를 실행한다.")
+    @Test
+    void update() throws SQLException {
+        when(preparedStatement.executeUpdate()).thenReturn(1);
+
+        int result = jdbcTemplate.update("UPDATE users SET name = ? WHERE id = ?", preparedStatement -> {
+                    preparedStatement.setString(1, "TRE");
+                    preparedStatement.setLong(2, 1L);
+                });
+
+        assertThat(result).isEqualTo(1);
+        verify(preparedStatement).setString(1, "TRE");
+        verify(preparedStatement).setLong(2, 1);
+        verify(preparedStatement).executeUpdate();
+    }
+
+    @DisplayName("조회 쿼리에 대한 목록을 반환한다.")
+    @Test
+    void queryForList() throws SQLException {
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, true, false);
+        when(resultSet.getString("name")).thenReturn("Potato1", "Potato2");
+
+        List<String> results = jdbcTemplate.queryForList("SELECT name FROM users", rs -> rs.getString("name"));
+
+        assertThat(results).hasSize(2)
+                .containsExactly("Potato1", "Potato2");
+    }
+
+    @DisplayName("조회 쿼리에 대한 객체를 반환한다.")
+    @Test
+    void queryForObject() throws SQLException {
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, true, true);
+        when(resultSet.getString("name")).thenReturn("takoyakimchi");
+
+        String result = jdbcTemplate.queryForObject("SELECT name FROM users WHERE id = ?",
+                rs -> rs.getString("name"),
+                parameterStatement -> parameterStatement.setLong(1, 1L)
+        );
+
+        assertThat(result).isEqualTo("takoyakimchi");
+    }
+
+    @DisplayName("쿼리를 실행한다.")
+    @Test
+    void execute() throws SQLException {
+        when(preparedStatement.executeUpdate()).thenReturn(1);
+        SqlFunction<PreparedStatement, Integer> action = PreparedStatement::executeUpdate;
+
+        int result = jdbcTemplate.execute("DELETE FROM users WHERE id = 1", action);
+
+        assertThat(result).isEqualTo(1);
+        verify(preparedStatement).executeUpdate();
+    }
 }
