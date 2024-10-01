80bd85b71f7bc2cd1160fb973dd4ab0edf7e74d4
JUHA <84626225+khabh@users.noreply.github.com>
2024-10-01 10:47:58 +0900

    feat: MappedResultSet 클래스 기능 구현

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/MappedResultSet.java
@@ -0,0 +1,59 @@
+package com.interface21.jdbc.core;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MappedResultSet<T> {
+
+    private static final int NO_LIMIT = -1;
+
+    private final List<T> results;
+
+    public static <T> MappedResultSet<T> create(RowMapper<T> rowMapper, PreparedStatement preparedStatement)
+            throws SQLException {
+        return create(rowMapper, preparedStatement, NO_LIMIT);
+    }
+
+    public static <T> MappedResultSet<T> create(RowMapper<T> rowMapper, PreparedStatement preparedStatement, int limitCount)
+            throws SQLException {
+        List<T> results = new ArrayList<>();
+        AtomicInteger remainingCount = new AtomicInteger(limitCount);
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (next(resultSet, remainingCount)) {
+                results.add(rowMapper.map(resultSet));
+            }
+        }
+
+        return new MappedResultSet<>(results);
+    }
+
+    private static boolean next(ResultSet resultSet, AtomicInteger remainingCount) throws SQLException {
+        if (remainingCount.get() == 0 || !resultSet.next()) {
+            return false;
+        }
+        if (remainingCount.get() != NO_LIMIT) {
+            remainingCount.decrementAndGet();
+        }
+        return true;
+    }
+
+    public MappedResultSet(List<T> results) {
+        this.results = new ArrayList<>(results);
+    }
+
+    public List<T> getResults() {
+        return Collections.unmodifiableList(results);
+    }
+
+    public T getFirst() {
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results.getFirst();
+    }
+}
