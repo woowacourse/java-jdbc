5463b23c16292f3b5ba61379c7695b069750c086
tkdgur0906 <tkdgur0906@naver.com>
2024-10-02 19:23:04 +0900

    feat: JdbcTemplate 데이터 변경 sql 처리하는 update 로직 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -2,8 +2,10 @@ package com.interface21.jdbc.core;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class JdbcTemplate {
 
@@ -13,5 +15,58 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... values) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = getConnection();
+            pstmt = getPreparedStatement(sql, conn);
+
+            log.debug("query : {}", sql);
+
+            assignSqlValues(values, pstmt);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            closePreparedStatement(pstmt);
+            closeConnection(conn);
+        }
+    }
+
+    private Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
+    private PreparedStatement getPreparedStatement(String sql, Connection conn) throws SQLException {
+        return conn.prepareStatement(sql);
+    }
+
+    private void assignSqlValues(Object[] values, PreparedStatement pstmt) throws SQLException {
+        for (int i = 1; i <= values.length; i++) {
+            pstmt.setObject(i, values[i - 1]);
+        }
+    }
+
+    private void closePreparedStatement(PreparedStatement pstmt) {
+        try {
+            if (pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException ignored) {
+        }
+    }
+
+    private void closeConnection(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ignored) {
+        }
     }
 }
