e8092ec21343bc8982dc4e2cd14581bd283aabd0
haiseong <wjdgotjd9908@gmail.com>
2024-10-02 22:08:08 +0900

    feat: JdbcTemplate query 메서드 작성

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -17,5 +23,36 @@ public class JdbcTemplate {
 
     public DataSource getDataSource() {
         return dataSource;
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement ps = connection.prepareStatement(sql)) {
+            setArguments(ps, args);
+            return executeQuery(ps, rowMapper);
+        } catch (final Exception e) {
+            log.error("query error", e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    private void setArguments(PreparedStatement ps, Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            ps.setObject(i + 1, args[i]);
+        }
+    }
+
+    private <T> List<T> executeQuery(PreparedStatement ps, RowMapper<T> rowMapper) throws SQLException {
+        try (ResultSet resultSet = ps.executeQuery()) {
+            return getResults(resultSet, rowMapper);
+        }
+    }
+
+    private <T> List<T> getResults(ResultSet resultSet, RowMapper<T> rowMapper) throws SQLException {
+        List<T> results = new ArrayList<>();
+        while (resultSet.next()) {
+            results.add(rowMapper.mapRow(resultSet, resultSet.getRow()));
+        }
+        return results;
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,50 @@
 package com.interface21.jdbc.core;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private static final RowMapper<String> TEST_ROW_MAPPER = (rs, rowNum) -> "test";
+
+    private final DataSource dataSource = mock(DataSource.class);
+    private final Connection connection = mock(Connection.class);
+    private final PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    private final ResultSet resultSet = mock(ResultSet.class);
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void set() {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Test
+    @DisplayName("여러건 조회 쿼리 수행한다.")
+    void query() throws SQLException {
+        String sql = "select * from test where arg1 = ? and arg2 = ?";
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true).thenReturn(true).thenReturn(false);
+
+        jdbcTemplate.query(sql, TEST_ROW_MAPPER, "arg1", "arg2");
+
+        assertAll(
+                () -> verify(preparedStatement, times(2)).setObject(anyInt(), any()),
+                () -> verify(resultSet, times(3)).next()
+        );
+    }
 }
