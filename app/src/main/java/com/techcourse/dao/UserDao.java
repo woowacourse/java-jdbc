970a044a6e507ca306c05c4910f79310d5e546eb
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-10-03 18:52:45 +0900

    feat(UserDao): UserDao CRUD 기능 추가

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,16 +1,18 @@
 package com.techcourse.dao;
 
-import com.techcourse.domain.User;
+import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.core.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
+import com.techcourse.domain.User;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserDao {
 
@@ -27,95 +29,111 @@ public class UserDao {
     }
 
     public void insert(final User user) {
-        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        final String sql = "insert into users (account, password, email) values (?, ?, ?)";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
+        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            statement.setString(1, user.getAccount());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getEmail());
 
-            log.debug("query : {}", sql);
-
-            pstmt.setString(1, user.getAccount());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getEmail());
-            pstmt.executeUpdate();
+            statement.executeUpdate();
+            log.info("query : {}", sql);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
+            throw new DataAccessException("회원을 저장하는데 실패했습니다. :" + user);
         }
     }
 
     public void update(final User user) {
-        // todo
+        final String sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE id = ?";
+
+        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            statement.setString(1, user.getAccount());
+            statement.setString(2, user.getPassword());
+            statement.setString(3, user.getEmail());
+            statement.setLong(4, user.getId());
+
+            statement.executeUpdate();
+            log.info("query : {}", sql);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException("회원 정보를 업데이트하는데 실패했습니다. :" + user);
+        }
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
+        final String sql = "SELECT id, account, password, email FROM users";
+
+        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            ResultSet resultSet = statement.executeQuery();
+            log.info("query : {}", sql);
+
+            return mapToUsers(resultSet);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException("회원 정보를 조회하는데 실패했습니다.");
+        }
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
+        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            statement.setLong(1, id);
 
-            log.debug("query : {}", sql);
+            ResultSet resultSet = statement.executeQuery();
+            log.info("query : {}", sql);
 
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
+            return mapToUser(resultSet);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
+            throw new DataAccessException("id가" + id + "인 회원 정보를 조회하는데 실패했습니다. :");
         }
     }
 
     public User findByAccount(final String account) {
-        // todo
+        final String sql = "SELECT id, account, password, email FROM users WHERE account = ?";
+
+        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            statement.setString(1, account);
+
+            ResultSet resultSet = statement.executeQuery();
+            log.info("query : {}", sql);
+
+            return mapToUser(resultSet);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException("account가" + account + "인 회원 정보를 조회하는데 실패했습니다. :");
+        }
+    }
+
+    private List<User> mapToUsers(ResultSet resultSet) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while (resultSet.next()) {
+            users.add(mapToUser(resultSet));
+        }
+        return users;
+    }
+
+    private User mapToUser(ResultSet resultSet) throws SQLException {
+        if (resultSet.next()) {
+            return new User(
+                    resultSet.getLong("id"),
+                    resultSet.getString("account"),
+                    resultSet.getString("password"),
+                    resultSet.getString("email")
+            );
+        }
         return null;
     }
 }
