d165c803943f11fa7f2da4fe5ec5a1d8a7aa7816
H <dmsgktn0208@gmail.com>
2024-10-03 16:23:43 +0900

    test: UserRowMapper 테스트 추가

---
+++ app/src/test/java/com/techcourse/dao/mapper/UserRowMapperTest.java
@@ -0,0 +1,33 @@
+package com.techcourse.dao.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.techcourse.domain.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.junit.jupiter.api.Test;
+
+class UserRowMapperTest {
+
+    private static final UserRowMapper userRowMapper = new UserRowMapper();
+
+    @Test
+    void mapRow() throws SQLException {
+        ResultSet resultSet = mock(ResultSet.class);
+        when(resultSet.getLong("id")).thenReturn(1L);
+        when(resultSet.getString("account")).thenReturn("kaki");
+        when(resultSet.getString("password")).thenReturn("1234");
+        when(resultSet.getString("email")).thenReturn("test@example.com");
+
+        User user = userRowMapper.mapRow(resultSet);
+
+        assertNotNull(user);
+        assertEquals(user.getId(), 1L);
+        assertEquals(user.getAccount(), "kaki");
+        assertEquals(user.getPassword(), "1234");
+        assertEquals(user.getEmail(), "test@example.com");
+    }
+}
