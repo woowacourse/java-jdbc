a99ed9d4411a3d2bb9de4c6eab6f11ec0f0f0d5a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-03 22:20:40 +0900

    feat(ResultMapper): ResultSet으로부터 매핑한 객체를 반환하는 객체 구현

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/ResultMapper.java
@@ -0,0 +1,44 @@
+package com.interface21.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ResultMapper {
+
+    public <T> Optional<T> findResult(final ResultSet resultSet, final RowMapper<T> rowMapper) {
+        if (existsNext(resultSet)) {
+            return Optional.ofNullable(getResult(resultSet, rowMapper, 1));
+        }
+        return Optional.empty();
+    }
+
+    public <T> List<T> getResults(final ResultSet resultSet, final RowMapper<T> rowMapper) {
+        int rowNum = 1;
+        List<T> results = new ArrayList<>();
+
+        while (existsNext(resultSet)) {
+            results.add(getResult(resultSet, rowMapper, rowNum++));
+        }
+
+        return results;
+    }
+
+    public <T> T getResult(final ResultSet resultSet, final RowMapper<T> rowMapper, final int rowNum) {
+        try {
+            return rowMapper.mapRow(resultSet, rowNum);
+        } catch (SQLException e) {
+            throw new IllegalStateException("ResultSet을 가져오는 중 에러가 발생했습니다.");
+        }
+    }
+
+    private boolean existsNext(final ResultSet resultSet) {
+        try {
+            return resultSet.next();
+        } catch (Exception e) {
+            throw new IllegalStateException("ResultSet을 가져오는 중 에러가 발생했습니다.");
+        }
+    }
+}
