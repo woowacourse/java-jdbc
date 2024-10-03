a14d63cf06a43a31c2c86b07ef35b49f8e8182bf
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-10-03 21:33:58 +0900

    feat(JdbcTemplate): JdbcTemplate 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,139 +1,57 @@
 package com.techcourse.dao;
 
-import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.core.JdbcTemplate;
+import com.interface21.jdbc.core.RowMapper;
 import com.techcourse.domain.User;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
+    private static final RowMapper<User> USER_ROW_MAPPER = (resultSet -> new User(
+            resultSet.getLong("id"),
+            resultSet.getString("account"),
+            resultSet.getString("password"),
+            resultSet.getString("email")
+    ));
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = null;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public void insert(final User user) {
         final String sql = "insert into users (account, password, email) values (?, ?, ?)";
 
-        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setString(1, user.getAccount());
-            statement.setString(2, user.getPassword());
-            statement.setString(3, user.getEmail());
-
-            statement.executeUpdate();
-            log.info("query : {}", sql);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException("회원을 저장하는데 실패했습니다. :" + user);
-        }
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
         final String sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE id = ?";
 
-        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setString(1, user.getAccount());
-            statement.setString(2, user.getPassword());
-            statement.setString(3, user.getEmail());
-            statement.setLong(4, user.getId());
-
-            statement.executeUpdate();
-            log.info("query : {}", sql);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException("회원 정보를 업데이트하는데 실패했습니다. :" + user);
-        }
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
         final String sql = "SELECT id, account, password, email FROM users";
 
-        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            ResultSet resultSet = statement.executeQuery();
-            log.info("query : {}", sql);
-
-            return mapToUsers(resultSet);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException("회원 정보를 조회하는데 실패했습니다.");
-        }
+        return jdbcTemplate.query(sql, USER_ROW_MAPPER);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
 
-        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setLong(1, id);
-
-            ResultSet resultSet = statement.executeQuery();
-            log.info("query : {}", sql);
-
-            return mapToUser(resultSet);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException("id가" + id + "인 회원 정보를 조회하는데 실패했습니다. :");
-        }
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, id);
     }
 
     public User findByAccount(final String account) {
         final String sql = "SELECT id, account, password, email FROM users WHERE account = ?";
 
-        try (Connection connection = Objects.requireNonNull(dataSource).getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql)
-        ) {
-            statement.setString(1, account);
-
-            ResultSet resultSet = statement.executeQuery();
-            log.info("query : {}", sql);
-
-            return mapToUser(resultSet);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException("account가" + account + "인 회원 정보를 조회하는데 실패했습니다. :");
-        }
-    }
-
-    private List<User> mapToUsers(ResultSet resultSet) throws SQLException {
-        List<User> users = new ArrayList<>();
-        while (resultSet.next()) {
-            users.add(mapToUser(resultSet));
-        }
-        return users;
-    }
-
-    private User mapToUser(ResultSet resultSet) throws SQLException {
-        if (resultSet.next()) {
-            return new User(
-                    resultSet.getLong("id"),
-                    resultSet.getString("account"),
-                    resultSet.getString("password"),
-                    resultSet.getString("email")
-            );
-        }
-        return null;
+        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, account);
     }
 }
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,49 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... args) throws DataAccessException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
+            }
+            statement.executeUpdate();
+            log.info("query : {}", sql);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException("SQL execution failed. : " + sql);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
+            }
+            ResultSet resultSet = statement.executeQuery();
+            log.info("query : {}", sql);
+
+            List<T> result = new ArrayList<>();
+            while (resultSet.next()) {
+                result.add(rowMapper.mapRow(resultSet));
+            }
+            return result;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException("SQL execution failed. : " + sql);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        List<T> result = query(sql, rowMapper, args);
+        if (result.isEmpty()) {
+            return null;
+        }
+        return result.getFirst();
     }
 }
