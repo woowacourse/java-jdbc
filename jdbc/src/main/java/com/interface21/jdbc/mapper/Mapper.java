e5b7a7839ff4f7f85f57f59988dd664e9a23954e
woojong <dwax1324@gmail.com>
2024-10-04 03:14:36 +0900

    feat: 매퍼 클래스 구현

--- jdbc/src/main/java/com/interface21/jdbc/mapper/Mapper.java
+++ jdbc/src/main/java/com/interface21/jdbc/mapper/Mapper.java
@@ -4,52 +4,56 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Mapper {
 
-    public static <T> T queryResolver(Class<T> clazz, String sql, ResultSet rs) throws SQLException {
-        if (!rs.next()) {
+    public static <T> List<T> queryResolver(Class<T> clazz, String sql, ResultSet rs) throws SQLException {
+        if (rs.isAfterLast()) {
             return null;
         }
 
         try {
-            T instance = clazz.getConstructor().newInstance();
-
             int from = sql.indexOf("from");
             List<String> fieldNames = Arrays.stream(sql.substring(6, from).trim().split(","))
                     .map(String::trim)
                     .toList();
+            final List<T> result = new ArrayList<>();
 
             while (rs.next()) {
+                T instance = clazz.getConstructor().newInstance();
                 for (String fieldName : fieldNames) {
-                    Field field = clazz.getDeclaredField(fieldName);
-                    Object resultSetValue = rs.getObject(getLableName(fieldName));
+                    String columnName = toColumnName(fieldName);
 
-                    System.out.println("@@" + field.getName() + "@@");
+                    Field field = clazz.getDeclaredField(fieldName);
+                    Object columnValue = rs.getObject(columnName);
+
                     field.setAccessible(true);
-                    field.set(instance, resultSetValue);
+                    field.set(instance, columnValue);
                     field.setAccessible(false);
                 }
+                result.add(instance);
+            }
+            for (var name : result) {
+                System.out.println(name.toString());
             }
 
-            return instance;
+            return result;
         } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException |
                  NoSuchFieldException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static String getLableName(String s) {
+    private static String toColumnName(String s) {
         StringBuilder sb = new StringBuilder(s);
         for (int i = 1; i < sb.length(); i++) {
             if (Character.isUpperCase(sb.charAt(i))) {
                 sb.insert(i, "_");
             }
         }
-        System.out.println("@@" + sb.toString() + "@@");
         return sb.toString().toLowerCase();
     }
-
 }
