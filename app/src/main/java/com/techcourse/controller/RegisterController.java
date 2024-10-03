30ff88a62dff78f743cf54cd5b0cd8788c9f5491
eun-byeol <joeunbyeol98@gmail.com>
2024-10-03 17:20:54 +0900

    refactor: app 패키지에서 H2 DB 사용하도록 변경
    - Controller에서 UserService 주입
    - UserService에서 UserDaoJdbcImpl 주입

--- app/src/main/java/com/techcourse/controller/RegisterController.java
+++ app/src/main/java/com/techcourse/controller/RegisterController.java
@@ -1,17 +1,30 @@
 package com.techcourse.controller;
 
-import com.techcourse.domain.User;
-import com.techcourse.repository.InMemoryUserRepository;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import com.interface21.webmvc.servlet.view.JspView;
-import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.context.stereotype.Controller;
+import com.interface21.jdbc.core.JdbcTemplate;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
+import com.interface21.webmvc.servlet.ModelAndView;
+import com.interface21.webmvc.servlet.view.JspView;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.dao.UserDaoJdbcImpl;
+import com.techcourse.dao.UserHistoryDaoJdbcImpl;
+import com.techcourse.domain.User;
+import com.techcourse.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 public class RegisterController {
+
+    private final UserService userService;
+
+    public RegisterController() {
+        userService = new UserService(
+                new UserDaoJdbcImpl(new JdbcTemplate(DataSourceConfig.getInstance())),
+                new UserHistoryDaoJdbcImpl(new JdbcTemplate(DataSourceConfig.getInstance()))
+        );
+    }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
     public ModelAndView register(final HttpServletRequest request, final HttpServletResponse response) {
@@ -19,7 +32,7 @@ public class RegisterController {
                 request.getParameter("account"),
                 request.getParameter("password"),
                 request.getParameter("email"));
-        InMemoryUserRepository.save(user);
+        userService.insert(user);
 
         return new ModelAndView(new JspView("redirect:/index.jsp"));
     }
