3fbcefbca6ed55b437b1a59d7893af96401e026c
H <dmsgktn0208@gmail.com>
2024-10-03 16:19:32 +0900

    test: 테이블 데이터 정리 클래스 추가

---
+++ app/src/test/java/com/techcourse/support/DataBaseCleaner.java
@@ -0,0 +1,20 @@
+package com.techcourse.support;
+
+import com.interface21.jdbc.core.JdbcTemplate;
+
+public class DataBaseCleaner {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public DataBaseCleaner(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void resetTable(String tableName) {
+        String truncateSql = String.format("TRUNCATE TABLE %s;", tableName);
+        jdbcTemplate.update(truncateSql);
+
+        String resetAutoIncrementSql = String.format("ALTER TABLE %s ALTER COLUMN ID RESTART WITH 1;", tableName);
+        jdbcTemplate.update(resetAutoIncrementSql);
+    }
+}
