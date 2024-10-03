9f83d1ef92750d1b32c3c27ff1e5fd6d89063673
takoyakimchi <c4nyou@gmail.com>
2024-10-03 23:50:38 +0900

    refactor: TestUser 분리

--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -15,6 +15,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import support.TestDataSourceConfig;
+import support.TestUser;
 
 class JdbcTemplateTest {
 
@@ -134,52 +135,6 @@ class JdbcTemplateTest {
             statement.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    private static class TestUser {
-
-        private Long id;
-        private String name;
-        private int age;
-
-        private TestUser() {
-        }
-
-        public TestUser(Long id, String name, int age) {
-            this.id = id;
-            this.name = name;
-            this.age = age;
-        }
-
-        public Long getId() {
-            return id;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            TestUser testUser = (TestUser) o;
-            return age == testUser.age && Objects.equals(id, testUser.id) && Objects.equals(name,
-                    testUser.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, name, age);
         }
     }
 }
