9ee3cd9bb21d52d5893fa97a252a965a2f2097b6
woojong <dwax1324@gmail.com>
2024-10-04 05:16:42 +0900

    test: mockstatic을 한번만 호출하도록 변경

--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -11,32 +11,37 @@ import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
 class JdbcTemplateTest {
 
-    DataSource source = mock(DataSource.class);
-    ResultSet resultSet;
+    private DataSource source;
+    private ResultSet resultSet;
 
     private JdbcTemplate jdbcTemplate;
 
+    @BeforeAll
+    static void beforeAll() {
+        mockStatic(Mapper.class);
+    }
+
     @BeforeEach
     void setUp() throws SQLException {
+        source = mock(DataSource.class);
+        resultSet = mock(ResultSet.class);
         Connection connection = mock(Connection.class);
         PreparedStatement preparedStatement = mock(PreparedStatement.class);
-        resultSet = mock(ResultSet.class);
-        when(resultSet.next()).thenReturn(false);
+
         when(source.getConnection()).thenReturn(connection);
         when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
         when(preparedStatement.executeQuery()).thenReturn(resultSet);
-        mockStatic(Mapper.class);
 
         jdbcTemplate = new JdbcTemplate(source);
     }
+
 
     @DisplayName("단건 조회시 데이터가 없다면 null을 반환한다.")
     @Test
@@ -47,7 +52,7 @@ class JdbcTemplateTest {
     @DisplayName("단건 조회시 데이터가 두 개 이상이라면 예외가 발생한다.")
     @Test
     void queryDuplicatedData() throws SQLException {
-        String sql = "select id from user";
+        String sql = "select id from users";
         when(Mapper.doQueryMapping(String.class, sql, resultSet)).thenReturn(List.of("one", "two"));
 
         Assertions.assertThatThrownBy(() -> jdbcTemplate.query(String.class, sql))
@@ -58,7 +63,7 @@ class JdbcTemplateTest {
     @DisplayName("리스트로 조회시 데이터가 없다면 빈리스트가 반환된다.")
     @Test
     void queryForAllEmptyList() throws SQLException {
-        String sql = "select id from user";
+        String sql = "select id from users";
 
         when(Mapper.doQueryMapping(String.class, sql, resultSet)).thenReturn(List.of());
 
