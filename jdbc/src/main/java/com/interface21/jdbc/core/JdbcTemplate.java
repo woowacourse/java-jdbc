f5b5261fb801f29212b6520e907e59b6ec005f5f
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-10-04 18:26:40 +0900

    refactor(JdbcTemplate): try-with-resource 사용

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -26,95 +26,63 @@ public class JdbcTemplate {
     public int update(String sql, Object... arguments) {
         log.debug("update query : {}", sql);
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             int count = 1;
             for (Object argument : arguments) {
                 pstmt.setObject(count, argument);
                 count++;
             }
-
             return pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            closeResources(pstmt, conn);
         }
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... arguments) {
         log.debug("query : {}", sql);
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             int count = START_ARGUMENT_COUNT;
             for (Object argument : arguments) {
                 pstmt.setObject(count, argument);
                 count++;
             }
-
-            rs = pstmt.executeQuery();
-            List<T> objects = new ArrayList<>();
-            while (rs.next()) {
-                objects.add(rowMapper.mapRow(rs, rs.getFetchSize()));
+            try (ResultSet rs = pstmt.executeQuery()) {
+                List<T> objects = new ArrayList<>();
+                while (rs.next()) {
+                    objects.add(rowMapper.mapRow(rs, rs.getFetchSize()));
+                }
+                return objects;
             }
-            return objects;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            closeResources(rs, pstmt, conn);
         }
     }
 
     public <T> T queryObject(String sql, RowMapper<T> rowMapper, Object... arguments) {
         log.debug("queryObject query : {}", sql);
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             int count = 1;
             for (Object argument : arguments) {
                 pstmt.setObject(count, argument);
                 count++;
             }
 
-            rs = pstmt.executeQuery();
-            if (rs.next()) {
-                return rowMapper.mapRow(rs, rs.getFetchSize());
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return rowMapper.mapRow(rs, rs.getFetchSize());
+                }
+                return null;
             }
-            return null;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            closeResources(rs, pstmt, conn);
-        }
-    }
-
-    private void closeResources(AutoCloseable... closeables) {
-        for (AutoCloseable closeable : closeables) {
-            try {
-                if (closeable != null) {
-                    closeable.close();
-                }
-            } catch (Exception e) {
-                log.error("There was a problem while closing. ", e);
-            }
         }
     }
 }
