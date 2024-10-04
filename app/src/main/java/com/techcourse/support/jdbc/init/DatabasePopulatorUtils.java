8924fd40b5d33e5e2ed6cced177ba105ab0af9b2
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-04 20:59:06 +0900

    fix: schema 경로를 찾지 못하는 문제 해결

--- app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
+++ app/src/main/java/com/techcourse/support/jdbc/init/DatabasePopulatorUtils.java
@@ -1,26 +1,26 @@
 package com.techcourse.support.jdbc.init;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatabasePopulatorUtils {
 
+    private static final String SCHEMA_PATH = "src/main/resources/schema.sql";
     private static final Logger log = LoggerFactory.getLogger(DatabasePopulatorUtils.class);
 
     public static void execute(final DataSource dataSource) {
         Connection connection = null;
         Statement statement = null;
         try {
-            final var url = DatabasePopulatorUtils.class.getClassLoader().getResource("schema.sql");
-            final var file = new File(url.getFile());
+            final var path = Paths.get(SCHEMA_PATH);
+            final var file = path.toFile();
             final var sql = Files.readString(file.toPath());
             connection = dataSource.getConnection();
             statement = connection.createStatement();
