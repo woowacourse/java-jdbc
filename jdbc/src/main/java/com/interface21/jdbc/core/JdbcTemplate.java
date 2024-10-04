dff336097cfe27eac4b2a4c9f5e0612e20c7459e
Nam Gi Beom <rlqja0523@naver.com>
2024-10-05 01:00:53 +0900

    feat: JdbcTemplate 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,16 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.EmptyResultDataAccessException;
+import com.interface21.dao.IncorrectResultSizeDataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,63 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(String sql, Object... args) {
+        return execute(sql, pstmt -> {
+            setValues(pstmt, args);
+            return pstmt.executeUpdate();
+        });
+    }
+
+    private void setValues(PreparedStatement pstmt, Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        return execute(sql, pstmt -> {
+            setValues(pstmt, args);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return mapRows(rs, rowMapper);
+            }
+        });
+    }
+
+    private <T> List<T> mapRows(ResultSet rs, RowMapper<T> rowMapper) throws SQLException {
+        List<T> results = new ArrayList<>();
+        while (rs.next()) {
+            results.add(rowMapper.mapRow(rs));
+        }
+        return results;
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        List<T> results = query(sql, rowMapper, args);
+        if (results.isEmpty()) {
+            throw new EmptyResultDataAccessException(1);
+        }
+        if (results.size() > 1) {
+            throw new IncorrectResultSizeDataAccessException(1, results.size());
+        }
+        return results.getFirst();
+    }
+
+    private <T> T execute(String sql, PreparedStatementCallBack<T> callBack) {
+        validateSql(sql);
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            return callBack.doInPreparedStatement(pstmt);
+        } catch (SQLException e) {
+            throw new RuntimeException(e); // todo
+        }
+    }
+
+    private void validateSql(String sql) {
+        if (sql.isBlank()) {
+            throw new IllegalArgumentException("sql must not be blank");
+        }
+        log.debug("Executing prepared SQL statement : [ {} ]", sql);
     }
 }
