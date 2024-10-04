a41ee31a2fb971d42f0838b22a160f8b2ca7a78c
eunjungL <dldmswjd322@gmail.com>
2024-10-04 16:31:12 +0900

    refactor: 사용하지 않는 import, 테스트 제거

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,14 +1,12 @@
 package com.techcourse.dao;
 
-import com.interface21.jdbc.core.JdbcTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
-import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class UserDaoTest {
 
