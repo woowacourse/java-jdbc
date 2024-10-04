e987f3458b47cf615c4828e0fe88e51f45eeef80
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-04 17:34:28 +0900

    feat: jdbcTemplate 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,13 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +17,26 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public ResultSet execute(String sql, String... parameters) throws SQLException {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql);
+        ) {
+            resolvePrepareStatement(pstmt, parameters);
+            return pstmt.executeQuery();
+        } catch (Exception exception) {
+            throw new DataAccessException(exception);
+        }
+    }
+
+    private void resolvePrepareStatement(PreparedStatement pstmt, String... parameters) {
+        try {
+            for (int i = 0; i < parameters.length; i++) {
+                pstmt.setString(i + 1, parameters[i]);
+            }
+        } catch (SQLException sqlException) {
+            throw new DataAccessException("sql 구성 과정에서 문제가 발생했습니다.", sqlException);
+        }
     }
 }
