cf6d7598b08182acbe7805f8379190fecd85ccc8
hangillee <skfcb10@naver.com>
2024-10-04 17:20:23 +0900

    refactor: 중복된 RowMapper 생성 로직 메소드로 추출

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,45 +1,37 @@
 package com.techcourse.dao;
 
-import com.techcourse.config.DataSourceConfig;
-import com.techcourse.domain.User;
 import com.interface21.jdbc.core.JdbcTemplate;
-import java.util.ArrayList;
+import com.interface21.jdbc.core.RowMapper;
+import com.techcourse.domain.User;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.List;
-import org.springframework.jdbc.core.RowMapper;
 
 public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
-    private final DataSource dataSource;
     private final JdbcTemplate jdbcTemplate;
 
     public UserDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = DataSourceConfig.getInstance();
         this.jdbcTemplate = jdbcTemplate;
     }
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        log.info("user: {}", user);
 
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
         final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        log.info("update user: {}", user.getId());
 
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
@@ -47,33 +39,28 @@ public class UserDao {
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
 
-        return jdbcTemplate.query(sql, (rs, rowNum) -> new User(
+        return jdbcTemplate.query(sql, getUserRowMapper());
+    }
+
+    private RowMapper<User> getUserRowMapper() {
+        return (rs, rowNum) -> new User(
                 rs.getLong(1),
                 rs.getString(2),
                 rs.getString(3),
-                rs.getString(4))
-        );
+                rs.getString(4));
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
+        log.info("find user by id: {}", id);
 
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new User(
-                rs.getLong(1),
-                rs.getString(2),
-                rs.getString(3),
-                rs.getString(4)
-        ), id);
+        return jdbcTemplate.queryForObject(sql, getUserRowMapper(), id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
+        log.info("find user by account: {}", account);
 
-        return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new User(
-                rs.getLong(1),
-                rs.getString(2),
-                rs.getString(3),
-                rs.getString(4)
-        ), account);
+        return jdbcTemplate.queryForObject(sql, getUserRowMapper(), account);
     }
 }
