9f92e6a28304e07f5682be3d55fb044cfc0c2da8
le2sky <leehaneul990623@gmail.com>
2024-10-05 17:03:15 +0900

    refactor: jdbctemplate 가독성 개선

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,7 +1,9 @@
 package com.interface21.jdbc.core;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
@@ -18,55 +20,73 @@ public class JdbcTemplate {
     }
 
     public <T> T queryOne(String sql, ResultSetCallBack<T> callBack, Object... args) {
-        log.debug("query : {}", sql);
-        try (var connection = dataSource.getConnection(); var pstmt = connection.prepareStatement(sql)) {
-            int index = 1;
-            for (Object arg : args) {
-                pstmt.setObject(index++, arg);
-            }
+        debugQuery(sql);
 
-            ResultSet rs = pstmt.executeQuery();
-            if (rs.next()) {
-                T result = callBack.callback(rs);
-                rs.close();
-                return result;
-            }
+        try (var conn = dataSource.getConnection(); var pstm = conn.prepareStatement(sql)) {
+            return executeQueryOne(callBack, pstm, args);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-            rs.close();
-        } catch (Exception ignored) {
+    private <T> T executeQueryOne(ResultSetCallBack<T> callBack, PreparedStatement pstm, Object... args) throws SQLException {
+        setArg(args, pstm);
+        ResultSet rs = pstm.executeQuery();
+
+        T result = null;
+        if (rs.next()) {
+            result = callBack.callback(rs);
         }
 
-        return null;
+        rs.close();
+        return result;
     }
 
     public <T> List<T> query(String sql, ResultSetCallBack<T> callBack, Object... args) {
-        log.debug("query : {}", sql);
+        debugQuery(sql);
+
+        try (var conn = dataSource.getConnection(); var pstm = conn.prepareStatement(sql)) {
+            return executeQuery(callBack, pstm, args);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> List<T> executeQuery(ResultSetCallBack<T> callBack, PreparedStatement pstm, Object... args) throws SQLException {
+        setArg(args, pstm);
+        ResultSet rs = pstm.executeQuery();
+
         List<T> results = new ArrayList<>();
-
-        try (var connection = dataSource.getConnection(); var pstmt = connection.prepareStatement(sql)) {
-            int index = 1;
-            for (Object arg : args) {
-                pstmt.setObject(index++, arg);
-            }
-
-            ResultSet rs = pstmt.executeQuery();
-            while (rs.next()) {
-                results.add(callBack.callback(rs));
-            }
-
-            rs.close();
-        } catch (Exception ignored) {
+        while (rs.next()) {
+            results.add(callBack.callback(rs));
         }
 
+        rs.close();
         return results;
     }
 
-    public void update(String sql, PreparedStatementCallBack callBack) {
-        log.debug("query : {}", sql);
-        try (var connection = dataSource.getConnection(); var pstmt = connection.prepareStatement(sql)) {
-            callBack.callback(pstmt);
-            pstmt.executeUpdate();
-        } catch (Exception ignored) {
+    private void setArg(Object[] args, PreparedStatement pstm) throws SQLException {
+        int index = 1;
+        for (Object arg : args) {
+            pstm.setObject(index++, arg);
         }
+    }
+
+    public void update(String sql, PreparedStatementCallBack callBack) {
+        debugQuery(sql);
+
+        try (var connection = dataSource.getConnection(); var pstm = connection.prepareStatement(sql)) {
+            executeUpdate(callBack, pstm);
+        } catch (SQLException ignored) {
+        }
+    }
+
+    private void executeUpdate(PreparedStatementCallBack callBack, PreparedStatement pstm) throws SQLException {
+        callBack.callback(pstm);
+        pstm.executeUpdate();
+    }
+
+    private void debugQuery(String sql) {
+        log.debug("query : {}", sql);
     }
 }
