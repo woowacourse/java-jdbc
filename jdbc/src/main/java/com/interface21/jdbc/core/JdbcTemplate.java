f2ec4630d18e418e5c853545341db678c3a238e0
hangillee <coderoad.io@gmail.com>
2024-10-06 00:24:48 +0900

    refactor: 단일 조회를 위한 검증 로직 추가 및 메소드 인덴트 개선

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,19 +1,23 @@
 package com.interface21.jdbc.core;
 
 import com.interface21.dao.DataAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    public static final String QUERY_RESULT_EXCEPTION_MESSAGE = "Incorrect result size: expected 1, actual ";
+    public static final int EMPTY_COUNT = 0;
+    public static final int MAXIMUM_QUERY_FOR_OBJECT_RESULT = 1;
 
     private final DataSource dataSource;
 
@@ -35,9 +39,9 @@ public class JdbcTemplate {
         }
     }
 
-    private void setPreparedStatement(PreparedStatement pstmt, Object[] args) throws SQLException {
-        for (int i = 0; i < args.length; i++) {
-            pstmt.setObject(i + 1, args[i]);
+    private void setPreparedStatement(final PreparedStatement pstmt, final Object[] args) throws SQLException {
+        for (int i = EMPTY_COUNT; i < args.length; i++) {
+            pstmt.setObject(i + MAXIMUM_QUERY_FOR_OBJECT_RESULT, args[i]);
         }
     }
 
@@ -48,16 +52,19 @@ public class JdbcTemplate {
         ) {
             log.debug("query : {}", sql);
 
-            List<T> results = new ArrayList<>();
-
-            while (rs.next()) {
-                results.add(rowMapper.mapRow(rs, rs.getRow()));
-            }
-            return results;
+            return extractData(rowMapper, rs);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
         }
+    }
+
+    private <T> List<T> extractData(RowMapper<T> rowMapper, ResultSet rs) throws SQLException {
+        List<T> results = new ArrayList<>();
+        while (rs.next()) {
+            results.add(rowMapper.mapRow(rs, rs.getRow()));
+        }
+        return results;
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, Object... args) {
@@ -65,21 +72,29 @@ public class JdbcTemplate {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)
         ) {
-            setPreparedStatement(pstmt, args);
-            rs = pstmt.executeQuery();
-
             log.debug("query : {}", sql);
 
-            if (rs.next()) {
-                return rowMapper.mapRow(rs, rs.getRow());
-            }
-            return null;
+            setPreparedStatement(pstmt, args);
+            rs = pstmt.executeQuery();
+            List<T> results = extractData(rowMapper, rs);
+
+            return getSingleResult(results);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
         } finally {
             closeResultSet(rs);
         }
+    }
+
+    private <T> T getSingleResult(final List<T> results) throws SQLException {
+        if (results.isEmpty()) {
+            throw new DataAccessException(QUERY_RESULT_EXCEPTION_MESSAGE + EMPTY_COUNT);
+        }
+        if (results.size() > MAXIMUM_QUERY_FOR_OBJECT_RESULT) {
+            throw new DataAccessException(QUERY_RESULT_EXCEPTION_MESSAGE + results.size());
+        }
+        return results.getFirst();
     }
 
     private void closeResultSet(ResultSet rs) {
