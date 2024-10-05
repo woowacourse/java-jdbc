b4c0cb2013c0a3d7c9cbd945af3c9d18ed6d6f14
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-06 01:25:38 +0900

    feat: userHistory에 jdbcTemplate 적용

---
+++ app/src/main/java/com/techcourse/dao/rowmapper/UserHistoryRowMapper.java
@@ -0,0 +1,30 @@
+package com.techcourse.dao.rowmapper;
+
+import com.interface21.jdbc.core.RowMapper;
+import com.techcourse.domain.UserHistory;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class UserHistoryRowMapper implements RowMapper<UserHistory> {
+
+    @Override
+    public UserHistory mapRow(ResultSet resultSet) {
+        try {
+            if (resultSet.next()) {
+                return new UserHistory(
+                        resultSet.getLong(1),
+                        resultSet.getLong(2),
+                        resultSet.getString(3),
+                        resultSet.getString(4),
+                        resultSet.getString(5),
+                        resultSet.getObject(6, LocalDateTime.class),
+                        resultSet.getString(7)
+                );
+            }
+            return null;
+        } catch (SQLException sqlException) {
+            throw new RuntimeException("UserHistory를 mapping하는 과정에서 문제가 발생했습니다.", sqlException);
+        }
+    }
+}
---
+++ app/src/test/java/com/techcourse/dao/UserHistoryDaoTest.java
@@ -0,0 +1,53 @@
+package com.techcourse.dao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.interface21.jdbc.core.JdbcTemplate;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.dao.rowmapper.UserHistoryRowMapper;
+import com.techcourse.domain.User;
+import com.techcourse.domain.UserHistory;
+import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserHistoryDaoTest {
+
+    private UserHistoryDao userHistoryDao;
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() {
+        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
+        this.jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
+        this.userHistoryDao = new UserHistoryDao(jdbcTemplate);
+    }
+
+    @AfterEach
+    void tearDown() {
+        jdbcTemplate.queryForUpdate("DELETE FROM user_history");
+        jdbcTemplate.queryForUpdate("ALTER TABLE user_history ALTER COLUMN id RESTART WITH 1");
+    }
+
+    @DisplayName("새로운 유저 기록을 DB에 저장할 수 있다")
+    @Test
+    void log() {
+        User user = new User(1L, "loki", "password", "hkkang@woowahan.com");
+        UserHistory history = new UserHistory(user, "coli");
+
+        userHistoryDao.log(history);
+
+        UserHistory savedHistory = (UserHistory) jdbcTemplate.queryForObject(
+                "select * from user_history where user_id = ?",
+                new UserHistoryRowMapper(),
+                1L
+        );
+
+        assertThat(history)
+                .usingRecursiveComparison()
+                .ignoringFields("id", "createdAt")
+                .isEqualTo(savedHistory);
+    }
+}
