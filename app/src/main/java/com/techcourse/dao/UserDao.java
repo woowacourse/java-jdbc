043a741ba3ebaa8c84c1278c0f1a1ec0c3d6e094
lemone <lejinhy@gmail.com>
2024-10-05 14:49:47 +0900

    test: 테스트 메서드 격리를 위해 setUp에 데이터 초기화 로직 추가

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -70,4 +70,9 @@ public class UserDao {
         );
         return result;
     }
+
+    public void reset() {
+        final var sql = "truncate table users restart identity";
+        dataSource.update(sql);
+    }
 }
