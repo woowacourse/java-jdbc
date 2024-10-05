8571e455ad8c2ecee21aa9f77223657857a691e8
BurningFalls <sherlocky31415@gmail.com>
2024-10-05 10:26:23 +0900

    refactor: 미사용 변수 및 import 삭제

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,22 +1,11 @@
 package com.techcourse.dao;
 
-import com.techcourse.domain.User;
-import com.interface21.jdbc.core.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-public class UserDao {
+import com.interface21.jdbc.core.JdbcTemplate;
+import com.techcourse.domain.User;
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
+public class UserDao {
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -26,13 +15,13 @@ public class UserDao {
 
     public void insert(final User user) {
         final var query = "insert into users (account, password, email) values (?, ?, ?)";
-        Object[] parameters = { user.getAccount(), user.getPassword(), user.getEmail() };
+        Object[] parameters = {user.getAccount(), user.getPassword(), user.getEmail()};
         jdbcTemplate.executeUpdate(query, parameters);
     }
 
     public void update(final User user) {
         final var query = "update users set account = ?, password = ?, email = ?";
-        Object[] parameters = { user.getAccount(), user.getPassword(), user.getEmail() };
+        Object[] parameters = {user.getAccount(), user.getPassword(), user.getEmail()};
         jdbcTemplate.executeUpdate(query, parameters);
     }
 
@@ -48,7 +37,7 @@ public class UserDao {
 
     public User findById(final Long id) {
         final var query = "select id, account, password, email from users where id = ?";
-        Object[] parameters = { id };
+        Object[] parameters = {id};
         List<User> users = jdbcTemplate.executeQuery(query, resultSet -> new User(
                 resultSet.getLong("id"),
                 resultSet.getString("account"),
@@ -62,7 +51,7 @@ public class UserDao {
 
     public User findByAccount(final String account) {
         final var query = "select * from users where account = ?";
-        Object[] parameters = { account };
+        Object[] parameters = {account};
         List<User> users = jdbcTemplate.executeQuery(query, resultSet -> new User(
                 resultSet.getLong("id"),
                 resultSet.getString("account"),
