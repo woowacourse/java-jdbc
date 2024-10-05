e7315714340ad113d42380d91c50da39963881f2
Minsu Kim <alstn113@gmail.com>
2024-10-05 21:59:17 +0900

    [1단계 - JDBC 라이브러리 구현하기] 구름(김민수) 미션 제출합니다. (#614)
    * feat: RowMapper 작성
    
    * feat: jdbcTemplate query 작성
    
    * feat: jdbcTemplate queryForObject 작성
    
    * feat: jdbcTemplate update 작성
    
    * refactor: userDao에 jdbcTemplate 적용
    
    * fix: javax -> jakarta
    
    * refactor: callback, setter 분리
    
    * refactor: user dao 개선
    
    * refactor: user dao test 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,17 @@
 package com.interface21.jdbc.core;
 
+import javax.annotation.Nullable;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +21,78 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(String sql, @Nullable PreparedStatementSetter pss, RowMapper<T> rowMapper) {
+        return execute(sql, pstmt -> {
+            List<T> results = new ArrayList<>();
+            if (pss != null) {
+                pss.setValues(pstmt);
+            }
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs));
+                }
+            }
+            return results;
+        });
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        return query(sql, new SimplePreparedStatementSetter(args), rowMapper);
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        return query(sql, null, rowMapper);
+    }
+
+    public <T> T queryForObject(String sql, @Nullable PreparedStatementSetter pss, RowMapper<T> rowMapper) {
+        List<T> results = query(sql, pss, rowMapper);
+        if (results.isEmpty()) {
+            throw new DataAccessException("Empty Result");
+        }
+        if (results.size() > 1) {
+            throw new DataAccessException("Incorrect Result Size");
+        }
+        return results.getFirst();
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        return queryForObject(sql, new SimplePreparedStatementSetter(args), rowMapper);
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper) {
+        return queryForObject(sql, null, rowMapper);
+    }
+
+    public int update(String sql, PreparedStatementSetter pss) {
+        return execute(sql, pstmt -> {
+            if (pss != null) {
+                pss.setValues(pstmt);
+            }
+            return pstmt.executeUpdate();
+        });
+    }
+
+    public int update(String sql, Object... args) {
+        return update(sql, new SimplePreparedStatementSetter(args));
+    }
+
+    private <T> T execute(String sql, PreparedStatementCallback<T> callback) throws DataAccessException {
+        Connection con = getConnection();
+        try (PreparedStatement pstmt = con.prepareStatement(sql)) {
+            return callback.doInStatement(pstmt);
+        } catch (SQLException e) {
+            throw new DataAccessException("execute error", e);
+        } finally {
+            DataSourceUtils.releaseConnection(con, dataSource);
+        }
+    }
+
+    private Connection getConnection() {
+        Connection con = DataSourceUtils.getConnection(dataSource);
+        log.debug("Connection : {}, Class : {}", con, con.getClass());
+        return con;
     }
 }
