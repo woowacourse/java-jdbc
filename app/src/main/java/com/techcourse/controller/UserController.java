99a22d2d0f45c1795fb146c0de794ce5ded6f7f3
eun-byeol <joeunbyeol98@gmail.com>
2024-10-06 00:17:16 +0900

    refactor: UserController에서 UserService 구현체 변경

--- app/src/main/java/com/techcourse/controller/UserController.java
+++ app/src/main/java/com/techcourse/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.techcourse.controller;
 
 import com.interface21.context.stereotype.Controller;
-import com.interface21.jdbc.core.LegacyJdbcTemplate;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
 import com.interface21.webmvc.servlet.view.JsonView;
 import com.techcourse.config.DataSourceConfig;
-import com.techcourse.dao.LegacyUserDaoImpl;
-import com.techcourse.dao.LegacyUserHistoryDaoImpl;
+import com.techcourse.dao.UserDaoImpl;
+import com.techcourse.dao.UserHistoryDaoImpl;
 import com.techcourse.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -24,8 +23,8 @@ public class UserController {
 
     public UserController() {
         userService = new UserService(
-                new LegacyUserDaoImpl(new LegacyJdbcTemplate(DataSourceConfig.getInstance())),
-                new LegacyUserHistoryDaoImpl(new LegacyJdbcTemplate(DataSourceConfig.getInstance()))
+                new UserDaoImpl(DataSourceConfig.getInstance()),
+                new UserHistoryDaoImpl(DataSourceConfig.getInstance())
         );
     }
 
