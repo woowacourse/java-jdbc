3e0d8e12fad7ba784674a71e6b525e0ffa170fe8
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-06 16:22:18 +0900

    refactor: 다양한 파라미터 setting을 지원하도록 setObject로 변경

--- jdbc/src/test/java/com/interface21/jdbc/core/PreparedStatementResolverTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/PreparedStatementResolverTest.java
@@ -7,26 +7,38 @@ import static org.mockito.Mockito.mock;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.ArgumentCaptor;
 
 class PreparedStatementResolverTest {
 
     @DisplayName("PreparedStatement에 파라미터 값을 세팅해준다")
-    @Test
-    void resolve() throws SQLException {
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+    @MethodSource("provideParameters")
+    @ParameterizedTest
+    void resolve(Object parameter1, Object parameter2) throws SQLException {
+        ArgumentCaptor<Object> captor = ArgumentCaptor.forClass(Object.class);
         PreparedStatementResolver resolver = new PreparedStatementResolver();
         PreparedStatement statement = mock(PreparedStatement.class);
 
         doNothing()
                 .when(statement)
-                .setString(anyInt(), captor.capture());
+                .setObject(anyInt(), captor.capture());
 
-        resolver.resolve(statement, "test1", "test2", "test3");
+        resolver.resolve(statement, parameter1, parameter2);
 
         assertThat(captor.getAllValues())
-                .containsExactly("test1", "test2", "test3");
+                .containsExactly(parameter1, parameter2);
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of("test1", "test2"),
+                Arguments.of(1L, 2L),
+                Arguments.of(new Object(), new Object())
+        );
     }
 }
