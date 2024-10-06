523703d8dd10eace652fa8e0a4cfd41fe44a1fd8
devhoya97 <diajbh123@gmail.com>
2024-10-06 18:02:54 +0900

    refactor: findAll 시 jdbcTemplate 활용

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -2,7 +2,10 @@ package com.interface21.jdbc.core;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,9 +32,7 @@ public class JdbcTemplate {
 
             log.debug("query : {}", sql);
 
-            for (int i = 0; i < parameters.length; i++) {
-                setParameter(i + PARAMETER_INDEX_OFFSET, pstmt, parameters[i]);
-            }
+            setParameters(pstmt, parameters);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
@@ -51,6 +52,12 @@ public class JdbcTemplate {
         }
     }
 
+    private void setParameters(PreparedStatement pstmt, Object[] parameters) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            setParameter(i + PARAMETER_INDEX_OFFSET, pstmt, parameters[i]);
+        }
+    }
+
     private void setParameter(int parameterIndex, PreparedStatement pstmt, Object parameter) throws SQLException {
         if (parameter instanceof String) {
             pstmt.setString(parameterIndex, (String) parameter);
@@ -63,7 +70,47 @@ public class JdbcTemplate {
         throw new IllegalArgumentException("준비되지 않은 파라미터 타입입니다. class = " + parameter.getClass().getCanonicalName());
     }
 
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            setParameters(pstmt, parameters);
+            rs = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            List<T> found = new ArrayList<>();
+            if (rs.next()) {
+                found.add(rowMapper.mapRow(rs));
+            }
+            return found;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {}
+        }
+    }
+
     // TODO: queryForObject() 구현
 
-    // TODO: query() 구현
 }
