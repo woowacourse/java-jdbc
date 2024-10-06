2b7ac3d38763115125f427e070940f0b2591962a
jongmee <ra123221@gmail.com>
2024-10-06 19:52:13 +0900

    feat: JdbcTemplate

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class JdbcTemplate {
 
@@ -11,7 +16,36 @@ public class JdbcTemplate {
 
     private final DataSource dataSource;
 
-    public JdbcTemplate(final DataSource dataSource) {
+    public JdbcTemplate(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... params) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            bindStatementParameters(preparedStatement, params);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> T query(String sql, ResultSetMapper<T> resultSetMapper, Object... params) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            bindStatementParameters(preparedStatement, params);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSetMapper.map(resultSet);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void bindStatementParameters(PreparedStatement preparedStatement, Object[] params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            preparedStatement.setObject(i + 1, params[i]);
+        }
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,85 @@
 package com.interface21.jdbc.core;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 class JdbcTemplateTest {
 
+    private DataSource dataSource = mock(DataSource.class);
+    private Connection connection = mock(Connection.class);
+    private PreparedStatement preparedStatement = mock(PreparedStatement.class);
+    private ResultSet resultSet = mock(ResultSet.class);
+
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        when(dataSource.getConnection()).thenReturn(connection);
+    }
+
+    @Test
+    void update() throws Exception {
+        // given
+        String sql = "insert into users (account, password, email) values (?, ?, ?)";
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+
+        // when
+        jdbcTemplate.update(sql, "mia", "password", "mia@gmail.com");
+
+        // then
+        verify(preparedStatement).setObject(1, "mia");
+        verify(preparedStatement).setObject(2, "password");
+        verify(preparedStatement).setObject(3, "mia@gmail.com");
+        verify(preparedStatement).executeUpdate();
+    }
+
+    @Test
+    void query() throws Exception {
+        // given
+        String sql = "select id, account, password, email from users where id = ?";
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(connection.prepareStatement(sql)).thenReturn(preparedStatement);
+
+        when(resultSet.next()).thenReturn(true).thenReturn(false);
+        when(resultSet.getLong("id")).thenReturn(1l);
+        when(resultSet.getString("account")).thenReturn("mia");
+
+        ResultSetMapper<TestUser> resultSetMapper = rs -> {
+            if (rs.next()) {
+                return new TestUser(rs.getLong("id"), rs.getString("account"));
+            }
+            return null;
+        };
+
+        // when
+        TestUser queriedTestUser = jdbcTemplate.query(sql, resultSetMapper, 1l);
+
+        // then
+        assertAll(() -> {
+            assertThat(queriedTestUser.id).isEqualTo(1);
+            assertThat(queriedTestUser.account).isEqualTo("mia");
+        });
+    }
+
+    private static class TestUser {
+        private Long id;
+        private String account;
+
+        public TestUser(Long id, String account) {
+            this.id = id;
+            this.account = account;
+        }
+    }
 }
