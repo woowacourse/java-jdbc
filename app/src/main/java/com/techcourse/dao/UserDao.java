4a704fc961fa9a7cec5de19e10a3f6749aefa062
lemone <lejinhy@gmail.com>
2024-10-06 19:21:38 +0900

    refactor: 변수명 변경 및 값 변경이 없는 경우 변수 추출 생략

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -3,14 +3,11 @@ package com.techcourse.dao;
 import com.interface21.jdbc.core.RowMapper;
 import com.techcourse.domain.User;
 import com.interface21.jdbc.core.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
     private static final RowMapper<User> ROW_MAPPER = (rs, rowNum) -> new User(
             rs.getLong("id"),
             rs.getString("account"),
@@ -18,42 +15,39 @@ public class UserDao {
             rs.getString("email")
     );
 
-    private final JdbcTemplate dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = jdbcTemplate;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-        dataSource.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
         final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
-        dataSource.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
-        final List<User> result = dataSource.query(sql, ROW_MAPPER);
-        return result;
+        return jdbcTemplate.query(sql, ROW_MAPPER);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        final User result = dataSource.queryForObject(sql, ROW_MAPPER, id);
-        return result;
+        return jdbcTemplate.queryForObject(sql, ROW_MAPPER, id);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-        final User result = dataSource.queryForObject(sql, ROW_MAPPER, account);
-        return result;
+        return jdbcTemplate.queryForObject(sql, ROW_MAPPER, account);
     }
 
     public void reset() {
         final var sql = "truncate table users restart identity";
-        dataSource.update(sql);
+        jdbcTemplate.update(sql);
     }
 }
