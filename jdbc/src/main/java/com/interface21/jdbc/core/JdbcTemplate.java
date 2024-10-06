8c58eae466653be7de77ba7ceb54a27cd16bcaae
masonkimseoul <masonkimseoul@gmail.com>
2024-10-06 14:28:16 +0900

    feat: UserDao에서 JdbcTemplate로 insert 로직 이동

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,8 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +16,26 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int executeUpdate(String sql, Object... values) {
+        try {
+            Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            log.debug("query : {}", sql);
+
+            setQueryParameter(pstmt, values);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setQueryParameter(PreparedStatement statement, Object... values) throws SQLException {
+        for(int i = 0; i < values.length; i++) {
+            statement.setObject(i + 1, values[i]);
+        }
     }
 }
