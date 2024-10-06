ac40bcd83a7305d22d9b0b9aa4a0e3ae4b0251da
eunjungL <62099953+eunjungL@users.noreply.github.com>
2024-10-07 01:13:01 +0900

    [1단계 - JDBC 라이브러리 구현하기] 클로버(이은정) 미션 제출합니다. (#616)
    * feat: JdbcTemplate query method 구현
    
    * feat: JDBCTemplate 자원 close 구현
    
    * feat: DataAccessException 추가
    
    * feat: JdbcTemplate 한 개의 결과에 대한 query 메서드 구현
    
    * feat: JdbcTemplate update 메서드 구현
    
    * refactor: UserDao 레거시 코드 제거 및 JdbcTemplate 사용하게 변경
    
    * refactor: JdbcTemplate PreparedStatement에 파라미터 세팅하는 메서드 분리
    
    * refactor: JdbcTemplate PreparedStatement에 파라미터 세팅하는 메서드 분리
    
    * refactor: Connection, PreparedStatment 자동 반환되게 try with resource문으로 변경
    
    * refactor: 사용하지 않는 import, 테스트 제거
    
    * refactor: JdbcTemplate을 인자로 받는 UserDao를 주생성자로 활용하게 변경
    
    * refactor: JdbcTemplate에서 배열 인자 모두 가변 인자로 변경
    
    * refactor: JdbcTemplate Parameter Index 상수화
    
    * test: JdbcTemplate 테스트 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,13 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,10 +16,86 @@ import javax.sql.DataSource;
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int PARAMETER_START_INDEX = 1;
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    private Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            log.error("GET_CONNECTION_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException("Connection을 생성하던 중 오류가 발생했습니다.");
+        }
+    }
+
+    private PreparedStatement getPreparedStatement(Connection connection, String sql) {
+        try {
+            return connection.prepareStatement(sql);
+        } catch (SQLException e) {
+            log.error("GET_PREPARED_STATEMENT_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException("PreparedStatement를 생성하던 중 오류가 발생했습니다.");
+        }
+    }
+
+    private void setPreparedStatementParameter(Object[] args, PreparedStatement preparedStatement) {
+        try {
+            for (int parameterIndex = PARAMETER_START_INDEX; parameterIndex <= args.length; parameterIndex++) {
+                preparedStatement.setObject(parameterIndex, args[parameterIndex - PARAMETER_START_INDEX]);
+            }
+        } catch (SQLException e) {
+            log.info("SET_PREPARED_STATEMENT_PARAMETER_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException("PreparedStatement에 파라미터를 설정하던 중 오류가 발생했습니다.");
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = getPreparedStatement(connection, sql)
+        ) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet, resultSet.getRow()));
+            }
+
+            return results;
+        } catch (SQLException e) {
+            log.error("EXECUTE_QUERY_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException(sql + "을 실행하던 중 오류가 발생했습니다.");
+        }
+    }
+
+    public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object ...args) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = getPreparedStatement(connection, sql)
+        ) {
+            setPreparedStatementParameter(args, preparedStatement);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                return Optional.of(rowMapper.mapRow(resultSet, resultSet.getRow()));
+            }
+            return Optional.empty();
+        } catch (SQLException e) {
+            log.info("EXECUTE_QUERY_FOR_OBJECT_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException(sql + "을 실행하던 중 오류가 발생했습니다.");
+        }
+    }
+
+    public int update(String sql, Object ...args) {
+        try (Connection connection = getConnection();
+             PreparedStatement preparedStatement = getPreparedStatement(connection, sql)){
+            setPreparedStatementParameter(args, preparedStatement);
+            return preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.info("EXECUTE_UPDATE_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException(sql + "을 실행하던 중 오류가 발생했습니다.");
+        }
     }
 }
