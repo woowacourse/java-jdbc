65473e4749758ae9be3adfe7f67c24dedd800fda
Eugene Jang <kyunellroll@gmail.com>
2024-10-06 22:11:54 +0900

    feat(JdbcTemplate): JdbcTemplate 수정
    - 잘 작동하는지 JdbcTemplate 작성

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -4,14 +4,84 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final int INIT_ROW_NUMBER = 0;
+    private static final int INIT_PARAMETER_INDEX = 1;
+    private static final int REQUIRED_DATA_SIZE = 1;
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(String sql, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            setStatement(args, statement);
+            int rows = statement.executeUpdate();
+            connection.commit();
+
+            return rows;
+        } catch (SQLException e) {
+            log.error("error : {}", e.getMessage(), e);
+            throw new IllegalArgumentException("데이터베이스 연결에 실패했습니다.");
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+            setStatement(args, statement);
+            ResultSet resultSet = statement.executeQuery();
+            return extractData(resultSet, rowMapper);
+        } catch (SQLException e) {
+            log.error("error : {}", e.getMessage(), e);
+            throw new IllegalArgumentException("데이터베이스 연결에 실패했습니다.");
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        List<T> data = query(sql, rowMapper, args);
+        return makeSingleResult(data);
+    }
+
+    private void setStatement(Object[] args, PreparedStatement statement) throws SQLException {
+        for (int index = 0; index < args.length; index++) {
+            statement.setObject(index + INIT_PARAMETER_INDEX, args[index]);
+        }
+    }
+
+    private <T> List<T> extractData(ResultSet resultSet, RowMapper<T> rowMapper) throws SQLException {
+        List<T> results = new ArrayList<>();
+        int rowNumber = INIT_ROW_NUMBER;
+
+        while (resultSet.next()) {
+            results.add(rowMapper.mapRow(resultSet, rowNumber++));
+        }
+
+        return results;
+    }
+
+    private <T> T makeSingleResult(Collection<T> data) {
+        if (data.isEmpty()) {
+            return null;
+        }
+        if (data.size() > REQUIRED_DATA_SIZE) {
+            throw new IllegalArgumentException("데이터가 여러개 존재합니다.");
+        }
+        return data.iterator().next();
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,81 @@
 package com.interface21.jdbc.core;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.*;
+
 class JdbcTemplateTest {
 
+    @Mock
+    private Connection connection;
+
+    @Mock
+    private DataSource dataSource;
+
+    @Mock
+    private ResultSet resultSet;
+
+    @Mock
+    private RowMapper<String> rowMapper;
+
+    @Mock
+    private PreparedStatement preparedStatement;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        MockitoAnnotations.openMocks(this);
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+
+        when(preparedStatement.executeUpdate()).thenReturn(1);
+
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(false);
+    }
+
+    @Test
+    @DisplayName("sql 문과 arg를 넣어 데이터베이스에 데이터의 상태를 변화 할 수 있다.")
+    void update() {
+        String sql = """
+                INSERT INTO users (account, password, email)
+                VALUES (?,?,?)
+                """;
+
+        int rowCount = jdbcTemplate.update(sql, "account", "password", "email");
+
+        assertAll(
+                () -> assertThat(rowCount).isEqualTo(1),
+                () -> verify(connection, atLeastOnce()).commit()
+        );
+    }
+
+    @Test
+    @DisplayName("sql 문과 rowMapper 그리고 필요한 값을 넣어 데이터베이스에서 필요한 값을 List로 가져올 수 있다.")
+    void query() {
+        String sql = """
+                SELECT id, account, password, email
+                FROM users
+                """;
+
+        List<String> expected = jdbcTemplate.query(sql, rowMapper);
+
+        assertThat(expected).isInstanceOf(List.class);
+    }
 }
