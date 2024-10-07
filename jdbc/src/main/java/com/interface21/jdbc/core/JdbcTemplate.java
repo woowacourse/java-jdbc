5943a5ee77a0c81fa065638bf63de86d827d4537
Wonjun Choi(타칸) <78288539+jhon3242@users.noreply.github.com>
2024-10-07 21:03:58 +0900

    [JDBC 라이브러리 구현하기 - 1단계] 타칸(최원준) 미션 제출합니다. (#650)
    * feat: JdbcTemplate 적용
    
    * test: JdbcTemplate 테스트 추가
    
    * feat: 컨벤션 통일

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.jdbc.CannotGetJdbcConnectionException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,64 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public Connection getConnection(){
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new CannotGetJdbcConnectionException(e.getMessage(), e);
+        }
+    }
+
+    public void update(String sql, Object... params){
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)){
+            setStatement(pstmt, params);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatement(pstmt, params);
+            ResultSet rs = pstmt.executeQuery();
+            log.debug("query : {}", sql);
+            if (rs.next()) {
+                return rowMapper.mapRow(rs);
+            }
+            throw new RuntimeException("no record found");
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (Connection conn = getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatement(pstmt, params);
+            ResultSet rs = pstmt.executeQuery();
+            log.debug("query : {}", sql);
+            List<T> result = new ArrayList<>();
+            while (rs.next()) {
+                result.add(rowMapper.mapRow(rs));
+            }
+            return result;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void setStatement(PreparedStatement pstmt, Object[] params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            pstmt.setObject(i + 1, params[i]);
+        }
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,115 @@
 package com.interface21.jdbc.core;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    JdbcTemplate jdbcTemplate;
+    PreparedStatement pstm;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        DataSource dataSource = mock(DataSource.class);
+        Connection connection = mock(Connection.class);
+        pstm = mock(PreparedStatement.class);
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(pstm);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Test
+    void update가_정상_작동한다() throws SQLException {
+        // given
+        String sql = "INSERT INTO user (id, name) VALUES (?, ?)";
+
+        // when
+        jdbcTemplate.update(sql, 1L, "userA");
+
+        // thenR
+        verify(pstm).setObject(1, 1L);
+        verify(pstm).setObject(2, "userA");
+    }
+
+    @Test
+    void query가_정상_작동한다() throws SQLException {
+        // given
+        ResultSet rs = mock(ResultSet.class);
+        when(pstm.executeQuery()).thenReturn(rs);
+        when(rs.next()).thenReturn(true, false);
+        when(rs.getLong("id")).thenReturn(1L);
+        when(rs.getString("name")).thenReturn("userA");
+
+        // when
+        List<TestUser> users = jdbcTemplate.query("SELECT id, name WHERE id = ?", testUserRowMapper(), 1L);
+
+        // then
+        Assertions.assertAll(
+                () -> assertThat(users).hasSize(1),
+                () -> assertThat(users.get(0)).isEqualTo(new TestUser(1L, "userA"))
+        );
+    }
+
+    @Test
+    void queryForObject가_정상_작동한다() throws SQLException {
+        // given
+        ResultSet rs = mock(ResultSet.class);
+        when(pstm.executeQuery()).thenReturn(rs);
+        when(rs.next()).thenReturn(true, false);
+        when(rs.getLong("id")).thenReturn(1L);
+        when(rs.getString("name")).thenReturn("userA");
+
+        // when
+        TestUser user = jdbcTemplate.queryForObject("SELECT id, name WHERE id = ?", testUserRowMapper(), 1L);
+
+        // then
+        assertThat(user).isEqualTo(new TestUser(1L, "userA"));
+    }
+
+    class TestUser {
+        private Long id;
+        private String name;
+
+        public TestUser(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) {
+                return true;
+            }
+            if (object == null || getClass() != object.getClass()) {
+                return false;
+            }
+            TestUser testUser = (TestUser) object;
+            return Objects.equals(id, testUser.id) && Objects.equals(name, testUser.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+    }
+
+    private RowMapper<TestUser> testUserRowMapper() {
+        return (rs) -> {
+            return new TestUser(rs.getLong("id"), rs.getString("name"));
+        };
+    }
 }
