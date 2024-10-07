42d195707a32d6e4625590338992e16d7f6d1d8a
takoyakimchi <c4nyou@gmail.com>
2024-10-07 17:55:13 +0900

    test: UserServiceTest 테스트 메서드 추가

--- app/src/test/java/com/techcourse/service/UserServiceTest.java
+++ app/src/test/java/com/techcourse/service/UserServiceTest.java
@@ -1,36 +1,84 @@
 package com.techcourse.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
-import com.interface21.dao.DataAccessException;
-import com.interface21.jdbc.core.JdbcTemplate;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class UserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
     private UserDao userDao;
 
+    private UserService userService;
+
     @BeforeEach
     void setUp() {
         this.jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
         this.userDao = new UserDao();
+        this.userService = new UserService(userDao, new UserHistoryDao());
 
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(user);
     }
 
+    @AfterEach
+    void tearDown() {
+        jdbcTemplate.executeUpdate("""
+                delete from users;
+                alter table users alter column id restart with 1;
+                """);
+    }
+
+    @DisplayName("성공: User 저장 후 id로 조회")
     @Test
-    void testChangePassword() {
+    void insert_findById() {
+        userService.insert(new User("tre", "pass123", "a@a.com"));
+
+        User user = userService.findById(2L)
+                .orElseThrow();
+
+        assertAll(
+                () -> assertThat(user.getId()).isEqualTo(2L),
+                () -> assertThat(user.getAccount()).isEqualTo("tre"),
+                () -> assertThat(user.getPassword()).isEqualTo("pass123"),
+                () -> assertThat(user.getEmail()).isEqualTo("a@a.com")
+        );
+    }
+
+    @DisplayName("성공: User 저장 후 findByAccount로 조회")
+    @Test
+    void insert_findByAccount() {
+        userService.insert(new User("tre", "pass123", "a@a.com"));
+
+        User user = userService.findByAccount("tre")
+                .orElseThrow();
+
+        assertAll(
+                () -> assertThat(user.getId()).isEqualTo(2L),
+                () -> assertThat(user.getAccount()).isEqualTo("tre"),
+                () -> assertThat(user.getPassword()).isEqualTo("pass123"),
+                () -> assertThat(user.getEmail()).isEqualTo("a@a.com")
+        );
+    }
+
+    @DisplayName("성공: 비밀번호 변경")
+    @Test
+    void changePassword() {
         final var userHistoryDao = new UserHistoryDao();
         final var userService = new UserService(userDao, userHistoryDao);
 
@@ -41,6 +89,19 @@ class UserServiceTest {
         final var actual = userService.findById(1L).get();
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
+    }
+
+    @DisplayName("실패: 존재하지 않는 사용자가 비밀번호 변경")
+    @Test
+    void changePassword_InvalidUser() {
+        final var userHistoryDao = new UserHistoryDao();
+        final var userService = new UserService(userDao, userHistoryDao);
+
+        final var newPassword = "qqqqq";
+        final var createBy = "gugu";
+
+        assertThatThrownBy(() -> userService.changePassword(2L, newPassword, createBy))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
