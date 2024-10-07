d89aac8ce777a77160c1731e38ad474d13689122
Gyeongho Yang <chchch1213@naver.com>
2024-10-07 11:25:31 +0900

    refactor: use field instead of method for mapper

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,9 +1,8 @@
 package com.techcourse.dao;
 
 import com.interface21.jdbc.core.JdbcTemplate;
+import com.interface21.jdbc.core.RowMapper;
 import com.techcourse.domain.User;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 import javax.sql.DataSource;
@@ -13,6 +12,13 @@ import org.slf4j.LoggerFactory;
 public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
+
+    private final RowMapper<User> mapper = (rs, rowNum) -> new User(
+            rs.getInt("id"),
+            rs.getString("account"),
+            rs.getString("password"),
+            rs.getString("email")
+    );
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -36,25 +42,16 @@ public class UserDao {
 
     public List<User> findAll() {
         String sql = "SELECT * FROM users";
-        return jdbcTemplate.query(sql, this::rowMapper);
+        return jdbcTemplate.query(sql, mapper);
     }
 
     public Optional<User> findById(final Long id) {
         final var sql = "SELECT * FROM users WHERE id = ?";
-        return jdbcTemplate.queryForObject(sql, this::rowMapper, id);
+        return jdbcTemplate.queryForObject(sql, mapper, id);
     }
 
     public Optional<User> findByAccount(final String account) {
         final var sql = "SELECT * FROM users WHERE account = ?";
-        return jdbcTemplate.queryForObject(sql, this::rowMapper, account);
-    }
-
-    private User rowMapper(ResultSet rs, int rowNum) throws SQLException {
-        return new User(
-                rs.getInt("id"),
-                rs.getString("account"),
-                rs.getString("password"),
-                rs.getString("email")
-        );
+        return jdbcTemplate.queryForObject(sql, mapper, account);
     }
 }
