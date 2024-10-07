939813a9a7dc7de79da0052000c6ed0a80bc5605
정회성 <ghltjd3@gmail.com>
2024-10-07 10:00:00 +0900

    [jdbc 구현하기] 초롱 1단계 제출합니다. (#643)
    * feat: UserDaoTest 통과하도록 UserDao 각 메소드 기능 구현
    
    * feat: 쿼리를 처리하는 로직을 JdbcTemplate에서 구현
    
    * test: jdbcTemplate 테스트 코드 작성
    
    * refactor: 불필요한 코드 제거
    
    * refactor: import문 정리
    
    * feat: queryForObject의 결과값이 여러개인 경우에 대한 예외처리 및 테스트 코드 작성
    
    * refactor: PreparedStatementCallback 함수형 인터페이스 분리
    
    * refactor: RuntimeException 대신 DataAccessException을 사용하도록 변경

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,19 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
+import com.interface21.dao.DataAccessException;
+import com.interface21.dao.IncorrectResultSizeException;
 
 public class JdbcTemplate {
 
@@ -13,5 +23,46 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
+        return execute(
+                sql,
+                pstmt -> {
+                    try (ResultSet rs = pstmt.executeQuery()) {
+                        List<T> results = new ArrayList<>();
+                        while (rs.next()) {
+                            results.add(rowMapper.map(rs));
+                        }
+                        return results;
+                    }
+                },
+                params
+        );
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
+        List<T> results = query(sql, rowMapper, params);
+        if (results.size() > 1) {
+            throw new IncorrectResultSizeException(1, results.size());
+        }
+        return results.getFirst();
+    }
+
+    public int update(String sql, Object... params) {
+        return execute(sql, PreparedStatement::executeUpdate, params);
+    }
+
+    private <T> T execute(String sql, PreparedStatementCallback<T> action, Object... params) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+            return action.doInPreparedStatement(pstmt);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,209 @@
 package com.interface21.jdbc.core;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.interface21.dao.IncorrectResultSizeException;
+
 class JdbcTemplateTest {
+
+    private static final RowMapper<User> ROW_MAPPER = (rs) -> new User(
+            rs.getLong("id"),
+            rs.getString("name"),
+            rs.getInt("age"));
+
+    @Mock
+    private DataSource dataSource;
+    @Mock
+    private Connection connection;
+    @Mock
+    private PreparedStatement preparedStatement;
+    @Mock
+    private ResultSet resultSet;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        MockitoAnnotations.openMocks(this);
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Nested
+    @DisplayName("query")
+    class Query {
+        @Test
+        @DisplayName("리스트를 반환한다.")
+        void returnsList() throws SQLException {
+            // given
+            when(preparedStatement.executeQuery()).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(true, true, false);
+            when(resultSet.getInt("age")).thenReturn(20, 25);
+
+            // when
+            List<User> users = jdbcTemplate.query(
+                    "select * from users where age = ?",
+                    ROW_MAPPER,
+                    18
+            );
+
+            // then
+            verify(preparedStatement).executeQuery();
+            verify(preparedStatement).setObject(1, 18);
+            assertThat(users).hasSize(2);
+            assertThat(users.get(0).getAge()).isEqualTo(20);
+            assertThat(users.get(1).getAge()).isEqualTo(25);
+        }
+
+        @Test
+        @DisplayName("파라미터를 전달하지 않은 경우에도 동작한다.")
+        void withoutParam() throws SQLException {
+            // given
+            when(preparedStatement.executeQuery()).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(true, true, false);
+            when(resultSet.getString("name")).thenReturn("user 1", "user 2");
+
+            // when
+            List<User> users = jdbcTemplate.query(
+                    "select * from users",
+                    ROW_MAPPER
+            );
+
+            // then
+            verify(preparedStatement).executeQuery();
+            verify(preparedStatement, never()).setObject(anyInt(), anyString());
+            assertThat(users).hasSize(2);
+            assertThat(users.get(0).getName()).isEqualTo("user 1");
+            assertThat(users.get(1).getName()).isEqualTo("user 2");
+        }
+    }
+
+    @Nested
+    @DisplayName("queryForObject")
+    class QueryForObject {
+        @Test
+        @DisplayName("객체를 반환한다.")
+        void returnObject() throws SQLException {
+            // given
+            when(preparedStatement.executeQuery()).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(true, false);
+            when(resultSet.getString("name")).thenReturn("user 1");
+
+            // when
+            User user = jdbcTemplate.queryForObject(
+                    "select * from users where id = ?",
+                    ROW_MAPPER,
+                    1
+            );
+
+            // then
+            verify(preparedStatement).executeQuery();
+            verify(preparedStatement).setObject(1, 1);
+            assertThat(user.getName()).isEqualTo("user 1");
+        }
+
+        @Test
+        @DisplayName("파라미터를 전달하지 않은 경우에도 동작한다.")
+        void withoutParam() throws SQLException {
+            // given
+            when(preparedStatement.executeQuery()).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(true, false);
+            when(resultSet.getString("name")).thenReturn("user 1");
+
+            // when
+            User user = jdbcTemplate.queryForObject(
+                    "select * from users where id = 1",
+                    ROW_MAPPER
+            );
+
+            // then
+            verify(preparedStatement).executeQuery();
+            verify(preparedStatement, never()).setObject(anyInt(), anyInt());
+            assertThat(user.getName()).isEqualTo("user 1");
+        }
+
+        @Test
+        @DisplayName("결과값이 여러개인 경우 IncorrectResultSizeException을 던진다.")
+        void incorrectResultSizeException() throws SQLException {
+            // given
+            when(preparedStatement.executeQuery()).thenReturn(resultSet);
+            when(resultSet.next()).thenReturn(true, true, false);
+            when(resultSet.getString("name")).thenReturn("user 1", "user 2");
+
+            // when & then
+            assertThatThrownBy(() -> jdbcTemplate.queryForObject("select * from users where id = ?", ROW_MAPPER, 1))
+                    .isInstanceOf(IncorrectResultSizeException.class)
+                    .hasMessage("Expected: 1, but actual: 2");
+        }
+    }
+
+    @Test
+    @DisplayName("update 관련 쿼리 테스트")
+    void update() throws SQLException {
+        // given
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, false);
+
+        // when
+        jdbcTemplate.update(
+                "insert into users (account, password, email) values (?, ?, ?)",
+                "account",
+                "password",
+                "email@email.com");
+
+        // then
+        verify(preparedStatement).executeUpdate();
+        verify(preparedStatement).setObject(1, "account");
+        verify(preparedStatement).setObject(2, "password");
+        verify(preparedStatement).setObject(3, "email@email.com");
+    }
+
+    private static class User {
+
+        private Long id;
+        private String name;
+        private int age;
+
+        public User(long id, String name, int age) {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+
+        public long getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+    }
 
 }
