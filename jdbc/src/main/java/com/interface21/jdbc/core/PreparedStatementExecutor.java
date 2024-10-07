dfaf0ce9475fcd581f2035adaff598737d2046e0
eun-byeol <joeunbyeol98@gmail.com>
2024-10-07 21:12:20 +0900

    refactor: 함수형 인터페이스 어노테이션 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementExecutor.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementExecutor.java
@@ -3,6 +3,7 @@ package com.interface21.jdbc.core;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementExecutor<T> {
 
     T apply(PreparedStatement preparedStatement) throws SQLException;
