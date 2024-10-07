52926d60ddde0713acea761d8f818a5c884dcd36
nhlee98 <e5slnh9876@gmail.com>
2024-10-07 16:26:11 +0900

    refactor: 다른 sql 쿼리문과 형식 통일

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -29,11 +29,7 @@ public class UserDao {
     }
 
     public void update(final User user) {
-        final var sql = """
-                UPDATE users
-                SET account = ?, password = ?, email = ?
-                WHERE users.id = ?;
-                """;
+        final var sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE users.id = ?";
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
