149d8e9c89edbee174429444761f55daa0d006dd
yoonseo choi <choiyoonseo02@gmail.com>
2024-10-07 22:26:03 +0900

    [1단계 - JDBC 라이브러리 구현하기] 릴리(최윤서) 미션 제출합니다. (#626)
    * feat: jdbc 라이브러리 구현 미션
    
    * feat: userHistoryDao가 jdbc 라이브러리를 사용하도록 수정
    
    * refactor: 조회 column 표기방식 일관되게 수정
    
    * refactor: queryForObject 예외처리
    
    * refactor: 메서드 분리
    
    * test: jdbcTemplateTest 추가
    
    * refactor: test케이스 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package com.interface21.jdbc.core;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import com.interface21.dao.DataAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,55 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        List<T> result = query(sql, rowMapper, parameters);
+        if (result.isEmpty()) {
+            throw new DataAccessException("조회된 데이터가 없습니다.");
+        }
+        if (result.size() > 1) {
+            throw new DataAccessException("조회된 데이터가 두 건 이상입니다.");
+        }
+        return result.getFirst();
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        return execute(sql, pstmt -> {
+            setObject(pstmt, parameters);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return mapResults(rs, rowMapper);
+            }
+        });
+    }
+
+    private <T> List<T> mapResults(ResultSet rs, RowMapper<T> rowMapper) throws SQLException {
+        List<T> result = new ArrayList<>();
+        while (rs.next()) {
+            result.add(rowMapper.mapRow(rs));
+        }
+        return result;
+    }
+
+    public void update(String sql, Object... parameters) {
+        execute(sql, pstmt -> {
+            setObject(pstmt, parameters);
+            return pstmt.executeUpdate();
+        });
+    }
+
+    private <T> T execute(String sql, PreparedStatementExecutor<T> executor) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            return executor.execute(pstmt);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setObject(PreparedStatement pstmt, Object... parameters) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            pstmt.setObject(i + 1, parameters[i]);
+        }
     }
 }
