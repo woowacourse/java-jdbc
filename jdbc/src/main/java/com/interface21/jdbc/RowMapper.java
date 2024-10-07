98abbe90149053e5cb0676c8da34372f6d296dd0
slimsha2dy <chy2581@naver.com>
2024-10-07 14:47:28 +0900

    feat: RowMapper 인터페이스 구현

---
+++ jdbc/src/main/java/com/interface21/jdbc/RowMapper.java
@@ -0,0 +1,9 @@
+package com.interface21.jdbc;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public interface RowMapper<T> {
+
+    T mapRow(ResultSet rs, int rowNum) throws SQLException;
+}
