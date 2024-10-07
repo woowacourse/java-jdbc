ddf371969cd6cdfe0716864eadecf287adb8d402
Jeong Jiho <jeongjiho0731@gmail.com>
2024-10-07 21:04:52 +0900

    [1단계 - JDBC 라이브러리 구현하기] 땡이(정지호) 미션 제출합니다. (#615)
    * feat: JdbcTemplate 클래스로 DB 사용 코드 중복 제거
    
    * refactor: try-with-resources 구문으로 대체
    
    * refactor: insert 메서드 제거
    
    * refactor: rowMapper에서 columnLabel을 사용하도록 변경
    
    * refactor: 사용하지 않는 코드 제거
    
    * refactor: rowMapper 상수 추출
    
    * refactor: 다수의 조회 조건을 수용할 수 있도록 변경, Object 타입의 인자를 입력받도록 변경

---
+++ app/src/main/java/com/techcourse/dao/UserRowMapper.java
@@ -0,0 +1,21 @@
+package com.techcourse.dao;
+
+import com.interface21.jdbc.core.RowMapper;
+import com.techcourse.domain.User;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserRowMapper implements RowMapper {
+
+    public User mapRow(final ResultSet resultSet) {
+        try {
+            return new User(
+                    resultSet.getLong("id"),
+                    resultSet.getString("account"),
+                    resultSet.getString("password"),
+                    resultSet.getString("email"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,62 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper rowMapper, final Object... conditions) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            for (int argumentIndex = 0; argumentIndex < conditions.length; argumentIndex++) {
+                preparedStatement.setObject(argumentIndex + 1, conditions[argumentIndex]);
+            }
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return rowMapper.mapRow(resultSet);
+                }
+                return null;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper rowMapper) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()
+        ) {
+            log.debug("query : {}", sql);
+
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet));
+            }
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void update(final String sql, final Object... arguments) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            for (int argumentIndex = 0; argumentIndex < arguments.length; argumentIndex++) {
+                preparedStatement.setObject(argumentIndex + 1, arguments[argumentIndex]);
+            }
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
