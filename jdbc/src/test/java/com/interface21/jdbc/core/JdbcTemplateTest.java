0936b2a752ebd4040c1293261b7f0fef13130fa0
nhlee98 <e5slnh9876@gmail.com>
2024-10-07 17:12:08 +0900

    test: JdbcTemplate의 테스트 코드 추가

--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -2,7 +2,6 @@ package com.interface21.jdbc.core;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
@@ -11,6 +10,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
@@ -46,6 +47,15 @@ class JdbcTemplateTest {
     }
 
     @Test
+    @DisplayName("update 가 정상적으로 변경한 row의 개수를 반환한다.")
+    void updateTest() throws SQLException {
+        String sql = "INSERT INTO users (name) VALUES (?)";
+        when(preparedStatement.executeUpdate()).thenReturn(1);
+
+        assertThat(jdbcTemplate.update(sql, "naknak")).isEqualTo(1);
+    }
+
+    @Test
     @DisplayName("queryForObject의 결과가 존재하지 않으면 예외가 발생한다.")
     void queryForObjectTestWithNoResult() throws SQLException {
         String sql = "SELECT * FROM user WHERE name = ?";
@@ -65,8 +75,7 @@ class JdbcTemplateTest {
         AtomicLong id = new AtomicLong(1L);
         RowMapper<User> rowMapper = rs -> new User(id.getAndIncrement(), "naknak");
 
-        when(resultSet.next())
-                .thenReturn(true, true, false);
+        when(resultSet.next()).thenReturn(true, true, false);
 
         assertThatThrownBy(() -> jdbcTemplate.queryForObject(sql, rowMapper, "naknak"))
                 .isInstanceOf(DataAccessException.class)
@@ -77,17 +86,29 @@ class JdbcTemplateTest {
     @DisplayName("queryForObject 결과가 1개면 정상적으로 객체를 반환한다.")
     void queryForObjectTest() throws SQLException {
         String sql = "SELECT * FROM user WHERE name = ?";
-        RowMapper<User> rowMapper = rs -> new User(1L, "naknak");
+        User expected = new User(1L, "naknak");
+        RowMapper<User> rowMapper = rs -> expected;
 
-        when(resultSet.next())
-                .thenReturn(true, false);
+        when(resultSet.next()).thenReturn(true, false);
 
-        User user = jdbcTemplate.queryForObject(sql, rowMapper, "naknak");
+        User actual = jdbcTemplate.queryForObject(sql, rowMapper, "naknak");
 
-        assertAll(
-                () -> assertThat(user.getId()).isEqualTo(1L),
-                () -> assertThat(user.getName()).isEqualTo("naknak")
-        );
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("query 가 정상적으로 객체들을 반환한다.")
+    void queryTest() throws SQLException {
+        String sql = "SELECT * FROM user";
+        List<User> expected = List.of(new User(1L, "naknak"), new User(2L, "liv"));
+        AtomicInteger index = new AtomicInteger(0);
+        RowMapper<User> rowMapper = rs -> expected.get(index.getAndIncrement());
+
+        when(resultSet.next()).thenReturn(true, true, false);
+
+        List<User> actual = jdbcTemplate.query(sql, rowMapper);
+
+        assertThat(actual).containsExactlyElementsOf(expected);
     }
 
 
@@ -99,6 +120,7 @@ class JdbcTemplateTest {
             this.id = id;
             this.name = name;
         }
+
 
         public Long getId() {
             return id;
