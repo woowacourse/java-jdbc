c577d9e6ed9eb050d1704e7447ee5705c59a7258
3juhwan <13selfesteem91@naver.com>
2024-10-08 00:57:38 +0900

    test: RowMapperFactory 캐싱 테스트

--- jdbc/src/test/java/com/interface21/jdbc/core/RowMapperFactoryTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/RowMapperFactoryTest.java
@@ -9,10 +9,12 @@ import static org.mockito.Mockito.when;
 import com.interface21.jdbc.support.TestDomain;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RowMapperFactoryTest {
 
+    @DisplayName("적절한 RowMapper를 생성한다.")
     @Test
     void getRowMapperTest() throws SQLException {
         // given
@@ -22,12 +24,25 @@ class RowMapperFactoryTest {
 
         // when
         RowMapper<TestDomain> rowMapper = RowMapperFactory.getRowMapper(TestDomain.class);
-        TestDomain mappedDomain = rowMapper.mapRow(resultSet);
+        TestDomain mappedDomain = rowMapper.mapRow(resultSet, 0);
 
         // then
         assertAll(
                 () -> assertThat(mappedDomain.getName()).isEqualTo("망쵸"),
                 () -> assertThat(mappedDomain.getAge()).isEqualTo(2L)
         );
+    }
+
+    @DisplayName("캐싱된 RowMapper를 반환한다.")
+    @Test
+    void getRowMapperTest1() throws SQLException {
+        // given
+        RowMapper<TestDomain> rowMapper = RowMapperFactory.getRowMapper(TestDomain.class);
+
+        // when
+        RowMapper<TestDomain> cachedRowMapper = RowMapperFactory.getRowMapper(TestDomain.class);
+
+        // then
+        assertThat(cachedRowMapper).isSameAs(rowMapper);
     }
 }
