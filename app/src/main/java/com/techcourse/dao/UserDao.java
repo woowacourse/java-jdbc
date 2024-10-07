60cff5f7663cf143f78f2a6bc7fccde6d04ee8f4
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-07 21:26:40 +0900

    fix: update 시 id를 입력받아 하나의 row만 갱신하도록 수정

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -25,8 +25,8 @@ public class UserDao {
     }
 
     public void update(final User user) {
-        String sql = "update users set account=?, password=?, email=?";
-        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
+        String sql = "update users set account=?, password=?, email=? where id = ?";
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
