35f4101a56f57827ee191c301d8b6e9c3f309fef
JUHA <84626225+khabh@users.noreply.github.com>
2024-10-07 22:18:41 +0900

    refactor: 개행 추가

--- jdbc/src/main/java/com/interface21/jdbc/exception/JdbcAccessException.java
+++ jdbc/src/main/java/com/interface21/jdbc/exception/JdbcAccessException.java
@@ -1,6 +1,7 @@
 package com.interface21.jdbc.exception;
 
 public class JdbcAccessException extends RuntimeException {
+
     public JdbcAccessException(String message, Throwable cause) {
         super(message, cause);
     }
