abf939f405763b5cfba4f12966f89d50437dbcea
tackyu <tack970913@gmail.com>
2024-10-07 17:35:57 +0900

    refactor: 직접적인 자료형 명시

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -24,32 +24,32 @@ public class UserDao {
     }
 
     public void insert(final User user) {
-        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        final String sql = "insert into users (account, password, email) values (?, ?, ?)";
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
-        final var sql = "update users set password=? where id=?";
+        final String sql = "update users set password=? where id=?";
         jdbcTemplate.update(sql, user.getPassword(), user.getId());
     }
 
     void delete(final User user) {
-        final var sql = "delete from users where account=?";
+        final String sql = "delete from users where account=?";
         jdbcTemplate.update(sql, user.getAccount());
     }
 
     public List<User> findAll() {
-        final var sql = "select id, account, password, email from users";
+        final String sql = "select id, account, password, email from users";
         return jdbcTemplate.queryForObject(sql, userRowMapper);
     }
 
     public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
+        final String sql = "select id, account, password, email from users where id = ?";
         return jdbcTemplate.query(sql, userRowMapper, id);
     }
 
     public User findByAccount(final String account) {
-        final var sql = "select id, account, password, email from users where account = ?";
+        final String sql = "select id, account, password, email from users where account = ?";
         return jdbcTemplate.query(sql, userRowMapper, account);
     }
 }
