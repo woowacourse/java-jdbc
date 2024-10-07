3ead3722070de29e3d17e6f68ca25d8016ccdd34
linirini <2001yerin@naver.com>
2024-10-08 00:44:23 +0900

    refactor: 테스트명 작성 및 주석 추가

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.config.DataSourceConfig;
@@ -28,49 +29,67 @@ class UserDaoTest {
         userDao.insert(user);
     }
 
+    @DisplayName("모든 사용자의 정보를 조회한다.")
     @Test
     void findAll() {
+        // when & then
         List<User> users = userDao.findAll();
 
         assertThat(users).isNotEmpty();
     }
 
+    @DisplayName("식별자로 사용자를 조회한다.")
     @Test
     void findById() {
+        // when & then
         Optional<User> user = userDao.findById(1L);
 
         assertThat(user.get().getAccount()).isEqualTo("gugu");
     }
 
+    @DisplayName("계정으로 사용자를 조회한다.")
     @Test
     void findByAccount() {
+        // given
         String account = "gugu";
+
+        // when
         Optional<User> user = userDao.findByAccount(account);
 
+        // then
         assertThat(user.get().getAccount()).isEqualTo(account);
     }
 
+    @DisplayName("새로운 사용자를 저장한다.")
     @Test
     void insert() {
+        // given
         String account = "insert-gugu";
         User user = new User(account, "password", "hkkang@woowahan.com");
+
+        // when
         userDao.insert(user);
 
-        final var actual = userDao.findById(2L);
+        // then
+        Optional<User> result = userDao.findById(2L);
 
-        assertThat(actual.get().getAccount()).isEqualTo(account);
+        assertThat(result.get().getAccount()).isEqualTo(account);
     }
 
+    @DisplayName("사용자의 정보를 변경한다.")
     @Test
     void update() {
+        // given
         String newPassword = "password99";
         User user = userDao.findById(1L).get();
         user.changePassword(newPassword);
 
+        // when
         userDao.update(user);
 
-        final var actual = userDao.findById(1L);
+        // then
+        Optional<User> result = userDao.findById(1L);
 
-        assertThat(actual.get().getPassword()).isEqualTo(newPassword);
+        assertThat(result.get().getPassword()).isEqualTo(newPassword);
     }
 }
