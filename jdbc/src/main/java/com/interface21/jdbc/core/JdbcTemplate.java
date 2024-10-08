0aa37a8e72cc9b51cd98f82673fd8e5e65e924c5
Lee Hangil <skfcb10@naver.com>
2024-10-08 10:33:42 +0900

    [1단계 - JDBC 라이브러리 구현하기] 리건(이한길) 미션 제출합니다. (#660)

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,108 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+    private static final String QUERY_RESULT_EXCEPTION_MESSAGE = "Incorrect result size: expected 1, actual ";
+    private static final int MAXIMUM_QUERY_FOR_OBJECT_RESULT = 1;
+    private static final int BASE_PARAMETER_INDEX = 1;
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            setPreparedStatement(pstmt, args);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void setPreparedStatement(final PreparedStatement pstmt, final Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + BASE_PARAMETER_INDEX, args[i]);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()
+        ) {
+            log.debug("query : {}", sql);
+
+            return extractData(rowMapper, rs);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        ResultSet rs = null;
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            setPreparedStatement(pstmt, args);
+            rs = pstmt.executeQuery();
+
+            return extractData(rowMapper, rs);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        } finally {
+            closeResultSet(rs);
+        }
+    }
+
+    private <T> List<T> extractData(final RowMapper<T> rowMapper, final ResultSet rs) throws SQLException {
+        List<T> results = new ArrayList<>();
+        while (rs.next()) {
+            results.add(rowMapper.mapRow(rs, rs.getRow()));
+        }
+        return results;
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, Object... args) {
+        List<T> results = query(sql, rowMapper, args);
+        return getSingleResult(results);
+    }
+
+    private <T> T getSingleResult(final List<T> results) {
+        if (results.size() != MAXIMUM_QUERY_FOR_OBJECT_RESULT) {
+            throw new DataAccessException(QUERY_RESULT_EXCEPTION_MESSAGE + results.size());
+        }
+        return results.getFirst();
+    }
+
+    private void closeResultSet(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ignored) {
+        }
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,128 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import support.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.*;
+
 class JdbcTemplateTest {
 
+    private DataSource dataSource;
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        dataSource = mock(DataSource.class);
+        connection = mock(Connection.class);
+        when(dataSource.getConnection()).thenReturn(connection);
+
+        preparedStatement = mock(PreparedStatement.class);
+        when(connection.prepareStatement(any())).thenReturn(preparedStatement);
+
+        resultSet = mock(ResultSet.class);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @Test
+    @DisplayName("query() 메소드를 통해 DB에서 값을 조회할 수 있다.")
+    void query() throws SQLException {
+        final String query = "select * from users";
+        final RowMapper<User> rowMapper = getUserRowMapper();
+
+        when(resultSet.next())
+                .thenReturn(true)
+                .thenReturn(false);
+        final List<User> users = jdbcTemplate.query(query, rowMapper);
+
+        assertAll(
+                () -> assertThat(users).hasSize(1),
+                () -> assertThat(users.getFirst().getAccount()).isEqualTo("gugu")
+        );
+    }
+
+    @Test
+    @DisplayName("모든 작업이 끝난 후에는 DataSource, Connection, ResultSet이 종료되어야 한다.")
+    void verifyResourceClosed() throws SQLException {
+        final String query = "select * from users";
+        final RowMapper<User> rowMapper = getUserRowMapper();
+
+        when(resultSet.next())
+                .thenReturn(true)
+                .thenReturn(false);
+        jdbcTemplate.query(query, rowMapper);
+
+        verify(preparedStatement, atLeastOnce()).close();
+        verify(connection, atLeastOnce()).close();
+        verify(resultSet, atLeastOnce()).close();
+    }
+
+    @Test
+    @DisplayName("queryForObject() 메소드를 통해 DB에서 값을 조회할 수 있다.")
+    void queryForObject() throws SQLException {
+        final String query = "select id, account, password, email from users where id = ?";
+        final RowMapper<User> rowMapper = getUserRowMapper();
+
+        when(resultSet.next())
+                .thenReturn(true)
+                .thenReturn(false);
+        final User user = jdbcTemplate.queryForObject(query, rowMapper, 1L);
+
+        assertThat(user.getAccount()).isEqualTo("gugu");
+    }
+
+    @Test
+    @DisplayName("queryForObject() 메소드는 유일한 값만 조회할 수 있다.")
+    void validateUniqueOfQueryForObjectResult() throws SQLException {
+        final String query = "select id, account, password, email from users where id = ?";
+        final RowMapper<User> rowMapper = getUserRowMapper();
+
+        when(resultSet.next())
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(false);
+
+        assertThatThrownBy(() -> jdbcTemplate.queryForObject(query, rowMapper, 1L))
+                .isInstanceOf(DataAccessException.class)
+                .hasMessage("Incorrect result size: expected 1, actual 2");
+    }
+
+    @Test
+    @DisplayName("queryForObject() 메소드의 결과는 항상 존재해야 한다.")
+    void validateExistOfQueryForObjectResult() throws SQLException {
+        final String query = "select id, account, password, email from users where id = ?";
+        final RowMapper<User> rowMapper = getUserRowMapper();
+
+        when(resultSet.next())
+                .thenReturn(false);
+
+        assertThatThrownBy(() -> jdbcTemplate.queryForObject(query, rowMapper, 1L))
+                .isInstanceOf(DataAccessException.class)
+                .hasMessage("Incorrect result size: expected 1, actual 0");
+    }
+
+    private RowMapper<User> getUserRowMapper() {
+        return (rs, rowNum) -> new User(
+                1L,
+                "gugu",
+                "password",
+                "gugu@test.com");
+    }
 }
