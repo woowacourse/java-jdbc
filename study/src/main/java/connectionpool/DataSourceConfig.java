e2f540ca46b06c0cbfb03c3386318a553c167a77
fromitive <fromitive@gmail.com>
2024-10-08 17:30:49 +0900

    refactor: DataSourceProperty를 이용해서 application.yml파일에 있는 설정 동기화

--- study/src/main/java/connectionpool/DataSourceConfig.java
+++ study/src/main/java/connectionpool/DataSourceConfig.java
@@ -2,28 +2,26 @@ package connectionpool;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class DataSourceConfig {
+    DataSourceProperty dataSourceProperty;
 
-    public static final int MAXIMUM_POOL_SIZE = 5;
-    private static final String H2_URL = "jdbc:h2:./test;DB_CLOSE_DELAY=-1";
-    private static final String USER = "sa";
-    private static final String PASSWORD = "";
-
+    public DataSourceConfig(DataSourceProperty dataSourceProperty) {
+        this.dataSourceProperty = dataSourceProperty;
+    }
 
     @Bean
     public DataSource hikariDataSource() {
         final var hikariConfig = new HikariConfig();
-        hikariConfig.setPoolName("gugu");
-        hikariConfig.setJdbcUrl(H2_URL);
-        hikariConfig.setUsername(USER);
-        hikariConfig.setPassword(PASSWORD);
-        hikariConfig.setMaximumPoolSize(MAXIMUM_POOL_SIZE);
+        hikariConfig.setJdbcUrl(dataSourceProperty.getJdbcUrl());
+        hikariConfig.setUsername(dataSourceProperty.getUserName());
+        hikariConfig.setPassword(dataSourceProperty.getPassword());
+        hikariConfig.setPoolName(dataSourceProperty.getPoolName());
+        hikariConfig.setMaximumPoolSize(dataSourceProperty.getMaximumPoolSize());
         hikariConfig.setConnectionTestQuery("VALUES 1");
         hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
         hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
