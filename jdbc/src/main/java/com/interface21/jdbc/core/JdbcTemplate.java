c66e0cd1b6b0ca9d6a0035c75e772d38342f5d8b
최휘용 <99064014+slimsha2dy@users.noreply.github.com>
2024-10-09 00:17:04 +0900

    [1단계 - JDBC 라이브러리 구현하기] - 알파카(최휘용) 미션 제출합니다. (#698)
    * feat: RowMapper 인터페이스 구현
    
    * feat: JdbcTemplate의 query, queryForObject 기능 구현
    
    * feat: JdbcTemplate의 update 기능 구현
    
    * refactor: JdbcTemplate 메서드 분리
    
    * test: JdbcTemplate 테스트 추가
    
    * test: DisplayName 추가
    
    * style: 타입 추론을 사용하지 않도록 코드 스타일 통일
    
    * feat: DataAccessException을 활용하도록 수정
    
    * feat: rowNum의 인자로 ResultSet의 행 번호를 넣도록 수정
    
    * feat: 학습 테스트 코드 반영

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,16 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.RowMapper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,53 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement psmt = conn.prepareStatement(sql)
+        ) {
+            setPreparedStatementParameters(args, psmt);
+            ResultSet rs = psmt.executeQuery();
+
+            return mapResultSetToList(rowMapper, rs);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> List<T> mapResultSetToList(RowMapper<T> rowMapper, ResultSet rs) throws SQLException {
+        List<T> result = new ArrayList<>();
+        while (rs.next()) {
+            T element = rowMapper.mapRow(rs, rs.getRow());
+            result.add(element);
+        }
+        return result;
+    }
+
+    private void setPreparedStatementParameters(Object[] args, PreparedStatement psmt) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            psmt.setObject(i + 1, args[i]);
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        List<T> result = query(sql, rowMapper, args);
+        if (result.size() != 1) {
+            throw new DataAccessException("조회하려는 데이터가 여러 개입니다.");
+        }
+        return result.get(0);
+    }
+
+    public int update(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement psmt = conn.prepareStatement(sql)
+        ) {
+            setPreparedStatementParameters(args, psmt);
+
+            return psmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,99 @@
 package com.interface21.jdbc.core;
 
-class JdbcTemplateTest {
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.RowMapper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+class JdbcTemplateTest {
+    @Mock
+    private DataSource dataSource;
+    @Mock
+    private Connection connection;
+    @Mock
+    private PreparedStatement preparedStatement;
+    @Mock
+    private ResultSet resultSet;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        MockitoAnnotations.openMocks(this);
+        when(dataSource.getConnection()).thenReturn(connection);
+        when(connection.prepareStatement(anyString())).thenReturn(preparedStatement);
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @DisplayName("여러 데이터를 조회하는 쿼리를 처리한다.")
+    @Test
+    public void query() throws Exception {
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, true, true, false);
+        when(resultSet.getString("name")).thenReturn("이은정", "클로버", "지니아");
+
+        RowMapper<String> rowMapper = (rs, rowNum) -> rs.getString("name");
+
+        List<String> result = jdbcTemplate.query("select name from users", rowMapper);
+
+        assertAll(
+                () -> assertThat(result.size())
+                        .isEqualTo(3),
+                () -> assertThat(result)
+                        .isEqualTo(List.of("이은정", "클로버", "지니아"))
+        );
+    }
+
+    @DisplayName("데이터를 업데이트하는 쿼리를 처리한다.")
+    @Test
+    public void update() throws Exception {
+        when(preparedStatement.executeUpdate()).thenReturn(1);
+
+        int rowsAffected = jdbcTemplate.update("update users set name = ? where id = ?", "클로버지니아", 1);
+
+        assertThat(rowsAffected)
+                .isEqualTo(1);
+    }
+
+    @DisplayName("한 개의 데이터를 조회하는 쿼리를 처리한다.")
+    @Test
+    public void queryForObject() throws Exception {
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, false);
+        when(resultSet.getString("name")).thenReturn("킹로버");
+
+        RowMapper<String> rowMapper = (rs, rowNum) -> rs.getString("name");
+
+        String result = jdbcTemplate.queryForObject("select name from users where id = ?", rowMapper, 1);
+
+        assertThat(result)
+                .isEqualTo("킹로버");
+    }
+
+    @DisplayName("조회하려는 데이터가 여러 개일 경우 예외로 처리한다.")
+    @Test
+    public void queryForObjectFailedWithMultipleResults() throws Exception {
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.next()).thenReturn(true, true, false);
+
+        RowMapper<String> rowMapper = (rs, rowNum) -> rs.getString("name");
+
+        assertThatThrownBy(() -> jdbcTemplate.queryForObject("select name from users where id = ?", rowMapper, 1))
+                .isInstanceOf(DataAccessException.class)
+                .hasMessage("조회하려는 데이터가 여러 개입니다.");
+    }
 }
