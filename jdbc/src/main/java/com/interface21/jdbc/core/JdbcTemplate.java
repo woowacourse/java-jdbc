34cee544b216c93b205f540872718efad557fcb9
Gyeongho Yang <chchch1213@naver.com>
2024-10-08 11:27:55 +0900

    [1단계 - JDBC 라이브러리 구현하기] 새양(양경호) 미션 제출합니다. (#638)
    * feat: add all sql on dao
    
    * feat: add rowMapper and used on update and selectAll
    
    * feat: use jdbcTemplate impl by self
    
    * feat: split sql executor by interface
    
    * refactor: use field instead of method for mapper
    
    * refactor: throw new exception on jdbc template when catching
    
    * refactor: remove unnecessary statement
    
    * refactor: add incorrect size exception
    
    * refactor: add exception on method signature
    
    * refactor: match type to domain
    
    * test: add tests for all cases
    
    * refactor: change exception to unchecked
    
    * feat: recreate table on every tests
    
    * refactor: set type as list instead of arraylist

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,18 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import com.interface21.dao.IncorrectResultSizeDataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -11,7 +20,54 @@ public class JdbcTemplate {
 
     private final DataSource dataSource;
 
-    public JdbcTemplate(final DataSource dataSource) {
+    public JdbcTemplate(DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(String sql, Object... args) {
+        Integer result = execute(sql, PreparedStatement::executeUpdate, args);
+        return Optional.ofNullable(result).orElse(0);
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
+        return execute(sql, statement -> {
+            List<T> results = new ArrayList<>();
+            ResultSet resultSet = statement.executeQuery();
+
+            for (int rowNum = 0; resultSet.next(); rowNum++) {
+                results.add(rowMapper.mapRow(resultSet, rowNum));
+            }
+
+            return Collections.unmodifiableList(results);
+        }, args);
+    }
+
+    public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object... args)
+            throws IncorrectResultSizeDataAccessException {
+        List<T> results = query(sql, rowMapper, args);
+        if (results.isEmpty()) {
+            return Optional.empty();
+        }
+        if (results.size() > 1) {
+            throw new IncorrectResultSizeDataAccessException(1, results.size());
+        }
+
+        return Optional.of(results.getFirst());
+    }
+
+    private <T> T execute(String sql, SqlExecutor<T> executor, Object... args) {
+        log.debug("query : {}", sql);
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            for (int i = 0; i < args.length; i++) {
+                statement.setObject(i + 1, args[i]);
+            }
+            return executor.execute(statement);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
     }
 }
