5897e9801bfdc1e065fbbfe9407b8367ce603be9
kunsanglee <85242378+kunsanglee@users.noreply.github.com>
2024-10-08 20:44:06 +0900

    [1단계 - JDBC 라이브러리 구현하기] - 이상(이건상) 미션 제출합니다. (#671)
    * fix: javax 의존성 jakarta로 변경
    
    * feat: UserDao todo 기능 구현
    
    * feat: JdbcTemplate 작성 및 UserDao에서 사용하도록 변경
    
    * test: JdbcTemplate 테스트 작성

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,63 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(String sql, Object ... objects) {
+        try (Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            setParameterIntoStatement(preparedStatement, objects);
+            int rowCount = preparedStatement.executeUpdate();
+            log.debug("query : {}, rowCount : {}", sql, rowCount);
+            return rowCount;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object ... objects) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            if (objects != null) {
+                setParameterIntoStatement(preparedStatement, objects);
+            }
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return getObject(rowMapper, resultSet);
+        } catch (SQLException e) {
+            log.error("SQL error during queryForObject: {}", e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        List<T> result = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()) {
+                T row = rowMapper.mapRow(resultSet, resultSet.getRow());
+                result.add(row);
+            }
+            log.debug("query : {}", sql);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
+        return result;
+    }
+
+    private void setParameterIntoStatement(PreparedStatement preparedStatement, Object ... objects) throws SQLException {
+        for (int i = 0; i < objects.length; i++) {
+            preparedStatement.setObject(i + 1, objects[i]);
+        }
+    }
+
+    private <T> T getObject(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
+        if (resultSet.next()) {
+            return rowMapper.mapRow(resultSet, resultSet.getRow());
+        }
+        return null;
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,153 @@
 package com.interface21.jdbc.core;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Objects;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private DataSource dataSource;
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+    private JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        dataSource = mock(DataSource.class);
+        connection = mock(Connection.class);
+        preparedStatement = mock(PreparedStatement.class);
+        resultSet = mock(ResultSet.class);
+
+        given(dataSource.getConnection()).willReturn(connection);
+        given(connection.prepareStatement(anyString())).willReturn(preparedStatement);
+        given(preparedStatement.executeQuery()).willReturn(resultSet);
+
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    @DisplayName("update")
+    @Test
+    void update() throws SQLException {
+        String sql = "update users set account = ?, password = ?, email = ? where id = ?";
+
+        jdbcTemplate.update(sql, "changeAccount", "changePassword", "changeEmail", 1L);
+
+        assertAll(
+                () -> then(connection).should().prepareStatement(sql),
+                () -> then(preparedStatement).should().setObject(1, "changeAccount"),
+                () -> then(preparedStatement).should().setObject(2, "changePassword"),
+                () -> then(preparedStatement).should().setObject(3, "changeEmail"),
+                () -> then(preparedStatement).should().setObject(4, 1L),
+                () -> then(preparedStatement).should().executeUpdate(),
+                () -> then(preparedStatement).should().close(),
+                () -> then(connection).should().close(),
+                () -> then(dataSource).should().getConnection()
+        );
+    }
+
+    @DisplayName("queryForObject")
+    @Test
+    void queryForObject() throws SQLException {
+        given(preparedStatement.executeQuery()).willReturn(resultSet);
+        given(resultSet.getRow()).willReturn(1);
+        given(resultSet.next()).willReturn(true);
+        given(resultSet.getLong("id")).willReturn(1L);
+        given(resultSet.getString("account")).willReturn("account");
+        given(resultSet.getString("password")).willReturn("password");
+        given(resultSet.getString("email")).willReturn("email");
+
+        String sql = "select * from users where id = ?";
+
+        User user = jdbcTemplate.queryForObject(sql, (resultSet, num) -> new User(
+                resultSet.getLong("id"),
+                resultSet.getString("account"),
+                resultSet.getString("password"),
+                resultSet.getString("email"))
+        );
+
+        assertAll(
+                () -> then(connection).should().prepareStatement(sql),
+                () -> then(preparedStatement).should().executeQuery(),
+                () -> then(resultSet).should().next(),
+                () -> then(resultSet).should().getLong("id"),
+                () -> then(resultSet).should().getString("account"),
+                () -> then(resultSet).should().getString("password"),
+                () -> then(resultSet).should().getString("email"),
+                () -> then(preparedStatement).should().close(),
+                () -> then(connection).should().close(),
+                () -> then(dataSource).should().getConnection(),
+                () -> assertThat(user).isEqualTo(new User(1L, "account", "password", "email"))
+        );
+    }
+
+    @DisplayName("query")
+    @Test
+    void query() throws SQLException {
+        String sql = "select * from users";
+
+        given(preparedStatement.executeQuery()).willReturn(resultSet);
+        given(resultSet.next()).willReturn(true, true, false);
+
+        List<User> users = jdbcTemplate.query(sql, (resultSet, rowNum) -> new User(
+                resultSet.getLong("id"),
+                resultSet.getString("account"),
+                resultSet.getString("password"),
+                resultSet.getString("email"))
+        );
+
+        assertAll(
+                () -> then(connection).should().prepareStatement(sql),
+                () -> then(preparedStatement).should().executeQuery(),
+                () -> then(resultSet).should(times(3)).next(),
+                () -> assertThat(users.size()).isEqualTo(2)
+        );
+    }
+
+    static class User {
+        private Long id;
+        private String account;
+        private String password;
+        private String email;
+
+        public User(Long id, String account, String password, String email) {
+            this.id = id;
+            this.account = account;
+            this.password = password;
+            this.email = email;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            User user = (User) o;
+            return Objects.equals(id, user.id) && Objects.equals(account, user.account)
+                    && Objects.equals(password, user.password) && Objects.equals(email, user.email);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, account, password, email);
+        }
+    }
 }
