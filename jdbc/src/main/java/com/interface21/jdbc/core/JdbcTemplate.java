ba1a533a963a90631f7276874e2793cddc07958b
cutehumanS2 <oddpinkjadeite@gmail.com>
2024-10-08 17:48:39 +0900

    refactor: SqlExecutor 클래스 분리

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,34 +1,27 @@
 package com.interface21.jdbc.core;
 
-import com.interface21.dao.DataAccessException;
 import com.interface21.dao.EmptyResultDataAccessException;
-import com.interface21.dao.IncorrectParameterCountException;
 import com.interface21.dao.IncorrectResultSizeDataAccessException;
-import java.sql.Connection;
-import java.sql.ParameterMetaData;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import javax.sql.DataSource;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
     private final DataSource dataSource;
+    private final SqlExecutor sqlExecutor;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+        this.sqlExecutor = new SqlExecutor();
     }
 
     public void executeUpdate(String sql, Object... parameters) {
         PreparedStatementExecutor<Integer> statementExecutor = PreparedStatement::executeUpdate;
-        execute(sql, statementExecutor, parameters);
+        sqlExecutor.execute(sql, dataSource, statementExecutor, parameters);
     }
 
     public <T> T fetchResult(String sql, ResultMapper<T> resultMapper, Object... parameters) {
@@ -48,23 +41,7 @@ public class JdbcTemplate {
                 return mapResults(resultSet, resultMapper);
             }
         };
-        return execute(sql, statementExecutor, parameters);
-    }
-
-    private <T> T execute(String sql, PreparedStatementExecutor<T> statementExecutor, Object... parameters) {
-        log.debug("실행 쿼리: {}", sql);
-        log.debug("파라미터: {}", Arrays.toString(parameters));
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
-
-            setParameters(preparedStatement, parameters);
-
-            return statementExecutor.execute(preparedStatement);
-
-        } catch (SQLException e) {
-            log.error("쿼리 실행에 실패했습니다: {}", sql, e);
-            throw new DataAccessException("쿼리 실행에 실패했습니다.", e);
-        }
+        return sqlExecutor.execute(sql, dataSource, statementExecutor, parameters);
     }
 
     private <T> List<T> mapResults(ResultSet resultSet, ResultMapper<T> resultMapper) throws SQLException {
@@ -73,20 +50,5 @@ public class JdbcTemplate {
             results.add(resultMapper.mapResult(resultSet));
         }
         return results;
-    }
-
-    private void setParameters(PreparedStatement preparedStatement, Object... parameters) throws SQLException {
-        validateParameterCount(preparedStatement, parameters);
-        for (int i = 0; i < parameters.length; i++) {
-            preparedStatement.setObject(i + 1, parameters[i]);
-        }
-    }
-
-    private void validateParameterCount(PreparedStatement preparedStatement, Object... parameters) throws SQLException {
-        ParameterMetaData parameterMetaData = preparedStatement.getParameterMetaData();
-        int expectedParameterCount = parameterMetaData.getParameterCount();
-        if (expectedParameterCount != parameters.length) {
-            throw new IncorrectParameterCountException(expectedParameterCount, parameters.length);
-        }
     }
 }
