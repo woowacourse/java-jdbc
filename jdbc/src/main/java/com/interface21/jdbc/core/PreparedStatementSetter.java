bbfa0d70185c00bad1425c081700438019035dcb
eunjungL <dldmswjd322@gmail.com>
2024-10-08 18:15:42 +0900

    refactor: PreparedStatmentSetter에서 null이면 동작 안하는 방어 로직 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementSetter.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementSetter.java
@@ -11,14 +11,26 @@ public class PreparedStatementSetter {
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
     private static final int PARAMETER_START_INDEX = 1;
 
-    public static void setValue(PreparedStatement preparedStatement, Object ...args) {
+    public static void setValue(PreparedStatement preparedStatement, Object... args) {
         try {
-            for (int parameterIndex = PARAMETER_START_INDEX; parameterIndex <= args.length; parameterIndex++) {
-                preparedStatement.setObject(parameterIndex, args[parameterIndex - PARAMETER_START_INDEX]);
-            }
+            setArgsToPreparedStatement(preparedStatement, args);
         } catch (SQLException e) {
             log.info("SET_PREPARED_STATEMENT_PARAMETER_ERROR :: {}", e.getMessage(), e);
             throw new DataAccessException("PreparedStatement에 파라미터를 설정하던 중 오류가 발생했습니다.");
+        }
+    }
+
+    private static void setArgsToPreparedStatement(
+            PreparedStatement preparedStatement,
+            Object[] args
+    ) throws SQLException
+    {
+        if (args == null) {
+            return;
+        }
+
+        for (int parameterIndex = PARAMETER_START_INDEX; parameterIndex <= args.length; parameterIndex++) {
+            preparedStatement.setObject(parameterIndex, args[parameterIndex - PARAMETER_START_INDEX]);
         }
     }
 }
