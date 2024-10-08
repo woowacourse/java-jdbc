3ca96df55af8e258e36ba8ec783fac6271202ce0
3juhwan <13selfesteem91@naver.com>
2024-10-08 13:57:39 +0900

    refacor: RowMapperFactory 인덴트 줄이기

--- jdbc/src/main/java/com/interface21/jdbc/core/RowMapperFactory.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/RowMapperFactory.java
@@ -15,17 +15,22 @@ public class RowMapperFactory {
 
     @SuppressWarnings("unchecked")
     public static <T> RowMapper<T> getRowMapper(Class<T> clazz) {
-        return (RowMapper<T>) RowMapperHolder.INSTANCE.computeIfAbsent(clazz, (ignored) ->
-                (rs, rowNum) -> {
-                    try {
-                        Constructor<?> constructor = findAllargsConstructor(clazz);
-                        constructor.setAccessible(true);
-                        Object[] constructorArguments = getConstructorArguments(constructor, clazz, rs);
-                        return (T) constructor.newInstance(constructorArguments);
-                    } catch (Exception e) {
-                        throw new RuntimeException(e.getMessage(), e);
-                    }
-                });
+        return (RowMapper<T>) RowMapperHolder.INSTANCE.computeIfAbsent(clazz, ignored -> createRowMapper(clazz));
+    }
+
+    private static <T> RowMapper<T> createRowMapper(Class<T> clazz) {
+        return (rs, rowNum) -> {
+            try {
+                Constructor<?> constructor = findAllargsConstructor(clazz);
+                constructor.setAccessible(true);
+                Object[] constructorArguments = getConstructorArguments(constructor, clazz, rs);
+                return (T) constructor.newInstance(constructorArguments);
+            } catch (Exception e) {
+                String errorMessage = String.format("Failed to map result set to %s: %s", clazz.getName(),
+                        e.getMessage());
+                throw new RuntimeException(errorMessage, e);
+            }
+        };
     }
 
     private static <T> Constructor<?> findAllargsConstructor(Class<T> clazz) throws NoSuchMethodException {
@@ -33,7 +38,8 @@ public class RowMapperFactory {
         return Arrays.stream(clazz.getDeclaredConstructors())
                 .filter(constructor -> constructor.getParameterCount() == fieldCount)
                 .findFirst()
-                .orElseThrow(() -> new NoSuchMethodException("모든 필드를 포함한 생성자를 찾을 수 없습니다. class: " + clazz.getName()));
+                .orElseThrow(
+                        () -> new NoSuchMethodException("모든 필드를 포함한 생성자를 찾을 수 없습니다. class: " + clazz.getName()));
     }
 
     private static Object[] getConstructorArguments(Constructor<?> constructor, Class<?> clazz, ResultSet rs)
