2e8d00a381ac378463d04e581d7a1d4146123aa5
yoonjuho <dbswn990@naver.com>
2024-10-08 17:07:43 +0900

    refactor: 공통 유틸리티 메서드 최상단에 위치

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -22,75 +22,6 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public int update(String sql, Object[] args) {
-        return queryExecute(sql, (preparedStatement, query) -> {
-            assignToPreparedStatement(preparedStatement, args);
-            return preparedStatement.executeUpdate();
-        });
-    }
-
-    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
-        return queryExecute(sql, (preparedStatement, query) -> {
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                return resultMapping(rowMapper, resultSet);
-            }
-        });
-    }
-
-    public <T> T queryForObject(String sql, Object[] args, RowMapper<T> rowMapper) {
-        return queryExecute(sql, (preparedStatement, query) -> {
-            assignToPreparedStatement(preparedStatement, args);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                return singleResultMapping(rowMapper, resultSet);
-            }
-        });
-    }
-
-    private <T> T singleResultMapping(RowMapper<T> rowMapper, ResultSet resultSet) {
-        try {
-            return getSingResult(rowMapper, resultSet);
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
-    }
-
-    private void assignToPreparedStatement(PreparedStatement preparedStatement, Object[] args) {
-        if (Objects.isNull(args) || args.length == 0) {
-            throw new DataAccessException("PreparedStatement에 바인딩할 파라미터가 없습니다.");
-        }
-        for (int index = 1; index <= args.length; index++) {
-            try {
-                preparedStatement.setObject(index, args[index - 1]);
-            } catch (SQLException e) {
-                log.error("파라미터 바인딩에 실패하였습니다. index: {}, arg: {}, 예외 메세지: {}", index, args[index - 1], e.getMessage(), e);
-                throw new DataAccessException(e);
-            }
-        }
-    }
-
-    private <T> List<T> resultMapping(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
-        List<T> results = new ArrayList<>();
-        while (resultSet.next()) {
-            results.add(rowMapper.mapRow(resultSet));
-        }
-        return results;
-    }
-
-    private <T> T getSingResult(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
-        if (resultSet.next()) {
-            T result = rowMapper.mapRow(resultSet);
-            validateSingleResult(resultSet);
-            return result;
-        }
-        throw new DataAccessException("단일 행 조회를 기대했지만, 조회된 행이 없습니다.");
-    }
-
-    private void validateSingleResult(ResultSet resultSet) throws SQLException {
-        if (resultSet.next()) {
-            throw new DataAccessException("단일 행 조회를 기대했지만, 여러 행이 조회되었습니다.");
-        }
-    }
-
     private <T> T queryExecute(final String sql, final SqlExecutor<T> sqlExecutor) {
         try (Connection connection = getConnection();
              PreparedStatement preparedStatement = getPreparedStatement(sql, connection)) {
@@ -115,6 +46,75 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error("SQL 쿼리에 대한 Statement을 준비하지 못했습니다. sql: {}. 예외 메세지: {}", sql, e.getMessage(), e);
             throw new DataAccessException(e);
+        }
+    }
+
+    public int update(String sql, Object[] args) {
+        return queryExecute(sql, (preparedStatement, query) -> {
+            assignToPreparedStatement(preparedStatement, args);
+            return preparedStatement.executeUpdate();
+        });
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
+        return queryExecute(sql, (preparedStatement, query) -> {
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return resultMapping(rowMapper, resultSet);
+            }
+        });
+    }
+
+    public <T> T queryForObject(String sql, Object[] args, RowMapper<T> rowMapper) {
+        return queryExecute(sql, (preparedStatement, query) -> {
+            assignToPreparedStatement(preparedStatement, args);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                return singleResultMapping(rowMapper, resultSet);
+            }
+        });
+    }
+
+    private void assignToPreparedStatement(PreparedStatement preparedStatement, Object[] args) {
+        if (Objects.isNull(args) || args.length == 0) {
+            throw new DataAccessException("PreparedStatement에 바인딩할 파라미터가 없습니다.");
+        }
+        for (int index = 1; index <= args.length; index++) {
+            try {
+                preparedStatement.setObject(index, args[index - 1]);
+            } catch (SQLException e) {
+                log.error("파라미터 바인딩에 실패하였습니다. index: {}, arg: {}, 예외 메세지: {}", index, args[index - 1], e.getMessage(), e);
+                throw new DataAccessException(e);
+            }
+        }
+    }
+
+    private <T> List<T> resultMapping(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
+        List<T> results = new ArrayList<>();
+        while (resultSet.next()) {
+            results.add(rowMapper.mapRow(resultSet));
+        }
+        return results;
+    }
+
+    private <T> T singleResultMapping(RowMapper<T> rowMapper, ResultSet resultSet) {
+        try {
+            return getSingResult(rowMapper, resultSet);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> T getSingResult(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
+        if (resultSet.next()) {
+            T result = rowMapper.mapRow(resultSet);
+            validateSingleResult(resultSet);
+            return result;
+        }
+        throw new DataAccessException("단일 행 조회를 기대했지만, 조회된 행이 없습니다.");
+    }
+
+    private void validateSingleResult(ResultSet resultSet) throws SQLException {
+        if (resultSet.next()) {
+            throw new DataAccessException("단일 행 조회를 기대했지만, 여러 행이 조회되었습니다.");
         }
     }
 }
