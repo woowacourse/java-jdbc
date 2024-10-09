ce90a50b2804a79613b2576ce0dd527cd1ff3d40
lemone <lejinhy@gmail.com>
2024-10-09 09:52:49 +0900

    feat: select문에 인자를 넣어 리스트를 반환하는 메서드 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -51,8 +51,21 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
-        try(final Connection connection = dataSource.getConnection();
-            final PreparedStatement statement = connection.prepareStatement(sql)) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
+            log.info("select query : {}", sql);
+            final ResultSet resultSet = statement.executeQuery();
+            return extractData(resultSet, rowMapper);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
+            setStatement(statement, args);
             log.info("select query : {}", sql);
             final ResultSet resultSet = statement.executeQuery();
             return extractData(resultSet, rowMapper);
