eae8824bf8039833b46d2adc4689979bd80fc822
hangillee <coderoad.io@gmail.com>
2024-10-09 17:19:41 +0900

    refactor: 함수형 인터페이스 어노테이션 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementSetter.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementSetter.java
@@ -3,6 +3,7 @@ package com.interface21.jdbc.core;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface PreparedStatementSetter {
     void setValues(PreparedStatement pstmt) throws SQLException;
 }
