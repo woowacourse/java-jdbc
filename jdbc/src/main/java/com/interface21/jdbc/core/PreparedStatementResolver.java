8163c3d4b97c5c048070129cc62a79f80661f293
linirini <2001yerin@naver.com>
2024-10-09 16:23:59 +0900

    fix: null 체크 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementResolver.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementResolver.java
@@ -2,24 +2,16 @@ package com.interface21.jdbc.core;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.interface21.dao.DataAccessException;
 
 public class PreparedStatementResolver {
 
-    private static final Logger log = LoggerFactory.getLogger(PreparedStatementResolver.class);
-
-    public static void setParameters(PreparedStatement pstmt, Object... args) {
+    public static void setParameters(PreparedStatement pstmt, Object... args) throws SQLException {
+        if (args == null) {
+            return;
+        }
         for (int parameterIndex = 0; parameterIndex < args.length; parameterIndex++) {
-            Object argument = args[parameterIndex];
             int position = parameterIndex + 1;
-            try {
-                pstmt.setObject(position, argument);
-            } catch (SQLException e) {
-                log.error(e.getMessage(), e);
-                throw new DataAccessException(e);
-            }
+            pstmt.setObject(position, args[parameterIndex]);
         }
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/PreparedStatementResolverTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/PreparedStatementResolverTest.java
@@ -1,10 +1,12 @@
 package com.interface21.jdbc.core;
 
 import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -12,7 +14,7 @@ class PreparedStatementResolverTest {
 
     @DisplayName("PreparedStatement의 인자를 설정한다.")
     @Test
-    void setParameters() {
+    void setParameters() throws SQLException {
         // given
         PreparedStatement pstmt = mock(PreparedStatement.class);
         String arg1 = "value1";
@@ -26,5 +28,15 @@ class PreparedStatementResolverTest {
                 () -> verify(pstmt).setObject(1, arg1),
                 () -> verify(pstmt).setObject(2, arg2)
         );
+    }
+
+    @DisplayName("PreparedStatement의 인자로 전달받은 값이 없으면 아무 예외도 발생하지 않는다.")
+    @Test
+    void doesNotSetParameters() {
+        // given
+        PreparedStatement pstmt = mock(PreparedStatement.class);
+
+        // when & then
+        assertDoesNotThrow(() -> PreparedStatementResolver.setParameters(pstmt, null));
     }
 }
