7168dc5a9e93e3db9db0253c50202f0c442dd578
slimsha2dy <chy2581@naver.com>
2024-10-09 17:43:57 +0900

    style: 학습 테스트 코드 주석 추가

--- study/src/test/java/transaction/stage2/Stage2Test.java
+++ study/src/test/java/transaction/stage2/Stage2Test.java
@@ -36,8 +36,8 @@ class Stage2Test {
     }
 
     /**
-     * 생성된 트랜잭션이 몇 개인가?
-     * 왜 그런 결과가 나왔을까?
+     * 생성된 트랜잭션이 몇 개인가? 1개
+     * 왜 그런 결과가 나왔을까? Second는 외부 트랜잭션인 First에 참여하게 된다.
      */
     @Test
     void testRequired() {
@@ -50,8 +50,8 @@ class Stage2Test {
     }
 
     /**
-     * 생성된 트랜잭션이 몇 개인가?
-     * 왜 그런 결과가 나왔을까?
+     * 생성된 트랜잭션이 몇 개인가? 2
+     * 왜 그런 결과가 나왔을까? REQUIRES_NEW는 외부 트랜잭션이 있어도 새로운 트랜잭션을 만든다.
      */
     @Test
     void testRequiredNew() {
@@ -67,6 +67,8 @@ class Stage2Test {
     /**
      * firstUserService.saveAndExceptionWithRequiredNew()에서 강제로 예외를 발생시킨다.
      * REQUIRES_NEW 일 때 예외로 인한 롤백이 발생하면서 어떤 상황이 발생하는 지 확인해보자.
+     *
+     * 외부 트랜잭션이 예외를 터뜨려도 내부 트랜잭션은 롤백되지 않는다.
      */
     @Test
     void testRequiredNewWithRollback() {
@@ -81,6 +83,11 @@ class Stage2Test {
     /**
      * FirstUserService.saveFirstTransactionWithSupports() 메서드를 보면 @Transactional이 주석으로 되어 있다.
      * 주석인 상태에서 테스트를 실행했을 때와 주석을 해제하고 테스트를 실행했을 때 어떤 차이점이 있는지 확인해보자.
+     *
+     * SUPPORTS는 트랜잭션이 존재할 경우에만 참여하고 그렇지 않을 경우 트랜잭션을 생성하지 않는다.
+     * 하지만 주석 처리 후 확인해보면 실제로는 트랜잭션 매니저가 Second를 처리하기 위해 논리 트랜잭션을 생성한다.
+     * 물리 트랜잭션: 하나의 커넥션으로 처리하는 실제 트랜잭션
+     * 논리 트랜잭션: 스프링이 처리하는 트랜잭션 영역
      */
     @Test
     void testSupports() {
@@ -96,6 +103,8 @@ class Stage2Test {
      * FirstUserService.saveFirstTransactionWithMandatory() 메서드를 보면 @Transactional이 주석으로 되어 있다.
      * 주석인 상태에서 테스트를 실행했을 때와 주석을 해제하고 테스트를 실행했을 때 어떤 차이점이 있는지 확인해보자.
      * SUPPORTS와 어떤 점이 다른지도 같이 챙겨보자.
+     *
+     * SUPPORTS와 전부 동일하지만 외부 트랜잭션이 존재하지 않을 경우 예외를 터뜨린다.
      */
     @Test
     void testMandatory() {
@@ -113,6 +122,10 @@ class Stage2Test {
      * 다시 테스트를 실행하면 몇 개의 물리적 트랜잭션이 동작할까?
      *
      * 스프링 공식 문서에서 물리적 트랜잭션과 논리적 트랜잭션의 차이점이 무엇인지 찾아보자.
+     * -> 위에 작성
+     *
+     * NOT_SUPPORTED는 외부 트랜잭션이 존재할 경우 그 트랜잭션과 별개로 트랜잭션 없이 진행한다.
+     * 존재하지 않을 경우에는 트랜잭션 없이 진행한다.
      */
     @Test
     void testNotSupported() {
@@ -128,6 +141,14 @@ class Stage2Test {
     /**
      * 아래 테스트는 왜 실패할까?
      * FirstUserService.saveFirstTransactionWithNested() 메서드의 @Transactional을 주석 처리하면 어떻게 될까?
+     *
+     * NESTED는 외부 트랜잭션이 존재할 경우 중첩 트랜잭션을 생성한다. 존재하지 않으면 트랜잭션을 생서한다.
+     * 중첩 트랜잭션의 동작은 다음과 같다
+     * 외부 트랜잭션 예외 -> 외부와 내부 둘다 롤백
+     * 내부 트랜잭션 예외 -> 내부만 롤백
+     * 내부는 외부에 영향을 받지만 내부는 외부에 영향을 받지 않는다.
+     * NESTED는 jdbc의 savepoint 기능을 사용하는데 DB 드라이버가 이를 지원하지 않을 경우 사용이 불가능하다.(JPA도)
+     * NESTED를 통해 중첩 트랜잭션을 생성하려고 하면 예외가 발생한다.
      */
     @Test
     void testNested() {
@@ -141,6 +162,8 @@ class Stage2Test {
 
     /**
      * 마찬가지로 @Transactional을 주석처리하면서 관찰해보자.
+     *
+     * NEVER는 외부 트랜잭션이 존재할 경우 예외를 발생한다. 존재하지 않을 경우 트랜잭션 없이 진행한다.
      */
     @Test
     void testNever() {
