e266e223d6f44973239d72d0d23be07fc5ae5a9c
lemone <55480011+JiHyeonL@users.noreply.github.com>
2024-10-09 13:24:19 +0900

    [1단계 - JDBC 라이브러리 구현하기] - 레모네(이지현) 미션 제출합니다. (#648)
    * feat: JdbcTemplate에 update, queryForObject, query 메서드 추가
    
    * feat: RowMapper 인터페이스 추가
    
    * feat: UserDao 기존 코드 JdbcTemplate으로 교체
    
    * feat: UserHistoryDao 로직 jdbcTemplate으로 교체
    
    * refactor: try-with-resource로 변경, 메서드 분리 및 변수명 수정
    
    * test: 테스트 메서드 격리를 위해 setUp에 데이터 초기화 로직 추가
    
    * refactor: RowMapper 상수화 & resultSet close 시 SQLException 발생하면 DataAccessException 던지도록 수정
    
    * refactor: 변수명 변경 및 값 변경이 없는 경우 변수 추출 생략
    
    * refactor: ResultSet close 로직 생략
    
    * test: JdbcTemplate 테스트 추가
    
    * feat: select문에 인자를 넣어 리스트를 반환하는 메서드 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,18 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
+import com.interface21.dao.DataAccessException;
 
 public class JdbcTemplate {
 
@@ -13,5 +22,83 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
+            setStatement(statement, args);
+            log.info("update query : {}", sql);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
+            setStatement(statement, args);
+            log.info("select query : {}", sql);
+            final ResultSet resultSet = statement.executeQuery();
+            final List<T> data = extractData(resultSet, rowMapper, args);
+            return result(data);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
+            log.info("select query : {}", sql);
+            final ResultSet resultSet = statement.executeQuery();
+            return extractData(resultSet, rowMapper);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement statement = connection.prepareStatement(sql)) {
+            setStatement(statement, args);
+            log.info("select query : {}", sql);
+            final ResultSet resultSet = statement.executeQuery();
+            return extractData(resultSet, rowMapper);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void setStatement(PreparedStatement statement, final Object... args)
+            throws SQLException {
+        int index = 1;
+        for (final Object arg : args) {
+            statement.setObject(index++, arg);
+        }
+    }
+
+    private <T> List<T> extractData(final ResultSet resultSet, final RowMapper<T> rowMapper, final Object... args)
+            throws SQLException {
+        final List<T> results = new ArrayList<>();
+        while (resultSet.next()) {
+            results.add(rowMapper.mapRow(resultSet, args.length));
+        }
+        return results;
+    }
+
+    private <T> T result(final List<T> data) throws SQLException {
+        if (data.isEmpty()) {
+            throw new SQLException("sql 결과 데이터가 존재하지 않습니다.");
+        }
+        if (data.size() > 1) {
+            throw new SQLException("sql 결과 데이터가 2개 이상 존재합니다.");
+        }
+        return data.getFirst();
     }
 }
