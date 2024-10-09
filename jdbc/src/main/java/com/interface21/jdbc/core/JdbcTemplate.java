3a0a00cfd17bc32bac243e3517ca09772a512300
ikjo39 <chi4321@naver.com>
2024-10-09 21:10:25 +0900

    feat(GeneratedKeyHolder): 생성된 id를 GeneratedKeyHolder에 저장하는 로직 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
@@ -14,6 +15,7 @@ import org.slf4j.LoggerFactory;
 
 public class JdbcTemplate {
 
+    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
     private static final PrepareStatementSetter STATEMENT_SETTER = (preparedStatement, objects) -> {
         if (objects == null) {
             return;
@@ -22,8 +24,6 @@ public class JdbcTemplate {
             preparedStatement.setObject(index + 1, objects[index]);
         }
     };
-
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
     private final DataSource dataSource;
 
@@ -42,12 +42,44 @@ public class JdbcTemplate {
         }
     }
 
+    /**
+     * 생성된 키를 즉시 사용해야 할 경우 이 메서드를 사용합니다.
+     *
+     * @param query     실행할 SQL 쿼리
+     * @param keyHolder 자동 생성된 키를 저장할 keyHolder
+     * @param objects   쿼리에 사용할 파라미터 값
+     */
+    public void update(String query, GeneratedKeyHolder keyHolder, Object... objects) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
+            log.debug("query : {}", query);
+            STATEMENT_SETTER.setValues(pstmt, objects);
+            int resultCount = pstmt.executeUpdate();
+
+            if (resultCount != 1) {
+                throw new DataAccessException("데이터 삽입 과정에서 문제가 발생하였습니다.");
+            }
+            addKeyHolder(keyHolder, pstmt);
+        } catch (SQLException e) {
+            throw new DataAccessException("데이터 접근 과정에서 문제가 발생하였습니다.", e);
+        }
+    }
+
+    private void addKeyHolder(GeneratedKeyHolder keyHolder, PreparedStatement pstmt) throws SQLException {
+        try (ResultSet generatedKeys = pstmt.getGeneratedKeys()) {
+            if (generatedKeys.next()) {
+                long id = generatedKeys.getLong(1);
+                keyHolder.addKey(id);
+            }
+        }
+    }
+
     public <T> T queryForObject(String query, RowMapper<T> mapper, Object... objects) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement pstmt = connection.prepareStatement(query)) {
             log.debug("query : {}", query);
             STATEMENT_SETTER.setValues(pstmt, objects);
-            ResultSet resultSet = executeQuery(pstmt);
+            ResultSet resultSet = pstmt.executeQuery();
 
             if (!resultSet.next()) {
                 throw new DataNotFoundException("데이터가 존재하지 않습니다.");
@@ -63,7 +95,7 @@ public class JdbcTemplate {
              PreparedStatement pstmt = connection.prepareStatement(query)) {
             log.debug("query : {}", query);
             STATEMENT_SETTER.setValues(pstmt, objects);
-            ResultSet resultSet = executeQuery(pstmt);
+            ResultSet resultSet = pstmt.executeQuery();
             List<T> results = new ArrayList<>();
             while (resultSet.next()) {
                 results.add(mapper.map(resultSet));
@@ -72,9 +104,5 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             throw new DataAccessException("데이터 접근 과정에서 문제가 발생하였습니다.", e);
         }
-    }
-
-    private ResultSet executeQuery(PreparedStatement preparedStatement) throws SQLException {
-        return preparedStatement.executeQuery();
     }
 }
