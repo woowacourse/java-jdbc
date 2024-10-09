89bcabb909a33d221fe1394102a908f4bc12d87c
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-10 00:40:47 +0900

    test: UserDaoTest 테스트마다 데이터 초기화되도록 변경

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -2,9 +2,11 @@ package com.techcourse.dao;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import java.util.List;
 import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -17,29 +19,32 @@ class UserDaoTest {
     void setup() {
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
         DataSource dataSource = DataSourceConfig.getInstance();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.write("TRUNCATE TABLE users RESTART IDENTITY");
         userDao = new UserDao(dataSource);
-        final var user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.save(user);
     }
 
     @Test
     void findAll() {
-        final var users = userDao.findAll();
+        userDao.save(new User("gugu", "password", "hkkang@woowahan.com"));
+        List<User> users = userDao.findAll();
 
         assertThat(users).isNotEmpty();
     }
 
     @Test
     void findById() {
-        final var user = userDao.findById(1L);
+        userDao.save(new User("gugu", "password", "hkkang@woowahan.com"));
+        User user = userDao.findById(1L);
 
         assertThat(user.getAccount()).isEqualTo("gugu");
     }
 
     @Test
     void findByAccount() {
-        final var account = "gugu";
-        final var user = userDao.findByAccount(account);
+        String account = "gugu";
+        userDao.save(new User(account, "password", "hkkang@woowahan.com"));
+        User user = userDao.findByAccount(account);
 
         assertThat(user.getAccount()).isEqualTo(account);
     }
@@ -47,23 +52,23 @@ class UserDaoTest {
     @Test
     void insert() {
         String account = "insert-gugu";
-        User user = new User(account, "password", "hkkang@woowahan.com");
-        userDao.save(user);
+        userDao.save(new User(account, "password", "hkkang@woowahan.com"));
 
-        User actual = userDao.findById(2L);
+        User actual = userDao.findById(1L);
 
         assertThat(actual.getAccount()).isEqualTo(account);
     }
 
     @Test
     void update() {
-        final var newPassword = "password99";
-        final var user = userDao.findById(1L);
+        String newPassword = "password99";
+        userDao.save(new User("gugu", "password", "hkkang@woowahan.com"));
+        User user = userDao.findById(1L);
         user.changePassword(newPassword);
 
         userDao.update(user);
 
-        final var actual = userDao.findById(1L);
+        User actual = userDao.findById(1L);
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
