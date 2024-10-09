ddf24b8b6cea6a512b8188274a9e961d31dbb580
unifolio0 <boho82ha@gmail.com>
2024-10-09 21:37:49 +0900

    chore: 불필요한 매개변수 제거

--- jdbc/src/main/java/com/interface21/jdbc/core/ArgumentPreparedStatementSetter.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/ArgumentPreparedStatementSetter.java
@@ -9,19 +9,19 @@ public class ArgumentPreparedStatementSetter implements PreparedStatementSetter 
 
     private final Object[] args;
 
-    public ArgumentPreparedStatementSetter(Object[] args) {
+    public ArgumentPreparedStatementSetter(Object ... args) {
         this.args = args;
     }
 
     @Override
     public void setValues(PreparedStatement preparedStatement) throws SQLException {
-        validateParameterCount(args, preparedStatement);
+        validateParameterCount(preparedStatement);
         for (int i = 0; i < args.length; i++) {
             preparedStatement.setObject(i + 1, args[i]);
         }
     }
 
-    private void validateParameterCount(Object[] args, PreparedStatement preparedStatement) throws SQLException {
+    private void validateParameterCount(PreparedStatement preparedStatement) throws SQLException {
         ParameterMetaData parameterMetaData = preparedStatement.getParameterMetaData();
         if (args.length != parameterMetaData.getParameterCount()) {
             throw new DataAccessException("파라미터 값의 개수가 올바르지 않습니다");
