2876c3023306ae38a5a7f1dd3fc04a1ca0a5d12c
BurningFalls <sherlocky31415@gmail.com>
2024-10-09 13:11:37 +0900

    test: ArgumentPreparedStatementSetter 테스트 작성

---
+++ jdbc/src/test/java/com/interface21/jdbc/core/ArgumentPreparedStatementSetterTest.java
@@ -0,0 +1,34 @@
+package com.interface21.jdbc.core;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ArgumentPreparedStatementSetterTest {
+
+    @DisplayName("PreparedStatement가 파라미터의 순서대로 값을 잘 저장한다.")
+    @Test
+    void testSetValues() throws SQLException {
+        // given
+        final Object[] params = { 1L, "a", "b", "c" };
+        final ArgumentPreparedStatementSetter pstmtSetter = new ArgumentPreparedStatementSetter(params);
+        final PreparedStatement pstmt = mock(PreparedStatement.class);
+
+        // when
+        pstmtSetter.setValues(pstmt);
+
+        // then
+        assertAll(
+                () -> verify(pstmt).setObject(1, params[0]),
+                () -> verify(pstmt).setObject(2, params[1]),
+                () -> verify(pstmt).setObject(3, params[2]),
+                () -> verify(pstmt).setObject(4, params[3])
+        );
+    }
+}
