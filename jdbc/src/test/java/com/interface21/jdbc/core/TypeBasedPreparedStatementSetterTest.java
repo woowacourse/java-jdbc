83cf50a007380c147f1a56f015d232291430ebf5
HyungHoKim00 <hkim1109@naver.com>
2024-10-09 14:41:06 +0900

    test: TypeBasedPSS 추가

---
+++ jdbc/src/test/java/com/interface21/jdbc/core/TypeBasedPreparedStatementSetterTest.java
@@ -0,0 +1,37 @@
+package com.interface21.jdbc.core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.mock;
+
+import java.sql.JDBCType;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TypeBasedPreparedStatementSetterTest {
+
+    @DisplayName("타입과 순서를 포함한 입력값에 따라 preparedStatement의 파라미터를 설정한다.")
+    @Test
+    void setValues() throws SQLException {
+        // given
+        Map<Integer, Object> parameters = new HashMap<>();
+        PreparedStatement ps = mock(PreparedStatement.class);
+        PreparedStatementSetter pss = new TypeBasedPreparedStatementSetter(
+                new SQLObject(1, "myungoh", JDBCType.VARCHAR),
+                new SQLObject(2, 25, JDBCType.INTEGER)
+        );
+        doAnswer((a) -> parameters.put(1, "myungoh")).when(ps).setObject(1, "myungoh", JDBCType.VARCHAR);
+        doAnswer((a) -> parameters.put(2, 25)).when(ps).setObject(2, 25, JDBCType.INTEGER);
+
+        // when
+        pss.setValues(ps);
+
+        // then
+        assertThat(parameters.get(1)).isEqualTo("myungoh");
+        assertThat(parameters.get(2)).isEqualTo(25);
+    }
+}
