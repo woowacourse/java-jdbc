aeca3a9e7c7d57399788c5a27eed189bd17957fe
masonkimseoul <masonkimseoul@gmail.com>
2024-10-10 20:08:24 +0900

    feat: PreparedStatementSetter 구현

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementSetter.java
@@ -0,0 +1,9 @@
+package com.interface21.jdbc.core;
+
+import java.sql.PreparedStatement;
+
+@FunctionalInterface
+public interface PreparedStatementSetter {
+
+    void setValues(PreparedStatement preparedStatement);
+}
