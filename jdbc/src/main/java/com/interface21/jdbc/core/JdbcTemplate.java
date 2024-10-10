fba99ce23a307bf74a8598b8a64be49e4f8c2a5c
yoonjuho <dbswn990@naver.com>
2024-10-11 02:06:12 +0900

    refactor: ResultMapper 클래스 분리

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -4,13 +4,15 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import com.interface21.dao.DataAccessException;
+
+import static com.interface21.jdbc.core.ResultMapper.multipleResultMapping;
+import static com.interface21.jdbc.core.ResultMapper.singleResultMapping;
 
 public class JdbcTemplate {
 
@@ -50,6 +52,12 @@ public class JdbcTemplate {
         }
     }
 
+    private <T> T queryResult(PreparedStatement preparedStatement, Function<ResultSet, T> resultMapper) throws SQLException {
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            return resultMapper.apply(resultSet);
+        }
+    }
+
     public int update(String sql, Object[] args) {
         return queryExecute(sql, (preparedStatement, query) -> {
             SqlParameterBinder.bind(preparedStatement, args);
@@ -74,54 +82,5 @@ public class JdbcTemplate {
             SqlParameterBinder.bind(preparedStatement, args);
             return queryResult(preparedStatement, resultSet -> singleResultMapping(rowMapper, resultSet));
         });
-    }
-
-    private <T> T queryResult(PreparedStatement preparedStatement, Function<ResultSet, T> resultMapper) throws SQLException {
-        try (ResultSet resultSet = preparedStatement.executeQuery()) {
-            return resultMapper.apply(resultSet);
-        }
-    }
-
-    private <T> List<T> multipleResultMapping(RowMapper<T> rowMapper, ResultSet resultSet) {
-        try {
-            return getMultipleResult(rowMapper, resultSet);
-        } catch (SQLException e) {
-            log.error("다중 결과 조회 중 SQL 예외가 발생했습니다. 예외 메시지: {}", e.getMessage(), e);
-            throw new DataAccessException("다중 결과 조회 중 오류가 발생했습니다. 원인: " + e.getMessage(), e);
-        }
-    }
-
-    private <T> List<T> getMultipleResult(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
-        List<T> results = new ArrayList<>();
-        while (resultSet.next()) {
-            results.add(rowMapper.mapRow(resultSet));
-        }
-        return results;
-    }
-
-    private <T> T singleResultMapping(RowMapper<T> rowMapper, ResultSet resultSet) {
-        try {
-            return getSingResult(rowMapper, resultSet);
-        } catch (SQLException e) {
-            log.error("단일 결과 조회 중 SQL 예외가 발생했습니다. 예외 메시지: {}", e.getMessage(), e);
-            throw new DataAccessException("단일 결과 조회 중 오류가 발생했습니다. 원인: " + e.getMessage(), e);
-        }
-    }
-
-    private <T> T getSingResult(RowMapper<T> rowMapper, ResultSet resultSet) throws SQLException {
-        if (resultSet.next()) {
-            T result = rowMapper.mapRow(resultSet);
-            validateSingleResult(resultSet);
-            return result;
-        }
-        log.error("단일 행 조회를 기대했으나, 조회된 데이터가 없습니다.");
-        throw new DataAccessException("단일 행 조회를 기대했지만, 조회된 행이 없습니다.");
-    }
-
-    private void validateSingleResult(ResultSet resultSet) throws SQLException {
-        if (resultSet.next()) {
-            log.error("단일 행 조회를 기대했으나, 여러 행이 조회되었습니다.");
-            throw new DataAccessException("단일 행 조회를 기대했지만, 여러 행이 조회되었습니다.");
-        }
     }
 }
