410ba5f5e5a41e28279b54344d17e82f6081d260
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-10-11 00:47:43 +0900

    test: JdbcTemplate 테스트 작성

--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,38 +1,90 @@
 package com.interface21.jdbc.core;
 
-import com.interface21.jdbc.config.DataSourceConfig;
-import com.interface21.jdbc.config.DatabasePopulatorUtils;
-import com.interface21.jdbc.result.SingleSelectResult;
+import com.interface21.jdbc.result.RowMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class JdbcTemplateTest {
 
     private JdbcTemplate jdbcTemplate;
+    private final RowMapper<TestObject> rowMapper =
+            resultSet -> new TestObject(resultSet.getLong("id"), resultSet.getString("content"));
 
     @BeforeEach
-    void setup() {
-        final DataSource dataSource = DataSourceConfig.getInstance();
-        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
+    void setup() throws SQLException {
+        final DataSource dataSource = getDataSource();
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     @Test
-    @DisplayName("Jdbc 템플릿을 통해 쿼리문을 실행 가능하다.")
-    void execute_sql() {
-        final var writeSql = "insert into test (content) values (?)";
-        jdbcTemplate.command(
-                writeSql, "sample"
-        );
+    @DisplayName("쿼리문을 실행해 객체를 생성한다.")
+    void query_for_object() {
+        final var readOne = "select * from test where id = ?";
+        final var result = jdbcTemplate.queryForObject(readOne, rowMapper, 1L);
 
-        final String selectSql = "select id, content from test where id = ?";
-        final SingleSelectResult result = jdbcTemplate.querySingle(selectSql, 1);
-        final String content = result.getColumnValue("content");
-        assertThat(content).isEqualTo("sample");
+        assertThat(result).isEqualTo(new TestObject(1L, "sample"));
     }
+
+    @Test
+    @DisplayName("쿼리문을 실행해 리스트를 생성한다.")
+    void query_for_list() {
+        final var readOne = "select * from";
+        final var result = jdbcTemplate.queryForList(readOne, rowMapper);
+
+        assertThat(result).containsAnyOf(
+                new TestObject(1L, "sample"),
+                new TestObject(2L, "sample2")
+        );
+    }
+
+    @Test
+    @DisplayName("쿼리문을 실행해 스트림을 생성한다.")
+    void query_for_stream() {
+        final var readOne = "select * from";
+        final var result = jdbcTemplate.queryForStream(readOne, rowMapper);
+
+        assertThat(result).containsAnyOf(
+                new TestObject(1L, "sample"),
+                new TestObject(2L, "sample2")
+        );
+    }
+
+
+    private DataSource getDataSource() throws SQLException {
+        final Connection connection = Mockito.mock(Connection.class);
+        final DataSource dataSource = Mockito.mock(DataSource.class);
+        final PreparedStatement preparedStatement = Mockito.mock(PreparedStatement.class);
+        Mockito.when(dataSource.getConnection())
+                .thenReturn(connection);
+        Mockito.when(connection.prepareStatement(Mockito.anyString()))
+                .thenReturn(preparedStatement);
+
+        final ResultSet resultSet = Mockito.mock(ResultSet.class);
+        Mockito.when(resultSet.next())
+                .thenReturn(true)
+                .thenReturn(true)
+                .thenReturn(false);  // 2개의 행 반환
+        Mockito.when(resultSet.getString("content"))
+                .thenReturn("sample")
+                .thenReturn("sample2");
+        Mockito.when(resultSet.getLong("id"))
+                .thenReturn(1L)
+                .thenReturn(2L);
+
+        Mockito.when(preparedStatement.executeQuery())
+                .thenReturn(resultSet);
+
+        return dataSource;
+    }
+
 }
