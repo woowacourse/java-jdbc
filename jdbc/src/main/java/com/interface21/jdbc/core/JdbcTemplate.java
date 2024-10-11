3695fdd6091c0f6ef6818a9d61a4f42b3a99e274
pricelees <priceelees@gmail.com>
2024-10-11 14:46:02 +0900

    refactor: 커스텀 예외인 DataAccessException 사용 및 예외 메시지 세분화

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -12,72 +12,74 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.interface21.dao.DataAccessException;
+
 public class JdbcTemplate {
 
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
+	private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
-    private final DataSource dataSource;
+	private final DataSource dataSource;
 
-    public JdbcTemplate(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+	public JdbcTemplate(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
-    public int update(String sql, PreparedStatementSetter pss) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pss.setValues(pstmt);
+	public int update(String sql, PreparedStatementSetter pss) {
+		try (Connection conn = dataSource.getConnection();
+			 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pss.setValues(pstmt);
 
-            return pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
+			return pstmt.executeUpdate();
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+			throw new DataAccessException("Failed to execute Query = " + sql, e);
+		}
+	}
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pss) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pss.setValues(pstmt);
+	public <T> T queryForObject(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pss) {
+		try (Connection conn = dataSource.getConnection();
+			 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pss.setValues(pstmt);
 
-            return fetchSingleResult(rowMapper, pstmt);
+			return fetchSingleResult(rowMapper, pstmt);
 
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+			throw new DataAccessException("Failed to execute Query = " + sql, e);
+		}
+	}
 
-    public <T> List<T> query(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pss) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            pss.setValues(pstmt);
+	public <T> List<T> query(String sql, RowMapper<T> rowMapper, PreparedStatementSetter pss) {
+		try (Connection conn = dataSource.getConnection();
+			 PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pss.setValues(pstmt);
 
-            return fetchResults(rowMapper, pstmt);
+			return fetchResults(rowMapper, pstmt);
 
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
+		} catch (SQLException e) {
+			log.error(e.getMessage(), e);
+			throw new DataAccessException("Failed to execute Query = " + sql, e);
+		}
+	}
 
-    private <T> T fetchSingleResult(RowMapper<T> rowMapper, PreparedStatement pstmt) throws SQLException {
-        List<T> results = fetchResults(rowMapper, pstmt);
-        if (results.isEmpty()) {
-            throw new RuntimeException("No result found");
-        }
-        if (results.size() > 1) {
-            throw new RuntimeException("Too many results found");
-        }
-        return results.getFirst();
-    }
+	private <T> T fetchSingleResult(RowMapper<T> rowMapper, PreparedStatement pstmt) throws SQLException {
+		List<T> results = fetchResults(rowMapper, pstmt);
+		if (results.isEmpty()) {
+			throw new DataAccessException("No result found");
+		}
+		if (results.size() > 1) {
+			throw new DataAccessException("More than One result found. Expected 1 but found " + results.size());
+		}
+		return results.getFirst();
+	}
 
-    private <T> List<T> fetchResults(RowMapper<T> rowMapper, PreparedStatement pstmt) throws SQLException {
-        try(ResultSet rs = pstmt.executeQuery()) {
-            List<T> results = new ArrayList<>();
-            while (rs.next()) {
-                results.add(rowMapper.mapRow(rs, rs.getRow()));
-            }
-            return results;
-        }
-    }
+	private <T> List<T> fetchResults(RowMapper<T> rowMapper, PreparedStatement pstmt) throws SQLException {
+		try (ResultSet rs = pstmt.executeQuery()) {
+			List<T> results = new ArrayList<>();
+			while (rs.next()) {
+				results.add(rowMapper.mapRow(rs, rs.getRow()));
+			}
+			return results;
+		}
+	}
 }
