2f18a5574e15331b6a0da1523c085de9af9ea58f
ay-eonii <naayen@naver.com>
2024-10-12 03:05:36 +0900

    refactor: 타입에 맞는 setter 사용하도록 변경

--- jdbc/src/test/java/com/interface21/jdbc/core/StatementParamSetterTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/StatementParamSetterTest.java
@@ -16,9 +16,10 @@ class StatementParamSetterTest {
     @DisplayName("파라미터를 순서대로 설정한다.")
     @Test
     void setParams() throws SQLException {
-        StatementParamSetter.setParams(prepareStatement, "potato", "hi");
+        StatementParamSetter.setParams(prepareStatement, "potato", "hi", 1);
 
-        verify(prepareStatement).setObject(1, "potato");
-        verify(prepareStatement).setObject(2, "hi");
+        verify(prepareStatement).setString(1, "potato");
+        verify(prepareStatement).setString(2, "hi");
+        verify(prepareStatement).setInt(3, 1);
     }
 }
