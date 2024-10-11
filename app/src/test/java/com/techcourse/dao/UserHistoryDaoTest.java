c91af0c32582b1ef95dad605946083bf1a8dfc50
Minjoo <m0522inju@gmail.com>
2024-10-11 21:32:17 +0900

    feat: 코드 변경에 따른 테스트코드 수정

--- app/src/test/java/com/techcourse/dao/UserHistoryDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserHistoryDaoTest.java
@@ -1,14 +1,12 @@
 package com.techcourse.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import java.sql.Connection;
 import com.interface21.jdbc.core.JdbcTemplate;
-import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.UserHistory;
-import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,12 +26,14 @@ class UserHistoryDaoTest {
     @DisplayName("UserHistory를 기록한다.")
     @Test
     void log() {
+        Connection connection = mock(Connection.class);
         UserHistory userHistory =
                 new UserHistory(1L, 1L, "daon", "daon1", "daon@wooteco.com", "liv");
 
-        userHistoryDao.log(userHistory);
+        userHistoryDao.log(connection, userHistory);
 
         verify(jdbcTemplate).update(
+                eq(connection),
                 eq("insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)"),
                 eq(userHistory.getUserId()),
                 eq(userHistory.getAccount()),
