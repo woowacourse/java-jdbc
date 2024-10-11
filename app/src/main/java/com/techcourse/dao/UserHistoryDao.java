c806a1abe6589faf122fbc24031652dbfb681c4f
Gyeongho Yang <chchch1213@naver.com>
2024-10-11 14:15:44 +0900

    feat: bind two different dao query with on transaction by connection

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -1,62 +1,40 @@
 package com.techcourse.dao;
 
-import com.techcourse.domain.UserHistory;
 import com.interface21.jdbc.core.JdbcTemplate;
+import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 public class UserHistoryDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserHistoryDao.class);
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
-    public UserHistoryDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
+    public UserHistoryDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public UserHistoryDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = null;
-    }
-
-    public void log(final UserHistory userHistory) {
-        final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            log.debug("query : {}", sql);
-
-            pstmt.setLong(1, userHistory.getUserId());
-            pstmt.setString(2, userHistory.getAccount());
-            pstmt.setString(3, userHistory.getPassword());
-            pstmt.setString(4, userHistory.getEmail());
-            pstmt.setObject(5, userHistory.getCreatedAt());
-            pstmt.setString(6, userHistory.getCreateBy());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
-        }
+    public void log(final UserHistory userHistory, Connection connection) {
+        jdbcTemplate.update(
+                """
+                        insert into user_history (
+                            user_id,
+                            account,
+                            password,
+                            email,
+                            created_at,
+                            created_by)
+                        values (?, ?, ?, ?, ?, ?)
+                        """,
+                connection,
+                userHistory.getUserId(),
+                userHistory.getAccount(),
+                userHistory.getPassword(),
+                userHistory.getEmail(),
+                userHistory.getCreatedAt(),
+                userHistory.getCreateBy()
+        );
     }
 }
