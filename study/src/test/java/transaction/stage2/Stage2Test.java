ab352489f83edb8cf69a5c3ff3177b2f087df319
ikjo39 <chi4321@naver.com>
2024-10-11 16:23:16 +0900

    test(Stage2Test): 정답 답변 형식 수정

--- study/src/test/java/transaction/stage2/Stage2Test.java
+++ study/src/test/java/transaction/stage2/Stage2Test.java
@@ -37,8 +37,8 @@ class Stage2Test {
 
     /**
      * 생성된 트랜잭션이 몇 개인가? 왜 그런 결과가 나왔을까?
-     * - 1 개이다.
-     * - propagation의 기본 설정은 REQUIRED는 외부 (논리적) 트랜잭션과 내부 (논리적) 트랜잭션이 동일한 물리적 트랜잭션에 매핑된다.
+     * -> 1 개이다.
+     * -> propagation의 기본 설정은 REQUIRED는 외부 (논리적) 트랜잭션과 내부 (논리적) 트랜잭션이 동일한 물리적 트랜잭션에 매핑된다.
      * 때문에 기본의 saveFirstTransactionWithRequired 트랜잭션에 병합되어 1개만 존재하게 되었다.
      */
     @Test
@@ -53,7 +53,8 @@ class Stage2Test {
 
     /**
      * 생성된 트랜잭션이 몇 개인가? 왜 그런 결과가 나왔을까?
-     * 2개이다. REQUIRED_NEW의 경우 트랜잭션의 경계에서 이미 진행 중인 트랜잭션이 있을 때와 없을 때와 관련없이 독립된 물리적 트랜잭션에 매핑된다.
+     * -> 2개이다.
+     * -> REQUIRED_NEW의 경우 트랜잭션의 경계에서 이미 진행 중인 트랜잭션이 있을 때와 없을 때와 관련없이 독립된 물리적 트랜잭션에 매핑된다.
      */
     @Test
     void testRequiredNew() {
@@ -69,7 +70,7 @@ class Stage2Test {
     /**
      * firstUserService.saveAndExceptionWithRequiredNew()에서 강제로 예외를 발생시킨다. REQUIRES_NEW 일 때 예외로 인한 롤백이 발생하면서 어떤 상황이 발생하는
      * 지 확인해보자.
-     * 서로 독립된 물리적 트랜잭션에 매핑되어 있어 이미 커밋이 진행된 내부 트랜잭션으로 인해 DB에 레코드가 1개 존재한다.
+     * -> 서로 독립된 물리적 트랜잭션에 매핑되어 있어 이미 커밋이 진행된 내부 트랜잭션으로 인해 DB에 레코드가 1개 존재한다.
      */
     @Test
     void testRequiredNewWithRollback() {
@@ -84,8 +85,8 @@ class Stage2Test {
     /**
      * FirstUserService.saveFirstTransactionWithSupports() 메서드를 보면 @Transactional이 주석으로 되어 있다. 주석인 상태에서 테스트를 실행했을 때와 주석을
      * 해제하고 테스트를 실행했을 때 어떤 차이점이 있는지 확인해보자.
-     * - 주석인 상태에서 테스트 한 경우 트랜잭션이 출력은 되나 활성화가 되지 않는다. 즉, 비트랜잭션으로 로직을 수행한다.
-     * - 주석을 해제한 경우 외부 트랜잭션과 하나의 물리 트랜잭션으로 매핑되어 로직이 수행된다.
+     * -> 주석인 상태에서 테스트 한 경우 트랜잭션이 출력은 되나 활성화가 되지 않는다. 즉, 비트랜잭션으로 로직을 수행한다.
+     * -> 주석을 해제한 경우 외부 트랜잭션과 하나의 물리 트랜잭션으로 매핑되어 로직이 수행된다.
      */
     @Test
     void testSupports() {
@@ -100,8 +101,8 @@ class Stage2Test {
     /**
      * FirstUserService.saveFirstTransactionWithMandatory() 메서드를 보면 @Transactional이 주석으로 되어 있다. 주석인 상태에서 테스트를 실행했을 때와
      * 주석을 해제하고 테스트를 실행했을 때 어떤 차이점이 있는지 확인해보자. SUPPORTS와 어떤 점이 다른지도 같이 챙겨보자.
-     * - 주석인 상태에서 테스트 한 경우 IllegalTransactionStateException 예외가 발생한다. (SUPPORTS와 다른 점)
-     * - 주석을 해제한 경우 외부 트랜잭션과 하나의 물리 트랜잭션으로 매핑되어 로직이 수행된다.
+     * -> 주석인 상태에서 테스트 한 경우 IllegalTransactionStateException 예외가 발생한다. (SUPPORTS와 다른 점)
+     * -> 주석을 해제한 경우 외부 트랜잭션과 하나의 물리 트랜잭션으로 매핑되어 로직이 수행된다.
      */
     @Test
     void testMandatory() {
@@ -116,12 +117,11 @@ class Stage2Test {
     /**
      * 아래 테스트는 몇 개의 물리적 트랜잭션이 동작할까? FirstUserService.saveFirstTransactionWithNotSupported() 메서드의 @Transactional을 주석
      * 처리하자. 다시 테스트를 실행하면 몇 개의 물리적 트랜잭션이 동작할까?
-     * - 둘 다 2 개의 물리 트랜잭션이 동작한다.
-     * - NOT_SUPPORTED 옵션은 비트랜잭션으로 동작하여 각각을 물리 트랜잭션으로 동작한다.
+     * -> 둘 다 2 개의 물리 트랜잭션이 동작한다.
+     * -> NOT_SUPPORTED 옵션은 비트랜잭션으로 동작하여 각각을 물리 트랜잭션으로 동작한다.
      *
      * 스프링 공식 문서에서 물리적 트랜잭션과 논리적 트랜잭션의 차이점이 무엇인지 찾아보자.
-     * - 스프링 공식문서를 찾아보았으나 어느 부분에 두 차이를 명시했는지 못 찾았다.
-     * 리뷰어님 아신다면 알려주세요.
+     * -> 리뷰어 아토가 제공해준 공식문서를 참고하여 논리적 트랜잭션은 Transactional이 붙은 메서드 단위, 물리적 트랜잭션은 최종적으로 DB에 커밋/롤백되는 트랜잭션을 간접적으로 그림으로 설명해주었다.
      */
     @Test
     void testNotSupported() {
@@ -136,11 +136,9 @@ class Stage2Test {
 
     /**
      * 아래 테스트는 왜 실패할까? FirstUserService.saveFirstTransactionWithNested() 메서드의 @Transactional을 주석 처리하면 어떻게 될까?
-     *
-     * Nested의 경우 외부 트랜잭션 안에서 논리적 트랜잭션을 생성한다. 떄문에 전체 트랜잭션을 롤백하는 대신 savepoint라는 개념으로
-     * 특정 지점까지만 롤백할 수 있다. 하지만 JPA는 이러한 기능을 지원하지 않아 예외가 발생한다.
-     *
-     * 기존 트랜잭션이 없다면 REQUIRED와 동일하게 동작하여 정상 동작한다.
+     * -> Nested의 경우 외부 트랜잭션 안에서 논리적 트랜잭션을 생성한다. 떄문에 전체 트랜잭션을 롤백하는 대신 savepoint라는 개념으로
+     * -> 특정 지점까지만 롤백할 수 있다. 하지만 JPA는 이러한 기능을 지원하지 않아 예외가 발생한다.
+     * -> 기존 트랜잭션이 없다면 REQUIRED와 동일하게 동작하여 정상 동작한다.
      */
     @Test
     void testNested() {
@@ -154,8 +152,7 @@ class Stage2Test {
 
     /**
      * 마찬가지로 @Transactional을 주석처리하면서 관찰해보자.
-     *
-     * Propagation NEVER 설정은 비트랜잭션으로 동작하되 트랜잭션이 존재하면 IllegalTransactionStateException 예외가 발생한다.
+     * -> Propagation NEVER 설정은 비트랜잭션으로 동작하되 트랜잭션이 존재하면 IllegalTransactionStateException 예외가 발생한다.
      */
     @Test
     void testNever() {
