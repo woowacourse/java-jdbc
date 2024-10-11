3d6da6813bfa6e3a1b28616e4eae2615cfbe5c84
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-11 09:43:17 +0900

    refactor: 생성자 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcException.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcException.java
@@ -2,6 +2,10 @@ package com.interface21.jdbc.core;
 
 public class JdbcException extends RuntimeException {
 
+    public JdbcException(String message) {
+        super(message);
+    }
+
     public JdbcException(String message, Throwable cause) {
         super(message, cause);
     }
