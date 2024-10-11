74589b1b40edb82e9d35444b0b7974aa42f1c4f0
HoYeon <114469256+hoyeonyy@users.noreply.github.com>
2024-10-11 14:09:13 +0900

    [1단계 - JDBC 라이브러리 구현하기] - 호기(조호연) 미션 제출합니다. (#676)
    * feat: 행을 반환하는 RowMapper 생성
    
    * feat: 결과를 반환하는 ResultMapper 생성
    
    * feat: 쿼리의 실행을 담당하는 QueryExecution 생성
    
    * feat: ParameterBinder 생성
    
    * refactor: JdbcTemplate 수정
    
    * refactor: UserDao 수정
    
    * refactor: final 키워드 삭제
    
    * refactor: 에러 핸들링 정보 추가
    
    * refactor: 매직넘버 제거

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/ParameterBinder.java
@@ -0,0 +1,27 @@
+package com.interface21.jdbc.core;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.interface21.dao.DataAccessException;
+
+public class ParameterBinder {
+
+	private static final int FIRST_INDEX = 1;
+
+	public void bindParameters(PreparedStatement preparedStatement, Object... args) {
+		AtomicInteger index = new AtomicInteger(FIRST_INDEX);
+		Arrays.stream(args)
+			.forEach(arg -> bindParameter(preparedStatement, index.getAndIncrement(), arg));
+	}
+
+	private void bindParameter(PreparedStatement preparedStatement, int index, Object arg) {
+		try {
+			preparedStatement.setObject(index, arg);
+		} catch (SQLException e) {
+			throw new DataAccessException("파라미터 값이 잘못 되었습니다.", e);
+		}
+	}
+}
---
+++ jdbc/src/main/java/com/interface21/jdbc/core/ResultMapper.java
@@ -0,0 +1,46 @@
+package com.interface21.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ResultMapper {
+
+	private static final int FIRST_ROW = 1;
+
+	public <T> Optional<T> findResult(ResultSet resultSet, RowMapper<T> rowMapper) {
+		if (existsNext(resultSet)) {
+			return Optional.ofNullable(getResult(resultSet, rowMapper, 1));
+		}
+		return Optional.empty();
+	}
+
+	public <T> List<T> getResults(ResultSet resultSet, RowMapper<T> rowMapper) {
+		int rowNum = FIRST_ROW;
+		List<T> results = new ArrayList<>();
+
+		while (existsNext(resultSet)) {
+			results.add(getResult(resultSet, rowMapper, rowNum++));
+		}
+
+		return results;
+	}
+
+	private boolean existsNext(ResultSet resultSet) {
+		try {
+			return resultSet.next();
+		} catch (Exception e) {
+			throw new IllegalStateException("결과 매핑 과정에서 실패했습니다.", e);
+		}
+	}
+
+	public <T> T getResult(ResultSet resultSet, RowMapper<T> rowMapper, int rowNum) {
+		try {
+			return rowMapper.mapRow(resultSet, rowNum);
+		} catch (SQLException e) {
+			throw new IllegalStateException("결과 매핑 과정에서 실패했습니다.", e);
+		}
+	}
+}
