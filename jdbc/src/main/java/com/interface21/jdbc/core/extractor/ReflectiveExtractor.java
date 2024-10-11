f54848ef0a4b47d29980f72d41599f1ac59a186b
woojong <dwax1324@gmail.com>
2024-10-11 15:54:22 +0900

    feat: Mappper의 리플렉션을 ReflectiveExtractor로 이동

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/extractor/ReflectiveExtractor.java
@@ -0,0 +1,37 @@
+package com.interface21.jdbc.core.extractor;
+
+import java.lang.reflect.Field;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.annotation.Nonnull;
+
+public final class ReflectiveExtractor<T> extends ResultSetExtractor<T> {
+    private final Class<T> clazz;
+
+    public ReflectiveExtractor(ResultSet resultSet, Class<T> clazz) {
+        super(resultSet);
+        this.clazz = clazz;
+    }
+
+    @Nonnull
+    @Override
+    public T extractOne() throws SQLException {
+        try {
+            T instance = clazz.getConstructor().newInstance();
+            injectValues(resultSet, instance);
+            return instance;
+        } catch (ReflectiveOperationException e) {
+            throw new SQLException(e);
+        }
+    }
+
+    private void injectValues(ResultSet resultSet, T instance) throws SQLException, IllegalAccessException {
+        Field[] declaredFields = clazz.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            T value = resultSet.getObject(declaredField.getName(), clazz);
+            declaredField.setAccessible(true);
+            declaredField.set(instance, value);
+        }
+    }
+}
