7da01eaa3e661e6fb6177c9c9b8e4f39e6123d67
Minjoo <m0522inju@gmail.com>
2024-10-11 21:31:02 +0900

    feat: UserService에서 트랜잭션 사용하도록 수정

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,5 +1,10 @@
 package com.techcourse.service;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.interface21.dao.DataAccessException;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -9,24 +14,39 @@ public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(UserDao userDao, UserHistoryDao userHistoryDao) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.dataSource = DataSourceConfig.getInstance();
     }
 
-    public User findById(final long id) {
+    public User findById(long id) {
         return userDao.findById(id);
     }
 
-    public void insert(final User user) {
+    public void insert(User user) {
         userDao.insert(user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(long id, String newPassword, String createBy) {
+        try (Connection connection = dataSource.getConnection()) {
+            try {
+                connection.setAutoCommit(false);
+
+                User user = findById(id);
+                user.changePassword(newPassword);
+                userDao.update(connection, user);
+                userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                throw new DataAccessException("Transaction failed and was rolled back", e);
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to acquire or manage database connection", e);
+        }
     }
 }
