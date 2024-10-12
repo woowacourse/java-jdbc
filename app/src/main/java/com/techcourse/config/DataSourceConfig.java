084170fdde64670b11cdb80b5cc000c34e3b51cf
fram1998 <koust6u@pusan.ac.kr>
2024-10-12 23:37:56 +0900

    refactor: 미사용 코드 삭제

--- app/src/main/java/com/techcourse/config/DataSourceConfig.java
+++ app/src/main/java/com/techcourse/config/DataSourceConfig.java
@@ -1,28 +1,14 @@
 package com.techcourse.config;
 
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.Objects;
 
 import javax.sql.DataSource;
 
 import org.h2.jdbcx.JdbcDataSource;
 
-import com.interface21.dao.DataAccessException;
-
 public class DataSourceConfig {
 
-    private static final Connection txConnection;
     private static DataSource INSTANCE;
-
-    static {
-        INSTANCE = createJdbcDataSource();
-        try {
-            txConnection = INSTANCE.getConnection();
-        } catch (SQLException e) {
-            throw new DataAccessException("고정 커넥션 생성 중 예외 발생");
-        }
-    }
 
     private DataSourceConfig() {
     }
@@ -32,10 +18,6 @@ public class DataSourceConfig {
             INSTANCE = createJdbcDataSource();
         }
         return INSTANCE;
-    }
-
-    public static Connection getTxConnection() {
-        return txConnection;
     }
 
     private static JdbcDataSource createJdbcDataSource() {
