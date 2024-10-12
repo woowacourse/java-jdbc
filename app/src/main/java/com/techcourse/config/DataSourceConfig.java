ff7cbca9cab578c6c88e54dbbcfc19d4238bc493
libienz <leekh10195@gmail.com>
2024-10-12 15:53:44 +0900

    feat: DataSourceConfig 커넥션 획득 메서드 작성

--- app/src/main/java/com/techcourse/config/DataSourceConfig.java
+++ app/src/main/java/com/techcourse/config/DataSourceConfig.java
@@ -1,8 +1,9 @@
 package com.techcourse.config;
 
-import org.h2.jdbcx.JdbcDataSource;
-
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Objects;
+import org.h2.jdbcx.JdbcDataSource;
 
 public class DataSourceConfig {
 
@@ -15,6 +16,14 @@ public class DataSourceConfig {
         return INSTANCE;
     }
 
+    public static Connection getConnection() {
+        try {
+            return getInstance().getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     private static JdbcDataSource createJdbcDataSource() {
         final var jdbcDataSource = new JdbcDataSource();
         jdbcDataSource.setUrl("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;");
@@ -23,5 +32,6 @@ public class DataSourceConfig {
         return jdbcDataSource;
     }
 
-    private DataSourceConfig() {}
+    private DataSourceConfig() {
+    }
 }
