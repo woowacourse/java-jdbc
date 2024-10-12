fed248e877fa9659320be6fa13e02fc9e13790dd
tackyu <tack970913@gmail.com>
2024-10-13 00:22:30 +0900

    feat: 트랜잭션 경계 설정

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,14 +1,19 @@
 package com.techcourse.service;
 
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource = DataSourceConfig.getInstance();
 
     public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
         this.userDao = userDao;
@@ -23,10 +28,22 @@ public class UserService {
         userDao.insert(user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
+    public void changePassword(final long id, final String newPassword, final String createBy) throws SQLException {
         final var user = findById(id);
         user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+            userDao.update(user, connection);
+            userHistoryDao.log(new UserHistory(user, createBy), connection);
+            connection.commit();
+        } catch (Throwable e) {
+            connection.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            connection.close();
+        }
     }
 }
