2715b4c915f46f6d30029af2604936b907ec4133
juha <khabh@naver.com>
2024-10-12 19:01:35 +0900

    feat: TransactionSynchronizationManager 구현

---
+++ jdbc/src/test/java/com/interface21/transaction/support/TransactionSynchronizationManagerTest.java
@@ -0,0 +1,51 @@
+package com.interface21.transaction.support;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.interface21.dao.DataAccessException;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+public class TransactionSynchronizationManagerTest {
+
+    private DataSource dataSource;
+    private Connection connection;
+
+    @BeforeEach
+    public void setUp() {
+        dataSource = Mockito.mock(DataSource.class);
+        connection = Mockito.mock(Connection.class);
+
+        TransactionSynchronizationManager.unbindResource(dataSource);
+    }
+
+    @Test
+    public void bindResource() {
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        assertThat(TransactionSynchronizationManager.getResource(dataSource))
+                .isEqualTo(connection);
+    }
+
+    @Test
+    public void resourceAlreadyBound() {
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        assertThatThrownBy(() -> TransactionSynchronizationManager.bindResource(dataSource, connection))
+                .isInstanceOf(DataAccessException.class)
+                .hasMessage("Transaction already started for this datasource");
+    }
+
+    @Test
+    public void testUnbindResource() {
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        TransactionSynchronizationManager.unbindResource(dataSource);
+
+        assertThat(TransactionSynchronizationManager.getResource(dataSource)).isNull();
+    }
+}
