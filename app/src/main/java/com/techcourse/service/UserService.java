9abfe3ce1bcb073ab8daa057d2cee7ff9f430018
hoyeonyy <highoption@naver.com>
2024-10-12 21:40:31 +0900

    feat: UserService 로그 추가

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,5 +1,8 @@
 package com.techcourse.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -7,6 +10,7 @@ import com.techcourse.domain.UserHistory;
 
 public class UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
@@ -16,15 +20,19 @@ public class UserService {
     }
 
     public User findById(final long id) {
+        log.info("{} 조회", id);
         return userDao.findById(id);
     }
 
     public void insert(final User user) {
+        log.info("유저 저장 id = {}", user.getId());
         userDao.insert(user);
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
         final var user = findById(id);
+        log.info("비밀번호 변경 요청 id = {}", user.getId());
+
         user.changePassword(newPassword);
         userDao.update(user);
         userHistoryDao.log(new UserHistory(user, createBy));
