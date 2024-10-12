cf406b0e5f3702c381b01e2165637a915be40b1e
hyeonjilee <skylar12200@gmail.com>
2024-10-12 17:33:05 +0900

    refactor: PreparedStatementSetter 구현

---
+++ jdbc/src/main/java/com/interface21/jdbc/PreparedStatementSetter.java
@@ -0,0 +1,10 @@
+package com.interface21.jdbc;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface PreparedStatementSetter {
+
+    void setValues(PreparedStatement pstmt) throws SQLException;
+}
