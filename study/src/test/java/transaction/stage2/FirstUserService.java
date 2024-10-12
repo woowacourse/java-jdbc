072d4fb9934242d2bf756d4fe6e46b904f14b836
ehtjsv2 <ehtjsv2@naver.com>
2024-10-12 15:43:49 +0900

    study: transaction stage2완료

--- study/src/test/java/transaction/stage2/FirstUserService.java
+++ study/src/test/java/transaction/stage2/FirstUserService.java
@@ -37,6 +37,7 @@ public class FirstUserService {
     @Transactional(propagation = Propagation.REQUIRED)
     public Set<String> saveFirstTransactionWithRequired() {
         final var firstTransactionName = TransactionSynchronizationManager.getCurrentTransactionName();
+        System.out.println("first: "+firstTransactionName);
         userRepository.save(User.createTest());
         logActualTransactionActive();
 
@@ -66,7 +67,7 @@ public class FirstUserService {
         throw new RuntimeException();
     }
 
-//    @Transactional(propagation = Propagation.REQUIRED)
+    @Transactional(propagation = Propagation.REQUIRED)
     public Set<String> saveFirstTransactionWithSupports() {
         final var firstTransactionName = TransactionSynchronizationManager.getCurrentTransactionName();
         userRepository.save(User.createTest());
@@ -77,7 +78,7 @@ public class FirstUserService {
         return of(firstTransactionName, secondTransactionName);
     }
 
-//    @Transactional(propagation = Propagation.REQUIRED)
+    @Transactional(propagation = Propagation.REQUIRED)
     public Set<String> saveFirstTransactionWithMandatory() {
         final var firstTransactionName = TransactionSynchronizationManager.getCurrentTransactionName();
         userRepository.save(User.createTest());
