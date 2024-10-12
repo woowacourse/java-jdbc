a8f3d13a2496ec9372d1b55dc64d86d8ac8ebd42
Jang Hyeok-su <76612738+zangsu@users.noreply.github.com>
2024-10-12 19:31:36 +0900

    [2단계 - 리팩터링] 짱수(장혁수) 미션 제출합니다. (#775)
    * study: stage1 학습
    
    * refactor: javax -> jakarta 로 패키지 변경
    
    * study: transaction stage1
    
    * study: transaction stage2
    
    * fix: UserDao 생성자 변경에 따른 코드 수정
    
    * fix: 복수 결과 조회 검증 수정
    
    * fix: 기본 유저가 중복 저장되지 않도록 코드 변경
    
    * fix: 중복 데이터 조회 테스트 수정

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,12 +1,15 @@
 package com.techcourse.dao;
 
-import com.techcourse.config.DataSourceConfig;
-import com.techcourse.domain.User;
-import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.interface21.dao.NoResultFoundException;
+import com.interface21.jdbc.core.JdbcTemplate;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 
 class UserDaoTest {
 
@@ -16,15 +19,20 @@ class UserDaoTest {
     void setup() {
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
 
-        userDao = new UserDao(DataSourceConfig.getInstance());
+        userDao = new UserDao(new JdbcTemplate(DataSourceConfig.getInstance()));
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.insert(user);
+
+        try {
+            userDao.findByAccount("gugu");
+        } catch (NoResultFoundException e) {
+            userDao.insert(user);
+        }
     }
 
     @Test
     void findAll() {
         final var users = userDao.findAll();
-
+        users.forEach(System.out::println);
         assertThat(users).isNotEmpty();
     }
 
--- study/src/test/java/connectionpool/stage1/Stage1Test.java
+++ study/src/test/java/connectionpool/stage1/Stage1Test.java
@@ -9,6 +9,8 @@ import java.sql.SQLException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import javax.sql.DataSource;
+
 class Stage1Test {
 
     private static final String H2_URL = "jdbc:h2:./test;DB_CLOSE_DELAY=-1";
@@ -28,16 +30,21 @@ class Stage1Test {
      */
     @Test
     void testJdbcConnectionPool() throws SQLException {
-        final JdbcConnectionPool jdbcConnectionPool = JdbcConnectionPool.create(H2_URL, USER, PASSWORD);
+        final DataSource jdbcConnectionPool = JdbcConnectionPool.create(H2_URL, USER, PASSWORD);
+        /*
+        이건 h2 의 클래스.
+        ConnectionPool 에 대한 인터페이스라던지 그런건 없을까?
+        -> ConnectionPool 역시 getConnection 으로 `Connection` 을 얻어오기 위한 객체라서 `DataSource` 로 추상화해서 사용해도 될 것 같음
+         */
 
-        assertThat(jdbcConnectionPool.getActiveConnections()).isZero();
+        assertThat(((JdbcConnectionPool) jdbcConnectionPool).getActiveConnections()).isZero();
         try (final var connection = jdbcConnectionPool.getConnection()) {
             assertThat(connection.isValid(1)).isTrue();
-            assertThat(jdbcConnectionPool.getActiveConnections()).isEqualTo(1);
+            assertThat(((JdbcConnectionPool) jdbcConnectionPool).getActiveConnections()).isEqualTo(1);
         }
-        assertThat(jdbcConnectionPool.getActiveConnections()).isZero();
+        assertThat(((JdbcConnectionPool) jdbcConnectionPool).getActiveConnections()).isZero();
 
-        jdbcConnectionPool.dispose();
+        ((JdbcConnectionPool) jdbcConnectionPool).dispose();
     }
 
     /**
@@ -54,20 +61,26 @@ class Stage1Test {
      *
      * HikariCP의 pool size는 몇으로 설정하는게 좋을까?
      * https://github.com/brettwooldridge/HikariCP/wiki/About-Pool-Sizing
+     * -> 코어 개수보다 스레드 개수가 더 많으면 타임 슬라이싱을 통해 스레드를 지원한다. 때문에 외려 더 느려질 수 있다. + alpha
      *
      * HikariCP를 사용할 때 적용하면 좋은 MySQL 설정
      * https://github.com/brettwooldridge/HikariCP/wiki/MySQL-Configuration
      */
     @Test
     void testHikariCP() {
-        final var hikariConfig = new HikariConfig();
+        /*final var hikariConfig = new HikariConfig();
+        //essential
         hikariConfig.setJdbcUrl(H2_URL);
         hikariConfig.setUsername(USER);
         hikariConfig.setPassword(PASSWORD);
-        hikariConfig.setMaximumPoolSize(5);
         hikariConfig.addDataSourceProperty("cachePrepStmts", "true");
         hikariConfig.addDataSourceProperty("prepStmtCacheSize", "250");
         hikariConfig.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        hikariConfig.addDataSourceProperty("useServerPrepStmts", "true");
+        hikariConfig.setMaximumPoolSize(5);
+        -> Config 파일을 properties 로 관리해 어플리케이션 코드의 변경을 방지할 수 있다.
+        */
+        final var hikariConfig = new HikariConfig("/Users/mac/Desktop/zangsu/wooteco/Level4/java-jdbc/study/src/test/java/connectionpool/stage1/hikari.properties");
 
         final var dataSource = new HikariDataSource(hikariConfig);
         final var properties = dataSource.getDataSourceProperties();
@@ -76,6 +89,7 @@ class Stage1Test {
         assertThat(properties.getProperty("cachePrepStmts")).isEqualTo("true");
         assertThat(properties.getProperty("prepStmtCacheSize")).isEqualTo("250");
         assertThat(properties.getProperty("prepStmtCacheSqlLimit")).isEqualTo("2048");
+        assertThat(properties.getProperty("useServerPrepStmts")).isEqualTo("true");
 
         dataSource.close();
     }
