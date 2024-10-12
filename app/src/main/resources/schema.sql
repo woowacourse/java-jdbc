4835ca96d2c24642a844b2305e1e7794da3cd8c8
gyungchan Jo <namejgc@naver.com>
2024-10-12 17:43:28 +0900

    refactor: schema 변경

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS users;
 
-create table if not exists users (
+create table users (
     id bigint auto_increment,
     account varchar(100) not null,
     password varchar(100) not null,
@@ -8,7 +8,7 @@ create table if not exists users (
     primary key(id)
 );
 
-create table if not exists user_history (
+create table user_history (
     id bigint auto_increment,
     user_id bigint not null,
     account varchar(100) not null,
