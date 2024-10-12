7b44a9e4fa900d7972c194b1cd096010c5e438b6
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-10-12 15:34:06 +0900

    refactor: 트랜잭션 관련 부분 분리 및 인덴트 1 유지

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -7,7 +7,9 @@ import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.function.Consumer;
 import javax.sql.DataSource;
+import org.h2.util.JdbcUtils;
 
 public class UserService {
 
@@ -30,31 +32,35 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        Connection connection = null;
-        try  {
-            connection = dataSource.getConnection();
-            connection.setAutoCommit(false);
-
+        processInTransaction(connection -> {
             final var user = findById(id);
             user.changePassword(newPassword);
             userDao.update(user, connection);
             userHistoryDao.log(new UserHistory(user, createBy), connection);
+        });
+    }
 
+    private void processInTransaction(final Consumer<Connection> consumer) {
+        Connection connection = null;
+        try  {
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+            consumer.accept(connection);
             connection.commit();
         } catch (SQLException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ignored) {}
+            rollback(connection);
             throw new DataAccessException(e);
         } catch (RuntimeException e) {
-            try {
-                connection.rollback();
-            } catch (SQLException ignored) {}
+            rollback(connection);
             throw e;
         } finally {
-            try {
-                connection.close();
-            } catch (SQLException ignored) {}
+            JdbcUtils.closeSilently(connection);
         }
+    }
+
+    private void rollback(final Connection connection) {
+        try {
+            connection.rollback();
+        } catch (SQLException ignored) {}
     }
 }
