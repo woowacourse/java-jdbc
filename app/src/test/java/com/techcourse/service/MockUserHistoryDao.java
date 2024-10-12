45458b31bfc1349ea247ef73fbbae61e3fae959c
tackyu <tack970913@gmail.com>
2024-10-13 00:16:22 +0900

    refactor: 테스트 코드 수정

--- app/src/test/java/com/techcourse/service/MockUserHistoryDao.java
+++ app/src/test/java/com/techcourse/service/MockUserHistoryDao.java
@@ -1,9 +1,9 @@
 package com.techcourse.service;
 
+import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.UserHistory;
-import com.interface21.dao.DataAccessException;
-import com.interface21.jdbc.core.JdbcTemplate;
+import java.sql.Connection;
 
 public class MockUserHistoryDao extends UserHistoryDao {
 
@@ -12,7 +12,7 @@ public class MockUserHistoryDao extends UserHistoryDao {
     }
 
     @Override
-    public void log(final UserHistory userHistory) {
-        throw new DataAccessException();
+    public void log(final UserHistory userHistory, final Connection connection) {
+        throw new RuntimeException();
     }
 }
