725c363a3c03289764931382904eac71c4df9ff5
Do Yeop Kim <113661364+Dobby-Kim@users.noreply.github.com>
2024-10-12 16:28:55 +0900

    refactor(JdbcTemplate): 요구사항에 맞게 리팩터링 적용

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.techcourse.dao;
 
 import com.interface21.jdbc.core.JdbcTemplate;
+import com.interface21.jdbc.core.PreparedStatementSetter;
 import com.interface21.jdbc.core.RowMapper;
 import com.techcourse.domain.User;
 import java.util.List;
@@ -9,7 +10,7 @@ import javax.sql.DataSource;
 public class UserDao {
 
     private final JdbcTemplate jdbcTemplate;
-    private final RowMapper<User> userRowMapper = (rs, rowNum) -> new User(
+    private final RowMapper<User> userRowMapper = rs -> new User(
             rs.getLong("id"),
             rs.getString("account"),
             rs.getString("password"),
@@ -26,31 +27,45 @@ public class UserDao {
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        PreparedStatementSetter pstmtSetter = pstmt -> {
+            pstmt.setString(1, user.getAccount());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getEmail());
+        };
 
-        jdbcTemplate.write(sql, user.getAccount(), user.getPassword(), user.getEmail());
+        jdbcTemplate.update(sql, pstmtSetter);
     }
 
     public void update(final User user) {
         final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        PreparedStatementSetter pstmtSetter = pstmt -> {
+            pstmt.setString(1, user.getAccount());
+            pstmt.setString(2, user.getPassword());
+            pstmt.setString(3, user.getEmail());
+            pstmt.setLong(4, user.getId());
+        };
 
-        jdbcTemplate.write(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
+        jdbcTemplate.update(sql, pstmtSetter);
     }
 
     public List<User> findAll() {
         final var sql = "select * from users";
 
-        return jdbcTemplate.readAll(sql, userRowMapper);
+        return jdbcTemplate.queryAll(sql, pstmt -> {
+        }, userRowMapper);
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
+        final PreparedStatementSetter pstmtSetter = pstmt -> pstmt.setLong(1, id);
 
-        return jdbcTemplate.read(sql, userRowMapper, id);
+        return jdbcTemplate.query(sql, pstmtSetter, userRowMapper);
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
+        final PreparedStatementSetter pstmtSetter = pstmt -> pstmt.setString(1, account);
 
-        return jdbcTemplate.read(sql, userRowMapper, account);
+        return jdbcTemplate.query(sql, pstmtSetter, userRowMapper);
     }
 }
