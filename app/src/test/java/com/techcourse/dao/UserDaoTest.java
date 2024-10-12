7bf65a9a84f0f73beb91fdb8a933ed65206f7271
zangsu <zangsu_@naver.com>
2024-10-12 15:31:48 +0900

    fix: 기본 유저가 중복 저장되지 않도록 코드 변경

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,13 +1,15 @@
 package com.techcourse.dao;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.interface21.dao.NoResultFoundException;
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 class UserDaoTest {
 
@@ -19,13 +21,18 @@ class UserDaoTest {
 
         userDao = new UserDao(new JdbcTemplate(DataSourceConfig.getInstance()));
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.insert(user);
+
+        try {
+            userDao.findByAccount("gugu");
+        } catch (NoResultFoundException e) {
+            userDao.insert(user);
+        }
     }
 
     @Test
     void findAll() {
         final var users = userDao.findAll();
-
+        users.forEach(System.out::println);
         assertThat(users).isNotEmpty();
     }
 
