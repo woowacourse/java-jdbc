c6379e174dfd28d83e0ee6928a53f8669c3e9b91
Hyeonji <110809927+skylar1220@users.noreply.github.com>
2024-10-12 13:47:45 +0900

    [1단계 - JDBC 라이브러리 구현하기] 커비(이현지) 미션 제출합니다. (#680)
    * docs: 기능 구현 목록 작성
    
    * feat: UserDao 구현
    
    * refactor: db 접근의 책임을 JdbcTemplate로 분리
    
    * refactor: 파라미터를 statement에 적용하는 메서드 및 쿼리 실행 결과를 매핑하는 메서드 분리
    
    * chore: 테스트에서 h2 dataBase 환경을 사용하기 위한 의존성 추가
    
    * chore: 테스트에서 h2 dataBase 연결을 위한 설정 파일 추가
    
    * refactor: 오타 수정 및 메서드 위치 정렬
    
    * refactor: query 메서드 resultSet 읽기 오류 수정
    
    * test: jdbcTemplate 테스트 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,73 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+
+    }
+
+    public List<Object> query(String sql, Object... param) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            setParamToStatement(pstmt, param);
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                List<Object> results = new ArrayList<>();
+                if (rs.next()) {
+                    results = mapResultByColumn(rs);
+                }
+                return results;
+            }
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<List<Object>> queryList(String sql, Object... param) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            setParamToStatement(pstmt, param);
+
+            List<List<Object>> results = new ArrayList<>();
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    results.add(mapResultByColumn(rs));
+                }
+            }
+            return results;
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void execute(String sql, Object... param) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            setParamToStatement(pstmt, param);
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParamToStatement(PreparedStatement pstmt, Object[] param) throws SQLException {
+        for (int i = 0; i < param.length; i++) {
+            pstmt.setObject(i + 1, param[i]);
+        }
+    }
+
+    private List<Object> mapResultByColumn(ResultSet rs) throws SQLException {
+        List<Object> results = new ArrayList<>();
+        for (int i = 0; i < rs.getMetaData().getColumnCount(); i++) {
+            results.add(rs.getObject(i + 1));
+        }
+        return results;
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,102 @@
 package com.interface21.jdbc.core;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    private JdbcTemplate jdbcTemplate;
+    private Connection connection;
+    private PreparedStatement preparedStatement;
+    private ResultSet resultSet;
+
+    @BeforeEach
+    void setup() throws SQLException {
+        DataSource dataSource = DataSourceConfig.getInstance();
+        jdbcTemplate = new JdbcTemplate(dataSource);
+        connection = dataSource.getConnection();
+        String truncate = "drop table if exists users;";
+        String createTable = """
+                    create table users (
+                    id bigint auto_increment,
+                    account varchar(100) not null,
+                    password varchar(100) not null,
+                    email varchar(100) not null,
+                    primary key(id)
+                );
+                """;
+        String initData = "insert into users (account, password, email) values ('gugu', '123', 'gugu@naver.com')";
+        preparedStatement = connection.prepareStatement(truncate + createTable + initData);
+        preparedStatement.execute();
+    }
+
+    @Test
+    void query() {
+        // given
+        String sql = "select id, account, password, email from users where id = ?";
+        long param = 1;
+
+        // when
+        List<Object> result = jdbcTemplate.query(sql, param);
+
+        // then
+        assertAll(
+                () -> assertThat(result.get(0)).isEqualTo(1L),
+                () -> assertThat(result.get(1)).isEqualTo("gugu"),
+                () -> assertThat(result.get(2)).isEqualTo("123"),
+                () -> assertThat(result.get(3)).isEqualTo("gugu@naver.com")
+        );
+    }
+
+    @Test
+    void queryList() throws SQLException {
+        // given
+        preparedStatement = connection.prepareStatement(
+                "insert into users (account, password, email) values ('gugu2', '1232', 'gugu2@naver.com')");
+        preparedStatement.execute();
+        String sql = "select id, account, password, email from users";
+
+        // when
+        List<List<Object>> results = jdbcTemplate.queryList(sql);
+
+        // then
+        assertAll(
+                () -> assertThat(results.get(0).get(0)).isEqualTo(1L),
+                () -> assertThat(results.get(0).get(1)).isEqualTo("gugu"),
+                () -> assertThat(results.get(0).get(2)).isEqualTo("123"),
+                () -> assertThat(results.get(0).get(3)).isEqualTo("gugu@naver.com"),
+                () -> assertThat(results.get(1).get(0)).isEqualTo(2L),
+                () -> assertThat(results.get(1).get(1)).isEqualTo("gugu2"),
+                () -> assertThat(results.get(1).get(2)).isEqualTo("1232"),
+                () -> assertThat(results.get(1).get(3)).isEqualTo("gugu2@naver.com")
+        );
+    }
+
+    @Test
+    void execute() throws SQLException {
+        // given
+        String sql = "update users set account = ?, password = ?, email = ? where id = ?";
+
+        // when
+        jdbcTemplate.execute(sql, "kirby", "111", "kirby@naver.com", 1L);
+
+        // then
+        preparedStatement = connection.prepareStatement("select account, password, email from users where id = 1");
+        resultSet = preparedStatement.executeQuery();
+        resultSet.next();
+        assertAll(
+                () -> assertThat(resultSet.getObject(1)).isEqualTo("kirby"),
+                () -> assertThat(resultSet.getObject(2)).isEqualTo("111"),
+                () -> assertThat(resultSet.getObject(3)).isEqualTo("kirby@naver.com")
+        );
+    }
 }
