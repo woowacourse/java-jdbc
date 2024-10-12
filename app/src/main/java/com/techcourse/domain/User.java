2380f0f1be7b788e04bb493b33ba921e00b3ab05
ikjo39 <chi4321@naver.com>
2024-10-12 20:31:35 +0900

    style: 코드 스타일 적용

--- app/src/main/java/com/techcourse/domain/User.java
+++ app/src/main/java/com/techcourse/domain/User.java
@@ -1,6 +1,6 @@
 package com.techcourse.domain;
 
-public record User (Long id,String account,String password,String email) {
+public record User(Long id, String account, String password, String email) {
 
     public User(String account, String password, String email) {
         this(null, account, password, email);
