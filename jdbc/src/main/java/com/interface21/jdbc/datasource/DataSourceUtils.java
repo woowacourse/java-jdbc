45c3f4cf57008adbd5626f1b20e18c5e50c7c7e8
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-13 00:21:47 +0900

    feat: unbind 로직 구현

--- jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
@@ -2,22 +2,27 @@ package com.interface21.jdbc.datasource;
 
 import com.interface21.jdbc.CannotGetJdbcConnectionException;
 import com.interface21.transaction.support.TransactionSynchronizationManager;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 // 4단계 미션에서 사용할 것
 public abstract class DataSourceUtils {
 
-    private DataSourceUtils() {}
+    private DataSourceUtils() {
+    }
 
     public static Connection getConnection(DataSource dataSource) throws CannotGetJdbcConnectionException {
         Connection connection = TransactionSynchronizationManager.getResource(dataSource);
-        if (connection != null) {
-            return connection;
+        if (connection == null) {
+            connection = bindConnection(dataSource);
         }
 
+        return connection;
+    }
+
+    private static Connection bindConnection(DataSource dataSource) {
+        Connection connection;
         try {
             connection = dataSource.getConnection();
             TransactionSynchronizationManager.bindResource(dataSource, connection);
@@ -29,9 +34,17 @@ public abstract class DataSourceUtils {
 
     public static void releaseConnection(Connection connection, DataSource dataSource) {
         try {
+            unbindResource(dataSource);
             connection.close();
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
+        }
+    }
+
+    private static void unbindResource(DataSource dataSource) {
+        Connection connection = TransactionSynchronizationManager.unbindResource(dataSource);
+        if (connection == null) {
+            throw new CannotGetJdbcConnectionException("Failed to unbind JDBC Connection");
         }
     }
 }
