d76f15cb38c9402552958e4aae80684ee6f2285a
libienz <leekh10195@gmail.com>
2024-10-12 15:55:19 +0900

    feat: UserService 트랜잭션 원자성 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,12 @@
 package com.techcourse.service;
 
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserService {
 
@@ -24,8 +27,22 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final User passwordChanged = findById(id).changePassword(newPassword);
-        userDao.update(passwordChanged);
-        userHistoryDao.log(new UserHistory(passwordChanged, createBy));
+        Connection connection = DataSourceConfig.getConnection();
+        try (connection) {
+            connection.setAutoCommit(false);
+
+            final User passwordChanged = findById(id).changePassword(newPassword);
+            userDao.update(passwordChanged);
+            userHistoryDao.log(new UserHistory(passwordChanged, createBy));
+
+            connection.commit();
+        } catch (Exception e) {
+            try {
+                connection.rollback(); // try-catch로 한 번 더 감싸야 하지만 예시니까 생략
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+            throw new RuntimeException(e);
+        }
     }
 }
