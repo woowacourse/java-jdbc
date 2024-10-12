1b5bcda66cb30dd76c01cc4eb4720acdeadd82f5
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-12 23:34:12 +0900

    feat: UserService에서 트랜잭션을 사용할 수 있도록 변경

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,13 @@
 package com.techcourse.service;
 
+import com.interface21.dao.DataAccessException;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserService {
 
@@ -24,9 +28,30 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        Connection connection = null;
+        try {
+            connection = DataSourceConfig.getInstance().getConnection();
+            connection.setAutoCommit(false);
+
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.updateWithConnection(connection, user);
+            userHistoryDao.logWithConnection(connection, new UserHistory(user, createBy));
+
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+                throw new DataAccessException("Failed to rollback transaction.", exception);
+            }
+            throw new DataAccessException("Failed to commit transaction.", e);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException exception) {
+                throw new DataAccessException("Failed to close connection.", exception);
+            }
+        }
     }
 }
