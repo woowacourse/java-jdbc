d5e220bd9c320168ec9527b2900ed62ae6e8de05
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-13 03:21:44 +0900

    refactor: UserService 생성

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,32 @@
+package com.techcourse.service;
+
+import com.interface21.jdbc.datasource.TransactionManager;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+
+public class TxUserService implements UserService {
+
+    private final TransactionManager transactionManager;
+    private final UserService userService;
+
+    public TxUserService(UserService userService) {
+        this.transactionManager = new TransactionManager(DataSourceConfig.getInstance());
+        this.userService = userService;
+    }
+
+    @Override
+    public User findById(long id) {
+        return transactionManager.transaction(() -> userService.findById(id));
+    }
+
+    @Override
+    public void save(User user) {
+        transactionManager.transaction(() -> userService.save(user));
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createdBy) {
+        transactionManager.transaction(() -> userService.changePassword(id, newPassword, createdBy));
+    }
+}
+
