7fff291d050f92aa88e237578aa5c307df5c28ae
Gyeongho Yang <chchch1213@naver.com>
2024-10-13 22:20:58 +0900

    test: solve stage 2 on aop

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,47 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
+import javax.sql.DataSource;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
 public class AopConfig {
 
+    @Bean
+    public TransactionAdvice transactionAdvice(DataSourceTransactionManager transactionManager) {
+        return new TransactionAdvice(transactionManager);
+    }
+
+    @Bean
+    public TransactionPointcut transactionPointcut() {
+        return new TransactionPointcut();
+    }
+
+    @Bean
+    public TransactionAdvisor transactionAdvisor(DataSourceTransactionManager transactionManager) {
+        return new TransactionAdvisor(transactionManager);
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        return new DefaultAdvisorAutoProxyCreator();
+    }
+
+    @Bean
+    public ProxyFactoryBean proxyFactory(TransactionAdvisor transactionAdvisor) {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.addAdvisor(transactionAdvisor);
+        return proxyFactoryBean;
+    }
 }
