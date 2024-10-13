f890af956f54c74f293a0a314439a78ac57ab5f8
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-10-13 19:43:11 +0900

    refactor(LoginController): 인터페이스로 선언

--- app/src/main/java/com/techcourse/controller/LoginController.java
+++ app/src/main/java/com/techcourse/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.techcourse.controller;
 
 import com.interface21.context.stereotype.Controller;
+import com.interface21.transaction.support.TransactionManager;
 import com.interface21.web.bind.annotation.RequestMapping;
 import com.interface21.web.bind.annotation.RequestMethod;
 import com.interface21.webmvc.servlet.ModelAndView;
@@ -10,6 +11,8 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.service.AppUserService;
+import com.techcourse.service.TxUserService;
+import com.techcourse.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.Optional;
@@ -22,13 +25,15 @@ public class LoginController {
 
     private static final Logger log = LoggerFactory.getLogger(LoginController.class);
 
-    private final AppUserService userService;
+    private final UserService userService;
 
     public LoginController() {
         DataSource dataSource = DataSourceConfig.getInstance();
         UserDao userDao = new UserDao(dataSource);
         UserHistoryDao userHistoryDao = new UserHistoryDao(dataSource);
-        this.userService = new AppUserService(userDao, userHistoryDao);
+        UserService appUserService = new AppUserService(userDao, userHistoryDao);
+        TransactionManager transactionManager = new TransactionManager(dataSource);
+        this.userService = new TxUserService(appUserService, transactionManager);
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
