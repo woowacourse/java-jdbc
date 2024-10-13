d912062d1c25924a2a2a660faa7a4e8908a92ec7
ikjo39 <chi4321@naver.com>
2024-10-13 14:05:18 +0900

    test(UserDaoTest): 커넥션을 이용한 유저 정보 업데이트 테스트 추가

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -7,6 +7,9 @@ import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,13 +17,15 @@ import org.junit.jupiter.api.Test;
 
 class UserDaoTest {
 
+    private DataSource dataSource;
     private UserDao userDao;
     private JdbcTemplate jdbcTemplate;
 
     @BeforeEach
     void setup() {
-        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
-        jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
+        dataSource = DataSourceConfig.getInstance();
+        DatabasePopulatorUtils.execute(dataSource);
+        jdbcTemplate = new JdbcTemplate(dataSource);
         userDao = new UserDao(jdbcTemplate);
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(user);
@@ -104,6 +109,23 @@ class UserDaoTest {
 
         userDao.update(newUser);
         final var actual = userDao.findById(1L)
+                .orElseThrow();
+
+        assertThat(actual.password()).isEqualTo(newPassword);
+    }
+
+    @DisplayName("커넥션을 이용해서 유저 정보를 업데이트 한다.")
+    @Test
+    void updateWithConnection() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        String newPassword = "password99";
+        User user = userDao.findById(1L)
+                .orElseThrow();
+
+        User newUser = user.changePassword(newPassword);
+
+        userDao.update(connection, newUser);
+        User actual = userDao.findById(1L)
                 .orElseThrow();
 
         assertThat(actual.password()).isEqualTo(newPassword);
