4c462e427c3c48e9e7bd55c58ce80dd73c596293
TRE <37261785+takoyakimchi@users.noreply.github.com>
2024-10-13 23:10:57 +0900

    [3단계 - JDBC 라이브러리 구현하기] 트레 미션 제출합니다. (#804)
    * feat: 비밀번호를 바꾸는 메서드에 트랜잭션 추가
    
    * style: 개행 수정

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -1,25 +1,25 @@
 package com.techcourse.dao;
 
 import com.interface21.jdbc.core.JdbcTemplate;
-import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserHistoryDao {
 
     private final JdbcTemplate jdbcTemplate;
 
     public UserHistoryDao() {
-        this.jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
+        this.jdbcTemplate = new JdbcTemplate();
     }
 
     public UserHistoryDao(final JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void log(final UserHistory userHistory) {
+    public void log(Connection connection, final UserHistory userHistory) throws SQLException {
         final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
-
-        jdbcTemplate.executeUpdate(sql,
+        jdbcTemplate.executeUpdate(connection, sql,
                 userHistory.getUserId(),
                 userHistory.getAccount(),
                 userHistory.getPassword(),
