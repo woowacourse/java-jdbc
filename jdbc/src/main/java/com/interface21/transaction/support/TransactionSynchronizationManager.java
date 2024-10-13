03649f5111b56c2f7482ea6120ca8fd828778684
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-10-13 17:26:11 +0900

    feat(TransactionSynchronizationManager): Thread 별 Connection 관리 객체 생성

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,30 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        return null;
+        return getConnectionMap().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        getConnectionMap().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        return getConnectionMap().remove(key);
+    }
+
+    private static Map<DataSource, Connection> getConnectionMap() {
+        return resources.get();
     }
 }
