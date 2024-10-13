92596675e1d2aeb7d003793d92975d489e4dcd53
ashsty <qkralsdk991@naver.com>
2024-10-13 20:10:07 +0900

    test: 파라미터 개수 검증 로직 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/utils/DefaultPreparedStatementSetterFactory.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/utils/DefaultPreparedStatementSetterFactory.java
@@ -9,6 +9,12 @@ public class DefaultPreparedStatementSetterFactory {
 
     public static PreparedStatementSetter createDefaultPreparedStatementSetter(Object... parameters) {
         return preparedStatement -> {
+            int statementParametersAmount = preparedStatement.getParameterMetaData().getParameterCount();
+
+            if (statementParametersAmount != parameters.length) {
+                throw new IllegalArgumentException("파라미터 개수가 잘못되었습니다. 의도된 개수: " + statementParametersAmount);
+            }
+
             for (int i = 1; i <= parameters.length; i++) {
                 preparedStatement.setObject(i, parameters[i - 1]);
             }
