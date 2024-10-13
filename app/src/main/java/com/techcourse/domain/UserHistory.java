d74e86c6c52b0be2cc7a74f34028fca1d393af97
reddevilmidzy <midzy0228@gmail.com>
2024-10-13 15:29:25 +0900

    refactor: UserHistory id final 추가 및 코드 포맷팅

--- app/src/main/java/com/techcourse/domain/UserHistory.java
+++ app/src/main/java/com/techcourse/domain/UserHistory.java
@@ -4,22 +4,20 @@ import java.time.LocalDateTime;
 
 public class UserHistory {
 
-    private Long id;
-
+    private final Long id;
     private final long userId;
     private final String account;
     private final String password;
     private final String email;
-
     private final LocalDateTime createdAt;
-
     private final String createBy;
 
     public UserHistory(final User user, final String createBy) {
         this(null, user.getId(), user.getAccount(), user.getPassword(), user.getEmail(), createBy);
     }
 
-    public UserHistory(final Long id, final long userId, final String account, final String password, final String email, final String createBy) {
+    public UserHistory(final Long id, final long userId, final String account, final String password,
+                       final String email, final String createBy) {
         this.id = id;
         this.userId = userId;
         this.account = account;
