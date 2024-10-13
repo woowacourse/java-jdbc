b250cdde11ccde9770a225a561ea82079c03f661
woojong <dwax1324@gmail.com>
2024-10-13 19:48:08 +0900

    refactor: 객체로 매핑해주는 클래스 추가

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/mapper/ObjectMappedStatement.java
@@ -0,0 +1,18 @@
+package com.interface21.jdbc.core.mapper;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ObjectMappedStatement extends PreparedStatementMapper {
+
+    public ObjectMappedStatement(PreparedStatement preparedStatement, Object[] params) throws SQLException {
+        super(preparedStatement, params);
+    }
+
+    @Override
+    protected void setStatement() throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            preparedStatement.setObject(i + DB_INDEX_OFFSET, params[i]);
+        }
+    }
+}
