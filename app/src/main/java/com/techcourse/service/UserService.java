9b0db1fe1ffa5e8863ba53ecea811bade2ef5381
gmuz1c <songjin1127@naver.com>
2024-10-14 00:05:37 +0900

    [3단계 - JDBC 라이브러리 구현하기] 시소(신지송) 미션 제출합니다 (#790)
    * feat(UserDao): connection 인자 받는 update 함수 구현
    
    * feat(UserHistoryDao): update 구현
    
    * feat(UserService): 트랜잭션 적용된 비밀번호 변경 구현
    
    * test(MockUserHistoryDao): 필요 함수 오버라이드
    
    * feat: TransactionManager 구현
    
    * feat(UserService): 에러 핸들링 추가
    
    * feat: rollback 로직 추가

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,11 +1,22 @@
 package com.techcourse.service;
 
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.CannotGetJdbcConnectionException;
+import com.interface21.transaction.support.TransactionManager;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
@@ -24,9 +35,22 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        TransactionManager transactionManager = new TransactionManager(DataSourceConfig.getInstance());
+        Connection connection = transactionManager.startTransaction();
+        try (connection) {
+            User user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            transactionManager.commit(connection);
+        } catch (SQLException e) {
+            log.error(e.getMessage());
+            transactionManager.rollback(connection);
+            throw new DataAccessException(e);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            transactionManager.rollback(connection);
+            throw new RuntimeException(e);
+        }
     }
 }
