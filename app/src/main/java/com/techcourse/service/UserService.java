0428a65cc6162962cdd7d109d948742a0bd2a893
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-10-13 15:40:06 +0900

    refactor: Transaction 관리 코드 분리

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,22 +1,19 @@
 package com.techcourse.service;
 
-import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.transaction.TransactionManager;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.function.Consumer;
 import javax.sql.DataSource;
-import org.h2.util.JdbcUtils;
 
-public class UserService {
+public class UserService extends TransactionManager {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final DataSource dataSource) {
+        super(dataSource);
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
     }
@@ -36,29 +33,5 @@ public class UserService {
             userDao.update(user, connection);
             userHistoryDao.log(new UserHistory(user, createBy), connection);
         });
-    }
-
-    private void processInTransaction(final Consumer<Connection> consumer) {
-        Connection connection = null;
-        try  {
-            connection = userDao.getConnection();
-            connection.setAutoCommit(false);
-            consumer.accept(connection);
-            connection.commit();
-        } catch (SQLException e) {
-            rollback(connection);
-            throw new DataAccessException(e);
-        } catch (RuntimeException e) {
-            rollback(connection);
-            throw e;
-        } finally {
-            JdbcUtils.closeSilently(connection);
-        }
-    }
-
-    private void rollback(final Connection connection) {
-        try {
-            connection.rollback();
-        } catch (SQLException ignored) {}
     }
 }
