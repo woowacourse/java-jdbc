33812150dd1d6ebdb363616ebfd7fbdf5b9fb28e
reddevilmidzy <midzy0228@gmail.com>
2024-10-13 17:23:40 +0900

    test: TransactionManagerTest 추가

---
+++ jdbc/src/test/java/com/interface21/jdbc/core/TransactionManagerTest.java
@@ -0,0 +1,57 @@
+package com.interface21.jdbc.core;
+
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class TransactionManagerTest {
+
+    private Connection connection;
+    private Runnable runnable;
+
+    @BeforeEach
+    void setUp() {
+        this.connection = mock(Connection.class);
+        this.runnable = mock(Runnable.class);
+    }
+
+    @Test
+    @DisplayName("정상적으로 트랜잭션이 실행되고 commit을 한다.")
+    void execute() throws SQLException {
+        //given
+
+        //when
+        TransactionManager.execute(connection, runnable);
+
+        //then
+        verify(connection).setAutoCommit(false);
+        verify(runnable).run();
+        verify(connection).commit();
+        verify(connection, never()).rollback();
+    }
+
+    @Test
+    @DisplayName("예외가 발생하면 rollback을 한다.")
+    void rollback() throws SQLException {
+        //given
+        doThrow(new SqlExecutionException("예외 발생"))
+                .when(runnable).run();
+
+        //when
+        TransactionManager.execute(connection, runnable);
+
+        //then
+        verify(runnable).run();
+        verify(connection, never()).commit();
+        verify(connection, atLeastOnce()).rollback();
+    }
+}
