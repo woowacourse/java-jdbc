24bb887f9921acc44f3ae05b56b04ea358a8b226
김민수 MINSOO KIM <alstn113@gmail.com>
2024-10-13 22:34:50 +0900

    test: aop 학습테스트 stage0

--- study/src/test/java/aop/stage0/Stage0Test.java
+++ study/src/test/java/aop/stage0/Stage0Test.java
@@ -1,5 +1,10 @@
 package aop.stage0;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import java.lang.reflect.Proxy;
 import aop.DataAccessException;
 import aop.StubUserHistoryDao;
 import aop.domain.User;
@@ -14,10 +19,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class Stage0Test {
@@ -45,7 +46,7 @@ class Stage0Test {
     @Test
     void testChangePassword() {
         final var appUserService = new AppUserService(userDao, userHistoryDao);
-        final UserService userService = null;
+        final UserService userService = createTransactionProxy(appUserService, UserService.class);
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -59,15 +60,23 @@ class Stage0Test {
     @Test
     void testTransactionRollback() {
         final var appUserService = new AppUserService(userDao, stubUserHistoryDao);
-        final UserService userService = null;
+        final UserService userService = createTransactionProxy(appUserService, UserService.class);
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
-        assertThrows(DataAccessException.class,
-                () -> userService.changePassword(1L, newPassword, createBy));
+        assertThrows(DataAccessException.class, () -> userService.changePassword(1L, newPassword, createBy));
 
         final var actual = userService.findById(1L);
 
         assertThat(actual.getPassword()).isNotEqualTo(newPassword);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T createTransactionProxy(final Object target, final Class<T> proxyInterface) {
+        return (T) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{proxyInterface},
+                new TransactionHandler(platformTransactionManager, target)
+        );
     }
 }
