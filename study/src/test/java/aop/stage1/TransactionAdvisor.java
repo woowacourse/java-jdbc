327c3a6874177e0bc9b628539245d28eb30d48e8
Gyeongho Yang <chchch1213@naver.com>
2024-10-13 21:55:55 +0900

    test: solve stage 1 on aop

--- study/src/test/java/aop/stage1/TransactionAdvisor.java
+++ study/src/test/java/aop/stage1/TransactionAdvisor.java
@@ -1,23 +1,31 @@
 package aop.stage1;
 
 import org.aopalliance.aop.Advice;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.PointcutAdvisor;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
- * 어드바이저(advisor). 포인트컷과 어드바이스를 하나씩 갖고 있는 객체.
- * AOP의 애스팩트(aspect)에 해당되는 클래스다.
+ * 어드바이저(advisor). 포인트컷과 어드바이스를 하나씩 갖고 있는 객체. AOP의 애스팩트(aspect)에 해당되는 클래스다.
  */
 public class TransactionAdvisor implements PointcutAdvisor {
 
-    @Override
-    public Pointcut getPointcut() {
-        return null;
+    private final Pointcut pointcut = new TransactionPointcut();
+    private final Advice advice;
+
+    public TransactionAdvisor(PlatformTransactionManager transactionManager) {
+        this.advice = new TransactionAdvice(transactionManager);
     }
 
     @Override
-    public Advice getAdvice() {
-        return null;
+    public @NotNull Pointcut getPointcut() {
+        return pointcut;
+    }
+
+    @Override
+    public @NotNull Advice getAdvice() {
+        return advice;
     }
 
     @Override
