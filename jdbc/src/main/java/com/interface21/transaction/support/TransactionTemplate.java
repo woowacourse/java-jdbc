a384916e2e3f391875316e5b48347b9d0c7ee240
seunghye218 <seunghye@student.42seoul.kr>
2024-10-13 22:07:44 +0900

    refactor(TransactionTemplate): execute 메서드 분리

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionTemplate.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionTemplate.java
@@ -14,39 +14,51 @@ public class TransactionTemplate {
         this.dataSource = dataSource;
     }
 
-    public <T> T execute(TransactionCallback<T> action) {
-        Connection connection = null;
-        try {
-            connection = dataSource.getConnection();
-            connection.setAutoCommit(false);
-
-            T result = action.doInTransaction(connection);
-
-            connection.commit();
-            return result;
-        } catch (Exception exception) {
-            if (connection != null) {
-                try {
-                    connection.rollback();
-                } catch (SQLException rollbackException) {
-                    throw new RuntimeException("Failed to rollback transaction", rollbackException);
-                }
-            }
-            throw new DataAccessException(exception.getMessage(), exception);
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException ignored) {
-                }
-            }
-        }
-    }
-
     public void executeWithoutResult(Consumer<Connection> action) {
         execute(connection -> {
             action.accept(connection);
             return null;
         });
+    }
+
+    public <T> T execute(TransactionCallback<T> action) {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            return transaction(connection, action);
+        } catch (SQLException exception) {
+            rollbackOnException(connection);
+            throw new DataAccessException(exception.getMessage(), exception);
+        } finally {
+            processConnectionClose(connection);
+        }
+    }
+
+    private <T> T transaction(Connection connection, TransactionCallback<T> action) throws SQLException {
+        connection.setAutoCommit(false);
+
+        T result = action.doInTransaction(connection);
+
+        connection.commit();
+        return result;
+    }
+
+    private void rollbackOnException(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.rollback();
+            } catch (SQLException rollbackException) {
+                throw new DataAccessException("Failed to rollback transaction", rollbackException);
+            }
+        }
+    }
+
+    private void processConnectionClose(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ignored) {
+            }
+        }
     }
 }
