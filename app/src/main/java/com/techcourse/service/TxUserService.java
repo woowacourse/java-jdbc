45a7384f7df3bb75045a8fcd7f6fe1aa81e9251c
임수빈 <robin980108@naver.com>
2024-10-13 21:49:19 +0900

    refactor: 트랜잭션 처리와 어플리케이션 로직 분리

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,43 @@
+package com.techcourse.service;
+
+import com.interface21.dao.DataAccessException;
+import com.interface21.transaction.support.TransactionSynchronizationManager;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TxUserService implements UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(TxUserService.class);
+    private final AppUserService appUserService;
+
+    public TxUserService(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @Override
+    public User findById(long id) {
+        return appUserService.findById(id);
+    }
+
+    @Override
+    public void insert(User user) {
+        appUserService.insert(user);
+    }
+
+    @Override
+    public void changePassword(long id, String newPassword, String createBy) {
+        DataSource dataSource = DataSourceConfig.getInstance();
+        TransactionSynchronizationManager.getTransactionStartedResource(dataSource);
+        try {
+            appUserService.changePassword(id, newPassword, createBy);
+        }catch (Exception e) {
+            log.error(e.getMessage(), e);
+            TransactionSynchronizationManager.unbindAndRollback(dataSource);
+            throw new DataAccessException(e);
+        }
+        TransactionSynchronizationManager.unbindAndCommit(dataSource);
+    }
+}
