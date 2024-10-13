d688714597777f215a2629b0f4aef5f2aeccdac1
eunjungL <62099953+eunjungL@users.noreply.github.com>
2024-10-14 02:20:42 +0900

    [3단계 - JDBC 라이브러리 구현하기] 클로버(이은정) 미션 제출합니다. (#787)
    * refactor: UserHistoryDao가 JdbcTemplate 사용하게 변경
    
    * feat: UserService changePassword에 transaction 적용
    
    * fix: UserService rollback 누락 수정
    
    * refactor: JdbcTemplate getConnection 메서드 접근 제어자 private으로 변경

--- app/src/main/java/com/techcourse/config/DataSourceConfig.java
+++ app/src/main/java/com/techcourse/config/DataSourceConfig.java
@@ -1,10 +1,17 @@
 package com.techcourse.config;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import org.h2.jdbcx.JdbcDataSource;
 
 import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DataSourceConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(DataSourceConfig.class);
 
     private static javax.sql.DataSource INSTANCE;
 
@@ -24,4 +31,20 @@ public class DataSourceConfig {
     }
 
     private DataSourceConfig() {}
+
+    public static Connection getConnection() {
+        try {
+            validateInstanceIsNull();
+            return INSTANCE.getConnection();
+        } catch (SQLException e) {
+            log.info("GET_CONNECTION_EXCEPTION :: {}", e.getMessage(), e);
+            throw new DataAccessException("DB 커넥션을 얻던 중 예외가 발생했습니다.");
+        }
+    }
+
+    private static void validateInstanceIsNull() {
+        if (Objects.isNull(INSTANCE)) {
+            throw new IllegalStateException("유효한 DataSource가 존재하지 않습니다.");
+        }
+    }
 }
--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,11 +1,19 @@
 package com.techcourse.service;
 
+import com.interface21.dao.DataAccessException;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserService {
+
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
@@ -23,10 +31,32 @@ public class UserService {
         userDao.insert(user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(long id, String newPassword, String createBy) {
+        Connection connection = DataSourceConfig.getConnection();
+        try (connection) {
+            connection.setAutoCommit(false);
+
+            User user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+
+            UserHistory userHistory = new UserHistory(user, createBy);
+            userHistoryDao.log(connection, userHistory);
+
+            connection.commit();
+        } catch (Exception e) {
+            rollbackTransaction(connection);
+            log.info("CHANGE_PASSWORD_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException("비밀번호를 변경하던 중 예외가 발생했습니다.");
+        }
+    }
+
+    private void rollbackTransaction(Connection connection) {
+        try {
+            connection.rollback();
+        } catch (SQLException e) {
+            log.info("ROLLBACK_TRANSACTION_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException("트랜잭션을 롤백하던 중 예외가 발생했습니다.");
+        }
     }
 }
