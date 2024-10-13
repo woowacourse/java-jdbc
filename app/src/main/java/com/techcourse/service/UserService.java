3cd24420e79d9f179dc899310aaeed79ce3a5bb9
정회성 <ghltjd3@gmail.com>
2024-10-13 10:24:46 +0900

    [JDBC 구현하기 3단계] 초롱 미션 제출합니다. (#798)
    * test: 학습테스트
    
    * fix: 무한루프 오류 해결
    
    * refactor: 타입에 맞는 setter 설정
    
    * refactor: @Disabled 제거
    
    * feat: 트랜잭션 경계 설정
    
    * refactor: 불필요한 코드 제거
    
    * refactor: 반복되는 코드 제거
    
    * refactor: 불필요한 코드 제거
    
    * fix: 실패하는 테스트 코드 수정

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,5 +1,12 @@
 package com.techcourse.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Function;
+
+import javax.sql.DataSource;
+
+import com.interface21.dao.DataAccessException;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -9,24 +16,73 @@ public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, DataSource dataSource) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.dataSource = dataSource;
     }
 
     public User findById(final long id) {
-        return userDao.findById(id);
+        return executeTransaction(connection -> userDao.findById(connection, id));
     }
 
     public void insert(final User user) {
-        userDao.insert(user);
+        executeTransaction(connection -> {
+            userDao.insert(connection, user);
+            return null;
+        });
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        executeTransaction(connection -> {
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            return null;
+        });
+    }
+
+    private <R> R executeTransaction(Function<Connection, R> function) {
+        Connection connection = getConnection();
+        try {
+            connection.setAutoCommit(false);
+            R result = function.apply(connection);
+            connection.commit();
+            return result;
+        } catch (SQLException e) {
+            rollback(connection);
+            throw new DataAccessException(e);
+        } finally {
+            close(connection);
+        }
+    }
+
+    private Connection getConnection() {
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void rollback(Connection connection) {
+        try {
+            connection.rollback();
+        } catch (SQLException ex) {
+            throw new DataAccessException(ex);
+        }
+    }
+
+    private void close(Connection connection) {
+        if(connection != null) {
+            try {
+                connection.close(); // 연결 닫기
+            } catch (SQLException closeEx) {
+                throw new RuntimeException("Connection close failed", closeEx);
+            }
+        }
     }
 }
