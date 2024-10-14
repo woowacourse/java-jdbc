3d5c651e4d89d4f4078e040162b55d2e5e8972c9
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-14 17:04:24 +0900

    test: 변경사항 테스트 적용

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -2,7 +2,6 @@ package com.techcourse.dao;
 
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
 import javax.sql.DataSource;
 
 public class UserHistoryDao {
@@ -17,9 +16,9 @@ public class UserHistoryDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void log(Connection connection, UserHistory userHistory) {
+    public void log(UserHistory userHistory) {
         String sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
-        jdbcTemplate.write(connection, sql,
+        jdbcTemplate.write(sql,
                 userHistory.getUserId(), userHistory.getAccount(), userHistory.getPassword(),
                 userHistory.getEmail(), userHistory.getCreatedAt(), userHistory.getCreateBy());
     }
