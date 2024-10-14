5d019954f6ca133dfd051bd22cd01db8d02938a1
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-10-14 16:12:20 +0900

    test: 0단계 TransactionHandler 구현

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,48 @@
 package aop.stage0;
 
+import aop.DataAccessException;
+import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TransactionHandler implements InvocationHandler {
+
+    private final PlatformTransactionManager platformTransactionManager;
+    private final Object target;
+
+    public TransactionHandler(PlatformTransactionManager platformTransactionManager, Object target) {
+        this.platformTransactionManager = platformTransactionManager;
+        this.target = target;
+    }
 
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        if (isNotExistsTransaction(method, args)) {
+            return method.invoke(target, args);
+        }
+        TransactionStatus transactionStatus = platformTransactionManager.getTransaction(
+                new DefaultTransactionDefinition());
+        Object result = null;
+        try {
+            result = method.invoke(target, args);
+            platformTransactionManager.commit(transactionStatus);
+        } catch (InvocationTargetException e) {
+            platformTransactionManager.rollback(transactionStatus);
+            throw new DataAccessException(e.getTargetException());
+        }
+        return result;
     }
+
+    private boolean isNotExistsTransaction(Method method, Object[] args) throws NoSuchMethodException {
+        Method targerMethod = target.getClass().getDeclaredMethod(method.getName(), method.getParameterTypes());
+        return !targerMethod.isAnnotationPresent(Transactional.class);
+    }
+
 }
