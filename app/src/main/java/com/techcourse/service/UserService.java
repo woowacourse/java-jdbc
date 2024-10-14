5eb8530f51fb3c8341ca7813eda88cd1604c5b15
fram1998 <koust6u@pusan.ac.kr>
2024-10-14 17:09:48 +0900

    feat: UserService 추상화

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,61 +1,12 @@
 package com.techcourse.service;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import com.interface21.dao.DataAccessException;
-import com.techcourse.config.DataSourceConfig;
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
-import com.techcourse.domain.UserHistory;
 
-public class UserService {
+public interface UserService {
 
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
+    User findById(long id);
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-    }
+    void insert(final User user);
 
-    public User findById(final long id) {
-        return userDao.findById(id);
-    }
-
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
-
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        Connection connection = null;
-        try {
-            connection = DataSourceConfig.getInstance().getConnection();
-            connection.setAutoCommit(false);
-
-            final var user = findById(id);
-            user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(connection, new UserHistory(user, createBy));
-
-            connection.commit();
-        } catch (final Exception e) {
-            if (connection != null) {
-                try {
-                    connection.rollback();
-                } catch (final SQLException exception) {
-                    throw new DataAccessException("롤백 과정에서 예외가 발생했습니다.");
-                }
-            }
-            throw new DataAccessException("Data source 커넥션 획득에 실패했습니다.");
-        } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (final SQLException e) {
-                }
-            }
-        }
-    }
+    void changePassword(final long id, final String newPassword, final String createBy);
 }
--- app/src/test/java/com/techcourse/service/MockUserHistoryDao.java
+++ app/src/test/java/com/techcourse/service/MockUserHistoryDao.java
@@ -1,7 +1,5 @@
 package com.techcourse.service;
 
-import java.sql.Connection;
-
 import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.dao.UserHistoryDao;
@@ -15,11 +13,6 @@ public class MockUserHistoryDao extends UserHistoryDao {
 
     @Override
     public void log(final UserHistory userHistory) {
-        throw new DataAccessException();
-    }
-
-    @Override
-    public void log(final Connection connection, final UserHistory userHistory) {
         throw new DataAccessException();
     }
 }
