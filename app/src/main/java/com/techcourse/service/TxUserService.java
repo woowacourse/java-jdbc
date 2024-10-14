cf9164af6031e18fa0a57df73807bd01eaa68248
tackyu <tack970913@gmail.com>
2024-10-14 14:00:03 +0900

    refactor: 비즈니스 로직, 데이터 액세스 로직 분리

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,53 @@
+package com.techcourse.service;
+
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+public class TxUserService implements UserService{
+
+    private final AppUserService appUserService;
+
+    public TxUserService(AppUserService appUserService) {
+        this.appUserService = appUserService;
+    }
+
+    @Override
+    public User findById(long id) {
+        return appUserService.findById(id);
+    }
+
+    @Override
+    public void save(User user) {
+        appUserService.save(user);
+    }
+
+    @Override
+    public void changePassword(long id, String newPassword, String createBy) {
+        DataSource dataSource = DataSourceConfig.getInstance();
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+
+        changePasswordWithLog(id, newPassword, createBy, connection, dataSource);
+    }
+
+    private void changePasswordWithLog(long id, String newPassword, String createBy, Connection connection, DataSource dataSource) {
+        try {
+            connection.setAutoCommit(false);
+            appUserService.changePassword(id, newPassword, createBy);
+            connection.commit();
+        } catch (Throwable e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new DataAccessException("Failed to rollback", ex);
+            }
+            throw new DataAccessException(e);
+        } finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+        }
+    }
+}
