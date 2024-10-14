4f28fe789f91ddfe092053edc887dc0195bf72f0
HoeSeong123 <ghltjd3@gmail.com>
2024-10-14 14:20:54 +0900

    feat: TransactionSynchronizationManager 기능 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,8 +1,9 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
@@ -11,13 +12,23 @@ public abstract class TransactionSynchronizationManager {
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
+        return dataSourceConnectionMap.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
+        dataSourceConnectionMap.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
+        Connection conn = dataSourceConnectionMap.remove(key);
+
+        if (dataSourceConnectionMap.isEmpty()) {
+            resources.remove();
+        }
+
+        return conn;
     }
 }
