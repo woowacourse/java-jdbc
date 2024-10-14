02859e3b73943db585ddca0c36e4d90b5ba23208
eunjungL <dldmswjd322@gmail.com>
2024-10-14 14:26:27 +0900

    refactor: TxUserService 공통 트랜잭션 로직 분리

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -7,6 +7,8 @@ import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.function.Supplier;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +22,26 @@ public class TxUserService implements UserService {
         this.userService = userService;
     }
 
+    private <T> T executeInTransaction(Supplier<T> service) {
+        DataSource dataSource = DataSourceConfig.getInstance();
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+
+        try (connection) {
+            connection.setAutoCommit(false);
+
+            T data = service.get();
+
+            connection.commit();
+            return data;
+        } catch (Exception e) {
+            rollbackTransaction(connection);
+            log.info("RUN_TRANSACTION_ERROR :: {}", e.getMessage(), e);
+            throw new DataAccessException("트랜잭션 실행 중 예외가 발생했습니다.");
+        } finally {
+            TransactionSynchronizationManager.unbindResource(dataSource);
+        }
+    }
+
     private void rollbackTransaction(Connection connection) {
         try {
             connection.rollback();
@@ -31,56 +53,22 @@ public class TxUserService implements UserService {
 
     @Override
     public User findById(long id) {
-        Connection connection = DataSourceUtils.getConnection(DataSourceConfig.getInstance());
-        try (connection) {
-            connection.setAutoCommit(false);
-
-            User user = userService.findById(id);
-
-            connection.commit();
-            return user;
-        } catch (SQLException e) {
-            rollbackTransaction(connection);
-            log.info("FIND_BY_ID_TRANSACTION_ERROR :: {}", e.getMessage(), e);
-            throw new DataAccessException("사용자를 조회하던 중 오류가 발생했습니다.");
-        } finally {
-            TransactionSynchronizationManager.unbindResource(DataSourceConfig.getInstance());
-        }
+        return executeInTransaction(() -> userService.findById(id));
     }
 
     @Override
     public void insert(User user) {
-        Connection connection = DataSourceUtils.getConnection(DataSourceConfig.getInstance());
-        try (connection) {
-            connection.setAutoCommit(false);
-
+        executeInTransaction(() -> {
             userService.insert(user);
-
-            connection.commit();
-        } catch (SQLException e) {
-            rollbackTransaction(connection);
-            log.info("INSERT_TRANSACTION_ERROR :: {}", e.getMessage(), e);
-            throw new DataAccessException("사용자를 생서하던 중 오류가 발생했습니다.");
-        } finally {
-            TransactionSynchronizationManager.unbindResource(DataSourceConfig.getInstance());
-        }
+            return null;
+        });
     }
 
     @Override
     public void changePassword(long id, String newPassword, String createBy) {
-        Connection connection = DataSourceUtils.getConnection(DataSourceConfig.getInstance());
-        try (connection) {
-            connection.setAutoCommit(false);
-
+        executeInTransaction(() -> {
             userService.changePassword(id, newPassword, createBy);
-
-            connection.commit();
-        } catch (SQLException e) {
-            rollbackTransaction(connection);
-            log.info("CHANGE_PASSWORD_TRANSACTION_ERROR :: {}", e.getMessage(), e);
-            throw new DataAccessException("비밀번호를 변경하던 중 오류가 발생했습니다.");
-        } finally {
-            TransactionSynchronizationManager.unbindResource(DataSourceConfig.getInstance());
-        }
+            return null;
+        });
     }
 }
