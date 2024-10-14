d954e660de7cb247396d7de5d4fe6b69be5946b3
SungKyum Kim <sungkyum1@naver.com>
2024-10-14 16:05:29 +0900

    refactor: Transaction synchronization 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
+import com.interface21.transaction.support.TransactionSynchronizationManager;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -37,25 +39,26 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createdBy) {
-        Connection connection = null;
+        Connection connection = DataSourceUtils.getConnection(dataSource);
         try {
-            connection = dataSource.getConnection();
             connection.setAutoCommit(false);
 
             final var user = findById(id);
             user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(connection, new UserHistory(user, createdBy));
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createdBy));
 
             connection.commit();
         } catch (SQLException e) {
             try {
-                assert connection != null;
                 connection.rollback();
             } catch (SQLException rollbackEx) {
                 throw new DataAccessException("Failed to rollback transaction", rollbackEx);
             }
             throw new DataAccessException(e);
+        }finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+            TransactionSynchronizationManager.unbindResource(dataSource);
         }
     }
 }
