5a5c6647c4b6783934d3db5f20e145049d527aee
woojong <dwax1324@gmail.com>
2024-10-14 17:28:03 +0900

    refactor: Transaction synchronization 적용

--- app/src/main/java/com/techcourse/domain/UserHistory.java
+++ app/src/main/java/com/techcourse/domain/UserHistory.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 public class UserHistory {
 
     private Long id;
-
     private final long userId;
     private final String account;
     private final String password;
