86611ad22e5bf5fa9d6d5e8809af6a841063b2bf
unifolio0 <boho82ha@gmail.com>
2024-10-14 13:55:26 +0900

    refactor: 자료형 수정

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -2,28 +2,28 @@ package com.interface21.transaction.support;
 
 import com.interface21.dao.DataAccessException;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final Map<DataSource, Connection> resources = new ConcurrentHashMap<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
-        return resources.get(key);
+        return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        if (resources.containsKey(key)) {
+        if (resources.get().containsKey(key)) {
             throw new DataAccessException("이미 존재하는 Connection 입니다");
         }
-        resources.put(key, value);
+        resources.get().put(key, value);
     }
 
     public static void unbindResource(DataSource key) {
-        resources.remove(key);
+        resources.get().remove(key);
     }
 }
