1b1db108895d27306d0ace26711074ace6e67c96
woojong <dwax1324@gmail.com>
2024-10-14 16:28:09 +0900

    fix: 깨지는 테스트 수정

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -3,6 +3,9 @@ package com.techcourse.dao;
 import com.techcourse.DatabaseTest;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,24 +14,30 @@ import static org.assertj.core.api.Assertions.assertThat;
 class UserDaoTest extends DatabaseTest {
 
     private UserDao userDao;
+    private Connection connection;
 
     @BeforeEach
-    void setup() {
+    void setup() throws SQLException {
         userDao = new UserDao(DataSourceConfig.getInstance());
+        connection = DataSourceConfig.getInstance().getConnection();
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.insert(user);
+        userDao.insert(connection, user);
+    }
+    @AfterEach
+    void dispose() throws SQLException {
+        connection.close();
     }
 
     @Test
     void findAll() {
-        final var users = userDao.findAll();
+        final var users = userDao.findAll(connection);
 
         assertThat(users).isNotEmpty();
     }
 
     @Test
     void findById() {
-        final var user = userDao.findById(1L);
+        final var user = userDao.findById(connection, 1L);
 
         assertThat(user.getAccount()).isEqualTo("gugu");
     }
@@ -36,7 +45,7 @@ class UserDaoTest extends DatabaseTest {
     @Test
     void findByAccount() {
         final var account = "gugu";
-        final var user = userDao.findByAccount(account);
+        final var user = userDao.findByAccount(connection,account);
 
         assertThat(user.getAccount()).isEqualTo(account);
     }
@@ -45,9 +54,9 @@ class UserDaoTest extends DatabaseTest {
     void insert() {
         final var account = "insert-gugu";
         final var user = new User(account, "password", "hkkang@woowahan.com");
-        userDao.insert(user);
+        userDao.insert(connection,user);
 
-        final var actual = userDao.findById(2L);
+        final var actual = userDao.findById(connection, 2L);
 
         assertThat(actual.getAccount()).isEqualTo(account);
     }
@@ -55,12 +64,12 @@ class UserDaoTest extends DatabaseTest {
     @Test
     void update() {
         final var newPassword = "password99";
-        final var user = userDao.findById(1L);
+        final var user = userDao.findById(connection, 1L);
         user.changePassword(newPassword);
 
-        userDao.update(user);
+        userDao.update(connection,user);
 
-        final var actual = userDao.findById(1L);
+        final var actual = userDao.findById(connection, 1L);
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
