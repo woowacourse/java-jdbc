a7a53d73b706c14c7b2e1f8c2ea113913eea6e1f
youngsu5582 <98307410+youngsu5582@users.noreply.github.com>
2024-10-14 15:19:01 +0900

    fix: 잘못된 의존성 변경

--- jdbc/src/main/java/com/interface21/jdbc/exception/ConnectionFailException.java
+++ jdbc/src/main/java/com/interface21/jdbc/exception/ConnectionFailException.java
@@ -1,6 +1,8 @@
 package com.interface21.jdbc.exception;
 
-public class ConnectionFailException extends DatabaseAccessException {
+import com.interface21.dao.DataAccessException;
+
+public class ConnectionFailException extends DataAccessException {
     public ConnectionFailException(final String sql, final Throwable cause) {
         super(String.format("DB 연결중 실패 - 쿼리: [%s]. 에러: [%s]",sql,cause.getMessage()), cause);
     }
--- jdbc/src/main/java/com/interface21/jdbc/exception/QueryExecutionException.java
+++ jdbc/src/main/java/com/interface21/jdbc/exception/QueryExecutionException.java
@@ -1,8 +1,10 @@
 package com.interface21.jdbc.exception;
 
+import com.interface21.dao.DataAccessException;
+
 import java.sql.PreparedStatement;
 
-public class QueryExecutionException extends DatabaseAccessException {
+public class QueryExecutionException extends DataAccessException {
     public QueryExecutionException(final PreparedStatement pstmt, final Throwable cause) {
         super(String.format("쿼리 실행중 실패 - 쿼리: [%s]. 에러: [%s]", pstmt.toString(), cause.getMessage()), cause);
     }
