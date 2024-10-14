b7979ef1b0764b04cafeaba103acf8526044e64d
leegwichan <cnddkscndgus@naver.com>
2024-10-14 09:46:46 +0900

    feat: TxUserService와 AppUserService를 통해 트랜잭션 코드와 비즈니스 로직 분리

---
+++ app/src/main/java/com/techcourse/service/AppUserService.java
@@ -0,0 +1,35 @@
+package com.techcourse.service;
+
+import com.interface21.jdbc.transaction.TransactionManager;
+import com.techcourse.dao.UserDao;
+import com.techcourse.dao.UserHistoryDao;
+import com.techcourse.domain.User;
+import com.techcourse.domain.UserHistory;
+import javax.sql.DataSource;
+
+public class AppUserService extends TransactionManager implements UserService {
+
+    private final UserDao userDao;
+    private final UserHistoryDao userHistoryDao;
+
+    public AppUserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final DataSource dataSource) {
+        super(dataSource);
+        this.userDao = userDao;
+        this.userHistoryDao = userHistoryDao;
+    }
+
+    public User findById(final long id) {
+        return userDao.findById(id);
+    }
+
+    public void insert(final User user) {
+        userDao.insert(user);
+    }
+
+    public void changePassword(final long id, final String newPassword, final String createBy) {
+        final var user = findById(id);
+        user.changePassword(newPassword);
+        userDao.update(user);
+        userHistoryDao.log(new UserHistory(user, createBy));
+    }
+}
