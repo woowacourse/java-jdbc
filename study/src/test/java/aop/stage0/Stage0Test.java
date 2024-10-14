2f7080403730df03e16159a605bba31f07d54b36
yoonjuho <dbswn990@naver.com>
2024-10-14 15:17:25 +0900

    test: AOP 학습 테스트

--- study/src/test/java/aop/stage0/Stage0Test.java
+++ study/src/test/java/aop/stage0/Stage0Test.java
@@ -1,12 +1,6 @@
 package aop.stage0;
 
-import aop.DataAccessException;
-import aop.StubUserHistoryDao;
-import aop.domain.User;
-import aop.repository.UserDao;
-import aop.repository.UserHistoryDao;
-import aop.service.AppUserService;
-import aop.service.UserService;
+import java.lang.reflect.Proxy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -14,6 +8,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
+import aop.DataAccessException;
+import aop.StubUserHistoryDao;
+import aop.domain.User;
+import aop.repository.UserDao;
+import aop.repository.UserHistoryDao;
+import aop.service.AppUserService;
+import aop.service.UserService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -45,7 +46,7 @@ class Stage0Test {
     @Test
     void testChangePassword() {
         final var appUserService = new AppUserService(userDao, userHistoryDao);
-        final UserService userService = null;
+        final UserService userService = appUserService;
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -59,7 +60,10 @@ class Stage0Test {
     @Test
     void testTransactionRollback() {
         final var appUserService = new AppUserService(userDao, stubUserHistoryDao);
-        final UserService userService = null;
+        final UserService userService = (UserService) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{UserService.class},
+                new TransactionHandler(platformTransactionManager, appUserService));
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -2,14 +2,44 @@ package aop.stage0;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import org.springframework.data.util.ReflectionUtils;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import aop.DataAccessException;
+import aop.Transactional;
 
 public class TransactionHandler implements InvocationHandler {
+
+    private PlatformTransactionManager transactionManager;
+    private Object target;
+
+    public TransactionHandler(PlatformTransactionManager transactionManager, Object target) {
+        this.transactionManager = transactionManager;
+        this.target = target;
+    }
 
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        Method targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
+        if (targetMethod.isAnnotationPresent(Transactional.class)) {
+            return executeWithTransaction(targetMethod, args);
+        }
+        return targetMethod.invoke(target, args);
+    }
+
+    private Object executeWithTransaction(Method method, Object[] args) {
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            Object result = method.invoke(target, args);
+            transactionManager.commit(status);
+            return result;
+        } catch (Exception e) {
+            transactionManager.rollback(status);
+            throw new DataAccessException();
+        }
     }
 }
