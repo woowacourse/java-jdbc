24f6bf079056501ab784fccb1c68807b7676b620
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-10-14 14:36:30 +0900

    feat(TransactionSynchronizationManager): 각 쓰레드별로 데이터베이스 Connection 객체를 관리

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,8 +1,11 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.annotation.Nullable;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
@@ -10,14 +13,30 @@ public abstract class TransactionSynchronizationManager {
 
     private TransactionSynchronizationManager() {}
 
+    @Nullable
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+        return map.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            map = new HashMap<>();
+            resources.set(map);
+        }
+        map.put(key, value);
     }
 
+    @Nullable
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+        return map.remove(key);
     }
 }
