c2fd77d96f90f4cf36475239d61d505831b14b9b
tsulocalize <tsulocalizing@gmail.com>
2024-10-14 13:53:36 +0900

    refactor: DataSourceUtils를 통해 Connection을 획득하도록 수정

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -1,9 +1,8 @@
 package com.techcourse.service;
 
 import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
 import com.techcourse.config.DataSourceConfig;
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -11,7 +10,6 @@ import java.sql.SQLException;
 public class TxUserService implements UserService {
 
     private final UserService service;
-
     public TxUserService(final UserService userService) {
         this.service = userService;
     }
@@ -21,7 +19,7 @@ public class TxUserService implements UserService {
         Connection connection = null;
         User user = null;
         try {
-            connection = DataSourceConfig.getInstance().getConnection();
+            connection = DataSourceUtils.getConnection(DataSourceConfig.getInstance());
             connection.setAutoCommit(false);
             user = service.findById(id);
             connection.commit();
@@ -37,7 +35,7 @@ public class TxUserService implements UserService {
     public void insert(User user) {
         Connection connection = null;
         try {
-            connection = DataSourceConfig.getInstance().getConnection();
+            connection = DataSourceUtils.getConnection(DataSourceConfig.getInstance());
             connection.setAutoCommit(false);
             service.insert(user);
             connection.commit();
@@ -52,7 +50,7 @@ public class TxUserService implements UserService {
     public void changePassword(long id, String newPassword, String createBy) {
         Connection connection = null;
         try {
-            connection = DataSourceConfig.getInstance().getConnection();
+            connection = DataSourceUtils.getConnection(DataSourceConfig.getInstance());
             connection.setAutoCommit(false);
             service.changePassword(id, newPassword, createBy);
             connection.commit();
