b612c05fb6fa78c110a2d59181e0ae7a16b7c831
zangsu <zangsu_@naver.com>
2024-10-14 14:57:06 +0900

    refactor: 트랜잭션 서비스 추상화 하기

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,37 +1,9 @@
 package com.techcourse.service;
 
-import com.interface21.jdbc.core.TransactionManager;
-import com.techcourse.config.DataSourceConfig;
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
-import com.techcourse.domain.UserHistory;
 
-public class UserService {
-
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
-    TransactionManager transactionManager = new TransactionManager(DataSourceConfig.getInstance());
-
-    public UserService(UserDao userDao, UserHistoryDao userHistoryDao) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-    }
-
-    public User findById(long id) {
-        return transactionManager.getResultInTransaction(() -> userDao.findById( id));
-    }
-
-    public void insert(User user) {
-        transactionManager.executeInTransaction(() -> userDao.insert( user));
-    }
-
-    public void changePassword(long id, String newPassword, String createBy) {
-        transactionManager.getResultInTransaction(() -> {
-            final var user = findById(id);
-            user.changePassword(newPassword);
-            userDao.update( user);
-            return userHistoryDao.log(new UserHistory(user, createBy));
-        });
-    }
+public interface UserService {
+    User findById(final long id);
+    void save(final User user);
+    void changePassword(final long id, final String newPassword, final String createdBy);
 }
--- app/src/test/java/com/techcourse/service/AppUserServiceTest.java
+++ app/src/test/java/com/techcourse/service/AppUserServiceTest.java
@@ -4,9 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import java.sql.Connection;
-import java.util.function.Function;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -20,7 +17,7 @@ import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 
-class UserServiceTest {
+class AppUserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
     private UserDao userDao;
@@ -41,7 +38,7 @@ class UserServiceTest {
     @Test
     void testChangePassword() {
         final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var userService = new TxUserService(new AppUserService(userDao, userHistoryDao));
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -56,7 +53,7 @@ class UserServiceTest {
     void testTransactionRollback() {
         // 트랜잭션 롤백 테스트를 위해 mock으로 교체
         final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var userService = new TxUserService(new AppUserService(userDao, userHistoryDao));
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
