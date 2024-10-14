1b5460ea2070b77058cb0fae0513d95ad8d21bd9
linirini <2001yerin@naver.com>
2024-10-14 19:39:44 +0900

    test: stage1 학습 테스트 수행

--- study/src/test/java/aop/stage1/Stage1Test.java
+++ study/src/test/java/aop/stage1/Stage1Test.java
@@ -1,17 +1,18 @@
 package aop.stage1;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
 import aop.DataAccessException;
 import aop.StubUserHistoryDao;
 import aop.domain.User;
 import aop.repository.UserDao;
 import aop.repository.UserHistoryDao;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.PlatformTransactionManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -33,15 +34,32 @@ class Stage1Test {
     @Autowired
     private PlatformTransactionManager platformTransactionManager;
 
+    /**
+     * ProxyFactoryBean: 스프링이 제공하는 클래스로,
+     * - 생성자가 아닌 다른 방식으로 스프링 빈을ㅇ 생성할 때 사용한다.
+     * - 프록시 객체를 생성한다.
+     * - default: JDK Proxy, setProxyTargetClass = true이면 CGLib
+     */
+    private ProxyFactoryBean proxyFactoryBean;
+
     @BeforeEach
     void setUp() {
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
+        proxyFactoryBean = new ProxyFactoryBean();
+        /**
+         * setProxyTargetClass(true): CGLIB 기반의 클래스 프록시를 사용하겠다는 설정
+         * 인터페이스가 없는 클래스에 대해 프록시를 생성하거나, 클래슷 자체를 프록시로 만들어야 할 때 사용한다.
+         */
+        proxyFactoryBean.setProxyTargetClass(true);
+        // TransactionAdvisor를 ProxyFactoryBean에 추가하여 트랜잭션 어드바이스를 타깃 객체에 적용
+        proxyFactoryBean.addAdvisor(new TransactionAdvisor(new TransactionPointcut(), new TransactionAdvice(platformTransactionManager)));
         userDao.insert(user);
     }
 
     @Test
     void testChangePassword() {
-        final UserService userService = null;
+        proxyFactoryBean.setTarget(new UserService(userDao, userHistoryDao));
+        UserService userService = (UserService) proxyFactoryBean.getObject();
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -54,7 +72,8 @@ class Stage1Test {
 
     @Test
     void testTransactionRollback() {
-        final UserService userService = null;
+        proxyFactoryBean.setTarget(new UserService(userDao, stubUserHistoryDao));
+        UserService userService = (UserService) proxyFactoryBean.getObject();
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
--- study/src/test/java/aop/stage1/TransactionPointcut.java
+++ study/src/test/java/aop/stage1/TransactionPointcut.java
@@ -1,19 +1,22 @@
 package aop.stage1;
 
-import org.springframework.aop.support.StaticMethodMatcherPointcut;
-
 import java.lang.reflect.Method;
+import org.springframework.aop.support.StaticMethodMatcherPointcut;
+import aop.Transactional;
 
 /**
  * 포인트컷(pointcut). 어드바이스를 적용할 조인 포인트를 선별하는 클래스.
  * TransactionPointcut 클래스는 메서드를 대상으로 조인 포인트를 찾는다.
- *
+ * <p>
  * 조인 포인트(join point). 어드바이스가 적용될 위치
+ * <p>
+ * StaticMethodMatcherPointcut: 해당 클래스를 상속 받아 정적인 메서드 매칭을 위해 matches를 구현한다.
+ * StaticMethodMatcherPointcut.matches: 어드바이스가 특정 메서드에 적용될지를 판단한다.
  */
 public class TransactionPointcut extends StaticMethodMatcherPointcut {
 
     @Override
     public boolean matches(final Method method, final Class<?> targetClass) {
-        return false;
+        return method.isAnnotationPresent(Transactional.class); // 어드바이스 적용 대상 여부 판단 기준
     }
 }
