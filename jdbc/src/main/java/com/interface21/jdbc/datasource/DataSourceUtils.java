74e1e11197b915ce93c1016f6b0744bc6c65bac0
jimi567 <28584160+jimi567@users.noreply.github.com>
2024-10-14 14:33:20 +0900

    refactor: releaseConncetion 내부에서 Connection unbind하도록 변경

--- jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
@@ -2,10 +2,9 @@ package com.interface21.jdbc.datasource;
 
 import com.interface21.jdbc.CannotGetJdbcConnectionException;
 import com.interface21.transaction.support.TransactionSynchronizationManager;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 // 4단계 미션에서 사용할 것
 public abstract class DataSourceUtils {
@@ -29,6 +28,7 @@ public abstract class DataSourceUtils {
 
     public static void releaseConnection(Connection connection, DataSource dataSource) {
         try {
+            TransactionSynchronizationManager.unbindResource(dataSource);
             connection.close();
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
