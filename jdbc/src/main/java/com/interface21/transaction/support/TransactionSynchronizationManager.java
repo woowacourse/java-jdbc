22dc6f56bd7819a7f4087098c5afb21b0712a727
leegwichan <cnddkscndgus@naver.com>
2024-10-14 09:45:01 +0900

    feat: 트랜잭션을 전역으로 관리하는 매니저 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,39 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
+import com.interface21.dao.DataAccessException;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import javax.annotation.Nullable;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
+    @Nullable
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> connectionManager = resources.get();
+        if (connectionManager == null) {
+            connectionManager = new HashMap<>();
+            resources.set(connectionManager);
+        }
+        return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> connectionManager = resources.get();
+        if (connectionManager == null) {
+            connectionManager = new HashMap<>();
+            resources.set(connectionManager);
+        }
+        connectionManager.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        return resources.get().remove(key);
     }
 }
