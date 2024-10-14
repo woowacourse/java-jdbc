b1e47de79e66727cdbc17a8e372f9998569d1d0b
Minjoo <m0522inju@gmail.com>
2024-10-14 19:10:58 +0900

    test: TxUserServiceTest로 이동

--- app/src/test/java/com/techcourse/service/TxUserServiceTest.java
+++ app/src/test/java/com/techcourse/service/TxUserServiceTest.java
@@ -1,9 +1,12 @@
 package com.techcourse.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import javax.sql.DataSource;
+import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.core.JdbcTemplate;
+import com.interface21.jdbc.core.JdbcTransactionManager;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
@@ -12,7 +15,7 @@ import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class AppUserServiceTest {
+public class TxUserServiceTest {
 
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
@@ -30,16 +33,21 @@ class AppUserServiceTest {
     }
 
     @Test
-    void testChangePassword() {
-        UserHistoryDao userHistoryDao = new UserHistoryDao(jdbcTemplate);
-        UserService userService = new AppUserService(userDao, userHistoryDao);
+    void testTransactionRollback() {
+        // 트랜잭션 롤백 테스트를 위해 mock으로 교체
+        UserHistoryDao userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
+        AppUserService appUserService = new AppUserService(userDao, userHistoryDao);
+        JdbcTransactionManager jdbcTransactionManager = new JdbcTransactionManager(dataSource);
+        UserService userService = new TxUserService(appUserService, jdbcTransactionManager);
 
-        String newPassword = "qqqqq";
+        String newPassword = "newPassword";
         String createBy = "gugu";
-        userService.changePassword(1L, newPassword, createBy);
+        // 트랜잭션이 정상 동작하는지 확인하기 위해 의도적으로 MockUserHistoryDao에서 예외를 발생시킨다.
+        assertThrows(DataAccessException.class,
+                () -> userService.changePassword(1L, newPassword, createBy));
 
         User actual = userService.findById(1L);
 
-        assertThat(actual.getPassword()).isEqualTo(newPassword);
+        assertThat(actual.getPassword()).isNotEqualTo(newPassword);
     }
 }
