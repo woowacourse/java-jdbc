62b73dc1b0d275a8d970446a6a4a9f7cb25bb306
J-I-H-O <jeongjiho0731@gmail.com>
2024-10-14 10:02:01 +0900

    feat: 트랜잭션 동기화 기능 구현

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,6 +1,7 @@
 package com.techcourse.service;
 
 import com.interface21.dao.DataAccessException;
+import com.interface21.transaction.support.TransactionSynchronizationManager;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
@@ -8,6 +9,7 @@ import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,26 +34,31 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        Connection connection = connect();
+        DataSource dataSource = DataSourceConfig.getInstance();
+        Connection connection = connect(dataSource);
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
         try (connection) {
             connection.setAutoCommit(false);
 
             final var user = findById(id);
             user.changePassword(newPassword);
-            userDao.updateWithConnection(connection, user);
-            userHistoryDao.logWithConnection(connection, new UserHistory(user, createBy));
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
 
             connection.commit();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             rollback(connection);
             throw new DataAccessException("Failed to commit transaction.", e);
+        } finally {
+            TransactionSynchronizationManager.unbindResource(dataSource);
         }
     }
 
-    private Connection connect() {
+    private Connection connect(DataSource dataSource) {
         try {
-            return DataSourceConfig.getInstance().getConnection();
+            return dataSource.getConnection();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException("Failed to connect.", e);
