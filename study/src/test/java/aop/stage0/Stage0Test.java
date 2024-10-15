b2e8c9145e4d16f0e2f28f08be33dc14fc8df156
ashsty <qkralsdk991@naver.com>
2024-10-15 14:29:13 +0900

    test: AOP 학습 테스트 stage 0 수행

--- study/src/test/java/aop/stage0/Stage0Test.java
+++ study/src/test/java/aop/stage0/Stage0Test.java
@@ -15,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.lang.reflect.Proxy;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -38,35 +40,43 @@ class Stage0Test {
 
     @BeforeEach
     void setUp() {
-        final var user = new User("gugu", "password", "hkkang@woowahan.com");
+        User user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(user);
     }
 
     @Test
     void testChangePassword() {
-        final var appUserService = new AppUserService(userDao, userHistoryDao);
-        final UserService userService = null;
+        AppUserService appUserService = new AppUserService(userDao, userHistoryDao);
+        UserService userService = (UserService) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{UserService.class},
+                new TransactionHandler(platformTransactionManager, appUserService)
+        );
 
-        final var newPassword = "qqqqq";
-        final var createBy = "gugu";
+        String newPassword = "qqqqq";
+        String createBy = "gugu";
         userService.changePassword(1L, newPassword, createBy);
 
-        final var actual = userService.findById(1L);
+        User actual = userService.findById(1L);
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
 
     @Test
     void testTransactionRollback() {
-        final var appUserService = new AppUserService(userDao, stubUserHistoryDao);
-        final UserService userService = null;
+        AppUserService appUserService = new AppUserService(userDao, stubUserHistoryDao);
+        UserService userService = (UserService) Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{UserService.class},
+                new TransactionHandler(platformTransactionManager, appUserService)
+        );
 
-        final var newPassword = "newPassword";
-        final var createBy = "gugu";
+        String newPassword = "newPassword";
+        String createBy = "gugu";
         assertThrows(DataAccessException.class,
                 () -> userService.changePassword(1L, newPassword, createBy));
 
-        final var actual = userService.findById(1L);
+        User actual = userService.findById(1L);
 
         assertThat(actual.getPassword()).isNotEqualTo(newPassword);
     }
--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,36 @@
 package aop.stage0;
 
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class TransactionHandler implements InvocationHandler {
 
-    /**
-     * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
-     */
+    private final PlatformTransactionManager platformTransactionManager;
+    private final Object target;
+
+    public TransactionHandler(PlatformTransactionManager platformTransactionManager, Object target) {
+        this.platformTransactionManager = platformTransactionManager;
+        this.target = target;
+    }
+
     @Override
-    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+    public Object invoke(Object proxy, final Method method, final Object[] args) throws Throwable {
+        TransactionStatus status = platformTransactionManager.getTransaction(TransactionDefinition.withDefaults());
+        try {
+            proxy = method.invoke(target, args);
+            platformTransactionManager.commit(status);
+            return proxy;
+        } catch (InvocationTargetException e) {
+            platformTransactionManager.rollback(status);
+            throw e.getCause();
+        } catch (Throwable throwable) {
+            platformTransactionManager.rollback(status);
+            throw throwable;
+        }
     }
 }
