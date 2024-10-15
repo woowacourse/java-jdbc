e03e94068c9136b107308ff116a93d51daa1e696
juha <khabh@naver.com>
2024-10-15 09:33:11 +0900

    test: 트랜잭션 어노테이션 검증 로직 수정

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -3,6 +3,7 @@ package aop.stage0;
 import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
@@ -18,7 +19,7 @@ public class TransactionHandler implements InvocationHandler {
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        if (!method.isAnnotationPresent(Transactional.class)) {
+        if (isNonTransactional(method)) {
             return method.invoke(target, args);
         }
         var status = transactionManager.getTransaction(new DefaultTransactionDefinition());
@@ -30,6 +31,19 @@ public class TransactionHandler implements InvocationHandler {
             transactionManager.rollback(status);
             throw throwable.getCause();
         }
+    }
+
+    private boolean isNonTransactional(Method method) {
+        return Arrays.stream(target.getClass().getMethods())
+                .filter(targetMethod -> isSameMethodSignature(targetMethod, method))
+                .findFirst()
+                .map(targetMethod -> !targetMethod.isAnnotationPresent(Transactional.class))
+                .orElseThrow();
+    }
+
+    private boolean isSameMethodSignature(Method targetMethod, Method method) {
+        return targetMethod.getName().equals(method.getName())
+                && Arrays.equals(targetMethod.getParameterTypes(), method.getParameterTypes());
     }
 }
 
