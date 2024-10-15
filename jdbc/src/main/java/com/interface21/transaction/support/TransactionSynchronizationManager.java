75ff435ea27f02cf7bfb237eb056894a0e434935
KIMGYUTAE <kiimwf0819@gmail.com>
2024-10-15 17:47:13 +0900

    refactor: ThreadLocal.withInitial 적용

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -7,36 +7,20 @@ import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {
     }
 
     public static Connection getResource(DataSource key) {
-        Map<DataSource, Connection> dataSourceConnection = resources.get();
-        if (dataSourceConnection == null) {
-            return null;
-        }
-
-        return dataSourceConnection.get(key);
+        return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        Map<DataSource, Connection> dataSourceConnection = resources.get();
-        if (dataSourceConnection == null) {
-            dataSourceConnection = new HashMap<>();
-            resources.set(dataSourceConnection);
-        }
-
-        dataSourceConnection.put(key, value);
+        resources.get().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        Map<DataSource, Connection> dataSourceConnection = resources.get();
-        if (dataSourceConnection == null || !dataSourceConnection.containsKey(key)) {
-            throw new IllegalArgumentException("No Connection For DataSource");
-        }
-
-        return dataSourceConnection.remove(key);
+        return resources.get().remove(key);
     }
 }
