1b5d4690a14ca51cce244f86eb4318d1398072d6
BurningFalls <sherlocky31415@gmail.com>
2024-10-15 10:44:04 +0900

    refactor: ThreadLocal.withInitial() 사용

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -4,32 +4,22 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.Optional;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
-        return Optional.ofNullable(resources.get())
-                .map(resourceMap -> resourceMap.get(key))
-                .orElse(null);
+        return resources.get().getOrDefault(key, null);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        final Map<DataSource, Connection> resourceMap = resources.get();
-        if (Objects.isNull(resourceMap)) {
-            resources.set(new HashMap<>());
-        }
         resources.get().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return Optional.ofNullable(resources.get())
-                .map(resourceMap -> resourceMap.remove(key))
-                .orElse(null);
+        return resources.get().remove(key);
     }
 }
