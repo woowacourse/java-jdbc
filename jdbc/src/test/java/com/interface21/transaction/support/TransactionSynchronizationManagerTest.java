9bd577aed9bf9e97b1a8d85e7289122ee3607bd8
juha <khabh@naver.com>
2024-10-15 10:19:00 +0900

    test: 예외 상황에 대한 테스트 추가

--- jdbc/src/test/java/com/interface21/transaction/support/TransactionSynchronizationManagerTest.java
+++ jdbc/src/test/java/com/interface21/transaction/support/TransactionSynchronizationManagerTest.java
@@ -2,6 +2,8 @@ package com.interface21.transaction.support;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.interface21.dao.DataAccessException;
 import org.junit.jupiter.api.*;
@@ -26,6 +28,7 @@ public class TransactionSynchronizationManagerTest {
         TransactionSynchronizationManager.unbindResource(dataSource);
     }
 
+    @DisplayName("커넥션을 바인딩한다.")
     @Test
     public void bindResource() {
         TransactionSynchronizationManager.bindResource(dataSource, connection);
@@ -34,21 +37,39 @@ public class TransactionSynchronizationManagerTest {
                 .isEqualTo(connection);
     }
 
+    @DisplayName("이미 커넥션이 존재하는 경우 예외가 발생한다.")
     @Test
     public void resourceAlreadyBound() {
         TransactionSynchronizationManager.bindResource(dataSource, connection);
+        Connection connection2 = Mockito.mock(Connection.class);
 
-        assertThatThrownBy(() -> TransactionSynchronizationManager.bindResource(dataSource, connection))
+        assertThatThrownBy(() -> TransactionSynchronizationManager.bindResource(dataSource, connection2))
                 .isInstanceOf(DataAccessException.class)
                 .hasMessage("Transaction already started for this datasource");
+        assertThat(TransactionSynchronizationManager.getResource(dataSource)).isEqualTo(connection);
     }
 
+    @DisplayName("커넥션을 언바인드한다.")
     @Test
-    public void testUnbindResource() {
+    public void unbindResource() {
         TransactionSynchronizationManager.bindResource(dataSource, connection);
 
         TransactionSynchronizationManager.unbindResource(dataSource);
 
         assertThat(TransactionSynchronizationManager.getResource(dataSource)).isNull();
+    }
+
+    @DisplayName("커넥션이 존재할 때 관리된다고 판단한다.")
+    @Test
+    public void dataSourceManaged() {
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        assertFalse(TransactionSynchronizationManager.doesNotManage(dataSource));
+    }
+
+    @DisplayName("커넥션이 존재하지 않을 때 관리되지 않는다고 판단한다.")
+    @Test
+    public void dataSourceNotManaged() {
+        assertTrue(TransactionSynchronizationManager.doesNotManage(dataSource));
     }
 }
