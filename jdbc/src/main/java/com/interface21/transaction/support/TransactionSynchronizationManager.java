3e2d23cd07644508939fd682af60fbf4783483f0
SCY <helenason@naver.com>
2024-10-16 00:58:38 +0900

    refactor: ThreadLocal 초기화 방식 변경

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -4,32 +4,30 @@ import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
 import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final Logger log = LoggerFactory.getLogger(TransactionSynchronizationManager.class);
+
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {
     }
 
     public static Connection getResource(DataSource key) {
-        if (resources.get() == null) {
-            return null;
-        }
+        log.info("get resource: {}", key);
         return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        if (resources.get() == null) {
-            Map<DataSource, Connection> map = new HashMap<>();
-            map.put(key, value);
-            resources.set(map);
-            return;
-        }
+        log.info("bind resource: {}", key);
         resources.get().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
+        log.info("unbind resource: {}", key);
         return resources.get().remove(key);
     }
 }
