7c423ea563a8e8755dab188cad8091d928c5ac5b
SCY <helenason@naver.com>
2024-10-15 21:24:57 +0900

    test: 학습테스트 ProxyFactoryBean

--- study/src/test/java/aop/stage1/TransactionAdvice.java
+++ study/src/test/java/aop/stage1/TransactionAdvice.java
@@ -1,15 +1,39 @@
 package aop.stage1;
 
+import aop.DataAccessException;
+import java.lang.reflect.Method;
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
  * 어드바이스(advice). 부가기능을 담고 있는 클래스
  */
-public class TransactionAdvice  implements MethodInterceptor {
+public class TransactionAdvice implements MethodInterceptor {
+
+    private final PlatformTransactionManager platformTransactionManager;
+    private final Object target;
+
+    public TransactionAdvice(PlatformTransactionManager platformTransactionManager, Object target) {
+        this.platformTransactionManager = platformTransactionManager;
+        this.target = target;
+    }
 
     @Override
     public Object invoke(final MethodInvocation invocation) throws Throwable {
-        return null;
+        TransactionStatus transactionStatus = platformTransactionManager.getTransaction(
+                new DefaultTransactionDefinition());
+        try {
+            Method method = invocation.getMethod();
+            Object[] args = invocation.getArguments();
+            Object ret = method.invoke(target, args);
+            platformTransactionManager.commit(transactionStatus);
+            return ret;
+        } catch (Exception e) {
+            platformTransactionManager.rollback(transactionStatus);
+            throw new DataAccessException(e);
+        }
     }
 }
