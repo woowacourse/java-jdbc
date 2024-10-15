1c6709648c1150924f8dfceca99144f1e0939688
임수빈 <robin980108@naver.com>
2024-10-16 02:48:49 +0900

    test: aop 학습 테스트 2단계

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,33 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AopConfig {
 
+    private final PlatformTransactionManager platformTransactionManager;
+
+    public AopConfig(PlatformTransactionManager platformTransactionManager) {
+        this.platformTransactionManager = platformTransactionManager;
+    }
+
+    @Bean
+    public TransactionAdvisor transactionAdvisor() {
+        return new TransactionAdvisor(transactionPointcut(), transactionAdvice());
+    }
+
+    @Bean
+    public TransactionPointcut transactionPointcut() {
+        return new TransactionPointcut();
+    }
+
+    @Bean
+    public TransactionAdvice transactionAdvice() {
+        return new TransactionAdvice(platformTransactionManager);
+    }
 }
--- study/src/test/java/aop/stage2/Stage2Test.java
+++ study/src/test/java/aop/stage2/Stage2Test.java
@@ -1,5 +1,8 @@
 package aop.stage2;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import aop.DataAccessException;
 import aop.StubUserHistoryDao;
 import aop.domain.User;
@@ -7,11 +10,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.framework.autoproxy.AbstractAutoProxyCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Stage2Test {
@@ -19,10 +21,13 @@ class Stage2Test {
     private static final Logger log = LoggerFactory.getLogger(Stage2Test.class);
 
     @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
     private UserService userService;
 
     @Autowired
     private StubUserHistoryDao stubUserHistoryDao;
+
 
     @BeforeEach
     void setUp() {
@@ -37,6 +42,7 @@ class Stage2Test {
         userService.changePassword(1L, newPassword, createBy);
 
         final var actual = userService.findById(1L);
+
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
@@ -53,5 +59,12 @@ class Stage2Test {
         final var actual = userService.findById(1L);
 
         assertThat(actual.getPassword()).isNotEqualTo(newPassword);
+    }
+
+    @Test
+    void abstractAutoProxyCreator() {
+        AbstractAutoProxyCreator bean = applicationContext.getBean(AbstractAutoProxyCreator.class);
+
+        System.out.println(bean);
     }
 }
