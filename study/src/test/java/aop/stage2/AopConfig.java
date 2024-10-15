64d719cc79dc8b278515f179c7befd71ceddbaf7
unifolio0 <boho82ha@gmail.com>
2024-10-15 11:27:53 +0900

    test: DefaultAdvisorAutoProxyCreator 빈 추가

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -3,6 +3,7 @@ package aop.stage2;
 import aop.stage1.TransactionAdvice;
 import aop.stage1.TransactionAdvisor;
 import aop.stage1.TransactionPointcut;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -23,5 +24,12 @@ public class AopConfig {
     @Bean
     public TransactionAdvisor transactionAdvisor(TransactionPointcut transactionPointcut, TransactionAdvice transactionAdvice) {
         return new TransactionAdvisor(transactionPointcut, transactionAdvice);
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator = new DefaultAdvisorAutoProxyCreator();
+        defaultAdvisorAutoProxyCreator.setProxyTargetClass(true);
+        return defaultAdvisorAutoProxyCreator;
     }
 }
