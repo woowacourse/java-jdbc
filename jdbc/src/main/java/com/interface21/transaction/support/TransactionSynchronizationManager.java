6f6d846fdbe0e57280e6ac0b177d0ba56713bad3
DESKTOP-E5VRH4Q\cnddk <cnddkscndgus@naver.com>
2024-10-16 11:30:15 +0900

    refactor: `ThreadLocal.withInitial()`을 이용하여 초기 리소스 설정

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,6 +1,5 @@
 package com.interface21.transaction.support;
 
-import com.interface21.dao.DataAccessException;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,28 +8,18 @@ import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {
     }
 
     @Nullable
     public static Connection getResource(DataSource key) {
-        Map<DataSource, Connection> connectionManager = resources.get();
-        if (connectionManager == null) {
-            connectionManager = new HashMap<>();
-            resources.set(connectionManager);
-        }
         return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        Map<DataSource, Connection> connectionManager = resources.get();
-        if (connectionManager == null) {
-            connectionManager = new HashMap<>();
-            resources.set(connectionManager);
-        }
-        connectionManager.put(key, value);
+        resources.get().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
