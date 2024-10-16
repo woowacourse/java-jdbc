7a10f4146b3188c7856e5c14845104633f533f12
hjk0761 <hjk0761@naver.com>
2024-10-16 23:33:19 +0900

    test: 학습 테스트 2단계 진행

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,32 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AopConfig {
+
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    @Bean
+    public TransactionAdvice transactionAdvice() {
+        return new TransactionAdvice(platformTransactionManager);
+    }
+
+    @Bean
+    public TransactionPointcut transactionPointcut() {
+        return new TransactionPointcut();
+    }
+
+    @Bean
+    public TransactionAdvisor transactionAdvisor() {
+        return new TransactionAdvisor(transactionPointcut(), transactionAdvice());
+    }
 
 }
