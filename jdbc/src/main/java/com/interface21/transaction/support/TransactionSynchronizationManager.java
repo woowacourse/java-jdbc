1bb5a1d12384ca99b578d68cf1471a83a747e523
Nam Gi Beom <rlqja0523@naver.com>
2024-10-16 11:27:27 +0900

    refactor: ThreadLocal.withInitial로 초기화

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -6,7 +6,7 @@ import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Object>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Object>> resources = ThreadLocal.withInitial(HashMap::new);
     private static final ThreadLocal<Boolean> isTransactionActive = new ThreadLocal<>();
 
     private TransactionSynchronizationManager() {
@@ -14,18 +14,11 @@ public abstract class TransactionSynchronizationManager {
 
     public static Object getResource(DataSource key) {
         Map<DataSource, Object> map = resources.get();
-        if (map == null) {
-            return null;
-        }
         return map.get(key);
     }
 
     public static void bindResource(DataSource key, Object value) {
         Map<DataSource, Object> map = resources.get();
-        if (map == null) {
-            map = new HashMap<>();
-            resources.set(map);
-        }
         Object oldValue = map.put(key, value);
         if (oldValue != null) {
             throw new IllegalStateException("Already value [ %s ] for key [ %s ] bound to thread [ %s]"
@@ -35,9 +28,6 @@ public abstract class TransactionSynchronizationManager {
 
     public static Object unbindResource(DataSource key) {
         Map<DataSource, Object> map = resources.get();
-        if (map == null) {
-            return null;
-        }
         Object value = map.remove(key);
         if (map.isEmpty()) {
             resources.remove();
