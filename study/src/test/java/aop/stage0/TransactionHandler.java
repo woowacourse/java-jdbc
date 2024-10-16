f721c85b0bab0d6544e43f3b899dcd5cda110aeb
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-10-17 01:33:23 +0900

    fix: Transactional 애너테이션 없는 곳에도 트랜잭션이 적용되는 버그 수정

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,8 +1,10 @@
 package aop.stage0;
 
 import aop.DataAccessException;
+import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
@@ -22,6 +24,9 @@ public class TransactionHandler implements InvocationHandler {
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+        if (hasNotTransactional(method)) {
+            return method.invoke(target, args);
+        }
         TransactionStatus status = platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
 
         try {
@@ -33,5 +38,17 @@ public class TransactionHandler implements InvocationHandler {
             platformTransactionManager.rollback(status);
             throw new DataAccessException();
         }
+    }
+
+    private boolean hasNotTransactional(Method method) {
+        return !target.getClass().isAnnotationPresent(Transactional.class) && hasNotMethodTransactional(method);
+    }
+
+    private boolean hasNotMethodTransactional(Method method) {
+        return !Arrays.stream(target.getClass().getMethods())
+                .filter(m -> m.getName().equals(method.getName()))
+                .findFirst()
+                .map(m -> m.isAnnotationPresent(Transactional.class))
+                .orElseThrow(() -> new IllegalArgumentException("메소드가 존재하지 않습니다."));
     }
 }
