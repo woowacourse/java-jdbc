2f5aa99bfe5d071047d1f0a9ef8bcc60be92a4a2
kunsanglee <gosmdochee2@gmail.com>
2024-10-16 09:29:15 +0900

    refactor: TransactionHandler @Transactional 애너테이션이 없는 경우에도 동작하도록 수정

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -5,6 +5,7 @@ import aop.Transactional;
 import aop.service.AppUserService;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -24,26 +25,38 @@ public class TransactionHandler implements InvocationHandler {
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) {
-        validateHasMethodAnnotation(method);
+        if (hasMethodAnnotation(method)) {
+            return invokeInTransaction(() -> invoke(method, args));
+        }
+        return invoke(method, args);
+    }
+
+    private boolean hasMethodAnnotation(Method method) {
+        try {
+            Method implementationMethod = appUserService.getClass().getMethod(method.getName(), method.getParameterTypes());
+            return implementationMethod.isAnnotationPresent(Transactional.class);
+        } catch (NoSuchMethodException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private Object invokeInTransaction(Callable<Object> callable) {
         TransactionStatus status = platformTransactionManager.getTransaction(TransactionDefinition.withDefaults());
         try {
-            Object result = method.invoke(appUserService, args);
+            Object result = callable.call();
             platformTransactionManager.commit(status);
             return result;
         } catch (Exception e) {
             platformTransactionManager.rollback(status);
-            throw new DataAccessException("An error occurred during transaction", e);
+            throw new DataAccessException("An error occurred in transaction", e);
         }
     }
 
-    private void validateHasMethodAnnotation(Method method) {
+    private Object invoke(Method method, Object[] args) {
         try {
-            Method implementationMethod = appUserService.getClass().getMethod(method.getName(), method.getParameterTypes());
-            if (!implementationMethod.isAnnotationPresent(Transactional.class)) {
-                throw new DataAccessException("Method has not Transactional annotation");
-            }
-        } catch (NoSuchMethodException e) {
-            throw new DataAccessException(e);
+            return method.invoke(appUserService, args);
+        } catch (Exception e) {
+            throw new DataAccessException("An error occurred during invoke method", e);
         }
     }
 }
