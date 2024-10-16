05405d1cba9c69f26043c6812381c56a947b76e5
Leetaehoon <66353672+xogns1514@users.noreply.github.com>
2024-10-16 09:40:22 +0900

    test(Stage2Test): Stage2Test 학습 테스트

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,24 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvisor;
+import org.springframework.aop.PointcutAdvisor;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AopConfig {
 
+    @Bean
+    public PointcutAdvisor transactionAdvisor(PlatformTransactionManager transactionManager) {
+        return new TransactionAdvisor(transactionManager);
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
+        proxyCreator.setProxyTargetClass(true);
+        return proxyCreator;
+    }
 }
