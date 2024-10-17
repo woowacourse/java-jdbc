38305a02f78a7d8cf357a7b1c2bfa5b2beb29b8d
ehtjsv2 <ehtjsv2@naver.com>
2024-10-17 10:19:36 +0900

    refactor: 테스트 클래스명 변경

--- app/src/test/java/com/techcourse/service/TxUserServiceTest.java
+++ app/src/test/java/com/techcourse/service/TxUserServiceTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class AppUserServiceTest {
+class TxUserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
     private UserDao userDao;
