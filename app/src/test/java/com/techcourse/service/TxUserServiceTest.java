6dde3211279a3e81d84b11983dc341955bd9aea1
Minjoo <m0522inju@gmail.com>
2024-10-17 20:17:15 +0900

    refactor: 불필요한 접근 제어자 제거

--- app/src/test/java/com/techcourse/service/TxUserServiceTest.java
+++ app/src/test/java/com/techcourse/service/TxUserServiceTest.java
@@ -17,7 +17,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class TxUserServiceTest {
+class TxUserServiceTest {
 
     private DataSource dataSource;
     private JdbcTemplate jdbcTemplate;
