fcb1efb8f176e9f59de594b7552fe0d3d35c0339
hyeonjilee <skylar12200@gmail.com>
2024-10-18 07:17:05 +0900

    refactor: 메서드 분리 및 예외 처리 추가, 콜백 함수 적용

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -4,6 +4,7 @@ import aop.DataAccessException;
 import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionDefinition;
 import org.springframework.transaction.TransactionStatus;
@@ -23,18 +24,29 @@ public class TransactionHandler implements InvocationHandler {
         Method targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
 
         if (!targetMethod.isAnnotationPresent(Transactional.class)) {
-            return targetMethod.invoke(target, args);
+            return invoke(args, targetMethod);
         }
 
+        return invokeTransaction(() -> invoke(args, targetMethod));
+    }
+
+    private Object invokeTransaction(Callable<Object> callable) {
         TransactionStatus status = platformTransactionManager.getTransaction(TransactionDefinition.withDefaults());
         try {
-            Object result = targetMethod.invoke(target, args);
+            Object result = callable.call();
             platformTransactionManager.commit(status);
             return result;
         } catch (Exception e) {
             platformTransactionManager.rollback(status);
             throw new DataAccessException(e);
         }
+    }
 
+    private Object invoke(Object[] args, Method targetMethod) {
+        try {
+            return targetMethod.invoke(target, args);
+        } catch (ReflectiveOperationException e) {
+            throw new IllegalStateException();
+        }
     }
 }
