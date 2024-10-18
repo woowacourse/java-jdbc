f6cea12067cd1fab9937d6d4a633c3616ffd037e
coli-geonwoo <coli-geonwoo@gmail.com>
2024-10-19 01:51:51 +0900

    refactor: 중복 제네릭 객체로 분리

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,9 +1,6 @@
 package com.interface21.transaction.support;
 
 import java.sql.Connection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicLong;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,7 +8,7 @@ import org.slf4j.LoggerFactory;
 public abstract class TransactionSynchronizationManager {
 
     private static final Logger logger = LoggerFactory.getLogger(TransactionSynchronizationManager.class);
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadMap<DataSource, Connection> resources = new ThreadMap<>();
     private static final ThreadLocal<Boolean> transactionActive = new ThreadLocal<>();
 
     private TransactionSynchronizationManager() {
@@ -20,28 +17,18 @@ public abstract class TransactionSynchronizationManager {
     public static Connection getResource(DataSource key) {
         logger.info("dataSource : {}", key);
         logger.info("thread : {}", Thread.currentThread().getName());
-        return getThreadResource().get(key);
+        return resources.getMap(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        getThreadResource().put(key, value);
+        resources.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return getThreadResource().remove(key);
-    }
-
-    private static Map<DataSource, Connection> getThreadResource() {
-        if (resources.get() == null) {
-            resources.set(new HashMap<>());
-        }
-        return resources.get();
+        return resources.remove(key);
     }
 
     public static boolean isTransactionActive() {
-        if (transactionActive.get() == null) {
-            transactionActive.set(false);
-        }
         return transactionActive.get();
     }
 
