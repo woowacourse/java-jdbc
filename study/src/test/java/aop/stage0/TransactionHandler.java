e852f53a27fa1afd9ce4678155d0904d271b8e42
le2sky <leehaneul990623@gmail.com>
2024-10-19 13:21:08 +0900

    test: aop 0단계 학습 테스트 구현

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,46 @@
 package aop.stage0;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import aop.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TransactionHandler implements InvocationHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(TransactionHandler.class);
+    private final Object target;
+    private final PlatformTransactionManager platformTransactionManager;
+
+    public TransactionHandler(Object target, PlatformTransactionManager platformTransactionManager) {
+        this.target = target;
+        this.platformTransactionManager = platformTransactionManager;
+    }
 
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        Method targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
+        if (!targetMethod.isAnnotationPresent(Transactional.class)) {
+            return method.invoke(target, args);
+        }
+
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        TransactionStatus status = platformTransactionManager.getTransaction(definition);
+
+        try {
+            Object invoke = method.invoke(target, args);
+            platformTransactionManager.commit(status);
+            return invoke;
+        } catch (InvocationTargetException e) {
+            platformTransactionManager.rollback(status);
+            throw e.getTargetException();
+        }
     }
 }
