9194b20eb68911026d56d2e2c3c3450aa73519ef
Arachne <66822642+Arachneee@users.noreply.github.com>
2024-10-20 21:02:25 +0900

    [4단계 - JDBC 라이브러리 구현하기] 백호(전호건) 미션 제출합니다. (#862)
    * feat: ThreadLocal Connection 구현
    
    * feat: Transaction 추상화
    
    * refactor: 불필요 로직 제거
    
    * test: 학습테스트 추가
    
    * refactor: 메소드 분리
    
    * fix: Transactional 애너테이션 없는 곳에도 트랜잭션이 적용되는 버그 수정

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,53 @@
+package com.techcourse.service;
+
+import com.interface21.jdbc.core.SQLExecuteException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+public class TxUserService implements UserService {
+
+    private final UserService userService;
+
+    public TxUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public User findById(long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void save(User user) {
+        userService.save(user);
+    }
+
+    @Override
+    public void changePassword(long id, String newPassword, String createdBy) {
+        DataSource dataSource = DataSourceConfig.getInstance();
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+
+        try {
+            connection.setAutoCommit(false);
+
+            userService.changePassword(id, newPassword, createdBy);
+
+            connection.commit();
+        } catch (Exception e) {
+            rollback(connection);
+            throw new SQLExecuteException(e.getMessage(), e);
+        } finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+        }
+    }
+
+    private void rollback(Connection connection) {
+        try {
+            connection.rollback();
+        } catch (SQLException ex) {}
+    }
+}
--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,54 @@
 package aop.stage0;
 
+import aop.DataAccessException;
+import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TransactionHandler implements InvocationHandler {
+
+    private final PlatformTransactionManager platformTransactionManager;
+    private final Object target;
+
+    public TransactionHandler(PlatformTransactionManager platformTransactionManager, Object target) {
+        this.platformTransactionManager = platformTransactionManager;
+        this.target = target;
+    }
 
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        if (hasNotTransactional(method)) {
+            return method.invoke(target, args);
+        }
+        TransactionStatus status = platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            Object invoke = method.invoke(target, args);
+
+            platformTransactionManager.commit(status);
+            return invoke;
+        } catch (Exception e) {
+            platformTransactionManager.rollback(status);
+            throw new DataAccessException();
+        }
+    }
+
+    private boolean hasNotTransactional(Method method) {
+        return !target.getClass().isAnnotationPresent(Transactional.class) && hasNotMethodTransactional(method);
+    }
+
+    private boolean hasNotMethodTransactional(Method method) {
+        return !Arrays.stream(target.getClass().getMethods())
+                .filter(m -> m.getName().equals(method.getName()))
+                .findFirst()
+                .map(m -> m.isAnnotationPresent(Transactional.class))
+                .orElseThrow(() -> new IllegalArgumentException("메소드가 존재하지 않습니다."));
     }
 }
