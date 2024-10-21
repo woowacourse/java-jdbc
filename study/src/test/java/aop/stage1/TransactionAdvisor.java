8c8956eac09cf0d7953ca896ff8995cde498721f
jinwoo22 <jenny010925@naver.com>
2024-10-21 12:36:08 +0900

    feat: stage1 학습 테스트

--- study/src/test/java/aop/stage1/TransactionAdvisor.java
+++ study/src/test/java/aop/stage1/TransactionAdvisor.java
@@ -3,6 +3,7 @@ package aop.stage1;
 import org.aopalliance.aop.Advice;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.PointcutAdvisor;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * 어드바이저(advisor). 포인트컷과 어드바이스를 하나씩 갖고 있는 객체.
@@ -10,14 +11,22 @@ import org.springframework.aop.PointcutAdvisor;
  */
 public class TransactionAdvisor implements PointcutAdvisor {
 
+    private final TransactionPointcut transactionPointcut;
+    private final TransactionAdvice transactionAdvice;
+
+    public TransactionAdvisor(PlatformTransactionManager transactionManager) {
+        this.transactionPointcut = new TransactionPointcut();
+        this.transactionAdvice = new TransactionAdvice(transactionManager);
+    }
+
     @Override
     public Pointcut getPointcut() {
-        return null;
+        return transactionPointcut;
     }
 
     @Override
     public Advice getAdvice() {
-        return null;
+        return transactionAdvice;
     }
 
     @Override
