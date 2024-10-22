a2c9497e53de2c5d937ac88bd97dd25470c13505
Choo <choo000407@naver.com>
2024-10-22 13:49:13 +0900

    test: AOP 학습테스트 Stage0 완료

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,10 +1,10 @@
 package aop.stage0;
 
+import aop.DataAccessException;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
-import transaction.stage1.jdbc.DataAccessException;
 
 public class TransactionHandler implements InvocationHandler {
 
@@ -20,13 +20,13 @@ public class TransactionHandler implements InvocationHandler {
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
-    public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
+    public Object invoke(final Object proxy, final Method method, final Object[] args) {
         var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
         try {
             Object ret = method.invoke(target, args); // 타겟 객체의 메서드 실행
             transactionManager.commit(transactionStatus);
             return ret;
-        } catch (RuntimeException e) {
+        } catch (Exception e) {
             transactionManager.rollback(transactionStatus);
             throw new DataAccessException(e);
         }
