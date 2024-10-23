a69481b2ed2bf52a600840d40dd50add9d1e3fb4
재우 <Akaps6768@gmail.com>
2024-10-23 15:40:30 +0900

    stage 2 test

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,25 @@
 package aop.stage2;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
 
 @Configuration
 public class AopConfig {
 
+    @Bean
+    public TransactionAdvisor transactionAdvisor(PlatformTransactionManager transactionManager) {
+        final var advice = new TransactionAdvice(transactionManager);
+        final var pointcut = new TransactionPointcut();
+        return new TransactionAdvisor(pointcut, advice);
+    }
+
+//    @Bean
+//    public DefaultAdvisorAutoProxyCreator defaultTransactionAdvisor() {
+//        return new DefaultAdvisorAutoProxyCreator();
+//    }
 }
