e782c07f4b3f3bbc62132f0024900a433ba661b9
masonkimseoul <87306418+masonkimseoul@users.noreply.github.com>
2024-10-24 23:26:54 +0900

    [4단계 - JDBC 라이브러리 구현하기] 메이슨(김경원) 미션 제출합니다. (#927)
    * test: 학습 테스트 0단계
    
    * test: 학습 테스트 1단계
    
    * test: 학습 테스트 2단계
    
    * refactor: Rollback 과정에 try-catch 적용

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,47 @@
 package aop.stage0;
 
+import aop.DataAccessException;
+import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionException;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TransactionHandler implements InvocationHandler {
 
-    /**
-     * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
-     */
+    private final PlatformTransactionManager platformTransactionManager;
+    private final Object target;
+
+    public TransactionHandler(PlatformTransactionManager platformTransactionManager, Object target) {
+        this.platformTransactionManager = platformTransactionManager;
+        this.target = target;
+    }
+
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        Method targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
+        if (!targetMethod.isAnnotationPresent(Transactional.class)) {
+            return method.invoke(target, args);
+        }
+        TransactionStatus transactionStatus
+                = platformTransactionManager.getTransaction(new DefaultTransactionDefinition());
+        try {
+            Object result = method.invoke(target, args);
+            platformTransactionManager.commit(transactionStatus);
+            return result;
+        } catch (Exception e) {
+            rollback(transactionStatus);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void rollback(TransactionStatus transactionStatus) {
+        try {
+            platformTransactionManager.rollback(transactionStatus);
+        } catch (TransactionException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
