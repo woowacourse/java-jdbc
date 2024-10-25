d37c20bfd4eb6c0345671b62cd419a33a5acf619
hyxrxn <gpfls981220@gmail.com>
2024-10-25 19:10:20 +0900

    test: 프록시 패턴 적용

--- study/src/test/java/aop/stage0/TransactionHandler.java
+++ study/src/test/java/aop/stage0/TransactionHandler.java
@@ -1,15 +1,42 @@
 package aop.stage0;
 
+import aop.Transactional;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 public class TransactionHandler implements InvocationHandler {
+
+    private final Object target;
+    private final PlatformTransactionManager transactionManager;
+
+
+    public TransactionHandler(Object target, PlatformTransactionManager transactionManager) {
+        this.target = target;
+        this.transactionManager = transactionManager;
+    }
 
     /**
      * @Transactional 어노테이션이 존재하는 메서드만 트랜잭션 기능을 적용하도록 만들어보자.
      */
     @Override
     public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
-        return null;
+        Method targetMethod = target.getClass().getMethod(method.getName(), method.getParameterTypes());
+        if (targetMethod.isAnnotationPresent(Transactional.class)) {
+            final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+            Object result;
+            try {
+                result = method.invoke(target, args);
+            } catch (InvocationTargetException e) {
+                transactionManager.rollback(transactionStatus);
+                throw e.getTargetException();
+            }
+            transactionManager.commit(transactionStatus);
+            return result;
+        }
+        return method.invoke(target, args);
     }
 }
