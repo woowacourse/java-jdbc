7baf4b4f5ffc35b9a090ee4847ddbdd853a4c75f
hoyeonyy <highoption@naver.com>
2024-10-26 22:33:54 +0900

    refactor: JdbcTemplate 구조 변경

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/ArgumentPreparedStatementSetter.java
@@ -0,0 +1,31 @@
+package com.interface21.jdbc.core;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.interface21.dao.DataAccessException;
+
+public class ArgumentPreparedStatementSetter implements PreparedStatementSetter {
+
+	private final Object[] args;
+
+	public ArgumentPreparedStatementSetter(final Object... args) {
+		this.args = args;
+	}
+
+	public void setValues(final PreparedStatement preparedStatement) {
+		AtomicInteger index = new AtomicInteger(1);
+		Arrays.stream(args)
+			.forEach(arg -> setValue(preparedStatement, index.getAndIncrement(), arg));
+	}
+
+	private void setValue(final PreparedStatement preparedStatement, int index, Object arg) {
+		try {
+			preparedStatement.setObject(index, arg);
+		} catch (SQLException e) {
+			throw new DataAccessException("SQL 인자가 올바르지 않거나, 데이터베이스에 엑세스하는 중 에러가 발생했습니다. 재시도해주세요.", e);
+		}
+	}
+}
