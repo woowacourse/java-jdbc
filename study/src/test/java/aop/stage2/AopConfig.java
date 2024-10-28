954b2a26165354c96d0cdcbfb3ad2625ee1937ff
hoyeonyy <highoption@naver.com>
2024-10-28 14:39:37 +0900

    test: 학습 테스트 구현

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,29 @@
 package aop.stage2;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class AopConfig {
 
+	@Bean
+	public TransactionAdvisor transactionAdvisor(final PlatformTransactionManager transactionManager) {
+		return new TransactionAdvisor(transactionManager);
+	}
+
+	@Bean
+	public TransactionPointcut transactionPointcut() {
+		return new TransactionPointcut();
+	}
+
+	public TransactionAdvice transactionAdvice(final PlatformTransactionManager transactionManager) {
+		return new TransactionAdvice(transactionManager);
+	}
 }
