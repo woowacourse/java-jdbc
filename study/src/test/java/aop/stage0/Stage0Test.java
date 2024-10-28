4639b27757b7a3a779bfcee09190a60cd70da237
reddevilmidzy <midzy0228@gmail.com>
2024-10-28 21:51:37 +0900

    stage0 학습 테스트 진행

--- study/src/test/java/aop/stage0/Stage0Test.java
+++ study/src/test/java/aop/stage0/Stage0Test.java
@@ -1,5 +1,17 @@
 package aop.stage0;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+import java.lang.reflect.Proxy;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+
 import aop.DataAccessException;
 import aop.StubUserHistoryDao;
 import aop.domain.User;
@@ -7,22 +19,9 @@ import aop.repository.UserDao;
 import aop.repository.UserHistoryDao;
 import aop.service.AppUserService;
 import aop.service.UserService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.PlatformTransactionManager;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class Stage0Test {
-
-    private static final Logger log = LoggerFactory.getLogger(Stage0Test.class);
 
     @Autowired
     private UserDao userDao;
@@ -45,7 +44,10 @@ class Stage0Test {
     @Test
     void testChangePassword() {
         final var appUserService = new AppUserService(userDao, userHistoryDao);
-        final UserService userService = null;
+        final UserService userService = (UserService) Proxy.newProxyInstance(
+                appUserService.getClass().getClassLoader(),
+                appUserService.getClass().getInterfaces(),
+                new TransactionHandler(platformTransactionManager, appUserService));
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -59,7 +61,10 @@ class Stage0Test {
     @Test
     void testTransactionRollback() {
         final var appUserService = new AppUserService(userDao, stubUserHistoryDao);
-        final UserService userService = null;
+        final UserService userService = (UserService) Proxy.newProxyInstance(
+                appUserService.getClass().getClassLoader(),
+                appUserService.getClass().getInterfaces(),
+                new TransactionHandler(platformTransactionManager, appUserService));
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
