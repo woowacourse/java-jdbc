c86582a4a8e744c8638afab2c8f4bdd5ed0d6ec4
seunghye218 <seunghye@student.42seoul.kr>
2024-10-29 15:06:27 +0900

    test: JDK Proxy로 프록시 패턴 적용하기

--- study/src/test/java/aop/stage0/Stage0Test.java
+++ study/src/test/java/aop/stage0/Stage0Test.java
@@ -1,5 +1,9 @@
 package aop.stage0;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
 import aop.DataAccessException;
 import aop.StubUserHistoryDao;
 import aop.domain.User;
@@ -7,6 +11,7 @@ import aop.repository.UserDao;
 import aop.repository.UserHistoryDao;
 import aop.service.AppUserService;
 import aop.service.UserService;
+import java.lang.reflect.Proxy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -14,10 +19,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class Stage0Test {
@@ -45,29 +46,40 @@ class Stage0Test {
     @Test
     void testChangePassword() {
         final var appUserService = new AppUserService(userDao, userHistoryDao);
-        final UserService userService = null;
 
+        // JDK Proxy를 활용하여 UserService 인터페이스를 기반으로 프록시 생성
+        Object proxyUserService = Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{UserService.class},
+                // TransactionHandler를 이용해 트랜잭션 적용
+                new TransactionHandler(appUserService, platformTransactionManager)
+        );
+        final UserService userService = (UserService) proxyUserService;
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
+
         userService.changePassword(1L, newPassword, createBy);
 
         final var actual = userService.findById(1L);
-
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
 
     @Test
     void testTransactionRollback() {
         final var appUserService = new AppUserService(userDao, stubUserHistoryDao);
-        final UserService userService = null;
-
+        Object proxyUserService = Proxy.newProxyInstance(
+                getClass().getClassLoader(),
+                new Class[]{UserService.class},
+                new TransactionHandler(appUserService, platformTransactionManager)
+        );
+        final UserService userService = (UserService) proxyUserService;
         final var newPassword = "newPassword";
         final var createBy = "gugu";
+
         assertThrows(DataAccessException.class,
                 () -> userService.changePassword(1L, newPassword, createBy));
 
         final var actual = userService.findById(1L);
-
         assertThat(actual.getPassword()).isNotEqualTo(newPassword);
     }
 }
