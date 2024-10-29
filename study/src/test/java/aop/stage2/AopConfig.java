dde05da83f6674e9776965795780c8a026d19cbe
seunghye218 <seunghye@student.42seoul.kr>
2024-10-29 16:18:33 +0900

    test: 스프링 AOP에 추가하기

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,27 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AopConfig {
 
+    @Bean
+    public TransactionAdvisor transactionAdvisor(PlatformTransactionManager transactionManager) {
+        TransactionPointcut pointCut = new TransactionPointcut();
+        TransactionAdvice advice = new TransactionAdvice(transactionManager);
+        return new TransactionAdvisor(pointCut, advice);
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
+        proxyCreator.setProxyTargetClass(true);
+        return proxyCreator;
+    }
 }
