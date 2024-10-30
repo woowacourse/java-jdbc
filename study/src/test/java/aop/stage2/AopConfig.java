8c443f841e09c66c3338dbdda86cb6f3137ff706
SungKyum Kim <sungkyum1@naver.com>
2024-10-30 13:12:56 +0900

    test: aop study stage2

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,26 @@
 package aop.stage2;
 
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import aop.stage1.TransactionAdvisor;
 
 @Configuration
+@EnableAspectJAutoProxy
 public class AopConfig {
 
+    @Bean
+    public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
+        DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
+        proxyCreator.setProxyTargetClass(true);  // CGLIB 기반 프록시 생성
+        return proxyCreator;
+    }
+
+    @Bean
+    public TransactionAdvisor transactionAdvisor(PlatformTransactionManager transactionManager) {
+        return new TransactionAdvisor(transactionManager);
+    }
 }
