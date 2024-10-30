4c64423f4bfc37d013fc194e882556b3f130bce1
SungKyum Kim <sungkyum1@naver.com>
2024-10-30 13:06:21 +0900

    test: aop study stage1

--- study/src/test/java/aop/stage1/TransactionAdvisor.java
+++ study/src/test/java/aop/stage1/TransactionAdvisor.java
@@ -3,6 +3,7 @@ package aop.stage1;
 import org.aopalliance.aop.Advice;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.PointcutAdvisor;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  * 어드바이저(advisor). 포인트컷과 어드바이스를 하나씩 갖고 있는 객체.
@@ -10,18 +11,24 @@ import org.springframework.aop.PointcutAdvisor;
  */
 public class TransactionAdvisor implements PointcutAdvisor {
 
+    private final PlatformTransactionManager platformTransactionManager;
+
+    public TransactionAdvisor(PlatformTransactionManager platformTransactionManager) {
+        this.platformTransactionManager = platformTransactionManager;
+    }
+
     @Override
     public Pointcut getPointcut() {
-        return null;
+        return new TransactionPointcut();
     }
 
     @Override
     public Advice getAdvice() {
-        return null;
+        return new TransactionAdvice(platformTransactionManager);
     }
 
     @Override
     public boolean isPerInstance() {
-        return false;
+        return true;
     }
 }
