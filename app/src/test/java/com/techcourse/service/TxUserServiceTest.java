f19d045de6a1a90f2835714027738b92d0e38d92
ehtjsv2 <ehtjsv2@naver.com>
2024-10-30 12:55:33 +0900

    test: changePasswrod tx테스트 exception 검증 제거

--- app/src/test/java/com/techcourse/service/TxUserServiceTest.java
+++ app/src/test/java/com/techcourse/service/TxUserServiceTest.java
@@ -48,17 +48,17 @@ class TxUserServiceTest {
     @Test
     void testTransactionRollback() {
         // 트랜잭션 롤백 테스트를 위해 mock으로 교체
-        final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
+        final var exceptionUserHistoryDao = new MockUserHistoryDao(jdbcTemplate);
         // 애플리케이션 서비스
-        final var appUserService = new AppUserService(userDao, userHistoryDao);
+        final var appUserService = new AppUserService(userDao, exceptionUserHistoryDao);
         // 트랜잭션 서비스 추상화
         final var userService = new TxUserService(appUserService, transactionManager);
 
         final var newPassword = "newPassword";
         final var createdBy = "gugu";
+
         // 트랜잭션이 정상 동작하는지 확인하기 위해 의도적으로 MockUserHistoryDao에서 예외를 발생시킨다.
-        assertThrows(DataAccessException.class,
-                () -> userService.changePassword(1L, newPassword, createdBy));
+        userService.changePassword(1L, newPassword, createdBy);
 
         final var actual = userService.findById(1L);
 
