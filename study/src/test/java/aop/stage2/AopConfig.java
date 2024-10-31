61f4b3236b0a7132e2f161a3d32a80d12e640822
pricelees <priceelees@gmail.com>
2024-10-31 16:41:00 +0900

    test: 학습 테스트 stage2

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,9 +1,15 @@
 package aop.stage2;
 
+import aop.repository.UserDao;
+import aop.repository.UserHistoryDao;
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
 import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -13,7 +19,27 @@ public class AopConfig {
     public DefaultAdvisorAutoProxyCreator advisorAutoProxyCreator() {
         DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
         proxyCreator.setProxyTargetClass(true);
+
         return proxyCreator;
     }
 
+    @Bean
+    public TransactionAdvisor transactionAdvisor(
+            TransactionPointcut transactionPointcut,
+            TransactionAdvice transactionAdvice
+    ) {
+        return new TransactionAdvisor(transactionPointcut, transactionAdvice);
+    }
+
+    @Bean
+    public TransactionPointcut transactionPointcut() {
+        return new TransactionPointcut();
+    }
+
+    @Bean
+    public TransactionAdvice transactionAdvice(
+            PlatformTransactionManager platformTransactionManager
+    ) {
+        return new TransactionAdvice(platformTransactionManager);
+    }
 }
