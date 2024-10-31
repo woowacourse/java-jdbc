80224bdd9f1c07d07789a7d9208813c04e6f38c0
pricelees <priceelees@gmail.com>
2024-10-31 14:03:18 +0900

    refactor: bindResource, unbindResource를 DataSourceUtils로 병합

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -1,16 +1,14 @@
 package com.techcourse.service;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.concurrent.Callable;
-
-import javax.sql.DataSource;
-
 import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.datasource.DataSourceUtils;
 import com.interface21.transaction.support.TransactionSynchronizationManager;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.concurrent.Callable;
+import javax.sql.DataSource;
 
 public class TxUserService implements UserService {
 
@@ -47,14 +45,13 @@ public class TxUserService implements UserService {
 		} catch (Exception e) {
 			rollbackWithException(connection, e);
 		} finally {
-			release(connection, dataSource);
+			DataSourceUtils.releaseConnection(connection, dataSource);
 		}
 	}
 
 	private <T> T executeInReadOnly(final Callable<T> callable) {
 		DataSource dataSource = DataSourceConfig.getInstance();
 		Connection connection = DataSourceUtils.getConnection(dataSource);
-		TransactionSynchronizationManager.bindResource(dataSource, connection);
 
 		try {
 			connection.setAutoCommit(false);
@@ -66,10 +63,9 @@ public class TxUserService implements UserService {
 		} catch (Exception e) {
 			rollbackWithException(connection, e);
 		} finally {
-			release(connection, dataSource);
+			DataSourceUtils.releaseConnection(connection, dataSource);
 		}
 
-		// unreachable line
 		return null;
 	}
 
@@ -80,10 +76,5 @@ public class TxUserService implements UserService {
 			throw new DataAccessException(rollbackException);
 		}
 		throw new DataAccessException(e);
-	}
-
-	private void release(Connection conn, DataSource dataSource) {
-		DataSourceUtils.releaseConnection(conn, dataSource);
-		TransactionSynchronizationManager.unbindResource(dataSource);
 	}
 }
