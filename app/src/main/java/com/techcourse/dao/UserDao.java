b9e336f9f09a14476f17489b0d91b3902d0521c0
Dompoo <dlckdrms0517@gmail.com>
2025-09-24 16:24:20 +0900

    refactor: 순서에 의존하지 않도록 수정

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -8,10 +8,10 @@ import java.util.List;
 public class UserDao {
 
     private static final ResultExtractor<User> EXTRACTOR = rs -> new User(
-            rs.getLong(1),
-            rs.getString(2),
-            rs.getString(3),
-            rs.getString(4)
+            rs.getLong("id"),
+            rs.getString("account"),
+            rs.getString("password"),
+            rs.getString("email")
     );
 
     private final JdbcTemplate jdbcTemplate;
