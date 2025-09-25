0d1ecae703869ad5f6b78587de9f1fa4e1c8bc79
Minuring <mw9505@gmail.com>
2025-09-26 00:37:46 +0900

    jdbctemplate 생성자에서 DB 연결 테스트

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,9 @@
 package com.interface21.jdbc.core;
 
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +13,21 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+        testConnection(dataSource);
+    }
+
+    private void testConnection(DataSource dataSource) {
+        try (var connection = dataSource.getConnection()) {
+            var databaseProductName = connection.getMetaData().getDatabaseProductName();
+            log.info("Connection established to database : {}", databaseProductName);
+
+        } catch (NullPointerException e) {
+            log.error("Connection is null on dataSource {}", dataSource);
+            throw new RuntimeException(e);
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e.getCause());
+            throw new RuntimeException(e);
+        }
     }
 }
