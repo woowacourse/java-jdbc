b106c108ff36696c1470c69d50e224ffc0b1b884
Dompoo <dlckdrms0517@gmail.com>
2025-09-26 09:51:53 +0900

    fix: 스네이크 케이스를 카멜 케이스로 변환하여 조회

--- jdbc/src/main/java/com/interface21/jdbc/dsl/ResultExtractorFactory.java
+++ jdbc/src/main/java/com/interface21/jdbc/dsl/ResultExtractorFactory.java
@@ -4,6 +4,7 @@ import com.interface21.jdbc.core.ResultExtractor;
 import java.lang.reflect.Field;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.Locale;
 
 public class ResultExtractorFactory {
 
@@ -15,11 +16,11 @@ public class ResultExtractorFactory {
                 int columnCount = metaData.getColumnCount();
 
                 for (int i = 1; i <= columnCount; i++) {
-                    String columnName = metaData.getColumnLabel(i);
+                    String columnName = metaData.getColumnName(i);
                     Object value = resultSet.getObject(i);
 
                     try {
-                        Field field = targetClass.getDeclaredField(columnName);
+                        Field field = targetClass.getDeclaredField(snakeToCamel(columnName));
                         field.setAccessible(true);
                         field.set(instance, value);
                     } catch (NoSuchFieldException ignore) {
@@ -30,5 +31,23 @@ public class ResultExtractorFactory {
                 throw new SQLException("Failed to map ResultSet to " + targetClass.getName(), e);
             }
         };
+    }
+
+    private static String snakeToCamel(String name) {
+        String lower = name.toLowerCase(Locale.ROOT);
+        if (lower.indexOf('_') < 0) return lower;
+        StringBuilder sb = new StringBuilder();
+        boolean up = false;
+        for (char c : lower.toCharArray()) {
+            if (c == '_') {
+                up = true;
+            } else if (up) {
+                sb.append(Character.toUpperCase(c));
+                up = false;
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
