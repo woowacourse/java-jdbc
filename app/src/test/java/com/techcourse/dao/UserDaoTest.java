b5b86c60348556eccd90f31d2637eef6df5a1aba
Minuring <mw9505@gmail.com>
2025-09-26 13:53:20 +0900

    불필요한 의존성 제거

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,18 +1,16 @@
 package com.techcourse.dao;
 
-import com.interface21.jdbc.core.JdbcTemplate;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 class UserDaoTest {
 
     private UserDao userDao;
-    private final JdbcTemplate jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
 
     @BeforeEach
     void setup() {
