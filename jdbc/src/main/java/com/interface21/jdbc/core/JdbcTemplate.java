f81effef318a07376c46fee38cfe498ec07ba1ff
kimdev <kim__dev@hufs.ac.kr>
2025-09-27 18:09:11 +0900

    feat: JdbcTemplate에 update, query, queryForObject 메서드 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.rowMapper.RowMapper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,59 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    /**
+     * INSERT, UPDATE, DELETE 전용 메서드
+     * @return 영향 받은 행(row) 수
+     */
+    public int update(String sql, Object... params) {
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            for (int i=0; i<params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+            return pstmt.executeUpdate();
+        } catch (SQLException exception) {
+            log.error(exception.getMessage(), exception);
+            throw new RuntimeException(exception);
+        }
+    }
+
+    /**
+     * SELECT 전용 메서드
+     * 여러 행을 반환
+     */
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            for (int i=0; i<params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+            try (ResultSet rs = pstmt.executeQuery()) {
+                List<T> results = new ArrayList<>();
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs));
+                }
+                return results;
+            }
+        } catch (SQLException exception) {
+            log.error(exception.getMessage(), exception);
+            throw new RuntimeException(exception);
+        }
+    }
+
+    /**
+     * SELECT 단건 조회 전용 메서드
+     * 결과가 없을 시 null을 반환
+     */
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
+        List<T> queryResult = query(sql, rowMapper, params);
+        if (queryResult.isEmpty()) {
+            return null;
+        }
+        return queryResult.getFirst();
     }
 }
