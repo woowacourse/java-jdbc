26fd52ab28071a988e462b67ca854c7e98bdaf58
sung-june27 <kmymy27@gmail.com>
2025-09-27 17:21:43 +0900

    feat: UserDao.insert 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,6 +2,7 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import com.interface21.jdbc.core.JdbcTemplate;
+import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -65,7 +66,43 @@ public class UserDao {
 
     public List<User> findAll() {
         // todo
-        return null;
+        final var sql = "select account, password, email from users";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+            final ResultSet resultSet = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            final ArrayList<User> users = new ArrayList<>();
+
+            while (resultSet.next()) {
+                final String account = resultSet.getString("account");
+                final String password = resultSet.getString("password");
+                final String email = resultSet.getString("email");
+                final User user = new User(account, password, email);
+                users.add(user);
+            }
+            return users;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {}
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {}
+        }
     }
 
     public User findById(final Long id) {
