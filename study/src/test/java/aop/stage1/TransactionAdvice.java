ad21a36fe1ac5209fc1862099657919906a4a890
eueo8259 <eueo8259@gmail.com>
2025-09-27 22:53:26 +0900

    test: 퀘스트 테스트

--- study/src/test/java/aop/stage1/TransactionAdvice.java
+++ study/src/test/java/aop/stage1/TransactionAdvice.java
@@ -2,14 +2,32 @@ package aop.stage1;
 
 import org.aopalliance.intercept.MethodInterceptor;
 import org.aopalliance.intercept.MethodInvocation;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 /**
  * 어드바이스(advice). 부가기능을 담고 있는 클래스
  */
-public class TransactionAdvice  implements MethodInterceptor {
+public class TransactionAdvice implements MethodInterceptor {
+
+    private final PlatformTransactionManager transactionManager;
+
+    public TransactionAdvice(final PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
 
     @Override
     public Object invoke(final MethodInvocation invocation) throws Throwable {
-        return null;
+        final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+        Object invoke = null;
+        try {
+            invoke = invocation.proceed();
+            transactionManager.commit(transactionStatus);
+        } catch (Exception e) {
+            transactionManager.rollback(transactionStatus);
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        return invoke;
     }
 }
