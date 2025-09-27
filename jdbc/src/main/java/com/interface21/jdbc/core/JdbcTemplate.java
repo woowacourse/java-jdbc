4a16166fb7163e320cba1db50c6a42850e711606
kimdev <kim__dev@hufs.ac.kr>
2025-09-27 18:20:48 +0900

    refactor: JdbcTemplate 메서드 분리

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -23,15 +23,16 @@ public class JdbcTemplate {
 
     /**
      * INSERT, UPDATE, DELETE 전용 메서드
+     *
+     * @param sql 실행할 sql
+     * @param params sql에 바인딩할 파라미터
      * @return 영향 받은 행(row) 수
      */
     public int update(String sql, Object... params) {
         try (Connection conn = dataSource.getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             log.debug("query : {}", sql);
-            for (int i=0; i<params.length; i++) {
-                pstmt.setObject(i + 1, params[i]);
-            }
+            setParametersToPreparedStatement(params, pstmt);
             return pstmt.executeUpdate();
         } catch (SQLException exception) {
             log.error(exception.getMessage(), exception);
@@ -39,23 +40,27 @@ public class JdbcTemplate {
         }
     }
 
+    private void setParametersToPreparedStatement(Object[] params, PreparedStatement pstmt) throws SQLException {
+        for (int i=0; i< params.length; i++) {
+            pstmt.setObject(i + 1, params[i]);
+        }
+    }
+
     /**
      * SELECT 전용 메서드
      * 여러 행을 반환
+     *
+     * @param sql 실행할 sql
+     * @param rowMapper sql 결과를 엔티티로 매핑해주는 함수형 인터페이스
+     * @param params sql에 바인딩할 파라미터
      */
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
         try (Connection conn = dataSource.getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql)) {
             log.debug("query : {}", sql);
-            for (int i=0; i<params.length; i++) {
-                pstmt.setObject(i + 1, params[i]);
-            }
+            setParametersToPreparedStatement(params, pstmt);
             try (ResultSet rs = pstmt.executeQuery()) {
-                List<T> results = new ArrayList<>();
-                while (rs.next()) {
-                    results.add(rowMapper.mapRow(rs));
-                }
-                return results;
+                return mapResultSet(rowMapper, rs);
             }
         } catch (SQLException exception) {
             log.error(exception.getMessage(), exception);
@@ -63,9 +68,21 @@ public class JdbcTemplate {
         }
     }
 
+    private <T> List<T> mapResultSet(RowMapper<T> rowMapper, ResultSet rs) throws SQLException {
+        List<T> results = new ArrayList<>();
+        while (rs.next()) {
+            results.add(rowMapper.mapRow(rs));
+        }
+        return results;
+    }
+
     /**
      * SELECT 단건 조회 전용 메서드
      * 결과가 없을 시 null을 반환
+     *
+     * @param sql 실행할 sql
+     * @param rowMapper sql 결과를 엔티티로 매핑해주는 함수형 인터페이스
+     * @param params sql에 바인딩할 파라미터
      */
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
         List<T> queryResult = query(sql, rowMapper, params);
