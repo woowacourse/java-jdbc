2ef9bc8aaf40046d59b05b4b5b45644d969d9d9d
Jin409 <sualng123@gmail.com>
2025-09-27 16:54:39 +0900

    feat: JdbcTemplate 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.lang.reflect.Constructor;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
@@ -13,5 +17,78 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);) {
+
+            log.debug("query : {}", sql);
+
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i + 1, args[i]);
+            }
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T getSingleResult(RowMapper<T> rowMapper, String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("args = " + args[i]);
+                pstmt.setObject(i + 1, args[i]);
+            }
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                log.info("query : {}", sql);
+
+                if (rs.next()) {
+                    return rowMapper.mapRow(rs);
+                }
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+
+        return null;
+    }
+
+    public <T> List<T> getResults(RowMapper<T> rowMapper, String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
+            if (args != null && args.length > 0) {
+                for (int i = 0; i < args.length; i++) {
+                    pstmt.setObject(i + 1, args[i]);
+                }
+            }
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                log.info("query : {}", sql);
+
+                List<T> result = new ArrayList<>();
+
+                while (rs.next()) {
+                    result.add(rowMapper.mapRow(rs));
+                }
+
+                return result;
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
     }
 }
