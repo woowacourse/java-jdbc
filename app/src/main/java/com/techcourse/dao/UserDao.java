feb9fa1fde0973597f30c0d3c0fbcecca52767f3
eueo8259 <eueo8259@gmail.com>
2025-09-27 23:24:34 +0900

    feat: dao와 jdbcTemplate 책임 분리

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,206 +2,57 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import com.interface21.jdbc.core.JdbcTemplate;
-import java.util.ArrayList;
+import java.util.Map;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
-    private final DataSource dataSource;
-
-    public UserDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+    private final JdbcTemplate jdbcTemplate;
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = jdbcTemplate.getDataSource();
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-            pstmt.setString(1, user.getAccount());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getEmail());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.queryForUpdate(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
-
     public List<User> findAll() {
-        // todo
-        final var sql = "select id, account, password, email from users";
-        final List<User> users = new ArrayList<>();
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-            try (ResultSet rs = pstmt.executeQuery()) {
-                while (rs.next()) {
-                    users.add(new User(
-                            rs.getLong(1),
-                            rs.getString(2),
-                            rs.getString(3),
-                            rs.getString(4)));
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return users;
+        List<Map<String, Object>> rows = jdbcTemplate.queryForResultList("SELECT * FROM users");
+        return rows.stream()
+                .map(this::mapToUser)
+                .toList();
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-            pstmt.setLong(1, id);
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return new User(
-                            rs.getLong(1),
-                            rs.getString(2),
-                            rs.getString(3),
-                            rs.getString(4));
-                }
-                return null;
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        Optional<Map<String, Object>> stringObjectMap = jdbcTemplate.queryForResult(sql, id);
+        return stringObjectMap.map(this::mapToUser).orElse(null);
     }
 
     public void update(final User user) {
         final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-            pstmt.setString(1, user.getAccount());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getEmail());
-            pstmt.setLong(4, user.getId());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        jdbcTemplate.queryForUpdate(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public User findByAccount(final String account) {
-        // todo
         final var sql = "select id, account, password, email from users where account = ?";
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-            pstmt.setString(1, account);
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return new User(
-                            rs.getLong(1),
-                            rs.getString(2),
-                            rs.getString(3),
-                            rs.getString(4));
-                }
-                return null;
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
+        Optional<Map<String, Object>> stringObjectMap = jdbcTemplate.queryForResult(sql, account);
+        return stringObjectMap.map(this::mapToUser).orElse(null);
     }
 
-    //    public void insert(final User user) {
-//        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-//
-//        Connection conn = null;
-//        PreparedStatement pstmt = null;
-//        try {
-//            conn = dataSource.getConnection();
-//            pstmt = conn.prepareStatement(sql);
-//
-//            log.debug("query : {}", sql);
-//
-//            pstmt.setString(1, user.getAccount());
-//            pstmt.setString(2, user.getPassword());
-//            pstmt.setString(3, user.getEmail());
-//            pstmt.executeUpdate();
-//        } catch (SQLException e) {
-//            log.error(e.getMessage(), e);
-//            throw new RuntimeException(e);
-//        } finally {
-//            try {
-//                if (pstmt != null) {
-//                    pstmt.close();
-//                }
-//            } catch (SQLException ignored) {}
-//
-//            try {
-//                if (conn != null) {
-//                    conn.close();
-//                }
-//            } catch (SQLException ignored) {}
-//        }
-//    }
-//    public User findById(final Long id) {
-//        final var sql = "select id, account, password, email from users where id = ?";
-//
-//        Connection conn = null;
-//        PreparedStatement pstmt = null;
-//        ResultSet rs = null;
-//        try {
-//            conn = dataSource.getConnection();
-//            pstmt = conn.prepareStatement(sql);
-//            pstmt.setLong(1, id);
-//            rs = pstmt.executeQuery();
-//
-//            log.debug("query : {}", sql);
-//
-//            if (rs.next()) {
-//                return new User(
-//                        rs.getLong(1),
-//                        rs.getString(2),
-//                        rs.getString(3),
-//                        rs.getString(4));
-//            }
-//            return null;
-//        } catch (SQLException e) {
-//            log.error(e.getMessage(), e);
-//            throw new RuntimeException(e);
-//        } finally {
-//            try {
-//                if (rs != null) {
-//                    rs.close();
-//                }
-//            } catch (SQLException ignored) {}
-//
-//            try {
-//                if (pstmt != null) {
-//                    pstmt.close();
-//                }
-//            } catch (SQLException ignored) {}
-//
-//            try {
-//                if (conn != null) {
-//                    conn.close();
-//                }
-//            } catch (SQLException ignored) {}
-//        }
-//    }
+    private User mapToUser(Map<String, Object> row) {
+        return new User(
+                (Long) row.get("ID"),
+                (String) row.get("ACCOUNT"),
+                (String) row.get("PASSWORD"),
+                (String) row.get("EMAIL")
+        );
+    }
 }
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,7 +23,77 @@ public class JdbcTemplate {
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
     public DataSource getDataSource(){
         return this.dataSource;
+    }
+
+    public List<Map<String, Object>> queryForResultList(String sql) {
+        final List<Map<String, Object>> results = new ArrayList<>();
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                final var metaData = rs.getMetaData();
+                final int columnCount = metaData.getColumnCount();
+
+                while (rs.next()) {
+                    Map<String, Object> row = new HashMap<>();
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnName(i);
+                        Object value = rs.getObject(i);
+                        row.put(columnName, value);
+                    }
+                    results.add(row);
+                }
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+        return results;
+    }
+
+    public Optional<Map<String, Object>> queryForResult(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParameter(args, pstmt);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    final var metaData = rs.getMetaData();
+                    final int columnCount = metaData.getColumnCount();
+                    Map<String, Object> row = new HashMap<>();
+                    for (int i = 1; i <= columnCount; i++) {
+                        String columnName = metaData.getColumnName(i);
+                        Object value = rs.getObject(i);
+                        row.put(columnName, value);
+                    }
+                    return Optional.of(row);
+                }
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+        return Optional.empty();
+    }
+
+    public void queryForUpdate(final String sql, final Object...args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParameter(args, pstmt);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParameter(Object[] args, PreparedStatement pstmt) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i+1, args[i]);
+        }
     }
 }
