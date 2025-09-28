134e5eea5467639ab07ae13be51a16be20a170b8
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-09-28 12:37:21 +0900

    [1단계 - JDBC 라이브러리 구현하기] 에드(김진우) 미션 제출합니다. (#940)
    * fix: 테스트 런타임 의존성 추가
    
    * feat: 모든 사용자 조회하는 findAll 메서드 구현
    
    * feat: 계정으로 사용자 조회하는 findByAccount 메서드 구현
    
    * feat: 사용자 정보 수정하는 update 메서드 구현
    
    * refactor: UserDao 리소스 관리 코드 try-with-resources로 리팩토링
    
    * feat: 데이터 수정 쿼리 실행 가능한 update 메서드 추가
    
    * refactor: insert, update에 대해JdbcTemplate 적용
    
    * refactor: select 쿼리에 JdbcTemplate 적용
    
    * refactor: StatementExecutor 인터페이스 도입 및 jdbcTemplate의 공통 로직 메서드 분리
    
    * refactor: RuntimeException 대신 DataAccessException 사용하도록 수정
    
    * feat: 결과 크기 검증 로직 추가 및 IncorrectResultSizeDataAccessException 클래스 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,81 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import com.interface21.dao.IncorrectResultSizeDataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
     private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
-
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    private <T> T execute(String sql, StatementExecutor<T> executor, Object... params) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            setParameters(pstmt, params);
+            log.debug("query : {}", sql);
+
+            return executor.execute(pstmt);
+
+        } catch (SQLException e) {
+            log.error("SQL execution failed. Query: {}", sql, e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    // INSERT, UPDATE, DELETE
+    //TODO: 별도의 콜백 인터페이스 구현하면 Objcet...처럼 순서에 의존하지 않아도 된다고 함. 다음 스텝에..  (2025-09-27, 토, 17:38)
+    public int update(String sql, Object... params) {
+        return execute(sql, PreparedStatement::executeUpdate, params);
+    }
+
+    // SELECT
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
+        return execute(sql, pstmt -> {
+            try (ResultSet rs = pstmt.executeQuery()) {
+                List<T> results = new ArrayList<>();
+                addResultSetToResults(rowMapper, rs, results);
+                return results;
+            }
+        }, params);
+    }
+
+    // SELECT 단일
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
+        List<T> results = query(sql, rowMapper, params);
+        validateResultsSize(results, 1);
+        return results.getFirst();
+    }
+
+    private <T> void validateResultsSize(List<T> results, int expectedSize) {
+        int actualSize = results.size();
+        if (actualSize != expectedSize) {
+            throw new IncorrectResultSizeDataAccessException(expectedSize, actualSize);
+        }
+    }
+
+    private void setParameters(PreparedStatement pstmt, Object... params) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            pstmt.setObject(i + 1, params[i]);
+        }
+    }
+
+    private <T> void addResultSetToResults(RowMapper<T> rowMapper, ResultSet rs, List<T> results) throws SQLException {
+        while (rs.next()) {
+            results.add(rowMapper.mapRow(rs));
+        }
     }
 }
