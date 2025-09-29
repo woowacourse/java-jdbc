38260ec3614fd929414d5d8d374bf7a4213aafd5
2jin2.1031 <2jin2.1031@gmail.com>
2025-09-30 00:50:43 +0900

    feat: insert 기능을 jdbcTemplate로 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -24,6 +24,22 @@ public class JdbcTemplate {
         return dataSource;
     }
 
+    // TODO. insert의 반환값은? void vs T
+    public void insertObject(String sql, Object o1, Object o2, Object o3) { // TODO. 현재는 3개의 인자에 대해서만, 나중에 args
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+
+            pstmt.setObject(1, o1);
+            pstmt.setObject(2, o2);
+            pstmt.setObject(3, o3);
+
+            pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     // TODO. 현재는 반환값을 User에 대해서만, 나중에 전역적인 Object
     public <T> T findByObject(String sql, RowMapper<T> rowMapper, Object object) { // TODO. 현재는 하나의 인자에 대해서만, 나중에 args
         ResultSet rs;
