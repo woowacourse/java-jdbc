1164a9f2b9863e3894a00b888950ab6ecc609272
시원 <cnc4750@inha.edu>
2025-09-29 22:12:06 +0900

    refactor: try-with-resources을 통해 자원 반환하도록 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -20,119 +20,64 @@ public class JdbcTemplate {
     }
 
     public int executeUpdate(final String sql, final Object... parameters) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            for (int i = 0; i < parameters.length; i++) {
-                final Object parameter = parameters[i];
-                if (parameter instanceof String) {
-                    pstmt.setString(i + 1, (String) parameter);
-                }
-                if (parameter instanceof Long) {
-                    pstmt.setLong(i + 1, (Long) parameter);
-                }
-            }
-
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatementParameters(pstmt, parameters);
             return pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
+            throw new RuntimeException("리소스 해제에 실패했습니다.");
         }
     }
 
     public <T> T executeQueryForObject(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            for (int i = 0; i < parameters.length; i++) {
-                final Object parameter = parameters[i];
-                if (parameter instanceof String) {
-                    pstmt.setString(i + 1, (String) parameter);
-                }
-                if (parameter instanceof Long) {
-                    pstmt.setLong(i + 1, (Long) parameter);
-                }
-            }
-
-            final ResultSet rs = pstmt.executeQuery();
-
+        ResultSet rs = null;
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatementParameters(pstmt, parameters);
+            rs = pstmt.executeQuery();
             return rowMapper.mapForObject(rs);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new RuntimeException("리소스 해제에 실패했습니다.");
         } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
+            closeResultSet(rs);
         }
     }
 
     public <T> List<T> executeQuery(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            for (int i = 0; i < parameters.length; i++) {
-                final Object parameter = parameters[i];
-                if (parameter instanceof String) {
-                    pstmt.setString(i + 1, (String) parameter);
-                }
-                if (parameter instanceof Long) {
-                    pstmt.setLong(i + 1, (Long) parameter);
-                }
-            }
-
-            final ResultSet rs = pstmt.executeQuery();
+        ResultSet rs = null;
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatementParameters(pstmt, parameters);
+            rs = pstmt.executeQuery();
             return rowMapper.mapForObjects(rs);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new RuntimeException("리소스 해제에 실패했습니다.");
         } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
+            closeResultSet(rs);
+        }
+    }
 
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
+    private void setStatementParameters(final PreparedStatement pstmt, final Object[] parameters) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            final Object parameter = parameters[i];
+            if (parameter instanceof String) {
+                pstmt.setString(i + 1, (String) parameter);
             }
+            if (parameter instanceof Long) {
+                pstmt.setLong(i + 1, (Long) parameter);
+            }
+        }
+    }
+
+    private void closeResultSet(final ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException ignored) {
         }
     }
 }
