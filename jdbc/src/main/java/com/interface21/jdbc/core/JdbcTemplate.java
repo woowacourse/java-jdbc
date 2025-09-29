93e7e247e4b4b67e7182f5db731c5b825610815c
DongchannN <96824025+DongchannN@users.noreply.github.com>
2025-09-29 13:28:20 +0900

    [1단계 - JDBC 라이브러리 구현하기] 차니(이동찬) 미션 제출합니다. (#945)
    * chore: Poling 오타 수정 (#928)
    
    * chore: 의존성 추가
    
    * feat: 데이터를 읽는 메서드와, 변경을 일으키는 메서드 구현
    
    * refactor: 기존 코드 jdbcTemplate 사용하도록 변경
    
    * feat: UserDao 클래스 구현
    
    * refactor: 사용하지 않는 Import 문 및 필드 제거
    
    * refactor: 사용자 정보를 추출하는 RowMapper 필드로 추출
    
    * refactor: try-with-resources 구문 사용
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,52 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> T doQueryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
+        final var results = doQuery(sql, rowMapper, args);
+        return results.getFirst();
+    }
+
+    public <T> List<T> doQuery(String sql, RowMapper<T> rowMapper, Object... args) {
+        try (var conn = dataSource.getConnection();
+             var pstmt = conn.prepareStatement(sql)) {
+
+            final int parameterCount = pstmt.getParameterMetaData().getParameterCount();
+            for (int i = 0; i < parameterCount; i++) {
+                pstmt.setObject(i + 1, args[i]);
+            }
+            try (var rs = pstmt.executeQuery()) {
+                List<T> results = new ArrayList<>();
+                int rowNum = 0;
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs, rowNum++));
+                }
+                log.debug("query : {}", sql);
+                return results;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int doUpdate(String sql, Object... args) {
+        try (var conn = dataSource.getConnection();
+             var pstmt = conn.prepareStatement(sql)) {
+
+            final int parameterCount = pstmt.getParameterMetaData().getParameterCount();
+            for (int i = 0; i < parameterCount; i++) {
+                pstmt.setObject(i + 1, args[i]);
+            }
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public interface RowMapper<T> {
+        T mapRow(ResultSet rs, int rowNum) throws SQLException;
     }
 }
