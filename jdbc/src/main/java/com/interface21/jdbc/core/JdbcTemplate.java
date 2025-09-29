f22921109c3b53a6c80979b96d9f17980d26a3f9
DongchannN <ldc991104@gmail.com>
2025-09-29 13:12:55 +0900

    refactor: try-with-resources 구문 사용

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -26,59 +26,31 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> doQuery(String sql, RowMapper<T> rowMapper, Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
+        try (var conn = dataSource.getConnection();
+             var pstmt = conn.prepareStatement(sql)) {
+
             final int parameterCount = pstmt.getParameterMetaData().getParameterCount();
             for (int i = 0; i < parameterCount; i++) {
                 pstmt.setObject(i + 1, args[i]);
             }
-            log.info("pstmt : {}", pstmt);
-            rs = pstmt.executeQuery();
-            List<T> results = new ArrayList<>();
-            int rowNum = 0;
-            while (rs.next()) {
-                results.add(rowMapper.mapRow(rs, rowNum++));
+            try (var rs = pstmt.executeQuery()) {
+                List<T> results = new ArrayList<>();
+                int rowNum = 0;
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs, rowNum++));
+                }
+                log.debug("query : {}", sql);
+                return results;
             }
-            log.debug("query : {}", sql);
-
-            return results;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
     public int doUpdate(String sql, Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
+        try (var conn = dataSource.getConnection();
+             var pstmt = conn.prepareStatement(sql)) {
 
             final int parameterCount = pstmt.getParameterMetaData().getParameterCount();
             for (int i = 0; i < parameterCount; i++) {
@@ -88,20 +60,6 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
