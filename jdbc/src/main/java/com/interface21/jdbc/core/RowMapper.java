ce388d1bc47fad4e4713cc309920c4d29f9246f5
sonjh919 <kingjh1125@gmail.com>
2025-09-29 10:30:43 +0900

    refactor: delete unused annotation

--- jdbc/src/main/java/com/interface21/jdbc/core/RowMapper.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/RowMapper.java
@@ -2,11 +2,9 @@ package com.interface21.jdbc.core;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import javax.annotation.Nullable;
 
 @FunctionalInterface
 public interface RowMapper<T> {
 
-    @Nullable
     T mapRow(final ResultSet rs, final int rowNum) throws SQLException;
 }
