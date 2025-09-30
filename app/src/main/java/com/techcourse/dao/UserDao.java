00384a939fdcc16cc6d71a632b7dfbed091745a0
조재중 <126754298+m-a-king@users.noreply.github.com>
2025-09-30 23:15:53 +0900

    feat: userDao 기능 추가
    - 관련된 모든 테스트 성공 확인.

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,121 +1,159 @@
 package com.techcourse.dao;
 
-import com.techcourse.domain.User;
 import com.interface21.jdbc.core.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
+import com.techcourse.domain.User;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@RequiredArgsConstructor
+@Slf4j
 public class UserDao {
 
-    private static final Logger log = LoggerFactory.getLogger(UserDao.class);
-
     private final DataSource dataSource;
-
-    public UserDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
 
     public UserDao(final JdbcTemplate jdbcTemplate) {
         this.dataSource = null;
     }
 
     public void insert(final User user) {
-        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        final var sql = """
+                insert into users 
+                (account, password, email)
+                values (?, ?, ?)
+                """;
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            log.debug("query : {}", sql);
-
-            pstmt.setString(1, user.getAccount());
-            pstmt.setString(2, user.getPassword());
-            pstmt.setString(3, user.getEmail());
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.getAccount());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
+            log.debug("query : {}", sql);
         }
     }
 
     public void update(final User user) {
-        // todo
+        final var sql = """
+                update users 
+                set account = ?,
+                    password = ?,
+                    email = ?
+                where id = ?
+                """;
+
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, user.getAccount());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setLong(4, user.getId());
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            log.debug("query : {}", sql);
+        }
     }
 
     public List<User> findAll() {
-        // todo
-        return null;
+        final var sql = """
+                select id, account, password, email 
+                from users
+                """;
+
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            final ResultSet rs = preparedStatement.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            final List<User> users = new ArrayList<>();
+            while (rs.next()) {
+                users.add(new User(
+                        rs.getLong(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4)));
+            }
+            return users;
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            log.debug("query : {}", sql);
+        }
     }
 
-    public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
+    public Optional<User> findById(final Long id) {
+        final var sql = """
+                select id, account, password, email 
+                from users
+                where id = ?
+                """;
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setLong(1, id);
+            final ResultSet rs = preparedStatement.executeQuery();
 
             log.debug("query : {}", sql);
 
             if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
+                return Optional.of(
+                        new User(
+                                rs.getLong(1),
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getString(4)));
             }
-            return null;
-        } catch (SQLException e) {
+            return Optional.empty();
+        } catch (final SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {}
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {}
+            log.debug("query : {}", sql);
         }
     }
 
-    public User findByAccount(final String account) {
-        // todo
-        return null;
+    public Optional<User> findByAccount(final String account) {
+        final var sql = """
+                select id, account, password, email 
+                from users
+                where account = ?
+                """;
+
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, account);
+            final ResultSet rs = preparedStatement.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            if (rs.next()) {
+                return Optional.of(
+                        new User(
+                                rs.getLong(1),
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getString(4)));
+            }
+            return Optional.empty();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            log.debug("query : {}", sql);
+        }
     }
 }
