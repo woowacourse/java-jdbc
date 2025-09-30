153e1ca3963751ed91046644964d76cbbda6660a
wogus <ind07152@naver.com>
2025-09-30 21:26:36 +0900

    refactor: 메서드 리팩토링

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,7 @@ import com.interface21.dao.DataAccessException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import javax.sql.DataSource;
@@ -22,13 +23,13 @@ public class JdbcTemplate {
 
     public void update(
             final String sql,
-            Object... args
+            final Object... args
     ) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            for (int i = 0; i < args.length; i++) {
-                pstmt.setObject(i + 1, args[i]);
-            }
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            setParameters(pstmt, args);
             pstmt.executeUpdate();
         } catch (Exception e) {
             log.error(e.getMessage(), e);
@@ -38,23 +39,19 @@ public class JdbcTemplate {
 
     public <T> T queryForObject(
             final String sql,
-            RowMapper<T> rowMapper,
-            Object... args
+            final RowMapper<T> rowMapper,
+            final Object... args
     ) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            for (int i = 0; i < args.length; i++) {
-                pstmt.setObject(i + 1, args[i]);
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            setParameters(pstmt, args);
+            final List<T> results = queryForList(rowMapper, pstmt);
+            if (results.isEmpty()) {
+                throw new DataAccessException("No Data");
             }
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return rowMapper.map(rs);
-                }
-                throw new DataAccessException("No data found");
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
-            }
+            return results.getFirst();
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -63,27 +60,43 @@ public class JdbcTemplate {
 
     public <T> List<T> query(
             final String sql,
-            RowMapper<T> rowMapper,
-            Object... args
+            final RowMapper<T> rowMapper,
+            final Object... args
     ) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-            for (int i = 0; i < args.length; i++) {
-                pstmt.setObject(i + 1, args[i]);
-            }
-            try (ResultSet resultSet = pstmt.executeQuery()) {
-                List<T> results = new ArrayList<>();
-                while (resultSet.next()) {
-                    results.add(rowMapper.map(resultSet));
-                }
-                return results;
-            } catch (Exception e) {
-                log.error(e.getMessage(), e);
-                throw new RuntimeException(e);
-            }
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            setParameters(pstmt, args);
+            return queryForList(rowMapper, pstmt);
         } catch (Exception e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
+        }
+    }
+
+    private <T> List<T> queryForList(
+            final RowMapper<T> rowMapper,
+            final PreparedStatement pstmt
+    ) {
+        try (ResultSet resultSet = pstmt.executeQuery()) {
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(rowMapper.map(resultSet));
+            }
+            return results;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParameters(
+            final PreparedStatement pstmt,
+            final Object[] args
+    ) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
         }
     }
 }
