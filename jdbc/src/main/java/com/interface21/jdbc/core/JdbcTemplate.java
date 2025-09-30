54b137240c398db89f4ae175e5f85667335aab8f
Hyunwook Kim <129609928+kimdevv@users.noreply.github.com>
2025-09-30 10:27:14 +0900

    [1단계 - JDBC 라이브러리 구현하기] 프리 제출합니다. (#946)
    * feat: JdbcTemplate에 update, query, queryForObject 메서드 구현
    
    * refactor: UserDao 리팩터링
    
    * refactor: JdbcTemplate 메서드 분리
    
    * refactor: 패키지 이름 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.rowmapper.RowMapper;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,76 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    /**
+     * INSERT, UPDATE, DELETE 전용 메서드
+     *
+     * @param sql 실행할 sql
+     * @param params sql에 바인딩할 파라미터
+     * @return 영향 받은 행(row) 수
+     */
+    public int update(String sql, Object... params) {
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParametersToPreparedStatement(params, pstmt);
+            return pstmt.executeUpdate();
+        } catch (SQLException exception) {
+            log.error(exception.getMessage(), exception);
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private void setParametersToPreparedStatement(Object[] params, PreparedStatement pstmt) throws SQLException {
+        for (int i=0; i< params.length; i++) {
+            pstmt.setObject(i + 1, params[i]);
+        }
+    }
+
+    /**
+     * SELECT 전용 메서드
+     * 여러 행을 반환
+     *
+     * @param sql 실행할 sql
+     * @param rowMapper sql 결과를 엔티티로 매핑해주는 함수형 인터페이스
+     * @param params sql에 바인딩할 파라미터
+     */
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParametersToPreparedStatement(params, pstmt);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return mapResultSet(rowMapper, rs);
+            }
+        } catch (SQLException exception) {
+            log.error(exception.getMessage(), exception);
+            throw new RuntimeException(exception);
+        }
+    }
+
+    private <T> List<T> mapResultSet(RowMapper<T> rowMapper, ResultSet rs) throws SQLException {
+        List<T> results = new ArrayList<>();
+        while (rs.next()) {
+            results.add(rowMapper.mapRow(rs));
+        }
+        return results;
+    }
+
+    /**
+     * SELECT 단건 조회 전용 메서드
+     * 결과가 없을 시 null을 반환
+     *
+     * @param sql 실행할 sql
+     * @param rowMapper sql 결과를 엔티티로 매핑해주는 함수형 인터페이스
+     * @param params sql에 바인딩할 파라미터
+     */
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
+        List<T> queryResult = query(sql, rowMapper, params);
+        if (queryResult.isEmpty()) {
+            return null;
+        }
+        return queryResult.getFirst();
     }
 }
