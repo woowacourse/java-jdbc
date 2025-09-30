5c76343c441a62915ce010aa813252bfb2eef487
시원 <cnc4750@inha.edu>
2025-09-30 14:47:37 +0900

    refactor: UserDao update 쿼리에 조건문 추가

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -18,8 +18,8 @@ public class UserDao {
     }
 
     public void update(final User user) {
-        final var sql = "update users set account = ?, password = ?, email = ?";
-        jdbcTemplate.executeUpdate(sql, user.getAccount(), user.getPassword(), user.getEmail());
+        final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        jdbcTemplate.executeUpdate(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
