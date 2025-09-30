0feabb2cffaaa6a2b3b8e6640c93e9f936ffe78c
RaZel713 <razel7130@gmail.com>
2025-10-01 01:56:29 +0900

    feat: 설명 주석 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/RowMapper.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/RowMapper.java
@@ -6,5 +6,9 @@ import java.sql.SQLException;
 @FunctionalInterface
 public interface RowMapper<T> {
 
+    /*
+     * 라이브러리 확장
+     * RowMapper 인터페이스는 제네릭을 사용해, 캐스팅을 사용하지 않도록 한다.
+     */
     T mapRow(ResultSet rs) throws SQLException;
 }
