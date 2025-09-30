d650c15127055e0a3e30ad6290fddf3f532b8a5b
eoehd1ek <eoehd2ek@gmail.com>
2025-10-01 02:33:33 +0900

    feat: UserDao findByAccount 메서드 기능 구현

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -46,8 +46,8 @@ public class UserDao {
     }
 
     public User findByAccount(final String account) {
-        // todo
-        return null;
+        final var sql = "select id, account, password, email from users where account = ?";
+        return jdbcTemplate.queryForObject(sql, getUserRowMapper(), account);
     }
 
     private RowMapper<User> getUserRowMapper() {
