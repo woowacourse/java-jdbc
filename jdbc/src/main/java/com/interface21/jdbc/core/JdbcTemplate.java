24b8b9aabbecfea4a3e41b7e3ff1bbc3d2de0971
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-10-01 16:19:19 +0900

    [1단계 - JDBC 라이브러리 구현하기] - 코기(장재현) 미션 제출합니다 (#966)
    * chore: Poling 오타 수정 (#928)
    
    * build: junit 빌드 변경
    
    * feat: UserDao sql 기능 추가
    
    - update
    - findAll
    - findByAccount
    
    * feat: 쿼리 실행 기능 추가
    
    * feat: userDao JdbcTemplate으로 변경
    
    * refactor: mapper 상수화
    
    * refactor: 메서드 리팩토링
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,84 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(
+            final String sql,
+            final Object... args
+    ) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            setParameters(pstmt, args);
+            pstmt.executeUpdate();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryForObject(
+            final String sql,
+            final RowMapper<T> rowMapper,
+            final Object... args
+    ) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            setParameters(pstmt, args);
+            final List<T> results = queryForList(rowMapper, pstmt);
+            if (results.isEmpty()) {
+                throw new DataAccessException("No Data");
+            }
+            return results.getFirst();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(
+            final String sql,
+            final RowMapper<T> rowMapper,
+            final Object... args
+    ) {
+        try (
+                final Connection conn = dataSource.getConnection();
+                final PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
+            setParameters(pstmt, args);
+            return queryForList(rowMapper, pstmt);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> List<T> queryForList(
+            final RowMapper<T> rowMapper,
+            final PreparedStatement pstmt
+    ) {
+        try (ResultSet resultSet = pstmt.executeQuery()) {
+            List<T> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(rowMapper.map(resultSet));
+            }
+            return results;
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParameters(
+            final PreparedStatement pstmt,
+            final Object[] args
+    ) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
     }
 }
