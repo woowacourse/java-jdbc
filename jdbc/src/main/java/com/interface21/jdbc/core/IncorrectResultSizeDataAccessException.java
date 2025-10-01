ede7b8ce12cca73558b455ad841df860cdc79380
threepebbles <threepebbles@naver.com>
2025-10-01 22:27:59 +0900

    refactor: try with resource 적용, 커스텀 Unchecked Exception 추가 및 적용

--- jdbc/src/main/java/com/interface21/jdbc/core/IncorrectResultSizeDataAccessException.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/IncorrectResultSizeDataAccessException.java
@@ -1,10 +1,6 @@
 package com.interface21.jdbc.core;
 
-/**
- * 조회 결과의 크기가 예상과 다를 때 발생하는 예외
- *
- * @see <a href="https://github.com/spring-projects/spring-framework/blob/main/spring-tx/src/main/java/org/springframework/dao/IncorrectResultSizeDataAccessException.java">Spring Framework IncorrectResultSizeDataAccessException</a>
- */
+// https://github.com/spring-projects/spring-framework/blob/main/spring-tx/src/main/java/org/springframework/dao/IncorrectResultSizeDataAccessException.java
 public class IncorrectResultSizeDataAccessException extends DataAccessException {
 
     public IncorrectResultSizeDataAccessException(String message) {
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -37,81 +37,65 @@ public class JdbcTemplate {
 
     // insert, update, delete
     public void update(final String sql, final Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
             log.debug("query : {}", sql);
 
             setParameters(pstmt, args);
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            closeResources(null, pstmt, conn);
+            throw new DataAccessException(e);
         }
     }
 
     // 단일 객체 조회
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
             log.debug("query : {}", sql);
 
             setParameters(pstmt, args);
-            rs = pstmt.executeQuery();
 
-            if (rs.next()) {
-                return rowMapper.mapRow(rs);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (!rs.next()) {
+                    throw new EmptyResultDataAccessException("Incorrect result size: expected 1, actual 0");
+                }
+                T result = rowMapper.mapRow(rs);
+                if (rs.next()) {
+                    throw new IncorrectResultSizeDataAccessException(1);
+                }
+                return result;
             }
-            return null;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            closeResources(rs, pstmt, conn);
+            throw new DataAccessException(e);
         }
     }
 
     // 객체 리스트 조회
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-
-        try {
-            conn = dataSource.getConnection();
-            // SQL 문을 미리 컴파일해서 PreparedStatement 객체 생성
-            pstmt = conn.prepareStatement(sql);
-
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)
+        ) {
             log.debug("query : {}", sql);
 
             setParameters(pstmt, args);
             // 쿼리 실행 결과(ResultSet) 반환
-            rs = pstmt.executeQuery();
-
-            List<T> results = new ArrayList<>();
-            // rs.next(): 다음 ResultSet이 있으면 true, 없으면 false 반환. 커서를 다음 행으로 이동.
-            while (rs.next()) {
-                T result = rowMapper.mapRow(rs);
-                results.add(result);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                List<T> results = new ArrayList<>();
+                // rs.next(): 다음 ResultSet이 있으면 true, 없으면 false 반환. 커서를 다음 행으로 이동.
+                while (rs.next()) {
+                    T result = rowMapper.mapRow(rs);
+                    results.add(result);
+                }
+                return results;
             }
-            return results;
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            closeResources(rs, pstmt, conn);
+            throw new DataAccessException(e);
         }
     }
 
@@ -120,35 +104,6 @@ public class JdbcTemplate {
             // SQL의 ?(placeholder)에 실제 값을 대입
             // SQL Injection 공격 방지
             pstmt.setObject(i + 1, args[i]);
-        }
-    }
-
-    // JDBC 리소스들 정리
-    // ResultSet, PreparedStatement, Connection을 역순으로 해제
-    // conn.close()만 성공해도 내부적으로 ResultSet, PreparedStatement도 자동 해제되지만, 역순 해제는 좋은 관습임. 생성 순서를 기억하고 있다는 의미.
-    private void closeResources(final ResultSet rs, final PreparedStatement pstmt, final Connection conn) {
-        try {
-            if (rs != null) {
-                // ResultSet 해제. 메모리 절약.
-                rs.close();
-            }
-        } catch (SQLException ignored) {
-        }
-
-        try {
-            if (pstmt != null) {
-                // PreparedStatement 해제. 메모리 절약.
-                pstmt.close();
-            }
-        } catch (SQLException ignored) {
-        }
-
-        try {
-            // Connection 반환을 위해 Connection 해제
-            if (conn != null) {
-                conn.close();
-            }
-        } catch (SQLException ignored) {
         }
     }
 }
