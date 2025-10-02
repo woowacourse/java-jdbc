938c60e99f726f79208aa1558838864a2c5aef64
posty <ykmxxi97@gmail.com>
2025-10-02 13:24:42 +0900

    [1단계 - JDBC 라이브러리 구현하기] - 포스티(윤강민) 미션 제출합니다. (#982)
    * chore: Poling 오타 수정 (#928)
    
    * DriverManager 오타 수정 (#960)
    
    * build: junit platform launcher 추가
    
    * feat: 매퍼 인터페이스 추가
    
    * refactor: JDBC 실행 JdbcTemplate로 위임
    
    * refactor: 데이터 접근 예외 throw로 수정
    
    * refactor: PreparedStatement 파라미터 설정 메서드 분리
    
    * refactor: 사용하지 않는 로거 제거
    
    * refactor: 예외 메시지 수정
    
    * refactor: 매퍼 객체 상수로 재사용
    
    * refactor: 예외 처리 추가
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>
    Co-authored-by: 슥 <ime.rokz@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,18 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.sql.DataSource;
+import com.interface21.dao.DataAccessException;
 
 public class JdbcTemplate {
 
@@ -13,5 +22,71 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... parameters) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            setParameters(pstmt, parameters);
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            setParameters(pstmt, parameters);
+
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                if (!resultSet.next()) {
+                    throw new DataAccessException("조회 결과가 존재하지 않습니다.");
+                }
+                T result = rowMapper.mapRow(resultSet, parameters.length);
+                if (resultSet.next()) {
+                    throw new DataAccessException("조회 결과가 2개 이상입니다.");
+                }
+                return result;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            setParameters(pstmt, parameters);
+
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                List<T> results = new ArrayList<>();
+                while (resultSet.next()) {
+                    results.add(rowMapper.mapRow(resultSet, parameters.length));
+                }
+                return results;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e.getMessage(), e);
+        }
+    }
+
+    private void setParameters(PreparedStatement pstmt, Object... parameters) throws SQLException {
+        if (parameters == null || parameters.length == 0) {
+            return;
+        }
+        for (int i = 0; i < parameters.length; i++) {
+            pstmt.setObject(i + 1, parameters[i]);
+        }
     }
 }
