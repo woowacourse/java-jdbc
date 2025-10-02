5cabed6cc09b54566d10b82da6721a8d907623d4
Seunghee Jin <77621712+Jin409@users.noreply.github.com>
2025-10-02 16:02:50 +0900

    [1단계 - JDBC 라이브러리 구현하기] 히로(진승희) 미션 제출합니다. (#936)
    * feat: JdbcTemplate 구현
    
    * fix: JdbcTemplate 을 주입하도록 수정
    
    * refactor: 파라미터 세팅 메서드 분리
    
    * refactor: 콜백 인터페이스로 공통 로직 추출
    
    * fix: 불필요한 세미콜론 제거
    
    * refactor: 안전하게 null 을 사용하도록 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.lang.reflect.Constructor;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class JdbcTemplate {
 
@@ -13,5 +17,48 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... args) {
+        execute(sql, PreparedStatement::executeUpdate, args);
+    }
+
+    public <T> T getSingleResult(RowMapper<T> rowMapper, String sql, Object... args) {
+        return execute(sql, pstmt -> {
+            try (ResultSet rs = pstmt.executeQuery()) {
+                return rs.next() ? rowMapper.mapRow(rs) : null;
+            }
+        }, args);
+    }
+
+    public <T> List<T> getResults(RowMapper<T> rowMapper, String sql, Object... args) {
+        return execute(sql, pstmt -> {
+            List<T> result = new ArrayList<>();
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) result.add(rowMapper.mapRow(rs));
+            }
+            return result;
+        }, args);
+    }
+
+    private <T> T execute(String sql, PreparedStatementCallback<T> preparedStatementCallback, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setParameters(pstmt, args);
+            return preparedStatementCallback.doInPreparedStatement(pstmt);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParameters(PreparedStatement pstmt, Object... args) throws SQLException {
+        if (args == null || args.length == 0) {
+            return;
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
     }
 }
