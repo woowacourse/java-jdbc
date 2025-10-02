06b0fbd86b663656f79d8081bd03e7b198b728be
threepebbles <threepebbles@naver.com>
2025-10-02 10:23:35 +0900

    comment: 예외 클래스 주석 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/DataAccessException.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/DataAccessException.java
@@ -1,5 +1,13 @@
 package com.interface21.jdbc.core;
 
+/**
+ * 데이터 접근 중 발생하는 예외의 최상위 클래스
+ * <p>
+ * SQLException과 같은 Checked Exception을 Unchecked Exception으로 변환하여
+ * 예외 처리를 선택적으로 만듭니다.
+ *
+ * @see <a href="https://github.com/spring-projects/spring-framework/blob/main/spring-tx/src/main/java/org/springframework/dao/DataAccessException.java">Spring Framework DataAccessException</a>
+ */
 public class DataAccessException extends RuntimeException {
 
     public DataAccessException(Throwable cause) {
--- jdbc/src/main/java/com/interface21/jdbc/core/EmptyResultDataAccessException.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/EmptyResultDataAccessException.java
@@ -1,6 +1,10 @@
 package com.interface21.jdbc.core;
 
-// https://github.com/spring-projects/spring-framework/blob/main/spring-tx/src/main/java/org/springframework/dao/EmptyResultDataAccessException.java
+/**
+ * 조회 결과가 비어있을 때 발생하는 예외
+ *
+ * @see <a href="https://github.com/spring-projects/spring-framework/blob/main/spring-tx/src/main/java/org/springframework/dao/EmptyResultDataAccessException.java">Spring Framework EmptyResultDataAccessException</a>
+ */
 public class EmptyResultDataAccessException extends DataAccessException {
 
     public EmptyResultDataAccessException(String message) {
