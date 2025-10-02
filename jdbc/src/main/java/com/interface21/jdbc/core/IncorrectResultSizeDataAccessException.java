c2bd1015866de18b94ebab3c2061786f97f2a682
threepebbles <threepebbles@naver.com>
2025-10-02 10:23:35 +0900

    comment: 예외 클래스 주석 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/IncorrectResultSizeDataAccessException.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/IncorrectResultSizeDataAccessException.java
@@ -1,6 +1,10 @@
 package com.interface21.jdbc.core;
 
-// https://github.com/spring-projects/spring-framework/blob/main/spring-tx/src/main/java/org/springframework/dao/IncorrectResultSizeDataAccessException.java
+/**
+ * 조회 결과의 크기가 예상과 다를 때 발생하는 예외
+ *
+ * @see <a href="https://github.com/spring-projects/spring-framework/blob/main/spring-tx/src/main/java/org/springframework/dao/IncorrectResultSizeDataAccessException.java">Spring Framework IncorrectResultSizeDataAccessException</a>
+ */
 public class IncorrectResultSizeDataAccessException extends DataAccessException {
 
     public IncorrectResultSizeDataAccessException(String message) {
