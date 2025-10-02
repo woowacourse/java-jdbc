52b89bec17d3867f242ee1a129cfea0c2cbb3657
Yeongjae Kim <sees77770@gmail.com>
2025-10-02 10:47:01 +0900

    refactor: queryForObject 예외 처리

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,6 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,7 +32,7 @@ public class JdbcTemplate {
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
     }
 
@@ -52,7 +53,7 @@ public class JdbcTemplate {
             }
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
     }
 
@@ -65,14 +66,21 @@ public class JdbcTemplate {
             try (ResultSet rs = pstmt.executeQuery()) {
                 log.debug("query : {}", sql);
 
-                if (rs.next()) {
-                    return rowMapper.mapRow(rs);
+                if (!rs.next()) {
+                    throw new DataAccessException("Expected 1 result, but found 0");
                 }
-                return null;
+
+                T result = rowMapper.mapRow(rs);
+
+                if (rs.next()) {
+                    throw new DataAccessException("Expected 1 result, but found more than 1");
+                }
+
+                return result;
             }
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+            throw new DataAccessException(e);
         }
     }
 
