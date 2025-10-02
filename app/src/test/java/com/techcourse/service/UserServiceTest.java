19830942b6fafcb75fe181dceb096fa83d922417
PraiseBak <60121346+PraiseBak@users.noreply.github.com>
2025-10-02 13:17:06 +0900

    refactor: 트랜잭션 매니저 방식으로 전환

--- app/src/test/java/com/techcourse/service/UserServiceTest.java
+++ app/src/test/java/com/techcourse/service/UserServiceTest.java
@@ -10,6 +10,8 @@ import com.interface21.jdbc.core.JdbcTemplate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import javax.sql.DataSource;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
