76ad6ed23ba77cb7199463a8887879ccbe32e38b
eueo8259 <eueo8259@gmail.com>
2025-10-02 11:15:43 +0900

    refactor: DataSource 및 JdbcTemplate 직접 주입

--- app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
+++ app/src/main/java/com/techcourse/AppWebApplicationInitializer.java
@@ -1,12 +1,15 @@
 package com.techcourse;
 
 import com.interface21.core.util.DIContainer;
+import com.interface21.jdbc.core.JdbcTemplate;
+import com.techcourse.config.DataSourceConfig;
 import jakarta.servlet.ServletContext;
 import com.interface21.webmvc.servlet.mvc.DispatcherServlet;
 import com.interface21.webmvc.servlet.mvc.asis.ControllerHandlerAdapter;
 import com.interface21.webmvc.servlet.mvc.tobe.AnnotationHandlerMapping;
 import com.interface21.webmvc.servlet.mvc.tobe.HandlerExecutionHandlerAdapter;
 import com.interface21.web.WebApplicationInitializer;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,6 +20,13 @@ public class AppWebApplicationInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(final ServletContext servletContext) {
         DIContainer diContainer = new DIContainer();
+
+        DataSource dataSource = DataSourceConfig.getInstance();
+        diContainer.registerBean(DataSource.class, dataSource);
+
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        diContainer.registerBean(JdbcTemplate.class, jdbcTemplate);
+
         diContainer.scanAndRegister("com.techcourse");
 
         final var dispatcherServlet = new DispatcherServlet();
