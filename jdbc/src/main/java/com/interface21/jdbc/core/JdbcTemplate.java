7208840f4dddf6c66f41584516b4c54db097c6a3
changui <122252160+changuii@users.noreply.github.com>
2025-10-02 16:17:03 +0900

    [1단계 - JDBC 라이브러리 구현하기] 부기(이창의) 미션 제출합니다.  (#980)
    * chore: Poling 오타 수정 (#928)
    
    * feat: UserDAO 구현
    
    * feat: JdbcTemplate로 마이그레이션
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,16 @@
 package com.interface21.jdbc.core;
 
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,122 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... paramters) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            log.debug("query : {}", sql);
+
+            for (int i = 1; i <= paramters.length; i++) {
+                pstmt.setObject(i, paramters[i - 1]);
+            }
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {
+            }
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
+    }
+
+    public <T> T query(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = dataSource.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            for (int i = 1; i <= parameters.length; i++) {
+                pstmt.setObject(i, parameters[i - 1]);
+            }
+
+            rs = pstmt.executeQuery();
+
+            log.debug("query : {}", sql);
+
+            if (rs.next()) {
+                return rowMapper.mapped(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {
+            }
+
+            try {
+                if (pstmt != null) {
+                    pstmt.close();
+                }
+            } catch (SQLException ignored) {
+            }
+
+            try {
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
+    }
+
+    public <T> List<T> queryAll(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        ResultSet rs = null;
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement pstmt = connection.prepareStatement(sql);
+        ) {
+            for (int i = 1; i <= parameters.length; i++) {
+                pstmt.setObject(i, parameters[i - 1]);
+            }
+
+            rs = pstmt.executeQuery();
+            List<T> results = new ArrayList<>();
+
+            while (rs.next()) {
+                T result = rowMapper.mapped(rs);
+                results.add(result);
+            }
+
+            return results;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException ignored) {
+            }
+        }
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
     }
 }
