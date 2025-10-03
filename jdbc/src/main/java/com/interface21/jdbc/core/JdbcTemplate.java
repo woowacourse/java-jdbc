d1a4c6b091c73759cc03e2ceb946736d7cf70b60
kjyyjk <rlawnsdud920@gmail.com>
2025-10-03 17:11:59 +0900

    refactor: db 관련 공통 로직 jdbcTemplate로 이동

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,12 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +20,64 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    // TODO 한개가 아닐 경우 예외 처리
+    public <T> T executeQueryObject(
+        final String sql,
+        final RowMapper<T> rowMapper,
+        final Object... args
+    ) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatementWithArguments(conn, sql, args);
+             ResultSet resultSet = pstmt.executeQuery();
+        ) {
+            log.debug("query : {}", sql);
+            return rowMapper.mapRow(resultSet);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> executeQuery(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatementWithArguments(conn, sql, args);
+             ResultSet resultSet = pstmt.executeQuery();
+        ) {
+            log.debug("query : {}", sql);
+            List<T> list = new ArrayList<>();
+            while (resultSet.next()) {
+                list.add(rowMapper.mapRow(resultSet));
+            }
+            return list;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int executeUpdate(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatementWithArguments(conn, sql, args);
+        ) {
+            log.debug("query : {}", sql);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement getPreparedStatementWithArguments(
+        final Connection conn,
+        final String sql,
+        final Object... args
+    ) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        for (int i = 1; i <= args.length; i++) {
+            pstmt.setObject(i, args[i]);
+        }
+        return pstmt;
     }
 }
---
+++ jdbc/src/main/java/com/interface21/jdbc/core/RowMapper.java
@@ -0,0 +1,10 @@
+package com.interface21.jdbc.core;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@FunctionalInterface
+public interface RowMapper<T> {
+
+    T mapRow(ResultSet rs) throws SQLException;
+}
