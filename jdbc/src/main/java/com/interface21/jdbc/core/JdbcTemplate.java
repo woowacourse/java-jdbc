13dd4156112d3a4cea389829c22c38d1aaefd19f
Chaeyoung714 <202102760@hufs.ac.kr>
2025-10-03 21:37:08 +0900

    feat: UserDao의 중복코드 JdbcTemplate로 이전

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,17 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.jdbc.ResultSetMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 public class JdbcTemplate {
 
@@ -13,5 +21,85 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object ... params) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            setParameter(params, pstmt);
+
+            pstmt.executeUpdate();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Optional<Object> queryForObject(
+            final String sql,
+            final ResultSetMapper<ResultSet, ?> mapper,
+            final Object ... params
+    ) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            setParameter(params, pstmt);
+
+            ResultSet resultSet = pstmt.executeQuery();
+            if (resultSet.next()) {
+                return Optional.of(mapper.map(resultSet));
+            }
+
+            return Optional.empty();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<Object> queryForList(
+            final String sql,
+            final ResultSetMapper<ResultSet, ?> mapper,
+            final Object ... params
+    ) {
+        try (final Connection connection = dataSource.getConnection();
+             final PreparedStatement pstmt = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            setParameter(params, pstmt);
+
+            ResultSet resultSet = pstmt.executeQuery();
+
+            List<Object> results = new ArrayList<>();
+            while (resultSet.next()) {
+                results.add(mapper.map(resultSet));
+            }
+            return results;
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParameter(final Object[] params, final PreparedStatement pstmt) throws SQLException {
+        if (params.length == 0) {
+            return;
+        }
+
+        for (int idx = 1; idx <= params.length; idx++) {
+            Object param = params[idx - 1];
+
+            if (param.getClass() == String.class) {
+                pstmt.setString(idx, (String) param);
+            } else if (param.getClass() == Long.class) {
+                pstmt.setLong(idx, (Long) param);
+            }
+        }
     }
 }
