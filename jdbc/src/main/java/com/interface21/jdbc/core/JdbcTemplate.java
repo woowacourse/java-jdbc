b0b245b2f3e7180ad0a2400eefaacf8c02cb26d6
eoehd1ek <eoehd2ek@gmail.com>
2025-10-04 06:35:54 +0900

    feat: 사용자 지정 PreparedStatementSetter 사용하는 메서드 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -22,10 +22,24 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
+    /**
+     * 파라미터 마커 나열 순서대로 parameters를 매핑하는 PreparedStatementSetter를 사용하여 SQL 업데이트를 실행합니다
+     *
+     * @param sql        실행할 SQL 업데이트 문
+     * @param parameters SQL 업데이트에 사용할 파라미터들
+     */
     public void update(final String sql, final Object... parameters) {
+        update(sql, DEFAULT_PREPARED_STATEMENT_SETTERS.getPreparedStatementSetter(parameters), parameters);
+    }
+
+    public void update(
+            final String sql,
+            final PreparedStatementSetter preparedStatementSetter,
+            final Object... parameters
+    ) {
         execute(
                 sql,
-                DEFAULT_PREPARED_STATEMENT_SETTERS.getPreparedStatementSetter(parameters),
+                preparedStatementSetter,
                 preparedStatement -> {
                     preparedStatement.executeUpdate();
                     return null;
@@ -34,10 +48,32 @@ public class JdbcTemplate {
         );
     }
 
+    /**
+     * 파라미터 마커 나열 순서대로 parameters를 매핑하는 PreparedStatementSetter를 사용하여 SQL 쿼리를 실행하고, 단일 결과를 RowMapper로 매핑하여 반환합니다
+     *
+     * @param sql        실행할 SQL 업데이트 문
+     * @param rowMapper  결과 행을 매핑하는 RowMapper
+     * @param parameters SQL 쿼리에 사용할 파라미터들
+     * @param <T>        매핑된 결과 객체의 타입
+     */
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        return execute(
+        return queryForObject(
                 sql,
                 DEFAULT_PREPARED_STATEMENT_SETTERS.getPreparedStatementSetter(parameters),
+                rowMapper,
+                parameters
+        );
+    }
+
+    public <T> T queryForObject(
+            final String sql,
+            final PreparedStatementSetter preparedStatementSetter,
+            final RowMapper<T> rowMapper,
+            final Object... parameters
+    ) {
+        return execute(
+                sql,
+                preparedStatementSetter,
                 preparedStatement -> {
                     try (final ResultSet resultSet = preparedStatement.executeQuery()) {
                         return mapSingleResult(resultSet, rowMapper);
@@ -54,10 +90,32 @@ public class JdbcTemplate {
         throw new SQLException("No data found");
     }
 
+    /**
+     * 파라미터 마커 나열 순서대로 parameters를 매핑하는 PreparedStatementSetter를 사용하여 SQL 쿼리를 실행하고, 결과를 RowMapper로 매핑하여 리스트로 반환합니다
+     *
+     * @param sql        실행할 SQL 업데이트 문
+     * @param rowMapper  결과 행을 매핑하는 RowMapper
+     * @param parameters SQL 쿼리에 사용할 파라미터들
+     * @param <T>        매핑된 결과 객체의 타입
+     */
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        return execute(
+        return query(
                 sql,
                 DEFAULT_PREPARED_STATEMENT_SETTERS.getPreparedStatementSetter(parameters),
+                rowMapper,
+                parameters
+        );
+    }
+
+    public <T> List<T> query(
+            final String sql,
+            final PreparedStatementSetter preparedStatementSetter,
+            final RowMapper<T> rowMapper,
+            final Object... parameters
+    ) {
+        return execute(
+                sql,
+                preparedStatementSetter,
                 preparedStatement -> {
                     try (final ResultSet resultSet = preparedStatement.executeQuery()) {
                         return mapResult(resultSet, rowMapper);
