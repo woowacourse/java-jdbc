610a7f010dfe43d7dec0f54c4b0f045351d51fd8
byesol <bsj5967@naver.com>
2025-10-05 01:42:52 +0900

    refactor: update 메서드 jdbcTemplate 사용하게 변경

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,10 +2,6 @@ package com.techcourse.dao;
 
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.domain.User;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
@@ -32,7 +28,8 @@ public class UserDao {
     }
 
     public void update(final User user) {
-        // todo
+        final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
@@ -40,54 +37,54 @@ public class UserDao {
         return null;
     }
 
-    public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
-
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setLong(1, id);
-            rs = pstmt.executeQuery();
-
-            log.debug("query : {}", sql);
-
-            if (rs.next()) {
-                return new User(
-                        rs.getLong(1),
-                        rs.getString(2),
-                        rs.getString(3),
-                        rs.getString(4));
-            }
-            return null;
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
-    }
+//    public User findById(final Long id) {
+//        final var sql = "select id, account, password, email from users where id = ?";
+//
+//        Connection conn = null;
+//        PreparedStatement pstmt = null;
+//        ResultSet rs = null;
+//        try {
+//            conn = dataSource.getConnection();
+//            pstmt = conn.prepareStatement(sql);
+//            pstmt.setLong(1, id);
+//            rs = pstmt.executeQuery();
+//
+//            log.debug("query : {}", sql);
+//
+//            if (rs.next()) {
+//                return new User(
+//                        rs.getLong(1),
+//                        rs.getString(2),
+//                        rs.getString(3),
+//                        rs.getString(4));
+//            }
+//            return null;
+//        } catch (SQLException e) {
+//            log.error(e.getMessage(), e);
+//            throw new RuntimeException(e);
+//        } finally {
+//            try {
+//                if (rs != null) {
+//                    rs.close();
+//                }
+//            } catch (SQLException ignored) {
+//            }
+//
+//            try {
+//                if (pstmt != null) {
+//                    pstmt.close();
+//                }
+//            } catch (SQLException ignored) {
+//            }
+//
+//            try {
+//                if (conn != null) {
+//                    conn.close();
+//                }
+//            } catch (SQLException ignored) {
+//            }
+//        }
+//    }
 
     public User findByAccount(final String account) {
         // todo
