d1490861718bc0f41b66cd27e1b4833b84ac6892
시원 <121426422+seaniiio@users.noreply.github.com>
2025-10-04 12:25:46 +0900

    [1단계 - JDBC 라이브러리 구현하기] 메이(김시원) 미션 제출합니다. (#956)
    * chore: Poling 오타 수정 (#928)
    
    * chore: junit 런처 라이브러리 추가
    
    * refactor: db connection, statement, resultSet 관련 코드를 UserDao에서 JdbcTemplate으로 이동
    
    * refactor: try-with-resources을 통해 자원 반환하도록 수정
    
    * refactor: 쿼리에 대한 로그 남기는 위치 수정
    
    * refactor: 사용하지 않는 로거 삭제
    
    * refactor: UserDao의 DataSource를 직접 주입받는 생성자 삭제
    
    * refactor: ResultSet을 try-with-resources로 관리하도록 수정
    
    * refactor: PreparedStatement의 파라미터 설정 시, 타입을 런타임에 결정하도록 수정
    
    * refactor: UserDao update 쿼리에 조건문 추가
    
    * refactor: RowMapper에서 ResultSet을 순회하는 책임 분리
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,58 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int executeUpdate(final String sql, final Object... parameters) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatementParameters(pstmt, parameters);
+            log.debug("query : {}", sql);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException("리소스 해제에 실패했습니다.");
+        }
+    }
+
+    public <T> T executeQueryForObject(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatementParameters(pstmt, parameters);
+            log.debug("query : {}", sql);
+            try (final ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return rowMapper.mapForObject(rs);
+                }
+                return null;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException("리소스 해제에 실패했습니다.");
+        }
+    }
+
+    public <T> List<T> executeQuery(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatementParameters(pstmt, parameters);
+            log.debug("query : {}", sql);
+            try (final ResultSet rs = pstmt.executeQuery()) {
+                final List<T> instances = new ArrayList<>();
+                while (rs.next()) {
+                    instances.add(rowMapper.mapForObject(rs));
+                }
+                return instances;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException("리소스 해제에 실패했습니다.");
+        }
+    }
+
+    private void setStatementParameters(final PreparedStatement pstmt, final Object[] parameters) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            pstmt.setObject(i + 1, parameters[i]);
+        }
     }
 }
