826c574793164dd8d818b403e5def17daeac1409
byesol <bsj5967@naver.com>
2025-10-05 02:00:27 +0900

    feat: RowMapper 인터페이스 추가 및 query 메서드 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -2,7 +2,10 @@ package com.interface21.jdbc.core;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +31,31 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error("SQL failed. query: {}", sql, e);
             throw new RuntimeException(e);
+        }
+    }
+
+    public List<Object> query(String sql, RowMapper rowMapper, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParameters(preparedStatement, args);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                List<Object> results = new ArrayList<>();
+                int rowNum = 0;
+                while (resultSet.next()) {
+                    results.add(rowMapper.mapRow(resultSet, rowNum++));
+                }
+                return results;
+            }
+        } catch (SQLException e) {
+            log.error("SQL query failed. query: {}", sql, e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParameters(PreparedStatement preparedStatement, Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject(i + 1, args[i]);
         }
     }
 }
