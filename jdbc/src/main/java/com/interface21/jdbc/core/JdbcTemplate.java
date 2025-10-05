331019012badd6aafd3fcca0adcbb56046784af1
jhpark1227 <junhyeok0246@naver.com>
2025-10-05 19:35:16 +0900

    refactor: 중복된 메서드 제거

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -21,7 +21,7 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void insert(String sql, Object... args) {
+    public void update(String sql, Object... args) {
         Connection conn = null;
         PreparedStatement pstmt = null;
         try {
@@ -89,26 +89,6 @@ public class JdbcTemplate {
             throw new RuntimeException(e);
         } finally {
             closeResultSet(resultSet);
-            closeStatement(pstmt);
-            closeConnection(conn);
-        }
-    }
-
-    public void update(String sql, Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-            setArguments(pstmt, args);
-
-            log.debug("query : {}", sql);
-
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
             closeStatement(pstmt);
             closeConnection(conn);
         }
