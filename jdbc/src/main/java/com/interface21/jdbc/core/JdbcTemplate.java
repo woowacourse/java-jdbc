ffcafdfddb73524933fdb55b472f2f227afbeb15
taek2222 <oot309@daum.net>
2025-10-05 20:25:38 +0900

    refactor: 파라미터 갯수 검증 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -31,9 +31,10 @@ public class JdbcTemplate {
 
     public ResultSet executeQuery(String sql, Object... params) {
         try {
-
             Connection conn = dataSource.getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            validationParamLength(params, pstmt);
 
             for (int i = 0; i < params.length; i++) {
                 pstmt.setObject(i + 1, params[i]);
@@ -42,6 +43,18 @@ public class JdbcTemplate {
             return pstmt.executeQuery();
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private void validationParamLength(Object[] params, PreparedStatement pstmt) throws SQLException {
+        int parameterCount = pstmt.getParameterMetaData().getParameterCount();
+        if (params.length != parameterCount) {
+            throw new IllegalArgumentException(
+                    String.format("파라미터 개수 불일치: SQL에 %d개 필요, %d개 제공됨",
+                            parameterCount,
+                            params.length
+                    )
+            );
         }
     }
 }
