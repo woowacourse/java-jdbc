6f774f67c25e794e1780a97db826d097018d8a6c
egaeng09 <egaeng09@gmail.com>
2025-10-05 13:59:57 +0900

    feat: 단일 read 기능 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -2,6 +2,7 @@ package com.interface21.jdbc.core;
 
 import com.interface21.dao.DataAccessException;
 import java.sql.SQLException;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -24,6 +25,23 @@ public class JdbcTemplate {
                 preparedStatement.setObject(i + 1, args[i]);
             }
             preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+            try (final var resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return rowMapper.mapRow(resultSet);
+                }
+                return null;
+            }
         } catch (final SQLException e) {
             throw new DataAccessException(e);
         }
