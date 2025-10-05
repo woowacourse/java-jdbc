5811e3256af5626ff8a522e3018df8d0cc6df275
Jin409 <sualng123@gmail.com>
2025-10-06 00:28:10 +0900

    refactor: 쿼리문 생성 로직이 가변적이도록 수정

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -2,13 +2,10 @@ package com.techcourse.dao;
 
 import com.techcourse.domain.User;
 import com.interface21.jdbc.core.JdbcTemplate;
+import java.lang.reflect.Field;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 public class UserDao {
@@ -22,13 +19,82 @@ public class UserDao {
     }
 
     public void insert(final User user) {
-        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
+        StringBuilder sb = new StringBuilder();
+        sb.append("INSERT INTO ");
+        sb.append(User.class.getSimpleName().toLowerCase()).append("s ");
+        sb.append(" (");
+
+        Field[] fields = user.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.getName().equals("id")) {
+                continue;
+            }
+
+            sb.append(field.getName()).append(", ");
+        }
+
+        sb.delete(sb.length() - 2, sb.length());
+        sb.append(") VALUES (");
+
+        sb.append("?, ".repeat(Math.max(0, fields.length - 1)));
+        sb.delete(sb.length() - 2, sb.length());
+        sb.append(")");
+
+        System.out.println(sb);
+
+        jdbcTemplate.update(sb.toString(), mapParameters(user));
     }
 
     public void update(final User user) {
-        final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
-        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
+        final var sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE id = ?";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("UPDATE ");
+        sb.append(User.class.getSimpleName().toLowerCase()).append("s ");
+        sb.append("SET");
+
+        Field[] fields = user.getClass().getDeclaredFields();
+        for (Field field : fields) {
+            if (field.getName().equals("id")) {
+                continue;
+            }
+
+            sb.append(field.getName()).append(" = ?, ");
+        }
+
+        sb.deleteCharAt(sb.length() - 2);
+
+        // TODO: 가변성 있게 바꾸기
+        sb.append("WHERE id = ?");
+
+        Object[] objects = mapParameters(user);
+        Object[] newObjects = new Object[objects.length + 1];
+        System.arraycopy(objects, 0, newObjects, 0, objects.length);
+        newObjects[newObjects.length - 1] = user.getId();
+
+        jdbcTemplate.update(sql, newObjects);
+    }
+
+    private Object[] mapParameters(Object obj) {
+        try {
+            Field[] fields = obj.getClass().getDeclaredFields();
+            Object[] params = new Object[fields.length - 1];
+
+            int paramIndex = 0;
+
+            for (Field field : fields) {
+                if (field.getName().equals("id")) {
+                    continue;
+                }
+                field.setAccessible(true);
+                params[paramIndex] = field.get(obj);
+                paramIndex++;
+            }
+
+            return params;
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<User> findAll() {
