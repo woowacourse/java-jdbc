dec36fbc1886bb90ee31ae0f0cf301d7cb8d6169
juhee <77716414+ljhee92@users.noreply.github.com>
2025-10-05 18:19:25 +0900

    [1단계 - JDBC 라이브러리 구현하기] 듀이(이주희) 미션 제출합니다.  (#998)
    * chore: Poling 오타 수정 (#928)
    
    * chore: 의존성 수정
    
    * feat: update, findAll, findByAccount 구현
    
    * feat: 중복 코드 제거
    
    * test: jdbcTemplate 테스트 코드 작성
    
    * fix: 제거되지 않은 executeUpdate 삭제
    
    * fix: log 출력 시점 수정
    
    * refactor: null 반환 대신 Optional 반환으로 수정
    
    * refactor: 인덱스가 아닌 컬럼명으로 매핑하도록 수정
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -1,8 +1,10 @@
 package com.techcourse.dao;
 
+import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -15,8 +17,9 @@ class UserDaoTest {
     @BeforeEach
     void setup() {
         DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
-
-        userDao = new UserDao(DataSourceConfig.getInstance());
+        final DataSource dataSource = DataSourceConfig.getInstance();
+        final JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        userDao = new UserDao(jdbcTemplate);
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(user);
     }
@@ -30,7 +33,7 @@ class UserDaoTest {
 
     @Test
     void findById() {
-        final var user = userDao.findById(1L);
+        final var user = userDao.findById(1L).get();
 
         assertThat(user.getAccount()).isEqualTo("gugu");
     }
@@ -38,7 +41,7 @@ class UserDaoTest {
     @Test
     void findByAccount() {
         final var account = "gugu";
-        final var user = userDao.findByAccount(account);
+        final var user = userDao.findByAccount(account).get();
 
         assertThat(user.getAccount()).isEqualTo(account);
     }
@@ -49,7 +52,7 @@ class UserDaoTest {
         final var user = new User(account, "password", "hkkang@woowahan.com");
         userDao.insert(user);
 
-        final var actual = userDao.findById(2L);
+        final var actual = userDao.findById(2L).get();
 
         assertThat(actual.getAccount()).isEqualTo(account);
     }
@@ -57,12 +60,12 @@ class UserDaoTest {
     @Test
     void update() {
         final var newPassword = "password99";
-        final var user = userDao.findById(1L);
+        final var user = userDao.findById(1L).get();
         user.changePassword(newPassword);
 
         userDao.update(user);
 
-        final var actual = userDao.findById(1L);
+        final var actual = userDao.findById(1L).get();
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,16 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,77 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(
+            final String sql,
+            final Object... params
+    ) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+
+            bindParameters(params, pstmt);
+            pstmt.executeUpdate();
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> Optional<T> queryForObject(
+            final String sql,
+            final RowMapper<T> rowMapper,
+            final Object... params
+    ) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+
+            bindParameters(params, pstmt);
+
+            try (final ResultSet rs = pstmt.executeQuery()) {
+                if (rs.next()) {
+                    return Optional.ofNullable(rowMapper.mapRow(rs));
+                }
+                return Optional.empty();
+            }
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(
+            final String sql,
+            final RowMapper<T> rowMapper
+    ) {
+        try (final Connection conn = dataSource.getConnection();
+             final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+
+            try (final ResultSet rs = pstmt.executeQuery()) {
+                final List<T> results = new ArrayList<>();
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs));
+                }
+                return results;
+            }
+        } catch (final SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void bindParameters(
+            final Object[] params,
+            final PreparedStatement pstmt
+    ) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            pstmt.setObject(i+1, params[i]);
+        }
     }
 }
