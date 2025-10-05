6cefb73e6bd8a484abc8f9e0b9b9c3826c54df2b
Taek_2 <oot309@daum.net>
2025-10-05 23:55:14 +0900

    [1단계 - JDBC 라이브러리 구현하기] 비타(오연택) 미션 제출합니다. (#1002)
    * chore: Junit 버전 수정
    
    * feat: update, findAll, findByAccount 추가
    
    * refactor: JdbcTemplate 클래스 활용한 재사용 형태로 변환
    
    * test: 주입 대상 변경
    
    * refactor: 파라미터 갯수 검증 추가
    
    * refactor: 유저 생성 메서드 분리
    
    * refactor: update도 검증 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,17 +1,62 @@
 package com.interface21.jdbc.core;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import javax.sql.DataSource;
 
 public class JdbcTemplate {
-
-    private static final Logger log = LoggerFactory.getLogger(JdbcTemplate.class);
 
     private final DataSource dataSource;
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void executeUpdate(String sql, Object... params) {
+        try {
+            Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            validationParamLength(params, pstmt);
+
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ResultSet executeQuery(String sql, Object... params) {
+        try {
+            Connection conn = dataSource.getConnection();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+
+            validationParamLength(params, pstmt);
+
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+
+            return pstmt.executeQuery();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void validationParamLength(Object[] params, PreparedStatement pstmt) throws SQLException {
+        int parameterCount = pstmt.getParameterMetaData().getParameterCount();
+        if (params.length != parameterCount) {
+            throw new IllegalArgumentException(
+                    String.format("파라미터 개수 불일치: SQL에 %d개 필요, %d개 제공됨",
+                            parameterCount,
+                            params.length
+                    )
+            );
+        }
     }
 }
