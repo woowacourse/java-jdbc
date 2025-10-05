5795eeae56454652ab0094cc481692dbabb6acff
백승주 <bsj5967@naver.com>
2025-10-05 21:46:13 +0900

    [1단계 - JDBC 라이브러리 구현하기] 벡터(백승주) 미션 제출합니다. (#1012)
    * chore: Poling 오타 수정 (#928)
    
    * chore: JUnit 플랫폼 런처 종속성 추가
    
    * refactor: JdbcTemplate 도입 및 update 메서드 추가
    
    * refactor: update 메서드 jdbcTemplate 사용하게 변경
    
    * feat: RowMapper 인터페이스 추가 및 query 메서드 구현
    
    * feat: findAll 메서드 구현 및 query 메서드 활용
    
    * feat: queryForObject 메서드 추가 및 단일 결과 조회 기능 구현
    
    * refactor: findById 메서드 jdbcTemplate 활용하도록 수정
    
    * feat: findByAccount 메서드 구현 및 queryForObject 활용
    
    * refactor: RowMapper 리팩토링 및 중복 제거
    
    * refactor: update 메서드 파라미터 설정 로직 분리
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,50 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParameters(preparedStatement, args);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            log.error("query 실패 {}", sql, e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void setParameters(PreparedStatement preparedStatement, Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            preparedStatement.setObject(i + 1, args[i]);
+        }
+    }
+
+    public List<Object> query(String sql, RowMapper rowMapper, Object... args) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            setParameters(preparedStatement, args);
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                List<Object> results = new ArrayList<>();
+                int rowNum = 0;
+                while (resultSet.next()) {
+                    results.add(rowMapper.mapRow(resultSet, rowNum++));
+                }
+                return results;
+            }
+        } catch (SQLException e) {
+            log.error("SQL query failed. query: {}", sql, e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Object queryForObject(String sql, RowMapper rowMapper, Object... args) {
+        List<Object> results = query(sql, rowMapper, args);
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results.get(0);
     }
 }
