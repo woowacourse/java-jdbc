04ceb58dee409fc5368f7e2c78cdabb53840c46f
갱민 <egaeng09@gmail.com>
2025-10-05 22:43:44 +0900

    [1단계 - JDBC 라이브러리 구현하기] 시소(이경민) 미션 제출합니다. (#1014)
    * chore: Poling 오타 수정 (#928)
    
    * DriverManager 오타 수정 (#960)
    
    * chore: 주석 추가
    
    * feat: update 기능 구현
    
    * feat: 단일 read 기능 구현
    
    * feat: read 기능 구현 및 구현 기능 반영
    
    * refactor: rowMapper 추출
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>
    Co-authored-by: 슥 <ime.rokz@gmail.com>

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,6 +4,7 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.util.NoSuchElementException;
 
 public class UserService {
 
@@ -16,7 +17,7 @@ public class UserService {
     }
 
     public User findById(final long id) {
-        return userDao.findById(id);
+        return userDao.findById(id).orElseThrow(NoSuchElementException::new);
     }
 
     public void insert(final User user) {
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,9 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +17,43 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... args) {
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+            preparedStatement.executeUpdate();
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final var connection = dataSource.getConnection();
+             final var preparedStatement = connection.prepareStatement(sql)) {
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i + 1, args[i]);
+            }
+            try (final var resultSet = preparedStatement.executeQuery()) {
+                final List<T> results = new ArrayList<>();
+                while (resultSet.next()) {
+                    results.add(rowMapper.mapRow(resultSet));
+                }
+                return results;
+            }
+        } catch (final SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        final List<T> results = query(sql, rowMapper, args);
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results.getFirst();
     }
 }
