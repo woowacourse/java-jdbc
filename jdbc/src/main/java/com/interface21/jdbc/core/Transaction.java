54ed35ba46f61f520919a2df0f17808ae10b3238
Dompoo <dlckdrms0517@gmail.com>
2025-10-05 17:15:54 +0900

    chore: Transaction에 로깅 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/Transaction.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/Transaction.java
@@ -3,8 +3,12 @@ package com.interface21.jdbc.core;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Transaction {
+
+    private static final Logger log = LoggerFactory.getLogger(Transaction.class);
 
     private final DataSource dataSource;
     private Connection connection;
@@ -22,6 +26,7 @@ public class Transaction {
             this.isStarted = true;
             TransactionHolder.setTransaction(this);
         } catch (SQLException e) {
+            log.error(e.getMessage(), e);
             // TODO : 예외처리 강화 필요
             throw new RuntimeException(e);
         }
@@ -30,9 +35,11 @@ public class Transaction {
     public void commit() {
         try {
             connection.commit();
+            connection.close();
             this.connection = null;
             this.isStarted = false;
         } catch (SQLException e) {
+            log.error(e.getMessage(), e);
             // TODO : 예외처리 강화 필요
             throw new RuntimeException(e);
         }
@@ -41,9 +48,11 @@ public class Transaction {
     public void rollback() {
         try {
             connection.rollback();
+            connection.close();
             this.connection = null;
             this.isStarted = false;
         } catch (SQLException e) {
+            log.error(e.getMessage(), e);
             // TODO : 예외처리 강화 필요
             throw new RuntimeException(e);
         }
