1babb4bf18dab371dca756ac78590f22922c4264
kjyyjk <rlawnsdud920@gmail.com>
2025-10-07 01:30:41 +0900

    [1단계 - JDBC 라이브러리 구현하기] 레오(김준영) 미션 제출합니다.  (#995)
    * DriverManager 오타 수정 (#960)
    
    * junit 버전 수정:
    
    * feat: jdbcTemplate을 활용한 UserDao 구현
    
    * refactor: db 관련 공통 로직 jdbcTemplate로 이동
    
    * fix: rowMapper 이전에 ResultSet.next 초기 호출
    
    * feat: jdbcTemplate#queryForObject 복수 결과 예외처리
    
    * refactor: UserHistoryDao 리팩터링
    
    ---------
    
    Co-authored-by: 슥 <ime.rokz@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,12 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +20,71 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> T queryForObject(
+        final String sql,
+        final RowMapper<T> rowMapper,
+        final Object... args
+    ) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatementWithArguments(conn, sql, args);
+             ResultSet resultSet = pstmt.executeQuery();
+        ) {
+            log.debug("query : {}", sql);
+
+            if (!resultSet.next()) { // 쿼리 결과 없음
+                return null;
+            }
+            T returnValue = rowMapper.mapRow(resultSet);
+            if (resultSet.next()) { // 결과 1개 초과
+                throw new IllegalArgumentException("query returns more than one row");
+            }
+            return returnValue;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatementWithArguments(conn, sql, args);
+             ResultSet resultSet = pstmt.executeQuery();
+        ) {
+            log.debug("query : {}", sql);
+            List<T> list = new ArrayList<>();
+            while (resultSet.next()) {
+                list.add(rowMapper.mapRow(resultSet));
+            }
+            return list;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public int update(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = getPreparedStatementWithArguments(conn, sql, args);
+        ) {
+            log.debug("query : {}", sql);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatement getPreparedStatementWithArguments(
+        final Connection conn,
+        final String sql,
+        final Object... args
+    ) throws SQLException {
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        for (int i =0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+        return pstmt;
     }
 }
