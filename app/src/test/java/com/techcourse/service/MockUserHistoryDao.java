1763e4e689cb9b263d11c92bebd0c004022f2930
MinJun Choi <mj043000@naver.com>
2025-10-06 22:50:42 +0900

    feat: Connection을 매개변수로 받는 MockUserHistoryDao 메서드 추가
    - `log` 메서드 오버라이드: `Connection`을 매개변수로 사용
    - 외부에서 연결 관리 가능하도록 구현
    - 코드 재사용성 및 확장성 강화

--- app/src/test/java/com/techcourse/service/MockUserHistoryDao.java
+++ app/src/test/java/com/techcourse/service/MockUserHistoryDao.java
@@ -5,6 +5,9 @@ import com.techcourse.domain.UserHistory;
 import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.core.JdbcTemplate;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class MockUserHistoryDao extends UserHistoryDao {
 
     public MockUserHistoryDao(final JdbcTemplate jdbcTemplate) {
@@ -13,6 +16,11 @@ public class MockUserHistoryDao extends UserHistoryDao {
 
     @Override
     public void log(final UserHistory userHistory) {
+        throw new DataAccessException();
+    }
+
+    @Override
+    public void log(final Connection conn, final UserHistory userHistory) throws SQLException {
         throw new DataAccessException();
     }
 }
