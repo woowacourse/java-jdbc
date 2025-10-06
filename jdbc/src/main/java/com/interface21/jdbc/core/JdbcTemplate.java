feeac177d8e66a066de0392bffe459cba15e4992
kali <111180367+2Jin1031@users.noreply.github.com>
2025-10-06 23:16:14 +0900

    [1단계 - JDBC 라이브러리 구현하기] 칼리(이진) 미션 제출합니다.  (#957)
    * chore: Poling 오타 수정 (#928)
    
    * test(Stage1Test): 격리 수준에 따른 dirty reads 테스트 표 작성
    
    * test(Stage1Test): 격리 수준에 따른 repeatable read 테스트 표 작성
    
    * test(Stage1Test): 멀티 쿼리 옵셥 적용
    
    * test(Stage1Test): 트랜잭션 격리에 따른 phantom read 테스트 표 작성
    
    * chore: 테스트 실행을 위한 인프라 의존성 추가
    
    * feat: findAll 기능 구현
    
    * chore: 테스트 실행을 위한 인프라 의존성 추가
    
    * feat: findByAccount 기능 구현
    
    * feat: update 기능 구현
    
    * feat: UserDao에 JdbcTemplate 구현 및 findById 리펙터링
    
    * feat: findAll 기능 구현
    
    * feat: findAll 기능을 jdbcTemplate로 수정
    
    * feat: UserDao 생성자에 DataSource 기반 초기화 추가
    
    * feat: insert 기능을 jdbcTemplate로 수정
    
    * feat: update 기능을 jdbcTemplate로 수정
    
    * feat: 메서드 가변 인자 처리 및 PreparedStatementSetter 적용
    
    * refactor: 메서드명 수정(findAll -> query)
    
    * feat: insertObject 반환값 수정(void -> int)
    
    * feat: 메서드 가변 인자 처리 및 PreparedStatementSetter 적용
    
    * refactor: insertObject 메서드 제거 및 update 메서드로 통합
    
    * feat: findByObject 및 query 메서드 구조 개선
    
    * refactor: 메서드명 수정(findByObject -> queryByObject)
    
    * feat: update 메서드의 반환 값 변경(void -> int)
    - 변경된 row 수 반환
    
    * feat: update 메서드 내 SQLException 예외 발생 시 로깅 추가
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -14,4 +19,60 @@ public class JdbcTemplate {
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
     }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public int update(String sql, Object... parameters) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+
+            createPreparedStatementSetter(parameters).setParameters(pstmt);
+
+            return pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            log.info("Fail update. sql={}, parameters={}", sql, parameters);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T queryByObject(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        List<T> list = query(sql, rowMapper, parameters);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.getFirst();
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        ResultSet rs;
+        List<T> list = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+
+            createPreparedStatementSetter(parameters).setParameters(pstmt);
+            rs = pstmt.executeQuery();
+
+            while (rs.next()) {
+                list.add(rowMapper.mapRow(rs, rs.getRow()));
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private PreparedStatementSetter createPreparedStatementSetter(Object... parameters) throws SQLException {
+        return pstmt -> {
+            for (int i = 0; i < parameters.length; i++) {
+                pstmt.setObject(i + 1, parameters[i]);
+            }
+        };
+    }
+}
+
+interface PreparedStatementSetter {
+    void setParameters(final PreparedStatement pstmt) throws SQLException;
 }
