c18bfbbb25b1c539dd378d42963f41413f1732bd
MinJun Choi <mj043000@naver.com>
2025-10-06 22:50:58 +0900

    test: UserServiceTest에서 @Disabled 제거 및 DataSourceConfig 적용
    - `@Disabled` 어노테이션 제거하여 테스트 활성화
    - `DataSourceConfig`를 사용해 `UserHistoryDao`와 `UserService` 초기화
    - `JdbcTemplate` 대신 `DataSource`를 사용하도록 수정
    - 트랜잭션 롤백 테스트에 DataSource 적용

--- app/src/test/java/com/techcourse/service/UserServiceTest.java
+++ app/src/test/java/com/techcourse/service/UserServiceTest.java
@@ -14,7 +14,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled
 class UserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
@@ -32,8 +31,9 @@ class UserServiceTest {
 
     @Test
     void testChangePassword() {
-        final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var dataSource = DataSourceConfig.getInstance();
+        final var userHistoryDao = new UserHistoryDao(dataSource);
+        final var userService = new UserService(dataSource, userDao, userHistoryDao);
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -46,9 +46,10 @@ class UserServiceTest {
 
     @Test
     void testTransactionRollback() {
+        final var dataSource = DataSourceConfig.getInstance();
         // 트랜잭션 롤백 테스트를 위해 mock으로 교체
         final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var userService = new UserService(dataSource, userDao, userHistoryDao);
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
