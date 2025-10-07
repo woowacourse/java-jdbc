a62e1de9b3d3c8e41e88dd28427ae75806065904
changui <122252160+changuii@users.noreply.github.com>
2025-10-07 16:27:43 +0900

    refactor: User의 account, email을 변경되지 않도록 final 키워드 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,12 +1,10 @@
 package com.interface21.jdbc.core;
 
-import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
