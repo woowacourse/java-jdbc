191baec0984a5ba7fb0a7bf0f8f3a052b8382520
goohong <goohong.chung@gmail.com>
2025-10-07 17:23:53 +0900

    refactor: JdbcTemplate에 try-with-resources 적용

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/DataAccessException.java
@@ -0,0 +1,8 @@
+package com.interface21.jdbc.core;
+
+public class DataAccessException extends RuntimeException {
+
+    public DataAccessException(final Throwable cause) {
+        super(cause);
+    }
+}
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,8 +1,6 @@
 package com.interface21.jdbc.core;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,55 +18,22 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(String sql, Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
+    public void update(final String sql, final Object... args) {
+        try (final var conn = dataSource.getConnection();
+             final var pstmt = setValues(conn.prepareStatement(sql), args)) {
             log.debug("query : {}", sql);
-
-            for (int index = 0; index < args.length; index++) {
-                pstmt.setString(index + 1, args[index].toString());
-            }
             pstmt.executeUpdate();
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
-    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            for (int index = 0; index < args.length; index++) {
-                pstmt.setString(index + 1, args[index].toString());
-            }
-
-            rs = pstmt.executeQuery();
-
+    public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final var conn = dataSource.getConnection();
+             final var pstmt = setValues(conn.prepareStatement(sql), args);
+             final var rs = pstmt.executeQuery()) {
             log.debug("query : {}", sql);
-
             if (rs.next()) {
                 return rowMapper.mapRow(rs);
             }
@@ -76,47 +41,15 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
     }
 
-    public <T> List<T> queryForList(String sql, RowMapper<T> rowMapper, Object... args) {
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        ResultSet rs = null;
-        List<T> result = new ArrayList<>();
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            for (int index = 0; index < args.length; index++) {
-                pstmt.setString(index + 1, args[index].toString());
-            }
-
-            rs = pstmt.executeQuery();
-
+    public <T> List<T> queryForList(final String sql, final RowMapper<T> rowMapper, final Object... args) {
+        try (final var conn = dataSource.getConnection();
+             final var pstmt = setValues(conn.prepareStatement(sql), args);
+             final var rs = pstmt.executeQuery()) {
+            final List<T> result = new ArrayList<>();
             log.debug("query : {}", sql);
-
             while (rs.next()) {
                 result.add(rowMapper.mapRow(rs));
             }
@@ -124,27 +57,13 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
         }
+    }
+
+    private PreparedStatement setValues(final PreparedStatement pstmt, final Object[] args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
+        return pstmt;
     }
 }
