3b028d64ea17aebab2d3dc1a3dfaf520e599ffee
rladmstn <oz33177351@gmail.com>
2025-10-07 23:02:00 +0900

    fix: jdbc 패키지 DataAccessException 제거

--- jdbc/src/main/java/com/interface21/jdbc/DataAccessException.java
+++
@@ -1,8 +0,0 @@
-package com.interface21.jdbc;
-
-public class DataAccessException extends RuntimeException {
-
-    public DataAccessException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}
