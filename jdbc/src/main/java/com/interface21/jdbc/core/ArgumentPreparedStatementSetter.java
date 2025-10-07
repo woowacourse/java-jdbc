c48dc150a8a52024c84a566f737b1fc3782da17a
junhaa <wooha34567@gmail.com>
2025-10-07 18:07:11 +0900

    feat: args 파라미터 수 검증 로직 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/ArgumentPreparedStatementSetter.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/ArgumentPreparedStatementSetter.java
@@ -1,5 +1,6 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
@@ -13,6 +14,14 @@ public class ArgumentPreparedStatementSetter implements PreparedStatementSetter 
 
     @Override
     public void setValues(PreparedStatement pstmt) throws SQLException {
+        int parameterCount = pstmt.getParameterMetaData().getParameterCount();
+
+        if (parameterCount != args.length) {
+            throw new DataAccessException(
+                String.format("Expected %d parameters, but got %d arguments", parameterCount, args.length)
+            );
+        }
+
         for (int i = 0; i < args.length; i++) {
             pstmt.setObject(i + 1, args[i]);
         }
