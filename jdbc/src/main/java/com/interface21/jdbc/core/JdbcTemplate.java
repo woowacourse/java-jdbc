ab070584c0dd05f0903a7d883d9735f481ced4ef
Gabin Choi <gazette9@naver.com>
2025-10-07 19:59:08 +0900

    [1단계 - JDBC 라이브러리 구현하기] 가콩(최가빈) 미션 제출합니다. (#1015)
    * chore: junit-platform-launcher 의존성 추가
    
    * test: 학습 테스트
    
    * feat: RowMapper interface와 JdbcTemplate 구현
    
    * refactor(UserDao): 기본 JDBC 코드를 JdbcTemplate으로 변경

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,59 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> mapper, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setPreparedStatement(pstmt, args);
+            ResultSet rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                return mapper.mapRow(rs, 1);
+            }
+            return null;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("DB 조회에 실패했습니다. :" + sql, e);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> mapper) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            final ResultSet rs = pstmt.executeQuery();
+
+            final List<T> result = new ArrayList<>();
+            int rowNum = 1;
+            while (rs.next()) {
+                result.add(mapper.mapRow(rs, rowNum++));
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RuntimeException("DB 조회에 실패했습니다. :" + sql, e);
+        }
+    }
+
+    public void update(String sql, Object... args) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setPreparedStatement(pstmt, args);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("DB 조회에 실패했습니다. :" + sql, e);
+        }
+    }
+
+    private void setPreparedStatement(PreparedStatement pstmt, Object[] args) {
+        int paramCount = 0;
+        try {
+            paramCount = pstmt.getParameterMetaData().getParameterCount();
+            for (int i = 0; i < args.length; i++) {
+                pstmt.setObject(i + 1, args[i]);
+            }
+        } catch (SQLException ex) {
+            throw new IllegalArgumentException("SQL 파라미터 개수 불일치: expected " + paramCount + ", actual " + args.length);
+        }
     }
 }
