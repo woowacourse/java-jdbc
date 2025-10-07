c47e443a118048bf0d671fc0d8d4cd0630cb3a07
Minuring <144205824+Minuring@users.noreply.github.com>
2025-10-07 15:55:58 +0900

    [1단계 - JDBC 라이브러리 구현하기] 포포(이민우) 미션 제출합니다. (#935)
    * JUnit Platform Launcher 의존성 추가
    
    * jdbctemplate 생성자에서 DB 연결 테스트
    
    * update, selectOne 쿼리 구현
    
    * findAll 쿼리 구현
    
    * findByAccount 구현
    
    * 불필요한 의존성 제거
    
    * UserDaoTest 원상복구
    
    * DataAccessException을 던지도록 변경
    
    * 중복되는 예외처리 코드 분리

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -11,7 +16,90 @@ public class JdbcTemplate {
 
     private final DataSource dataSource;
 
-    public JdbcTemplate(final DataSource dataSource) {
+    public JdbcTemplate(final DataSource dataSource) throws DataAccessException {
         this.dataSource = dataSource;
+        testConnection(dataSource);
+    }
+
+    public void update(String sql, Object... args) {
+        execute(PreparedStatement::execute, sql, args);
+    }
+
+    public <T> T selectOne(RowMapper<T> rowMapper, String sql, Object... args) {
+        StatementExecutor<T> stmtExecutor = pstmt -> {
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return rowMapper.mapRow(rs);
+            }
+            return null;
+        };
+
+        return execute(stmtExecutor, sql, args);
+    }
+
+    public <T> List<T> selectMulti(RowMapper<T> rowMapper, String sql, Object... args) {
+        StatementExecutor<List<T>> stmtExecutor = pstmt -> {
+            ResultSet rs = pstmt.executeQuery();
+            var list = new ArrayList<T>();
+            while (rs.next()) {
+                var mappedRow = rowMapper.mapRow(rs);
+                list.add(mappedRow);
+            }
+            return list;
+        };
+
+        return execute(stmtExecutor, sql, args);
+    }
+
+    private <T> T execute(StatementExecutor<T> stmtExecutor, String sql, Object... args) {
+        ResultSet rs = null;
+        try (var conn = dataSource.getConnection();
+             var pstmt = conn.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+
+            setParameters(pstmt, args);
+            return stmtExecutor.execute(pstmt);
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+
+        } finally {
+            closeResultSet(rs);
+        }
+    }
+
+    private void setParameters(PreparedStatement pstmt, Object... args) throws SQLException {
+        for (int i = 1; i <= args.length; i++) {
+            pstmt.setObject(i, args[i - 1]);
+        }
+    }
+
+    private void testConnection(DataSource dataSource) {
+        try (var connection = dataSource.getConnection()) {
+            var databaseProductName = connection.getMetaData().getDatabaseProductName();
+            log.info("Connection established to database : {}", databaseProductName);
+
+        } catch (NullPointerException e) {
+            log.error("Connection is null on dataSource {}", dataSource);
+            throw new DataAccessException(e);
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e.getCause());
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void closeResultSet(ResultSet rs) {
+        if (rs == null) {
+            return;
+        }
+
+        try {
+            rs.close();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 }
--- jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
+++ jdbc/src/test/java/com/interface21/jdbc/core/JdbcTemplateTest.java
@@ -1,5 +1,22 @@
 package com.interface21.jdbc.core;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
+
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class JdbcTemplateTest {
 
+    @Test
+    @DisplayName("DataSource와 연결에 실패하면 생성 시점에 예외가 발생한다.")
+    void constructor() throws Exception {
+        var dataSource = mock(DataSource.class);
+        doThrow(SQLException.class).when(dataSource).getConnection();
+
+        assertThatThrownBy(() -> new JdbcTemplate(dataSource));
+    }
 }
