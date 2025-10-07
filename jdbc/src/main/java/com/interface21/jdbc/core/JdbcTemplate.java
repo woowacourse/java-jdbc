d96aee656da876a98b21d58e8cbd080dd4beac4b
changui <122252160+changuii@users.noreply.github.com>
2025-10-07 16:34:31 +0900

    refactor: parameter 매핑 메서드 추출

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -20,16 +20,14 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(String sql, Object... paramters) {
+    public void update(String sql, Object... parameters) {
         try (
                 Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql);
         ) {
             log.debug("query : {}", sql);
 
-            for (int i = 1; i <= paramters.length; i++) {
-                pstmt.setObject(i, paramters[i - 1]);
-            }
+            setParameters(parameters, pstmt);
 
             pstmt.executeUpdate();
         } catch (SQLException e) {
@@ -43,9 +41,7 @@ public class JdbcTemplate {
                 Connection conn = dataSource.getConnection();
                 PreparedStatement pstmt = conn.prepareStatement(sql);
         ) {
-            for (int i = 1; i <= parameters.length; i++) {
-                pstmt.setObject(i, parameters[i - 1]);
-            }
+            setParameters(parameters, pstmt);
 
             log.debug("query : {}", sql);
 
@@ -63,9 +59,7 @@ public class JdbcTemplate {
                 Connection connection = dataSource.getConnection();
                 PreparedStatement pstmt = connection.prepareStatement(sql);
         ) {
-            for (int i = 1; i <= parameters.length; i++) {
-                pstmt.setObject(i, parameters[i - 1]);
-            }
+            setParameters(parameters, pstmt);
 
             List<T> results = new ArrayList<>();
             try (ResultSet rs = pstmt.executeQuery()) {
@@ -79,6 +73,12 @@ public class JdbcTemplate {
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
+        }
+    }
+
+    private void setParameters(Object[] parameters, PreparedStatement pstmt) throws SQLException {
+        for (int i = 1; i <= parameters.length; i++) {
+            pstmt.setObject(i, parameters[i - 1]);
         }
     }
 }
