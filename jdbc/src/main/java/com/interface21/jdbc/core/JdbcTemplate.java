ec2f3b5b2d7595068e70a85884203f919769cd31
minSsan <msp1125@hanyang.ac.kr>
2025-10-08 17:12:22 +0900

    feat: JdbcTemplate 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -4,6 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,54 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public int update(String sql, Object... params) {
+        try (final var conn = dataSource.getConnection();
+             final var pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            bindParams(pstmt, params);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    public <T> T queryForObject(String sql, Function<ResultSet, T> rowMapper, Object... params) {
+        final List<T> results = query(sql, rowMapper, params);
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results.getFirst();
+    }
+
+    public <T> List<T> query(String sql, Function<ResultSet, T> rowMapper, Object... params) {
+        final var result = new ArrayList<T>();
+        try (final var conn = dataSource.getConnection();
+             final var pstmt = conn.prepareStatement(sql)) {
+
+            log.debug("query : {}", sql);
+            bindParams(pstmt, params);
+            try (final var rs = pstmt.executeQuery()) {
+                while (rs.next()) {
+                    result.add(rowMapper.apply(rs));
+                }
+            }
+            return result;
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    private void bindParams(PreparedStatement pstmt, Object... params) throws SQLException {
+        if (params == null) {
+            return;
+        }
+        for (int i = 0; i < params.length; i++) {
+            pstmt.setObject(i + 1, params[i]);
+        }
     }
 }
