02ea725447e48e5db2853ed2a9d6cb6329237c56
minSsan <msp1125@hanyang.ac.kr>
2025-10-09 00:56:37 +0900

    refactor: PreparedStatementSetter 적용

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -1,6 +1,7 @@
 package com.techcourse.dao;
 
 import com.interface21.jdbc.core.JdbcTemplate;
+import com.interface21.jdbc.core.PreparedStatementSetter;
 import com.interface21.jdbc.core.RowMapper;
 import com.techcourse.domain.User;
 import org.slf4j.Logger;
@@ -25,27 +26,51 @@ public class UserDao {
 
     public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
+        jdbcTemplate.update(
+                sql,
+                getPreparedStatementSetter(
+                        user.getAccount(),
+                        user.getPassword(),
+                        user.getEmail()
+                ));
     }
 
     public void update(final User user) {
         final String sql = "update users set account = ?, password = ?, email = ? where id = ?";
-        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
+        jdbcTemplate.update(
+                sql,
+                getPreparedStatementSetter(
+                        user.getAccount(),
+                        user.getPassword(),
+                        user.getEmail(),
+                        user.getId()
+                ));
     }
 
     public List<User> findAll() {
         final String sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(sql, getRowMapper());
+        return jdbcTemplate.query(sql, getPreparedStatementSetter(), getRowMapper());
     }
 
     public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, getRowMapper(), id);
+        return jdbcTemplate.queryForObject(sql, getPreparedStatementSetter(id), getRowMapper());
     }
 
     public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(sql, getRowMapper(), account);
+        return jdbcTemplate.queryForObject(sql, getPreparedStatementSetter(account), getRowMapper());
+    }
+
+    private static PreparedStatementSetter getPreparedStatementSetter(Object... params) {
+        return (pstmt) -> {
+            if (params == null) {
+                return;
+            }
+            for (int i = 0; i < params.length; i++) {
+                pstmt.setObject(i + 1, params[i]);
+            }
+        };
     }
 
     private static RowMapper<User> getRowMapper() {
