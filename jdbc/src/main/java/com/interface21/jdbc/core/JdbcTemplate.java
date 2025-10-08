9b2b442395ba6aa2b99b003c5818a319d879560c
eunseongu <86725408+eunseongu@users.noreply.github.com>
2025-10-08 22:47:31 +0900

    [1단계 - JDBC 라이브러리 구현하기] 하루(구은선) 미션 제출합니다. (#1021)
    * chore: Poling 오타 수정 (#928)
    
    * chore: 의존성 추가
    
    * feat: RowMapper 구현
    
    * feat: IncorrectResultSizeException 구현
    
    * feat: JdbcTemplate 구현
    
    * refactor: UserDao에서 jdbcTemplate을 사용하도록 변경
    
    * refactor: try-with-resources 사용하도록 변경
    
    * refactor: queryForObject 메서드에서 Optional을 반환하도록 변경
    
    * refactor: Optional 처리하도록 변경
    
    * test: Optional 처리 및 findByAccount 테스트 통과하도록 데이터 수정
    
    * refactor: 컨벤션 통일
    
    * refactor: DataAccessException를 상속하도록 변경
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,16 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.IncorrectResultSizeException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,60 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... parameters) {
+        execute(sql, parameters);
+    }
+
+    private void execute(String sql, Object... parameters) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement pstmt = connection.prepareStatement(sql);
+        ) {
+            log.debug("query : {}", sql);
+            setParameters(pstmt, parameters);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement pstmt = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            setParameters(pstmt, parameters);
+            try (ResultSet rs = pstmt.executeQuery()) {
+                final List<T> results = new ArrayList<>();
+                while (rs.next()) {
+                    results.add(rowMapper.mapRow(rs));
+                }
+                return results;
+            }
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> Optional<T> queryForObject(String sql, RowMapper<T> rowMapper, Object... parameters) {
+        List<T> results = query(sql, rowMapper, parameters);
+        if (results.isEmpty()) {
+            return Optional.empty();
+        }
+        if (results.size() != 1) {
+            throw new IncorrectResultSizeException(1, results.size());
+        }
+        return Optional.ofNullable(results.getFirst());
+    }
+
+    private void setParameters(PreparedStatement pstmt, Object... parameters) throws SQLException {
+        for (int i = 0; i < parameters.length; i++) {
+            pstmt.setObject(i + 1, parameters[i]);
+        }
     }
 }
