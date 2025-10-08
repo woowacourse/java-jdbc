e8359484f6e76f3a9a1d4bcfa09367d8d6a7f931
AppleMint98 <applemint9821@gmail.com>
2025-10-08 18:12:24 +0900

    feat: 0건시와 2건 이상시의 예외 구분

--- jdbc/src/main/java/com/interface21/jdbc/core/QueryObjectCallback.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/QueryObjectCallback.java
@@ -1,5 +1,6 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.EmptyResultDataAccessException;
 import com.interface21.dao.IncorrectResultSizeDataAccessException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -14,10 +15,14 @@ public class QueryObjectCallback<T> implements JdbcCallback<T> {
     @Override
     public T call(PreparedStatement pstmt) throws SQLException {
         try (ResultSet rs = pstmt.executeQuery()) {
-            if (rs.next()) {
-                return rowMapper.call(rs);
+            if (!rs.next()) {
+                throw new EmptyResultDataAccessException("No data found");
             }
-            throw new IncorrectResultSizeDataAccessException("Data size is incorrect");
+            T result = rowMapper.call(rs);
+            if (rs.next()) {
+                throw new IncorrectResultSizeDataAccessException("Data size is incorrect");
+            }
+            return result;
         }
     }
 }
