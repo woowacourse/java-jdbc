6e4f792f109836244d322b3bf1e6700de826c0a1
byesol <bsj5967@naver.com>
2025-10-09 21:05:49 +0900

    refactor: RowMapper 인터페이스에 제네릭 타입 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/RowMapper.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/RowMapper.java
@@ -3,6 +3,6 @@ package com.interface21.jdbc.core;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public interface RowMapper {
-    Object mapRow(ResultSet rs, int rowNum) throws SQLException;
+public interface RowMapper<T> {
+    T mapRow(ResultSet rs, int rowNum) throws SQLException;
 }
