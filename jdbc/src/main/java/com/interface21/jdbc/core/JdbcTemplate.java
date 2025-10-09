d3893a26d1b356d29708d65a4e6d05ab4ebf5280
YuyoungRhee <bbclara@naver.com>
2025-10-09 15:29:49 +0900

    refactor: queryForObject에서 query를 재활용하도록 중복코드 개선

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -37,25 +37,16 @@ public class JdbcTemplate {
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        log.debug("query : {}", sql);
 
-            log.debug("query : {}", sql);
-
-            for (int i = 0; i < params.length; i++) {
-                pstmt.setObject(i + 1, params[i]);
-            }
-
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return rowMapper.mapRow(rs);
-                }
-                return null;
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
+        List<T> results = query(sql, rowMapper, params);
+        if (results.isEmpty()) {
+            throw new RuntimeException("쿼리 실행 결과가 존재하지 않습니다.");
         }
+        if (results.size() > 1) {
+            throw new RuntimeException("쿼리 실행 결과가 2개 이상입니다.");
+        }
+        return results.get(0);
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
