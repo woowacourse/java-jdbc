5f5d83632e623fe48352e9f62a47e599616593ce
RaZel713 <razel7130@gmail.com>
2025-10-09 21:05:36 +0900

    refactor: 중복된 로직을 메서드로 분리

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -1,14 +1,13 @@
 package com.techcourse.dao;
 
-import com.techcourse.domain.UserHistory;
 import com.interface21.jdbc.core.JdbcTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
+import com.techcourse.domain.UserHistory;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserHistoryDao {
 
@@ -35,13 +34,7 @@ public class UserHistoryDao {
 
             log.debug("query : {}", sql);
 
-            pstmt.setLong(1, userHistory.getUserId());
-            pstmt.setString(2, userHistory.getAccount());
-            pstmt.setString(3, userHistory.getPassword());
-            pstmt.setString(4, userHistory.getEmail());
-            pstmt.setObject(5, userHistory.getCreatedAt());
-            pstmt.setString(6, userHistory.getCreateBy());
-            pstmt.executeUpdate();
+            setUserHistoryParametersAndExecute(userHistory, pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
@@ -50,13 +43,15 @@ public class UserHistoryDao {
                 if (pstmt != null) {
                     pstmt.close();
                 }
-            } catch (SQLException ignored) {}
+            } catch (SQLException ignored) {
+            }
 
             try {
                 if (conn != null) {
                     conn.close();
                 }
-            } catch (SQLException ignored) {}
+            } catch (SQLException ignored) {
+            }
         }
     }
 
@@ -66,16 +61,23 @@ public class UserHistoryDao {
         try (var pstmt = connection.prepareStatement(sql)) {
             log.debug("query : {}", sql);
 
-            pstmt.setLong(1, userHistory.getUserId());
-            pstmt.setString(2, userHistory.getAccount());
-            pstmt.setString(3, userHistory.getPassword());
-            pstmt.setString(4, userHistory.getEmail());
-            pstmt.setObject(5, userHistory.getCreatedAt());
-            pstmt.setString(6, userHistory.getCreateBy());
-            pstmt.executeUpdate();
+            setUserHistoryParametersAndExecute(userHistory, pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
+    }
+
+    private void setUserHistoryParametersAndExecute(
+            final UserHistory userHistory, final PreparedStatement pstmt
+    ) throws SQLException {
+        pstmt.setLong(1, userHistory.getUserId());
+        pstmt.setString(2, userHistory.getAccount());
+        pstmt.setString(3, userHistory.getPassword());
+        pstmt.setString(4, userHistory.getEmail());
+        pstmt.setObject(5, userHistory.getCreatedAt());
+        pstmt.setString(6, userHistory.getCreateBy());
+
+        pstmt.executeUpdate();
     }
 }
