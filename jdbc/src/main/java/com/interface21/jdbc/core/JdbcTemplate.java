2683259443c7b73615653157ba114dc03b408f86
sooyang <86940801+ppparkta@users.noreply.github.com>
2025-10-09 18:22:04 +0900

    [1단계 - JDBC 라이브러리 구현하기] 수양(양수빈) 미션 제출합니다. (#1054)
    * DriverManager 오타 수정 (#960)
    
    * feat: rowMapper 인터페이스 추가
    
    * feat: command 처리 부분 구현
    
    * build: 테스트 빌드 안 되는 문제 해결
    
    * feat: query 처리 부분 구현
    
    * refactor: 보일러플레이트 jdbcTemplate으로 교체
    
    * feat: catch 시 로그 추가
    
    * feat: sql 디버깅용 로그 추가
    
    * refactor: 반환 예외를 DataAccessException으로 수정
    
    * refactor: 변수명 통일
    
    * refactor: 들여쓰기 깊은 라인 메서드 분리
    
    * refactor: 들여쓰기 깊은 라인 메서드 분리
    
    ---------
    
    Co-authored-by: 슥 <ime.rokz@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,15 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +19,59 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... params) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preStmt = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            initParameters(params, preStmt);
+            preStmt.executeUpdate();
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private void initParameters(Object[] params, PreparedStatement preStmt) throws SQLException {
+        for (int i = 0; i < params.length; i++) {
+            preStmt.setObject(i + 1, params[i]);
+        }
+    }
+
+    public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (
+                Connection connection = dataSource.getConnection();
+                PreparedStatement preStmt = connection.prepareStatement(sql)
+        ) {
+            log.debug("query : {}", sql);
+            initParameters(params, preStmt);
+            return executeQuery(rowMapper, preStmt);
+
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> List<T> executeQuery(RowMapper<T> rowMapper, PreparedStatement preStmt) throws SQLException {
+        try (ResultSet rs = preStmt.executeQuery()) {
+            List<T> results = new ArrayList<>();
+            while (rs.next()) {
+                results.add(rowMapper.mapRow(rs));
+            }
+            return results;
+        }
+    }
+
+    public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
+        List<T> results = query(sql, rowMapper, params);
+        if (results.isEmpty()) {
+            return null;
+        }
+        return results.getFirst();
     }
 }
