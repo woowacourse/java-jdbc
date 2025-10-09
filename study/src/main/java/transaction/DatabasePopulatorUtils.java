4903588f2baca0a8c6e4dd062cbe123e0e8e0239
songsunkook <songsunkook@gmail.com>
2025-10-09 14:13:06 +0900

    fix: url 불러오기 실패 버그 수정

--- study/src/main/java/transaction/DatabasePopulatorUtils.java
+++ study/src/main/java/transaction/DatabasePopulatorUtils.java
@@ -1,15 +1,17 @@
 package transaction;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DatabasePopulatorUtils {
 
@@ -20,12 +22,12 @@ public class DatabasePopulatorUtils {
         Statement statement = null;
         try {
             final var url = DatabasePopulatorUtils.class.getClassLoader().getResource("schema.sql");
-            final var file = new File(url.getFile());
+            final var file = new File(url.toURI());
             final var sql = Files.readString(file.toPath());
             connection = dataSource.getConnection();
             statement = connection.createStatement();
             statement.execute(sql);
-        } catch (NullPointerException | IOException | SQLException e) {
+        } catch (NullPointerException | IOException | SQLException | URISyntaxException e) {
             log.error(e.getMessage(), e.getCause());
         } finally {
             try {
