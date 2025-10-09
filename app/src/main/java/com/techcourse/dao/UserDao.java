251d0849a80d820321121b51329c5cc148c1665c
posty <ykmxxi97@gmail.com>
2025-10-09 20:14:55 +0900

    [2단계 - 리팩터링] 포스티(윤강민) 미션 제출합니다. (#1064)
    * feat: 콜백 적용으로 중복 코드 제거
    
    * refactor: JdbcTemplate.update 반환 타입 변경
    
    * refactor: sql String Templates 적용
    
    * refactor: 쿼리 실행 List 반환 메서드명 변경
    
    * refactor: 쿼리 실행 결과 데이터 추출 중복 제거

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -28,27 +28,47 @@ public class UserDao {
     }
 
     public void insert(final User user) {
-        final var sql = "insert into users (account, password, email) values (?, ?, ?)";
+        final var sql = """
+            insert into users (account, password, email)
+            values (?, ?, ?)
+            """;
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
     public void update(final User user) {
-        final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
+        final var sql = """
+            update users
+            set account = ?,
+                password = ?,
+                email = ?
+            where id = ?
+            """;
         jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
     public List<User> findAll() {
-        final var sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(sql, USER_ROW_MAPPER);
+        final var sql = """
+            select id, account, password, email
+            from users
+            """;
+        return jdbcTemplate.queryForList(sql, USER_ROW_MAPPER);
     }
 
     public User findById(final Long id) {
-        final var sql = "select id, account, password, email from users where id = ?";
+        final var sql = """
+            select id, account, password, email
+            from users
+            where id = ?
+            """;
         return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, id);
     }
 
     public User findByAccount(final String account) {
-        final var sql = "select id, account, password, email from users where account = ?";
+        final var sql = """
+            select id, account, password, email
+            from users
+            where account = ?
+            """;
         return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, account);
     }
 }
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -24,57 +24,53 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    public void update(final String sql, final Object... parameters) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-
-            log.debug("query : {}", sql);
-            setParameters(pstmt, parameters);
-
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e.getMessage(), e);
-        }
+    public int update(final String sql, final Object... parameters) {
+        return execute(sql, PreparedStatement::executeUpdate, parameters);
     }
 
     public <T> T queryForObject(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)) {
-
-            log.debug("query : {}", sql);
-            setParameters(pstmt, parameters);
-
-            try (ResultSet resultSet = pstmt.executeQuery()) {
-                if (!resultSet.next()) {
-                    throw new DataAccessException("조회 결과가 존재하지 않습니다.");
-                }
-                T result = rowMapper.mapRow(resultSet, parameters.length);
-                if (resultSet.next()) {
-                    throw new DataAccessException("조회 결과가 2개 이상입니다.");
-                }
-                return result;
+        return executeUsingExtractor(sql, resultSet -> {
+            if (!resultSet.next()) {
+                throw new DataAccessException("조회 결과가 존재하지 않습니다.");
             }
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e.getMessage(), e);
-        }
+            T result = rowMapper.mapRow(resultSet, 0);
+            if (resultSet.next()) {
+                throw new DataAccessException("조회 결과가 2개 이상입니다.");
+            }
+            return result;
+        }, parameters);
     }
 
-    public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
+    public <T> List<T> queryForList(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
+        return executeUsingExtractor(sql, resultSet -> {
+            List<T> results = new ArrayList<>();
+            int rowNum = 0;
+            while (resultSet.next()) {
+                results.add(rowMapper.mapRow(resultSet, rowNum++));
+            }
+            return results;
+        }, parameters);
+    }
+
+    private <T> T executeUsingExtractor(
+        final String sql,
+        final ResultSetExtractor<T> extractor,
+        final Object... parameters
+    ) {
+        return execute(sql, pstmt -> {
+            try (ResultSet resultSet = pstmt.executeQuery()) {
+                return extractor.extractData(resultSet);
+            }
+        }, parameters);
+    }
+
+    private <T> T execute(final String sql, final PreparedStatementCallback<T> callback, final Object... parameters) {
         try (Connection conn = dataSource.getConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             log.debug("query : {}", sql);
             setParameters(pstmt, parameters);
-
-            try (ResultSet resultSet = pstmt.executeQuery()) {
-                List<T> results = new ArrayList<>();
-                while (resultSet.next()) {
-                    results.add(rowMapper.mapRow(resultSet, parameters.length));
-                }
-                return results;
-            }
+            return callback.doInPreparedStatement(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e.getMessage(), e);
