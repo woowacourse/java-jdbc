f563f0b199c8f5ceaa6f4d4b95203c991537230b
threepebbles <threepebbles@naver.com>
2025-10-09 16:55:31 +0900

    feat: TransactionSynchronizationManager 기능 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,44 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
 
+// 하나의 트랜잭션 = 하나의 Connection을 보장해주는 역할
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+        return map.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            map = new HashMap<>();
+            resources.set(map);
+        }
+        map.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+        Connection connection = map.remove(key);
+        if (map.isEmpty()) {
+            resources.remove();
+        }
+        return connection;
     }
 }
