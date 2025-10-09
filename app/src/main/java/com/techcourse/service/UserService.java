0564ba0c3d50814303f461e4b6c7dfe753f793a0
jbilee <julishahn@gmail.com>
2025-10-09 12:09:16 +0900

    feat: 트랜잭션 지원

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,13 @@
 package com.techcourse.service;
 
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.core.ConnectionManager;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
 
 public class UserService {
 
@@ -28,9 +32,21 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        Connection connection = null;
+        try {
+            connection = ConnectionManager.startTransaction(DataSourceConfig.getInstance());
+
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+            ConnectionManager.commitTransaction(connection);
+        } catch (Exception e) {
+            if (connection != null) {
+                ConnectionManager.rollbackTransaction(connection);
+            }
+            throw new DataAccessException(e);
+        }
     }
 }
--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -61,7 +61,7 @@ class UserDaoTest {
         final var user = userDao.findById(1L);
         user.changePassword(newPassword);
 
-        userDao.update(user);
+        userDao.update(null, user);
 
         final var actual = userDao.findById(1L);
 
