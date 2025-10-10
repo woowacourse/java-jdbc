a8088d803bed49d218aabcf7490b4a7426ba2a6c
CheChe903 <cheche903@naver.com>
2025-10-11 00:18:16 +0900

    feat: 오류 제거

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -16,7 +16,8 @@ public class UserService {
     }
 
     public User findById(final long id) {
-        return userDao.findById(id);
+        return userDao.findById(id)
+                .orElseThrow(() -> new IllegalStateException("멤버가 존재하지 않습니다."));
     }
 
     public void insert(final User user) {
