39bd0903362f471e1f40a463e48911da7d4ae624
KJungW <128235227+KJungW@users.noreply.github.com>
2025-10-10 22:34:49 +0900

    [1단계 - JDBC 라이브러리 구현하기] 랜디(김정우) 미션 제출합니다. (#1030)
    * chore: Poling 오타 수정 (#928)
    
    * chore: 테스트 관련 의존성 추가
    
    * feat: JdbcTemplate 구현
    
    * refactor: UserDao에 JdbcTemplate 적용
    
    * refactor: UserHistoryDao에 JdbcTemplate 적용
    
    * refactor: 일부 예외처리 수정
    
    * feat: 스레드풀 학습 진행
    
    * refactor: 단건 조회 예외처리 추가
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>
    Co-authored-by: kjungw <sagakkb@gmail.com>

--- study/src/test/java/connectionpool/stage2/Stage2Test.java
+++ study/src/test/java/connectionpool/stage2/Stage2Test.java
@@ -1,20 +1,20 @@
 package connectionpool.stage2;
 
+import static com.zaxxer.hikari.util.UtilityElf.quietlySleep;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.zaxxer.hikari.HikariDataSource;
 import com.zaxxer.hikari.pool.HikariPool;
 import connectionpool.DataSourceConfig;
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.sql.DataSource;
-import java.lang.reflect.Field;
-import java.sql.Connection;
-
-import static com.zaxxer.hikari.util.UtilityElf.quietlySleep;
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Stage2Test {
@@ -22,10 +22,9 @@ class Stage2Test {
     private static final Logger log = LoggerFactory.getLogger(Stage2Test.class);
 
     /**
-     * spring boot에서 설정 파일인 application.yml를 사용하여 DataSource를 설정할 수 있다.
-     * 하지만 DataSource를 여러 개 사용하거나 세부 설정을 하려면 빈을 직접 생성하는 방법을 사용한다.
-     * DataSourceConfig 클래스를 찾아서 어떻게 빈으로 직접 생성하는지 확인해보자.
-     * 그리고 아래 DataSource가 직접 생성한 빈으로 주입 받았는지 getPoolName() 메서드로 확인해보자.
+     * spring boot에서 설정 파일인 application.yml를 사용하여 DataSource를 설정할 수 있다. 하지만 DataSource를 여러 개 사용하거나 세부 설정을 하려면 빈을 직접 생성하는
+     * 방법을 사용한다. DataSourceConfig 클래스를 찾아서 어떻게 빈으로 직접 생성하는지 확인해보자. 그리고 아래 DataSource가 직접 생성한 빈으로 주입 받았는지 getPoolName()
+     * 메서드로 확인해보자.
      */
     @Autowired
     private DataSource dataSource;
@@ -56,6 +55,14 @@ class Stage2Test {
         assertThat(hikariDataSource.getPoolName()).isEqualTo("gugu");
     }
 
+    // Connection Leak Detection 테스트 (Connection Leak Detection 로그 출력 확인)
+    @Test
+    void test2() throws SQLException {
+        try (Connection ignored = dataSource.getConnection()) {
+            quietlySleep(3000);
+        }
+    }
+
     // 데이터베이스에 연결만 하는 메서드. 커넥션 풀에 몇 개의 연결이 생기는지 확인하는 용도.
     private Runnable getConnection() {
         return () -> {
@@ -71,8 +78,7 @@ class Stage2Test {
     }
 
     // 학습 테스트를 위해 HikariPool을 추출
-    public static HikariPool getPool(final HikariDataSource hikariDataSource)
-    {
+    public static HikariPool getPool(final HikariDataSource hikariDataSource) {
         try {
             Field field = hikariDataSource.getClass().getDeclaredField("pool");
             field.setAccessible(true);
