72ee5a73139d464ee3b48f332864236ec4498ff0
Dompoo <dlckdrms0517@gmail.com>
2025-10-10 21:59:49 +0900

    refactor: UserService에 템플릿 사용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,6 +1,7 @@
 package com.techcourse.service;
 
 import com.interface21.jdbc.core.TransactionManager;
+import com.interface21.jdbc.core.TransactionTemplate;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -10,12 +11,12 @@ public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
-    private final TransactionManager transactionManager;
+    private final TransactionTemplate transactionTemplate;
 
     public UserService(UserDao userDao, UserHistoryDao userHistoryDao, TransactionManager transactionManager) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
-        this.transactionManager = transactionManager;
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
     }
 
     public User findById(final long id) {
@@ -27,16 +28,11 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        transactionManager.start();
-        try {
+        transactionTemplate.run(() -> {
             final var user = findById(id);
             user.changePassword(newPassword);
             userDao.update(user);
             userHistoryDao.log(new UserHistory(user, createBy));
-            transactionManager.commit();
-        } catch (Exception e) {
-            transactionManager.rollback();
-            throw e;
-        }
+        });
     }
 }
