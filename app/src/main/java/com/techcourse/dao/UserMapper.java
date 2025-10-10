71faf595d6c4fae8d4f3f5eaf4e07c85e91ad643
taek2222 <oot309@daum.net>
2025-10-10 11:35:03 +0900

    refactor: 컬럼 순서가 아닌 명칭으로 구분

--- app/src/main/java/com/techcourse/dao/UserMapper.java
+++ app/src/main/java/com/techcourse/dao/UserMapper.java
@@ -10,9 +10,9 @@ public class UserMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet rs) throws SQLException {
         return new User(
-                rs.getLong(1),
-                rs.getString(2),
-                rs.getString(3),
-                rs.getString(4));
+                rs.getLong("id"),
+                rs.getString("account"),
+                rs.getString("password"),
+                rs.getString("email"));
     }
 }
