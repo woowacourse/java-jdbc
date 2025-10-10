30bd0d1fb732b46b025c01215b10d6ce95e37ea3
Firedrago95 <yonghwa95@naver.com>
2025-10-10 17:43:54 +0900

    fix: connection 덮어쓰기 방지 로직 구현

--- jdbc/src/main/java/com/interface21/jdbc/core/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/TransactionSynchronizationManager.java
@@ -7,10 +7,12 @@ public class TransactionSynchronizationManager {
     private static final ThreadLocal<Connection> connectionThreadLocal = new ThreadLocal<>();
 
     public static void setConnection(final Connection connection) {
-        connectionThreadLocal.set(connection);
+        if (!hasConnection()) {
+            connectionThreadLocal.set(connection);
+        }
     }
 
-    public static boolean hasConnection() {
+    private static boolean hasConnection() {
         return connectionThreadLocal.get() != null;
     }
 
