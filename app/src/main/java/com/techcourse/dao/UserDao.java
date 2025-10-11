cf5d02128b8d2e81b05a4dee08f064fabbb8b3f2
jhpark1227 <junhyeok0246@naver.com>
2025-10-11 15:46:45 +0900

    refactor: RowMapper의 구현을 람다식으로 변경

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -3,8 +3,6 @@ package com.techcourse.dao;
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.interface21.jdbc.core.RowMapper;
 import com.techcourse.domain.User;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
@@ -12,16 +10,13 @@ import org.slf4j.LoggerFactory;
 
 public class UserDao {
 
-    private static final RowMapper<User> USER_ROW_MAPPER = new RowMapper<>() {
-        @Override
-        public User map(ResultSet resultSet) throws SQLException {
-            return new User(
-                    resultSet.getLong("id"),
-                    resultSet.getString("account"),
-                    resultSet.getString("password"),
-                    resultSet.getString("email"));
-        }
-    };
+    private static final RowMapper<User> USER_ROW_MAPPER = resultSet -> new User(
+            resultSet.getLong("id"),
+            resultSet.getString("account"),
+            resultSet.getString("password"),
+            resultSet.getString("email")
+    );
+
     private static final Logger log = LoggerFactory.getLogger(UserDao.class);
 
     private final JdbcTemplate jdbcTemplate;
