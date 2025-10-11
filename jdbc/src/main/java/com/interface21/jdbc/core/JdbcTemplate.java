ce2c024e0fafd102d946285cf7c28437e207ecd8
kali <111180367+2Jin1031@users.noreply.github.com>
2025-10-11 13:43:38 +0900

    [2단계 - 리팩터링] 칼리(이진) 미션 제출합니다. (#1081)
    * chore: Poling 오타 수정 (#928)
    
    * refactor: 메서드명 수정
    
    * refactor: 메서드명 수정 (setParameters -> setValues)
    
    * refactor: RowMapper 메서드의 두번째 인자 제거
    
    * refactor: 내부 인터페이스를 독립 파일로 분리
    
    * refactor: 변경된 RowMapper 인터페이스의 시그니처에 맞춰 수정
    
    * refactor: query 메서드에서 ResultSet을 try-with-resources 구문으로 포함하도록 수정하여 자원 누수 방지
    
    * refactor: 불필요한 throws 제거
    
    * refactor: PreparedStatement 실행 공통 로직을 execute 메서드로 추출
    
    * feat: @FunctionalInterface 추가
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,5 +1,6 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -25,17 +26,7 @@ public class JdbcTemplate {
     }
 
     public int update(String sql, Object... parameters) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement pstmt = connection.prepareStatement(sql)) {
-
-            createPreparedStatementSetter(parameters).setParameters(pstmt);
-
-            return pstmt.executeUpdate();
-
-        } catch (SQLException e) {
-            log.info("Fail update. sql={}, parameters={}", sql, parameters);
-            throw new RuntimeException(e);
-        }
+        return execute(sql, PreparedStatement::executeUpdate, parameters);
     }
 
     public <T> T queryByObject(String sql, RowMapper<T> rowMapper, Object... parameters) {
@@ -47,32 +38,33 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... parameters) {
-        ResultSet rs;
-        List<T> list = new ArrayList<>();
+        return execute(sql, pstmt -> {
+            List<T> list = new ArrayList<>();
+            try (ResultSet rs = pstmt.executeQuery()) {
+                while (rs.next()) list.add(rowMapper.mapRow(rs));
+            }
+            return list;
+        }, parameters);
+    }
+
+    private <T> T execute(String sql, PreparedStatementCallback<T> action, Object... parameters) {
         try (Connection connection = dataSource.getConnection();
              PreparedStatement pstmt = connection.prepareStatement(sql)) {
 
-            createPreparedStatementSetter(parameters).setParameters(pstmt);
-            rs = pstmt.executeQuery();
+            createPreparedStatementSetter(parameters).setValues(pstmt);
+            return action.doInPreparedStatement(pstmt);
 
-            while (rs.next()) {
-                list.add(rowMapper.mapRow(rs, rs.getRow()));
-            }
-            return list;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            log.error("Database operation failed. sql={}", sql, e);
+            throw new DataAccessException("Failed to execute SQL", e);
         }
     }
 
-    private PreparedStatementSetter createPreparedStatementSetter(Object... parameters) throws SQLException {
+    private PreparedStatementSetter createPreparedStatementSetter(Object... parameters) {
         return pstmt -> {
             for (int i = 0; i < parameters.length; i++) {
                 pstmt.setObject(i + 1, parameters[i]);
             }
         };
     }
-}
-
-interface PreparedStatementSetter {
-    void setParameters(final PreparedStatement pstmt) throws SQLException;
 }
