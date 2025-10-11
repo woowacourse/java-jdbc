9d6f8d05f7ec325c463f49a261dc5fc6216ca9f9
minSsan <msp1125@hanyang.ac.kr>
2025-10-11 20:57:27 +0900

    feat: UserService#changePassword 트랜잭션 내에서 실행하도록 변경 - 커넥션 공유

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,15 @@
 package com.techcourse.service;
 
+import com.interface21.jdbc.CannotGetJdbcConnectionException;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserService {
 
@@ -23,10 +29,38 @@ public class UserService {
         userDao.insert(user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(
+            final long id,
+            final String newPassword,
+            final String createBy
+    ) {
+        final var connection = getConnection();
+        try {
+            connection.setAutoCommit(false);
+
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ignored) {}
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException ignored) {}
+        }
+    }
+
+    private Connection getConnection() {
+        final DataSource dataSource = DataSourceConfig.getInstance();
+        try {
+            return dataSource.getConnection();
+        } catch (SQLException e) {
+            throw new CannotGetJdbcConnectionException(e.getMessage(), e);
+        }
     }
 }
