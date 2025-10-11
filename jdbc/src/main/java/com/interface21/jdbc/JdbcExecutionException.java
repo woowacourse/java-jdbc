1f2bb1f856dc41bf5aae90815cfa79d95115539d
시원 <cnc4750@inha.edu>
2025-10-12 01:59:36 +0900

    refactor: 예외 메시지 수정 및 예외 원인에 대한 정보 추가

--- jdbc/src/main/java/com/interface21/jdbc/JdbcExecutionException.java
+++ jdbc/src/main/java/com/interface21/jdbc/JdbcExecutionException.java
@@ -1,8 +1,12 @@
 package com.interface21.jdbc;
 
 public class JdbcExecutionException extends RuntimeException {
-    
+
     public JdbcExecutionException(String message) {
         super(message);
+    }
+
+    public JdbcExecutionException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -56,7 +56,7 @@ public class JdbcTemplate {
             return executor.execute(pstmt);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
-            throw new JdbcExecutionException(String.format("\"%s\" 쿼리 실행 중 리소스 반환에 실패했습니다.", sql));
+            throw new JdbcExecutionException(String.format("\"%s\" 쿼리 실행 중 오류가 발생했습니다.", sql), e);
         }
     }
 
