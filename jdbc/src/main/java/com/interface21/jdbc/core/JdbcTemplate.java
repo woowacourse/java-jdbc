f73cbae287f58d4fe14d38b018e8b3e9fba2306c
jjunh0 <94045552+jjunh0@users.noreply.github.com>
2025-10-11 18:07:47 +0900

    [1단계 - JDBC 라이브러리 구현하기] 두리(김준호) 미션 제출합니다. (#1092)
    * DriverManager 오타 수정 (#960)
    
    * chore: junit 의존성 문제 해결
    
    * feat: todo 구현
    
    * refactor: jdbcTemplate을 이용한 boilerPlate 코드 제거
    
    ---------
    
    Co-authored-by: 슥 <ime.rokz@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,72 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(String sql, Object... params) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+        ) {
+            setParams(params, pstmt);
+            pstmt.executeUpdate();
+            log.debug("query : {}", sql);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> List<T> find(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+        ) {
+            setParams(params, pstmt);
+            log.debug("query : {}", sql);
+            return executeQuery(pstmt, rowMapper);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    public <T> T findOne(String sql, RowMapper<T> rowMapper, Object... params) {
+        try (
+                Connection conn = dataSource.getConnection();
+                PreparedStatement pstmt = conn.prepareStatement(sql);
+        ) {
+            setParams(params, pstmt);
+            log.debug("query : {}", sql);
+            return executeQueryOne(pstmt, rowMapper);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private <T> List<T> executeQuery(final PreparedStatement pstmt, final RowMapper<T> rowMapper) throws SQLException {
+        try (ResultSet rs = pstmt.executeQuery()) {
+            List<T> objects = new ArrayList<>();
+            while (rs.next()) {
+                objects.add(rowMapper.map(rs));
+            }
+            return objects;
+        }
+    }
+
+    private <T> T executeQueryOne(final PreparedStatement pstmt, final RowMapper<T> rowMapper) throws SQLException {
+        try(ResultSet rs = pstmt.executeQuery()) {
+            if (rs.next()) {
+                return rowMapper.map(rs);
+            }
+            return null;
+        }
+    }
+
+    private static void setParams(final Object[] params, final PreparedStatement pstmt) throws SQLException {
+        for (int i = 1; i <= params.length; i++) {
+            pstmt.setObject(i, params[i - 1]);
+        }
     }
 }
