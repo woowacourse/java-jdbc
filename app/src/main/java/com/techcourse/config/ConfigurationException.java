c5ac0d4b8bef74158bd766a0e4ee54cf53ac92ff
taek2222 <oot309@daum.net>
2025-10-11 10:51:13 +0900

    refactor: null 검증 추가 및 메서드 분리로 가독성 향상

--- app/src/main/java/com/techcourse/config/ConfigurationException.java
+++ app/src/main/java/com/techcourse/config/ConfigurationException.java
@@ -1,6 +1,11 @@
 package com.techcourse.config;
 
 public class ConfigurationException extends RuntimeException {
+
+    public ConfigurationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
     public ConfigurationException(String message) {
         super(message);
     }
--- app/src/main/java/com/techcourse/config/DataSourceConfig.java
+++ app/src/main/java/com/techcourse/config/DataSourceConfig.java
@@ -4,18 +4,22 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Objects;
 import java.util.Properties;
+import javax.sql.DataSource;
 import org.h2.jdbcx.JdbcDataSource;
 
 public class DataSourceConfig {
-
     private static final String PROPERTIES_FILE = "application.properties";
-    private static final String DATASOURCE_URL_PROPERTY = "datasource.url";
-    private static final String DATASOURCE_USERNAME_PROPERTY = "datasource.username";
-    private static final String DATASOURCE_PASSWORD_PROPERTY = "datasource.password";
 
-    private static javax.sql.DataSource INSTANCE;
+    private static final String URL_PROPERTY = "datasource.url";
+    private static final String USERNAME_PROPERTY = "datasource.username";
+    private static final String PASSWORD_PROPERTY = "datasource.password";
 
-    public static javax.sql.DataSource getInstance() {
+    private static DataSource INSTANCE;
+
+    private DataSourceConfig() {
+    }
+
+    public static DataSource getInstance() {
         if (Objects.isNull(INSTANCE)) {
             INSTANCE = createJdbcDataSource();
         }
@@ -23,27 +27,34 @@ public class DataSourceConfig {
     }
 
     private static JdbcDataSource createJdbcDataSource() {
-        try (final InputStream inputStream = DataSourceConfig.class.getResourceAsStream("/" + PROPERTIES_FILE)) {
-            if (inputStream == null) {
-                throw new ConfigurationException(
-                        String.format("%s not found in classpath", PROPERTIES_FILE)
-                );
-            }
-            final Properties properties = new Properties();
-            properties.load(inputStream);
+        Properties properties = loadProperties();
 
-            final var jdbcDataSource = new JdbcDataSource();
-            jdbcDataSource.setUrl(properties.getProperty(DATASOURCE_URL_PROPERTY));
-            jdbcDataSource.setUser(properties.getProperty(DATASOURCE_USERNAME_PROPERTY));
-            jdbcDataSource.setPassword(properties.getProperty(DATASOURCE_PASSWORD_PROPERTY));
-            return jdbcDataSource;
-        } catch (final IOException e) {
-            throw new ConfigurationException(
-                    String.format("Failed to load %s", PROPERTIES_FILE)
-            );
+        final var jdbcDataSource = new JdbcDataSource();
+        jdbcDataSource.setUrl(getRequiredProperty(properties, URL_PROPERTY));
+        jdbcDataSource.setUser(getRequiredProperty(properties, USERNAME_PROPERTY));
+        jdbcDataSource.setPassword(getRequiredProperty(properties, PASSWORD_PROPERTY));
+        return jdbcDataSource;
+    }
+
+    private static Properties loadProperties() {
+        try (InputStream input = DataSourceConfig.class.getResourceAsStream("/" + PROPERTIES_FILE)) {
+            if (input == null) {
+                throw new ConfigurationException(PROPERTIES_FILE + " not found in classpath");
+            }
+
+            Properties properties = new Properties();
+            properties.load(input);
+            return properties;
+        } catch (IOException e) {
+            throw new ConfigurationException("Failed to load properties from " + PROPERTIES_FILE, e);
         }
     }
 
-    private DataSourceConfig() {
+    private static String getRequiredProperty(Properties properties, String key) {
+        String value = properties.getProperty(key);
+        if (value == null) {
+            throw new ConfigurationException("Missing required property: " + key);
+        }
+        return value;
     }
 }
