c7af196fe716d018e0e3a9413ebea54897a56872
soeun2537 <soeun2537@gmail.com>
2025-10-11 21:10:47 +0900

    refactor: 불필요한 메서드 삭제

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -3,7 +3,6 @@ package com.techcourse.service;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
-import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 import com.interface21.dao.DataAccessException;
 
@@ -18,14 +17,6 @@ public class UserService {
     public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
-    }
-
-    public User findById(final long id) {
-        return userDao.findById(id);
-    }
-
-    public void insert(final User user) {
-        userDao.insert(user);
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
