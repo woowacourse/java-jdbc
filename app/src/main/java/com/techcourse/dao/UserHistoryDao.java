2afc112214fe8bbcdad97d73e703c346cd0c8ada
kimdev <kim__dev@hufs.ac.kr>
2025-10-12 18:42:14 +0900

    refactor: UserService 트랜잭션 처리 로직 추가 및 JdbcTemplate 메서드 오버로드 적용

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -2,6 +2,7 @@ package com.techcourse.dao;
 
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
 
 public class UserHistoryDao {
 
@@ -11,7 +12,7 @@ public class UserHistoryDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public void log(final UserHistory userHistory) {
+    public void log(final Connection connection, final UserHistory userHistory) {
         final String sql = """
             INSERT INTO user_history(user_id, account, password, email, created_at, created_by)
             VALUES(?, ?, ?, ?, ?, ?)
@@ -24,6 +25,6 @@ public class UserHistoryDao {
             userHistory.getCreatedAt(),
             userHistory.getCreateBy()
         };
-        jdbcTemplate.update(sql, params);
+        jdbcTemplate.update(connection, sql, params);
     }
 }
--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,16 +1,22 @@
 package com.techcourse.service;
 
+import com.interface21.dao.DataAccessException;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class UserService {
 
+    private final DataSource dataSource;
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final DataSource dataSource, final UserDao userDao, final UserHistoryDao userHistoryDao) {
+        this.dataSource = dataSource;
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
     }
@@ -24,9 +30,20 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        try (Connection connection = dataSource.getConnection()) {
+            try {
+                connection.setAutoCommit(false);
+                final var user = findById(id);
+                user.changePassword(newPassword);
+                userDao.update(connection, user);
+                userHistoryDao.log(connection, new UserHistory(user, createBy));
+                connection.commit();
+            } catch (Exception exception) {
+                connection.rollback();
+                throw new DataAccessException(exception);
+            }
+        } catch (SQLException exception) {
+            throw new DataAccessException("DB 연결 도중 오류가 발생하였습니다.");
+        }
     }
 }
