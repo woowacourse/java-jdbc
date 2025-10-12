9901e7f7c2fcac999bc9b1eee5427b19b8b52d0a
taek2222 <oot309@daum.net>
2025-10-13 02:04:40 +0900

    test: 테스트 커넥션 닫힘 추가

--- app/src/test/java/com/techcourse/dao/UserDaoTest.java
+++ app/src/test/java/com/techcourse/dao/UserDaoTest.java
@@ -8,6 +8,7 @@ import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
 import java.sql.Connection;
 import java.sql.SQLException;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -25,6 +26,11 @@ class UserDaoTest {
 
         final var user = new User("gugu", "password", "hkkang@woowahan.com");
         userDao.insert(conn, user);
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        conn.close();
     }
 
     @Test
