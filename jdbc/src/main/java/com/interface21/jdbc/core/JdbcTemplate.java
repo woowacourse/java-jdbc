30c319306b6ddef865f5de58721bd4eeb84ce01b
DongchannN <ldc991104@gmail.com>
2025-10-12 21:44:38 +0900

    refactor: Template Method 패턴을 통한 중복 코드 제거

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -38,57 +38,19 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... args) {
-        try (
-                var conn = dataSource.getConnection();
-                var pstmt = conn.prepareStatement(sql)
-        ) {
-            setPreparedStatementParams(pstmt, args);
-            try (var rs = pstmt.executeQuery()) {
-                log.debug("query : {}", sql);
-                return extractResults(rs, rowMapper);
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
-        }
+        return execute(conn -> queryInternal(conn, sql, rowMapper, args));
     }
 
     public <T> List<T> query(String sql, Connection connection, RowMapper<T> rowMapper, Object... args) {
-        try (var pstmt = connection.prepareStatement(sql)) {
-            setPreparedStatementParams(pstmt, args);
-            try (var rs = pstmt.executeQuery()) {
-                log.debug("query : {}", sql);
-                return extractResults(rs, rowMapper);
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
-        }
+        return executeWithConnection(connection, conn -> queryInternal(conn, sql, rowMapper, args));
     }
 
     public int update(String sql, Object... args) {
-        try (
-                var conn = dataSource.getConnection();
-                var pstmt = conn.prepareStatement(sql)
-        ) {
-            setPreparedStatementParams(pstmt, args);
-            return pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
-        }
+        return execute(conn -> updateInternal(conn, sql, args));
     }
 
     public int update(String sql, Connection connection, Object... args) {
-        try (
-                var pstmt = connection.prepareStatement(sql)
-        ) {
-            setPreparedStatementParams(pstmt, args);
-            return pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
-        }
+        return executeWithConnection(connection, conn -> updateInternal(conn, sql, args));
     }
 
     private void setPreparedStatementParams(final PreparedStatement pstmt, final Object... args) throws SQLException {
@@ -101,6 +63,34 @@ public class JdbcTemplate {
         }
     }
 
+    private <T> T execute(ConnectionCallback<T> callback) {
+        try (var conn = dataSource.getConnection()) {
+            return callback.doInConnection(conn);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> T executeWithConnection(Connection connection, ConnectionCallback<T> callback) {
+        try {
+            return callback.doInConnection(connection);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    private <T> List<T> queryInternal(Connection conn, String sql, RowMapper<T> rowMapper, Object... args) throws SQLException {
+        try (var pstmt = conn.prepareStatement(sql)) {
+            setPreparedStatementParams(pstmt, args);
+            try (var rs = pstmt.executeQuery()) {
+                log.debug("query : {}", sql);
+                return extractResults(rs, rowMapper);
+            }
+        }
+    }
+
     private <T> List<T> extractResults(final ResultSet rs, final RowMapper<T> rowMapper) throws SQLException {
         List<T> results = new ArrayList<>();
         int rowNum = 0;
@@ -108,5 +98,12 @@ public class JdbcTemplate {
             results.add(rowMapper.mapRow(rs, rowNum++));
         }
         return results;
+    }
+
+    private int updateInternal(Connection conn, String sql, Object... args) throws SQLException {
+        try (var pstmt = conn.prepareStatement(sql)) {
+            setPreparedStatementParams(pstmt, args);
+            return pstmt.executeUpdate();
+        }
     }
 }
