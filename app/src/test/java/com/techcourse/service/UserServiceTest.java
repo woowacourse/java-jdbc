5c534cb5a3762b0879b48901dd8da43b027b558b
kimdev <kim__dev@hufs.ac.kr>
2025-10-12 18:42:26 +0900

    test: UserServiceTest에 DataSource 의존성 추가

--- app/src/test/java/com/techcourse/service/UserServiceTest.java
+++ app/src/test/java/com/techcourse/service/UserServiceTest.java
@@ -30,8 +30,9 @@ class UserServiceTest {
 
     @Test
     void testChangePassword() {
+        final var dataSource = DataSourceConfig.getInstance();
         final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var userService = new UserService(dataSource, userDao, userHistoryDao);
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -45,8 +46,9 @@ class UserServiceTest {
     @Test
     void testTransactionRollback() {
         // 트랜잭션 롤백 테스트를 위해 mock으로 교체
+        final var dataSource = DataSourceConfig.getInstance();
         final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var userService = new UserService(dataSource, userDao, userHistoryDao);
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
