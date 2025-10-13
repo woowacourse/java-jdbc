0f85c4cc2be2ed0179bc26fa65c73f041d0e2d92
포라 <forarium20@gmail.com>
2025-10-13 15:23:48 +0900

    [1단계 - JDBC 라이브러리 구현하기] 포라(이승연) 미션 제출합니다. (#1029)
    * chore: Poling 오타 수정 (#928)
    
    * DriverManager 오타 수정 (#960)
    
    * feat: jdbc 라이브러리 구현
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>
    Co-authored-by: 슥 <ime.rokz@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,14 @@
 package com.interface21.jdbc.core;
 
+import com.interface21.dao.DataAccessException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 public class JdbcTemplate {
 
@@ -13,5 +18,61 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final PreparedStatementSetter pss) {
+        try (final var conn = dataSource.getConnection(); final var pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            pss.setParameters(pstmt);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public void update(final String sql, final Object... parameters) {
+        update(sql, createPreparedStatementSetter(parameters));
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rm, final PreparedStatementSetter pss) {
+        try (final var conn = dataSource.getConnection(); final var pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            pss.setParameters(pstmt);
+            return mapResultSet(rm, pstmt);
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, final RowMapper<T> rm, final Object... parameters) {
+        return query(sql, rm, createPreparedStatementSetter(parameters));
+    }
+
+    public <T> T queryForObject(final String sql, final RowMapper<T> rm, final Object... parameters) {
+        final var list = query(sql, rm, parameters);
+        if (list.isEmpty()) {
+            return null;
+        }
+        return list.get(0);
+    }
+
+    private <T> List<T> mapResultSet(final RowMapper<T> rm, final PreparedStatement pstmt) {
+        try (final ResultSet rs = pstmt.executeQuery()) {
+            final List<T> list = new ArrayList<>();
+            while (rs.next()) {
+                list.add(rm.mapRow(rs));
+            }
+            return list;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
+    }
+
+    private PreparedStatementSetter createPreparedStatementSetter(final Object... parameters) {
+        return pstmt -> {
+            for (int i = 0; i < parameters.length; i++) {
+                pstmt.setObject(i + 1, parameters[i]);
+            }
+        };
     }
 }
