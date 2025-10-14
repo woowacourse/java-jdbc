c1857fec5a91508bdd7349954b8b0791b2c9314d
Hista <dtmdgus901@gmail.com>
2025-10-14 17:19:34 +0900

    [1단계 - JDBC 라이브러리 구현하기] 히스타(오승현) 미션 제출합니다. (#1113)
    * chore: Poling 오타 수정 (#928)
    
    * DriverManager 오타 수정 (#960)
    
    * feat: 1단계 - jdbc 구현
    
    * refactor: UserHistoryDao jdbc 리팩터링
    
    ---------
    
    Co-authored-by: 이창근 <dlckdrms0517@gmail.com>
    Co-authored-by: 슥 <ime.rokz@gmail.com>

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,9 +1,16 @@
 package com.interface21.jdbc.core;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
+
+import com.interface21.dao.DataAccessException;
 
 public class JdbcTemplate {
 
@@ -13,5 +20,53 @@ public class JdbcTemplate {
 
     public JdbcTemplate(final DataSource dataSource) {
         this.dataSource = dataSource;
+    }
+
+    public void update(final String sql, final Object... args) {
+        try (final var conn = dataSource.getConnection(); final var pstmt = conn.prepareStatement(sql)) {
+            setParameters(pstmt, args);
+            final var result = pstmt.executeUpdate();
+            log.debug("query : {}, result : {}", sql, result);
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to execute update", e);
+        }
+    }
+
+    public <T> List<T> query(final String sql, RowMapper<T> rowMapper, final Object... args) {
+        try (final var conn = dataSource.getConnection(); final var pstmt = conn.prepareStatement(sql)) {
+            setParameters(pstmt, args);
+            return mappingResultSet(pstmt, rowMapper);
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to execute query", e);
+        }
+    }
+
+    public <T> T queryForObject(final String sql, RowMapper<T> rowMapper, final Object... args) {
+        List<T> results = query(sql, rowMapper, args);
+        if (results.isEmpty()) {
+            throw new DataAccessException("No data found");
+        }
+        if (results.size() > 1) {
+            throw new DataAccessException("More than one result found");
+        }
+        return results.get(0);
+    }
+
+    private <T> List<T> mappingResultSet(PreparedStatement pstmt, RowMapper<T> rowMapper) {
+        try (final var rs = pstmt.executeQuery()) {
+            final List<T> results = new ArrayList<>();
+            while (rs.next()) {
+                results.add(rowMapper.mapRow(rs));
+            }
+            return results;
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to map ResultSet", e);
+        }
+    }
+
+    private void setParameters(final PreparedStatement pstmt, final Object... args) throws SQLException {
+        for (int i = 0; i < args.length; i++) {
+            pstmt.setObject(i + 1, args[i]);
+        }
     }
 }
