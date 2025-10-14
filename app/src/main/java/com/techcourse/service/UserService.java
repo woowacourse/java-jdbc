93d3e46bc46946a77ac3f0ce1dbecd6f4894a938
DEV_WOONG <ywcsuwon@gmail.com>
2025-10-14 23:00:45 +0900

    feat: 트랙잭션 구현
    - changePassword 메서드에 트랜잭션 구현

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,16 +1,34 @@
 package com.techcourse.service;
 
-import com.interface21.jdbc.core.JdbcTemplate;
+import com.interface21.dao.DataAccessException;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserService {
 
-    private final UserDao userDao = new UserDao(DataSourceConfig.getInstance());
-    private final UserHistoryDao userHistoryDao = new UserHistoryDao(DataSourceConfig.getInstance());
+    private final Logger log = LoggerFactory.getLogger(UserService.class);
+
+    private final DataSource dataSource;
+    private final UserDao userDao;
+    private final UserHistoryDao userHistoryDao;
+
+    public UserService() {
+        this(DataSourceConfig.getInstance(), new UserDao(DataSourceConfig.getInstance()), new UserHistoryDao(DataSourceConfig.getInstance()));
+    }
+
+    public UserService(DataSource dataSource, UserDao userDao, UserHistoryDao userHistoryDao) {
+        this.dataSource = dataSource;
+        this.userDao = userDao;
+        this.userHistoryDao = userHistoryDao;
+    }
 
     public User findById(final long id) {
         return userDao.findById(id);
@@ -25,9 +43,44 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+
+            connection.commit();
+        }catch (Exception e) {
+            rollbackSafely(connection);
+            throw new DataAccessException(e);
+        } finally {
+            closeSafely(connection);
+        }
+
+    }
+
+    private void rollbackSafely(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                log.error(e.getMessage());
+            }
+        }
+    }
+
+    private void closeSafely(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                log.error(e.getMessage());
+            }
+        }
     }
 }
+
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -23,20 +23,29 @@ public class JdbcTemplate {
         this.dataSource = dataSource;
     }
 
-    private <T> T execute(SqlExecutor<T> executor) {
+    private <T> void execute(SqlExecutor<T> executor) {
         try(Connection conn = dataSource.getConnection()) {
-            return executor.execute(conn);
+            executor.execute(conn);
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
         }
     }
 
-    public void executeUpdate(String sql, Object ... params) {
+    private <T> void execute(Connection conn, SqlExecutor<T> executor) {
+        try {
+            executor.execute(conn);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new DataAccessException(e);
+        }
+    }
+
+    public void executeUpdate(String sql, Object... params) {
         Object[] safeParams = (params == null) ? new Object[0] : params;
 
         execute(conn -> {
-            try (PreparedStatement pstmt = conn.prepareStatement(sql)){
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
                 log.debug("query : {}", sql);
 
                 for (int i = 0; i < safeParams.length; i++) {
@@ -45,47 +54,65 @@ public class JdbcTemplate {
                 pstmt.executeUpdate();
                 return null;
             }
-        } );
+        });
+    }
+
+    public void executeUpdate(Connection connection, String sql, Object... params) {
+        Object[] safeParams = (params == null) ? new Object[0] : params;
+
+        execute(connection, conn -> {
+            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                log.debug("query : {}", sql);
+
+                for (int i = 0; i < safeParams.length; i++) {
+                    pstmt.setObject(i + 1, safeParams[i]);
+                }
+                pstmt.executeUpdate();
+                return null;
+            }
+        });
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper) {
-        return execute(conn -> {
-            try (PreparedStatement pstmt = conn.prepareStatement(sql);
-                 ResultSet rs = pstmt.executeQuery()) {
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql);
+             ResultSet rs = pstmt.executeQuery()
+        ) {
+            log.debug("query : {}", sql);
 
-                log.debug("query : {}", sql);
-
-                List<T> results = new ArrayList<>();
-                while (rs.next()) {
-                    results.add(rowMapper.mapRow(rs));
-                }
-                return results;
+            List<T> results = new ArrayList<>();
+            while (rs.next()) {
+                results.add(rowMapper.mapRow(rs));
             }
-        });
+            return results;
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
         Object[] safeParams = (params == null) ? new Object[0] : params;
 
-        return execute(conn -> {
-            try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
-                log.debug("query : {}", sql);
-                for (int i = 0; i < safeParams.length; i++) {
-                    pstmt.setObject(i + 1, safeParams[i]);
-                }
-
-                try (ResultSet rs = pstmt.executeQuery()) {
-                    if (!rs.next()) {
-                        throw new EmptyResultDataAccessException();
-                    }
-                    T result = rowMapper.mapRow(rs);
-                    if (rs.next()) {
-                        throw new OverSizeResultDataAccessException();
-                    }
-                    return result;
-                }
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            log.debug("query : {}", sql);
+            for (int i = 0; i < safeParams.length; i++) {
+                pstmt.setObject(i + 1, safeParams[i]);
             }
-        });
+
+            try (ResultSet rs = pstmt.executeQuery()) {
+                if (!rs.next()) {
+                    throw new EmptyResultDataAccessException();
+                }
+                T result = rowMapper.mapRow(rs);
+                if (rs.next()) {
+                    throw new OverSizeResultDataAccessException();
+                }
+                return result;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
+        }
     }
 
 }
