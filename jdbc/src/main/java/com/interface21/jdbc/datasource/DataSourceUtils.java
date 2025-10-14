68f48eea85810cf23a29580bf549260349309f39
julie <7jujubuilds@gmail.com>
2025-10-14 21:36:36 +0900

    feat: DataSourceUtils 및 TransactionSynchronizationManager 내부 기능 구현

--- jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
@@ -2,10 +2,9 @@ package com.interface21.jdbc.datasource;
 
 import com.interface21.jdbc.CannotGetJdbcConnectionException;
 import com.interface21.transaction.support.TransactionSynchronizationManager;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 // 4단계 미션에서 사용할 것
 public abstract class DataSourceUtils {
@@ -32,6 +31,8 @@ public abstract class DataSourceUtils {
             connection.close();
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
+        } finally {
+            TransactionSynchronizationManager.unbindResource(dataSource);
         }
     }
 }
