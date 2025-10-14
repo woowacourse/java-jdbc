fe8184457910a2535a037810964e10b95bc4565f
시원 <cnc4750@inha.edu>
2025-10-14 17:32:31 +0900

    refactor: 서비스 코드에서 트랜잭션 관리 로직 분리

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,24 +1,22 @@
 package com.techcourse.service;
 
-import com.interface21.dao.DataAccessException;
-import com.techcourse.config.DataSourceConfig;
+import com.interface21.transaction.TransactionManager;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
-import java.sql.SQLException;
 import javax.sql.DataSource;
 
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
-    private final DataSource dataSource;
+    private final TransactionManager transactionManager;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final DataSource dataSource) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
-        this.dataSource = DataSourceConfig.getInstance();
+        this.transactionManager = new TransactionManager(dataSource);
     }
 
     public User findById(final long id) {
@@ -33,20 +31,9 @@ public class UserService {
         final var user = findById(id);
         user.changePassword(newPassword);
 
-        try (final var connection = dataSource.getConnection();) {
-            connection.setAutoCommit(false);
-
-            try {
-                userDao.update(connection, user);
-                userHistoryDao.log(connection, new UserHistory(user, createBy));
-                connection.commit();
-            } catch (Exception e) {
-                connection.rollback();
-                throw new DataAccessException("비밀번호 변경 중 오류가 발생했습니다.");
-            }
-
-        } catch (SQLException e) {
-            throw new RuntimeException("Connection 오류 발생");
-        }
+        transactionManager.executeTransaction((connection) -> {
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+        });
     }
 }
