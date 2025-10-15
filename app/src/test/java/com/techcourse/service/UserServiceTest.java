21f92a30f0ef35b3a8b754f39e869cad922e101e
threepebbles <threepebbles@naver.com>
2025-10-15 19:16:49 +0900

    test: import 수정

--- app/src/test/java/com/techcourse/service/UserServiceTest.java
+++ app/src/test/java/com/techcourse/service/UserServiceTest.java
@@ -1,6 +1,6 @@
 package com.techcourse.service;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.interface21.dao.DataAccessException;
