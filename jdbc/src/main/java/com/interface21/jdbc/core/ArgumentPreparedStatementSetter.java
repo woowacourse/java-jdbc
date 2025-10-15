7dc4804cc21b7383870eb021164f5a4c80f8901c
Link <kingjh1125@gmail.com>
2025-10-15 10:45:39 +0900

    [2단계 - 리팩터링] 링크(손준형) 미션 제출합니다. (#959)
    * test: update test
    
    * refactor: isolation of methods and interfaces
    
    * refactor: delete unused annotation
    
    * refactor: handle resource leak

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/ArgumentPreparedStatementSetter.java
@@ -0,0 +1,19 @@
+package com.interface21.jdbc.core;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class ArgumentPreparedStatementSetter implements PreparedStatementSetter {
+    private final Object[] args;
+
+    public ArgumentPreparedStatementSetter(Object[] args) {
+        this.args = args;
+    }
+
+    @Override
+    public void setValues(PreparedStatement pstmt) throws SQLException {
+        for (int idx = 1; idx <= args.length; idx++) {
+            pstmt.setObject(idx, args[idx - 1]);
+        }
+    }
+}
