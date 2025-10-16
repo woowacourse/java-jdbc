2d2fe4c57932675d03b602d7506c5a637019d519
Hista <dtmdgus901@gmail.com>
2025-10-16 12:02:52 +0900

    refactor: JdbcTemplate import 구분 리팩터링 (#1146)

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -5,10 +5,10 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import javax.sql.DataSource;
 
 import com.interface21.dao.DataAccessException;
 
