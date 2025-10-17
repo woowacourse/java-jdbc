da01386eb6702d29c3202549ae397d636b99ad1c
Jeffrey <141808008+AppleMint98@users.noreply.github.com>
2025-10-17 22:17:21 +0900

    feat: UserService 인터페이스화
    - 비즈니스 로직 담당 AppUserService
    - 트랜잭션 담당 TxUserService

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,28 @@
+package com.techcourse.service;
+
+import com.techcourse.domain.User;
+
+public class TxUserService implements UserService {
+
+    private final UserService userService;
+
+    public TxUserService(final UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public User findById(final long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void save(final User user) {
+        userService.save(user);
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createdBy) {
+        // TODO: 트랜잭션 처리
+        userService.changePassword(id, newPassword, createdBy);
+    }
+}
