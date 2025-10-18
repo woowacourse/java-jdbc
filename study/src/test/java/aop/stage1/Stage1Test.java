ec33afef4e6d6b488a3a27bfbbb46f0ec063f64c
7i.w__k <tizmfns3@gmail.com>
2025-10-19 00:46:25 +0900

    feat: 미션 수행

--- study/src/test/java/aop/stage1/Stage1Test.java
+++ study/src/test/java/aop/stage1/Stage1Test.java
@@ -1,20 +1,22 @@
 package aop.stage1;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import aop.DataAccessException;
 import aop.StubUserHistoryDao;
 import aop.domain.User;
 import aop.repository.UserDao;
 import aop.repository.UserHistoryDao;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.aop.framework.ProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.PlatformTransactionManager;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class Stage1Test {
@@ -41,7 +43,9 @@ class Stage1Test {
 
     @Test
     void testChangePassword() {
-        final UserService userService = null;
+        ProxyFactoryBean proxyFactoryBean = getProxyFactoryBean(userHistoryDao);
+
+        final UserService userService = (UserService) proxyFactoryBean.getObject();
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
@@ -54,7 +58,8 @@ class Stage1Test {
 
     @Test
     void testTransactionRollback() {
-        final UserService userService = null;
+        ProxyFactoryBean proxyFactoryBean = getProxyFactoryBean(stubUserHistoryDao);
+        final UserService userService = (UserService) proxyFactoryBean.getObject();
 
         final var newPassword = "newPassword";
         final var createBy = "gugu";
@@ -64,5 +69,16 @@ class Stage1Test {
         final var actual = userService.findById(1L);
 
         assertThat(actual.getPassword()).isNotEqualTo(newPassword);
+    }
+
+    private @NotNull ProxyFactoryBean getProxyFactoryBean(UserHistoryDao userHistoryDao) {
+        ProxyFactoryBean proxyFactoryBean = new ProxyFactoryBean();
+        proxyFactoryBean.setTarget(new UserService(userDao, userHistoryDao));
+        proxyFactoryBean.setProxyTargetClass(true);
+        TransactionPointcut transactionPointcut = new TransactionPointcut();
+        TransactionAdvice transactionAdvice = new TransactionAdvice(platformTransactionManager);
+        TransactionAdvisor transactionAdvisor = new TransactionAdvisor(transactionPointcut, transactionAdvice);
+        proxyFactoryBean.addAdvisor(transactionAdvisor);
+        return proxyFactoryBean;
     }
 }
