fee467c93931e04b2b2aa2f357efa8731350a063
JuYongeun <65019047+jye326@users.noreply.github.com>
2025-10-18 14:14:50 +0900

    refactor: DbCleaner에서 예외 처리 제거 및 JdbcExecutionException 개선
    - DbCleaner에서 불필요한 JdbcExecutionException 활용 예외 처리 코드 제거
    - JdbcExecutionException 생성자에 cause 파라미터 추가하여 예외 추적성 강화
    - DbCleaner의 SQL 및 코드 가독성 개선

--- app/src/test/java/com/techcourse/DbCleaner.java
+++ app/src/test/java/com/techcourse/DbCleaner.java
@@ -1,6 +1,5 @@
 package com.techcourse;
 
-import com.interface21.jdbc.JdbcExecutionException;
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.interface21.jdbc.core.RowMapper;
 import java.util.List;
@@ -17,21 +16,17 @@ public class DbCleaner {
         jdbcTemplate.update("SET REFERENTIAL_INTEGRITY FALSE");
 
         String sql = """
-            SELECT table_name
-            FROM INFORMATION_SCHEMA.TABLES
-            WHERE table_schema = SCHEMA()
-              AND (table_type = 'BASE TABLE' OR table_type = 'TABLE')
-              AND LOWER(table_name) <> 'flyway_schema_history'
-            """;
+                SELECT table_name
+                FROM INFORMATION_SCHEMA.TABLES
+                WHERE table_schema = SCHEMA()
+                  AND (table_type = 'BASE TABLE' OR table_type = 'TABLE')
+                  AND LOWER(table_name) <> 'flyway_schema_history'
+                """;
 
         List<String> tables = jdbcTemplate.query(sql, stringMapper());
 
         for (String table : tables) {
-            try {
-                jdbcTemplate.update("TRUNCATE TABLE " + table);
-            } catch (JdbcExecutionException e) {
-                throw e;
-            }
+            jdbcTemplate.update("TRUNCATE TABLE " + table);
         }
 
         jdbcTemplate.update("ALTER TABLE \"USERS\" ALTER COLUMN \"ID\" RESTART WITH 1");
--- jdbc/src/main/java/com/interface21/jdbc/JdbcExecutionException.java
+++ jdbc/src/main/java/com/interface21/jdbc/JdbcExecutionException.java
@@ -1,7 +1,12 @@
 package com.interface21.jdbc;
 
 public class JdbcExecutionException extends RuntimeException {
+
     public JdbcExecutionException(String message) {
         super(message);
+    }
+
+    public JdbcExecutionException(String message, Exception e) {
+        super(message, e);
     }
 }
