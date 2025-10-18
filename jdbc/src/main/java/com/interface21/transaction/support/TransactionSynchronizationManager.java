05ea15b2fc3a86cfb8c020e3b29ea68217092655
yesjuhee <juhee0924@g.skku.edu>
2025-10-19 01:38:29 +0900

    feat: TransactionSynchronizationManager가 올바르게 작동하도록 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,32 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.Map;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
-
-    public static Connection getResource(DataSource key) {
-        return null;
+    private TransactionSynchronizationManager() {
     }
 
-    public static void bindResource(DataSource key, Connection value) {
+    public static Connection getResource(final DataSource key) {
+        final Map<DataSource, Connection> connections = resources.get();
+        return connections.get(key);
     }
 
-    public static Connection unbindResource(DataSource key) {
-        return null;
+    public static void bindResource(final DataSource key, final Connection value) {
+        final Map<DataSource, Connection> connections = resources.get();
+        if (connections == null) {
+            resources.set(Map.of(key, value));
+        } else {
+            connections.put(key, value);
+        }
+    }
+
+    public static Connection unbindResource(final DataSource key) {
+        final Map<DataSource, Connection> connections = resources.get();
+        return connections.remove(key);
     }
 }
