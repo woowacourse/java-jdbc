162abd2bfa8bd8b1a9f67164f05691458e553820
Wooyoung <quantumjump10@gmail.com>
2025-10-18 21:33:57 +0900

    refactor: changePassword 로직 내부를 한 트랜잭션으로 연결

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,13 +1,20 @@
 package com.techcourse.service;
 
+import com.interface21.dao.DataAccessException;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Optional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
@@ -27,8 +34,22 @@ public class UserService {
     public void changePassword(final long id, final String newPassword, final String createBy) {
         final var user = findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 id의 유저가 존재하지 않습니다" + id));
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        try (final Connection conn = DataSourceConfig.getInstance().getConnection()) {
+            try {
+                conn.setAutoCommit(false);
+
+                user.changePassword(newPassword);
+                userDao.update(conn, user);
+                userHistoryDao.log(conn, new UserHistory(user, createBy));
+
+                conn.commit();
+            } catch (Exception e) {
+                conn.rollback();
+                throw new DataAccessException("change Password failed", e);
+            }
+        } catch (SQLException e) {
+            log.error("Database connection failed", e);
+            throw new DataAccessException("Database connection failed", e);
+        }
     }
 }
