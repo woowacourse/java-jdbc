5d742d3c064cc8dbbad629c95ea1347a96eb376f
ykmxxi <ykmxxi97@gmail.com>
2025-10-18 16:39:23 +0900

    refactor: 함수형 인터페이스 어노테이션 추가

--- jdbc/src/main/java/com/interface21/jdbc/core/ResultSetExtractor.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/ResultSetExtractor.java
@@ -3,6 +3,7 @@ package com.interface21.jdbc.core;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface ResultSetExtractor<T> {
 
     T extractData(ResultSet resultSet) throws SQLException;
