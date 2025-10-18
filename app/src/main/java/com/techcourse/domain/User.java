1e3ef1b2c2558bd4678a5cbade2cb63a77133200
조재중 <9gle3492@gmail.com>
2025-10-18 23:10:42 +0900

    [3단계 - Transaction 적용하기] 강산(조재중) 미션 제출합니다. (#1157)
    * test: step3 시작, test disable 삭제
    
    * feat: UserHistoryDao 작동하도록 수정
    
    * refactor: User 엔티티 개선
    
    * feat: 트랜잭션 경계 설정
    
    - userService에서 connection을 관리
    - 원자적으로 처리하고 싶은 작업에게 같은 connection을 전달
    
    * test: MockDao에 트랜잭션을 위한 메서드 추가
    
    - 테스트 개선.

--- app/src/main/java/com/techcourse/domain/User.java
+++ app/src/main/java/com/techcourse/domain/User.java
@@ -1,56 +1,34 @@
 package com.techcourse.domain;
 
+import lombok.Getter;
+
+@Getter
 public class User {
 
-    private Long id;
+    private final Long id;
     private final String account;
-    private String password;
     private final String email;
+    private String password;
 
-    public User(long id, String account, String password, String email) {
+    public User(final long id, final String account, final String password, final String email) {
         this.id = id;
         this.account = account;
         this.password = password;
         this.email = email;
     }
 
-    public User(String account, String password, String email) {
+    public User(final String account, final String password, final String email) {
+        this.id = null;
         this.account = account;
         this.password = password;
         this.email = email;
     }
 
-    public boolean checkPassword(String password) {
+    public boolean checkPassword(final String password) {
         return this.password.equals(password);
     }
 
-    public void changePassword(String password) {
+    public void changePassword(final String password) {
         this.password = password;
-    }
-
-    public String getAccount() {
-        return account;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", account='" + account + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
     }
 }
