df198e822a21127989cd2eaf930b3e05ebf75d94
kjungw <sagakkb@gmail.com>
2025-10-19 01:37:47 +0900

    fix: 커넥션 적용 버그 수정

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -50,7 +50,7 @@ public class UserDao {
 
     public User findById(final Connection connection, final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(sql, USER_ROW_MAPPER, id);
+        return jdbcTemplate.queryForObject(connection, sql, USER_ROW_MAPPER, id);
     }
 
     public User findByAccount(final String account) {
