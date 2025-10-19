b3eeede4bb82378d2da664f25c6590105627a30c
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2025-10-19 23:07:30 +0900

    [3단계 - Transaction 적용하기] 코기(장재현) 미션 제출합니다. (#1127)
    * feat: 외부에서 커넥션을 받고 업데이트하는 기능 추가
    
    * refactor: jdbcTemplate 기반으로 변경
    
    * feat: 외부 커넥션을 사용해 업데이트하는 기능 추가
    
    * feat: 외부 커넥션을 사용해 업데이트하는 기능 추가
    
    * feat: 트랜잭션 기능 추가
    
    * test: 트랜잭션 테스트
    
    * refactor: 메서드 추출
    
    * refactor: 트랜잭션 메서드 추출
    
    * refactor: Datasource 의존성 주입

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,18 +1,29 @@
 package com.techcourse.service;
 
+import com.interface21.jdbc.exception.JdbcException;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Consumer;
+import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UserService {
 
+    private static final Logger log = LoggerFactory.getLogger(UserService.class);
+
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final DataSource dataSource) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.dataSource = dataSource;
     }
 
     public User findById(final long id) {
@@ -23,10 +34,55 @@ public class UserService {
         userDao.insert(user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(
+            final long id,
+            final String newPassword,
+            final String createBy
+    ) {
+        transaction((connection -> {
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+        }));
+    }
+
+    private void transaction(final Consumer<Connection> businessLogic) {
+        Connection conn = null;
+        try {
+            conn = dataSource.getConnection();
+            conn.setAutoCommit(false);
+            businessLogic.accept(conn);
+            conn.commit();
+        } catch (SQLException e) {
+            connectionRollback(conn);
+            throw new JdbcException(e);
+        } catch (Exception e) {
+            connectionRollback(conn);
+            throw e;
+        } finally {
+            connectionClose(conn);
+        }
+    }
+
+    private void connectionClose(final Connection conn) {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException exception) {
+                throw new JdbcException(exception);
+            }
+        }
+    }
+
+    private void connectionRollback(final Connection conn) {
+        if (conn != null) {
+            try {
+                log.warn("rollback");
+                conn.rollback();
+            } catch (SQLException exception) {
+                throw new JdbcException(exception);
+            }
+        }
     }
 }
