85bedeb0a9059a9506f1fff071ae15d9aa89a145
wogus <ind07152@naver.com>
2025-10-20 01:36:12 +0900

    feat: resources 초기화 추가

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -7,7 +7,7 @@ import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {
     }
@@ -19,15 +19,12 @@ public abstract class TransactionSynchronizationManager {
 
     public static void bindResource(DataSource key, Connection value) {
         Map<DataSource, Connection> connectionMap = resources.get();
-        if (connectionMap == null) {
-            connectionMap = new HashMap<>();
-        }
         connectionMap.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
         Map<DataSource, Connection> connectionMap = resources.get();
-        if (connectionMap == null) {
+        if (!connectionMap.containsKey(key)) {
             return null;
         }
         Connection removed = connectionMap.remove(key);
@@ -37,6 +34,9 @@ public abstract class TransactionSynchronizationManager {
         return removed;
     }
 
+    // 현재는 등록한 DataSource가 하나만 있으므로 unbindResource를 호출하면 ThreadLocal의 자원을 정리한다.
+    // 하지만 만약 DataSource가 여러 개라면 unbindResource를 각 DataSoruce마다 하지 않는다면 ThreadLocal 누수 위험성이 존재하게된다.
+    // 이를 방지하기 위해 전체 자원 정리 메서드를 만듬. 현재는 쓰이지 않음.
     public static void clear() {
         resources.remove();
     }
