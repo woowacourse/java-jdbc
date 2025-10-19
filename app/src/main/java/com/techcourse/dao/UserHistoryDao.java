5c765430f66213f0d1addd0a9d4b92b012100a82
Hista <dtmdgus901@gmail.com>
2025-10-19 18:04:11 +0900

    feat: 미션 - Transaction 적용하기 완료 (#1151)

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -1,5 +1,8 @@
 package com.techcourse.dao;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import javax.sql.DataSource;
 
 import org.slf4j.Logger;
@@ -25,6 +28,17 @@ public class UserHistoryDao {
     public void log(final UserHistory userHistory) {
         final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
         jdbcTemplate.update(sql,
+            userHistory.getUserId(),
+            userHistory.getAccount(),
+            userHistory.getPassword(),
+            userHistory.getEmail(),
+            userHistory.getCreatedAt(),
+            userHistory.getCreateBy());
+    }
+
+    public void log(Connection connection, final UserHistory userHistory) {
+        final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
+        jdbcTemplate.update(connection, sql,
             userHistory.getUserId(),
             userHistory.getAccount(),
             userHistory.getPassword(),
--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,5 +1,13 @@
 package com.techcourse.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+import com.interface21.dao.DataAccessException;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -7,10 +15,12 @@ import com.techcourse.domain.UserHistory;
 
 public class UserService {
 
+    private final DataSource dataSource;
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
     public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+        this.dataSource = DataSourceConfig.getInstance();
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
     }
@@ -23,10 +33,29 @@ public class UserService {
         userDao.insert(user);
     }
 
+    // Spring의 @Transactional을 사용하면 Thread Local에 Connection이 바인딩되어 같은 Connection을 사용하게 된다.
+    // 하지만 현재는 학습 목적으로 @Transactional을 사용하지 않고 수동으로 같은 Connection을 사용하도록 구현하겠다.
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        try (Connection connection = dataSource.getConnection()) {
+            connection.setAutoCommit(false);
+
+            try {
+                final var user = findById(id);
+                user.changePassword(newPassword);
+                userDao.update(connection, user);
+                userHistoryDao.log(connection, new UserHistory(user, createBy));
+            } catch (Exception e) { // 비즈니스 로직 실패
+                try {
+                    connection.rollback();
+                    throw new DataAccessException("Transaction rolled back due to an error", e);
+                } catch (SQLException ex) { // 롤백 실패
+                    throw new DataAccessException("Failed to rollback transaction", ex);
+                }
+            }
+
+            connection.commit();
+        } catch (SQLException e) {
+            throw new DataAccessException("Failed to change password", e);
+        }
     }
 }
