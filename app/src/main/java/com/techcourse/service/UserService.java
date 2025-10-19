5f401876d20c7b21801fe97761b60bd73d579679
wogus <ind07152@naver.com>
2025-10-20 00:48:16 +0900

    refactor: 트랜잭션 메니저를 사용한 데이터 소스 획득으로 변경

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -42,8 +42,8 @@ public class UserService {
         transaction((connection -> {
             final var user = findById(id);
             user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
         }));
     }
 
