8221abddf7003783297443350a319878826b4fad
eunseongu <rndmstjs75@gmail.com>
2025-10-19 15:55:32 +0900

    feat: 트랜잭션 서비스 추상화

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -3,50 +3,32 @@ package com.techcourse.service;
 import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.datasource.DataSourceUtils;
 import com.interface21.transaction.support.TransactionSynchronizationManager;
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
-import com.techcourse.domain.UserHistory;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class UserService {
+public class TxUserService implements UserServiceInterface {
 
-    private static final Logger log = LoggerFactory.getLogger(UserService.class);
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
-    private final DataSource dataSource;
+    private static final Logger log = LoggerFactory.getLogger(TxUserService.class);
+    private final UserServiceInterface userServiceInterface;
 
-    public UserService(UserDao userDao, UserHistoryDao userHistoryDao, DataSource dataSource) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-        this.dataSource = dataSource;
+    public TxUserService(UserServiceInterface userServiceInterface) {
+        this.userServiceInterface = userServiceInterface;
     }
 
-    public User findById(final long id) {
-        return userDao.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("사용자 정보가 존재하지 않습니다."));
-    }
-
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
-
-    public void changePassword(final long id, final String newPassword, final String createBy) {
+    // override 대상인 메서드는 userService의 메서드를 그대로 위임(delegate)한다.
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createdBy) {
+        DataSource dataSource = DataSourceConfig.getInstance();
         Connection connection = null;
         try {
             connection = DataSourceUtils.getConnection(dataSource);
             connection.setAutoCommit(false);
-
-            final var user = userDao.findById(connection, id)
-                    .orElseThrow(() -> new IllegalArgumentException("사용자 정보가 존재하지 않습니다."));
-            user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(new UserHistory(user, createBy));
-
+            userServiceInterface.changePassword(id, newPassword, createdBy);
             connection.commit();
         } catch (Exception e) {
             if (connection != null) {
@@ -68,4 +50,15 @@ public class UserService {
             }
         }
     }
+
+    @Override
+    public void save(User user) {
+        userServiceInterface.save(user);
+    }
+
+    @Override
+    public User findById(long id) {
+        return userServiceInterface.findById(id);
+    }
 }
+
