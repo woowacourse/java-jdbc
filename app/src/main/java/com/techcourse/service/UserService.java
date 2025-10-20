7c1cc4d35b679d4f27081b4d09b4d1070ddcd087
byesol <bsj5967@naver.com>
2025-10-21 02:43:21 +0900

    feat: Connection 기반 findById 메서드 추가 및 서비스 로직 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -32,7 +32,7 @@ public class UserService {
             connection = userDao.getJdbcTemplate().getDataSource().getConnection();
             connection.setAutoCommit(false);
 
-            final var user = findById(id);
+            final var user = userDao.findById(connection, id);
             user.changePassword(newPassword);
             userDao.update(connection, user);
             userHistoryDao.log(connection, new UserHistory(user, createBy));
