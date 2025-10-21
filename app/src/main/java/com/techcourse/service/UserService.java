eca6b6029e03f2f495651c1ab3a301b162ea0b37
byesol <bsj5967@naver.com>
2025-10-21 10:37:43 +0900

    refactor: `UserService`에 `DataSource` 의존성 추가 및 커넥션 생성 로직 수정

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,21 +1,26 @@
 package com.techcourse.service;
 
 import com.interface21.dao.DataAccessException;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource;
+
 
     public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.dataSource = DataSourceConfig.getInstance();
     }
 
     public User findById(final long id) {
@@ -29,7 +34,7 @@ public class UserService {
     public void changePassword(final long id, final String newPassword, final String createBy) {
         Connection connection = null;
         try {
-            connection = userDao.getJdbcTemplate().getDataSource().getConnection();
+            connection = dataSource.getConnection();
             connection.setAutoCommit(false);
 
             final var user = userDao.findById(connection, id);
