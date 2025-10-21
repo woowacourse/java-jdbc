17f26b9807d2596be514b2581898fe72c845618d
RaZel713 <razel7130@gmail.com>
2025-10-21 12:03:04 +0900

    refactor: UserHistoryDao에서 JdbcTemplate을 사용하여 SQL 실행 로직 개선

--- app/src/main/java/com/techcourse/dao/UserHistoryDao.java
+++ app/src/main/java/com/techcourse/dao/UserHistoryDao.java
@@ -3,8 +3,6 @@ package com.techcourse.dao;
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.techcourse.domain.UserHistory;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,73 +11,44 @@ public class UserHistoryDao {
 
     private static final Logger log = LoggerFactory.getLogger(UserHistoryDao.class);
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
 
     public UserHistoryDao(final DataSource dataSource) {
-        this.dataSource = dataSource;
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
     public UserHistoryDao(final JdbcTemplate jdbcTemplate) {
-        this.dataSource = null;
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    // 기존 버전 (호환성을 위해서 유지)
     public void log(final UserHistory userHistory) {
         final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
+        log.debug("query : {}", sql);
 
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        try {
-            conn = dataSource.getConnection();
-            pstmt = conn.prepareStatement(sql);
-
-            log.debug("query : {}", sql);
-
-            setUserHistoryParametersAndExecute(userHistory, pstmt);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        } finally {
-            try {
-                if (pstmt != null) {
-                    pstmt.close();
-                }
-            } catch (SQLException ignored) {
-            }
-
-            try {
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (SQLException ignored) {
-            }
-        }
+        jdbcTemplate.executeUpdate(
+                sql,
+                userHistory.getUserId(),
+                userHistory.getAccount(),
+                userHistory.getPassword(),
+                userHistory.getEmail(),
+                userHistory.getCreatedAt(),
+                userHistory.getCreateBy()
+        );
     }
 
-    // connection 버전
     public void log(final Connection connection, final UserHistory userHistory) {
         final var sql = "insert into user_history (user_id, account, password, email, created_at, created_by) values (?, ?, ?, ?, ?, ?)";
+        log.debug("query : {}", sql);
 
-        try (var pstmt = connection.prepareStatement(sql)) {
-            log.debug("query : {}", sql);
-
-            setUserHistoryParametersAndExecute(userHistory, pstmt);
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void setUserHistoryParametersAndExecute(
-            final UserHistory userHistory, final PreparedStatement pstmt
-    ) throws SQLException {
-        pstmt.setLong(1, userHistory.getUserId());
-        pstmt.setString(2, userHistory.getAccount());
-        pstmt.setString(3, userHistory.getPassword());
-        pstmt.setString(4, userHistory.getEmail());
-        pstmt.setObject(5, userHistory.getCreatedAt());
-        pstmt.setString(6, userHistory.getCreateBy());
-
-        pstmt.executeUpdate();
+        jdbcTemplate.executeUpdate(
+                connection,
+                sql,
+                userHistory.getUserId(),
+                userHistory.getAccount(),
+                userHistory.getPassword(),
+                userHistory.getEmail(),
+                userHistory.getCreatedAt(),
+                userHistory.getCreateBy()
+        );
     }
 }
