fbd54973e213a013e72f0ea5265f3ab49d7b6f14
kjyyjk <rlawnsdud920@gmail.com>
2025-10-22 01:42:25 +0900

    refactor: Transaction 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,5 +1,12 @@
 package com.techcourse.service;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.interface21.dao.DataAccessException;
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -7,10 +14,12 @@ import com.techcourse.domain.UserHistory;
 
 public class UserService {
 
+    private final DataSource dataSource;
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
     public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+        dataSource = DataSourceConfig.getInstance();
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
     }
@@ -24,9 +33,31 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            connection.commit();
+        } catch (Exception e) {
+            try {
+                connection.rollback();
+            } catch (SQLException sqlException) {
+                throw new DataAccessException(sqlException);
+            }
+            throw new DataAccessException(e);
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new DataAccessException(e);
+                }
+            }
+        }
     }
 }
