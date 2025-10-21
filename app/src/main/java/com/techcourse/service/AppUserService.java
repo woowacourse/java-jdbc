23d3b57ef6f3c04c6def39935ac954ec1f45e85f
yeonnhuu <yeonnhuu@gmail.com>
2025-10-21 11:17:35 +0900

    refactor: var 대신 명시적 타입 User 사용

--- app/src/main/java/com/techcourse/service/AppUserService.java
+++ app/src/main/java/com/techcourse/service/AppUserService.java
@@ -27,7 +27,7 @@ public class AppUserService implements UserService {
 
     @Override
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
+        final User user = findById(id);
         user.changePassword(newPassword);
         userDao.update(user);
         userHistoryDao.log(new UserHistory(user, createBy));
