0f9874af6d221fb1094bbc78f16a097566cb60f5
minjae <minjae8563@naver.com>
2025-10-21 21:17:07 +0900

    feat: '3단계 - Transaction 적용하기' 미션 구현 (#1152)
    Co-authored-by: (minjae8563) <(minjae8563@naver.com)>

--- app/src/main/java/com/techcourse/dao/UserDao.java
+++ app/src/main/java/com/techcourse/dao/UserDao.java
@@ -3,7 +3,7 @@ package com.techcourse.dao;
 import com.interface21.jdbc.core.JdbcTemplate;
 import com.interface21.jdbc.core.RowMapper;
 import com.techcourse.domain.User;
-
+import java.sql.Connection;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -22,6 +22,19 @@ public class UserDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
+    public void insert(Connection connection, User user) {
+        String sql = "INSERT INTO users (account, password, email) VALUES (?, ?, ?)";
+        jdbcTemplate.update(
+                connection,
+                sql,
+                setter -> {
+            setter.setString(1, user.getAccount());
+            setter.setString(2, user.getPassword());
+            setter.setString(3, user.getEmail());
+        }
+        );
+    }
+
     public void insert(final User user) {
         final String sql = "INSERT INTO users (account, password, email) VALUES (?, ?, ?)";
 
@@ -29,6 +42,17 @@ public class UserDao {
             setter.setString(1, user.getAccount());
             setter.setString(2, user.getPassword());
             setter.setString(3, user.getEmail());
+        });
+    }
+
+    public void update(final Connection connection, final User user) {
+        final String sql = "UPDATE users SET account = ?, password = ?, email = ? WHERE id = ?";
+
+        jdbcTemplate.update(connection, sql, setter -> {
+            setter.setString(1, user.getAccount());
+            setter.setString(2, user.getPassword());
+            setter.setString(3, user.getEmail());
+            setter.setLong(4, user.getId());
         });
     }
 
@@ -43,6 +67,17 @@ public class UserDao {
         });
     }
 
+    public List<User> findAll(final Connection connection) {
+        final String sql = "SELECT * FROM users";
+
+        return jdbcTemplate.queryForObjects(
+                connection,
+                sql,
+                setter -> {},
+                userMapper
+        );
+    }
+
     public List<User> findAll() {
         final String sql = "SELECT * FROM users";
 
@@ -50,6 +85,19 @@ public class UserDao {
                 sql,
                 setter -> {},
                 userMapper
+        );
+    }
+
+    public User findById(final Connection connection, final Long id) {
+        final String sql = "SELECT * FROM users WHERE id = ?";
+
+        return jdbcTemplate.queryForObject(
+                connection,
+                sql,
+                setter -> setter.setLong(1, id),
+                userMapper
+        ).orElseThrow(() ->
+                new NoSuchElementException("[ERROR] no such user id: " + id)
         );
     }
 
@@ -62,6 +110,19 @@ public class UserDao {
                 userMapper
         ).orElseThrow(() ->
                 new NoSuchElementException("[ERROR] no such user id: " + id)
+        );
+    }
+
+    public User findByAccount(final Connection connection, final String account) {
+        final String sql = "SELECT * FROM users WHERE account = ?";
+
+        return jdbcTemplate.queryForObject(
+                connection,
+                sql,
+                setter -> setter.setString(1, account),
+                userMapper
+        ).orElseThrow(() ->
+                new NoSuchElementException("[ERROR] no such user account: " + account)
         );
     }
 
--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -4,29 +4,42 @@ import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import com.techcourse.service.TransactionService.Transaction;
+import java.sql.Connection;
 
 public class UserService {
 
+    private final TransactionService transactionService;
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(TransactionService transactionService, final UserDao userDao, final UserHistoryDao userHistoryDao) {
+        this.transactionService = transactionService;
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
     }
 
     public User findById(final long id) {
-        return userDao.findById(id);
+        try(Transaction begin = transactionService.begin()) {
+            return userDao.findById(begin.getConnection(), id);
+        }
     }
 
     public void insert(final User user) {
-        userDao.insert(user);
+        try(Transaction transaction = transactionService.begin()) {
+            userDao.insert(transaction.getConnection(), user);
+            transaction.commit();
+        }
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+        try(Transaction transaction = transactionService.begin()) {
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            Connection connection = transaction.getConnection();
+            userDao.update(connection, user);
+            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            transaction.commit();
+        }
     }
 }
