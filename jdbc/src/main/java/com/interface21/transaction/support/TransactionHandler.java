6355d8230328c80cc443db603e2d46d7fa82d501
7i.w__k <tizmfns3@gmail.com>
2025-10-23 02:49:29 +0900

    fix: 중첩 트랜잭션 해결

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionHandler.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionHandler.java
@@ -1,6 +1,5 @@
 package com.interface21.transaction.support;
 
-import com.interface21.jdbc.CannotGetJdbcConnectionException;
 import com.interface21.jdbc.datasource.DataSourceUtils;
 import com.interface21.transaction.Transactional;
 import java.lang.reflect.InvocationHandler;
@@ -9,8 +8,13 @@ import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.sql.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class TransactionHandler implements InvocationHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(TransactionHandler.class);
+
 
     private final DataSource dataSource;
     private final Object target;
@@ -28,26 +32,38 @@ public class TransactionHandler implements InvocationHandler {
             return method.invoke(target, args);
         }
 
-        Connection connection = DataSourceUtils.getConnection(dataSource);
-        if (connection == null) {
-            throw new CannotGetJdbcConnectionException("Failed to obtain JDBC Connection");
-        }
+        Connection existingConnection = TransactionSynchronizationManager.getResource(dataSource);
+        boolean isNewTransaction = (existingConnection == null);
 
+        Connection connection = DataSourceUtils.getConnection(dataSource);
         boolean originalAutoCommit = connection.getAutoCommit();
 
         try {
-            if (originalAutoCommit) {
+            if (isNewTransaction && originalAutoCommit) {
                 connection.setAutoCommit(false);
             }
             return getObject(args, method, connection);
         } finally {
-            if (originalAutoCommit) {
-                connection.setAutoCommit(true);
-            }
+            finalizeTransaction(isNewTransaction, originalAutoCommit, connection);
+        }
+    }
+
+    private void finalizeTransaction(boolean isNewTransaction, boolean originalAutoCommit, Connection connection) {
+        if (isNewTransaction) {
+            revertAutoCommit(originalAutoCommit, connection);
             TransactionSynchronizationManager.unbindResource(dataSource);
             DataSourceUtils.releaseConnection(connection, dataSource);
         }
+    }
 
+    private void revertAutoCommit(boolean originalAutoCommit, Connection connection) {
+        try {
+            if (originalAutoCommit) {
+                connection.setAutoCommit(true);
+            }
+        } catch (SQLException e) {
+            log.error("Could not reset JDBC Connection after transaction", e);
+        }
     }
 
     private Object getObject(Object[] args, Method targetMethod, Connection connection) throws Throwable {
