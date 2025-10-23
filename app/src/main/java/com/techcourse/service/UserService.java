69c54d46e643347c979032935ad72f535a1a1b5d
kjyyjk <rlawnsdud920@gmail.com>
2025-10-24 01:00:29 +0900

    refactor: try with resource 적용 및 롤백 시 원본 예외 전달

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -5,6 +5,9 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.interface21.dao.DataAccessException;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
@@ -13,6 +16,8 @@ import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 
 public class UserService {
+
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final DataSource dataSource;
     private final UserDao userDao;
@@ -33,31 +38,24 @@ public class UserService {
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        Connection connection = null;
-        try {
-            connection = dataSource.getConnection();
+        try (Connection connection = dataSource.getConnection()){
             connection.setAutoCommit(false);
-
-            final var user = findById(id);
-            user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(connection, new UserHistory(user, createBy));
-            connection.commit();
-        } catch (Exception e) {
             try {
-                connection.rollback();
-            } catch (SQLException sqlException) {
-                throw new DataAccessException(sqlException);
-            }
-            throw new DataAccessException(e);
-        } finally {
-            if (connection != null) {
+                final var user = findById(id);
+                user.changePassword(newPassword);
+                userDao.update(connection, user);
+                userHistoryDao.log(connection, new UserHistory(user, createBy));
+                connection.commit();
+            } catch (Exception e) {
                 try {
-                    connection.close();
-                } catch (SQLException e) {
-                    throw new DataAccessException(e);
+                    connection.rollback();
+                } catch (SQLException sqlException) {
+                    logger.error("롤백 실패", sqlException);
                 }
+                throw new DataAccessException(e);
             }
+        } catch (SQLException e) {
+            throw new DataAccessException(e);
         }
     }
 }
