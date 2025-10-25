fde91271855546327ef4e3bd8e486c1079c96753
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-10-25 22:57:21 +0900

    refactor: 트랜잭션 관련 로직 분리를 위한 UserService 추상화
    # Conflicts:
    #	app/src/main/java/com/techcourse/service/TxUserService.java

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -10,9 +10,11 @@ import javax.sql.DataSource;
 
 public class TxUserService implements  UserService {
 
+    private final DataSource dataSource;
     private final UserService userService;
 
     public TxUserService(final UserService userService) {
+        this.dataSource = DataSourceConfig.getInstance();
         this.userService = userService;
     }
 
@@ -28,7 +30,6 @@ public class TxUserService implements  UserService {
 
     @Override
     public void changePassword(long id, String newPassword, String createBy) {
-        DataSource dataSource = DataSourceConfig.getInstance();
         Connection connection = DataSourceUtils.getConnection(dataSource);
 
         try {
@@ -37,15 +38,13 @@ public class TxUserService implements  UserService {
             userService.changePassword(id, newPassword, createBy);
 
             connection.commit();
-        } catch (RuntimeException exception) {
+        } catch (RuntimeException | SQLException exception) {
             try {
                 connection.rollback();
                 throw exception;
             } catch (SQLException rollbackException) {
                 throw new DataAccessException(rollbackException.getMessage());
             }
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
         } finally {
             DataSourceUtils.releaseConnection(connection, dataSource);
         }
