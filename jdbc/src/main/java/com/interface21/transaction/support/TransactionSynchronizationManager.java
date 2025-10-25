70331d2d35b3473a4d7198a29a1b486fa744cc43
Jin409 <sualng123@gmail.com>
2025-10-25 15:02:42 +0900

    feat: `TransactionSynchronizationManager`구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,5 +1,7 @@
 package com.interface21.transaction.support;
 
+import com.interface21.jdbc.datasource.DataSourceUtils;
+import java.util.HashMap;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.util.Map;
@@ -8,16 +10,40 @@ public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
+        if (dataSourceConnectionMap == null) {
+            return null;
+        }
+
+        return dataSourceConnectionMap.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
+
+        if (dataSourceConnectionMap == null) {
+            dataSourceConnectionMap = new HashMap<>();
+            resources.set(dataSourceConnectionMap);
+        }
+
+        dataSourceConnectionMap.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+
+        Connection removed = map.remove(key);
+
+        if (map.isEmpty()) {
+            resources.remove();
+        }
+        return removed;
     }
 }
