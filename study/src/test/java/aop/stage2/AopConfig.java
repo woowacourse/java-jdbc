e3bd1c9873f4855cd569a5abd4d5a05df23c0d0d
Yeongjae Kim <sees77770@gmail.com>
2025-10-25 20:02:42 +0900

    refactor: 스프링 AOP에 추가하기

--- study/src/test/java/aop/stage2/AopConfig.java
+++ study/src/test/java/aop/stage2/AopConfig.java
@@ -1,8 +1,38 @@
 package aop.stage2;
 
+import aop.stage1.TransactionAdvice;
+import aop.stage1.TransactionAdvisor;
+import aop.stage1.TransactionPointcut;
+import org.springframework.aop.framework.autoproxy.DefaultAdvisorAutoProxyCreator;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AopConfig {
 
+    @Bean
+    public TransactionPointcut transactionPointcut() {
+        return new TransactionPointcut();
+    }
+
+    @Bean
+    public TransactionAdvice transactionAdvice(final PlatformTransactionManager transactionManager) {
+        return new TransactionAdvice(transactionManager);
+    }
+
+    @Bean
+    public TransactionAdvisor transactionAdvisor(
+            final TransactionPointcut pointcut,
+            final TransactionAdvice advice
+    ) {
+        return new TransactionAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public DefaultAdvisorAutoProxyCreator defaultAdvisorAutoProxyCreator() {
+        final DefaultAdvisorAutoProxyCreator proxyCreator = new DefaultAdvisorAutoProxyCreator();
+        proxyCreator.setProxyTargetClass(true);
+        return proxyCreator;
+    }
 }
