5e3fe9eed98dfa792dd95ee3c9584fe1ae7372f0
ykmxxi <ykmxxi97@gmail.com>
2025-10-26 13:02:42 +0900

    refactor: 트랜잭션 커넥션 관리를 유틸과 동기화 매니저에 위임

--- jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
@@ -1,18 +1,19 @@
 package com.interface21.jdbc.datasource;
 
-import com.interface21.jdbc.CannotGetJdbcConnectionException;
-import com.interface21.transaction.support.TransactionSynchronizationManager;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-// 4단계 미션에서 사용할 것
+import javax.sql.DataSource;
+
+import com.interface21.jdbc.CannotGetJdbcConnectionException;
+import com.interface21.transaction.support.TransactionSynchronizationManager;
+
 public abstract class DataSourceUtils {
 
-    private DataSourceUtils() {}
+    private DataSourceUtils() {
+    }
 
-    public static Connection getConnection(DataSource dataSource) throws CannotGetJdbcConnectionException {
+    public static Connection getConnection(final DataSource dataSource) throws CannotGetJdbcConnectionException {
         Connection connection = TransactionSynchronizationManager.getResource(dataSource);
         if (connection != null) {
             return connection;
@@ -27,7 +28,12 @@ public abstract class DataSourceUtils {
         }
     }
 
-    public static void releaseConnection(Connection connection, DataSource dataSource) {
+    public static void releaseConnection(final Connection connection, final DataSource dataSource) {
+        Connection txSyncConnection = TransactionSynchronizationManager.getResource(dataSource);
+        if (txSyncConnection != null && txSyncConnection == connection) {
+            return;
+        }
+        
         try {
             connection.close();
         } catch (SQLException ex) {
--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,40 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> resource = resources.get();
+        if (resource == null) {
+            return null;
+        }
+        return resource.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> resource = new HashMap<>();
+        if (resources.get().containsKey(key)) {
+            throw new IllegalStateException("이미 연결된 바인딩된 커넥션이 존재합니다.");
+        }
+        resource.put(key, value);
+        resources.set(resource);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> resource = resources.get();
+        if (resource == null) {
+            return null;
+        }
+        return resource.remove(key);
     }
 }
