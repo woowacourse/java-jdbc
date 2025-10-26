553b36540cfec1efefbca26faceebd683dd60d35
kimdev <kim__dev@hufs.ac.kr>
2025-10-26 16:27:41 +0900

    refactor: 반복되는 트랜잭션 시작 및 종료 코드를 TransactionExecutionManager으로 분리

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -1,11 +1,7 @@
 package com.techcourse.service;
 
-import com.interface21.dao.DataAccessException;
-import com.interface21.jdbc.datasource.DataSourceUtils;
-import com.interface21.transaction.support.TransactionSynchronizationManager;
+import com.interface21.transaction.support.TransactionExecutionManager;
 import com.techcourse.domain.User;
-import java.sql.Connection;
-import java.sql.SQLException;
 import javax.sql.DataSource;
 
 public class TxUserService implements UserService {
@@ -30,21 +26,9 @@ public class TxUserService implements UserService {
 
     @Override
     public void changePassword(final long id, final String newPassword, final String createdBy) {
-        Connection connection = DataSourceUtils.getConnection(dataSource);
-        try {
-            connection.setAutoCommit(false);
-            userService.changePassword(id, newPassword, createdBy);
-            connection.commit();
-        } catch (Exception exception) {
-            try {
-                connection.rollback();
-            } catch (SQLException sqlException) {
-                throw new DataAccessException("DB 처리에 실패하여 DB 롤백을 시도하던 도중 오류가 발생하였습니다.", sqlException);
-            }
-            throw new DataAccessException("DB 처리 도중 오류가 발생하였습니다.", exception);
-        } finally {
-            DataSourceUtils.releaseConnection(connection, dataSource);
-            TransactionSynchronizationManager.unbindResource(dataSource);
-        }
+        TransactionExecutionManager.executeInTransaction(
+            dataSource,
+            () -> userService.changePassword(id, newPassword, createdBy)
+        );
     }
 }
