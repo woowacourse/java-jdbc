0d2c25c7f12743ded4e5dad134a51ee2996a3daa
mimi <110226866+hongkikii@users.noreply.github.com>
2025-10-26 17:55:10 +0900

    feat: DataSourceUtils 사용

--- jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
@@ -2,36 +2,42 @@ package com.interface21.jdbc.datasource;
 
 import com.interface21.jdbc.CannotGetJdbcConnectionException;
 import com.interface21.transaction.support.TransactionSynchronizationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-// 4단계 미션에서 사용할 것
 public abstract class DataSourceUtils {
 
-    private DataSourceUtils() {}
+    private static final Logger log = LoggerFactory.getLogger(DataSourceUtils.class);
 
-    public static Connection getConnection(DataSource dataSource) throws CannotGetJdbcConnectionException {
-        Connection connection = TransactionSynchronizationManager.getResource(dataSource);
+    private DataSourceUtils() {
+    }
+
+    public static Connection getConnection(final DataSource dataSource) {
+        final var connection = TransactionSynchronizationManager.getResource(dataSource);
         if (connection != null) {
             return connection;
         }
-
         try {
-            connection = dataSource.getConnection();
-            TransactionSynchronizationManager.bindResource(dataSource, connection);
-            return connection;
-        } catch (SQLException ex) {
-            throw new CannotGetJdbcConnectionException("Failed to obtain JDBC Connection", ex);
+            return dataSource.getConnection();
+        } catch (final SQLException e) {
+            throw new CannotGetJdbcConnectionException("Failed to obtain JDBC Connection", e);
         }
     }
 
-    public static void releaseConnection(Connection connection, DataSource dataSource) {
-        try {
-            connection.close();
-        } catch (SQLException ex) {
-            throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
+    public static void releaseConnection(final Connection connection, final DataSource dataSource) {
+        if (TransactionSynchronizationManager.hasResource(dataSource)) {
+            return;
+        }
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (final SQLException e) {
+                log.debug("Could not close JDBC Connection", e);
+            }
         }
     }
 }
--- study/src/test/java/transaction/stage1/UserDao.java
+++ study/src/test/java/transaction/stage1/UserDao.java
@@ -4,56 +4,49 @@ import transaction.stage1.jdbc.JdbcTemplate;
 import transaction.stage1.jdbc.RowMapper;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
 import java.util.List;
 
 public class UserDao {
 
-    // spring jdbc가 아닌 직접 구현한 JdbcTemplate을 사용한다.
     private final JdbcTemplate jdbcTemplate;
 
     public UserDao(final DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void insert(final Connection connection, final User user) {
+    public void insert(final User user) {
         final var sql = "insert into users (account, password, email) values (?, ?, ?)";
-        jdbcTemplate.update(connection, sql, user.getAccount(), user.getPassword(), user.getEmail());
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail());
     }
 
-    public void update(final Connection connection, final User user) {
+    public void update(final User user) {
         final var sql = "update users set account = ?, password = ?, email = ? where id = ?";
-        jdbcTemplate.update(connection, sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
+        jdbcTemplate.update(sql, user.getAccount(), user.getPassword(), user.getEmail(), user.getId());
     }
 
-    public void updatePasswordGreaterThan(final Connection connection, final String password, final long id) {
+    public void updatePasswordGreaterThan(final String password, final long id) {
         final var sql = "update users set password = ? where id >= ?";
-        jdbcTemplate.update(connection, sql, password, id);
+        jdbcTemplate.update(sql, password, id);
     }
 
-    public User findById(final Connection connection, final Long id) {
+    public User findById(final Long id) {
         final var sql = "select id, account, password, email from users where id = ?";
-        return jdbcTemplate.queryForObject(connection, sql, createRowMapper(), id);
+        return jdbcTemplate.queryForObject(sql, createRowMapper(), id);
     }
 
-    public User findByAccount(final Connection connection, final String account) {
+    public User findByAccount(final String account) {
         final var sql = "select id, account, password, email from users where account = ?";
-        return jdbcTemplate.queryForObject(connection, sql, createRowMapper(), account);
+        return jdbcTemplate.queryForObject(sql, createRowMapper(), account);
     }
 
-    public List<User> findGreaterThan(final Connection connection, final long id) {
+    public List<User> findGreaterThan(final long id) {
         final var sql = "select id, account, password, email from users where id >= ?";
-        return jdbcTemplate.query(connection, sql, createRowMapper(), id);
+        return jdbcTemplate.query(sql, createRowMapper(), id);
     }
 
-    public List<User> findByAccountGreaterThan(final Connection connection, final String account) {
-        final var sql = "select id, account, password, email from users where account >= ?";
-        return jdbcTemplate.query(connection, sql, createRowMapper(), account);
-    }
-
-    public List<User> findAll(final Connection connection) {
+    public List<User> findAll() {
         final var sql = "select id, account, password, email from users";
-        return jdbcTemplate.query(connection, sql, createRowMapper());
+        return jdbcTemplate.query(sql, createRowMapper());
     }
 
     private static RowMapper<User> createRowMapper() {
