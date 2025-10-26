ce5e3c842197752261fd037b6600827cd43001f9
kjungw <sagakkb@gmail.com>
2025-10-26 22:34:46 +0900

    refactor: TransactionSynchronizationManager 초기화 로직 수정

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -9,25 +9,31 @@ public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    static {
-        resources.set(new HashMap<>());
-    }
-
     private TransactionSynchronizationManager() {
     }
 
     public static Connection getResource(DataSource key) {
-        Map<DataSource, Connection> savedResource = resources.get();
+        Map<DataSource, Connection> savedResource = getResource();
         return savedResource.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
-        Map<DataSource, Connection> savedResource = resources.get();
+        Map<DataSource, Connection> savedResource = getResource();
         savedResource.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        Map<DataSource, Connection> savedResource = resources.get();
+        Map<DataSource, Connection> savedResource = getResource();
         return savedResource.remove(key);
+    }
+
+    private static Map<DataSource, Connection> getResource() {
+        Map<DataSource, Connection> savedResource = resources.get();
+        if (savedResource == null) {
+            Map<DataSource, Connection> newSavedResource = new HashMap<>();
+            resources.set(newSavedResource);
+            return newSavedResource;
+        }
+        return savedResource;
     }
 }
