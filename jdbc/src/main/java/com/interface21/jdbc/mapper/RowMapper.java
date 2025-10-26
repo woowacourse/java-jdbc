84f8561632f66ce717b93e29b7339cfef73989f6
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-26 15:36:32 +0900

    refactor: 함수형 인터페이스 어노테이션 추가

--- jdbc/src/main/java/com/interface21/jdbc/mapper/RowMapper.java
+++ jdbc/src/main/java/com/interface21/jdbc/mapper/RowMapper.java
@@ -3,6 +3,7 @@ package com.interface21.jdbc.mapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@FunctionalInterface
 public interface RowMapper<T> {
 
     T mapRowToResult(final ResultSet rs) throws SQLException;
