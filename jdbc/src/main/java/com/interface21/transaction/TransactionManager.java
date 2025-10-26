d3ea10370fc1bd8339149d426b9fd24c8730bb82
ryan-dia <cjfdnjs158@naver.com>
2025-10-27 07:13:59 +0900

    fix: 예외 발생시 예외 정보도 포함될 수 있도록 수정

--- jdbc/src/main/java/com/interface21/transaction/TransactionManager.java
+++ jdbc/src/main/java/com/interface21/transaction/TransactionManager.java
@@ -21,11 +21,11 @@ public class TransactionManager {
                 connection.commit();
             } catch (Exception e) {
                 connection.rollback();
-                throw new DataAccessException("트랜잭션 내부 작업 수행 중 오류가 발생하여 롤백이 수행되었습니다.");
+                throw new DataAccessException("트랜잭션 내부 작업 수행 중 오류가 발생하여 롤백이 수행되었습니다.", e);
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException("트랜잭션 로직을 실행하는 중 Connection 오류가 발생했습니다.");
+            throw new RuntimeException("트랜잭션 로직을 실행하는 중 Connection 오류가 발생했습니다.", e);
         }
     }
 }
