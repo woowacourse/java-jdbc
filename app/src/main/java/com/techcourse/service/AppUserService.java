f787fb401006d0bac6696125006d824f6dc774ed
(minjae8563) <(minjae8563@naver.com)>
2025-10-27 00:22:12 +0900

    feat: UserService 구현하도록 수정

---
+++ app/src/main/java/com/techcourse/service/AppUserService.java
@@ -0,0 +1,45 @@
+package com.techcourse.service;
+
+import com.techcourse.dao.UserDao;
+import com.techcourse.dao.UserHistoryDao;
+import com.techcourse.domain.User;
+import com.techcourse.domain.UserHistory;
+import com.techcourse.service.TransactionService.Transaction;
+
+public class AppUserService implements UserService{
+
+    private final TransactionService transactionService;
+    private final UserDao userDao;
+    private final UserHistoryDao userHistoryDao;
+
+    public AppUserService(TransactionService transactionService, final UserDao userDao, final UserHistoryDao userHistoryDao) {
+        this.transactionService = transactionService;
+        this.userDao = userDao;
+        this.userHistoryDao = userHistoryDao;
+    }
+
+    @Override
+    public User findById(final long id) {
+        return userDao.findById(id);
+    }
+
+    @Override
+    public void save(User user) {
+        userDao.insert(user);
+    }
+
+    public void insert(final User user) {
+        userDao.insert(user);
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createBy) {
+        try(Transaction transaction = transactionService.begin()) {
+            final var user = findById(id);
+            user.changePassword(newPassword);
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
+            transaction.commit();
+        }
+    }
+}
