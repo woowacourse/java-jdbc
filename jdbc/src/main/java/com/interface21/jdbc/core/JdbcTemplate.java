131245db46c5a51a05433b369f428ab17697d07b
rladmstn <oz33177351@gmail.com>
2025-10-26 18:16:42 +0900

    feat: 트랜잭션에 포함된 dataSource 가져오도록 JdbcTemplate 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -2,6 +2,7 @@ package com.interface21.jdbc.core;
 
 import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.IncorrectResultSizeException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -23,22 +24,8 @@ public class JdbcTemplate {
     }
 
     public void update(String sql, Object... params){
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)){
-            log.debug("query : {}", sql);
-            setParameters(pstmt, params);
-            pstmt.executeUpdate();
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e.getMessage(), e);
-        }
-    }
-
-    /*
-    Connection 공유 버전 오버로딩
-     */
-    public void update(Connection connection, String sql, Object... params){
-        try (PreparedStatement pstmt = connection.prepareStatement(sql)){
+        Connection conn = DataSourceUtils.getConnection(dataSource);
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)){
             log.debug("query : {}", sql);
             setParameters(pstmt, params);
             pstmt.executeUpdate();
@@ -49,9 +36,8 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params){
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement pstmt = conn.prepareStatement(sql)){
-
+        Connection conn = DataSourceUtils.getConnection(dataSource);
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)){
             log.debug("query : {}", sql);
             setParameters(pstmt, params);
             return executionResult(rowMapper, pstmt);
