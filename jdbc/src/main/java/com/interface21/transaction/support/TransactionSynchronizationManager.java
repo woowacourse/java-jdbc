36f6a17caf2c4a3d16ad8ad465a5e207aeee1f9c
DEV_WOONG <ywcsuwon@gmail.com>
2025-10-27 15:07:15 +0900

    feat: TransactionSynchronizationManager 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,43 @@
 package com.interface21.transaction.support;
 
+import com.interface21.dao.DataAccessException;
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
+        Connection connection = dataSourceConnectionMap.get(key);
+        if (connection == null) {
+            throw new DataAccessException("해당하는 리소스가 없습니다.");
+        }
+        return connection;
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
+        if (dataSourceConnectionMap.containsKey(key)) {
+            throw new DataAccessException("이미 바인딩되어있는 리소스 입니다.");
+        }
+        dataSourceConnectionMap.put(key, value);
     }
 
-    public static Connection unbindResource(DataSource key) {
-        return null;
+    public static void unbindResource(DataSource key) {
+        Map<DataSource, Connection> dataSourceConnectionMap = resources.get();
+        if (!dataSourceConnectionMap.containsKey(key)) {
+            throw new DataAccessException("존재하지 않는 key입니다.");
+        }
+        dataSourceConnectionMap.remove(key);
+
+        if (dataSourceConnectionMap.isEmpty()) {
+            resources.remove();
+        }
     }
 }
