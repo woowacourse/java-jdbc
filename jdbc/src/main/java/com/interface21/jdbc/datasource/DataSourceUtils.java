12a1b3fae04d489e67667b3c55744a8ddf157dbb
julie <7jujubuilds@gmail.com>
2025-10-27 15:04:30 +0900

    refactor: DataSourceUtils 내 불필요한 if 검증 제거
    - 트랜잭션 매니저에서 리소스를 unbind하는 로직은 bind된 리소스가 없어도 예외가 발생하지 않기 때문에 auto-commit 여부와 상관없이 호출하도록 하여 if문 제거

--- jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
@@ -30,9 +30,7 @@ public abstract class DataSourceUtils {
 
     public static void releaseConnection(Connection connection, DataSource dataSource) {
         try {
-            if (!connection.getAutoCommit()) {
-                TransactionSynchronizationManager.unbindResource(dataSource);
-            }
+            TransactionSynchronizationManager.unbindResource(dataSource);
             connection.close();
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to close JDBC Connection");
