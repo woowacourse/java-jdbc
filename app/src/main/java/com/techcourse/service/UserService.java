573e1fb77e588890c14847bb735917cde12e0c90
Lee Dong Yeop <s01054830455@gmail.com>
2025-10-27 18:07:55 +0900

    feat: UserService 메서드 내부에 트랜잭션 처리 로직 추가

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,9 +1,12 @@
 package com.techcourse.service;
 
+import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class UserService {
 
@@ -16,11 +19,29 @@ public class UserService {
     }
 
     public User findById(final long id) {
-        return userDao.findById(id);
+        try (Connection connection = DataSourceConfig.getInstance().getConnection()) {
+            connection.setAutoCommit(false);
+
+            final var user = userDao.findById(connection, id);
+
+            connection.commit();
+
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void insert(final User user) {
-        userDao.insert(user);
+        try (Connection connection = DataSourceConfig.getInstance().getConnection()) {
+            connection.setAutoCommit(false);
+
+            userDao.insert(connection, user);
+
+            connection.commit();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void changePassword(final long id, final String newPassword, final String createBy) {
