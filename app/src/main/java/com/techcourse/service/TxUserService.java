3419563c65b17ad63b96802a57d983b94666aa4f
supernovaMK <minkikim2002@gmail.com>
2025-10-27 21:03:17 +0900

    feat: UserService 인터페이스 구현 및 Service 관심사 분리

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,62 @@
+package com.techcourse.service;
+
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
+import com.interface21.transaction.support.TransactionSynchronizationManager;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+public class TxUserService implements UserService {
+
+    private final UserService userService;
+    private final DataSource dataSource = DataSourceConfig.getInstance();
+
+    public TxUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public User findById(long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void save(User user) {
+        userService.save(user);
+    }
+
+    @Override
+    public void changePassword(long id, String newPassword, String createdBy) {
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
+        try {
+            connection.setAutoCommit(false);
+            userService.changePassword(id, newPassword, createdBy);
+            connection.commit();
+        } catch (Exception e) {
+            rollbackTransaction(connection);
+            throw new DataAccessException(e);
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    private void closeConnection(Connection connection) {
+        if (connection != null) {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+            TransactionSynchronizationManager.unbindResource(dataSource);
+        }
+    }
+
+    private void rollbackTransaction(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.rollback();
+            }
+        } catch (SQLException rollbackException) {
+            throw new DataAccessException(rollbackException);
+        }
+    }
+}
--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,76 +1,9 @@
 package com.techcourse.service;
 
-import com.interface21.dao.DataAccessException;
-import com.techcourse.config.DataSourceConfig;
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
-import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
-import java.sql.SQLException;
-import javax.sql.DataSource;
 
-public class UserService {
-
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
-    private final DataSource dataSource = DataSourceConfig.getInstance();
-
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-    }
-
-    public User findById(final long id) {
-        return userDao.findById(id)
-                .orElseThrow();
-    }
-
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
-
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        Connection connection = null;
-
-        try {
-            connection = dataSource.getConnection();
-
-            connection.setAutoCommit(false);
-
-            final var user = userDao.findById(connection,id)
-                    .orElseThrow();
-            user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(connection, new UserHistory(user, createBy));
-
-            connection.commit();
-        } catch (Exception e) {
-            rollbackTransaction(connection);
-            throw new DataAccessException(e);
-        } finally {
-            closeConnection(connection);
-        }
-    }
-
-    private static void closeConnection(Connection connection) {
-        try {
-            if (connection != null) {
-                connection.setAutoCommit(true);
-                connection.close();
-            }
-        } catch (SQLException closeException) {
-            throw new DataAccessException(closeException);
-        }
-    }
-
-    private static void rollbackTransaction(Connection connection) {
-        try {
-            if (connection != null) {
-                connection.rollback();
-            }
-        } catch (SQLException rollbackException) {
-            throw new DataAccessException(rollbackException);
-        }
-    }
+public interface UserService {
+    User findById(final long id);
+    void save(final User user);
+    void changePassword(final long id, final String newPassword, final String createdBy);
 }
