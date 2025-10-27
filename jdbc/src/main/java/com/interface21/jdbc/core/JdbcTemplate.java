5931f32d37596e6e3b3e58f5d837fd66e63993e1
ppparkta <pppppark2@gmail.com>
2025-10-27 14:03:34 +0900

    refactor: 중복된 기능 메서드 분리

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -31,19 +31,15 @@ public class JdbcTemplate {
     }
 
     public <T> List<T> query(String sql, RowMapper<T> rowMapper, Object... params) {
-        return execute(sql, preStmt -> {
-            try (ResultSet rs = preStmt.executeQuery()) {
-                List<T> results = new ArrayList<>();
-                while (rs.next()) {
-                    results.add(rowMapper.mapRow(rs));
-                }
-                return results;
-            }
-        }, params);
+        return execute(sql, getQueryCallback(rowMapper), params);
     }
 
     public <T> List<T> query(Connection connection, String sql, RowMapper<T> rowMapper, Object... params) {
-        return execute(connection, sql, preStmt -> {
+        return execute(connection, sql, getQueryCallback(rowMapper), params);
+    }
+
+    private <T> PreparedStatementCallback<List<T>> getQueryCallback(final RowMapper<T> rowMapper) {
+        return preStmt -> {
             try (ResultSet rs = preStmt.executeQuery()) {
                 List<T> results = new ArrayList<>();
                 while (rs.next()) {
@@ -51,22 +47,20 @@ public class JdbcTemplate {
                 }
                 return results;
             }
-        }, params);
+        };
     }
 
     public <T> T queryForObject(String sql, RowMapper<T> rowMapper, Object... params) {
         List<T> results = query(sql, rowMapper, params);
-        if (results.isEmpty()) {
-            throw new DataAccessException("데이터가 존재하지 않습니다");
-        }
-        if (results.size() > 1) {
-            throw new DataAccessException("한 개의 결과만을 반환해야 합니다: " + results.size());
-        }
-        return results.getFirst();
+        return getSingleResult(results);
     }
 
     public <T> T queryForObject(Connection connection, String sql, RowMapper<T> rowMapper, Object... params) {
         List<T> results = query(connection, sql, rowMapper, params);
+        return getSingleResult(results);
+    }
+
+    private <T> T getSingleResult(List<T> results) {
         if (results.isEmpty()) {
             throw new DataAccessException("데이터가 존재하지 않습니다");
         }
@@ -91,14 +85,7 @@ public class JdbcTemplate {
         Connection connection = null;
         try {
             connection = DataSourceUtils.getConnection(dataSource);
-            try (PreparedStatement preStmt = connection.prepareStatement(sql)) {
-                log.debug("query : {}", sql);
-                setParameters(params, preStmt);
-                return callback.doInPreparedStatement(preStmt);
-            }
-        } catch (SQLException e) {
-            log.error(e.getMessage(), e);
-            throw new DataAccessException(e);
+            return execute(connection, sql, callback, params);
         } finally {
             DataSourceUtils.releaseConnection(connection, dataSource);
         }
