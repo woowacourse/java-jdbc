91214c492b2bcac6b561c71abdcac46801be671e
julie <7jujubuilds@gmail.com>
2025-10-27 15:09:13 +0900

    refactor: TxUserService 내 모든 메서드에 트랜잭션 경계 적용

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -7,6 +7,7 @@ import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.function.Supplier;
 import javax.sql.DataSource;
 
 public class TxUserService implements UserService {
@@ -21,29 +22,51 @@ public class TxUserService implements UserService {
 
     @Override
     public User findById(final long id) {
-        return userService.findById(id);
+        return executeInTransaction(() -> userService.findById(id));
     }
 
     @Override
     public User findByAccount(final String account) {
-        return userService.findByAccount(account);
+        return executeInTransaction(() -> userService.findByAccount(account));
     }
 
     @Override
     public void save(final User user) {
-        userService.save(user);
+        executeInTransaction(() -> userService.save(user));
     }
 
     @Override
     public void changePassword(final long id, final String newPassword, final String createBy) {
+        executeInTransaction(() -> userService.changePassword(id, newPassword, createBy));
+    }
+
+    private void executeInTransaction(final Runnable runnable) {
         Connection connection = DataSourceUtils.getConnection(dataSource);
         TransactionSynchronizationManager.bindResource(dataSource, connection);
         try {
             connection.setAutoCommit(false);
 
-            userService.changePassword(id, newPassword, createBy);
+            runnable.run();
 
             connection.commit();
+        } catch (SQLException e) {
+            handleRollback(connection);
+            throw new RuntimeException(e);
+        } finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
+        }
+    }
+
+    private <T> T executeInTransaction(final Supplier<T> supplier) {
+        Connection connection = DataSourceUtils.getConnection(dataSource);
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+        try {
+            connection.setAutoCommit(false);
+
+            T result = supplier.get();
+
+            connection.commit();
+            return result;
         } catch (SQLException e) {
             handleRollback(connection);
             throw new RuntimeException(e);
