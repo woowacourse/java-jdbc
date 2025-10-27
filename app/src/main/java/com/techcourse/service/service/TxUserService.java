b3109bc068a0bed0810b7aa686fe6367136fbc2b
DEV_WOONG <ywcsuwon@gmail.com>
2025-10-28 02:01:47 +0900

    feat: UserService 추상화

---
+++ app/src/main/java/com/techcourse/service/service/TxUserService.java
@@ -0,0 +1,44 @@
+package com.techcourse.service.service;
+
+import com.interface21.dao.DataAccessException;
+import com.techcourse.domain.User;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+public class TxUserService implements UserService {
+
+    private final PlatformTransactionManager transactionManager;
+    private final UserService userService;
+
+    public TxUserService(
+            final PlatformTransactionManager transactionManager,
+            final UserService userService
+    ) {
+        this.transactionManager = transactionManager;
+        this.userService = userService;
+    }
+
+    @Override
+    public User findById(final long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void insert(final User user) {
+        userService.insert(user);
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createBy) {
+        /* ===== 트랜잭션 영역 ===== */
+        final var transactionStatus = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            userService.changePassword(id, newPassword, createBy);
+        } catch (RuntimeException e) {
+            transactionManager.rollback(transactionStatus);
+            throw new DataAccessException(e);
+        }
+        transactionManager.commit(transactionStatus);
+    }
+}
