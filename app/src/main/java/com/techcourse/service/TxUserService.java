ce0f941e5fb4dc528868279f408b002738a03da8
goohong <goohong.chung@gmail.com>
2025-10-27 15:07:54 +0900

    refactor: UserService에서 트랜잭션 로직 분리

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,37 @@
+package com.techcourse.service;
+
+import com.interface21.transaction.TransactionTemplate;
+import com.techcourse.config.DataSourceConfig;
+import com.techcourse.domain.User;
+
+public class TxUserService implements UserService {
+
+    private final UserService userService;
+    private final TransactionTemplate transactionTemplate;
+
+    public TxUserService(final UserService userService) {
+        this.userService = userService;
+        this.transactionTemplate = new TransactionTemplate(DataSourceConfig.getInstance());
+    }
+
+    @Override
+    public User findById(final long id) {
+        return transactionTemplate.executeReadOnly(() -> userService.findById(id));
+    }
+
+    @Override
+    public void save(final User user) {
+        transactionTemplate.execute(() -> {
+            userService.save(user);
+            return null;
+        });
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createBy) {
+        transactionTemplate.execute(() -> {
+            userService.changePassword(id, newPassword, createBy);
+            return null;
+        });
+    }
+}
