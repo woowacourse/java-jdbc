07047a273cb6aabe74df6a119a65f86a60b6d92c
saera <yook.saera@gmail.com>
2025-10-27 15:22:28 +0900

    fix: 테스트 실행 시 테이블 삭제 후 생성하도록 수정

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,4 +1,7 @@
-create table if not exists users (
+drop table if exists users;
+drop table if exists user_history;
+
+create table users (
     id bigint auto_increment,
     account varchar(100) not null,
     password varchar(100) not null,
@@ -6,7 +9,7 @@ create table if not exists users (
     primary key(id)
 );
 
-create table if not exists user_history (
+create table user_history (
     id bigint auto_increment,
     user_id bigint not null,
     account varchar(100) not null,
