63018945a183ae8d2de24e532a76aca9d615704a
ppparkta <pppppark2@gmail.com>
2025-10-27 13:38:30 +0900

    refactor: 트랜잭션 동기화 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,6 +1,8 @@
 package com.techcourse.service;
 
 import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
+import com.interface21.transaction.support.TransactionSynchronizationManager;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
@@ -26,10 +28,6 @@ public class UserService {
         return userDao.findById(id);
     }
 
-    public User findById(final Connection connection, final long id) {
-        return userDao.findById(connection, id);
-    }
-
     public void insert(final User user) {
         userDao.insert(user);
     }
@@ -37,20 +35,21 @@ public class UserService {
     public void changePassword(final long id, final String newPassword, final String createBy) {
         Connection connection = null;
         try {
-            connection = dataSource.getConnection();
+            connection = DataSourceUtils.getConnection(dataSource);
             connection.setAutoCommit(false);
 
-            final var user = findById(connection, id);
+            final var user = findById(id);
             user.changePassword(newPassword);
-            userDao.update(connection, user);
-            userHistoryDao.log(connection, new UserHistory(user, createBy));
+            userDao.update(user);
+            userHistoryDao.log(new UserHistory(user, createBy));
 
             connection.commit();
         } catch (SQLException e) {
             rollbackTransaction(connection);
             throw new DataAccessException(e);
         } finally {
-            closeConnection(connection);
+            DataSourceUtils.releaseConnection(connection, dataSource);
+            TransactionSynchronizationManager.unbindResource(dataSource);
         }
     }
 
@@ -60,16 +59,6 @@ public class UserService {
                 connection.rollback();
             } catch (SQLException e) {
                 throw new DataAccessException("트랜잭션을 롤백할 수 없습니다!", e);
-            }
-        }
-    }
-
-    private void closeConnection(Connection connection) {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                throw new DataAccessException("DB 커넥션을 닫는데 실패했습니다.", e);
             }
         }
     }
--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -1,6 +1,7 @@
 package com.interface21.jdbc.core;
 
 import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -87,15 +88,19 @@ public class JdbcTemplate {
     }
 
     private <T> T execute(String sql, PreparedStatementCallback<T> callback, Object... params) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement preStmt = connection.prepareStatement(sql)
-        ) {
-            log.debug("query : {}", sql);
-            setParameters(params, preStmt);
-            return callback.doInPreparedStatement(preStmt);
+        Connection connection = null;
+        try {
+            connection = DataSourceUtils.getConnection(dataSource);
+            try (PreparedStatement preStmt = connection.prepareStatement(sql)) {
+                log.debug("query : {}", sql);
+                setParameters(params, preStmt);
+                return callback.doInPreparedStatement(preStmt);
+            }
         } catch (SQLException e) {
             log.error(e.getMessage(), e);
             throw new DataAccessException(e);
+        } finally {
+            DataSourceUtils.releaseConnection(connection, dataSource);
         }
     }
 
