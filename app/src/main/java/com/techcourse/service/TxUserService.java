3e783cc27beb79739d525930e7cdef94dcb2c34c
yesjuhee <juhee0924@g.skku.edu>
2025-10-27 14:12:58 +0900

    refactor: `TransactionSynchronizationManager`를 `DataSourceUtils`내부에서만 사용하도록 수정

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -2,7 +2,6 @@ package com.techcourse.service;
 
 import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.datasource.DataSourceUtils;
-import com.interface21.transaction.support.TransactionSynchronizationManager;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import java.sql.Connection;
@@ -45,7 +44,6 @@ public class TxUserService implements UserService {
             throw new DataAccessException(e);
         } finally {
             DataSourceUtils.releaseConnection(conn, dataSource);
-            TransactionSynchronizationManager.unbindResource(dataSource);
         }
     }
 
