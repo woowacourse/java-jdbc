9144235c1009378484a2f6f0f021da6fa3768a5d
AppleMint98 <applemint9821@gmail.com>
2025-10-27 14:18:17 +0900

    feat: ThreadLocal Map 초기화 로직 추가

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -7,33 +7,23 @@ import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {
     }
 
     public static Connection getConnection(final DataSource dataSource) {
         final Map<DataSource, Connection> map = resources.get();
-        if (map == null) {
-            return null;
-        }
         return map.get(dataSource);
     }
 
     public static void bindConnection(final DataSource dataSource, final Connection connection) {
         Map<DataSource, Connection> map = resources.get();
-        if (map == null) {
-            map = new HashMap<>();
-            resources.set(map);
-        }
         map.put(dataSource, connection);
     }
 
     public static Connection unbindConnection(final DataSource dataSource) {
         final Map<DataSource, Connection> map = resources.get();
-        if (map == null) {
-            return null;
-        }
         final Connection connection = map.remove(dataSource);
         if (map.isEmpty()) {
             resources.remove();
@@ -43,7 +33,7 @@ public abstract class TransactionSynchronizationManager {
 
     public static boolean hasConnection(final DataSource dataSource) {
         final Map<DataSource, Connection> map = resources.get();
-        return map != null && map.containsKey(dataSource);
+        return map.containsKey(dataSource);
     }
 
     public static boolean hasNotConnection(final DataSource dataSource) {
