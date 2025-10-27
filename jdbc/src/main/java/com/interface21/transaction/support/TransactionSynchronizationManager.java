cee90264d24df284d0ddbe5bbde03e971c79d58a
DongchannN <ldc991104@gmail.com>
2025-10-27 20:07:14 +0900

    feat: TransactionSynchronizationManager 기능 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -2,6 +2,7 @@ package com.interface21.transaction.support;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TransactionSynchronizationManager {
@@ -11,13 +12,32 @@ public abstract class TransactionSynchronizationManager {
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
-        return null;
+        if (resources.get() == null) {
+            return null;
+        }
+        return resources.get().get(key);
+    }
+
+    public static boolean hasResource(DataSource key) {
+        if (resources.get() == null) {
+            return false;
+        }
+        return resources.get().containsKey(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            map = new HashMap<>();
+            resources.set(map);
+        }
+        resources.get().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        if (resources.get() == null) {
+            throw new IllegalStateException("Cannot Unbind Uninitialized resource");
+        }
+        return resources.get().remove(key);
     }
 }
