37eb953565fe18a54951438d0827a20652066cc1
eoehd1ek <eoehd2ek@gmail.com>
2025-10-29 02:15:20 +0900

    feat: DataSourceUtils 구현

--- jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
+++ jdbc/src/main/java/com/interface21/jdbc/datasource/DataSourceUtils.java
@@ -2,32 +2,33 @@ package com.interface21.jdbc.datasource;
 
 import com.interface21.jdbc.CannotGetJdbcConnectionException;
 import com.interface21.transaction.support.TransactionSynchronizationManager;
-
-import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javax.sql.DataSource;
 
-// 4단계 미션에서 사용할 것
 public abstract class DataSourceUtils {
 
-    private DataSourceUtils() {}
+    private DataSourceUtils() {
+    }
 
-    public static Connection getConnection(DataSource dataSource) throws CannotGetJdbcConnectionException {
-        Connection connection = TransactionSynchronizationManager.getResource(dataSource);
+    public static Connection getConnection(final DataSource dataSource) throws CannotGetJdbcConnectionException {
+        final Connection connection = TransactionSynchronizationManager.getResource(dataSource);
         if (connection != null) {
             return connection;
         }
 
         try {
-            connection = dataSource.getConnection();
-            TransactionSynchronizationManager.bindResource(dataSource, connection);
-            return connection;
+            return dataSource.getConnection();
         } catch (SQLException ex) {
             throw new CannotGetJdbcConnectionException("Failed to obtain JDBC Connection", ex);
         }
     }
 
-    public static void releaseConnection(Connection connection, DataSource dataSource) {
+    public static void releaseConnection(final Connection connection, final DataSource dataSource) {
+        if ((connection == null) || (connection == TransactionSynchronizationManager.getResource(dataSource))) {
+            return;
+        }
+
         try {
             connection.close();
         } catch (SQLException ex) {
