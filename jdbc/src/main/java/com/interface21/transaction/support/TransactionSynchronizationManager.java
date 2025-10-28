74fa2a1597709d3f5cbb9818b5fe5a4816c8c83d
minSsan <msp1125@hanyang.ac.kr>
2025-10-28 10:40:38 +0900

    feat: TransactionSynchronizationManager 코드 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -2,22 +2,33 @@ package com.interface21.transaction.support;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
+    static {
+        resources.set(new HashMap<>());
+    }
+
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
-        return null;
+        return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        final var changed = resources.get();
+        changed.put(key, value);
+        resources.set(changed);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        final var changed = resources.get();
+        final var removed = changed.remove(key);
+        resources.set(changed);
+        return removed;
     }
 }
