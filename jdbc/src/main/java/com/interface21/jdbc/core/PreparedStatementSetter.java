3045d06e79bf114dcf8638de43a161a4fede9c69
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-10-28 11:25:06 +0900

    feat: PreparedStatementSetter 구현

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/PreparedStatementSetter.java
@@ -0,0 +1,8 @@
+package com.interface21.jdbc.core;
+
+import java.sql.PreparedStatement;
+
+@FunctionalInterface
+public interface PreparedStatementSetter {
+    void setValues(PreparedStatement pstmt);
+}
