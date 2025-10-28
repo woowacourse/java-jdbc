691e0d9a1866b8a203ad96defce5dd3144dcbd14
eoehd1ek <eoehd2ek@gmail.com>
2025-10-29 01:02:32 +0900

    feat: TransactionSynchronizationManager 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,53 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
 
-public abstract class TransactionSynchronizationManager {
+public class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
-
-    public static Connection getResource(DataSource key) {
-        return null;
+    private TransactionSynchronizationManager() {
     }
 
-    public static void bindResource(DataSource key, Connection value) {
+    public static Connection getResource(final DataSource key) {
+        final Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+        return map.get(key);
     }
 
-    public static Connection unbindResource(DataSource key) {
-        return null;
+    public static void bindResource(final DataSource key, final Connection value) {
+        if (getResource(key) != null) {
+            throw new IllegalStateException("Already bound resource, key: " + key);
+        }
+
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            // 쓰레드 로컬에 커넥션을 관리하는 Map이 없는 경우 생성
+            map = new HashMap<>();
+            resources.set(map);
+        }
+        map.put(key, value);
+    }
+
+    public static Connection unbindResource(final DataSource key) {
+        final Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            throw new IllegalStateException("Not bound resource, key: " + key);
+        }
+
+        final Connection value = map.remove(key);
+        if (value == null) {
+            throw new IllegalStateException("Not bound resource, key: " + key);
+        }
+
+        if (map.isEmpty()) {
+            resources.remove();
+        }
+        return value;
     }
 }
