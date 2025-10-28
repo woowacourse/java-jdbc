ba6fffa71348841610560da5fab2f99af63d5ec7
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2025-10-28 13:17:40 +0900

    feat: DataSource 및 트랜잭션 리소스 관리 로직 구현

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,40 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> threadResources = resources.get();
+        if (threadResources == null) {
+            return null;
+        }
+        return threadResources.get(key);
+
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> threadResources = resources.get();
+        if (threadResources == null) {
+            threadResources = new HashMap<>();
+            resources.set(threadResources);
+        }
+        threadResources.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> threadResources = resources.get();
+        if (threadResources == null) {
+            return null;
+        }
+        return threadResources.remove(key);
     }
 }
