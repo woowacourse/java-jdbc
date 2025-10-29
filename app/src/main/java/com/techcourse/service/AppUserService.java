1b20607bfa2bc4e6384ef1f8dc6f898fb39fe707
eoehd1ek <eoehd2ek@gmail.com>
2025-10-29 18:16:07 +0900

    refactor: Service 계층 트랜잭션 메서드 미사용하도록 변경

--- app/src/main/java/com/techcourse/service/AppUserService.java
+++ app/src/main/java/com/techcourse/service/AppUserService.java
@@ -1,11 +1,9 @@
 package com.techcourse.service;
 
-import com.interface21.jdbc.datasource.DataSourceUtils;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
 import javax.sql.DataSource;
 
 public class AppUserService implements UserService {
@@ -32,10 +30,9 @@ public class AppUserService implements UserService {
 
     @Override
     public void changePassword(final long id, final String newPassword, final String createBy) {
-        final Connection connection = DataSourceUtils.getConnection(dataSource);
-        final User user = userDao.transactionFindById(connection, id);
+        final User user = userDao.findById(id);
         user.changePassword(newPassword);
-        userDao.transactionUpdate(connection, user);
-        userHistoryDao.log(connection, new UserHistory(user, createBy));
+        userDao.update(user);
+        userHistoryDao.log(new UserHistory(user, createBy));
     }
 }
