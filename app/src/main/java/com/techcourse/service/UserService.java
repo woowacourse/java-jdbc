ad1b1630b4a818630a02b3c2491f55aeca9e67dd
Hyeonsu Lee <leehyeonsu4888@gmail.com>
2025-10-29 20:20:59 +0900

    [4단계 - Transaction synchronization 적용하기] 한스(이현수) 미션 제출합니다. (#1168)
    * feat: AppUserService, TxUserService 분리 / Transaction synchronization 적용
    
    * refactor: ThreadLocal withInitial 적용
    
    * refactor: 롤백에서 발생한 예외가 원본 예외를 덮어씌우지 않도록 addSuppressed 적용

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,51 +1,12 @@
 package com.techcourse.service;
 
-import com.interface21.dao.DataAccessException;
-import com.techcourse.dao.UserDao;
-import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
-import com.techcourse.domain.UserHistory;
-import java.sql.Connection;
-import java.sql.SQLException;
-import javax.sql.DataSource;
 
-public class UserService {
+public interface UserService {
 
-    private final UserDao userDao;
-    private final UserHistoryDao userHistoryDao;
-    private final DataSource dataSource;
+    User findById(final long id);
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, DataSource dataSource) {
-        this.userDao = userDao;
-        this.userHistoryDao = userHistoryDao;
-        this.dataSource = dataSource;
-    }
+    void save(final User user);
 
-    public User findById(final long id) {
-        return userDao.findById(id).orElseThrow(() -> new DataAccessException("존재하지 않는 유저입니다."));
-    }
-
-    public void insert(final User user) {
-        userDao.insert(user);
-    }
-
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-
-        try (Connection connection = dataSource.getConnection()) {
-            connection.setAutoCommit(false);
-            try {
-                userDao.update(connection, user);
-                userHistoryDao.log(connection, new UserHistory(user, createBy));
-
-                connection.commit();
-            } catch (Exception e) {
-                connection.rollback();
-                throw new DataAccessException(e);
-            }
-        } catch (SQLException e) {
-            throw new DataAccessException(e);
-        }
-    }
+    void changePassword(final long id, final String newPassword, final String createdBy);
 }
--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,37 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources = ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {}
 
+    public static boolean hasConnection(DataSource dataSource) {
+        return resources.get().containsKey(dataSource);
+    }
+
     public static Connection getResource(DataSource key) {
-        return null;
+        return resources.get().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        resources.get().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        final Map<DataSource, Connection> resourceMap = resources.get();
+        if (!resourceMap.containsKey(key)) {
+            throw new IllegalStateException("unbind할 리소스가 존재하지 않습니다.");
+        }
+        return resourceMap.remove(key);
+    }
+
+    public static void clear() {
+        resources.remove();
     }
 }
