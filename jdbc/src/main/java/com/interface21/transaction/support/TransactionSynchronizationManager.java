1ea81736d4028435c7ddf3ff78bc0827b35053bf
egaeng09 <egaeng09@gmail.com>
2025-10-29 15:36:32 +0900

    feat: TransactionSynchronizationManager 구현
    - ThreadLocal을 기반으로 DataSource별 Connection 관리

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -11,13 +11,31 @@ public abstract class TransactionSynchronizationManager {
     private TransactionSynchronizationManager() {}
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+        return map.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            map = new java.util.HashMap<>();
+            resources.set(map);
+        }
+        map.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> map = resources.get();
+        if (map == null) {
+            return null;
+        }
+        Connection connection = map.remove(key);
+        if (map.isEmpty()) {
+            resources.remove();
+        }
+        return connection;
     }
 }
