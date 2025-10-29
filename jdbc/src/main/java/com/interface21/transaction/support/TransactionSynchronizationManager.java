27b7510e615580c4699c77c5e7127f8295c0091e
ykmxxi <ykmxxi97@gmail.com>
2025-10-29 14:06:37 +0900

    refactor: ThreadLocal 초기화 정팩메 적용

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -8,25 +8,19 @@ import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> RESOURCES = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> RESOURCES =
+        ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {
     }
 
-    public static Connection getResource(DataSource key) {
-        Map<DataSource, Connection> resource = RESOURCES.get();
-        if (resource == null) {
-            return null;
-        }
-        return resource.get(key);
+    public static Connection getResource(final DataSource key) {
+        return RESOURCES.get()
+            .get(key);
     }
 
-    public static void bindResource(DataSource key, Connection value) {
+    public static void bindResource(final DataSource key, final Connection value) {
         Map<DataSource, Connection> resource = RESOURCES.get();
-        if (resource == null) {
-            resource = new HashMap<>();
-            RESOURCES.set(resource);
-        }
         if (resource.containsKey(key)) {
             throw new IllegalStateException("이미 연결된 바인딩된 커넥션이 존재합니다.");
         }
@@ -34,10 +28,7 @@ public abstract class TransactionSynchronizationManager {
     }
 
     public static Connection unbindResource(DataSource key) {
-        Map<DataSource, Connection> resource = RESOURCES.get();
-        if (resource == null) {
-            return null;
-        }
-        return resource.remove(key);
+        return RESOURCES.get()
+            .remove(key);
     }
 }
