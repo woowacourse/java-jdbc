3b59628aebe050c13d1fe82cef4f42ccba0f4b61
egaeng09 <egaeng09@gmail.com>
2025-10-30 01:18:52 +0900

    refactor: 트랜잭션 적용 메서드 분리

--- app/src/main/java/com/techcourse/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -30,22 +30,30 @@ public class TxUserService implements UserService {
 
     @Override
     public void changePassword(final long id, final String newPassword, final String createdBy) {
-        Connection connection = DataSourceUtils.getConnection(dataSource);
+        executeInTransaction(() -> userService.changePassword(id, newPassword, createdBy));
+    }
+
+    private void executeInTransaction(final Runnable runnable) {
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
 
         try {
             connection.setAutoCommit(false);
-            userService.changePassword(id, newPassword, createdBy);
+            runnable.run();
             connection.commit();
-        } catch (Exception e) {
-            try {
-                connection.rollback();
-            } catch (SQLException rollbackEx) {
-                e.addSuppressed(rollbackEx);
-            }
-            throw new DataAccessException("비밀번호 변경 실패", e);
+        } catch (final Exception e) {
+            rollback(connection, e);
         } finally {
             DataSourceUtils.releaseConnection(connection, dataSource);
             TransactionSynchronizationManager.unbindResource(dataSource);
         }
+    }
+
+    private void rollback(final Connection connection, final Exception originalException) {
+        try {
+            connection.rollback();
+        } catch (final SQLException rollbackException) {
+            originalException.addSuppressed(rollbackException);
+        }
+        throw new DataAccessException("트랜잭션 실행 실패", originalException);
     }
 }
