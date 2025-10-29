7b39efc3e1b96326313863bff99743d2fc506c71
시원 <121426422+seaniiio@users.noreply.github.com>
2025-10-29 15:42:48 +0900

    [3단계 - Transaction 적용하기] 메이(김시원) 미션 제출합니다. (#1131)
    * refactor: UserServiceTest의 `@Disabled` 제거
    
    * refactor: 비밀번호 변경 작업에 트랜잭션 적용
    
    * refactor: 서비스 코드에서 트랜잭션 관리 로직 분리
    
    * refactor: PreparedStatement에 파라미터 세팅 전에 null 여부 체크
    
    * refactor: UserHistoryDao에서 JdbcTemplate을 이용하도록 수정

--- jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
+++ jdbc/src/main/java/com/interface21/jdbc/core/JdbcTemplate.java
@@ -25,6 +25,10 @@ public class JdbcTemplate {
         execute(sql, PreparedStatement::executeUpdate, parameters);
     }
 
+    public void update(final Connection conn, final String sql, final Object... parameters) {
+        execute(conn, sql, PreparedStatement::executeUpdate, parameters);
+    }
+
     public <T> List<T> query(final String sql, final RowMapper<T> rowMapper, final Object... parameters) {
         return execute(sql, (pstmt) -> {
             try (final ResultSet rs = pstmt.executeQuery()) {
@@ -60,7 +64,22 @@ public class JdbcTemplate {
         }
     }
 
+    private <T> T execute(final Connection conn, final String sql, final PreparedStatementExecutor<T> executor,
+                          final Object... parameters) {
+        try (final PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            setStatementParameters(pstmt, parameters);
+            log.debug("query : {}", sql);
+            return executor.execute(pstmt);
+        } catch (SQLException e) {
+            log.error(e.getMessage(), e);
+            throw new JdbcExecutionException(String.format("\"%s\" 쿼리 실행 중 오류가 발생했습니다.", sql), e);
+        }
+    }
+
     private void setStatementParameters(final PreparedStatement pstmt, final Object[] parameters) throws SQLException {
+        if (parameters == null) {
+            throw new JdbcExecutionException(String.format("쿼리에 필요한 파라미터가 전달되지 않았습니다."));
+        }
         for (int i = 0; i < parameters.length; i++) {
             pstmt.setObject(i + 1, parameters[i]);
         }
---
+++ jdbc/src/main/java/com/interface21/transaction/TransactionManager.java
@@ -0,0 +1,31 @@
+package com.interface21.transaction;
+
+import com.interface21.dao.DataAccessException;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+public class TransactionManager {
+
+    private final DataSource dataSource;
+
+    public TransactionManager(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void executeTransaction(final TransactionalAction action) {
+        try (final var connection = dataSource.getConnection();) {
+            connection.setAutoCommit(false);
+
+            try {
+                action.execute(connection);
+                connection.commit();
+            } catch (Exception e) {
+                connection.rollback();
+                throw new DataAccessException("트랜잭션 내부 작업 수행 중 오류가 발생하여 롤백이 수행되었습니다.");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("트랜잭션 로직을 실행하는 중 Connection 오류가 발생했습니다.");
+        }
+    }
+}
