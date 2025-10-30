c0c7c42b45850a07ddafef1d5345d5b7481c5641
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-10-30 13:02:30 +0900

    test: Connection 관리 및 모든 메서드에 Connection 전달하도록 수정

--- app/src/test/java/com/techcourse/service/UserServiceTest.java
+++ app/src/test/java/com/techcourse/service/UserServiceTest.java
@@ -1,45 +1,61 @@
 package com.techcourse.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.core.JdbcTemplate;
+import com.interface21.jdbc.core.TransactionTemplate;
 import com.techcourse.config.DataSourceConfig;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.support.jdbc.init.DatabasePopulatorUtils;
-import com.interface21.dao.DataAccessException;
-import com.interface21.jdbc.core.JdbcTemplate;
+import javax.sql.DataSource;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.sql.Connection;
+import java.sql.SQLException;
 
-@Disabled
 class UserServiceTest {
 
     private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
     private UserDao userDao;
+    private Connection connection;
 
     @BeforeEach
-    void setUp() {
-        this.jdbcTemplate = new JdbcTemplate(DataSourceConfig.getInstance());
-        this.userDao = new UserDao(jdbcTemplate);
+    void setUp() throws SQLException {
+        dataSource = DataSourceConfig.getInstance();
+        DatabasePopulatorUtils.execute(dataSource);
 
-        DatabasePopulatorUtils.execute(DataSourceConfig.getInstance());
-        final var user = new User("gugu", "password", "hkkang@woowahan.com");
-        userDao.insert(user);
+        jdbcTemplate = new JdbcTemplate();
+        userDao = new UserDao(jdbcTemplate);
+        connection = dataSource.getConnection();
+        var user = new User("gugu", "password", "hkkang@woowahan.com");
+        userDao.insert(connection, user);
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        if (connection != null) {
+            connection.close();
+        }
     }
 
     @Test
     void testChangePassword() {
         final var userHistoryDao = new UserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var transactionTemplate = new TransactionTemplate(dataSource);
+        final var userService = new UserService(userDao, userHistoryDao, transactionTemplate);
 
         final var newPassword = "qqqqq";
         final var createBy = "gugu";
         userService.changePassword(1L, newPassword, createBy);
 
-        final var actual = userService.findById(1L);
+        final var actual = userService.findById(connection, 1L);
 
         assertThat(actual.getPassword()).isEqualTo(newPassword);
     }
@@ -48,16 +64,18 @@ class UserServiceTest {
     void testTransactionRollback() {
         // 트랜잭션 롤백 테스트를 위해 mock으로 교체
         final var userHistoryDao = new MockUserHistoryDao(jdbcTemplate);
-        final var userService = new UserService(userDao, userHistoryDao);
+        final var transactionTemplate = new TransactionTemplate(dataSource);
+        final var userService = new UserService(userDao, userHistoryDao, transactionTemplate);
 
         final var newPassword = "newPassword";
-        final var createBy = "gugu";
+        final var createdBy = "gugu";
         // 트랜잭션이 정상 동작하는지 확인하기 위해 의도적으로 MockUserHistoryDao에서 예외를 발생시킨다.
-        assertThrows(DataAccessException.class,
-                () -> userService.changePassword(1L, newPassword, createBy));
+        assertThrows(RuntimeException.class,
+                () -> userService.changePassword(1L, newPassword, createdBy));
 
-        final var actual = userService.findById(1L);
+        final var actual = userService.findById(connection, 1L);
 
         assertThat(actual.getPassword()).isNotEqualTo(newPassword);
     }
 }
+
