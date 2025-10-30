8f76ab711b6a53034e06a304df537051f6a8b1a3
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-10-30 13:01:56 +0900

    refactor: Connection 파라미터 추가

--- app/src/test/java/com/techcourse/service/MockUserHistoryDao.java
+++ app/src/test/java/com/techcourse/service/MockUserHistoryDao.java
@@ -5,6 +5,8 @@ import com.techcourse.domain.UserHistory;
 import com.interface21.dao.DataAccessException;
 import com.interface21.jdbc.core.JdbcTemplate;
 
+import java.sql.Connection;
+
 public class MockUserHistoryDao extends UserHistoryDao {
 
     public MockUserHistoryDao(final JdbcTemplate jdbcTemplate) {
@@ -12,7 +14,7 @@ public class MockUserHistoryDao extends UserHistoryDao {
     }
 
     @Override
-    public void log(final UserHistory userHistory) {
+    public void log(final Connection connection, final UserHistory userHistory) {
         throw new DataAccessException();
     }
 }
