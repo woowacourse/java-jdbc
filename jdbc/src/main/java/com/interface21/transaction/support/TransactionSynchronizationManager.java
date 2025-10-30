1127f96f9cb2b28bd29630d5cde40cd3c962839b
taek2222 <oot309@daum.net>
2025-10-30 09:30:31 +0900

    refactor: 트랜잭션 동기화 방식 셋팅

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,44 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Integer> count = new ThreadLocal<>();
 
-    private TransactionSynchronizationManager() {}
+    private TransactionSynchronizationManager() {
+    }
 
     public static Connection getResource(DataSource key) {
-        return null;
+        Map<DataSource, Connection> connectionMap = resources.get();
+        if (connectionMap == null) {
+            return null;
+        }
+
+        count.set(count.get() + 1);
+        return connectionMap.get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Map<DataSource, Connection> connectionMap = new HashMap<>();
+        connectionMap.put(key, value);
+        resources.set(connectionMap);
+
+        count.set(1);
     }
 
-    public static Connection unbindResource(DataSource key) {
-        return null;
+    public static Connection unbindResource(DataSource key, Connection connection) throws SQLException {
+        count.set(count.get() - 1);
+
+        if (count.get() == 0) {
+            connection.close();
+            return resources.get().remove(key);
+        }
+        return resources.get().get(key);
     }
 }
