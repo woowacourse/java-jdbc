4c61a7bba5cdfe28492e12e9ff5ed20704935f6d
kwonkeonhyeong <rjsgud19566@gmail.com>
2025-10-30 10:38:17 +0900

    refactor: static 초기화 제거

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -9,24 +9,34 @@ public abstract class TransactionSynchronizationManager {
     // ThreadLocal을 사용하여 스레드별로 트랜잭션 리소스와 동기화 정보를 관리한다.
     private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
 
-    static {
-        resources.set(new HashMap<>());
-    }
-
     private TransactionSynchronizationManager() {
     }
 
     // 현재 스레드에 바인딩된 리소스를 조회
     public static Connection getResource(DataSource key) {
-        return resources.get().get(key);
+        Map<DataSource, Connection> dataSources = resources.get();
+        if (dataSources == null) {
+            return null;
+        }
+        return dataSources.get(key);
     }
 
     // 현재 스레드에 리소스를 바인딩
     public static void bindResource(DataSource key, Connection value) {
-        resources.get().put(key, value);
+        Map<DataSource, Connection> dataSources = resources.get();
+        if (dataSources == null) {
+            dataSources = new HashMap<>();
+            resources.set(dataSources);
+        }
+
+        dataSources.put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return resources.get().remove(key);
+        Map<DataSource, Connection> dataSources = resources.get();
+        if (dataSources == null) {
+            return null;
+        }
+        return dataSources.remove(key);
     }
 }
