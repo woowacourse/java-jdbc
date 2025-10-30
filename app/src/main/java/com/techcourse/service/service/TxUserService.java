dcf4a6a588e63c2f4a9bcabb447513fcbf2d83e2
DEV_WOONG <ywcsuwon@gmail.com>
2025-10-30 09:53:53 +0900

    refactor: DataSource 의존성 주입 방식으로 변경

--- app/src/main/java/com/techcourse/service/service/TxUserService.java
+++ app/src/main/java/com/techcourse/service/service/TxUserService.java
@@ -1,9 +1,7 @@
 package com.techcourse.service.service;
 
 import com.interface21.dao.DataAccessException;
-import com.interface21.jdbc.datasource.DataSourceUtils;
 import com.interface21.transaction.support.TransactionSynchronizationManager;
-import com.techcourse.config.DataSourceConfig;
 import com.techcourse.domain.User;
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,12 +10,11 @@ import javax.sql.DataSource;
 public class TxUserService implements UserService {
 
     private final UserService userService;
-    private final DataSource dataSource = DataSourceConfig.getInstance();
+    private final DataSource dataSource;
 
-    public TxUserService(
-            final UserService userService
-    ) {
+    public TxUserService(final UserService userService, final DataSource dataSource) {
         this.userService = userService;
+        this.dataSource = dataSource;
     }
 
     @Override
