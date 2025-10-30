a8c8bd4aa87e8aff4161452812d2fdcf3c2517d3
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-10-30 13:04:06 +0900

    feat: 트랜잭션 관리 템플릿 클래스 구현

---
+++ jdbc/src/main/java/com/interface21/jdbc/core/TransactionTemplate.java
@@ -0,0 +1,33 @@
+package com.interface21.jdbc.core;
+
+import java.sql.Connection;
+import javax.sql.DataSource;
+
+public class TransactionTemplate {
+
+    private final DataSource dataSource;
+
+    public TransactionTemplate(final DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public <T> T execute(TransactionCallBack<T> transactionCallBack) throws Exception {
+        Connection connection = null;
+        try {
+            connection = dataSource.getConnection();
+            connection.setAutoCommit(false);
+            T result = transactionCallBack.execute(connection);
+            connection.commit();
+            return result;
+        } catch (Exception e) {
+            if (connection != null) {
+                connection.rollback();
+            }
+            throw e;
+        } finally {
+            if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+}
