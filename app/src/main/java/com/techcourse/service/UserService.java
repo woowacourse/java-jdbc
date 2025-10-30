009ae4ce98aede4481b6f5dc3879545139fb5bea
PARK HYUNG GYUN <qhk1198@Gmail.com>
2025-10-30 12:58:14 +0900

    refactor: TransactionTemplate 사용하도록 변경

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,32 +1,44 @@
 package com.techcourse.service;
 
+import com.interface21.jdbc.core.TransactionTemplate;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
 import com.techcourse.domain.UserHistory;
 
+import java.sql.Connection;
+
 public class UserService {
 
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final TransactionTemplate transactionTemplate;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final TransactionTemplate transactionTemplate) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.transactionTemplate = transactionTemplate;
     }
 
-    public User findById(final long id) {
-        return userDao.findById(id);
+    public User findById(final Connection connection, final long id) {
+        return userDao.findById(connection, id);
     }
 
-    public void insert(final User user) {
-        userDao.insert(user);
+    public void insert(final Connection connection, final User user) {
+        userDao.insert(connection, user);
     }
 
-    public void changePassword(final long id, final String newPassword, final String createBy) {
-        final var user = findById(id);
-        user.changePassword(newPassword);
-        userDao.update(user);
-        userHistoryDao.log(new UserHistory(user, createBy));
+    public void changePassword(final long id, final String newPassword, final String createdBy) {
+        try {
+            transactionTemplate.execute(connection -> {
+                final var user = findById(connection, id);
+                user.changePassword(newPassword);
+                userDao.update(connection, user);
+                userHistoryDao.log(connection, new UserHistory(user, createdBy));
+                return null;
+            });
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
