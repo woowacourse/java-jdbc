43fef4191c4c8525430f4a489387492830e5f7bc
Link <kingjh1125@gmail.com>
2025-10-31 11:46:25 +0900

    [4단계 - Transaction synchronization 적용하기] 링크(손준형) 미션 제출합니다. (#1215)
    * feat: UserService 인터페이스 생성
    
    * feat: AppUserService 구현
    
    * feat: 트랜잭션 서비스 추상화
    
    * test: UserServiceTest를 서비스 추상화 패턴에 맞게 수정
    
    * refactor: DataSourceUtils에서 자동 바인딩 제거
    
    * refactor: TxUserService에서 Connection 명시적 바인딩
    
    * refactor: Connection Pool 재사용을 위해 autoCommit 상태 복원
    
    * refactor: unbind resource
    
    * refactor: change originalAutoCommit init

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,71 @@
+package com.techcourse.service;
+
+import com.techcourse.domain.User;
+import com.interface21.dao.DataAccessException;
+import com.interface21.jdbc.datasource.DataSourceUtils;
+import com.interface21.transaction.support.TransactionSynchronizationManager;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TxUserService implements UserService {
+
+    private final UserService userService;
+    private final DataSource dataSource;
+
+    public TxUserService(final UserService userService, final DataSource dataSource) {
+        this.userService = userService;
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public User findById(final long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void insert(final User user) {
+        userService.insert(user);
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createdBy) {
+        final Connection connection = DataSourceUtils.getConnection(dataSource);
+        TransactionSynchronizationManager.bindResource(dataSource, connection);
+
+        boolean originalAutoCommit = true;
+        try {
+            originalAutoCommit = connection.getAutoCommit();
+            connection.setAutoCommit(false);
+
+            userService.changePassword(id, newPassword, createdBy);
+
+            connection.commit();
+        } catch (Exception e) {
+            try {
+                connection.rollback();
+            } catch (SQLException rollbackException) {
+                throw new DataAccessException("Failed to rollback transaction", rollbackException);
+            }
+            throw new DataAccessException(e);
+        } finally {
+            try {
+                connection.setAutoCommit(originalAutoCommit);
+            } catch (SQLException e) {
+                throw new DataAccessException("Failed to reset autoCommit", e);
+            }
+            try {
+                TransactionSynchronizationManager.unbindResource(dataSource);
+            } catch (IllegalStateException e) {
+                throw new DataAccessException("Failed to unbind resource", e);
+            }
+            try {
+                DataSourceUtils.releaseConnection(connection, dataSource);
+            } catch (Exception e) {
+                throw new DataAccessException("Failed to release connection", e);
+            }
+        }
+    }
+
+}
