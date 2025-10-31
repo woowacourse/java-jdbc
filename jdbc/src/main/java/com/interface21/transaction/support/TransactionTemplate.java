f9583032320d4f8b9b1ca76bd01880ec5db540d6
YuyoungRhee <bbclara@naver.com>
2025-10-31 15:14:03 +0900

    refactor: 예외 덮어쓰기 오류 수정

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionTemplate.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionTemplate.java
@@ -1,4 +1,3 @@
-// jdbc/src/main/java/com/interface21/transaction/support/TransactionTemplate.java
 package com.interface21.transaction.support;
 
 import com.interface21.dao.DataAccessException;
@@ -16,6 +15,9 @@ public class TransactionTemplate {
 
     public void execute(final TransactionalWork work) {
         Connection connection = null;
+
+        DataAccessException originalException = null;
+
         try {
             connection = dataSource.getConnection();
             connection.setAutoCommit(false);
@@ -24,14 +26,18 @@ public class TransactionTemplate {
 
             try {
                 work.execute();
-
                 connection.commit();
             } catch (Exception e) {
                 connection.rollback();
-                throw new DataAccessException("트랜잭션 수행 중 오류가 발생하여 롤백합니다.", e);
+
+                if (e instanceof DataAccessException) {
+                    originalException = (DataAccessException) e;
+                } else {
+                    originalException = new DataAccessException("트랜잭션 수행 중 오류가 발생하여 롤백합니다.", e);
+                }
             }
         } catch (SQLException e) {
-            throw new DataAccessException(e);
+            originalException = new DataAccessException("커넥션 설정 중 오류가 발생했습니다.", e);
         } finally {
             try {
                 TransactionSynchronizationManager.unbindResource(dataSource);
@@ -40,8 +46,16 @@ public class TransactionTemplate {
                     connection.close();
                 }
             } catch (SQLException e) {
-                throw new DataAccessException("Connection 닫기 실패", e);
+                if (originalException == null) {
+                    originalException = new DataAccessException("Connection 닫기 실패", e);
+                } else {
+                    originalException.addSuppressed(e);
+                }
             }
+        }
+
+        if (originalException != null) {
+            throw originalException;
         }
     }
 }
