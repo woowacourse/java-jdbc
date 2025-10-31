fce12ad6a98b6f2508727559cb7d99d4002b798d
YuyoungRhee <bbclara@naver.com>
2025-10-31 11:28:39 +0900

    feat: TransactionSynchronizationManager 및 DataSourceUtils 구현 및 수정

--- jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
+++ jdbc/src/main/java/com/interface21/transaction/support/TransactionSynchronizationManager.java
@@ -1,23 +1,36 @@
 package com.interface21.transaction.support;
 
-import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import javax.sql.DataSource;
 
 public abstract class TransactionSynchronizationManager {
 
-    private static final ThreadLocal<Map<DataSource, Connection>> resources = new ThreadLocal<>();
+    private static final ThreadLocal<Map<DataSource, Connection>> resources =
+            ThreadLocal.withInitial(HashMap::new);
 
     private TransactionSynchronizationManager() {}
 
+    public static Map<DataSource, Connection> getResources() {
+        return resources.get();
+    }
+
+    public static boolean hasResource(DataSource key) {
+        return getResources().containsKey(key);
+    }
+
     public static Connection getResource(DataSource key) {
-        return null;
+        return getResources().get(key);
     }
 
     public static void bindResource(DataSource key, Connection value) {
+        Objects.requireNonNull(value, "Connection must not be null");
+        getResources().put(key, value);
     }
 
     public static Connection unbindResource(DataSource key) {
-        return null;
+        return getResources().remove(key);
     }
 }
