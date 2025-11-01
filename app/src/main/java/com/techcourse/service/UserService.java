a93d1b0ffae10362a6bfdfa29160eaaa61aacfdd
Wooyoung <quantumjump10@gmail.com>
2025-11-01 13:09:54 +0900

    refactor: DataSource 의존성 주입 및 트랜잭션 동기화 적용
    - DataSourceConfig 정적 호출 제거
    - 생성자 주입으로 DataSource 관리
    - DataSourceUtils로 ThreadLocal 기반 트랜잭션 동기화

--- app/src/main/java/com/techcourse/service/UserService.java
+++ app/src/main/java/com/techcourse/service/UserService.java
@@ -1,7 +1,7 @@
 package com.techcourse.service;
 
 import com.interface21.dao.DataAccessException;
-import com.techcourse.config.DataSourceConfig;
+import com.interface21.jdbc.datasource.DataSourceUtils;
 import com.techcourse.dao.UserDao;
 import com.techcourse.dao.UserHistoryDao;
 import com.techcourse.domain.User;
@@ -9,6 +9,7 @@ import com.techcourse.domain.UserHistory;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Optional;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,10 +18,12 @@ public class UserService {
     private static final Logger log = LoggerFactory.getLogger(UserService.class);
     private final UserDao userDao;
     private final UserHistoryDao userHistoryDao;
+    private final DataSource dataSource;
 
-    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao) {
+    public UserService(final UserDao userDao, final UserHistoryDao userHistoryDao, final DataSource dataSource) {
         this.userDao = userDao;
         this.userHistoryDao = userHistoryDao;
+        this.dataSource = dataSource;
     }
 
     public Optional<User> findById(final long id) {
@@ -34,7 +37,7 @@ public class UserService {
     public void changePassword(final long id, final String newPassword, final String createBy) {
         final var user = findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 id의 유저가 존재하지 않습니다" + id));
-        try (final Connection conn = DataSourceConfig.getInstance().getConnection()) {
+        try (final Connection conn = DataSourceUtils.getConnection(dataSource)) {
             try {
                 conn.setAutoCommit(false);
 
