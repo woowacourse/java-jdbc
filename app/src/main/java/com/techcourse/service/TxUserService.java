b500a69fb4425e3d5da86337cd4047910e21f0db
Cheol Won <76567238+Ryan-Dia@users.noreply.github.com>
2025-11-03 13:34:36 +0900

    [4단계 - Transaction synchronization 적용하기] 새로이(강철원) 미션 제출합니다. (#1250)
    * feat: TransactionManager 안정성 및 예외 처리 개선
    
    * feat: userService 트랜잭션 사용과 미사용을 기준으로 분리
    
    * feat: changePassword 메서드 구현
    
    * feat: TxUserService에 트랜잭션 구현 및 Connection 전파 로직 추가
    
    * feat: TransactionSynchronizationManager 메서드 구현
    
    * refactor: Connection 파라미터 전달 방식 제거 및 ThreadLocal 기반 자동 전파로 개선

---
+++ app/src/main/java/com/techcourse/service/TxUserService.java
@@ -0,0 +1,33 @@
+package com.techcourse.service;
+
+import com.interface21.transaction.TransactionManager;
+import com.techcourse.domain.User;
+import javax.sql.DataSource;
+
+public class TxUserService implements UserService {
+
+    private final UserService userService;
+    private final TransactionManager transactionManager;
+
+    public TxUserService(UserService userService, DataSource dataSource) {
+        this.userService = userService;
+        this.transactionManager = new TransactionManager(dataSource);
+    }
+
+    @Override
+    public User findById(long id) {
+        return userService.findById(id);
+    }
+
+    @Override
+    public void save(User user) {
+        userService.save(user);
+    }
+
+    @Override
+    public void changePassword(final long id, final String newPassword, final String createdBy) {
+        transactionManager.executeTransaction(connection -> {
+            userService.changePassword(id, newPassword, createdBy);
+        });
+    }
+}
